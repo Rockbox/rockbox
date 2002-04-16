41e6d5f98ab1bfd87be4424003aa94397748cc42
Alan Korr <alkorr@rockbox.org>
2002-04-16 18:49:03 +0000

    module fat and templates : just added templates files.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@108 a1c6a512-1295-4272-9138-f99709370657

---
+++ firmware/test/fat/fat.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 by Alan Korr
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#ifndef __LIBRARY_FAT_H__
+#  define __LIBRARY_FAT_H__
+#  include <fat/config.h>
+#  include <fat/defines.h>
+#  include <fat/types.h>
+#  include <fat/return_values.h>
+#  include <fat/inlines.h>
+#  include <fat/functions.h>
+#endif
---
+++ firmware/test/fat/return_values.h
@@ -0,0 +1,24 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id:
+ *
+ * Copyright (C) 2002 by Alan Korr
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#ifndef __LIBRARY_FAT_H__
+#  error "This header file must be included ONLY from fat.h."
+#endif
+#ifndef __LIBRARY_FAT_RETURN_VALUES_H__
+#  define __LIBRARY_FAT_RETURN_VALUES_H__
+#endif
