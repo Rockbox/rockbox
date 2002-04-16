efca1cd39a29afa32a00748e432513c9eec066aa
Daniel Stenberg <daniel@haxx.se>
2002-04-16 13:34:35 +0000

    added keypad defines
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@100 a1c6a512-1295-4272-9138-f99709370657

--- firmware/config-playerold.h
+++ firmware/config-playerold.h
@@ -1,3 +1,5 @@
 /* define this if you have a charcell LCD display */
 #define HAVE_LCD_CHARCELLS 1
 
+/* define this if you have the Player's keyboard */
+#define HAVE_PLAYER_KEYPAD 1
--- firmware/config-recorder.h
+++ firmware/config-recorder.h
@@ -3,3 +3,6 @@
 
 /* define this if you have a bitmap LCD display */
 #define HAVE_LCD_BITMAP 1
+
+/* define this if you have the Recorder's 10-key keyboard */
+#define HAVE_RECORDER_KEYPAD 1
