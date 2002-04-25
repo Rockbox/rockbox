b3674e25123bdb083eac3113c6dd40bca26352dc
Björn Stenberg <bjorn@haxx.se>
2002-04-25 15:09:32 +0000

    Added HAVE_NEW_CHARCELL_LCD
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@237 a1c6a512-1295-4272-9138-f99709370657

--- firmware/config-player.h
+++ firmware/config-player.h
@@ -3,3 +3,6 @@
 
 /* define this if you have the Player's keyboard */
 #define HAVE_PLAYER_KEYPAD 1
+
+/* define this if you have a post-4.50 charcell LCD display */
+#define HAVE_NEW_CHARCELL_LCD 1
