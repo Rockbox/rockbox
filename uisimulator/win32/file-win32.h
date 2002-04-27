ae43fcaa497ab1fb9f00f3553bc2837c8988dc3d
Felix Arends <edx@rockbox.org>
2002-04-27 17:11:23 +0000

    file header
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@264 a1c6a512-1295-4272-9138-f99709370657

---
+++ uisimulator/win32/file-win32.h
@@ -0,0 +1,37 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 by Felix Arends
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#ifndef __FILE_WIN32_H__
+#define __FILE_WIN32_H__
+
+#include <io.h>
+
+typedef unsigned short ino_t;
+typedef long off_t;
+typedef _finddata_t DIR;
+
+struct dirent
+{
+  long __d_reserved[4];
+  ino_t d_ino; /* Just for compatibility, it's junk */
+  char d_name[256];		/* FIXME: use NAME_MAX? */
+};
+
+
+#endif // #ifndef __FILE_WIN32_H__
