1ba5d1f6b646bad8ca56801bb2458202693717b7
Linus Nielsen Feltzing <linus@haxx.se>
2002-05-03 13:13:21 +0000

    Doubled the buffer size
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@409 a1c6a512-1295-4272-9138-f99709370657

--- firmware/test/i2c/gendata.c
+++ firmware/test/i2c/gendata.c
@@ -4,21 +4,21 @@ int main(int argc, char *argv[])
 {
     FILE *f;
     int i;
-    unsigned char buf[64000];
+    unsigned char buf[128000];
 
     f = fopen("mp.mp3", "r");
     
     if(f)
     {
-	if(fread(buf, 1, 64000, f) < 64000)
+	if(fread(buf, 1, 128000, f) < 128000)
 	{
 	    fprintf(stderr, "FAN!\n");
 	    exit(1);
 	}
 
-	printf("int mp3datalen = 64000;\n");
-	printf("unsigned char mp3data[64000] =\n{");
-	for(i = 0;i < 64000;i++)
+	printf("int mp3datalen = 128000;\n");
+	printf("unsigned char mp3data[128000] =\n{");
+	for(i = 0;i < 128000;i++)
 	{
 	    if(i % 8 == 0)
 	    {
