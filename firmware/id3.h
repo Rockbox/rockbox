fa1bce237a707c9447e0a660a4ea7eb22521c510
Daniel Stenberg <daniel@haxx.se>
2002-05-05 10:31:21 +0000

    proto and struct for id3-info
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@430 a1c6a512-1295-4272-9138-f99709370657

---
+++ firmware/id3.h
@@ -0,0 +1,34 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 by Daniel Stenberg
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+struct mp3entry {
+    char *path;
+    char *title;
+    char *artist;
+    char *album;
+    int bitrate;
+    int frequency;
+    int id3v2len;
+    int id3v1len;
+    int filesize; /* in bytes */
+    int length;   /* song length */
+};
+
+typedef struct mp3entry mp3entry;
+
+bool mp3info(mp3entry *entry, char *filename);
