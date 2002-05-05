91f165e3daa0957aa27f3439ca60eede128e441e
Daniel Stenberg <daniel@haxx.se>
2002-05-05 10:28:23 +0000

    support open() properly so that the root dir works in the archos subdir
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@425 a1c6a512-1295-4272-9138-f99709370657

--- uisimulator/x11/io.c
+++ uisimulator/x11/io.c
@@ -13,3 +13,16 @@ DIR *x11_opendir(char *name)
   }
   return opendir(name);
 }
+
+int x11_open(char *name, int opts)
+{
+  char buffer[256]; /* sufficiently big */
+
+  if(name[0] == '/') {
+    sprintf(buffer, "%s/%s", SIMULATOR_ARCHOS_ROOT, name);
+
+    Logf("We open the real file '%s'", buffer);
+    return open(buffer, opts);
+  }
+  return open(name, opts);
+}
