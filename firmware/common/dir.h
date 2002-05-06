27a153e421269320bbeed359e538907be3ca4b60
Björn Stenberg <bjorn@haxx.se>
2002-05-06 14:19:10 +0000

    Added atributes
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@477 a1c6a512-1295-4272-9138-f99709370657

--- firmware/common/dir.h
+++ firmware/common/dir.h
@@ -21,6 +21,13 @@
 
 #ifndef DIRENT_DEFINED
 
+#define ATTR_READ_ONLY   0x01
+#define ATTR_HIDDEN      0x02
+#define ATTR_SYSTEM      0x04
+#define ATTR_VOLUME_ID   0x08
+#define ATTR_DIRECTORY   0x10
+#define ATTR_ARCHIVE     0x20
+
 struct dirent {
     unsigned char d_name[256];
     int attribute;
