3d05c6301a991152f9dacccfa6673794557e14d8
Robert Hak <adiamas@rockbox.org>
2002-05-08 08:26:59 +0000

    creation
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@507 a1c6a512-1295-4272-9138-f99709370657

---
+++ uisimulator/icons.c
@@ -0,0 +1,32 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 Robert E. Hak
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#include <lcd.h>
+
+#include "icons.h"
+
+#ifdef HAVE_LCD_BITMAP
+
+unsigned char bitmap_icons_6x8[LastIcon][6] =
+{
+    { 0x60, 0x7f, 0x03, 0x63, 0x7f, 0x00 },
+    { 0x3e, 0x26, 0x26, 0x24, 0x3c, 0x00 }
+};
+
+
+#endif
---
+++ uisimulator/icons.h
@@ -0,0 +1,35 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 Robert E. Hak
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#include <lcd.h>
+
+/* 
+ * Icons of size 6x8 pixels 
+ */
+
+#ifdef HAVE_LCD_BITMAP
+
+enum icons_6x8 {
+	FileIcon, DirIcon, LastIcon
+};
+
+extern unsigned char bitmap_icons_6x8[LastIcon][6];
+extern icons_6x8;
+
+#endif /* End HAVE_LCD_BITMAP */
+
