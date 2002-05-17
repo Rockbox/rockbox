e324db6e1e650c5ae4c7a3cf17861ccc644e3723
Björn Stenberg <bjorn@haxx.se>
2002-05-17 19:50:37 +0000

    Added init()
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@626 a1c6a512-1295-4272-9138-f99709370657

--- apps/main.c
+++ apps/main.c
@@ -16,10 +16,50 @@
  * KIND, either express or implied.
  *
  ****************************************************************************/
+#include "ata.h"
+#include "disk.h"
+#include "fat.h"
+#include "lcd.h"
+#include "debug.h"
+#include "led.h"
+#include "kernel.h"
+#include "button.h"
 #include "tree.h"
+
+int init(void)
+{
+    debug_init();
+    kernel_init();
+    set_irq_level(0);
+
+    if(ata_init()) {
+        DEBUGF("*** Warning! The disk is uninitialized\n");
+    }
+    DEBUGF("ATA initialized\n");
+
+    if (disk_init()) {
+        DEBUGF("*** Failed reading partitions\n");
+        return -1;
+    }
+
+    if(fat_mount(part[0].start)) {
+        DEBUGF("*** Failed mounting fat\n");
+    }
+
+    button_init();
+
+    return 0;
+}
 
 int main(void)
 {
+    init();
+
     browse_root();
+
+    while(1) {
+        led(true); sleep(HZ/10);
+        led(false); sleep(HZ/10);
+    }
     return 0;
 }
