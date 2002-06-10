604cce76cffcad3e890084f90b1199a0ce46e176
Daniel Stenberg <daniel@haxx.se>
2002-06-10 10:17:54 +0000

    add thread functionality, powered by pthreads
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@928 a1c6a512-1295-4272-9138-f99709370657

---
+++ uisimulator/x11/thread.c
@@ -0,0 +1,69 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 Daniel Stenberg
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include <stdio.h>
+#include <pthread.h>
+
+/*
+ * We emulate the target threads by using pthreads. We have a mutex that only
+ * allows one thread at a time to execute. It forces each thread to yield()
+ * for the other(s) to run.
+ */
+
+pthread_mutex_t mp;
+
+void init_threads(void)
+{  
+  pthread_mutex_init(&mp, NULL);
+}
+/* 
+   int pthread_create(pthread_t *new_thread_ID,
+   const pthread_attr_t *attr,
+   void * (*start_func)(void *), void *arg);
+*/
+
+void yield(void)
+{
+  pthread_mutex_unlock(&mp); /* return */
+  pthread_mutex_lock(&mp); /* get it again */
+}
+
+int create_thread(void* fp, void* sp, int stk_size)
+{
+  pthread_t tid;
+  int i;
+  int error;
+
+  /* we really don't care about these arguments */
+  (void)sp;
+  (void)stk_size;
+  error = pthread_create(&tid,
+                         NULL, /* default attributes please */
+                         fp,   /* function to start */
+                         NULL  /* start argument */);
+  if(0 != error)
+    fprintf(stderr, "Couldn't run thread number %d, errno %d\n", i, error);
+  else 
+    fprintf(stderr, "Thread %d is running\n", tid);
+
+  /* get mutex to only allow one thread running at a time */
+  pthread_mutex_lock(&mp);
+
+  return error;
+}
