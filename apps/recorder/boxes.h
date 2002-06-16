4990c7511a48fec81ca2b438b318f6056b023fa6
Robert Hak <adiamas@rockbox.org>
2002-06-16 23:21:57 +0000

    renamed screensaver.[ch] to boxes
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@1019 a1c6a512-1295-4272-9138-f99709370657

---
+++ apps/recorder/boxes.h
@@ -0,0 +1,26 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 Robert E. Hak
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#ifndef __BOXES_H__
+#define __BOXE_H__
+
+void boxes(void);
+
+#endif /* __BOXES_H__ */
+
