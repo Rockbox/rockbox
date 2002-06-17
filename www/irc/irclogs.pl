c0b4dde4e1b2cff4253b3656f74512039a2b5237
Björn Stenberg <bjorn@haxx.se>
2002-06-17 15:45:32 +0000

    First
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@1050 a1c6a512-1295-4272-9138-f99709370657

---
+++ www/irc/irclogs.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+require "/home/dast/perl/date.pm";
+
+opendir(DIR, ".") or
+    die "Can't opendir()";
+@logs = sort grep { /^rockbox-/ } readdir(DIR);
+closedir DIR;
+
+print "<table><tr valign=top>\n";
+
+$lasty = 0;
+$lastm = 0;
+
+for ( @logs ) {
+    $size = (stat("$_"))[7];
+    $file = $_;
+    $log = "";
+    if (/-(\d+)/) {
+        if ( $1 =~ /(\d\d\d\d)(\d\d)(\d\d)/ ) {
+            $y = $1;
+            $m = $2;
+            $d = $3;
+            $mname = ucfirst MonthNameEng($m);
+            if ( $m != $lastm ) {
+                print "</ul></td>\n" if $lastm != 0;
+                print "<td align=\"right\"><b>$mname $y</b>\n";
+                print "<ul>\n";
+                $lastm = $m;
+            }
+            print "<li><a test href=\"$file\">$mname $d</a>\n";
+        }
+    }
+    #print "<li><a href=\"daily/$_\">$_</a> ($size bytes) $log\n";
+}
+print "</ul></td></tr></table>\n";
