f888c252ed548b6f1363781df81542767d1a121f
Felix Arends <edx@rockbox.org>
2002-06-28 14:57:03 +0000

    win32 simulator up-to-date
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@1249 a1c6a512-1295-4272-9138-f99709370657

--- uisimulator/win32/mpeg.c
+++ uisimulator/win32/mpeg.c
@@ -21,6 +21,67 @@
    the target */
 
 #include "debug.h"
+#include "mpeg.h"
+
+static char *units[] =
+{
+    "%",    /* Volume */
+    "%",    /* Bass */
+    "%"     /* Treble */
+};
+
+static int numdecimals[] =
+{
+    0,    /* Volume */
+    0,    /* Bass */
+    0     /* Treble */
+};
+
+static int minval[] =
+{
+    0,    /* Volume */
+    0,    /* Bass */
+    0     /* Treble */
+};
+
+static int maxval[] =
+{
+    50,    /* Volume */
+    50,    /* Bass */
+    50     /* Treble */
+};
+
+static int defaultval[] =
+{
+    70/2,    /* Volume */
+    50/2,    /* Bass */
+    50/2     /* Treble */
+};
+
+char *mpeg_sound_unit(int setting)
+{
+    return units[setting];
+}
+
+int mpeg_sound_numdecimals(int setting)
+{
+    return numdecimals[setting];
+}
+
+int mpeg_sound_min(int setting)
+{
+    return minval[setting];
+}
+
+int mpeg_sound_max(int setting)
+{
+    return maxval[setting];
+}
+
+int mpeg_sound_default(int setting)
+{
+    return defaultval[setting];
+}
 
 void mpeg_volume(void)
 {
@@ -59,11 +120,36 @@ struct mp3entry* mpeg_current_track(void)
     return 0;
 }
 
+void mpeg_sound_set(int setting, int value)
+{
+}
+
 #ifndef MPEGPLAY
 void mpeg_play(char *tune)
 {
   DEBUGF("We instruct the MPEG thread to play %s for us\n",
          tune);
+}
+
+int mpeg_val2phys(int setting, int value)
+{
+    int result = 0;
+    
+    switch(setting)
+    {
+        case SOUND_VOLUME:
+            result = value * 2;
+            break;
+        
+        case SOUND_BASS:
+            result = value * 2;
+            break;
+        
+        case SOUND_TREBLE:
+            result = value * 2;
+            break;
+    }
+    return result;
 }
 
 #endif
