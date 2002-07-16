42b764553281b7049cbcb9009e29cb6d2bc5a1e7
Björn Stenberg <bjorn@haxx.se>
2002-07-16 12:08:57 +0000

    Only one pixel clearing necessary
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@1369 a1c6a512-1295-4272-9138-f99709370657

--- firmware/drivers/lcd.c
+++ firmware/drivers/lcd.c
@@ -555,7 +555,7 @@ void lcd_putspropxy(int x, int y, unsigned char *str, int thisfont)
             break;
 
         src = char_dw_8x8_prop[ch];
-        lcd_clearrect (lcd_x, lcd_y, nx+1, ny);
+        lcd_clearrect (lcd_x+nx, lcd_y, 1, ny);
         lcd_bitmap (src, lcd_x, lcd_y, nx, ny, true);
 
         lcd_x += nx+1;
