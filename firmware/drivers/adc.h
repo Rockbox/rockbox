416870d781d84ea61e9c1ca7b86f2008dacd1151
Linus Nielsen Feltzing <linus@haxx.se>
2002-07-29 12:56:22 +0000

    Commented the ADC channels
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@1490 a1c6a512-1295-4272-9138-f99709370657

--- firmware/drivers/adc.h
+++ firmware/drivers/adc.h
@@ -21,14 +21,18 @@
 
 #define NUM_ADC_CHANNELS 8
 
-#define ADC_BATTERY             0
-#define ADC_CHARGE_REGULATOR    1
-#define ADC_USB_POWER           2
+#define ADC_BATTERY             0 /* Battery voltage always reads 0x3FF due to
+                                     silly scaling */
+#define ADC_CHARGE_REGULATOR    1 /* Regulator reference voltage, should read
+                                     about 0x1c0 when charging, else 0x3FF */
+#define ADC_USB_POWER           2 /* USB, reads 0x3FF when USB is inserted */
 
-#define ADC_BUTTON_ROW1         4
-#define ADC_BUTTON_ROW2         5
-#define ADC_UNREG_POWER         6
-#define ADC_EXT_POWER           7
+#define ADC_BUTTON_ROW1         4 /* Used for scanning the keys, different
+                                     voltages for different keys */
+#define ADC_BUTTON_ROW2         5 /* Used for scanning the keys, different
+                                     voltages for different keys */
+#define ADC_UNREG_POWER         6 /* Battery voltage with a better scaling */
+#define ADC_EXT_POWER           7 /* The external power voltage, V=X*0.0148 */
 
 #ifdef ARCHOS_RECORDER
 #define BATTERY_SCALE_FACTOR 6465
