2ec1e170ab8b47855f2cc201de4ac0c29c1d3e09
Daniel Stenberg <daniel@haxx.se>
2002-08-02 12:33:48 +0000

    just defensivelt check for defined defines before we define them
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@1524 a1c6a512-1295-4272-9138-f99709370657

--- firmware/common/file.h
+++ firmware/common/file.h
@@ -22,9 +22,15 @@
 
 #define MAX_PATH 260
 
+#ifndef SEEK_SET
 #define SEEK_SET 0
+#endif
+#ifndef SEEK_CUR
 #define SEEK_CUR 1
+#endif
+#ifndef SEEK_END
 #define SEEK_END 2
+#endif
 
 #ifndef O_RDONLY
 #define O_RDONLY 0
