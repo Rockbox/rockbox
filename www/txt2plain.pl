4d52ba710a622a5f561fbad0a47e08bfba28f33d
Daniel Stenberg <daniel@haxx.se>
2002-08-19 15:01:42 +0000

    convert the faq more
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@1807 a1c6a512-1295-4272-9138-f99709370657

--- www/txt2plain.pl
+++ www/txt2plain.pl
@@ -1,15 +1,34 @@
 #!/usr/bin/perl
 
+# this is really a faq2html and should only be used for this purpose
+
 while(<STDIN>) {
 
     $_ =~ s/\</&lt;/g;
     $_ =~ s/\>/&gt;/g;
 
-    $_ =~ s/(http:\/\/([a-zA-Z0-9_.\/-]*)[^).])/\<a href=\"$1\"\>$1\<\/a\>/g;
+    $_ =~ s/(http:\/\/([a-zA-Z0-9_.\/-]*)[^\).])/\<a href=\"$1\"\>$1\<\/a\>/g;
 
     $_ =~ s/^$/\&nbsp;/g;   # empty lines are nbsp
     $_ =~ s/(\\|\/)$/$1&nbsp;/g; # clobber backslash on end of line
 
+
+    # detect and mark Q-sections
+    if( $_ =~ /^Q(\d*)/) {
+        print "</pre>\n<a name=\"$1\"></a><p class=\"faqq\">$_";
+        my $line;
+        while(<STDIN>) {
+            $line = $_;
+            if($_ !~ /^A/) {
+                print "$_";
+            }
+            else {
+                last;
+            }
+        }
+        print "</p>\n<pre class=\"faqa\">\n$line";
+        next;
+    }
 
     print $_;
 }
