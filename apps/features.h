d851531d0817858f86fccf000edf50e1bb1ee3df
Daniel Stenberg <daniel@haxx.se>
2002-09-06 06:41:02 +0000

    meant to control which app features to build-in
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@2201 a1c6a512-1295-4272-9138-f99709370657

---
+++ apps/features.h
@@ -0,0 +1,31 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 Daniel Stenberg
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#ifndef __FEATURES_H__
+#define __FEATURES_H__
+
+#ifndef DISABLE_SCREENSAVERS
+#define USE_SCREENSAVERS
+#endif
+
+#ifndef DISABLE_GAMES
+#define USE_GAMES
+#endif
+
+#endif /* End __FEATURES_H__ */
