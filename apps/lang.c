df322a6c302cfcf8ae8992a3fcf53ba3a871a306
Daniel Stenberg <daniel@haxx.se>
2002-09-17 07:20:05 +0000

    regenerated with the new genlang
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@2311 a1c6a512-1295-4272-9138-f99709370657

---
+++ apps/lang.c
@@ -0,0 +1,9 @@
+/* This file was automaticly generated using genlang, the strings come
+   from "lang/english.lang" */
+
+unsigned char *language_strings[]={
+   "Sound Settings",
+   "General Settings",
+   "Games",
+};
+/* end of generated string list */
--- apps/lang.h
+++ apps/lang.h
@@ -1,10 +1,17 @@
-/* This file was automaticly generated using genlan */
+/* This file was automaticly generated using genlang */
 /*
  * The str() macro/functions is how to access strings that might be
  * translated. Use it like str(MACRO) and except a string to be
  * returned!
  */
-#define str(x) x
-#define LANG_SOUND_SETTINGS "Sound Settings"
-#define LANG_GENERAL_SETTINGS "General Settings"
-#define LANG_GAMES "Games"
+#define str(x) language_strings[x]
+
+/* this is the array with all the strings */
+extern unsigned char *language_strings[];
+
+enum {
+ LANG_SOUND_SETTINGS,
+ LANG_GENERAL_SETTINGS,
+ LANG_GAMES,
+};
+/* end of generated enum list */
