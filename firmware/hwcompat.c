3148bc45b0ab0d72d9dabe54d84648554f96015b
Linus Nielsen Feltzing <linus@haxx.se>
2002-10-15 11:10:49 +0000

    Hardware compatibility functions
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@2650 a1c6a512-1295-4272-9138-f99709370657

---
+++ firmware/hwcompat.c
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 by Linus Nielsen Feltzing
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#include <stdbool.h>
+#include "config.h"
+#include "debug.h"
+
+int read_rom_version(void)
+{
+    int ver = *(short *)0x020000fe;
+
+    return ver;
+}
+
+int read_hw_mask(void)
+{
+#ifdef ARCHOS_PLAYER
+    int mask = 0; /* Fake value for simplicity */
+#else
+    int mask = *(short *)0x020000fc;
+#endif
+    
+    return mask;
+}
