b4ec73daaaa1b9abf3585fd07be71ff3624b13f3
Daniel Stenberg <daniel@haxx.se>
2002-10-15 07:22:43 +0000

    Added cube to the demo menu.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@2631 a1c6a512-1295-4272-9138-f99709370657

--- apps/demo_menu.c
+++ apps/demo_menu.c
@@ -1,21 +1,21 @@
 /***************************************************************************
- *             __________               __   ___.
- *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
- *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
- *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
- *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
- *                     \/            \/     \/    \/            \/
- * $Id$
- *
- * Copyright (C) 2002 Robert Hak
- *
- * All files in this archive are subject to the GNU General Public License.
- * See the file COPYING in the source tree root for full license agreement.
- *
- * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
- * KIND, either express or implied.
- *
- ****************************************************************************/
+*             __________               __   ___.
+*   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+*   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+*   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+*   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+*                     \/            \/     \/    \/            \/
+* $Id$
+*
+* Copyright (C) 2002 Robert Hak
+*
+* All files in this archive are subject to the GNU General Public License.
+* See the file COPYING in the source tree root for full license agreement.
+*
+* This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+* KIND, either express or implied.
+*
+****************************************************************************/
 #include "config.h"
 #include "options.h"
 
@@ -35,6 +35,7 @@
 
 extern bool bounce(void);
 extern bool snow(void);
+extern bool cube(void);
 extern bool oscillograph(void);
 
 bool demo_menu(void)
@@ -48,6 +49,7 @@ bool demo_menu(void)
 #ifndef SIMULATOR
         { "Oscillograph", oscillograph },
 #endif
+        { str(LANG_CUBE), cube },
     };
 
     m=menu_init( items, sizeof items / sizeof(struct menu_items) );
@@ -59,3 +61,4 @@ bool demo_menu(void)
 
 #endif
 #endif
+
