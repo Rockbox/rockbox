f5aa13a88f75b4f0e1ed2a02ca32f280aebb0984
Björn Stenberg <bjorn@haxx.se>
2003-01-11 22:39:25 +0000

    Increased CONFIG_BLOCK_VERSION to indicate modified config block format.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@3072 a1c6a512-1295-4272-9138-f99709370657

--- apps/settings.c
+++ apps/settings.c
@@ -56,7 +56,7 @@
 struct user_settings global_settings;
 char rockboxdir[] = ROCKBOX_DIR;       /* config/font/data file directory */
 
-#define CONFIG_BLOCK_VERSION 3
+#define CONFIG_BLOCK_VERSION 4
 #define CONFIG_BLOCK_SIZE 512
 #define RTC_BLOCK_SIZE 44
 
