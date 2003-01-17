8c5e3f833b8d0f92c46679077acd755ed6075e05
Daniel Stenberg <daniel@haxx.se>
2003-01-17 16:50:36 +0000

    added stub functions for the rename
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@3126 a1c6a512-1295-4272-9138-f99709370657

---
+++ uisimulator/win32/file.h
@@ -0,0 +1,28 @@
+/***************************************************************************
+ *             __________               __   ___.                  
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___  
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /  
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <   
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \  
+ *                     \/            \/     \/    \/            \/ 
+ * $Id$
+ *
+ * Copyright (C) 2002 by Daniel Stenberg <daniel@haxx.se>
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+int win32_rename(char *oldpath, char *newpath);
+
+#define rename(x,y) win32_rename(x,y)
+
+#include "../../firmware/common/file.h"
+
---
+++ uisimulator/win32/io.c
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 Daniel Stenberg
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include "file.h"
+#include "debug.h"
+
+#define SIMULATOR_ARCHOS_ROOT "archos"
+
+int win32_rename(char *oldpath, char* newpath)
+{
+  char buffer1[256];
+  char buffer2[256];
+
+  if(oldpath[0] == '/') {
+      sprintf(buffer1, "%s%s", SIMULATOR_ARCHOS_ROOT, oldpath);
+      sprintf(buffer2, "%s%s", SIMULATOR_ARCHOS_ROOT, newpath);
+
+      debugf("We rename the real file '%s' to '%s'\n", buffer1, buffer2);
+      return rename(buffer1, buffer2);
+  }
+  return -1;
+}
