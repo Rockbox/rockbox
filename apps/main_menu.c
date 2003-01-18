bc8029ee59ff6aeb55d9f90cd943c3f331c69301
Uwe Freese <thebreaker@rockbox.org>
2003-01-18 18:21:13 +0000

    avoid mem info overflow when having 8MB ;-)
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@3128 a1c6a512-1295-4272-9138-f99709370657

--- apps/main_menu.c
+++ apps/main_menu.c
@@ -156,7 +156,8 @@ extern unsigned char mp3end[];
 bool show_info(void)
 {
     char s[32];
-    int buflen = ((mp3end - mp3buf) * 1000) / 0x100000;
+    /* avoid overflow for 8MB mod :) was: ((mp3end - mp3buf) * 1000) / 0x100000; */
+    int buflen = ((mp3end - mp3buf) * 100) / 0x19999;
     int integer, decimal;
     bool done = false;
     int key;
