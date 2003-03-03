5522cdf13c74ebefa60fa11308ce44921c6b35d9
Robert Hak <adiamas@rockbox.org>
2003-03-03 09:26:51 +0000

    correction for irc logs now spanning years
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@3368 a1c6a512-1295-4272-9138-f99709370657

--- www/irc/irclogs.pl
+++ www/irc/irclogs.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-require "/home/dast/perl/date.pm";
+#require "/home/dast/perl/date.pm";
 
 opendir(DIR, ".") or
     die "Can't opendir()";
@@ -17,30 +17,36 @@ for ( @logs ) {
     $size = (stat("$_"))[7];
     $file = $_;
     $log = "";
+
     if (/-(\d+)/) {
-        if ( $1 =~ /(\d\d\d\d)(\d\d)(\d\d)/ ) {
+       if ( $1 =~ /(\d\d\d\d)(\d\d)(\d\d)/ ) {
             $y = $1;
             $m = $2;
             $d = $3;
-            $mname = ucfirst MonthNameEng($m);
+
+$mname = $m;
+#            $mname = ucfirst MonthNameEng($m);
+            if ($y != $lasty) {
+                print "</tr><tr>\n" if $lasty != 0;
+                print "<th>$y</th>\n";
+                $lasty = $y;
+            } else {
+                print "</tr><tr>\n<th></th>" if ( $m != $lastm );
+            }
+
             if ( $m != $lastm ) {
                 $count=0;
-                print "</tr><tr>\n" if $lastm != 0;
-#                if ( $m % 6 == 0 ) {
-#                    print "</tr><tr valign=top>\n";
-#                }
                 print "<th>$mname</th>\n";
                 $lastm = $m;
             }
-#            $lines = `wc -l $file` + 0;
-#            print "<li><a test href=\"$file\">$mname $d</a> <small>($lines lines)</small>\n";
+
             print "<td><a test href=\"$file\">$d</a></td>\n";
+
             if ( ++$count > 15 ) {
-                print "</tr><tr><th></th>\n";
+                print "</tr><tr><th></th><th></th>\n";
                 $count=0;
             }
         }
     }
-    #print "<li><a href=\"daily/$_\">$_</a> ($size bytes) $log\n";
 }
 print "</ul></td></tr></table>\n";
