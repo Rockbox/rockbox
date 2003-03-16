2955a2ead367c14169a74cfc2557d0ceb2bd334f
Daniel Stenberg <daniel@haxx.se>
2003-03-16 19:12:48 +0000

    initial test code for the snprintf() (and family) function
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@3449 a1c6a512-1295-4272-9138-f99709370657

---
+++ firmware/test/snprintf/test.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+#include "sprintf.h"
+
+int main(int argc, char **argv)
+{
+    char buffer[256];
+    snprintf(buffer, 5, "123456789");
+    printf("%s\n", buffer);
+    snprintf(buffer, 6, "123456789");
+    printf("%s\n", buffer);
+    snprintf(buffer, 7, "123456789");
+    printf("%s\n", buffer);
+    snprintf(buffer, 7, "123%s", "mooooooooooo");
+    printf("%s\n", buffer);
+}
