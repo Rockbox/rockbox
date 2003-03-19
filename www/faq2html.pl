8c8a606601c7df0c4cc21fab613cdf9e353e08b7
Robert Hak <adiamas@rockbox.org>
2003-03-19 20:22:39 +0000

    account for the # in anchor tags
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@3483 a1c6a512-1295-4272-9138-f99709370657

--- www/faq2html.pl
+++ www/faq2html.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/local/bin/perl
 
 # this is really a faq2html and should only be used for this purpose
 
@@ -6,7 +6,7 @@ sub fixline {
     $_ =~ s/\</&lt;/g;
     $_ =~ s/\>/&gt;/g;
 
-    $_ =~ s/(http:\/\/([a-zA-Z0-9_.\/-]*)[^\) .\n])/\<a href=\"$1\"\>$1\<\/a\>/g;
+    $_ =~ s/(http:\/\/([a-zA-Z0-9_.\#\/-]*)[^\) .\n])/\<a href=\"$1\"\>$1\<\/a\>/g;
 
     $_ =~ s/(\\|\/)$/$1&nbsp;/g; # clobber backslash on end of line
 }
