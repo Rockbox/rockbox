004f690405685cbfdc4a37346dac89a2394abc97
Daniel Stenberg <daniel@haxx.se>
2003-04-25 12:36:14 +0000

    use the rockbox versions of the sprintf* and friends
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@3611 a1c6a512-1295-4272-9138-f99709370657

--- uisimulator/x11/sprintf.h
+++ uisimulator/x11/sprintf.h
@@ -1,5 +1,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int snprintf (char *buf, size_t size, const char *fmt, ...);
+int rockbox_snprintf (char *buf, int size, const char *fmt, ...);
+int rockbox_vsnprintf (char *buf, int size, const char *fmt, va_list ap);
+int rockbox_fprintf (int fd, const char *fmt, ...);
 
+#define snprintf rockbox_snprintf
+#define vsnprintf rockbox_vsnprintf
+#define fprintf rockbox_fprintf
