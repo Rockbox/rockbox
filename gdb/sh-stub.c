644c6695190ae4427d8337f6c7acb0bb70eea8cf
Linus Nielsen Feltzing <linus@haxx.se>
2003-05-18 00:41:18 +0000

    The run length encoding hung if it found >100 equal chars
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@3684 a1c6a512-1295-4272-9138-f99709370657

--- gdb/sh-stub.c
+++ gdb/sh-stub.c
@@ -656,7 +656,7 @@ static void putpacket (register char *buffer)
             /* Do run length encoding */
             for (runlen = 0; runlen < 100; runlen ++) 
             {
-                if (src[0] != src[runlen]) 
+                if (src[0] != src[runlen] || runlen == 99) 
                 {
                     if (runlen > 3) 
                     {
