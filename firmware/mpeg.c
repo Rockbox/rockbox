1a658f0c4ec009e9ee3703c1e05cfdd6c02f78b5
Björn Stenberg <bjorn@haxx.se>
2003-07-02 19:44:15 +0000

    Only recorders use PA11 for /PR. Players use it for the STOP button...
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@3801 a1c6a512-1295-4272-9138-f99709370657

--- firmware/mpeg.c
+++ firmware/mpeg.c
@@ -2973,10 +2973,10 @@ void mpeg_init(int volume, int bass, int treble, int balance, int loudness,
     loudness = bass_boost = avc;
 #endif
 
-    PAIOR |= 0x0800; /* output for /PR */
     setup_sci0();
 
 #ifdef HAVE_MAS3587F
+    PAIOR |= 0x0800; /* output for /PR */
     init_playback();
     
     mas_version_code = mas_readver();
