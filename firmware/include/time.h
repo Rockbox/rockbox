12e237a3f16600ee4ea647f082d8b6971c245d7f
Björn Stenberg <bjorn@haxx.se>
2003-12-06 23:22:05 +0000

    Cleanup
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@4112 a1c6a512-1295-4272-9138-f99709370657

--- firmware/include/time.h
+++ firmware/include/time.h
@@ -1,32 +1,11 @@
 /*
  * time.h
  * 
- * Struct and function declarations for dealing with time.
+ * Struct declaration for dealing with time.
  */
 
 #ifndef _TIME_H_
 #define _TIME_H_
-
-#include "_ansi.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#ifndef NULL
-#define	NULL	0
-#endif
-
-#ifndef _CLOCKS_PER_SEC_
-#define _CLOCKS_PER_SEC_ 1000
-#endif
-
-#define CLOCKS_PER_SEC _CLOCKS_PER_SEC_
-#define CLK_TCK CLOCKS_PER_SEC
-#define __need_size_t
-#include <stddef.h>
-
-#include <sys/types.h>
 
 struct tm
 {
@@ -41,8 +20,5 @@ struct tm
   int	tm_isdst;
 };
 
-#ifdef __cplusplus
-}
-#endif
 #endif /* _TIME_H_ */
 
