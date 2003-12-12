0115af21d9badaab2fdb7e2404bea69a0504e4b0
Daniel Stenberg <daniel@haxx.se>
2003-12-12 14:07:28 +0000

    removed the neo-specific scramble-tool and made it an option to scramble instead, and made the makefile use this
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@4140 a1c6a512-1295-4272-9138-f99709370657

--- tools/scramble.c
+++ tools/scramble.c
@@ -31,9 +31,10 @@ int main (int argc, char** argv)
     int headerlen = 6;
     FILE* file;
     int version;
+    int scramble=1;
 
     if (argc < 3) {
-       printf("usage: %s [-fm] [-v2] <input file> <output file>\n",argv[0]);
+       printf("usage: %s [-fm] [-v2] [-neo]<input file> <output file>\n",argv[0]);
        return -1;
     }
 
@@ -44,12 +45,20 @@ int main (int argc, char** argv)
         version = 4;
     }
     
-    if(!strcmp(argv[1], "-v2")) {
+    else if(!strcmp(argv[1], "-v2")) {
         headerlen = 24;
         iname = argv[2];
         oname = argv[3];
         version = 2;
     }
+
+    else if(!strcmp(argv[1], "-neo")) {
+        headerlen = 17;
+        iname = argv[2];
+        oname = argv[3];
+        scramble = 0;
+    }
+
     
     /* open file */
     file = fopen(iname,"rb");
@@ -83,46 +92,63 @@ int main (int argc, char** argv)
     }
     fclose(file);
 
-    /* scramble */
-    slen = length/4;
-    for (i = 0; i < length; i++) {
-       unsigned long addr = (i >> 2) + ((i % 4) * slen);
-       unsigned char data = inbuf[i];
-       data = ~((data << 1) | ((data >> 7) & 1)); /* poor man's ROL */
-       outbuf[addr] = data;
+    if(scramble) {
+        /* scramble */
+        slen = length/4;
+        for (i = 0; i < length; i++) {
+            unsigned long addr = (i >> 2) + ((i % 4) * slen);
+            unsigned char data = inbuf[i];
+            data = ~((data << 1) | ((data >> 7) & 1)); /* poor man's ROL */
+            outbuf[addr] = data;
+        }
     }
     
     /* calculate checksum */
     for (i=0;i<length;i++)
        crc += inbuf[i];
 
-    /* make header */
     memset(header, 0, sizeof header);
-    if (headerlen == 6) {
-        header[0] = (length >> 24) & 0xff;
-        header[1] = (length >> 16) & 0xff;
-        header[2] = (length >> 8) & 0xff;
-        header[3] = length & 0xff;
-        header[4] = (crc >> 8) & 0xff;
-        header[5] = crc & 0xff;
+    if(scramble) {
+        if (headerlen == 6) {
+            header[0] = (length >> 24) & 0xff;
+            header[1] = (length >> 16) & 0xff;
+            header[2] = (length >> 8) & 0xff;
+            header[3] = length & 0xff;
+            header[4] = (crc >> 8) & 0xff;
+            header[5] = crc & 0xff;
+        }
+        else {
+            header[0] =
+                header[1] =
+                header[2] =
+                header[3] = 0xff; /* ??? */
+            
+            header[6] = (crc >> 8) & 0xff;
+            header[7] = crc & 0xff;
+
+            header[11] = version;
+
+            header[15] = headerlen; /* really? */
+
+            header[20] = (length >> 24) & 0xff;
+            header[21] = (length >> 16) & 0xff;
+            header[22] = (length >> 8) & 0xff;
+            header[23] = length & 0xff;
+        }
     }
     else {
-        header[0] =
-            header[1] =
-            header[2] =
-            header[3] = 0xff; /* ??? */
-            
-        header[6] = (crc >> 8) & 0xff;
-        header[7] = crc & 0xff;
+#define MY_FIRMWARE_TYPE "Rockbox"
+#define MY_HEADER_VERSION 1
 
-        header[11] = version;
-
-        header[15] = headerlen; /* really? */
-
-        header[20] = (length >> 24) & 0xff;
-        header[21] = (length >> 16) & 0xff;
-        header[22] = (length >> 8) & 0xff;
-        header[23] = length & 0xff;
+        strncpy(header,MY_FIRMWARE_TYPE,9);
+        header[9]='\0'; /*shouldn't have to, but to be SURE */
+        header[10]=MY_HEADER_VERSION&0xFF;
+        header[11]=(crc>>8)&0xFF;
+        header[12]=crc&0xFF;
+        header[13]=(sizeof(header)>>24)&0xFF;
+        header[14]=(sizeof(header)>>16)&0xFF;
+        header[15]=(sizeof(header)>>8)&0xFF;
+        header[16]=sizeof(header)&0xFF;
     }
 
     /* write file */
