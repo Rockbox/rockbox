313f67651fd964367538d3ee0213d6796e90d0e9
Linus Nielsen Feltzing <linus@haxx.se>
2004-06-04 11:49:15 +0000

    We now put ID3v2.3 tags in the recorded files instead of ID3v2.4, because some auido editing tools can't handle ID3v2.4
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@4720 a1c6a512-1295-4272-9138-f99709370657

--- firmware/mpeg.c
+++ firmware/mpeg.c
@@ -952,7 +952,7 @@ static bool swap_one_chunk(void)
 
 static const unsigned char empty_id3_header[] =
 {
-    'I', 'D', '3', 0x04, 0x00, 0x00,
+    'I', 'D', '3', 0x03, 0x00, 0x00,
     0x00, 0x00, 0x1f, 0x76 /* Size is 4096 minus 10 bytes for the header */
 };
 
