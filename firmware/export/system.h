abeb582a9cb347bcf14b265ad1fa5ff5caa5ed6b
Linus Nielsen Feltzing <linus@haxx.se>
2004-10-27 06:49:15 +0000

    Coldfire: set_irq_level() mixed up the in/out registers
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@5363 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/system.h
+++ firmware/export/system.h
@@ -147,12 +147,12 @@ enum {
 #define HIGHEST_IRQ_LEVEL (7<<8)
 static inline int set_irq_level(int level)
 {
-    int i;
+    int oldlevel;
     /* Read the old level and set the new one */
     asm volatile ("move.w %%sr,%0\n"
                   "or.l #0x2000,%1\n"
-                  "move.w %1,%%sr\n" : "=r" (i) : "r" (level));
-    return i;
+                  "move.w %1,%%sr\n" : "=r" (oldlevel), "+r" (level) : );
+    return oldlevel;
 }
 
 #endif
