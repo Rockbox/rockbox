3804e7a874a516a417e2b321c0a047055e5f0614
Linus Nielsen Feltzing <linus@haxx.se>
2005-01-28 13:14:14 +0000

    Correct values for the -add format
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@5695 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/config-h100.h
+++ firmware/export/config-h100.h
@@ -39,9 +39,9 @@
 #define CONFIG_LCD LCD_S1D15E06
 
 /* Offset ( in the firmware file's header ) to the file CRC */
-#define FIRMWARE_OFFSET_FILE_CRC 4
+#define FIRMWARE_OFFSET_FILE_CRC 0
 
 /* Offset ( in the firmware file's header ) to the real data */
-#define FIRMWARE_OFFSET_FILE_DATA 6
+#define FIRMWARE_OFFSET_FILE_DATA 8
 
 #endif
