b4527a342144b1cbdba9d88fa220739a83997f85
Jens Arnold <amiconn@rockbox.org>
2005-02-11 23:56:38 +0000

    Simulators: Made the mp3 buffer size depend on the MEMORYSIZE setting for better simulation.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@5922 a1c6a512-1295-4272-9138-f99709370657

--- firmware/buffer.c
+++ firmware/buffer.c
@@ -20,7 +20,7 @@
 #include "buffer.h"
 
 #ifdef SIMULATOR
-unsigned char mp3buffer[0x200000];
+unsigned char mp3buffer[(MEM*1024-256)*1024];
 unsigned char *mp3end = mp3buffer + sizeof(mp3buffer);
 #else
 /* defined in linker script */
