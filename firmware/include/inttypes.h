de3d58c4dc8bf9b4d64769009dccd440aed39b14
Dave Chapman <dave@dchapman.com>
2005-02-15 19:45:38 +0000

    Initial check-in - needed by various codec libraries
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@5956 a1c6a512-1295-4272-9138-f99709370657

---
+++ firmware/include/inttypes.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2005 by Dave Chapman
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#ifndef __INTTYPES_H__ 
+#define __INTTYPES_H__ 
+
+#define int8_t char
+#define int16_t short
+#define int32_t long
+#define int64_t long long
+
+#define uint8_t unsigned char
+#define uint16_t unsigned short
+#define uint32_t unsigned long
+#define uint64_t unsigned long long
+
+#endif /* __INTTYPES_H__ */
