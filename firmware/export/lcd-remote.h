394c2d91993f6b5a0c21fb7c395da8f5059a3a19
Christian Gmeiner <christian.gmeiner@gmail.com>
2005-04-15 12:36:03 +0000

    compile fix
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@6293 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/lcd-remote.h
+++ firmware/export/lcd-remote.h
@@ -7,7 +7,7 @@
  *                     \/            \/     \/    \/            \/
  * $Id$
  *
- * Copyright (C) 2005 by Richard S. La Charité III
+ * Copyright (C) 2002 by Alan Korr
  *
  * All files in this archive are subject to the GNU General Public License.
  * See the file COPYING in the source tree root for full license agreement.
@@ -20,18 +20,22 @@
 #ifndef __LCD_REMOTE_H__
 #define __LCD_REMOTE_H__
 
-#ifdef IRIVER_H100
+#if CONFIG_CPU == MCF5249
 #include <stdbool.h>
 #include "cpu.h"
 #include "config.h"
 
 #define REMOTE_DRAW_PIXEL(x,y)		lcd_remote_framebuffer[(y)/8][(x)] |= (1<<((y)&7))
 #define REMOTE_CLEAR_PIXEL(x,y)		lcd_remote_framebuffer[(y)/8][(x)] &= ~(1<<((y)&7))
-#define REMOTE_INVERT_PIXEL(x,y)	lcd_remote_framebuffer[(y)/8][(x)] ^= (1<<((y)&7))
+#define REMOTE_INVERT_PIXEL(x,y)		lcd_remote_framebuffer[(y)/8][(x)] ^= (1<<((y)&7))
 
 extern void lcd_remote_init(void);
+extern void lcd_remote_clear_display(void);
 extern void lcd_remote_backlight_on(void);
 extern void lcd_remote_backlight_off(void);
+extern void lcd_remote_set_contrast(int val);
+extern void lcd_remote_set_invert_display(bool yesno);
+extern int  lcd_remote_default_contrast(void);
 
 extern unsigned char lcd_remote_framebuffer[LCD_REMOTE_HEIGHT/8][LCD_REMOTE_WIDTH];
 
