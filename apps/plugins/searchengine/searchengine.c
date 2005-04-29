f5f8c05a31b43b6b0368d5c1a0af360e5cbee465
Michiel Van Der Kolk <not.valid@email.address>
2005-04-29 23:20:41 +0000

    *whistles*
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@6386 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/searchengine/searchengine.c
+++ apps/plugins/searchengine/searchengine.c
@@ -37,6 +37,7 @@ void *my_malloc(size_t size)
             = rb->plugin_get_audio_buffer(&audio_buffer_free);
         audio_bufferpointer+=3;
         audio_bufferpointer&=~3;
+        audio_buffer_free-=audio_bufferpointer-audiobufferbase;
     }
     if (size + 4 > audio_buffer_free)
         return 0;
