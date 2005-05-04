f64b52ae380edf76220b3fc53998e14af8e55c4a
Linus Nielsen Feltzing <linus@haxx.se>
2005-05-04 05:37:12 +0000

    Oops
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@6402 a1c6a512-1295-4272-9138-f99709370657

--- firmware/drivers/ata.c
+++ firmware/drivers/ata.c
@@ -1085,7 +1085,7 @@ int ata_hard_reset(void)
     /* state HRR1 */
     or_b(0x02, &PADRH); /* negate _RESET */
     sleep(1); /* > 2ms */
-#elif config_cpu == MCF5249
+#elif CONFIG_CPU == MCF5249
     GPIO_OUT &= ~0x00080000;
     sleep(1); /* > 25us */
 
