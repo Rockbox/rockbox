e6ad9458d16f1cab9bc6ab6b32deab290c142468
Christian Gmeiner <christian.gmeiner@gmail.com>
2005-08-02 04:47:24 +0000

    headphone volume should be correct now
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@7279 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/tlv320.h
+++ firmware/export/tlv320.h
@@ -34,27 +34,27 @@ extern int tlv320_mute(bool mute);
 /* registers */
 /* REG_LLIV: Left line input channel volume control */
 #define REG_LLIV        0x0
-#define LLIV_LRS        (0 << 8)    /* simultaneous volume/mute update */
-#define LIM             (1 << 7)    /* Left line input mute */
-#define LIV             ((x) & 0x1f)/* Left line input volume control */
+#define LLIV_LRS        (0 << 8)    /* simultaneous volume/mute update  */
+#define LIM             (1 << 7)    /* Left line input mute             */
+#define LIV             ((x) & 0x1f)/* Left line input volume control   */
 
 /* REG_RLIV: Right line input channel volume control */
 #define REG_RLIV        0x1
-#define RLIV_RLS        (0 << 8)    /* simultaneous volume/mute update */
-#define RLIV_RIM        (0 << 7)    /* Right line input mute */
-#define RLIV_LIV        ((x) & 0x1f)/* Right line input volume control */
+#define RLIV_RLS        (0 << 8)    /* simultaneous volume/mute update  */
+#define RLIV_RIM        (0 << 7)    /* Right line input mute            */
+#define RLIV_LIV        ((x) & 0x1f)/* Right line input volume control  */
 
 /* REG_LHV: Left Channel Headphone Volume Control */
 #define REG_LHV         0x2
 #define LHV_LRS         (0 << 8)    /* simultaneous volume/mute update */
-#define LHV_LZC         (0 << 7)	/* Left-channel zero-cross detect */
-#define LHV_LHV         ((x) & 0x1f)/* Left headphone volume control */
+#define LHV_LZC         (0 << 7)	/* Left-channel zero-cross detect  */
+#define LHV_LHV         ((x) & 0x7f)/* Left headphone volume control    */
 
 /* REG_RHV: Right Channel Headphone Volume Control */
 #define REG_RHV         0x3
-#define RHV_LRS         (0 << 8)    /* simultaneous volume/mute update */
-#define RHV_RZC         (0 << 7)    /* Right-channel zero-cross detect */
-#define RHV_LHV         ((x) & 0x1f)/* Right headphone volume control */
+#define RHV_LRS         (0 << 8)    /* simultaneous volume/mute update  */
+#define RHV_RZC         (0 << 7)    /* Right-channel zero-cross detect  */
+#define RHV_LHV         ((x) & 0x7f)/* Right headphone volume control   */
 
 /* REG_AAP: Analog Audio Path Control */
 #define REG_AAP         0x4
@@ -83,7 +83,7 @@ extern int tlv320_mute(bool mute);
 #define PC_ADC          (0 << 2)    /* ADC              */
 #define PC_MIC          (0 << 1)    /* Microphone input */
 #define PC_LINE         (0 << 0)    /* Line input       */
-                                
+
 /* REG_DAIF: Digital Audio Interface Format */
 #define REG_DAIF        0x7
 #define DAIF_MS         (1 << 6)    /* Master/slave mode                        */
@@ -100,11 +100,11 @@ extern int tlv320_mute(bool mute);
 
 /* REG_SRC: Sample Rate Control */
 #define REG_SRC         0x8
-#define SRC_CLKIN       (0 << 6)    /* Clock input divider */ 
-#define SRC_CLKOUT      (0 << 7)    /* Clock output divider */
+#define SRC_CLKIN       (0 << 6)    /* Clock input divider                          */ 
+#define SRC_CLKOUT      (0 << 7)    /* Clock output divider                         */
 /*#define SRC_SR        ()*/
-#define SRC_BOSR        (1 << 1)    /* Base oversampling rate, depends on SRC_USB */
-#define SRC_USB         (1 << 0)    /* Clock mode select */
+#define SRC_BOSR        (1 << 1)    /* Base oversampling rate, depends on SRC_USB   */
+#define SRC_USB         (1 << 0)    /* Clock mode select                            */
 
 /* REG_DIA: Digital Interface Activation */
 #define REG_DIA         0x9
