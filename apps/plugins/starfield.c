d4bccf7f92c0de9af8240b25226918bd3812d563
Dave Chapman <dave@dchapman.com>
2005-10-28 13:18:11 +0000

    iso-8859-1 police
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@7672 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/starfield.c
+++ apps/plugins/starfield.c
@@ -5,7 +5,7 @@
 *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
 *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
 *                     \/            \/     \/    \/            \/
-* Copyright (C) 2005 Kï¿½in Ferrare
+* Copyright (C) 2005 Kévin Ferrare
 *
 * All files in this archive are subject to the GNU General Public License.
 * See the file COPYING in the source tree root for full license agreement.
