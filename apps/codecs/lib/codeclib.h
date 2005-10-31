c3f901be565682a6765249cebe8e58f9afeb8cfd
Dave Chapman <dave@dchapman.com>
2005-10-31 20:41:54 +0000

    Correct the prototype for memmove (oops, forgot this file)
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@7702 a1c6a512-1295-4272-9138-f99709370657

--- apps/codecs/lib/codeclib.h
+++ apps/codecs/lib/codeclib.h
@@ -40,7 +40,7 @@ void codec_free(void* ptr);
 void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void *s, int c, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
-void *memmove(const void *s1, const void *s2, size_t n);
+void *memmove(void *s1, const void *s2, size_t n);
 
 size_t strlen(const char *s);
 char *strcpy(char *dest, const char *src);
