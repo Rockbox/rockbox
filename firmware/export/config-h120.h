da1949b109ba4813673d6939a8bce2ac7e9fa14a
Dave Chapman <dave@dchapman.com>
2005-10-31 20:45:56 +0000

    Increase codec size to 512KB in preparation for AAC codec
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@7704 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/config-h120.h
+++ firmware/export/config-h120.h
@@ -38,7 +38,7 @@
 #define HAVE_SW_POWEROFF
 
 /* The number of bytes reserved for loadable codecs */
-#define CODEC_SIZE 0x40000
+#define CODEC_SIZE 0x80000
 
 /* The number of bytes reserved for loadable plugins */
 #define PLUGIN_BUFFER_SIZE 0xC0000
