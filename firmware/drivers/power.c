b6c0f9596024baa5265e29734cc5d8686d81e950
Linus Nielsen Feltzing <linus@haxx.se>
2005-11-14 20:49:21 +0000

    Added pcf50606 init
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@7881 a1c6a512-1295-4272-9138-f99709370657

--- firmware/drivers/power.c
+++ firmware/drivers/power.c
@@ -24,6 +24,8 @@
 #include "system.h"
 #include "power.h"
 #include "hwcompat.h"
+#include "logf.h"
+#include "pcf50606.h"
 
 #ifdef HAVE_CHARGE_CTRL
 bool charger_enabled;
@@ -60,6 +62,7 @@ int radio_get_status(void)
 void power_init(void)
 {
 #if CONFIG_CPU == MCF5249
+#if IRIVER_H100_SERIES
     or_l(0x00080000, &GPIO1_OUT);
     or_l(0x00080000, &GPIO1_ENABLE);
     or_l(0x00080000, &GPIO1_FUNCTION);
@@ -72,6 +75,9 @@ void power_init(void)
     or_l(0x80000000, &GPIO_FUNCTION);
 #ifdef HAVE_SPDIF_POWER
     spdif_power_enable(false);
+#endif
+#elif defined(IRIVER_H300_SERIES)
+    pcf50606_init();
 #endif
 #elif CONFIG_CPU == PP5020
     /* TODO: Implement power_init() */
