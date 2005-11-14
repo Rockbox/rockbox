51413e444296f4444a7ff6e1a005e18b30e0f77e
Daniel Stenberg <daniel@haxx.se>
2005-11-14 15:06:51 +0000

    Added first take at WPS bundling magic. There's only one single WPS in there yet, but it should work to start with...
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@7864 a1c6a512-1295-4272-9138-f99709370657

--- tools/buildzip.pl
+++ tools/buildzip.pl
@@ -1,4 +1,12 @@
 #!/usr/bin/perl
+#             __________               __   ___.
+#   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+#   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+#   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+#   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+#                     \/            \/     \/    \/            \/
+# $Id$
+#
 
 $ROOT="..";
 
@@ -190,6 +198,14 @@ STOP
          "NODO",
          "TECH")) {
         `cp $ROOT/docs/$_ .rockbox/docs/$_.txt`;
+    }
+
+    # Now do the WPS dance
+    if(-d "$ROOT/wps") {
+        system("perl $ROOT/wps/wpsbuild.pl $ROOT/wps/WPSLIST $target");
+    }
+    else {
+        print STDERR "No wps module present, can't do the WPS magic!\n";
     }
 
     # now copy the file made for reading on the unit:
