4efecf6435b9c5afeb1d67f98d6aec992c9c990e
Jens Arnold <amiconn@rockbox.org>
2005-11-26 23:47:06 +0000

    Fix yellow builds :/
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@8073 a1c6a512-1295-4272-9138-f99709370657

--- firmware/include/dircache.h
+++ firmware/include/dircache.h
@@ -72,6 +72,7 @@ int dircache_build(int last_size);
 bool dircache_is_enabled(void);
 int dircache_get_cache_size(void);
 int dircache_get_reserve_used(void);
+int dircache_get_build_ticks(void);
 void dircache_disable(void);
 const struct dircache_entry *dircache_get_entry_ptr(const char *filename);
 void dircache_copy_path(const struct dircache_entry *entry, char *buf, int size);
