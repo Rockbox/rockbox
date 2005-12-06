71ad5f4d04b40d5f15949b7ce1a3efa140ed49d1
Marcoen Hirschberg <marcoen@gmail.com>
2005-12-06 15:17:53 +0000

    forgot to add codepage_tables.h
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@8176 a1c6a512-1295-4272-9138-f99709370657

---
+++ tools/codepage_tables.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ *
+ * Copyright (C) 2005 by Frank Dischner
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+extern const unsigned short iso8859_7_to_uni[];
+extern const unsigned short cp1251_to_uni[];
+extern const unsigned short iso8859_2_to_uni[];
+extern const unsigned short cp932_table[];
+extern const unsigned short cp936_table[];
+extern const unsigned short cp949_table[];
+extern const unsigned short cp950_table[];
+
