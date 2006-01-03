88f4c1178902c5aa700cbcdf6d1973c09ca9a5d0
Dave Chapman <dave@dchapman.com>
2006-01-03 11:58:39 +0000

    iPod - Tremor code has become too big for IRAM (as of 28 December 2005), so temporarily disable ICODE_ATTR to fix the broken builds.  This needs reviewing anyway when the time comes to optimise Tremor for the iPod.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@8298 a1c6a512-1295-4272-9138-f99709370657

--- apps/codecs/Tremor/config-tremor.h
+++ apps/codecs/Tremor/config-tremor.h
@@ -9,5 +9,12 @@
 #define BIG_ENDIAN 0
 #endif
 
+/* As of 28 Dec 2005, the Tremor code is too big for IRAM on the iPod,
+   so we temporarily disable ICODE_ATTR - this needs reviewing when it comes
+   to optimising Tremor for the iPod */
+#ifdef CPU_ARM
+#undef ICODE_ATTR
+#define ICODE_ATTR
+#endif
 
 // #define _LOW_ACCURACY_
