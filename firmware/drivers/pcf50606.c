13414ae77a77d96ef2cb210d644880b847c9ca6f
Brandon Low <lostlogic@rockbox.org>
2006-01-19 07:47:02 +0000

    Initialize the MBC to off, idle state JinC Fix a comment that had wrong voltage
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@8382 a1c6a512-1295-4272-9138-f99709370657

--- firmware/drivers/pcf50606.c
+++ firmware/drivers/pcf50606.c
@@ -259,7 +259,7 @@ static void set_voltages(void)
 {
     static const unsigned char buf[5] =
         {
-            0xf4,   /* IOREGC = 3.3V, ON in all states */
+            0xf4,   /* IOREGC = 2.9V, ON in all states */
             0xef,   /* D1REGC = 2.4V, ON in all states */
             0x18,   /* D2REGC = 3.3V, OFF in all states */
             0xf0,   /* D3REGC = 2.5V, ON in all states */
@@ -283,6 +283,7 @@ void pcf50606_init(void)
 
     pcf50606_write(0x08, 0x60); /* Wake on USB and charger insertion */
     pcf50606_write(0x09, 0x05); /* USB and ON key debounce: 14ms */
+    pcf50606_write(0x29, 0x1C); /* Disable the unused MBC module */
     
      /* Backlight PWM = 512Hz 50/50 */
     /*pcf50606_write(0x35, 0x13);*/
