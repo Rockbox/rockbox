f3ee1fe09b6fa9a7ce1d9e86be546eb1a94c6b54
Jens Arnold <amiconn@rockbox.org>
2006-02-24 07:59:48 +0000

    Ooops, commit missing chessbox changes. Silly cvs...
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@8823 a1c6a512-1295-4272-9138-f99709370657

---
+++ apps/plugins/chessbox.c
@@ -0,0 +1,34 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2005 Jens Arnold
+ *
+ * Overlay loader stub plugin for chessbox on Archos
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#include "plugin.h"
+
+#if MEM <= 8 && !defined(SIMULATOR)
+
+#include "overlay.h"
+
+PLUGIN_HEADER
+
+/* this is the plugin entry point */
+enum plugin_status plugin_start(struct plugin_api* api, void* parameter)
+{
+    return run_overlay(api, parameter, "/.rockbox/rocks/chessbox.ovl", "ChessBox");
+}
+#endif
