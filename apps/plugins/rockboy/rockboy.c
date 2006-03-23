354941d9667f33b026cbe567745a322b7a8adffe
Zakk Roberts <midk@rockbox.org>
2006-03-23 23:49:22 +0000

    Make fullscreen off by default for Rockboy.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@9220 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/rockboy/rockboy.c
+++ apps/plugins/rockboy/rockboy.c
@@ -70,7 +70,7 @@ void* memcpy(void* dst, const void* src, size_t size)
     return rb->memcpy(dst, src, size);
 }
 
-void setmallocpos(void *pointer) 
+void setmallocpos(void *pointer)
 {
     audio_bufferpointer = pointer;
     audio_buffer_free = audio_bufferpointer - audio_bufferbase;
@@ -81,17 +81,17 @@ void setoptions (void) {
    DIR* dir;
    char optionsave[sizeof(savedir)+sizeof(optionname)];
 
-   	dir=opendir(savedir);
-	if(!dir)
-	  mkdir(savedir,0);
-	else
-	  closedir(dir);
+    dir=opendir(savedir);
+    if(!dir)
+      mkdir(savedir,0);
+    else
+      closedir(dir);
 
    snprintf(optionsave, sizeof(optionsave), "%s/%s", savedir, optionname);
 
    fd = open(optionsave, O_RDONLY);
    if(fd < 0) // no options to read, set defaults
-   { 
+   {
 #if (CONFIG_KEYPAD == IRIVER_H100_PAD)
       options.A=BUTTON_ON;
       options.B=BUTTON_OFF;
@@ -138,7 +138,7 @@ void setoptions (void) {
       options.maxskip=4;
       options.fps=0;
       options.showstats=0;
-      options.fullscreen=1;
+      options.fullscreen=0;
       options.sound=1;
    }
    else
@@ -164,14 +164,14 @@ enum plugin_status plugin_start(struct plugin_api* api, void* parameter)
     /* if you are using a global api pointer, don't forget to copy it!
        otherwise you will get lovely "I04: IllInstr" errors... :-) */
     rb = api;
-    
-	rb->lcd_setfont(0);
+
+    rb->lcd_setfont(0);
 
     if (!parameter) {
         rb->splash(HZ*3, true, "Play gameboy ROM file! (.gb/.gbc)");
         return PLUGIN_OK;
     }
-    
+
     audio_bufferbase = audio_bufferpointer
         = rb->plugin_get_audio_buffer((int *)&audio_buffer_free);
 #if MEM <= 8 && !defined(SIMULATOR)
