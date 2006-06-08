4ba0a33204b0667933d99c48280272c24e1bfce4
Peter D'Hoye <peter.dhoye@gmail.com>
2006-06-08 22:08:24 +0000

    Let panicf() also check on/play button on h300
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@10094 a1c6a512-1295-4272-9138-f99709370657

--- firmware/panic.c
+++ firmware/panic.c
@@ -100,6 +100,8 @@ void panicf( const char *fmt, ...)
         /* try to restart firmware if ON is pressed */
 #ifdef IRIVER_H100_SERIES
         if ((GPIO1_READ & 0x22) == 0) /* check for ON button and !hold */
+#elif IRIVER_H300_SERIES
+        if ((GPIO1_READ & 0x22) == 0) /* check for ON button and !hold */
 #elif CONFIG_CPU == SH7034
 #if CONFIG_KEYPAD == PLAYER_PAD
         if (!(PADRL & 0x20))
