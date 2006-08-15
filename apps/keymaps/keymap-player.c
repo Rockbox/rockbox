1526fd7ecb7b3f58c22e4e51bef848f503d8be60
Linus Nielsen Feltzing <linus@haxx.se>
2006-08-15 21:27:53 +0000

    Decent button mapping for Archos Player
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@10595 a1c6a512-1295-4272-9138-f99709370657

--- apps/keymaps/keymap-player.c
+++ apps/keymaps/keymap-player.c
@@ -19,20 +19,11 @@
 /* *
  * Button Code Definitions for archos player targets
  *
- * \TODO test!
  */
 
 #include "config.h"
 #include "action.h"
 #include "button.h"
-#include "settings.h"
-
-/* CONTEXT_CUSTOM's used in this file...
-
-CONTEXT_CUSTOM|1 = the standard list/tree defines (without directions)
-
-
-*/
 
 struct button_mapping button_context_standard[]  = {
     { ACTION_STD_PREV,       BUTTON_LEFT,                 BUTTON_NONE },
@@ -43,26 +34,25 @@ struct button_mapping button_context_standard[]  = {
     { ACTION_STD_CONTEXT,    BUTTON_MENU|BUTTON_REPEAT, BUTTON_MENU },    
     { ACTION_STD_CANCEL,     BUTTON_STOP,                BUTTON_NONE },
     { ACTION_STD_OK,         BUTTON_PLAY,               BUTTON_NONE },
-    { ACTION_STD_OK,         BUTTON_ON|BUTTON_REL,      BUTTON_ON },
     { ACTION_STD_MENU,       BUTTON_MENU|BUTTON_REL,    BUTTON_MENU },
 
     LAST_ITEM_IN_LIST
 };
 
 struct button_mapping button_context_wps[]  = {
-    { ACTION_WPS_PLAY,          BUTTON_PLAY,                BUTTON_NONE },
-    { ACTION_WPS_SKIPNEXT,      BUTTON_RIGHT,               BUTTON_NONE },
-    { ACTION_WPS_SKIPPREV,      BUTTON_LEFT,                BUTTON_NONE },
-    { ACTION_WPS_SEEKBACK,      BUTTON_LEFT|BUTTON_REPEAT,  BUTTON_LEFT },
-    { ACTION_WPS_SEEKFWD,       BUTTON_RIGHT|BUTTON_REPEAT, BUTTON_RIGHT },
-    { ACTION_WPS_STOPSEEK,      BUTTON_LEFT|BUTTON_REL,     BUTTON_LEFT|BUTTON_REPEAT },
-    { ACTION_WPS_STOPSEEK,      BUTTON_RIGHT|BUTTON_REL,    BUTTON_RIGHT|BUTTON_REPEAT },
-    { ACTION_WPS_STOP,          BUTTON_STOP,                BUTTON_NONE },
+    { ACTION_WPS_PLAY,      BUTTON_PLAY,                BUTTON_NONE },
+    { ACTION_WPS_SKIPNEXT,  BUTTON_RIGHT,               BUTTON_NONE },
+    { ACTION_WPS_SKIPPREV,  BUTTON_LEFT,                BUTTON_NONE },
+    { ACTION_WPS_SEEKBACK,  BUTTON_LEFT|BUTTON_REPEAT,  BUTTON_LEFT },
+    { ACTION_WPS_SEEKFWD,   BUTTON_RIGHT|BUTTON_REPEAT, BUTTON_RIGHT },
+    { ACTION_WPS_STOPSEEK,  BUTTON_LEFT|BUTTON_REL,     BUTTON_LEFT|BUTTON_REPEAT },
+    { ACTION_WPS_STOPSEEK,  BUTTON_RIGHT|BUTTON_REL,    BUTTON_RIGHT|BUTTON_REPEAT },
+    { ACTION_WPS_STOP,      BUTTON_STOP,                BUTTON_NONE },
     
-    { ACTION_WPS_VOLDOWN,   BUTTON_MENU|BUTTON_STOP,                BUTTON_MENU },
-    { ACTION_WPS_VOLDOWN,   BUTTON_MENU|BUTTON_STOP|BUTTON_REPEAT,  BUTTON_MENU|BUTTON_STOP },
-    { ACTION_WPS_VOLUP,     BUTTON_MENU|BUTTON_PLAY,                BUTTON_MENU },
-    { ACTION_WPS_VOLUP,     BUTTON_MENU|BUTTON_PLAY|BUTTON_REPEAT,  BUTTON_MENU|BUTTON_PLAY },
+    { ACTION_WPS_VOLDOWN,   BUTTON_MENU|BUTTON_LEFT,    BUTTON_MENU },
+    { ACTION_WPS_VOLDOWN,   BUTTON_MENU|BUTTON_LEFT|BUTTON_REPEAT,  BUTTON_MENU|BUTTON_LEFT },
+    { ACTION_WPS_VOLUP,     BUTTON_MENU|BUTTON_RIGHT,   BUTTON_MENU },
+    { ACTION_WPS_VOLUP,     BUTTON_MENU|BUTTON_RIGHT|BUTTON_REPEAT,  BUTTON_MENU|BUTTON_RIGHT },
     
     { ACTION_WPS_BROWSE,    BUTTON_ON|BUTTON_REL,     BUTTON_ON },
     { ACTION_WPS_MENU,      BUTTON_MENU|BUTTON_REL,   BUTTON_MENU },
@@ -83,25 +73,13 @@ struct button_mapping button_context_settings[] = {
 };
 
 struct button_mapping button_context_tree[]  = {
-    { ACTION_TREE_WPS,    BUTTON_PLAY,     BUTTON_NONE },
-    { ACTION_TREE_STOP,   BUTTON_STOP,     BUTTON_NONE },
+    { ACTION_TREE_WPS,    BUTTON_ON|BUTTON_REL,     BUTTON_NONE },
     
     LAST_ITEM_IN_LIST
 }; /* button_context_listtree */
-struct button_mapping button_context_tree_scroll_lr[]  = {
-    { ACTION_NONE,              BUTTON_LEFT,                BUTTON_NONE },
-    { ACTION_STD_CANCEL,        BUTTON_LEFT|BUTTON_REL,     BUTTON_LEFT },
-    { ACTION_TREE_PGLEFT,       BUTTON_LEFT|BUTTON_REPEAT,  BUTTON_LEFT },
-    { ACTION_TREE_PGLEFT,       BUTTON_LEFT|BUTTON_REL,     BUTTON_LEFT|BUTTON_REPEAT },
-    { ACTION_NONE,              BUTTON_RIGHT,               BUTTON_NONE },
-    { ACTION_STD_OK,            BUTTON_RIGHT|BUTTON_REL,    BUTTON_RIGHT },
-    { ACTION_TREE_PGRIGHT,      BUTTON_RIGHT|BUTTON_REPEAT, BUTTON_RIGHT },
-    { ACTION_TREE_PGRIGHT,      BUTTON_RIGHT|BUTTON_REL,    BUTTON_RIGHT|BUTTON_REPEAT },    
-    LAST_ITEM_IN_LIST__NEXTLIST(CONTEXT_CUSTOM|1),
-};
 
 struct button_mapping button_context_yesno[]  = {
-    { ACTION_YESNO_ACCEPT,          BUTTON_PLAY,                  BUTTON_NONE },
+    { ACTION_YESNO_ACCEPT,  BUTTON_PLAY,      BUTTON_NONE },
     LAST_ITEM_IN_LIST
 }; /* button_context_settings_yesno */
 
@@ -122,10 +100,6 @@ struct button_mapping* get_context_mapping( int context )
             return button_context_yesno;
 
         case CONTEXT_TREE:
-            if (global_settings.hold_lr_for_scroll_in_list)
-                return button_context_tree_scroll_lr;
-            /* else fall through to CUSTOM|1 */
-        case CONTEXT_CUSTOM|1:
             return button_context_tree;
             
         case CONTEXT_LIST:
