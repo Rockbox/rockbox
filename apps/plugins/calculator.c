33032fad038cb230ff5f07de9d48231f2a7cc374
Linus Nielsen Feltzing <linus@haxx.se>
2006-08-30 13:30:38 +0000

    Correct credits for the calculator plugin
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@10804 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/calculator.c
+++ apps/plugins/calculator.c
@@ -7,7 +7,7 @@
  *                     \/            \/     \/    \/            \/
  * $Id$
  *
- * Copyright (C) 2004 Pengxuan(Isaac)  <tinousus@yahoo.com>
+ * Copyright (C) 2004 Pengxuan Liu (Isaac)
  *
  * All files in this archive are subject to the GNU General Public License.
  * See the file COPYING in the source tree root for full license agreement.
