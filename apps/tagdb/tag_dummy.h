e48a0354093a5c7f26d67f5584dfb2f38f680870
Jonathan Gordon <rockbox@jdgordon.info>
2006-10-25 14:30:45 +0000

    remove the old tagdb source, tagcache replaced this a while ago
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@11340 a1c6a512-1295-4272-9138-f99709370657

--- apps/tagdb/tag_dummy.h
+++
@@ -1,5 +0,0 @@
-#ifdef HAVE_TAGCACHE
-#include "db.h"
-
-int tag_dummy(char *file, struct tag_info *t);
-#endif
--- apps/tagdb/unique.h
+++
@@ -1,8 +0,0 @@
-#ifdef
-#ifndef __UNIQUE_H__
-#define __UNIQUE_H__
-
-char *create_unique_name(char *buffer, const char *prefix, const char *suffix, int digits);
-
-#endif
-#endif
