2f428871ea2ad279b5c2327fdf3add487c09a942
Jens Arnold <amiconn@rockbox.org>
2007-01-07 11:59:59 +0000

    Allow one more thread on swcodec to make mpegplayer work on targets with remote LCD and voice file present.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@11937 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/thread.h
+++ firmware/export/thread.h
@@ -39,7 +39,7 @@
 #define PRIORITY_BACKGROUND      8 /* Normal application threads */
 
 #if CONFIG_CODEC == SWCODEC
-#define MAXTHREADS	15
+#define MAXTHREADS	16
 #else
 #define MAXTHREADS  11
 #endif
