6c6c35231064ee0c586d7f507bb639a5b3ca559e
Dave Chapman <dave@dchapman.com>
2007-02-28 01:36:32 +0000

    Character-encoding policing
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@12516 a1c6a512-1295-4272-9138-f99709370657

--- apps/gui/gwps-common.c
+++ apps/gui/gwps-common.c
@@ -7,7 +7,7 @@
  *                     \/            \/     \/    \/            \/
  * $Id$
  *
- * Copyright (C) 2002 BjÃ¶rn Stenberg
+ * Copyright (C) 2002 Björn Stenberg
  *
  * All files in this archive are subject to the GNU General Public License.
  * See the file COPYING in the source tree root for full license agreement.
