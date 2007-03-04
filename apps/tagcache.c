416218660aee3df058b8f6d7dbd49b79adddd020
Linus Nielsen Feltzing <linus@haxx.se>
2007-03-04 19:30:03 +0000

    Ignore me. I am silly.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@12599 a1c6a512-1295-4272-9138-f99709370657

--- apps/tagcache.c
+++ apps/tagcache.c
@@ -92,7 +92,6 @@ static const char tagcache_thread_name[] = "tagcache";
 #endif
 
 #define UNTAGGED "<Untagged>"
-#define SIZEOF_UNTAGGED 11 /* Including \0 */
 
 /* Previous path when scanning directory tree recursively. */
 static char curpath[TAG_MAXLEN+32];
@@ -1525,7 +1524,7 @@ static int check_if_empty(char **tag)
     if (*tag == NULL || *tag[0] == '\0')
     {
         *tag = UNTAGGED;
-        return SIZEOF_UNTAGGED; /* Tag length */
+        return sizeof(UNTAGGED); /* Tag length */
     }
     
     length = strlen(*tag);
