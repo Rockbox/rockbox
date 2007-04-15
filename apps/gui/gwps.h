f53a8f8cd48b0c679b4bb7da2947e39ff2f7e223
Miika Pekkarinen <miipekk@ihme.org>
2007-04-15 15:46:46 +0000

    Added autoscore tag (%ra) to wps.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@13169 a1c6a512-1295-4272-9138-f99709370657

--- apps/gui/gwps.h
+++ apps/gui/gwps.h
@@ -166,6 +166,7 @@ enum wps_token_type {
     /* Database */
     WPS_TOKEN_DATABASE_PLAYCOUNT,
     WPS_TOKEN_DATABASE_RATING,
+    WPS_TOKEN_DATABASE_AUTOSCORE,
 
     /* File */
     WPS_TOKEN_FILE_BITRATE,
