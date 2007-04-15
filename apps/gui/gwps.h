1c0f41447ade94e89fa0d773792205ab99a73538
Miika Pekkarinen <miipekk@ihme.org>
2007-04-15 15:38:08 +0000

    Reverted FS#6949 as we have a strict policy against anonymous contributions.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@13168 a1c6a512-1295-4272-9138-f99709370657

--- apps/gui/gwps.h
+++ apps/gui/gwps.h
@@ -166,7 +166,6 @@ enum wps_token_type {
     /* Database */
     WPS_TOKEN_DATABASE_PLAYCOUNT,
     WPS_TOKEN_DATABASE_RATING,
-    WPS_TOKEN_DATABASE_SCORE,
 
     /* File */
     WPS_TOKEN_FILE_BITRATE,
