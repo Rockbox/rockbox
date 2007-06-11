541f4033428a4cff8311e2081b1d6d2479180bdc
Steve Bavin <pondlife@pondlife.me>
2007-06-11 08:34:42 +0000

    ..and add a couple of comments.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@13617 a1c6a512-1295-4272-9138-f99709370657

--- apps/talk.h
+++ apps/talk.h
@@ -72,6 +72,6 @@ int talk_number(long n, bool enqueue); /* say a number */
 int talk_value(long n, int unit, bool enqueue); /* say a numeric value */
 int talk_spell(const char* spell, bool enqueue); /* spell a string */
 bool talk_menus_enabled(void); /* returns true if menus should be voiced */
-void talk_disable_menus(void);
-void talk_enable_menus(void);
+void talk_disable_menus(void); /* disable voice menus (temporarily, not persisted) */
+void talk_enable_menus(void); /* re-enable voice menus */
 #endif /* __TALK_H__ */
