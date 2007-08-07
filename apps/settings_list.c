ef8aedd75a380be4e5a5ea74eb983f2425d395e5
Robert Keevil <rkeevil+rockbox@gmail.com>
2007-08-07 17:57:42 +0000

    Fix the headphone unplug autoresume menu entry
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@14235 a1c6a512-1295-4272-9138-f99709370657

--- apps/settings_list.c
+++ apps/settings_list.c
@@ -1129,9 +1129,9 @@ const struct settings_list settings[] = {
     CHOICE_SETTING(0, unplug_mode, LANG_HEADPHONE_UNPLUG, 0,
         "pause on headphone unplug", "off,pause,pause and resume", NULL, 3,
         ID2P(LANG_OFF), ID2P(LANG_PAUSE), ID2P(LANG_HEADPHONE_UNPLUG_RESUME)),
-    INT_SETTING(0, unplug_rw, LANG_HEADPHONE_UNPLUG_RW, 0, "rewind duration on pause",                      
+    INT_SETTING(0, unplug_rw, LANG_HEADPHONE_UNPLUG_RW, 0, "rewind duration on pause",
                     UNIT_SEC, 0, 15, 1, NULL, NULL,NULL) ,
-    OFFON_SETTING(0,unplug_autoresume,LANG_HEADPHONE_UNPLUG_RESUME,false,
+    OFFON_SETTING(0,unplug_autoresume,LANG_HEADPHONE_UNPLUG_DISABLE_AUTORESUME,false,
         "disable autoresume if phones not present",NULL),
 #endif
 #if CONFIG_TUNER
