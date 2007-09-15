b96ab07d6f93d80d7d3dd91930c620cd8ae1a940
Nils Wallménius <nils@rockbox.org>
2007-09-15 07:46:43 +0000

    Allow higher battery capacities to be set for ipod 3G, closes FS#7782
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@14707 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/config-ipod3g.h
+++ firmware/export/config-ipod3g.h
@@ -74,7 +74,7 @@
 
 #define BATTERY_CAPACITY_DEFAULT 630 /* default battery capacity */
 #define BATTERY_CAPACITY_MIN 630  /* min. capacity selectable */
-#define BATTERY_CAPACITY_MAX 1000 /* max. capacity selectable */
+#define BATTERY_CAPACITY_MAX 1200 /* max. capacity selectable */
 #define BATTERY_CAPACITY_INC 10   /* capacity increment */
 #define BATTERY_TYPES_COUNT  1    /* only one type */
 
