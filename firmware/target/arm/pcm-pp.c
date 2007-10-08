eb1e35f6af617b61a033fd6a43c2cb545d44c8e8
Thom Johansen <thomj@rockbox.org>
2007-10-08 21:20:06 +0000

    Make Ipod Nano use packed 16 bit IIS FIFO transfers as well.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@15040 a1c6a512-1295-4272-9138-f99709370657

--- firmware/target/arm/pcm-pp.c
+++ firmware/target/arm/pcm-pp.c
@@ -30,7 +30,7 @@
    and codec setup using "arithmetic" the hardware supports like halfword
    swapping. Try to use 32-bit packed in IIS modes if possible. */
 #if defined(SANSA_C200) || defined(SANSA_E200) \
-    || defined(IRIVER_H10) || defined(IRIVER_H10_5GB)
+    || defined(IRIVER_H10) || defined(IRIVER_H10_5GB) || defined(IPOD_NANO)
 /* 16-bit, L-R packed into 32 bits with left in the least significant halfword */
 #define SAMPLE_SIZE   16
 #define TRANSFER_SIZE 32
