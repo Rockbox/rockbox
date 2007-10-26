c935ed5112a0d7b400d8ae4ff974ff606b84c407
Michael Sevakis <jethead71@rockbox.org>
2007-10-26 23:16:41 +0000

    Argh. Forgot to 'svn add' as usual.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@15322 a1c6a512-1295-4272-9138-f99709370657

---
+++ uisimulator/sdl/system-sdl.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+ *             __________               __   ___.                  
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___  
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /  
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <   
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \  
+ *                     \/            \/     \/    \/            \/ 
+ * $Id$
+ *
+ * Copyright (C) 2007 by Michael Sevakis
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#ifndef _SYSTEM_SDL_H_
+#define _SYSTEM_SDL_H_
+
+#include <stdbool.h>
+
+#define HIGHEST_IRQ_LEVEL 1
+
+int set_irq_level(int level);
+void sim_enter_irq_handler(void);
+void sim_exit_irq_handler(void);
+bool sim_kernel_init(void);
+void sim_kernel_shutdown(void);
+
+#endif /* _SYSTEM_SDL_H_ */
