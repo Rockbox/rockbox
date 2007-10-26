f026c0fc826149a3c88d462cca02b69ef5690c30
Dave Chapman <dave@dchapman.com>
2007-10-26 21:17:37 +0000

    Remove unnecessary #include - this fixes compilation of the standalone demac tool
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@15320 a1c6a512-1295-4272-9138-f99709370657

--- apps/codecs/demac/libdemac/filter.c
+++ apps/codecs/demac/libdemac/filter.c
@@ -25,7 +25,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
 #include <string.h>
 #include <inttypes.h>
 
-#include "codecs.h" /* for Rockbox CPU definitions etc */
 #include "demac.h"
 #include "filter.h"
 
