3082a40b0a919c68fe03748e65e02d768cd3e4c9
Jens Arnold <amiconn@rockbox.org>
2007-11-24 11:30:11 +0000

    Fix warnings.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@15789 a1c6a512-1295-4272-9138-f99709370657

--- apps/gui/option_select.c
+++ apps/gui/option_select.c
@@ -18,6 +18,7 @@
  *
  ****************************************************************************/
 #include <stdlib.h>
+#include <string.h>
 #include "config.h"
 #include "option_select.h"
 #include "sprintf.h"
