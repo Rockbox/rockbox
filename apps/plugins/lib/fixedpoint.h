dae719dff712419b3770ed8532f71effa5b2e8c3
Marianne Arnold <pixelma@rockbox.org>
2008-01-22 12:44:24 +0000

    Revert today's fire commits because it crashed all the targets I could test with (OndioFM, M5, C250). The plugin action conversion also caused problems (e.g. in a c200 sim which was working) I couldn't control flame type and movement and couldn't exit.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@16142 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/lib/fixedpoint.h
+++ apps/plugins/lib/fixedpoint.h
@@ -24,11 +24,3 @@ long fsqrt(long a, unsigned int fracbits);
 long cos_int(int val);
 long sin_int(int val);
 long flog(int x);
-
-/* fast unsigned multiplication (16x16bit->32bit or 32x32bit->32bit,
- * whichever is faster for the architecture) */
-#ifdef CPU_ARM
-#define FMULU(a, b) ((uint32_t) (((uint32_t) (a)) * ((uint32_t) (b))))
-#else /* SH1, coldfire */
-#define FMULU(a, b) ((uint32_t) (((uint16_t) (a)) * ((uint16_t) (b))))
-#endif
