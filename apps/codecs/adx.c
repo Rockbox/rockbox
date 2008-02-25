2668547a554da72093e28d69ea1294fb471e1c7e
Adam Gashlin <agashlin@gmail.com>
2008-02-25 21:47:56 +0000

    Fix ADX decoder, old constant coefficients were for 44.1khz only, they are now calculated at runtime.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@16418 a1c6a512-1295-4272-9138-f99709370657

--- apps/codecs/adx.c
+++ apps/codecs/adx.c
@@ -6,7 +6,8 @@
  *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
  *                     \/            \/     \/    \/            \/
  *
- * Copyright (C) 2006-2007 Adam Gashlin (hcs)
+ * Copyright (C) 2006-2008 Adam Gashlin (hcs)
+ * Copyright (C) 2006 Jens Arnold
  *
  * All files in this archive are subject to the GNU General Public License.
  * See the file COPYING in the source tree root for full license agreement.
@@ -17,14 +18,12 @@
  ****************************************************************************/
 #include "codeclib.h"
 #include "inttypes.h"
+#include "math.h"
 
 CODEC_HEADER
 
 /* Maximum number of bytes to process in one iteration */
 #define WAV_CHUNK_SIZE (1024*2)
-
-/* Volume for ADX decoder */
-#define BASE_VOL 0x2000
 
 /* Number of times to loop looped tracks when repeat is disabled */
 #define LOOP_TIMES 2
@@ -32,7 +31,131 @@ CODEC_HEADER
 /* Length of fade-out for looped tracks (milliseconds) */
 #define FADE_LENGTH 10000L
 
+/* Default high pass filter cutoff frequency is 500 Hz.
+ * Others can be set, but the default is nearly always used,
+ * and there is no way to determine if another was used, anyway.
+ */
+const long cutoff = 500;
+
 static int16_t samples[WAV_CHUNK_SIZE] IBSS_ATTR;
+
+/* fixed point stuff from apps/plugins/lib/fixedpoint.c */
+
+/* Inverse gain of circular cordic rotation in s0.31 format. */
+static const long cordic_circular_gain = 0xb2458939; /* 0.607252929 */
+
+/* Table of values of atan(2^-i) in 0.32 format fractions of pi where pi = 0xffffffff / 2 */
+static const unsigned long atan_table[] = {
+    0x1fffffff, /* +0.785398163 (or pi/4) */
+    0x12e4051d, /* +0.463647609 */
+    0x09fb385b, /* +0.244978663 */
+    0x051111d4, /* +0.124354995 */
+    0x028b0d43, /* +0.062418810 */
+    0x0145d7e1, /* +0.031239833 */
+    0x00a2f61e, /* +0.015623729 */
+    0x00517c55, /* +0.007812341 */
+    0x0028be53, /* +0.003906230 */
+    0x00145f2e, /* +0.001953123 */
+    0x000a2f98, /* +0.000976562 */
+    0x000517cc, /* +0.000488281 */
+    0x00028be6, /* +0.000244141 */
+    0x000145f3, /* +0.000122070 */
+    0x0000a2f9, /* +0.000061035 */
+    0x0000517c, /* +0.000030518 */
+    0x000028be, /* +0.000015259 */
+    0x0000145f, /* +0.000007629 */
+    0x00000a2f, /* +0.000003815 */
+    0x00000517, /* +0.000001907 */
+    0x0000028b, /* +0.000000954 */
+    0x00000145, /* +0.000000477 */
+    0x000000a2, /* +0.000000238 */
+    0x00000051, /* +0.000000119 */
+    0x00000028, /* +0.000000060 */
+    0x00000014, /* +0.000000030 */
+    0x0000000a, /* +0.000000015 */
+    0x00000005, /* +0.000000007 */
+    0x00000002, /* +0.000000004 */
+    0x00000001, /* +0.000000002 */
+    0x00000000, /* +0.000000001 */
+    0x00000000, /* +0.000000000 */
+};
+
+/**
+ * Implements sin and cos using CORDIC rotation.
+ *
+ * @param phase has range from 0 to 0xffffffff, representing 0 and
+ *        2*pi respectively.
+ * @param cos return address for cos
+ * @return sin of phase, value is a signed value from LONG_MIN to LONG_MAX,
+ *         representing -1 and 1 respectively. 
+ */
+static long fsincos(unsigned long phase, long *cos)
+{
+    int32_t x, x1, y, y1;
+    unsigned long z, z1;
+    int i;
+
+    /* Setup initial vector */
+    x = cordic_circular_gain;
+    y = 0;
+    z = phase;
+
+    /* The phase has to be somewhere between 0..pi for this to work right */
+    if (z < 0xffffffff / 4) {
+        /* z in first quadrant, z += pi/2 to correct */
+        x = -x;
+        z += 0xffffffff / 4;
+    } else if (z < 3 * (0xffffffff / 4)) {
+        /* z in third quadrant, z -= pi/2 to correct */
+        z -= 0xffffffff / 4;
+    } else {
+        /* z in fourth quadrant, z -= 3pi/2 to correct */
+        x = -x;
+        z -= 3 * (0xffffffff / 4);
+    }
+
+    /* Each iteration adds roughly 1-bit of extra precision */
+    for (i = 0; i < 31; i++) {
+        x1 = x >> i;
+        y1 = y >> i;
+        z1 = atan_table[i];
+
+        /* Decided which direction to rotate vector. Pivot point is pi/2 */
+        if (z >= 0xffffffff / 4) {
+            x -= y1;
+            y += x1;
+            z -= z1;
+        } else {
+            x += y1;
+            y -= x1;
+            z += z1;
+        }
+    }
+
+    if (cos)
+        *cos = x;
+
+    return y;
+}
+
+/**
+ * Fixed point square root via Newton-Raphson.
+ * @param a square root argument.
+ * @param fracbits specifies number of fractional bits in argument.
+ * @return Square root of argument in same fixed point format as input. 
+ */
+static long fsqrt(long a, unsigned int fracbits)
+{
+    long b = a/2 + (1 << fracbits); /* initial approximation */
+    unsigned n;
+    const unsigned iterations = 8;  /* bumped up from 4 as it wasn't
+                                       nearly enough for 28 fractional bits */
+
+    for (n = 0; n < iterations; ++n)
+        b = (b + (long)(((long long)(a) << fracbits)/b))/2;
+
+    return b;
+}
 
 /* this is the codec entry point */
 enum codec_status codec_main(void)
@@ -50,6 +173,8 @@ enum codec_status codec_main(void)
     int fade_frames; /* length of fade in frames */
     off_t start_adr, end_adr; /* loop points */
     off_t chanstart, bufoff;
+    /*long coef1=0x7298L,coef2=-0x3350L;*/
+    long coef1, coef2;
 
     /* Generic codec initialisation */
     /* we only render 16 bits */
@@ -89,6 +214,46 @@ next_track:
     /* useful for seeking and reporting current playback position */
     avgbytespersec = ci->id3->frequency * 18 * channels / 32;
     DEBUGF("avgbytespersec=%ld\n",(unsigned long)avgbytespersec);
+
+    /* calculate filter coefficients */
+
+    /**
+     * A simple table of these coefficients would be nice, but
+     * some very odd frequencies are used and if I'm going to
+     * interpolate I might as well just go all the way and
+     * calclate them precisely.
+     * Speed is not an issue as this only needs to be done once per file.
+     */
+    {
+        const int64_t big28 = 0x10000000LL;
+        const int64_t big32 = 0x100000000LL;
+        int64_t frequency = ci->id3->frequency;
+        int64_t phasemultiple = cutoff*big32/frequency;
+
+        long z;
+        int64_t a;
+        const int64_t b = (M_SQRT2*big28)-big28;
+        int64_t c;
+        int64_t d;
+        
+        fsincos((unsigned long)phasemultiple,&z);
+
+        a = (M_SQRT2*big28)-(z*big28/LONG_MAX);
+
+        /**
+         * In the long passed to fsqrt there are only 4 nonfractional bits,
+         * which is sufficient here, but this is the only reason why I don't
+         * use 32 fractional bits everywhere.
+         */
+        d = fsqrt((a+b)*(a-b)/big28,28);
+        c = (a-d)*big28/b;
+
+        coef1 = (c*8192) >> 28;
+        coef2 = (c*c/big28*-4096) >> 28;
+        DEBUGF("ADX: samprate=%lld ",frequency);
+        DEBUGF("coef1 %04x ",(unsigned int)(coef1*4));
+        DEBUGF("coef2 %04x\n",(unsigned int)(coef2*-4));
+    }
 
     /* Get loop data */
     
@@ -248,13 +413,13 @@ next_track:
                 return CODEC_ERROR;
             }
 
-            scale = (((buf[0] << 8) | (buf[1])) +1) * BASE_VOL;
+            scale = ((buf[0] << 8) | (buf[1])) +1;
   
             for (i = 2; i < 18; i++)
             {
                 d = (buf[i] >> 4) & 15;
                 if (d & 8) d-= 16;
-                ch1_0 = (d*scale + 0x7298L*ch1_1 - 0x3350L*ch1_2) >> 14;
+                ch1_0 = d*scale + ((coef1*ch1_1 + coef2*ch1_2) >> 12);
 	            if (ch1_0 > 32767) ch1_0 = 32767;
                 else if (ch1_0 < -32768) ch1_0 = -32768;
 	            samples[sampleswritten] = ch1_0;
@@ -263,7 +428,7 @@ next_track:
 
                 d = buf[i] & 15;
                 if (d & 8) d -= 16;
-                ch1_0 = (d*scale + 0x7298L*ch1_1 - 0x3350L*ch1_2) >> 14;
+                ch1_0 = d*scale + ((coef1*ch1_1 + coef2*ch1_2) >> 12);
                 if (ch1_0 > 32767) ch1_0 = 32767;
                 else if (ch1_0 < -32768) ch1_0 = -32768; 
   	            samples[sampleswritten] = ch1_0;
@@ -286,7 +451,7 @@ next_track:
                     return CODEC_ERROR;
                 }
 
-                scale = (((buf[0] << 8)|(buf[1]))+1)*BASE_VOL;
+                scale = ((buf[0] << 8)|(buf[1]))+1;
   
                 sampleswritten-=63;
 
@@ -294,7 +459,7 @@ next_track:
                 {
                     d = (buf[i] >> 4) & 15;
                     if (d & 8) d-= 16;
-                    ch2_0 = (d*scale + 0x7298L*ch2_1 - 0x3350L*ch2_2) >> 14;
+                    ch2_0 = d*scale + ((coef1*ch2_1 + coef2*ch2_2) >> 12);
 	                if (ch2_0 > 32767) ch2_0 = 32767;
                     else if (ch2_0 < -32768) ch2_0 = -32768;
 	                samples[sampleswritten] = ch2_0;
@@ -303,7 +468,7 @@ next_track:
 
                     d = buf[i] & 15;
                     if (d & 8) d -= 16;
-                    ch2_0 = (d*scale + 0x7298L*ch2_1 - 0x3350L*ch2_2) >> 14;
+                    ch2_0 = d*scale + ((coef1*ch2_1 + coef2*ch2_2) >> 12);
                     if (ch2_0 > 32767) ch2_0 = 32767;
                     else if (ch2_0 < -32768) ch2_0 = -32768; 
   	                samples[sampleswritten] = ch2_0;
