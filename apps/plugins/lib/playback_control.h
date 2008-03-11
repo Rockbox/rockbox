c194b388fa04b2a89f361eea45e14f2796402306
Jonathan Gordon <rockbox@jdgordon.info>
2008-03-11 09:16:44 +0000

    pointers suck... spotted in IRC
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@16625 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/lib/playback_control.h
+++ apps/plugins/lib/playback_control.h
@@ -21,7 +21,7 @@
 
 /* Use these if your menu uses the new menu api. 
    REMEBER to call playback_control_init(rb) before rb->do_menu()... */
-extern const struct menu_item_ex *playback_control_menu;
+extern const struct menu_item_ex playback_control_menu;
 void playback_control_init(struct plugin_api* newapi);
 
 /* Use this if your menu still uses the old menu api */
