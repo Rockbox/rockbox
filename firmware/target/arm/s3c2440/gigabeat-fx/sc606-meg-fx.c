2541302089cc02d7d3ba4333aa73437a406d086d
Michael Sevakis <jethead71@rockbox.org>
2008-03-31 04:53:03 +0000

    meg-fx: Add headers to files in the target directory that were missing them. Fix the Id marker in a couple. Fix svn properties in some others. Please have a look at the copyrights added and make any corrections that you feel should be made since this particular tree branch has a twisted history so I just pasted them in based on the target header and used my name for files that have a preponderance of my junk.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@16895 a1c6a512-1295-4272-9138-f99709370657

--- firmware/target/arm/s3c2440/gigabeat-fx/sc606-meg-fx.c
+++ firmware/target/arm/s3c2440/gigabeat-fx/sc606-meg-fx.c
@@ -1,3 +1,21 @@
+/***************************************************************************
+*             __________               __   ___.
+*   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+*   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+*   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+*   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+*                     \/            \/     \/    \/            \/
+* $Id$
+*
+* Copyright (C) 2007 by Greg White
+*
+* All files in this archive are subject to the GNU General Public License.
+* See the file COPYING in the source tree root for full license agreement.
+*
+* This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+* KIND, either express or implied.
+*
+****************************************************************************/
 #include "config.h"
 #include "cpu.h"
 #include <stdbool.h>
