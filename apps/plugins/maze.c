Rockbox/rockbox

commit 80ad31c5e37a0fa053200c22a127269c30c04130

Keep the old colours for maze and rocklife.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@17382 a1c6a512-1295-4272-9138-f99709370657

apps/plugins/maze.c
@@ -484,7 +484,7 @@ enum plugin_status plugin_start(struct plugin_api* api, void* parameter)
     
 #if LCD_DEPTH > 1
     rb->lcd_set_backdrop(NULL);
-    rb->lcd_set_background(LCD_DEFAULT_BG);
+    rb->lcd_set_background(LCD_RGBPACK(182, 198, 229)); /* rockbox blue */
 #if LCD_DEPTH >= 16
     rb->lcd_set_foreground( LCD_RGBPACK( 0, 0, 0));
 #elif LCD_DEPTH == 2

apps/plugins/rocklife.c
@@ -409,7 +409,7 @@ enum plugin_status plugin_start(struct plugin_api* api, void* parameter)
     backlight_force_on(rb); /* backlight control in lib/helper.c */
 #if LCD_DEPTH > 1
     rb->lcd_set_backdrop(NULL);
-    rb->lcd_set_background(LCD_DEFAULT_BG);
+    rb->lcd_set_background(LCD_RGBPACK(182, 198, 229)); /* rockbox blue */
 #endif
 
     /* link pointers to grids */

commit 8d8576a54ff53b2ef1ea2eb31cde726fba324024

Fix maze and rocklife on greyscale targets which were broken by r17382. Maybe the colours or the code can be improved a bit, just get it working again for now.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@17390 a1c6a512-1295-4272-9138-f99709370657

apps/plugins/maze.c
@@ -484,11 +484,12 @@ enum plugin_status plugin_start(struct plugin_api* api, void* parameter)
     
 #if LCD_DEPTH > 1
     rb->lcd_set_backdrop(NULL);
-    rb->lcd_set_background(LCD_RGBPACK(182, 198, 229)); /* rockbox blue */
 #if LCD_DEPTH >= 16
     rb->lcd_set_foreground( LCD_RGBPACK( 0, 0, 0));
+    rb->lcd_set_background(LCD_RGBPACK(182, 198, 229)); /* rockbox blue */
 #elif LCD_DEPTH == 2
     rb->lcd_set_foreground(0);
+    rb->lcd_set_background(LCD_DEFAULT_BG);
 #endif
 #endif
     maze_init(&maze);

apps/plugins/rocklife.c
@@ -409,8 +409,12 @@ enum plugin_status plugin_start(struct plugin_api* api, void* parameter)
     backlight_force_on(rb); /* backlight control in lib/helper.c */
 #if LCD_DEPTH > 1
     rb->lcd_set_backdrop(NULL);
+#ifdef HAVE_LCD_COLOR
     rb->lcd_set_background(LCD_RGBPACK(182, 198, 229)); /* rockbox blue */
-#endif
+#else
+    rb->lcd_set_background(LCD_DEFAULT_BG);
+#endif /* HAVE_LCD_COLOR */
+#endif /* LCD_DEPTH > 1 */
 
     /* link pointers to grids */
     pgrid      = (char *)grid_a;