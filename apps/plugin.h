92dae7c50b888d41482a7a836770068f1293d30a
Jonathan Gordon <rockbox@jdgordon.info>
2008-05-15 07:03:12 +0000

    actually bump the API version
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@17521 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugin.h
+++ apps/plugin.h
@@ -120,12 +120,12 @@
 #define PLUGIN_MAGIC 0x526F634B /* RocK */
 
 /* increase this every time the api struct changes */
-#define PLUGIN_API_VERSION 112
+#define PLUGIN_API_VERSION 113
 
 /* update this to latest version if a change to the api struct breaks
    backwards compatibility (and please take the opportunity to sort in any
    new function which are "waiting" at the end of the function table) */
-#define PLUGIN_MIN_API_VERSION 112
+#define PLUGIN_MIN_API_VERSION 113
 
 /* plugin return codes */
 enum plugin_status {
