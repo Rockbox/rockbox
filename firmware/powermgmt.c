Rockbox/rockbox

commit 6f6fcfc3a8510d222981d856e81250561bcc5921

fix FS#8187 - charging breaks sleep timer. Now if the timer goes off and the player is charging or connected to USB it will turn off the audio, but stay on
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@17872 a1c6a512-1295-4272-9138-f99709370657

firmware/powermgmt.c
@@ -462,21 +462,23 @@ static void handle_auto_poweroff(void)
     else
     {
         /* Handle sleeptimer */
-        if(sleeptimer_active && !usb_inserted())
+        if(sleeptimer_active)
         {
             if(TIME_AFTER(current_tick, sleeptimer_endtick))
             {
                 audio_stop();
+                if (!usb_inserted()
 #if CONFIG_CHARGING && !defined(HAVE_POWEROFF_WHILE_CHARGING)
-                if((charger_input_state == CHARGER) ||
-                   (charger_input_state == CHARGER_PLUGGED))
+                    || ((charger_input_state == CHARGER) ||
+                    (charger_input_state == CHARGER_PLUGGED))
+#endif
+                   )
                 {
                     DEBUGF("Sleep timer timeout. Stopping...\n");
                     set_sleep_timer(0);
                     backlight_off(); /* Nighty, nighty... */
                 }
                 else
-#endif
                 {
                     DEBUGF("Sleep timer timeout. Shutting off...\n");
                     sys_poweroff();

commit 5cb85a81d298d4f8d27b7e7e5e0038f2032c75fa

fix the sleeptimer which got broken in r17872
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@18006 a1c6a512-1295-4272-9138-f99709370657

firmware/powermgmt.c
@@ -467,7 +467,7 @@ static void handle_auto_poweroff(void)
             if(TIME_AFTER(current_tick, sleeptimer_endtick))
             {
                 audio_stop();
-                if (!usb_inserted()
+                if (usb_inserted()
 #if CONFIG_CHARGING && !defined(HAVE_POWEROFF_WHILE_CHARGING)
                     || ((charger_input_state == CHARGER) ||
                     (charger_input_state == CHARGER_PLUGGED))