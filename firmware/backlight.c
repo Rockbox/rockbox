1a904eb1ebec1b92d9bba7b1f471f51832ea073d
Bertrik Sikken <bertrik@sikken.nl>
2008-09-15 20:01:07 +0000

    Revert v18522 for firmware/backlight.c, it breaks a couple of bootloaders
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@18523 a1c6a512-1295-4272-9138-f99709370657

--- firmware/backlight.c
+++ firmware/backlight.c
@@ -860,6 +860,7 @@ void backlight_init(void)
 
 void backlight_on(void) {}
 void backlight_off(void) {}
+void buttonlight_on(void) {}
 void backlight_set_timeout(int value) {(void)value;}
 
 bool is_backlight_on(bool ignore_always_off)
