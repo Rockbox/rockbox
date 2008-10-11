3c65d2b17f3f76e22629d1e78c2897d2840ad67f
Dave Chapman <dave@dchapman.com>
2008-10-11 13:11:47 +0000

    Fix another bug in dualboot.S, add Rafael Carre as the (C) holder of dualboot.S (most of the code there is his) and use the standard Rockbox header in mkamsboot.c.  This code is still untested on target.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@18771 a1c6a512-1295-4272-9138-f99709370657

--- rbutil/mkamsboot/mkamsboot.c
+++ rbutil/mkamsboot/mkamsboot.c
@@ -1,26 +1,26 @@
-/*
-
-mkamsboot.c - a tool for merging bootloader code into an Sansa V2
-              (AMS) firmware file
-
-Copyright (C) Dave Chapman 2008
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
-
-*/
-
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id:$
+ *
+ * mkamsboot.c - a tool for merging bootloader code into an Sansa V2
+ *               (AMS) firmware file
+ *
+ * Copyright (C) 2008 Dave Chapman
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
 
 /*
 
