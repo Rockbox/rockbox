e511017293264eb514721c073451232c2945241e
Magnus Holmgren <magnushol@gmail.com>
2008-10-26 20:11:26 +0000

    Bump plugin API version as needed.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@18890 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugin.h
+++ apps/plugin.h
@@ -131,7 +131,7 @@ void* plugin_get_buffer(size_t *buffer_size);
 #define PLUGIN_MAGIC 0x526F634B /* RocK */
 
 /* increase this every time the api struct changes */
-#define PLUGIN_API_VERSION 125
+#define PLUGIN_API_VERSION 126
 
 /* update this to latest version if a change to the api struct breaks
    backwards compatibility (and please take the opportunity to sort in any
