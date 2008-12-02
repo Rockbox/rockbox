c1cd0469ca9f084b39d747ccca5d64442c3833ca
Jens Arnold <amiconn@rockbox.org>
2008-12-02 02:26:04 +0000

    Implement mono predictor in assembler for coldfire, yielding a ~6% speedup for mono -c1000. Apply ideas gained from it back to the stereo predictor, saving 4 instructions. No speed increase for stereo, probably due to cache aliasing effects. * 80-column police.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@19296 a1c6a512-1295-4272-9138-f99709370657

--- apps/codecs/demac/libdemac/predictor.c
+++ apps/codecs/demac/libdemac/predictor.c
@@ -211,6 +211,7 @@ void ICODE_ATTR_DEMAC predictor_decode_stereo(struct predictor_t* p,
 }
 #endif
 
+#if !defined(CPU_COLDFIRE)
 void ICODE_ATTR_DEMAC predictor_decode_mono(struct predictor_t* p,
                                             int32_t* decoded0,
                                             int count)
@@ -269,3 +270,4 @@ void ICODE_ATTR_DEMAC predictor_decode_mono(struct predictor_t* p,
 
     p->YlastA = currentA;
 }
+#endif
