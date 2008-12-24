be26da8662f19b0c72c98bbe9eeb00b0468af23c
Mark Arigo <markarigo@gmail.com>
2008-12-24 03:45:32 +0000

    Remove some unused code in the Philips SA9200 port.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@19573 a1c6a512-1295-4272-9138-f99709370657

--- firmware/target/arm/philips/sa9200/adc-target.h
+++ firmware/target/arm/philips/sa9200/adc-target.h
@@ -21,25 +21,7 @@
 #ifndef _ADC_TARGET_H_
 #define _ADC_TARGET_H_
 
-/* ADC channels */
-#define NUM_ADC_CHANNELS 13
-
-#define ADC_BVDD         0  /* Battery voltage of 4V LiIo accumulator */
-#define ADC_RTCSUP       1  /* RTC backup battery voltage */
-#define ADC_UVDD         2  /* USB host voltage */
-#define ADC_CHG_IN       3  /* Charger input voltage */
-#define ADC_CVDD         4  /* Charger pump output voltage */
-#define ADC_BATTEMP      5  /* Battery charging temperature */
-#define ADC_MICSUP1      6  /* Voltage on MicSup1 for remote control
-                                or external voltage measurement */
-#define ADC_MICSUP2      7  /* Voltage on MicSup1 for remote control
-                                or external voltage measurement */
-#define ADC_VBE1         8  /* Measuring junction temperature @ 2uA */
-#define ADC_VBE2         9  /* Measuring junction temperature @ 1uA */
-#define ADC_I_MICSUP1    10 /* Current of MicSup1 for remote control detection */
-#define ADC_I_MICSUP2    11 /* Current of MicSup2 for remote control detection */
-#define ADC_VBAT         12 /* Single cell battery voltage */
-
-#define ADC_UNREG_POWER  ADC_BVDD   /* For compatibility */
+/* The ADC sources and channels are common to all targets with AS3514 */
+#include "as3514.h"
 
 #endif
