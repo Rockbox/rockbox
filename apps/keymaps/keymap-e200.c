d4b99fdfe06bf5f30f4c3a68f08aedaaa6f432f9
Andrew Mahone <andrew.mahone@gmail.com>
2009-01-29 10:56:06 +0000

    radio keymap fix for e200 from FS#9846
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@19879 a1c6a512-1295-4272-9138-f99709370657

--- apps/keymaps/keymap-e200.c
+++ apps/keymaps/keymap-e200.c
@@ -227,12 +227,16 @@ static const struct button_mapping button_context_recscreen[]  = {
 
 /** FM Radio Screen **/
 static const struct button_mapping button_context_radio[]  = {
+    { ACTION_STD_PREV,       BUTTON_LEFT,                       BUTTON_NONE },
+    { ACTION_STD_PREVREPEAT, BUTTON_LEFT|BUTTON_REPEAT,         BUTTON_NONE },
+    { ACTION_STD_NEXT,       BUTTON_RIGHT,                      BUTTON_NONE },
+    { ACTION_STD_NEXTREPEAT, BUTTON_RIGHT|BUTTON_REPEAT,        BUTTON_NONE },
     { ACTION_FM_MENU,        BUTTON_SELECT|BUTTON_REPEAT,       BUTTON_NONE },
     { ACTION_FM_PRESET,      BUTTON_SELECT|BUTTON_REL,          BUTTON_NONE },
-    { ACTION_FM_STOP,        BUTTON_POWER|BUTTON_REL,           BUTTON_NONE  },
-    { ACTION_FM_MODE,        BUTTON_UP|BUTTON_REPEAT,           BUTTON_UP },
+    { ACTION_FM_STOP,        BUTTON_POWER|BUTTON_REL,           BUTTON_NONE },
+    { ACTION_FM_MODE,        BUTTON_UP|BUTTON_REPEAT,           BUTTON_UP   },
     { ACTION_FM_EXIT,        BUTTON_DOWN|BUTTON_REL,            BUTTON_NONE },
-    { ACTION_FM_PLAY,        BUTTON_UP|BUTTON_REL,              BUTTON_UP },
+    { ACTION_FM_PLAY,        BUTTON_UP|BUTTON_REL,              BUTTON_UP   },
     LAST_ITEM_IN_LIST__NEXTLIST(CONTEXT_SETTINGS)
 }; /* button_context_radio */
 
