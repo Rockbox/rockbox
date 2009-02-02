3ca9946cb1198272bddff3b0824868507289ab5a
Maurus Cuelenaere <mcuelenaere@gmail.com>
2009-02-02 00:19:32 +0000

    Checkwps: the scripts of course shouldn't be searching for output files in rootdir..
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@19901 a1c6a512-1295-4272-9138-f99709370657

--- tools/checkwps/buildall.sh
+++ tools/checkwps/buildall.sh
@@ -4,7 +4,7 @@ rootdir=`dirname $0`
 cat $rootdir/targets.txt | (
     while read target model
     do
-        rm -f $rootdir/checkwps.$model
+        rm -f checkwps.$model
         make -s -C $rootdir MODEL=$model TARGET=$target checkwps
     done
 )
--- tools/checkwps/cleanall.sh
+++ tools/checkwps/cleanall.sh
@@ -4,6 +4,6 @@ rootdir=`dirname $0`
 cat $rootdir/targets.txt | (
     while read target model
     do
-        rm -f $rootdir/checkwps.$model
+        rm -f checkwps.$model
     done
 )
