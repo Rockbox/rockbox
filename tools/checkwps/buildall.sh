304aefe80256429e8c1cbdbb5afd1693d90041aa
Maurus Cuelenaere <mcuelenaere@gmail.com>
2009-02-02 00:35:58 +0000

    Checkwps: move output files to current directory
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@19902 a1c6a512-1295-4272-9138-f99709370657

--- tools/checkwps/buildall.sh
+++ tools/checkwps/buildall.sh
@@ -6,5 +6,6 @@ cat $rootdir/targets.txt | (
     do
         rm -f checkwps.$model
         make -s -C $rootdir MODEL=$model TARGET=$target checkwps
+        mv $rootdir/checkwps.$model ./checkwps.$model > /dev/null 2>&1
     done
 )
