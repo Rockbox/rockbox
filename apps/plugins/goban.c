b51785008b02e1d7c6d5b7fff8948bb48282bc18
Mustapha Senhaji <moos@rockbox.org>
2009-02-21 00:08:57 +0000

    And now with the added file...
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@20071 a1c6a512-1295-4272-9138-f99709370657

---
+++ apps/plugins/goban.c
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2007-2009 Joshua Simmons <mud at majidejima dot com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#include "plugin.h"
+
+#if PLUGIN_BUFFER_SIZE < 0x10000 && !defined(SIMULATOR)
+
+#include "lib/overlay.h"
+
+PLUGIN_HEADER
+
+enum plugin_status plugin_start(const void* parameter)
+{
+    return run_overlay(parameter, PLUGIN_GAMES_DIR "/goban.ovl", "Goban");
+}
+#endif
