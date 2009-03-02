3fe6152a1a6095e41426709d4a2eb3780529d518
Barry Wardell <rockbox@barrywardell.net>
2009-03-02 22:42:13 +0000

    Make the H10 5GB bootloader able to load the OF again. From FS#9955.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@20185 a1c6a512-1295-4272-9138-f99709370657

--- firmware/target/arm/iriver/h10/lcd-h10_5gb.c
+++ firmware/target/arm/iriver/h10/lcd-h10_5gb.c
@@ -108,7 +108,11 @@ void lcd_set_flip(bool yesno)
 /* LCD init */
 void lcd_init_device(void)
 {  
+#ifndef BOOTLOADER
+    /* The OF won't boot if this is done in the bootloader - ideally we should 
+       tweak the lcd controller speed settings but this will do for now */
     CLCD_CLOCK_SRC |= 0xc0000000; /* Set LCD interface clock to PLL */
+#endif
     /* H10 LCD is initialised by the bootloader */
 }
 
