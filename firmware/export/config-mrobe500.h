a50e17a6baeddee9a3c936e78436420df10f9854
Karl Kurbjun <kkurbjun@gmail.com>
2009-04-10 03:51:16 +0000

    Enable Albumart for M:Robe 500
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@20673 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/config-mrobe500.h
+++ firmware/export/config-mrobe500.h
@@ -42,7 +42,7 @@
 #define HAVE_LCD_COLOR
 
 /* define this if you want album art for this target */
-//#define HAVE_ALBUMART
+#define HAVE_ALBUMART
 
 /* define this if you have access to the quickscreen */
 #define HAVE_QUICKSCREEN
@@ -161,7 +161,6 @@
 /* Define this if hardware supports alternate blitting */
 #define HAVE_LCD_MODES LCD_MODE_RGB565 | LCD_MODE_YUV | LCD_MODE_PAL256
 
-/* Define this if you have a Motorola SCF5249 */
 #define CONFIG_CPU DM320
 
 #define CONFIG_I2C I2C_DM320
