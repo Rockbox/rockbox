f85c0c67def1184dcd8f7e850391233a2d3e88ce
Dave Chapman <dave@dchapman.com>
2009-05-10 18:11:09 +0000

    Revert r20704 and only use the -r option when compiling binutils.  This is good enough to fix compilation of the toolchains on OSX, but doesn't break compiling the m68k gcc, which makes use of built-ins and hence doesn't work with -r.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@20899 a1c6a512-1295-4272-9138-f99709370657

--- tools/rockboxdev.sh
+++ tools/rockboxdev.sh
@@ -24,9 +24,9 @@ builddir="${RBDEV_BUILD:-/tmp/rbdev-build}"
 # by running the "make" command, on most BSD systems, GNU Make is invoked
 # by running the "gmake" command. Set the "make" variable accordingly.
 if [ -f "`which gmake 2>/dev/null`" ]; then
-    make="gmake -r"
+    make="gmake"
 else
-    make="make -r"
+    make="make"
 fi
 
 if [ -z $GNU_MIRROR ] ; then
@@ -295,7 +295,9 @@ echo "ROCKBOXDEV: binutils/configure"
 # Ubuntu installations
 CFLAGS=-U_FORTIFY_SOURCE ../binutils-$binutils/configure --target=$target --prefix=$prefix/$target $binutilsconf
 echo "ROCKBOXDEV: binutils/make"
-$make
+# We add -r when building binutils to fix compilation on OSX - Apple's make has
+# extra built-ins which cause problems.
+$make -r
 echo "ROCKBOXDEV: binutils/make install to $prefix/$target"
 $make install
 cd .. # get out of build-binu-$1
