0e725551af7c4b337aebcb8d922dba34cbb20565
Maurus Cuelenaere <mcuelenaere@gmail.com>
2009-05-21 12:29:51 +0000

    Replace some TABs with spaces (no functional changes)
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@21009 a1c6a512-1295-4272-9138-f99709370657

--- firmware/target/mips/ingenic_jz47xx/debug-jz4740.c
+++ firmware/target/mips/ingenic_jz47xx/debug-jz4740.c
@@ -54,93 +54,93 @@ static void printf(const char *format, ...)
 
 static void display_clocks(void)
 {
-	unsigned int cppcr = REG_CPM_CPPCR;  /* PLL Control Register */
-	unsigned int cpccr = REG_CPM_CPCCR;  /* Clock Control Register */
-	unsigned int div[] = {1, 2, 3, 4, 6, 8, 12, 16, 24, 32};
-	unsigned int od[4] = {1, 2, 2, 4};
+    unsigned int cppcr = REG_CPM_CPPCR;  /* PLL Control Register */
+    unsigned int cpccr = REG_CPM_CPCCR;  /* Clock Control Register */
+    unsigned int div[] = {1, 2, 3, 4, 6, 8, 12, 16, 24, 32};
+    unsigned int od[4] = {1, 2, 2, 4};
 
-	printf("CPPCR   :    0x%08x", cppcr);
-	printf("CPCCR   :    0x%08x", cpccr);
-	printf("PLL     :    %s", (cppcr & CPM_CPPCR_PLLEN) ? "ON" : "OFF");
-	printf("m:n:o   :    %d:%d:%d",
-			__cpm_get_pllm() + 2,
-			__cpm_get_plln() + 2,
-			od[__cpm_get_pllod()]
-		);
-	printf("C:H:M:P :   %d:%d:%d:%d", 
-			div[__cpm_get_cdiv()],
-			div[__cpm_get_hdiv()],
-			div[__cpm_get_mdiv()],
-			div[__cpm_get_pdiv()]
-		);
-    printf("U:L:I:P:M : %d:%d:%d:%d:%d",
-			__cpm_get_udiv() + 1,
-			__cpm_get_ldiv() + 1,
-			__cpm_get_i2sdiv()+1,
-			__cpm_get_pixdiv()+1,
-			__cpm_get_mscdiv()+1
+    printf("CPPCR   :    0x%08x", cppcr);
+    printf("CPCCR   :    0x%08x", cpccr);
+    printf("PLL     :    %s", (cppcr & CPM_CPPCR_PLLEN) ? "ON" : "OFF");
+    printf("m:n:o   :    %d:%d:%d",
+            __cpm_get_pllm() + 2,
+            __cpm_get_plln() + 2,
+            od[__cpm_get_pllod()]
         );
-	printf("PLL Freq:    %3d.%02d MHz", TO_MHZ(__cpm_get_pllout()));
-	printf("CCLK    :    %3d.%02d MHz", TO_MHZ(__cpm_get_cclk()));
-	printf("HCLK    :    %3d.%02d MHz", TO_MHZ(__cpm_get_hclk()));
-	printf("MCLK    :    %3d.%02d MHz", TO_MHZ(__cpm_get_mclk()));
-	printf("PCLK    :    %3d.%02d MHz", TO_MHZ(__cpm_get_pclk()));
-	printf("LCDCLK  :    %3d.%02d MHz", TO_MHZ(__cpm_get_lcdclk()));
-	printf("PIXCLK  : %6d.%02d KHz",    TO_KHZ(__cpm_get_pixclk()));
-	printf("I2SCLK  :    %3d.%02d MHz", TO_MHZ(__cpm_get_i2sclk()));
-	printf("USBCLK  :    %3d.%02d MHz", TO_MHZ(__cpm_get_usbclk()));
-	printf("MSCCLK  :    %3d.%02d MHz", TO_MHZ(__cpm_get_mscclk()));
-	printf("EXTALCLK:    %3d.%02d MHz", TO_MHZ(__cpm_get_extalclk()));
-	printf("RTCCLK  :    %3d.%02d KHz", TO_KHZ(__cpm_get_rtcclk()));
+    printf("C:H:M:P :   %d:%d:%d:%d", 
+            div[__cpm_get_cdiv()],
+            div[__cpm_get_hdiv()],
+            div[__cpm_get_mdiv()],
+            div[__cpm_get_pdiv()]
+        );
+    printf("U:L:I:P:M : %d:%d:%d:%d:%d",
+            __cpm_get_udiv() + 1,
+            __cpm_get_ldiv() + 1,
+            __cpm_get_i2sdiv()+1,
+            __cpm_get_pixdiv()+1,
+            __cpm_get_mscdiv()+1
+        );
+    printf("PLL Freq:    %3d.%02d MHz", TO_MHZ(__cpm_get_pllout()));
+    printf("CCLK    :    %3d.%02d MHz", TO_MHZ(__cpm_get_cclk()));
+    printf("HCLK    :    %3d.%02d MHz", TO_MHZ(__cpm_get_hclk()));
+    printf("MCLK    :    %3d.%02d MHz", TO_MHZ(__cpm_get_mclk()));
+    printf("PCLK    :    %3d.%02d MHz", TO_MHZ(__cpm_get_pclk()));
+    printf("LCDCLK  :    %3d.%02d MHz", TO_MHZ(__cpm_get_lcdclk()));
+    printf("PIXCLK  : %6d.%02d KHz",    TO_KHZ(__cpm_get_pixclk()));
+    printf("I2SCLK  :    %3d.%02d MHz", TO_MHZ(__cpm_get_i2sclk()));
+    printf("USBCLK  :    %3d.%02d MHz", TO_MHZ(__cpm_get_usbclk()));
+    printf("MSCCLK  :    %3d.%02d MHz", TO_MHZ(__cpm_get_mscclk()));
+    printf("EXTALCLK:    %3d.%02d MHz", TO_MHZ(__cpm_get_extalclk()));
+    printf("RTCCLK  :    %3d.%02d KHz", TO_KHZ(__cpm_get_rtcclk()));
 }
 
 static void display_enabled_clocks(void)
 {
-	unsigned long lcr = REG_CPM_LCR;
-	unsigned long clkgr = REG_CPM_CLKGR;
+    unsigned long lcr = REG_CPM_LCR;
+    unsigned long clkgr = REG_CPM_CLKGR;
 
-	printf("Low Power Mode : %s", 
-			((lcr & CPM_LCR_LPM_MASK) == CPM_LCR_LPM_IDLE) ?
-			"IDLE" : (((lcr & CPM_LCR_LPM_MASK) == CPM_LCR_LPM_SLEEP) ? "SLEEP" : "HIBERNATE")
+    printf("Low Power Mode : %s", 
+            ((lcr & CPM_LCR_LPM_MASK) == CPM_LCR_LPM_IDLE) ?
+            "IDLE" : (((lcr & CPM_LCR_LPM_MASK) == CPM_LCR_LPM_SLEEP) ? "SLEEP" : "HIBERNATE")
           );
-    
-	printf("Doze Mode      : %s", 
-			(lcr & CPM_LCR_DOZE_ON) ? "ON" : "OFF");
-	if (lcr & CPM_LCR_DOZE_ON)
-		printf("     duty      : %d", (int)((lcr & CPM_LCR_DOZE_DUTY_MASK) >> CPM_LCR_DOZE_DUTY_BIT));
-    
-	printf("IPU            : %s",
-			(clkgr & CPM_CLKGR_IPU) ? "stopped" : "running");
-	printf("DMAC           : %s",
-			(clkgr & CPM_CLKGR_DMAC) ? "stopped" : "running");
-	printf("UHC            : %s",
-			(clkgr & CPM_CLKGR_UHC) ? "stopped" : "running");
-	printf("UDC            : %s",
-			(clkgr & CPM_CLKGR_UDC) ? "stopped" : "running");
-	printf("LCD            : %s",
-			(clkgr & CPM_CLKGR_LCD) ? "stopped" : "running");
-	printf("CIM            : %s",
-			(clkgr & CPM_CLKGR_CIM) ? "stopped" : "running");
-	printf("SADC           : %s",
-			(clkgr & CPM_CLKGR_SADC) ? "stopped" : "running");
-	printf("MSC            : %s",
-			(clkgr & CPM_CLKGR_MSC) ? "stopped" : "running");
-	printf("AIC1           : %s",
-			(clkgr & CPM_CLKGR_AIC1) ? "stopped" : "running");
-	printf("AIC2           : %s",
-			(clkgr & CPM_CLKGR_AIC2) ? "stopped" : "running");
-	printf("SSI            : %s",
-			(clkgr & CPM_CLKGR_SSI) ? "stopped" : "running");
-	printf("I2C            : %s",
-			(clkgr & CPM_CLKGR_I2C) ? "stopped" : "running");
-	printf("RTC            : %s",
-			(clkgr & CPM_CLKGR_RTC) ? "stopped" : "running");
-	printf("TCU            : %s",
-			(clkgr & CPM_CLKGR_TCU) ? "stopped" : "running");
-	printf("UART1          : %s",
-			(clkgr & CPM_CLKGR_UART1) ? "stopped" : "running");
-	printf("UART0          : %s",
-			(clkgr & CPM_CLKGR_UART0) ? "stopped" : "running");
+
+    printf("Doze Mode      : %s", 
+            (lcr & CPM_LCR_DOZE_ON) ? "ON" : "OFF");
+    if (lcr & CPM_LCR_DOZE_ON)
+        printf("     duty      : %d", (int)((lcr & CPM_LCR_DOZE_DUTY_MASK) >> CPM_LCR_DOZE_DUTY_BIT));
+
+    printf("IPU            : %s",
+            (clkgr & CPM_CLKGR_IPU) ? "stopped" : "running");
+    printf("DMAC           : %s",
+            (clkgr & CPM_CLKGR_DMAC) ? "stopped" : "running");
+    printf("UHC            : %s",
+            (clkgr & CPM_CLKGR_UHC) ? "stopped" : "running");
+    printf("UDC            : %s",
+            (clkgr & CPM_CLKGR_UDC) ? "stopped" : "running");
+    printf("LCD            : %s",
+            (clkgr & CPM_CLKGR_LCD) ? "stopped" : "running");
+    printf("CIM            : %s",
+            (clkgr & CPM_CLKGR_CIM) ? "stopped" : "running");
+    printf("SADC           : %s",
+            (clkgr & CPM_CLKGR_SADC) ? "stopped" : "running");
+    printf("MSC            : %s",
+            (clkgr & CPM_CLKGR_MSC) ? "stopped" : "running");
+    printf("AIC1           : %s",
+            (clkgr & CPM_CLKGR_AIC1) ? "stopped" : "running");
+    printf("AIC2           : %s",
+            (clkgr & CPM_CLKGR_AIC2) ? "stopped" : "running");
+    printf("SSI            : %s",
+            (clkgr & CPM_CLKGR_SSI) ? "stopped" : "running");
+    printf("I2C            : %s",
+            (clkgr & CPM_CLKGR_I2C) ? "stopped" : "running");
+    printf("RTC            : %s",
+            (clkgr & CPM_CLKGR_RTC) ? "stopped" : "running");
+    printf("TCU            : %s",
+            (clkgr & CPM_CLKGR_TCU) ? "stopped" : "running");
+    printf("UART1          : %s",
+            (clkgr & CPM_CLKGR_UART1) ? "stopped" : "running");
+    printf("UART0          : %s",
+            (clkgr & CPM_CLKGR_UART0) ? "stopped" : "running");
 }
 
 bool __dbg_ports(void)
@@ -155,7 +155,7 @@ bool __dbg_hw_info(void)
     int touch;
 #endif
     struct tm *cur_time;
-    
+
     lcd_setfont(FONT_SYSFIXED);
     while(btn ^ BUTTON_POWER)
     {
