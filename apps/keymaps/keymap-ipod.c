d76c820587d9990c1d3c97fdd68f7345f2072851
Jonathan Gordon <rockbox@jdgordon.info>
2009-05-31 05:48:09 +0000

    fix FS#10261 - the files context menu (as an example) would get skipped and the first items context menu get shown instead
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@21140 a1c6a512-1295-4272-9138-f99709370657

--- apps/keymaps/keymap-ipod.c
+++ apps/keymaps/keymap-ipod.c
@@ -51,7 +51,7 @@ static const struct button_mapping button_context_standard[]  = {
     { ACTION_STD_OK,            BUTTON_SELECT|BUTTON_REL,           BUTTON_SELECT },
     { ACTION_STD_MENU,          BUTTON_MENU|BUTTON_REL,             BUTTON_MENU },
     { ACTION_STD_QUICKSCREEN,   BUTTON_MENU|BUTTON_REPEAT,          BUTTON_MENU },
-    { ACTION_STD_CONTEXT,       BUTTON_SELECT|BUTTON_REPEAT,        BUTTON_NONE },
+    { ACTION_STD_CONTEXT,       BUTTON_SELECT|BUTTON_REPEAT,        BUTTON_SELECT },
     { ACTION_STD_CANCEL,        BUTTON_PLAY|BUTTON_REPEAT,          BUTTON_NONE },
 
     LAST_ITEM_IN_LIST
