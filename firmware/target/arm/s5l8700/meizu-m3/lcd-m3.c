aecf2b12dbf59f4e1c6e394f454d142053444319
Bertrik Sikken <bertrik@sikken.nl>
2009-06-22 19:46:09 +0000

    Meizu lcd-m3: whitespace fixes
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@21469 a1c6a512-1295-4272-9138-f99709370657

--- firmware/target/arm/s5l8700/meizu-m3/lcd-m3.c
+++ firmware/target/arm/s5l8700/meizu-m3/lcd-m3.c
@@ -74,30 +74,30 @@ static void lcd_sleep(uint32_t t)
 
 static uint8_t lcd_readdata()
 {
-        LCD_RDATA = 0;
-        lcd_sleep(64);
-        return (LCD_DBUFF/* & 0xff*/);
+    LCD_RDATA = 0;
+    lcd_sleep(64);
+    return (LCD_DBUFF/* & 0xff*/);
 }
 
 static void lcd_writereg(uint32_t reg, uint32_t data)
 {
-        LCD_WCMD = reg >> 8;
-	LCD_WCMD = reg & 0xff;
-	LCD_WDATA = data >> 8;
-	LCD_WDATA = data & 0xff;
+    LCD_WCMD = reg >> 8;
+    LCD_WCMD = reg & 0xff;
+    LCD_WDATA = data >> 8;
+    LCD_WDATA = data & 0xff;
 }
 
 void lcd_on() {
     if (lcd_type == 1) {
         LCD_WCMD = 0x29;    
     } else {
-	lcd_writereg(0x7, 0x21);
-	lcd_writereg(0x12, 0x137);
-	lcd_sleep(70000);
-	lcd_writereg(0x7, 0x21);
-	lcd_writereg(0x12, 0x1137);
-	lcd_sleep(700000);
-	lcd_writereg(0x7, 0x233);
+        lcd_writereg(0x7, 0x21);
+        lcd_writereg(0x12, 0x137);
+        lcd_sleep(70000);
+        lcd_writereg(0x7, 0x21);
+        lcd_writereg(0x12, 0x1137);
+        lcd_sleep(700000);
+        lcd_writereg(0x7, 0x233);
     }
 }
 
@@ -105,7 +105,7 @@ void lcd_off() {
     /* FIXME wait for DMA to finnish */
     if (lcd_type == 1) {
         LCD_WCMD = 0x28;
-	LCD_WDATA = 0;
+        LCD_WDATA = 0;
     } else {
     
     }
@@ -129,23 +129,23 @@ void lcd_init_device(void)
 
 /* detect lcd type, it's not detected the first time for some reason */
     for(i=0;i<3;++i) {
-	LCD_WCMD = 0x1;
-	lcd_sleep(166670);
-	LCD_WCMD = 0x11;
-	lcd_sleep(2000040);
-	lcd_readdata();
-	LCD_WCMD = 0x4;
-	lcd_sleep(100);
-	data[0]=lcd_readdata();
-	data[1]=lcd_readdata();
-	data[2]=lcd_readdata();
-	data[3]=lcd_readdata();
-	data[4]=lcd_readdata();
+        LCD_WCMD = 0x1;
+        lcd_sleep(166670);
+        LCD_WCMD = 0x11;
+        lcd_sleep(2000040);
+        lcd_readdata();
+        LCD_WCMD = 0x4;
+        lcd_sleep(100);
+        data[0]=lcd_readdata();
+        data[1]=lcd_readdata();
+        data[2]=lcd_readdata();
+        data[3]=lcd_readdata();
+        data[4]=lcd_readdata();
 
-	lcd_type=0;
-	if (((data[1]==0x38) && ((data[2] & 0xf0) == 0x80)) ||
-    	    ((data[2]==0x38) && ((data[3] & 0xf0) == 0x80)))
-    	    lcd_type=1;
+        lcd_type=0;
+        if (((data[1]==0x38) && ((data[2] & 0xf0) == 0x80)) ||
+            ((data[2]==0x38) && ((data[3] & 0xf0) == 0x80)))
+            lcd_type=1;
     }
 /* init lcd */
     if (lcd_type == 1) {
@@ -168,69 +168,69 @@ void lcd_init_device(void)
         LCD_WCMD = 0x0;
         LCD_WCMD = 0x0;
         lcd_sleep(700000);
-	lcd_writereg(0xa4, 0x1);
+        lcd_writereg(0xa4, 0x1);
         lcd_sleep(1100000);
-	lcd_writereg(0x1, 0x100);
-	lcd_writereg(0x2, 0x300);
-	lcd_writereg(0x3, 0x9230);
-	lcd_writereg(0x8, 0x404);
-	lcd_writereg(0xe, 0x10);
-	lcd_writereg(0x70, 0x1000);
-	lcd_writereg(0x71, 0x1);
-	lcd_writereg(0x30, 0x2);
-	lcd_writereg(0x31, 0x400);
-	lcd_writereg(0x32, 0x7);
-	lcd_writereg(0x33, 0x500);
-	lcd_writereg(0x34, 0x7);
-	lcd_writereg(0x35, 0x703);
-	lcd_writereg(0x36, 0x507);
-	lcd_writereg(0x37, 0x5);
-	lcd_writereg(0x38, 0x1404);
-	lcd_writereg(0x39, 0xe);
-	lcd_writereg(0x40, 0x202);
-	lcd_writereg(0x41, 0x3);
-	lcd_writereg(0x42, 0x0);
-	lcd_writereg(0x43, 0x200);
-	lcd_writereg(0x44, 0x707);
-	lcd_writereg(0x45, 0x407);
-	lcd_writereg(0x46, 0x505);
-	lcd_writereg(0x47, 0x2);
-	lcd_writereg(0x48, 0x4);
-	lcd_writereg(0x49, 0x4);
-	lcd_writereg(0x60, 0x202);
-	lcd_writereg(0x61, 0x3);
-	lcd_writereg(0x62, 0x0);
-	lcd_writereg(0x63, 0x200);
-	lcd_writereg(0x64, 0x707);
-	lcd_writereg(0x65, 0x407);
-	lcd_writereg(0x66, 0x505);
-	lcd_writereg(0x67, 0x2);
-	lcd_writereg(0x68, 0x4);
-	lcd_writereg(0x69, 0x4);
-	lcd_writereg(0x7, 0x1);
-	lcd_writereg(0x18, 0x1);
-	lcd_writereg(0x10, 0x1690);
-	lcd_writereg(0x11, 0x100);
-	lcd_writereg(0x12, 0x117);
-	lcd_writereg(0x13, 0xf80);
-	lcd_writereg(0x12, 0x137);
-	lcd_writereg(0x20, 0x0);
-	lcd_writereg(0x21, 0x0);
-	lcd_writereg(0x50, 0x0);
-	lcd_writereg(0x51, 0xaf);
-	lcd_writereg(0x52, 0x0);
-	lcd_writereg(0x53, 0x83);
-	lcd_writereg(0x90, 0x0);
-	lcd_writereg(0x91, 0x0);
-	lcd_writereg(0x92, 0x0);
-	lcd_writereg(0x98, 0x0);
-	lcd_writereg(0x99, 0x903);
-	lcd_writereg(0x9a, 0x502);
-	lcd_writereg(0x9b, 0x300);
-	LCD_WCMD = 0x0;
-	LCD_WCMD = 0x22;
-	lcd_sleep(700000);
-	lcd_on();
+        lcd_writereg(0x1, 0x100);
+        lcd_writereg(0x2, 0x300);
+        lcd_writereg(0x3, 0x9230);
+        lcd_writereg(0x8, 0x404);
+        lcd_writereg(0xe, 0x10);
+        lcd_writereg(0x70, 0x1000);
+        lcd_writereg(0x71, 0x1);
+        lcd_writereg(0x30, 0x2);
+        lcd_writereg(0x31, 0x400);
+        lcd_writereg(0x32, 0x7);
+        lcd_writereg(0x33, 0x500);
+        lcd_writereg(0x34, 0x7);
+        lcd_writereg(0x35, 0x703);
+        lcd_writereg(0x36, 0x507);
+        lcd_writereg(0x37, 0x5);
+        lcd_writereg(0x38, 0x1404);
+        lcd_writereg(0x39, 0xe);
+        lcd_writereg(0x40, 0x202);
+        lcd_writereg(0x41, 0x3);
+        lcd_writereg(0x42, 0x0);
+        lcd_writereg(0x43, 0x200);
+        lcd_writereg(0x44, 0x707);
+        lcd_writereg(0x45, 0x407);
+        lcd_writereg(0x46, 0x505);
+        lcd_writereg(0x47, 0x2);
+        lcd_writereg(0x48, 0x4);
+        lcd_writereg(0x49, 0x4);
+        lcd_writereg(0x60, 0x202);
+        lcd_writereg(0x61, 0x3);
+        lcd_writereg(0x62, 0x0);
+        lcd_writereg(0x63, 0x200);
+        lcd_writereg(0x64, 0x707);
+        lcd_writereg(0x65, 0x407);
+        lcd_writereg(0x66, 0x505);
+        lcd_writereg(0x67, 0x2);
+        lcd_writereg(0x68, 0x4);
+        lcd_writereg(0x69, 0x4);
+        lcd_writereg(0x7, 0x1);
+        lcd_writereg(0x18, 0x1);
+        lcd_writereg(0x10, 0x1690);
+        lcd_writereg(0x11, 0x100);
+        lcd_writereg(0x12, 0x117);
+        lcd_writereg(0x13, 0xf80);
+        lcd_writereg(0x12, 0x137);
+        lcd_writereg(0x20, 0x0);
+        lcd_writereg(0x21, 0x0);
+        lcd_writereg(0x50, 0x0);
+        lcd_writereg(0x51, 0xaf);
+        lcd_writereg(0x52, 0x0);
+        lcd_writereg(0x53, 0x83);
+        lcd_writereg(0x90, 0x0);
+        lcd_writereg(0x91, 0x0);
+        lcd_writereg(0x92, 0x0);
+        lcd_writereg(0x98, 0x0);
+        lcd_writereg(0x99, 0x903);
+        lcd_writereg(0x9a, 0x502);
+        lcd_writereg(0x9b, 0x300);
+        LCD_WCMD = 0x0;
+        LCD_WCMD = 0x22;
+        lcd_sleep(700000);
+        lcd_on();
     }
 }
 
@@ -273,21 +273,21 @@ void lcd_update(void)
     /* Copy display bitmap to hardware */
     if (lcd_type == 1) {
         LCD_WCMD = 0x2a;
-	LCD_WDATA = 0;
-	LCD_WDATA = 0;
-	LCD_WDATA = 0;
-	LCD_WDATA = 0xaf;
+        LCD_WDATA = 0;
+        LCD_WDATA = 0;
+        LCD_WDATA = 0;
+        LCD_WDATA = 0xaf;
         LCD_WCMD = 0x2b;
-	LCD_WDATA = 0;
-	LCD_WDATA = 0;
-	LCD_WDATA = 0;
-	LCD_WDATA = 0x83;
-	LCD_WCMD = 0x2c;
+        LCD_WDATA = 0;
+        LCD_WDATA = 0;
+        LCD_WDATA = 0;
+        LCD_WDATA = 0x83;
+        LCD_WCMD = 0x2c;
     } else {
-	lcd_writereg(0x20, 0x0);
-	lcd_writereg(0x21, 0x0);
-	LCD_WCMD = 0;
-	LCD_WCMD = 0x22;
+        lcd_writereg(0x20, 0x0);
+        lcd_writereg(0x21, 0x0);
+        LCD_WCMD = 0;
+        LCD_WCMD = 0x22;
     }
     for(p=&lcd_framebuffer[0][0], i=0;i<LCD_WIDTH*LCD_FBHEIGHT;++i, ++p) {
         LCD_WDATA = RGB_UNPACK_RED(*p);
