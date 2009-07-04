861b8d8606059de2f7527e9429dc109e8b89c03c
Maurus Cuelenaere <mcuelenaere@gmail.com>
2009-07-04 12:32:48 +0000

    Set svn:keywords property
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@21634 a1c6a512-1295-4272-9138-f99709370657

--- apps/fixedpoint.h
+++ apps/fixedpoint.h
@@ -5,7 +5,7 @@
  *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
  *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
  *                     \/            \/     \/    \/            \/
- * $Id: fixedpoint.h -1   $
+ * $Id$
  *
  * Copyright (C) 2006 Jens Arnold
  *
