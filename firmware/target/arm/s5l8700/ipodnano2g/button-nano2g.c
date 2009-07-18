9be28179e467274e97b809bd45ca1ef34104a98d
Dave Chapman <dave@dchapman.com>
2009-07-18 11:36:35 +0000

    Nano2g button driver - currently only implementing the hold switch.  Should have been part of previous commit.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@21949 a1c6a512-1295-4272-9138-f99709370657

---
+++ firmware/target/arm/s5l8700/ipodnano2g/button-nano2g.c
@@ -0,0 +1,40 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id: button-m3.c 21787 2009-07-11 20:00:07Z bertrik $
+ *
+ * Copyright (C) 2009 Dave Chapman
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include <stdbool.h>
+#include "config.h"
+
+#include "s5l8700.h"
+#include "button-target.h"
+
+void button_init_device(void)
+{
+}
+
+int button_read_device(void)
+{
+}
+
+bool button_hold(void)
+{
+    return ((PDAT14 & (1 << 6)) == 0);
+}
+
