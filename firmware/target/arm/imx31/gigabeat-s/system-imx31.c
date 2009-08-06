a26ea3f67a52eabd9dc3c25eaa402d5959d9bd03
Bertrik Sikken <bertrik@sikken.nl>
2009-08-06 09:44:09 +0000

    Revert static-ing of iim_prod_rev, this function is never used so it causes a compiler warning.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@22185 a1c6a512-1295-4272-9138-f99709370657

--- firmware/target/arm/imx31/gigabeat-s/system-imx31.c
+++ firmware/target/arm/imx31/gigabeat-s/system-imx31.c
@@ -41,7 +41,7 @@ unsigned int iim_system_rev(void)
     return system_rev & IIM_SREV_SREV;
 }
 
-static unsigned int iim_prod_rev(void)
+unsigned int iim_prod_rev(void)
 {
     return product_rev;
 }
