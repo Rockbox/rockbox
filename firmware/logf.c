Rockbox/rockbox

commit 9a5174c8a3a1a2a358be57ecb72fa2740b10cd0d

Fix logf() multilines handling
Each line (of MAX_LOGF_ENTRY characters) would be marked with
LOGF_TERMINATE_CONTINUE_LINE, but still be padded with a '\0'

This also reverts r22250 which worked around the problem in logfdump()

Flyspray: FS#10513
Author: Amaury Pouly

git-svn-id: svn://svn.rockbox.org/rockbox/trunk@22253 a1c6a512-1295-4272-9138-f99709370657

firmware/logf.c
@@ -145,16 +145,17 @@ void _logf(const char *format, ...)
     while(len > MAX_LOGF_ENTRY)
     {
         ptr = logfbuffer[logfindex];
-        strlcpy(ptr, buf + tlen, MAX_LOGF_ENTRY);
+        memcpy(ptr, buf + tlen, MAX_LOGF_ENTRY);
         ptr[MAX_LOGF_ENTRY] = LOGF_TERMINATE_CONTINUE_LINE;
         logfindex++;
         check_logfindex();
-        len -= MAX_LOGF_ENTRY-1;
-        tlen += MAX_LOGF_ENTRY-1;
+        len -= MAX_LOGF_ENTRY;
+        tlen += MAX_LOGF_ENTRY;
         multiline = true;
     }
+
     ptr = logfbuffer[logfindex];
-    strcpy(ptr, buf + tlen);
+    memcpy(ptr, buf + tlen,len-tlen);
 
     if(len < MAX_LOGF_ENTRY)
         /* pad with spaces up to the MAX_LOGF_ENTRY byte border */

apps/logfdisp.c
@@ -190,8 +190,8 @@ bool logfdump(void)
             ptr = buffer;
             do {
                 tindex++;
-                memcpy(ptr, logfbuffer[tindex], MAX_LOGF_ENTRY-1);
-                ptr += MAX_LOGF_ENTRY-1;
+                memcpy(ptr, logfbuffer[tindex], MAX_LOGF_ENTRY);
+                ptr += MAX_LOGF_ENTRY;
                 if (tindex >= MAX_LOGF_LINES)
                     tindex = 0;
             } while(logfbuffer[tindex][MAX_LOGF_ENTRY] == LOGF_TERMINATE_CONTINUE_LINE);

commit 27934e26b7b8ea470db4c92936e61c835635ac0b

Fix a wrong memcpy in logf() introduced in r22253
Flyspray: FS#10515
Author: Amaury Pouly

git-svn-id: svn://svn.rockbox.org/rockbox/trunk@22276 a1c6a512-1295-4272-9138-f99709370657

firmware/logf.c
@@ -155,7 +155,7 @@ void _logf(const char *format, ...)
     }
 
     ptr = logfbuffer[logfindex];
-    memcpy(ptr, buf + tlen,len-tlen);
+    memcpy(ptr, buf + tlen,len);
 
     if(len < MAX_LOGF_ENTRY)
         /* pad with spaces up to the MAX_LOGF_ENTRY byte border */