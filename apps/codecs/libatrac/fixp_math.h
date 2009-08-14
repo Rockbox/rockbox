4f26112b1b50b644c3c3245906c996c16bb0c616
Mohamed Tarek <mt@rockbox.org>
2009-08-14 17:31:08 +0000

    Revert the asm fixmul stuff for now.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@22310 a1c6a512-1295-4272-9138-f99709370657

--- apps/codecs/libatrac/fixp_math.h
+++ apps/codecs/libatrac/fixp_math.h
@@ -1,9 +1,5 @@
 #include <stdlib.h>
-#ifdef ROCKBOX
-#include "asm_arm.h"
-#include "asm_mcf5249.h"
-#include "codeclib_misc.h"
-#endif
+#include <inttypes.h>
 
 /* Macros for converting between various fixed-point representations and floating point. */
 #define ONE_16 (1L << 16)
@@ -13,12 +9,7 @@
 #define fix31tof64(x)     (float)((float)(x) / (float)(1 << 31))
 
 /* Fixed point math routines for use in atrac3.c */
-#ifdef ROCKBOX
-#define fixmul31(x,y) (MULT31(x,y))
-#define fixmul16(x,y) (MULT32(x,y))
-#else
 inline int32_t fixmul16(int32_t x, int32_t y);
 inline int32_t fixmul31(int32_t x, int32_t y);
-#endif /* ROCKBOX */
 inline int32_t fixdiv16(int32_t x, int32_t y);
 inline int32_t fastSqrt(int32_t n);
