Rockbox/rockbox

commit 4c48b59be8ba41b80350d5329d0f13240229da0a

User definable UI viewport, to be able to restrict the UI into a viewport for all bitmap displays.
Flyspray: FS#8799

This will allow for pretty themes, for example those with nice glass effects on their backdrops (some might argue they're wasting screen space but it's upto them),
as well as allowing for future background WPS updates in the main UI.

Plugins are not converted yet, they simply use the full screen. Ideally, any plugin that does *not* want the UI viewport, should take care of that itself (i.e. plugins should normally use the UI viewport).

git-svn-id: svn://svn.rockbox.org/rockbox/trunk@22365 a1c6a512-1295-4272-9138-f99709370657

apps/main.c
@@ -72,6 +72,7 @@
 #include "eeprom_settings.h"
 #include "scrobbler.h"
 #include "icon.h"
+#include "viewport.h"
 
 #ifdef IPOD_ACCESSORY_PROTOCOL
 #include "iap.h"
@@ -135,9 +136,8 @@ static void app_main(void)
         screens[i].update();
     }
     tree_gui_init();
-    viewportmanager_set_statusbar(VP_SB_ALLSCREENS);
-    add_event(GUI_EVENT_STATUSBAR_TOGGLE, false, 
-              viewportmanager_statusbar_changed);
+    gui_syncstatusbar_init(&statusbars);
+    viewportmanager_init();
 #ifdef HAVE_USBSTACK
     /* All threads should be created and public queues registered by now */
     usb_start_monitoring();
@@ -296,8 +296,6 @@ static void init(void)
 #ifdef DEBUG
     debug_init();
 #endif
-    /* Must be done before any code uses the multi-screen APi */
-    gui_syncstatusbar_init(&statusbars);
     storage_init();
     settings_reset();
     settings_load(SETTINGS_ALL);
@@ -418,9 +416,6 @@ static void init(void)
 #if CONFIG_TUNER
     radio_init();
 #endif
-
-    /* Must be done before any code uses the multi-screen APi */
-    gui_syncstatusbar_init(&statusbars);
 
 #if CONFIG_CHARGING && (CONFIG_CPU == SH7034)
     /* charger_inserted() can't be used here because power_thread()

apps/appevents.h
@@ -48,9 +48,9 @@ enum {
 
 /** Generic GUI class events **/
 enum {
-    GUI_EVENT_THEME_CHANGED = (EVENT_CLASS_GUI|1),
-    GUI_EVENT_STATUSBAR_TOGGLE,
+    GUI_EVENT_STATUSBAR_TOGGLE = (EVENT_CLASS_GUI|1),
     GUI_EVENT_ACTIONUPDATE,
+    GUI_EVENT_REFRESH,
 };
 
 #endif

apps/debug_menu.c
@@ -100,6 +100,7 @@
 #if CONFIG_RTC == RTC_PCF50605
 #include "pcf50605.h"
 #endif
+#include "appevents.h"
 
 #if CONFIG_CPU == DM320 || CONFIG_CPU == S3C2440 || CONFIG_CPU == TCC7801 \
     || CONFIG_CPU == IMX31L || CONFIG_CPU == AS3525 || CONFIG_CPU == JZ4732
@@ -2790,6 +2791,7 @@ static int menu_action_callback(int btn, struct gui_synclist *lists)
         int oldbars = viewportmanager_set_statusbar(VP_SB_HIDE_ALL);
         menuitems[gui_synclist_get_sel_pos(lists)].function();
         btn = ACTION_REDRAW;
+        send_event(GUI_EVENT_REFRESH, NULL);
         viewportmanager_set_statusbar(oldbars);
     }
     return btn;

apps/filetree.c
@@ -181,6 +181,11 @@ static void check_file_thumbnails(struct tree_context* c)
     closedir(dir);
 }
 
+static void filetree_drawlists(void)
+{
+    gui_synclist_draw(&tree_lists);
+}
+
 /* support function for qsort() */
 static int compare(const void* p1, const void* p2)
 {
@@ -508,7 +513,13 @@ int ft_enter(struct tree_context* c)
                 splash(0, ID2P(LANG_WAIT));
                 if (!settings_load_config(buf,true))
                     break;
-                gui_synclist_draw(&tree_lists);
+                /* do both steps seperately so that the redrawing after theme
+                 * changing is independant of whether the theme has a custom ui
+                 * vp or not */
+                send_event(GUI_EVENT_REFRESH, NULL);
+                /* for the statusbar */
+                send_event(GUI_EVENT_ACTIONUPDATE, (void*)true);
+                filetree_drawlists();
                 splash(HZ, ID2P(LANG_SETTINGS_LOADED));
                 break;
 
@@ -617,6 +628,8 @@ int ft_enter(struct tree_context* c)
                 break;
             }
         }
+
+        send_event(GUI_EVENT_REFRESH, filetree_drawlists);
 
         if ( play ) {
             /* the resume_index must always be the index in the

apps/gui/list.c
@@ -55,29 +55,37 @@ static int offset_step = 16; /* pixels per screen scroll step */
 /* should lines scroll out of the screen */
 static bool offset_out_of_view = false;
 #endif
-static int force_list_reinit = false;
 
 static void gui_list_select_at_offset(struct gui_synclist * gui_list,
                                       int offset);
 void list_draw(struct screen *display, struct gui_synclist *list);
 
 #ifdef HAVE_LCD_BITMAP
-static struct viewport parent[NB_SCREENS];
-void list_init_viewports(struct gui_synclist *list)
+static void list_init_viewports(struct gui_synclist *list)
 {
+    struct viewport* vp;
     int i;
-    struct viewport *vp;
-    FOR_NB_SCREENS(i)
+    bool parent_used = (*list->parent != NULL);
+    if (!parent_used)
     {
-        vp = &parent[i];
-        if (!list || list->parent[i] == vp)
-            viewport_set_defaults(vp, i);
+        vp = viewport_get_current_vp();
+        FOR_NB_SCREENS(i)
+            list->parent[i] = &vp[i];
     }
 #ifdef HAVE_BUTTONBAR
-    if (list && (list->parent[0] == &parent[0]) && global_settings.buttonbar)
+    if (list && !parent_used[SCREEN_MAIN] && global_settings.buttonbar)
         list->parent[0]->height -= BUTTONBAR_HEIGHT;
 #endif
-    force_list_reinit = false;
+}
+#else
+#define list_init_viewports(a)
+#endif
+
+#ifdef HAVE_LCD_BITMAP
+bool list_display_title(struct gui_synclist *list, enum screen_type screen)
+{
+    return list->title != NULL &&
+        viewport_get_nb_lines(list->parent[screen])>2;
 }
 #else
 static struct viewport parent[NB_SCREENS] =
@@ -90,20 +98,6 @@ static struct viewport parent[NB_SCREENS] =
         .height   = LCD_HEIGHT
     },
 };
-void list_init_viewports(struct gui_synclist *list)
-{
-    (void)list;
-    force_list_reinit = false;
-}
-#endif
-
-#ifdef HAVE_LCD_BITMAP
-bool list_display_title(struct gui_synclist *list, enum screen_type screen)
-{
-    return list->title != NULL &&
-        viewport_get_nb_lines(list->parent[screen])>2;
-}
-#else
 #define list_display_title(l, i) false
 #endif
 
@@ -141,9 +135,12 @@ void gui_synclist_init(struct gui_synclist * gui_list,
         if (list_parent)
             gui_list->parent[i] = &list_parent[i];
         else
-        {
-            gui_list->parent[i] = &parent[i];
-        }
+            gui_list->parent[i] =
+#ifdef HAVE_LCD_BITMAP
+                                    NULL;
+#else
+                                    &parent[i];
+#endif
     }
     list_init_viewports(gui_list);
     gui_list->limit_scroll = false;
@@ -162,7 +159,6 @@ void gui_synclist_init(struct gui_synclist * gui_list,
     gui_list->title_color = -1;
     gui_list->callback_get_item_color = NULL;
 #endif
-    force_list_reinit = true;
 }
 
 /* this toggles the selection bar or cursor */
@@ -218,7 +214,7 @@ void gui_synclist_draw(struct gui_synclist *gui_list)
 #ifdef HAVE_BUTTONBAR
     static bool last_buttonbar = false;
 #endif
-    if (force_list_reinit ||
+    if (
 #ifdef HAVE_BUTTONBAR
         last_buttonbar != screens[SCREEN_MAIN].has_buttonbar ||
 #endif
@@ -436,7 +432,6 @@ void gui_synclist_set_title(struct gui_synclist * gui_list,
     } else {
         gui_list->title_width = 0;
     }
-    force_list_reinit = true;
 }
 
 

apps/gui/list.h
@@ -139,8 +139,6 @@ extern void gui_list_screen_scroll_step(int ofs);
 extern void gui_list_screen_scroll_out_of_view(bool enable);
 #endif /* HAVE_LCD_BITMAP */
 
-void list_init_viewports(struct gui_synclist * lists);
-
 extern void gui_synclist_init(
     struct gui_synclist * lists,
     list_get_name callback_get_item_name,

apps/gui/statusbar.c
@@ -263,7 +263,7 @@ void gui_statusbar_draw(struct gui_statusbar * bar, bool force_redraw)
         memcmp(&(bar->info), &(bar->lastinfo), sizeof(struct status_info)))
     {
         struct viewport vp;
-        viewport_set_defaults(&vp, display->screen_type);
+        viewport_set_fullscreen(&vp, display->screen_type);
         vp.height = STATUSBAR_HEIGHT;
         vp.x = STATUSBAR_X_POS;
         if (statusbar_position(display->screen_type) != STATUSBAR_BOTTOM)    

apps/gui/viewport.c
@@ -56,6 +56,22 @@
 
 static int statusbar_enabled = 0;
 
+#ifdef HAVE_LCD_BITMAP
+
+static struct {
+    struct  viewport* vp;
+    int     active;
+} ui_vp_info;
+
+static struct viewport custom_vp[NB_SCREENS];
+
+/* callbacks for GUI_EVENT_* events */
+static void viewportmanager_ui_vp_changed(void *param);
+static void statusbar_toggled(void* param);
+static int viewport_init_ui_vp(void);
+#endif
+static void viewportmanager_redraw(void* data);
+
 int viewport_get_nb_lines(struct viewport *vp)
 {
 #ifdef HAVE_LCD_BITMAP
@@ -78,9 +94,9 @@ static bool showing_bars(enum screen_type screen)
 #endif
     }
     return false;
-}    
+}
 
-void viewport_set_defaults(struct viewport *vp, enum screen_type screen)
+void viewport_set_fullscreen(struct viewport *vp, enum screen_type screen)
 {
     vp->x = 0;
     vp->width = screens[screen].lcdwidth;
@@ -120,6 +136,30 @@ void viewport_set_defaults(struct viewport *vp, enum screen_type screen)
 #endif
 }
 
+void viewport_set_defaults(struct viewport *vp, enum screen_type screen)
+{
+#ifdef HAVE_LCD_BITMAP
+    if (ui_vp_info.active)
+        *vp = custom_vp[screen];
+    else
+#endif
+        viewport_set_fullscreen(vp, screen);
+}
+
+void viewportmanager_init(void)
+{
+    viewportmanager_set_statusbar(VP_SB_ALLSCREENS);
+#ifdef HAVE_LCD_BITMAP
+    add_event(GUI_EVENT_STATUSBAR_TOGGLE, false, statusbar_toggled);
+    ui_vp_info.active = viewport_init_ui_vp();
+    ui_vp_info.vp = custom_vp;
+#endif
+}
+
+int viewportmanager_get_statusbar(void)
+{
+    return statusbar_enabled;
+}
 
 int viewportmanager_set_statusbar(int enabled)
 {
@@ -133,37 +173,101 @@ int viewportmanager_set_statusbar(int enabled)
             if (showing_bars(i))
                 gui_statusbar_draw(&statusbars.statusbars[i], true);
         }
-        add_event(GUI_EVENT_ACTIONUPDATE, false, viewportmanager_draw_statusbars);
+        add_event(GUI_EVENT_ACTIONUPDATE, false, viewportmanager_redraw);
     }
     else
     {
-        remove_event(GUI_EVENT_ACTIONUPDATE, viewportmanager_draw_statusbars);
+        remove_event(GUI_EVENT_ACTIONUPDATE, viewportmanager_redraw);
     }
     return old;
 }
 
-void viewportmanager_draw_statusbars(void* data)
+static void viewportmanager_redraw(void* data)
 {
     int i;
 
     FOR_NB_SCREENS(i)
     {
         if (showing_bars(i))
-            gui_statusbar_draw(&statusbars.statusbars[i], (bool)data);
+            gui_statusbar_draw(&statusbars.statusbars[i], NULL != data);
+    }
+}
+#ifdef HAVE_LCD_BITMAP
+
+static void statusbar_toggled(void* param)
+{
+    (void)param;
+    /* update vp manager for the new setting and reposition vps
+     * if necessary */
+    viewportmanager_theme_changed(THEME_STATUSBAR);
+}
+
+void viewportmanager_theme_changed(int which)
+{
+    if (which & THEME_UI_VIEWPORT)
+    {
+        /* reset the ui viewport */
+        if ((ui_vp_info.active = viewport_init_ui_vp()))
+            add_event(GUI_EVENT_REFRESH, false, viewportmanager_ui_vp_changed);
+        else
+            remove_event(GUI_EVENT_REFRESH, viewportmanager_ui_vp_changed);
+        /* and point to it */
+        ui_vp_info.vp = custom_vp;
+    }
+    if (which & THEME_STATUSBAR)
+    {
+        statusbar_enabled = 0;
+        if (global_settings.statusbar != STATUSBAR_OFF)
+            statusbar_enabled = VP_SB_ONSCREEN(SCREEN_MAIN);
+#ifdef HAVE_REMOTE_LCD
+        if (global_settings.remote_statusbar != STATUSBAR_OFF)
+            statusbar_enabled |= VP_SB_ONSCREEN(SCREEN_REMOTE);
+#endif
+        if (statusbar_enabled)
+            add_event(GUI_EVENT_ACTIONUPDATE, false, viewportmanager_redraw);
+        else
+            remove_event(GUI_EVENT_ACTIONUPDATE, viewportmanager_redraw);
+
+        /* reposition viewport to fit statusbar, only if not using the ui vp */
+        if (!ui_vp_info.active)
+        {
+            int i;
+            FOR_NB_SCREENS(i)
+                viewport_set_fullscreen(&custom_vp[i], i);
+        }
     }
 }
 
-void viewportmanager_statusbar_changed(void* data)
+static void viewportmanager_ui_vp_changed(void *param)
 {
-    (void)data;
-    statusbar_enabled = 0;
-    if (global_settings.statusbar != STATUSBAR_OFF)
-        statusbar_enabled = VP_SB_ONSCREEN(SCREEN_MAIN);
-#ifdef HAVE_REMOTE_LCD
-    if (global_settings.remote_statusbar != STATUSBAR_OFF)
-        statusbar_enabled |= VP_SB_ONSCREEN(SCREEN_REMOTE);
-#endif
+    /* if the user changed the theme, we need to initiate a full redraw */
+    int i;
+    /* cast param to a function */
+    void (*draw_func)(void) = ((void(*)(void))param);
+    /* start with clearing the screen */
+    FOR_NB_SCREENS(i)
+        screens[i].clear_display();
+    /* redraw the statusbar if it was enabled */
     viewportmanager_set_statusbar(statusbar_enabled);
+    /* call the passed function which will redraw the content of
+     * the current screen */
+    if (param != NULL)
+        draw_func();
+    FOR_NB_SCREENS(i)
+        screens[i].update();
+}
+
+void viewport_set_current_vp(struct viewport* vp)
+{
+    if (vp != NULL)
+        ui_vp_info.vp = vp;
+    else
+        ui_vp_info.vp = custom_vp;
+}
+
+struct viewport* viewport_get_current_vp(void)
+{
+    return ui_vp_info.vp;
 }
 
 #ifdef HAVE_LCD_COLOR
@@ -172,7 +276,6 @@ void viewportmanager_statusbar_changed(void* data)
 #define ARG_STRING(_depth) "dddddgg"
 #endif
 
-#ifdef HAVE_LCD_BITMAP
 const char* viewport_parse_viewport(struct viewport *vp,
                                     enum screen_type screen,
                                     const char *bufptr,
@@ -213,9 +316,9 @@ const char* viewport_parse_viewport(struct viewport *vp,
             return NULL;
     }
     else
-#undef ARG_STRING
 #endif
     {}
+#undef ARG_STRING
 
     /* X and Y *must* be set */
     if (!LIST_VALUE_PARSED(set, PL_X) || !LIST_VALUE_PARSED(set, PL_Y))
@@ -257,6 +360,42 @@ const char* viewport_parse_viewport(struct viewport *vp,
     vp->drawmode = DRMODE_SOLID;
 
     return ptr;
+}
+
+/*
+ * (re)parse the UI vp from the settings
+ *  - Returns
+ *          0 if no UI vp is used
+ *          >0 if it's used at least partly (on multiscreen targets)
+ *          NB_SCREENS if all screens have a UI vp
+ */
+static int viewport_init_ui_vp(void)
+{
+    int screen, ret = NB_SCREENS;
+    FOR_NB_SCREENS(screen)
+    {
+#ifdef HAVE_REMOTE_LCD
+        if ((screen == SCREEN_REMOTE))
+        {
+            if(!(viewport_parse_viewport(&custom_vp[screen], screen,
+                     global_settings.remote_ui_vp_config, ',')))
+            {
+                viewport_set_fullscreen(&custom_vp[screen], screen);
+                ret--;
+            }
+        }
+        else
+#endif
+        {
+            if (!(viewport_parse_viewport(&custom_vp[screen], screen,
+                     global_settings.ui_vp_config, ',')))
+            {
+                viewport_set_fullscreen(&custom_vp[screen], screen);
+                ret--;
+            }
+        }
+    }
+    return ret;
 }
 
 #endif /* HAVE_LCD_BITMAP */

commit baef536e29b53a8ced5c6158163ec72144893edd

Fix crash in early USB introduced in r22365 by moving the gui_syncstatusbar_init() back to where it needs to be. The comment clearly stated that it needs to happen that early.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@22903 a1c6a512-1295-4272-9138-f99709370657

apps/main.c
@@ -139,7 +139,6 @@ static void app_main(void)
     list_init();
 #endif
     tree_gui_init();
-    gui_syncstatusbar_init(&statusbars);
     viewportmanager_init();
 #ifdef HAVE_USBSTACK
     /* All threads should be created and public queues registered by now */
@@ -310,6 +309,8 @@ static void init(void)
 #ifdef DEBUG
     debug_init();
 #endif
+    /* Must be done before any code uses the multi-screen API */
+    gui_syncstatusbar_init(&statusbars);
     storage_init();
     settings_reset();
     settings_load(SETTINGS_ALL);
@@ -430,6 +431,9 @@ static void init(void)
 #if CONFIG_TUNER
     radio_init();
 #endif
+
+    /* Must be done before any code uses the multi-screen API */
+    gui_syncstatusbar_init(&statusbars);
 
 #if CONFIG_CHARGING && (CONFIG_CPU == SH7034)
     /* charger_inserted() can't be used here because power_thread()