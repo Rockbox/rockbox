7220cbb890d8ad2a8047f960f5a3d7c27c9e06a9
Mohamed Tarek <mt@rockbox.org>
2009-10-09 01:05:40 +0000

    Fix a bug in a bidirectional text scrolling; s->offset doesn't need to be set when backward scrolling starts. This fixes bidirectional scrolling in RTL and for certain file-names in LTR.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@23022 a1c6a512-1295-4272-9138-f99709370657

--- firmware/drivers/lcd-bitmap-common.c
+++ firmware/drivers/lcd-bitmap-common.c
@@ -321,7 +321,6 @@ void LCDFN(scroll_fn)(void)
             }
             if (abs(s->offset) >= s->width - (current_vp->width - xpos)) {
                 /* at end of line */
-                s->offset = s->width - (current_vp->width - xpos);
                 s->backward = true;
                 s->start_tick = current_tick + LCDFN(scroll_info).delay * 2;
             }
