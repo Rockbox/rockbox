de310fba38b7e081c9f15bc4f94d2727e4844cef
Michael Sparmann <theseven@rockbox.org>
2009-10-12 11:32:29 +0000

    Ditch additional copyright notice quoted from iBugger.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@23128 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/crypt_firmware.c
+++ apps/plugins/crypt_firmware.c
@@ -33,25 +33,6 @@
 
    Heavily based on Payloads/CryptFirmware/main.c from iBugger.
 
-   The (C) from that file is as follows:
-
-    Copyright 2009 TheSeven
-
-    This file is part of TheSeven's iBugger.
-
-    TheSeven's iBugger is free software: you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation, either version 2 of the
-    License, or (at your option) any later version.
-
-    TheSeven's iBugger is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-    See the GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with TheSeven's iBugger.  If not, see <http://www.gnu.org/licenses/>.
-
 */
 
 #include "plugin.h"
