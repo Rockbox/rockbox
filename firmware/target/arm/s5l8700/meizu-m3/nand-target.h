151c2b25b204c5c69a6f9b174391c174a5128925
Bertrik Sikken <bertrik@sikken.nl>
2009-12-20 17:15:03 +0000

    Make Meizu M3 bootloader compile again
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@24084 a1c6a512-1295-4272-9138-f99709370657

---
+++ firmware/target/arm/s5l8700/meizu-m3/nand-target.h
@@ -0,0 +1,58 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2009 by Michael Sparmann
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#ifndef __NAND_TARGET_H__
+#define __NAND_TARGET_H__
+
+#include "config.h"
+#include "inttypes.h"
+
+
+struct nand_device_info_type
+{
+    uint32_t id;
+    uint16_t blocks;
+    uint16_t userblocks;
+    uint16_t pagesperblock;
+	uint8_t blocksizeexponent;
+    uint8_t tunk1;
+    uint8_t twp;
+    uint8_t tunk2;
+    uint8_t tunk3;
+} __attribute__((packed));
+
+uint32_t nand_read_page(uint32_t bank, uint32_t page, void* databuffer,
+                        void* sparebuffer, uint32_t doecc,
+                        uint32_t checkempty);
+uint32_t nand_write_page(uint32_t bank, uint32_t page, void* databuffer,
+                         void* sparebuffer, uint32_t doecc);
+uint32_t nand_block_erase(uint32_t bank, uint32_t page);
+
+const struct nand_device_info_type* nand_get_device_type(uint32_t bank);
+uint32_t nand_reset(uint32_t bank);
+uint32_t nand_device_init(void);
+void nand_set_active(void);
+long nand_last_activity(void);
+void nand_power_up(void);
+void nand_power_down(void);
+
+
+#endif
