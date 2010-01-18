135d983433e741cf9658ff5d7457bdf37ef48ce0
Nils Wallménius <nils@rockbox.org>
2010-01-18 04:32:47 +0000

    This should fix FS#10917, and also fix channel swapping i introduced in r23784. Lets hope the third time's the charm
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@24271 a1c6a512-1295-4272-9138-f99709370657

--- apps/codecs/mp3_enc.c
+++ apps/codecs/mp3_enc.c
@@ -2091,12 +2091,12 @@ STATICIRAM void to_mono_mm(void)
      * |mmmmmmmmmmmmmmmm|mmmmmmmmmmmmmmmm|
      */
     uint16_t *samp = &mfbuf[2*512];
-    uint16_t *samp_end = samp + samp_per_frame;
+    uint16_t *samp_end = samp + 2*samp_per_frame;
 
     inline void to_mono(uint16_t **samp)
     {
-        int16_t l = **samp;
-        int16_t r = **(samp+1);
+        int16_t r = **samp;
+        int16_t l = *(*samp+1);
         int32_t m;
 
         switch(cfg.rec_mono_mode)
