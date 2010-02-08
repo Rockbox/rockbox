de3489a6c85562c0de3f300a272286c2bbd8594e
Jack Halpin <jack.halpin@gmail.com>
2010-02-08 06:43:47 +0000

    Sansa Clip+ :  Add dualboot support for sansa clip+ to mkamsboot.
    Dualboot works on the sansa clip+ by holding either the left "|<<" or home buttons.
    The USB pin is still undiscovered at this point and should replace the home button when found.
    By including the home button for dualboot we avoid bricking due to a single button failure.
    
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@24561 a1c6a512-1295-4272-9138-f99709370657

--- rbutil/mkamsboot/mkamsboot.c
+++ rbutil/mkamsboot/mkamsboot.c
@@ -222,9 +222,7 @@ static struct md5sums sansasums[] = {
     { MODEL_CLIPV2, "2.01.16", "c57fb3fcbe07c2c9b360f060938f80cb" },
     { MODEL_CLIPV2, "2.01.32", "0ad3723e52022509089d938d0fbbf8c5" },
 
-#if 0 /* uncomment when Clip+ support is tested */
     { MODEL_CLIPPLUS, "01.02.09", "656d38114774c2001dc18e6726df3c5d" },
-#endif
 };
 
 #define NUM_MD5S (sizeof(sansasums)/sizeof(sansasums[0]))
