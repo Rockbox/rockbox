2c25fa88a580b1ee2cb4d60eda7430cf824c8850
Mohamed Tarek <mt@rockbox.org>
2010-02-16 10:35:22 +0000

    Add the Rockbox GNU header to some files in libatrac.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@24692 a1c6a512-1295-4272-9138-f99709370657

--- apps/codecs/libatrac/atrac3data_fixed.h
+++ apps/codecs/libatrac/atrac3data_fixed.h
@@ -1,4 +1,24 @@
-
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2009 Michael Giacomelli
+ * Copyright (C) 2009 Mohamed Tarek
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
 /* tables for the scalefactor decoding */
 /* scaled by 2^31*/
 static const int32_t iMaxQuant_fix[8] ICONST_ATTR = {
