8ecced28cb5b0f2461cbef051f93fb3e286994bb
Szymon Dziok <b0hoon@o2.pl>
2010-02-21 12:29:14 +0000

    Packard Bell Vibe 500: Enable recording.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@24805 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/config/vibe500.h
+++ firmware/export/config/vibe500.h
@@ -11,12 +11,11 @@
 #define CONFIG_STORAGE STORAGE_ATA
 
 /* define this if you have recording possibility */
-/* line-in, microphone - but not yet :-/ */
-/*#define HAVE_RECORDING*/
+#define HAVE_RECORDING
 
 /* Define bitmask of input sources - recordable bitmask can be defined
    explicitly if different */
-/*#define INPUT_SRC_CAPS (SRC_CAP_MIC | SRC_CAP_LINEIN | SRC_CAP_FMRADIO)*/
+#define INPUT_SRC_CAPS (SRC_CAP_MIC | SRC_CAP_LINEIN)
 
 /* define the bitmask of hardware sample rates */
 #define HW_SAMPR_CAPS   (SAMPR_CAP_88 | SAMPR_CAP_44 | SAMPR_CAP_22 | SAMPR_CAP_11)
