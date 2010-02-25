99655d66bd5fb15c8207a7c4b897c9cf8ab8b1b9
Rafaël Carré <rafael.carre@gmail.com>
2010-02-25 14:45:06 +0000

    Alarm clock plugin for RTC targets
    Author: Clément Pit-Claudel
    Flyspray: FS#11056
    
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@24912 a1c6a512-1295-4272-9138-f99709370657

---
+++ apps/plugins/alarmclock.c
@@ -0,0 +1,164 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2010 Clément Pit-Claudel
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include "plugin.h"
+#include "lib/pluginlib_actions.h"
+
+PLUGIN_HEADER
+
+const struct button_mapping *plugin_contexts[] = {generic_directions,
+                                                  generic_actions};
+
+static int current = 0;
+static int alarm[2] = {0, 0}, maxval[2] = {24, 60};
+static bool quit = false, usb = false, waiting = false, done = false;
+
+static inline int get_button(void) {
+    return pluginlib_getaction(HZ/2, plugin_contexts, 2);
+}
+
+int rem_seconds(void) {
+    return (((alarm[0] - rb->get_time()->tm_hour) * 3600)
+           +((alarm[1] - rb->get_time()->tm_min)  * 60)
+           -(rb->get_time()->tm_sec));
+}
+
+void draw(void) {
+    char info[128];
+    rb->lcd_clear_display();
+
+    int secs = rem_seconds();
+
+    if (waiting)
+        rb->snprintf(info, sizeof(info), "Next alarm in %02dh,"
+                                         " %02dmn, and %02ds.",
+                           secs / 3600, (secs / 60) % 60, secs % 60);
+    else {
+        if (current == 0)
+            rb->snprintf(info, sizeof(info), "Set alarm at [%02d]:%02d.",
+                                                                 alarm[0],
+                                                                 alarm[1]);
+        else
+            rb->snprintf(info, sizeof(info), "Set alarm at %02d:[%02d].",
+                                                                 alarm[0],
+                                                                 alarm[1]);
+    }
+
+    int w, h;
+    rb->lcd_getstringsize(info, &w, &h);
+
+    if (w > LCD_WIDTH || h > LCD_HEIGHT)
+        rb->splash(0, info);
+    else {
+        rb->lcd_putsxy((LCD_WIDTH - w) / 2, (LCD_HEIGHT - h) / 2, info);
+        rb->lcd_update();
+    }
+}
+
+bool can_play(void) {
+    int audio_status = rb->audio_status();
+    if ((!audio_status && rb->global_status->resume_index != -1)
+        && (rb->playlist_resume() != -1)) {
+        return true;
+    }
+    else if (audio_status & AUDIO_STATUS_PAUSE)
+        return true;
+
+    return false;   
+}
+
+void play(void) {
+    int audio_status = rb->audio_status();
+    if (!audio_status && rb->global_status->resume_index != -1) {
+        if (rb->playlist_resume() != -1) {
+            rb->playlist_start(rb->global_status->resume_index,
+                rb->global_status->resume_offset);
+        }
+    }
+    else if (audio_status & AUDIO_STATUS_PAUSE)
+        rb->audio_resume();
+}
+
+enum plugin_status plugin_start(const void* parameter)
+{
+    int button;
+    (void)parameter;
+
+    if (!can_play()) {
+        rb->splash(4*HZ, "No track to resume! This plugin will resume a track "
+                         "at a specific time. Therefore, you need to first play"
+                         " one, and then pause it and start the plugin again.");
+        quit = true;
+    }
+
+    while(!quit) {
+        button = get_button();
+
+        if (button == PLA_QUIT)
+            quit = true;
+
+        draw();
+        if (waiting) {
+            if (rem_seconds() <= 0) {
+                quit = done = true;
+                play();
+            }
+        }
+        else {
+            switch (button) {
+                case PLA_UP:
+                case PLA_UP_REPEAT:
+                    alarm[current] = (alarm[current] + 1) % maxval[current];
+                    break;
+
+                case PLA_DOWN:
+                case PLA_DOWN_REPEAT:
+                    alarm[current] = (alarm[current] + maxval[current] - 1)
+                        % maxval[current];
+                    break;
+
+                case PLA_LEFT:
+                case PLA_LEFT_REPEAT:
+                case PLA_RIGHT:
+                case PLA_RIGHT_REPEAT:
+                    current = (current + 1) % 2;
+                    break;
+
+                case PLA_FIRE: {
+                    if (rem_seconds() < 0)
+                        alarm[0] += 24;
+
+                    waiting = true;
+                    break;
+                }
+
+                default:
+                    if (rb->default_event_handler(button) == SYS_USB_CONNECTED)
+                        quit = usb = true;
+                    break;
+            }
+        }
+    }
+
+    return (usb) ? PLUGIN_USB_CONNECTED
+                 : (done ? PLUGIN_GOTO_WPS : PLUGIN_OK);
+}
+
