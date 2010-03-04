b678be335a0ecc3427a88c87aae0e37f8328dc38
Rafaël Carré <rafael.carre@gmail.com>
2010-03-04 00:37:41 +0000

    AS3525v2: disable cpu frequency changes
    CGU_PROC looks different from as3525v1 and there have been reports of
    Clip+ becoming warm when boosted
    
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@25020 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/config/sansaclipplus.h
+++ firmware/export/config/sansaclipplus.h
@@ -187,7 +187,7 @@
 #define CONFIG_LED LED_VIRTUAL
 
 /* Define this if you have adjustable CPU frequency */
-#define HAVE_ADJUSTABLE_CPU_FREQ
+//#define HAVE_ADJUSTABLE_CPU_FREQ
 
 #define BOOTFILE_EXT    "sansa"
 #define BOOTFILE        "rockbox." BOOTFILE_EXT
