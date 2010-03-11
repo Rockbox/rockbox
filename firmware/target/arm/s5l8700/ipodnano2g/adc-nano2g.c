462bdfe6126541485d7b68dbdce51e248cd1f7f1
Michael Sparmann <theseven@rockbox.org>
2010-03-11 14:02:59 +0000

    Implement (unused) ADC wrapper for Nano2G to fix the red
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@25114 a1c6a512-1295-4272-9138-f99709370657

---
+++ firmware/target/arm/s5l8700/ipodnano2g/adc-nano2g.c
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id: adc-s5l8700.c 21775 2009-07-11 14:12:02Z bertrik $
+ *
+ * Copyright (C) 2009 by Bertrik Sikken
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+ 
+#include "config.h"
+
+#include "inttypes.h"
+#include "s5l8700.h"
+#include "adc.h"
+#include "adc-target.h"
+#include "pmu-target.h"
+#include "kernel.h"
+
+unsigned short adc_read(int channel)
+{
+    return pmu_read_adc(channel);
+}
+
+void adc_init(void)
+{
+}
+
