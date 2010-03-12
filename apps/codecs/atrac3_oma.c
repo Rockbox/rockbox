Rockbox/rockbox

commit 3716abba9274f544dd31cdf4e6c83a845bf2a801

commit FS#10424 and FS#10425 - wav(RIFF) supports Microsoft ADPCM, Dialogic OKI ADPCM, YAMAHA ADPCM, Adobe SWF ADPCM. - AIFF supports QuickTime IMA ADPCM. - DVI ADPCM(IMA ADPCM) reworks.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@24782 a1c6a512-1295-4272-9138-f99709370657

apps/metadata/wave.c
@@ -28,6 +28,7 @@
 #include "metadata.h"
 #include "metadata_common.h"
 #include "metadata_parsers.h"
+#include "logf.h"
 
 #   define AV_WL32(p, d) do {                   \
         ((uint8_t*)(p))[0] = (d);               \
@@ -40,29 +41,101 @@
         ((uint8_t*)(p))[1] = (d)>>8;            \
     } while(0)
 
+enum
+{
+    WAVE_FORMAT_PCM = 0x0001,   /* Microsoft PCM Format */
+    WAVE_FORMAT_ADPCM = 0x0002, /* Microsoft ADPCM Format */
+    WAVE_FORMAT_IEEE_FLOAT = 0x0003, /* IEEE Float */
+    WAVE_FORMAT_ALAW = 0x0006,  /* Microsoft ALAW */
+    WAVE_FORMAT_MULAW = 0x0007, /* Microsoft MULAW */
+    WAVE_FORMAT_DVI_ADPCM = 0x0011, /* Intel's DVI ADPCM */
+    WAVE_FORMAT_DIALOGIC_OKI_ADPCM = 0x0017, /* Dialogic OKI ADPCM */
+    WAVE_FORMAT_YAMAHA_ADPCM = 0x0020, /* Yamaha ADPCM */
+    WAVE_FORMAT_XBOX_ADPCM = 0x0069, /* XBOX ADPCM */
+    IBM_FORMAT_MULAW = 0x0101,  /* same as WAVE_FORMAT_MULAW */
+    IBM_FORMAT_ALAW = 0x0102,   /* same as WAVE_FORMAT_ALAW */
+    WAVE_FORMAT_ATRAC3 = 0x0270, /* Atrac3 stream */
+    WAVE_FORMAT_SWF_ADPCM = 0x5346, /* Adobe SWF ADPCM */
+};
+
+struct wave_fmt {
+    unsigned int formattag;
+    unsigned long channels;
+    unsigned int blockalign;
+    unsigned long bitspersample;
+    unsigned int samplesperblock;
+    unsigned long numbytes;
+};
+
+static unsigned long get_totalsamples(struct wave_fmt *fmt, struct mp3entry* id3)
+{
+    unsigned long totalsamples = 0;
+
+    switch (fmt->formattag)
+    {
+        case WAVE_FORMAT_PCM:
+        case WAVE_FORMAT_IEEE_FLOAT:
+        case WAVE_FORMAT_ALAW:
+        case WAVE_FORMAT_MULAW:
+        case IBM_FORMAT_ALAW:
+        case IBM_FORMAT_MULAW:
+            totalsamples =
+                 fmt->numbytes / ((((fmt->bitspersample - 1) / 8) + 1) * fmt->channels);
+            break;
+        case WAVE_FORMAT_ADPCM:
+        case WAVE_FORMAT_DVI_ADPCM:
+        case WAVE_FORMAT_XBOX_ADPCM:
+            totalsamples = (fmt->numbytes / fmt->blockalign) * fmt->samplesperblock;
+            break;
+        case WAVE_FORMAT_YAMAHA_ADPCM:
+            if (fmt->samplesperblock == 0)
+            {
+                if (fmt->blockalign == ((id3->frequency / 60) + 4) * fmt->channels)
+                    fmt->samplesperblock = id3->frequency / 30;
+                else
+                    fmt->samplesperblock = fmt->blockalign * 2 / fmt->channels;
+            }
+            totalsamples = (fmt->numbytes / fmt->blockalign) * fmt->samplesperblock;
+            break;
+        case WAVE_FORMAT_DIALOGIC_OKI_ADPCM:
+            totalsamples = 2 * fmt->numbytes;
+            break;
+        case WAVE_FORMAT_SWF_ADPCM:
+            if (fmt->samplesperblock == 0)
+                fmt->samplesperblock = (((fmt->blockalign << 3) - 2) / fmt->channels - 22)
+                                                                     / fmt->bitspersample;
+
+            totalsamples = (fmt->numbytes / fmt->blockalign) * fmt->samplesperblock;
+            break;
+        default:
+            totalsamples = 0;
+            break;
+    }
+    return totalsamples;
+}
+
 bool get_wave_metadata(int fd, struct mp3entry* id3)
 {
     /* Use the trackname part of the id3 structure as a temporary buffer */
     unsigned char* buf = (unsigned char *)id3->path;
+    struct wave_fmt fmt;
     unsigned long totalsamples = 0;
-    unsigned long channels = 0;
-    unsigned long bitspersample = 0;
-    unsigned long numbytes = 0;
     unsigned long offset = 0;
     int read_bytes;
     int i;
 
+    memset(&fmt, 0, sizeof(struct wave_fmt));
+
     /* get RIFF chunk header */
-    if ((lseek(fd, 0, SEEK_SET) < 0)
-        || ((read_bytes = read(fd, buf, 12)) < 12))
+    if ((lseek(fd, 0, SEEK_SET) < 0) || (read(fd, buf, 12) < 12))
     {
         return false;
     }
     offset += 12;
 
-    if ((memcmp(buf, "RIFF",4) != 0)
-        || (memcmp(&buf[8], "WAVE", 4) !=0 ))
+    if ((memcmp(buf, "RIFF", 4) != 0) || (memcmp(&buf[8], "WAVE", 4) != 0))
     {
+        DEBUGF("metadata error: missing riff header.\n");
         return false;
     }
 
@@ -70,7 +143,7 @@ bool get_wave_metadata(int fd, struct mp3entry* id3)
     while (true)
     {
         /* get chunk header */
-        if ((read_bytes = read(fd, buf, 8)) < 8)
+        if (read(fd, buf, 8) < 8)
             return false;
         offset += 8;
 
@@ -80,26 +153,41 @@ bool get_wave_metadata(int fd, struct mp3entry* id3)
         if (memcmp(buf, "fmt ", 4) == 0)
         {
             /* get rest of chunk */
-            if ((read_bytes = read(fd, buf, 16)) < 16)
+            if (i < 16)
                 return false;
-            offset += 16;
-            i -= 16;
 
+            read_bytes = 16;
+            if (i > 19)
+                read_bytes = 20;
 
-            /* skipping wFormatTag */
+            if (read(fd, buf, read_bytes) != read_bytes)
+                return false;
+
+            offset += read_bytes;
+            i -= read_bytes;
+
+            /* wFormatTag */
+            fmt.formattag = buf[0] | (buf[1] << 8);
             /* wChannels */
-            channels = buf[2] | (buf[3] << 8);
+            fmt.channels = buf[2] | (buf[3] << 8);
             /* dwSamplesPerSec */
             id3->frequency = get_long_le(&buf[4]);
             /* dwAvgBytesPerSec */
             id3->bitrate = (get_long_le(&buf[8]) * 8) / 1000;
             /* wBlockAlign */
-            id3->bytesperframe = buf[12] | (buf[13] << 8);
+            fmt.blockalign = buf[12] | (buf[13] << 8);
+            id3->bytesperframe = fmt.blockalign;
             /* wBitsPerSample */
-            bitspersample = buf[14] | (buf[15] << 8);
+            fmt.bitspersample = buf[14] | (buf[15] << 8);
+            if (read_bytes > 19)
+            {
+                /* wSamplesPerBlock */
+                fmt.samplesperblock = buf[18] | (buf[19] << 8);
+            }
+
             /* Check for ATRAC3 stream */
-            if((buf[0] | (buf[1] << 8)) == 0x0270) 
-            {   
+            if (fmt.formattag == WAVE_FORMAT_ATRAC3)
+            {
                 int jsflag = 0;
                 if(id3->bitrate == 66 || id3->bitrate == 94)
                     jsflag = 1;
@@ -107,7 +195,7 @@ bool get_wave_metadata(int fd, struct mp3entry* id3)
                 id3->extradata_size = 14;
                 id3->channels = 2;
                 id3->codectype = AFMT_OMA_ATRAC3;
-               /* Store the extradata for the codec */
+                /* Store the extradata for the codec */
                 AV_WL16(&id3->id3v2buf[0],  1);             // always 1
                 AV_WL32(&id3->id3v2buf[2],  id3->frequency);    // samples rate
                 AV_WL16(&id3->id3v2buf[6],  jsflag);        // coding mode
@@ -118,8 +206,9 @@ bool get_wave_metadata(int fd, struct mp3entry* id3)
         }
         else if (memcmp(buf, "data", 4) == 0)
         {
-            numbytes = i;
-            id3->first_frame_offset = offset;
+            fmt.numbytes = i;
+            if (fmt.formattag == WAVE_FORMAT_ATRAC3)
+                id3->first_frame_offset = offset;
             break;
         }
         else if (memcmp(buf, "fact", 4) == 0)
@@ -128,7 +217,7 @@ bool get_wave_metadata(int fd, struct mp3entry* id3)
             if (i >= 4)
             {
                 /* get rest of chunk */
-                if ((read_bytes = read(fd, buf, 4)) < 4)
+                if (read(fd, buf, 4) < 4)
                     return false;
                 offset += 4;
                 i -= 4;
@@ -145,16 +234,15 @@ bool get_wave_metadata(int fd, struct mp3entry* id3)
         offset += i;
     }
 
-    if ((numbytes == 0) || (channels == 0))
+    if ((fmt.numbytes == 0) || (fmt.channels == 0) || (fmt.blockalign == 0))
     {
+        DEBUGF("metadata error: numbytes, channels, or blockalign is 0.\n");
         return false;
     }
 
     if (totalsamples == 0)
     {
-        /* for PCM only */
-        totalsamples = numbytes
-            / ((((bitspersample - 1) / 8) + 1) * channels);
+        totalsamples = get_totalsamples(&fmt, id3);
     }
 
     id3->vbr = false;   /* All WAV files are CBR */

apps/codecs/aiff.c
@@ -36,6 +36,7 @@ enum {
     AIFC_FORMAT_MULAW        = FOURCC('u', 'l', 'a', 'w'), /* AIFC uLaw compressed */
     AIFC_FORMAT_IEEE_FLOAT32 = FOURCC('f', 'l', '3', '2'), /* AIFC IEEE float 32 bit */
     AIFC_FORMAT_IEEE_FLOAT64 = FOURCC('f', 'l', '6', '4'), /* AIFC IEEE float 64 bit */
+    AIFC_FORMAT_QT_IMA_ADPCM = FOURCC('i', 'm', 'a', '4'), /* AIFC QuickTime IMA ADPCM */
 };
 
 static const struct pcm_entry pcm_codecs[] = {
@@ -44,9 +45,10 @@ static const struct pcm_entry pcm_codecs[] = {
     { AIFC_FORMAT_MULAW,        get_itut_g711_mulaw_codec },
     { AIFC_FORMAT_IEEE_FLOAT32, get_ieee_float_codec      },
     { AIFC_FORMAT_IEEE_FLOAT64, get_ieee_float_codec      },
+    { AIFC_FORMAT_QT_IMA_ADPCM, get_qt_ima_adpcm_codec    },
 };
 
-#define NUM_FORMATS 5
+#define NUM_FORMATS 6
 
 static int32_t samples[PCM_CHUNK_SIZE] IBSS_ATTR;
 
@@ -70,7 +72,7 @@ enum codec_status codec_main(void)
 {
     int status = CODEC_OK;
     struct pcm_format format;
-    uint32_t bytesdone, decodedbytes;
+    uint32_t bytesdone, decodedsamples;
     uint32_t num_sample_frames = 0;
     uint32_t i = CODEC_OK;
     size_t n;
@@ -128,7 +130,7 @@ next_track:
     format.is_signed = true;
     format.is_little_endian = false;
 
-    decodedbytes = 0;
+    decodedsamples = 0;
     codec = 0;
 
     /* read until 'SSND' chunk, which typically is last */
@@ -168,8 +170,11 @@ next_track:
                  * aiff's sample_size is uncompressed sound data size.
                  * But format.bitspersample is compressed sound data size.
                  */
-                if (format.formattag == AIFC_FORMAT_ALAW || format.formattag == AIFC_FORMAT_MULAW)
+                if (format.formattag == AIFC_FORMAT_ALAW ||
+                    format.formattag == AIFC_FORMAT_MULAW)
                     format.bitspersample = 8;
+                else if (format.formattag == AIFC_FORMAT_QT_IMA_ADPCM)
+                    format.bitspersample = 4;
             }
             else
                 format.formattag = AIFC_FORMAT_PCM;
@@ -184,9 +189,9 @@ next_track:
             /* offset2snd */
             offset2snd = (buf[8]<<24)|(buf[9]<<16)|(buf[10]<<8)|buf[11];
             /* block_size */
-            format.blockalign = (buf[12]<<24)|(buf[13]<<16)|(buf[14]<<8)|buf[15];
+            format.blockalign = ((buf[12]<<24)|(buf[13]<<16)|(buf[14]<<8)|buf[15]) >> 3;
             if (format.blockalign == 0)
-                format.blockalign = format.channels*format.bitspersample;
+                format.blockalign = format.channels * format.bitspersample >> 3;
             format.numbytes = i - 8 - offset2snd;
             i = 8 + offset2snd; /* advance to the beginning of data */
         } else if (is_aifc && (memcmp(buf, "FVER", 4)==0)) {
@@ -228,7 +233,7 @@ next_track:
         goto done;
     }
 
-    if (!codec->set_format(&format, 0))
+    if (!codec->set_format(&format))
     {
         i = CODEC_ERROR;
         goto done;
@@ -242,6 +247,30 @@ next_track:
         ci->configure(DSP_SET_STEREO_MODE, STEREO_MONO);
     } else {
         DEBUGF("CODEC_ERROR: more than 2 channels unsupported\n");
+        i = CODEC_ERROR;
+        goto done;
+    }
+
+    if (format.samplesperblock == 0)
+    {
+        DEBUGF("CODEC_ERROR: samplesperblock is 0\n");
+        i = CODEC_ERROR;
+        goto done;
+    }
+    if (format.blockalign == 0)
+    {
+        DEBUGF("CODEC_ERROR: blockalign is 0\n");
+        i = CODEC_ERROR;
+        goto done;
+    }
+
+    /* check chunksize */
+    if ((format.chunksize / format.blockalign) * format.samplesperblock * format.channels
+           > PCM_CHUNK_SIZE)
+        format.chunksize = (PCM_CHUNK_SIZE / format.blockalign) * format.blockalign;
+    if (format.chunksize == 0)
+    {
+        DEBUGF("CODEC_ERROR: chunksize is 0\n");
         i = CODEC_ERROR;
         goto done;
     }
@@ -260,11 +289,14 @@ next_track:
             break;
 
         if (ci->seek_time) {
-            uint32_t newpos = codec->get_seek_pos(ci->seek_time);
-            if (newpos > format.numbytes)
+            /* 2nd args(read_buffer) is unnecessary in the format which AIFF supports. */
+            struct pcm_pos *newpos = codec->get_seek_pos(ci->seek_time, NULL);
+
+            decodedsamples = newpos->samples;
+            if (newpos->pos > format.numbytes)
                 break;
-            if (ci->seek_buffer(firstblockposn + newpos))
-                bytesdone = newpos;
+            if (ci->seek_buffer(firstblockposn + newpos->pos))
+                bytesdone = newpos->pos;
             ci->seek_complete();
         }
         aifbuf = (uint8_t *)ci->request_buffer(&n, format.chunksize);
@@ -288,11 +320,11 @@ next_track:
 
         ci->advance_buffer(n);
         bytesdone += n;
-        decodedbytes += bufcount;
+        decodedsamples += bufcount;
         if (bytesdone >= format.numbytes)
             endofstream = 1;
 
-        ci->set_elapsed(decodedbytes*1000LL/ci->id3->frequency);
+        ci->set_elapsed(decodedsamples*1000LL/ci->id3->frequency);
     }
     i = CODEC_OK;
 

apps/codecs/libpcm/SOURCES
@@ -2,3 +2,10 @@ linear_pcm.c
 itut_g711.c
 dvi_adpcm.c
 ieee_float.c
+adpcm_seek.c
+dialogic_oki_adpcm.c
+ms_adpcm.c
+yamaha_adpcm.c
+ima_adpcm_common.c
+qt_ima_adpcm.c
+swf_adpcm.c

apps/codecs/libpcm/adpcm_seek.c
@@ -1,0 +1,101 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2010 Yoshihisa Uchida
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#include "adpcm_seek.h"
+#include "codeclib.h"
+
+/*
+ * The helper functions in order to seek for the adpcm codec
+ * which does not include the header each the data block.
+ */
+
+#define MAX_STORE_COUNT 1000
+
+static struct adpcm_data seek_table[MAX_STORE_COUNT];
+static int seek_count;
+static int cur_count;
+static int max_ratio;
+static int cur_ratio;
+
+void init_seek_table(uint32_t max_count)
+{
+    int i = 0;
+
+    for ( ; i < MAX_STORE_COUNT; i++)
+    {
+        seek_table[i].is_valid = false;
+    }
+    seek_count = max_count / MAX_STORE_COUNT + 1;
+    max_ratio  = max_count / seek_count + 1;
+    cur_count  = 0;
+    cur_ratio  = -1;
+}
+
+void add_adpcm_data(struct adpcm_data *data)
+{
+    if (--cur_count <= 0)
+    {
+        cur_count = seek_count;
+        if (++cur_ratio >= max_ratio)
+            cur_ratio = max_ratio - 1;
+
+        if (!seek_table[cur_ratio].is_valid)
+        {
+            seek_table[cur_ratio].pcmdata[0] = data->pcmdata[0];
+            seek_table[cur_ratio].pcmdata[1] = data->pcmdata[1];
+            seek_table[cur_ratio].step[0]    = data->step[0];
+            seek_table[cur_ratio].step[1]    = data->step[1];
+            seek_table[cur_ratio].is_valid   = true;
+        }
+    }
+}
+
+uint32_t seek(uint32_t count, struct adpcm_data *seek_data,
+              uint8_t *(*read_buffer)(size_t *realsize),
+              int (*decode)(const uint8_t *inbuf, size_t inbufsize))
+{
+    int new_ratio = count / seek_count;
+
+    if (new_ratio >= max_ratio)
+        new_ratio = max_ratio - 1;
+
+    if (!seek_table[new_ratio].is_valid)
+    {
+        uint8_t *buffer;
+        size_t n;
+
+        do
+        {
+            buffer = read_buffer(&n);
+            if (n == 0)
+                break;
+            decode(buffer, n);
+        } while (cur_ratio < new_ratio);
+    }
+
+    seek_data->pcmdata[0] = seek_table[new_ratio].pcmdata[0];
+    seek_data->pcmdata[1] = seek_table[new_ratio].pcmdata[1];
+    seek_data->step[0]    = seek_table[new_ratio].step[0];
+    seek_data->step[1]    = seek_table[new_ratio].step[1];
+
+    cur_ratio = new_ratio;
+    cur_count = seek_count;
+    return cur_ratio * seek_count;
+}

apps/codecs/libpcm/adpcm_seek.h
@@ -7,7 +7,7 @@
  *                     \/            \/     \/    \/            \/
  * $Id$
  *
- * Copyright (C) 2009 Yoshihisa Uchida
+ * Copyright (C) 2010 Yoshihisa Uchida
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -18,38 +18,22 @@
  * KIND, either express or implied.
  *
  ****************************************************************************/
-#ifndef CODEC_LIBPCM_SUPPORT_FORMATS_H
-#define CODEC_LIBPCM_SUPPORT_FORMATS_H
+#ifndef CODEC_LIBPCM_ADPCM_SEEK_H
+#define CODEC_LIBPCM_ADPCM_SEEK_H
 
-#include "pcm_common.h"
+#include <sys/types.h>
+#include <stdbool.h>
+#include <inttypes.h>
 
-/* Linear PCM */
-const struct pcm_codec *get_linear_pcm_codec(void);
+struct adpcm_data {
+    int16_t  pcmdata[2];
+    uint16_t step[2];
+    bool     is_valid;
+};
 
-/* ITU-T G.711 A-law */
-const struct pcm_codec *get_itut_g711_alaw_codec(void);
-
-/* ITU-T G.711 mu-law */
-const struct pcm_codec *get_itut_g711_mulaw_codec(void);
-
-/* Intel DVI ADPCM (IMA ADPCM) */
-const struct pcm_codec *get_dvi_adpcm_codec(void);
-
-/* IEEE float */
-const struct pcm_codec *get_ieee_float_codec(void);
-
-/* Microsoft ADPCM */
-const struct pcm_codec *get_ms_adpcm_codec(void);
-
-/* Dialogic OKI ADPCM */
-const struct pcm_codec *get_dialogic_oki_adpcm_codec(void);
-
-/* YAMAHA ADPCM */
-const struct pcm_codec *get_yamaha_adpcm_codec(void);
-
-/* Apple QuickTime IMA ADPCM */
-const struct pcm_codec *get_qt_ima_adpcm_codec(void);
-
-/* Adobe SWF ADPCM */
-const struct pcm_codec *get_swf_adpcm_codec(void);
+void init_seek_table(uint32_t max_count);
+void add_adpcm_data(struct adpcm_data *data);
+uint32_t seek(uint32_t seek_time, struct adpcm_data *seek_data,
+              uint8_t *(*read_buffer)(size_t *realsize),
+              int (*decode)(const uint8_t *inbuf, size_t inbufsize));
 #endif

apps/codecs/libpcm/dialogic_oki_adpcm.c
@@ -1,0 +1,183 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2010 Yoshihisa Uchida
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#include "codeclib.h"
+#include "pcm_common.h"
+#include "adpcm_seek.h"
+
+/*
+ * Dialogic OKI ADPCM
+ *
+ * References
+ * [1] Dialogic Corporation, Dialogic ADPCM Algorithm, 1988
+ * [2] MultimediaWiki, Dialogic IMA ADPCM, URL:http://wiki.multimedia.cx/index.php?title=Dialogic_IMA_ADPCM
+ * [3] sox source code, src/adpcms.c
+ * [4] Tetsuya Isaki, NetBSD:/sys/dev/audio.c, http://www.tri-tree.gr.jp/~isaki/NetBSD/src/sys/dev/ic/msm6258.c.html
+ */
+
+static const uint16_t step_table[] ICONST_ATTR = {
+    16,  17,   19,   21,   23,   25,   28,  31,  34,  37,  41,  45,  50,  55,
+    60,  66,   73,   80,   88,   97,  107, 118, 130, 143, 157, 173, 190, 209,
+   230, 253,  279,  307,  337,  371,  408, 449, 494, 544, 598, 658, 724, 796,
+   876, 963, 1060, 1166, 1282, 1411, 1552,
+};
+
+static const int index_table[] ICONST_ATTR = {
+    -1, -1, -1, -1, 2, 4, 6, 8
+};
+
+static struct adpcm_data cur_data;
+
+static struct pcm_format *fmt;
+
+static bool set_format(struct pcm_format *format)
+{
+    uint32_t max_chunk_count;
+
+    fmt = format;
+
+    if (fmt->bitspersample != 4)
+    {
+        DEBUGF("CODEC_ERROR: dialogic oki adpcm must be 4 bitspersample: %d\n",
+                             fmt->bitspersample);
+        return false;
+    }
+
+    if (fmt->channels != 1)
+    {
+        DEBUGF("CODEC_ERROR: dialogic oki adpcm must be monaural\n");
+        return false;
+    }
+
+    /* blockalign = 2 samples */
+    fmt->blockalign = 1;
+    fmt->samplesperblock = 2;
+
+    /* chunksize = about 1/32[sec] data */
+    fmt->chunksize = ci->id3->frequency >> 6;
+
+    max_chunk_count = (uint64_t)ci->id3->length * ci->id3->frequency
+                                         / (2000LL * fmt->chunksize);
+
+    /* initialize seek table */
+    init_seek_table(max_chunk_count);
+    /* add first data */
+    add_adpcm_data(&cur_data);
+
+    return true;
+}
+
+static int16_t create_pcmdata(uint8_t nibble)
+{
+    int16_t delta;
+    int16_t index = cur_data.step[0];
+    int16_t step = step_table[index];
+
+    delta = (step >> 3);
+    if (nibble & 4) delta += step;
+    if (nibble & 2) delta += (step >> 1);
+    if (nibble & 1) delta += (step >> 2);
+
+    if (nibble & 0x08)
+        cur_data.pcmdata[0] -= delta;
+    else
+        cur_data.pcmdata[0] += delta;
+
+    CLIP(cur_data.pcmdata[0], -2048, 2047);
+
+    index += index_table[nibble & 0x07];
+    CLIP(index, 0, 48);
+    cur_data.step[0] = index;
+
+    return cur_data.pcmdata[0];
+}
+
+static int decode(const uint8_t *inbuf, size_t inbufsize,
+                  int32_t *outbuf, int *outbufcount)
+{
+    size_t nsamples = 0;
+
+    while (inbufsize)
+    {
+        *outbuf++ = create_pcmdata(*inbuf >> 4) << 17;
+        *outbuf++ = create_pcmdata(*inbuf     ) << 17;
+        nsamples += 2;
+
+        inbuf++;
+        inbufsize--;
+    }
+
+    *outbufcount = nsamples;
+    add_adpcm_data(&cur_data);
+
+    return CODEC_OK;
+}
+
+static int decode_for_seek(const uint8_t *inbuf, size_t inbufsize)
+{
+    while (inbufsize)
+    {
+        create_pcmdata(*inbuf >> 4);
+        create_pcmdata(*inbuf     );
+
+        inbuf++;
+        inbufsize--;
+    }
+
+    add_adpcm_data(&cur_data);
+
+    return CODEC_OK;
+}
+
+static struct pcm_pos *get_seek_pos(long seek_time,
+                                    uint8_t *(*read_buffer)(size_t *realsize))
+{
+    static struct pcm_pos newpos;
+    uint32_t seek_count = 0;
+    uint32_t new_count;
+
+    if (seek_time > 0)
+        seek_count = (uint64_t)seek_time * ci->id3->frequency
+                                         / (2000LL * fmt->chunksize);
+
+    new_count = seek(seek_count, &cur_data, read_buffer, &decode_for_seek);
+    newpos.pos     = new_count * fmt->chunksize;
+    newpos.samples = (newpos.pos / fmt->blockalign) * fmt->samplesperblock;
+    return &newpos;
+}
+
+static const struct pcm_codec codec = {
+                                          set_format,
+                                          get_seek_pos,
+                                          decode,
+                                      };
+
+const struct pcm_codec *get_dialogic_oki_adpcm_codec(void)
+{
+    /*
+     * initialize first pcm data, step index
+     * because the dialogic oki adpcm is always monaural,
+     * pcmdata[1], step[1] do not use.
+     */
+    cur_data.pcmdata[0] = 0;
+    cur_data.step[0]    = 0;
+
+    return &codec;
+}

commit 2ac401f4861e16c8c26517348f4b96b6dfeed41a

Make atrac3 streams in wav containers work again (error was introduced with r24782). Add additional info to atrac3 debug information.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@25134 a1c6a512-1295-4272-9138-f99709370657

apps/codecs/atrac3_oma.c
@@ -60,7 +60,7 @@ next_track:
     
     res =atrac3_decode_init(&q, ci->id3);
     if(res < 0) {
-        DEBUGF("failed to initialize atrac decoder\n");
+        DEBUGF("failed to initialize OMA atrac decoder\n");
         return CODEC_ERROR;
     }
 

apps/codecs/atrac3_rm.c
@@ -83,7 +83,7 @@ next_track:
     
     res =atrac3_decode_init(&q, ci->id3);
     if(res < 0) {
-        DEBUGF("failed to initialize atrac decoder\n");
+        DEBUGF("failed to initialize RM atrac decoder\n");
         return CODEC_ERROR;
     }
 

apps/metadata/wave.c
@@ -196,6 +196,8 @@ static void parse_riff_format(unsigned char* buf, int fmtsize, struct wave_fmt *
         id3->extradata_size = 14;
         id3->channels = 2;
         id3->codectype = AFMT_OMA_ATRAC3;
+        id3->bytesperframe = fmt->blockalign;
+        
         /* Store the extradata for the codec */
         AV_WL16(&id3->id3v2buf[0],  1);             // always 1
         AV_WL32(&id3->id3v2buf[2],  id3->frequency);// samples rate
@@ -303,7 +305,7 @@ static bool read_header(int fd, struct mp3entry* id3, const unsigned char **chun
     id3->filesize = filesize(fd);
 
     /* Calculate track length (in ms) and estimate the bitrate (in kbit/s) */
-    if(fmt.formattag != AFMT_OMA_ATRAC3)
+    if(fmt.formattag != WAVE_FORMAT_ATRAC3)
     {
         if (id3->frequency != 0)
             id3->length = ((int64_t) fmt.totalsamples * 1000) / id3->frequency;