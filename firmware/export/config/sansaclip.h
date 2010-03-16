04937cb20760ee38237c9d016f87a4c2c8d390b5
Rafaël Carré <rafael.carre@gmail.com>
2010-03-16 15:57:44 +0000

    Clipv1 & m200v4 : reduce plugin buffer size to strict minimum
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@25219 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/config/sansaclip.h
+++ firmware/export/config/sansaclip.h
@@ -111,7 +111,7 @@
 #define CODEC_SIZE 0x48000  /* in IRAM */
 
 /* The number of bytes reserved for loadable plugins */
-#define PLUGIN_BUFFER_SIZE 0x60000
+#define PLUGIN_BUFFER_SIZE 0x45000
 
 #define AB_REPEAT_ENABLE 1
 
