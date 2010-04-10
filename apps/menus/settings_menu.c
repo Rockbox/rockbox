d873f4f4749c9e1dea5fa012cf0522c7e24fe784
Alexander Levin <al.le@rockbox.org>
2010-04-10 20:11:40 +0000

    Use a special constant so that we don't have to care about the actual line numbers
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@25572 a1c6a512-1295-4272-9138-f99709370657

--- apps/menus/settings_menu.c
+++ apps/menus/settings_menu.c
@@ -413,10 +413,10 @@ static void view_hotkey_info(void)
     simplelist_info_init(&info, str(LANG_VIEW_HOTKEY), 0, NULL);
     info.hide_selection = true;
     info.scroll_all = true;
-    simplelist_set_line_count(2);
-    simplelist_addline(0, str(LANG_HOTKEY_VIEW_WPS),
+    simplelist_set_line_count(0);
+    simplelist_addline(SIMPLELIST_ADD_LINE, str(LANG_HOTKEY_VIEW_WPS),
         str(get_hotkey_desc_id(global_settings.hotkey_wps)));
-    simplelist_addline(1, str(LANG_HOTKEY_VIEW_FILE_BROWSER),
+    simplelist_addline(SIMPLELIST_ADD_LINE, str(LANG_HOTKEY_VIEW_FILE_BROWSER),
         str(get_hotkey_desc_id(global_settings.hotkey_tree)));
     simplelist_show_list(&info);
 }
