e3379ed812f72aea187edc4da2c9922c2493f152
Michael Chicoine <mc2739@gmail.com>
2010-04-10 15:55:31 +0000

    Fix scrolling line artifacts on USB screen - should fix FS#11154
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@25569 a1c6a512-1295-4272-9138-f99709370657

--- apps/gui/usb_screen.c
+++ apps/gui/usb_screen.c
@@ -182,6 +182,13 @@ static void usb_screen_fix_viewports(struct screen *screen,
 static void usb_screens_draw(struct usb_screen_vps_t *usb_screen_vps_ar)
 {
     int i;
+
+    /* Clear main and remote screens to remove scrolling line artifacts */
+    lcd_clear_display();
+#ifdef HAVE_LCD_REMOTE
+    lcd_remote_clear_display();
+#endif
+
     FOR_NB_SCREENS(i)
     {
         struct screen *screen = &screens[i];
