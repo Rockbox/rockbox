f359ec763094d895a8f6dc96a2496180431ef2f9
Michael Sevakis <jethead71@rockbox.org>
2010-04-13 04:57:35 +0000

    Gigabeat S: Employ more accurate battery charging curve.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@25615 a1c6a512-1295-4272-9138-f99709370657

--- firmware/target/arm/imx31/gigabeat-s/powermgmt-gigabeat-s.c
+++ firmware/target/arm/imx31/gigabeat-s/powermgmt-gigabeat-s.c
@@ -49,8 +49,7 @@ const unsigned short percent_to_volt_discharge[BATTERY_TYPES_COUNT][11] =
 const unsigned short percent_to_volt_charge[11] =
 {
     /* Toshiba Gigabeat S Li Ion 700mAH figured from charge curve */
-    /* TODO - get actual charge curve (this is copy of discharge) */
-    3659, 3719, 3745, 3761, 3785, 3813, 3856, 3926, 3984, 4040, 4121
+    4028, 4063, 4087, 4111, 4135, 4156, 4173, 4185, 4194, 4202, 4208
 };
 
 /* Returns battery voltage from ADC [millivolts] */
