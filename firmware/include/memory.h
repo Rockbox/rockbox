8ffbe2e467efcdf539f5989c043d19c4d3b6d687
Michael Sevakis <jethead71@rockbox.org>
2010-05-07 04:14:59 +0000

    Remove stray function prototype used only for long-ago-removed codec swapping. Function definition was removed when adding internal voice codec.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@25868 a1c6a512-1295-4272-9138-f99709370657

--- firmware/include/memory.h
+++ firmware/include/memory.h
@@ -26,15 +26,4 @@
 
 void memset16(void *dst, int val, size_t len);
 
-/**
- * memswap128
- *
- * Exchanges the contents of two buffers.
- * For maximum efficiency, this function performs no aligning of addresses
- * and buf1, buf2 and len should be 16 byte (128 bit) aligned. Not being at
- * least longword aligned will fail on some architechtures. Any len mod 16
- * at the end is not swapped.
- */
-void memswap128(void *buf1, void *buf2, size_t len);
-
 #endif /* _MEMORY_H_ */
