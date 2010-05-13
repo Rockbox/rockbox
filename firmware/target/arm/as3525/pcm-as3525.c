Rockbox/rockbox

commit cd8ed00dfe54eab69783496e422137551dc1ca79

as3525: use the correct uncached address for rec peak buffer
also avoid disabling completely the DMA engine in pitch_detector

git-svn-id: svn://svn.rockbox.org/rockbox/trunk@25989 a1c6a512-1295-4272-9138-f99709370657

firmware/target/arm/as3525/pcm-as3525.c
@@ -260,7 +260,6 @@ void pcm_rec_dma_stop(void)
 {
     dma_disable_channel(1);
     rec_dma_size = 0;
-    dma_release();
 
     I2SOUT_CONTROL &= ~(1<<5); /* source = i2soutif fifo */
     I2SIN_CONTROL &= ~(1<<11); /* disable dma */
@@ -291,6 +290,7 @@ void pcm_rec_dma_start(void *addr, size_t size)
 
 void pcm_rec_dma_close(void)
 {
+    dma_release();
 }
 
 
@@ -311,7 +311,7 @@ void pcm_rec_dma_init(void)
 
 const void * pcm_rec_dma_get_peak_buffer(void)
 {
-    return rec_dma_start_addr;
+    return UNCACHED_ADDR((void*)DMAC_CH_DST_ADDR(1));
 }
 
 #endif /* HAVE_RECORDING */

commit 90963dcc0b46197f336cc8f9ec7399628a64fe6f

as3525: retain & release DMA engine when starting and stopping recording
This fixes the error introduced in r25989, where it would release the
DMA engine when uninitializing recording.
The real bug has been fixed instead (in pitch_detector)

git-svn-id: svn://svn.rockbox.org/rockbox/trunk@26003 a1c6a512-1295-4272-9138-f99709370657

firmware/target/arm/as3525/pcm-as3525.c
@@ -259,6 +259,7 @@ void pcm_rec_dma_record_more(void *start, size_t size)
 void pcm_rec_dma_stop(void)
 {
     dma_disable_channel(1);
+    dma_release();
     rec_dma_size = 0;
 
     I2SOUT_CONTROL &= ~(1<<5); /* source = i2soutif fifo */
@@ -290,7 +291,6 @@ void pcm_rec_dma_start(void *addr, size_t size)
 
 void pcm_rec_dma_close(void)
 {
-    dma_release();
 }
 
 