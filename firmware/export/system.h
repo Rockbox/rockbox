b6c3bc1cc90a05d462abc674ee7fc2f9567be453
Thomas Martitz <kugel@rockbox.org>
2010-05-15 21:11:20 +0000

    Get checkwps going again, don't mess with sdl in it.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@26066 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/system.h
+++ firmware/export/system.h
@@ -234,7 +234,7 @@ enum {
 
 #if !defined(SIMULATOR) && !defined(__PCTOOL__) 
 #include "system-target.h"
-#else /* SIMULATOR */
+#elif defined(HAVE_SDL) /* SIMULATOR */
 #include "system-sdl.h"
 static inline uint16_t swap16(uint16_t value)
     /*
