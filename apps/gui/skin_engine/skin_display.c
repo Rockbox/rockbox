dcbf4bbdeae394330b0e0f9bf27a9140dad48692
Jonathan Gordon <rockbox@jdgordon.info>
2010-05-21 02:35:11 +0000

    make radioart work in the sbs
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@26221 a1c6a512-1295-4272-9138-f99709370657

--- apps/gui/skin_engine/skin_display.c
+++ apps/gui/skin_engine/skin_display.c
@@ -446,7 +446,7 @@ static void wps_display_images(struct gui_wps *gwps, struct viewport* vp)
     {
         int handle = playback_current_aa_hid(data->playback_aa_slot);
 #if CONFIG_TUNER
-        if (in_radio_screen())
+        if (in_radio_screen() || (get_radio_status() != FMRADIO_OFF))
         {
             struct dim dim = {data->albumart->width, data->albumart->height};
             handle = radio_get_art_hid(&dim);
