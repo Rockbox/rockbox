36f3a8d67e6dd8015e323a237ca31891cbb6b808
Wincent Balin <wincent@rockbox.org>
2010-06-01 21:44:55 +0000

    Added myself and Pure Data team to credits. Updated copyright years where needed.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@26457 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/pdbox/pdbox.c
+++ apps/plugins/pdbox/pdbox.c
@@ -7,7 +7,7 @@
  *                     \/            \/     \/    \/            \/
  * $Id$
  *
- * Copyright (C) 2009 Wincent Balin
+ * Copyright (C) 2009, 2010 Wincent Balin
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
