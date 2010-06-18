94f18b1f9f84115850082d57a95997f79819287f
Rafaël Carré <rafael.carre@gmail.com>
2010-06-18 20:03:39 +0000

    clip+ keymap: revert r26396
    software hold is now on home+select and quickscreen on long home like on
    other clips
    
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@26938 a1c6a512-1295-4272-9138-f99709370657

--- apps/keymaps/keymap-clip.c
+++ apps/keymaps/keymap-clip.c
@@ -77,11 +77,7 @@ static const struct button_mapping button_context_wps[]  = {
     { ACTION_WPS_BROWSE,        BUTTON_SELECT|BUTTON_REL,     BUTTON_SELECT },
     { ACTION_WPS_CONTEXT,       BUTTON_SELECT|BUTTON_REPEAT,  BUTTON_SELECT },
 
-#ifndef HAS_BUTTON_HOLD /* Clip+ */
-    { ACTION_WPS_QUICKSCREEN,   BUTTON_HOME|BUTTON_SELECT,    BUTTON_HOME },
-#else
     { ACTION_WPS_QUICKSCREEN,   BUTTON_HOME|BUTTON_REPEAT,    BUTTON_HOME },
-#endif
     { ACTION_WPS_MENU,          BUTTON_HOME|BUTTON_REL,      BUTTON_HOME },
 
     { ACTION_WPS_PITCHSCREEN,   BUTTON_SELECT|BUTTON_UP,        BUTTON_SELECT },
@@ -93,7 +89,7 @@ static const struct button_mapping button_context_wps[]  = {
 #endif
 
 #ifndef HAS_BUTTON_HOLD /* Clip+ */
-    { ACTION_STD_KEYLOCK,       BUTTON_HOME|BUTTON_REPEAT,      BUTTON_HOME },
+    { ACTION_STD_KEYLOCK,       BUTTON_HOME|BUTTON_SELECT,      BUTTON_NONE },
 #endif /* HAS_BUTTON_HOLD */
 
     LAST_ITEM_IN_LIST
