ac58d02ceef534ef3a42c8d40ac106f1943f351d
Robert Bieber <robby@bieberphoto.com>
2010-07-05 20:38:04 +0000

    Theme Editor: Added a small PHP script to auto-generate a qrc entry for a directory with aliases for each file
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@27298 a1c6a512-1295-4272-9138-f99709370657

---
+++ utils/themeeditor/addresourcedir.php
@@ -0,0 +1,46 @@
+#!/usr/bin/php -q
+<?php
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2010 Robert Bieber
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+ 
+if($argc < 2)
+    $path = getcwd();
+else
+    $path = $argv[1];
+if($path[count($path) - 1] != '/')
+    $path = $path . '/';
+$dir = dir($path);
+$split = explode("/", $path);
+$last = $split[count($split) - 2];
+echo "\t<qresource prefix=\"/$last\">\n";
+while(false !== ($entry = $dir->read()))
+{
+    if($entry == '.' || $entry == '..')
+        continue;
+    echo "\t\t";
+    echo "<file alias = \"$entry\">";
+    echo $path . $entry . "</file>";
+    echo "\n";
+}
+echo "\t</qresource>\n";
+$dir->close();
+
+?>
