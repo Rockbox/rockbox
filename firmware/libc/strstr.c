c9f997cc91db23e0ba3ac60fa6c37ae066e5265d
Rafaël Carré <rafael.carre@gmail.com>
2010-07-11 22:37:31 +0000

    strstr(): cosmetics
    remove extra ';'
    replace tabs by spaces
    
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@27396 a1c6a512-1295-4272-9138-f99709370657

--- firmware/libc/strstr.c
+++ firmware/libc/strstr.c
@@ -18,22 +18,22 @@
 
 char *strstr(const char *s1, const char *s2)
 {
-	register const char *s = s1;
-	register const char *p = s2;
+    register const char *s = s1;
+    register const char *p = s2;
 
-	do {
-		if (!*p) {
-			return (char *) s1;;
-		}
-		if (*p == *s) {
-			++p;
-			++s;
-		} else {
-			p = s2;
-			if (!*s) {
-				return NULL;
-			}
-			s = ++s1;
-		}
-	} while (1);
+    do {
+        if (!*p) {
+            return (char *) s1;
+        }
+        if (*p == *s) {
+            ++p;
+            ++s;
+        } else {
+            p = s2;
+            if (!*s) {
+                return NULL;
+            }
+            s = ++s1;
+        }
+    } while (1);
 }
