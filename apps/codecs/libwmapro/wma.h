86c3765246e87ec161f96192e539997e82a60b56
Mohamed Tarek <mt@rockbox.org>
2010-07-17 08:09:17 +0000

    libwmapro : remove dead/unneeded code from wma.[ch]
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@27455 a1c6a512-1295-4272-9138-f99709370657

--- apps/codecs/libwmapro/wma.h
+++ apps/codecs/libwmapro/wma.h
@@ -50,8 +50,6 @@
 #define VLCBITS 9
 #define VLCMAX ((22+VLCBITS-1)/VLCBITS)
 
-typedef float WMACoef;          ///< type for decoded coefficients, int16_t would be enough for wma 1/2
-
 typedef struct CoefVLCTable {
     int n;                      ///< total number of codes
     int max_level;
@@ -63,7 +61,6 @@ typedef struct CoefVLCTable {
 
 int ff_wma_get_frame_len_bits(int sample_rate, int version,
                                       unsigned int decode_flags);
-int ff_wma_total_gain_to_bits(int total_gain);
 unsigned int ff_wma_get_large_val(GetBitContext* gb);
 int ff_wma_run_level_decode(GetBitContext* gb,
                             VLC *vlc,
