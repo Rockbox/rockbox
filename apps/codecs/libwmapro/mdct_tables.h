9dddfa78e60e1494006f5fb5492a49b06497bf04
Mohamed Tarek <mt@rockbox.org>
2010-08-05 06:10:14 +0000

    oops ! shouldn't have removed mdct_tables.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@27706 a1c6a512-1295-4272-9138-f99709370657

---
+++ apps/codecs/libwmapro/mdct_tables.h
@@ -0,0 +1,10 @@
+#ifndef _MDCT_TABLES_H_
+#define _MDCT_TABLES_H_
+#include <inttypes.h>
+
+extern const int32_t *sine_windows[6];
+/* rockbox: not used
+extern const int32_t sincos_lookup_wmap[8064];
+*/
+#endif /* _MDCT_TABLES_H_ */
+
