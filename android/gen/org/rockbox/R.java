6063e9138b2c5a1ca25b3cd1ece0f9ce248f56a1
Thomas Martitz <kugel@rockbox.org>
2010-08-06 00:53:02 +0000

    Fix launcher icon (once more).
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@27733 a1c6a512-1295-4272-9138-f99709370657

--- android/gen/org/rockbox/R.java
+++ android/gen/org/rockbox/R.java
@@ -12,7 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int rb=0x7f020001;
+        public static final int launcher=0x7f020001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
