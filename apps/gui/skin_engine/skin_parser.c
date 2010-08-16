a71db516f2e005ffd5642e568fa38f61faf4d047
Jonathan Gordon <rockbox@jdgordon.info>
2010-08-16 10:40:50 +0000

    fix FS#11542 - %t() not working
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@27826 a1c6a512-1295-4272-9138-f99709370657

--- apps/gui/skin_engine/skin_parser.c
+++ apps/gui/skin_engine/skin_parser.c
@@ -528,6 +528,8 @@ static int parse_timeout_tag(struct skin_element *element,
     else
         val = element->params[0].data.number;
     token->value.i = val * TIMEOUT_UNIT;
+    if (token->type == SKIN_TOKEN_SUBLINE_TIMEOUT)
+        curr_line->timeout = token->value.i;
     return 0;
 }
 
