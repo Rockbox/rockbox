f6912d3bbb9a2f7f46c72a42fc2d7d7b3c3343b3
Frank Gevaerts <frank@gevaerts.be>
2010-09-17 19:05:27 +0000

    Make the call to buttonlight_on() properly depend on HAVE_BUTTON_LIGHT
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@28096 a1c6a512-1295-4272-9138-f99709370657

--- firmware/target/arm/sandisk/sansa-e200/button-e200.c
+++ firmware/target/arm/sandisk/sansa-e200/button-e200.c
@@ -171,7 +171,9 @@ void clickwheel_int(void)
          * than every 1/4 second */
         next_backlight_on = current_tick + HZ/4;
         backlight_on();
+#ifdef HAVE_BUTTON_LIGHT
         buttonlight_on();
+#endif
         reset_poweroff_timer();
     }
 
