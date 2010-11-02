58369ca292727811eed8f4fc654fbaf75fa70ae3
Robert Menes <rmenes@rockbox.org>
2010-11-02 20:09:02 +0000

    More plugin keymaps for the HDD6330. Quite a lot of commits. ;)
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@28450 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/snake.c
+++ apps/plugins/snake.c
@@ -163,7 +163,8 @@ dir is the current direction of the snake - 0=up, 1=right, 2=down, 3=left;
 #define SNAKE_DOWN BUTTON_DOWN
 #define SNAKE_PLAYPAUSE BUTTON_PLAY
 
-#elif CONFIG_KEYPAD == PHILIPS_HDD1630_PAD
+#elif (CONFIG_KEYPAD == PHILIPS_HDD1630_PAD) || \
+    (CONFIG_KEYPAD == PHILIPS_HDD6330_PAD)
 #define SNAKE_QUIT BUTTON_POWER
 #define SNAKE_LEFT BUTTON_LEFT
 #define SNAKE_RIGHT BUTTON_RIGHT
