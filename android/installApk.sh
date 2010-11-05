5e8863073e9b128b13bda8e1d1f014ec820a42cc
Maurus Cuelenaere <mcuelenaere@gmail.com>
2010-11-05 23:40:00 +0000

    Android: add uninstall option to installApk.sh script
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@28507 a1c6a512-1295-4272-9138-f99709370657

--- android/installApk.sh
+++ android/installApk.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 ADB="$ANDROID_SDK_PATH/tools/adb"
 
+if [ "$1" == "-r" ]; then
+    echo 'pm uninstall org.rockbox; exit' | $ADB shell
+fi
 $ADB install rockbox.apk
 echo 'am start -W -a android.intent.action.MAIN -n org.rockbox/.RockboxActivity; exit' | $ADB shell
