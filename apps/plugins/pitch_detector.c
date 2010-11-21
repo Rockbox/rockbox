28ecf9b6d17b79c4b009aabf7b361a0ea734dd51
Andree Buschmann <AndreeBuschmann@t-online.de>
2010-11-21 22:02:06 +0000

    Use MEM_ALIGN_ATTR in pitch detector plugin.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@28636 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/pitch_detector.c
+++ apps/plugins/pitch_detector.c
@@ -244,7 +244,7 @@ static int audio_head = 0; /* which of the two buffers to use? */
 static volatile int audio_tail = 0; /* which of the two buffers to record? */
 /* It's stereo, so make the buffer twice as big */
 #ifndef SIMULATOR
-static int16_t audio_data[2][BUFFER_SIZE] __attribute__((aligned(CACHEALIGN_SIZE)));
+static int16_t audio_data[2][BUFFER_SIZE] MEM_ALIGN_ATTR;
 static fixed yin_buffer[YIN_BUFFER_SIZE] IBSS_ATTR;
 #ifdef PLUGIN_USE_IRAM
 static int16_t iram_audio_data[BUFFER_SIZE] IBSS_ATTR;
