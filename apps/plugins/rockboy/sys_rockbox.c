e00acb5979e071d81cb9f5493bf9f75b2f5248bb
Frank Gevaerts <frank@gevaerts.be>
2010-11-26 17:55:08 +0000

    3.7 branch: backport r28669, fix for FS#11745 (rockboy screen on mono screens with height 64)
    git-svn-id: svn://svn.rockbox.org/rockbox/branches/v3_7@28671 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/rockboy/sys_rockbox.c
+++ apps/plugins/rockboy/sys_rockbox.c
@@ -236,7 +236,7 @@ void vid_update(int scanline)
         scanline-=16;
     else if (fb.mode==2)
         scanline-=8;
-    scanline_remapped = scanline / 16;
+    scanline_remapped = (scanline / 16 + 7) % 8;
     frameb = rb->lcd_framebuffer + scanline_remapped * LCD_WIDTH;
     while (cnt < 160) {
         balance += LCD_WIDTH;
