fd240ab240352c43159929f20f8ce63e56c60133
Alexander Levin <al.le@rockbox.org>
2011-01-11 21:29:36 +0000

    pitch_detector: bring the name of the settings file in line with the plugin name
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@29031 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/pitch_detector.c
+++ apps/plugins/pitch_detector.c
@@ -300,7 +300,7 @@ static struct tuner_settings
 /* Settings loading and saving(adapted from the clock plugin)      */
 /*=================================================================*/
 
-#define SETTINGS_FILENAME PLUGIN_APPS_DIR "/.pitch_settings"
+#define SETTINGS_FILENAME PLUGIN_APPS_DIR "/.pitch_detector_settings"
 
 /* The settings as they exist on the hard disk, so that
  * we can know at saving time if changes have been made */
