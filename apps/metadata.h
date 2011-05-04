37a9a200b2ab93f02b8085e78aac91b8ff68419c
Michael Giacomelli <giac2000@hotmail.com>
2011-05-04 17:29:53 +0000

    Commit FS#12096 by Stephan Grossklass.  Increases the maximum ID3v2 field length to 240 bytes on large memory targets, and 90 bytes on low memory targets.  Total bytes for all tags combined remains unchanged.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@29821 a1c6a512-1295-4272-9138-f99709370657

--- apps/metadata.h
+++ apps/metadata.h
@@ -176,10 +176,10 @@ extern const struct afmt_entry audio_formats[AFMT_NUM_CODECS];
 
 #if MEMORYSIZE > 2
 #define ID3V2_BUF_SIZE 900
-#define ID3V2_MAX_ITEM_SIZE 120
+#define ID3V2_MAX_ITEM_SIZE 240
 #else
 #define ID3V2_BUF_SIZE 300
-#define ID3V2_MAX_ITEM_SIZE 60
+#define ID3V2_MAX_ITEM_SIZE 90
 #endif
 
 enum {
