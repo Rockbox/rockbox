Rockbox/rockbox

commit 0e263d5b779f19033730fff7ec6f16b0715a976d

accept FS#10992 by Rui Araújo to fix FS#10976 - make the <Untagged> string translatable
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@26192 a1c6a512-1295-4272-9138-f99709370657

apps/tagtree.c
@@ -1169,6 +1169,12 @@ static int retrieve_entries(struct tree_context *c, int offset, bool init)
         if (total_count++ < offset)
             continue;
         
+        if ( strcmp(tcs.result , UNTAGGED ) == 0)
+        {
+            tcs.result_len = strlcpy(tcs.result, 
+                                     str(LANG_TAGNAVI_UNTAGGED), TAG_MAXLEN )+1;
+        }
+        
         dptr->newtable = NAVIBROWSE;
         if (tag == tag_title || tag == tag_filename)
         {

apps/lang/english.lang
@@ -13731,4 +13731,17 @@
     gigabeats: "3-D Enhancement"
   </voice>
 </phrase>
-
+<phrase>
+  id: LANG_TAGNAVI_UNTAGGED
+  desc: "<untagged>" entry in tag browser
+  user: core
+  <source>
+    *: "<Untagged>"
+  </source>
+  <dest>
+    *: "<Untagged>"
+  </dest>
+  <voice>
+    *: "Untagged"
+  </voice>
+</phrase>

apps/lang/portugues.lang
@@ -13214,6 +13214,20 @@
   </voice>
 </phrase>
 <phrase>
+  id: LANG_TAGNAVI_UNTAGGED
+  desc: "<untagged>" entry in tag browser
+  user: core
+  <source>
+    *: "<Untagged>"
+  </source>
+  <dest>
+    *: "<Sem Etiqueta>"
+  </dest>
+  <voice>
+    *: "Sem etiqueta"
+  </voice>
+</phrase>
+<phrase>
   id: LANG_OF
   desc: Shown in WPS: X of Y (tracks)
   user: core

apps/tagcache.c
@@ -95,8 +95,6 @@ static long tagcache_stack[(DEFAULT_STACK_SIZE + 0x4000)/sizeof(long)];
 static const char tagcache_thread_name[] = "tagcache";
 #endif
 
-#define UNTAGGED "<Untagged>"
-
 /* Previous path when scanning directory tree recursively. */
 static char curpath[TAG_MAXLEN+32];
 

apps/tagcache.h
@@ -95,6 +95,9 @@ enum tag_type { tag_artist = 0, tag_album, tag_genre, tag_title,
 /* Serialized DB. */
 #define TAGCACHE_STATEFILE       ROCKBOX_DIR "/database_state.tcd"
 
+/* Tag to be used on untagged files. */
+#define UNTAGGED "<Untagged>"
+
 /* Numeric tags (we can use these tags with conditional clauses). */
 #define TAGCACHE_NUMERIC_TAGS ((1LU << tag_year) | (1LU << tag_discnumber) | \
     (1LU << tag_tracknumber) | (1LU << tag_length) | (1LU << tag_bitrate) | \

commit 05a1984eb32dd07bcf5d54c147ace4d6f56a00b4

FS#12065 - Fix two regressions introduced with r26192 (FS#10976 / FS#10992 - make the <Untagged> string translatable):
* The translation was done too early: before the tagnavi format
  clauses were checked.  This prevented comparisons against
  "<Untagged>" when a non-English translation was active.

* The code overwrote the buffer containing the constant string
  "<Untagged>" with the translation.  This is neither safe nor
  necessary: The result pointer simply can be swapped with a pointer
  to the translation.

git-svn-id: svn://svn.rockbox.org/rockbox/trunk@29941 a1c6a512-1295-4272-9138-f99709370657

apps/tagtree.c
@@ -1251,12 +1251,6 @@ static int retrieve_entries(struct tree_context *c, int offset, bool init)
         if (total_count++ < offset)
             continue;
         
-        if ( strcmp(tcs.result , UNTAGGED ) == 0)
-        {
-            tcs.result_len = strlcpy(tcs.result, 
-                                     str(LANG_TAGNAVI_UNTAGGED), TAG_MAXLEN )+1;
-        }
-        
         dptr->newtable = NAVIBROWSE;
         if (tag == tag_title || tag == tag_filename)
         {
@@ -1281,6 +1275,13 @@ static int retrieve_entries(struct tree_context *c, int offset, bool init)
             }
         }
 
+        if (strcmp(tcs.result, UNTAGGED) == 0)
+        {
+            tcs.result = str(LANG_TAGNAVI_UNTAGGED);
+            tcs.result_len = strlen(tcs.result);
+            tcs.ramresult = true;
+        }
+        
         if (!tcs.ramresult || fmt)
         {
             char buf[MAX_PATH];