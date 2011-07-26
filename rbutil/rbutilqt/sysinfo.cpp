1ec180803a43396f1d2fa7d4b7df77005e8aac28
Dominik Riebeling <Dominik.Riebeling@gmail.com>
2011-07-26 20:54:49 +0000

    Show cluster size in System Info dialog.
    Enlarge the default size of the dialog to something more reasonable for the
    extended information displayed.
    
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30215 a1c6a512-1295-4272-9138-f99709370657

--- rbutil/rbutilqt/sysinfo.cpp
+++ rbutil/rbutilqt/sysinfo.cpp
@@ -64,13 +64,15 @@ QString Sysinfo::getInfo()
     QStringList drives = Utils::mountpoints();
     info += "<table>";
     info += "<tr><td>" + tr("Mountpoint") + "</td><td>" + tr("Label")
-            + "</td><td>" + tr("Free") + "</td><td>" + tr("Total") + "</td></tr>";
+            + "</td><td>" + tr("Free") + "</td><td>" + tr("Total") + "</td><td>"
+            + tr("Cluster Size") + "</td></tr>";
     for(int i = 0; i < drives.size(); i++) {
-        info += tr("<tr><td>%1</td><td>%4</td><td>%2 GiB</td><td>%3 GiB</td></tr>")
+        info += tr("<tr><td>%1</td><td>%4</td><td>%2 GiB</td><td>%3 GiB</td><td>%5</td></tr>")
             .arg(QDir::toNativeSeparators(drives.at(i)))
             .arg((double)Utils::filesystemFree(drives.at(i)) / (1<<30), 0, 'f', 2)
             .arg((double)Utils::filesystemTotal(drives.at(i)) / (1<<30), 0, 'f', 2)
-            .arg(Utils::filesystemName(drives.at(i)));
+            .arg(Utils::filesystemName(drives.at(i)))
+            .arg(Utils::filesystemClusterSize(drives.at(i)));
     }
     info += "</table>";
     info += "<hr/>";
