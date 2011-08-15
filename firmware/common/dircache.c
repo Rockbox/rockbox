Rockbox/rockbox

commit d1322b71595336740eb5e18e5deed056ddb71c7a

GSoC/Buflib: Replace all direct accesses to audiobuf with buffer API functions.
Namely, introduce buffer_get_buffer() and buffer_release_buffer().
buffer_get_buffer() aquires all available and grabs a lock, attempting to
call buffer_alloc() or buffer_get_buffer() while this lock is locked will cause
a panicf() (doesn't actually happen, but is for debugging purpose).
buffer_release_buffer() unlocks that lock and can additionally increment the
audiobuf buffer to make an allocation. Pass 0 to only unlock if buffer was
used temporarily only.
buffer_available() is a replacement function to query audiobuflen, i.e. what's
left in the buffer.
Buffer init is moved up in the init chain and handles ipodvideo64mb internally.

Further changes happened to mp3data.c and talk.c as to not call the above API
functions, but get the buffer from callers. The caller is the audio system
which has the buffer lock while mp3data.c and talk mess with the buffer.
mpeg.c now implements some buffer related functions of playback.h, especially
audio_get_buffer(), allowing to reduce #ifdef hell a tiny bit.

audiobuf and audiobufend are local to buffer.c now.

git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30308 a1c6a512-1295-4272-9138-f99709370657

firmware/common/dircache.c
@@ -862,22 +862,26 @@ int dircache_build(int last_size)
      * and their corresponding d_name from the end
      * after generation the buffer will be compacted with DIRCACHE_RESERVE
      * free bytes inbetween */
-    audiobuf = ALIGN_UP(audiobuf, sizeof(struct dircache_entry));
-    dircache_root = (struct dircache_entry*)audiobuf;
-    d_names_start = d_names_end = audiobufend - 1;
+    size_t got_size;
+    char* buf = buffer_get_buffer(&got_size);
+    ALIGN_BUFFER(buf, got_size, sizeof(struct dircache_entry));
+    d_names_start = d_names_end = (char*)dircache_root + got_size - 1;
     dircache_size = 0;
     generate_dot_d_names();
 
     /* Start a non-transparent rebuild. */
     int res = dircache_do_rebuild();
     if (res < 0)
-        return res;
+        goto fail;
 
     /* now compact the dircache buffer */
     char* dst = ((char*)&dircache_root[entry_count] + DIRCACHE_RESERVE);
     ptrdiff_t offset = d_names_start - dst;
     if (offset <= 0) /* something went wrong */
-        return -1;
+    {
+        res = -1;
+        goto fail;
+    }
 
     /* memmove d_names down, there's a possibility of overlap
      * equivaent to dircache_size - entry_count*sizeof(struct dircache_entry) */
@@ -896,15 +900,19 @@ int dircache_build(int last_size)
     /* equivalent to dircache_size + DIRCACHE_RESERVE */
     allocated_size = (d_names_end - (char*)dircache_root);
     reserve_used = 0;
-    audiobuf += allocated_size;
 
+    buffer_release_buffer(allocated_size);
+    return res;
+fail:
+    dircache_disable();
+    buffer_release_buffer(0);
     return res;
 }
 
 /**
  * Steal the allocated dircache buffer and disable dircache.
  */
-void* dircache_steal_buffer(long *size)
+void* dircache_steal_buffer(size_t *size)
 {
     dircache_disable();
     if (dircache_size == 0)

apps/filetree.c
@@ -600,6 +600,7 @@ int ft_enter(struct tree_context* c)
                 /* firmware file */
             case FILE_ATTR_MOD:
                 splash(0, ID2P(LANG_WAIT));
+                audio_hard_stop();
                 rolo_load(buf);
                 break;
 #endif

apps/main.c
@@ -337,11 +337,11 @@ static void init_tagcache(void)
 static void init(void)
 {
     system_init();
+    buffer_init();
     kernel_init();
 #ifdef APPLICATION
     paths_init();
 #endif
-    buffer_init();
     enable_irq();
     lcd_init();
 #ifdef HAVE_REMOTE_LCD
@@ -428,13 +428,7 @@ static void init(void)
 #endif
 
     system_init();
-#if defined(IPOD_VIDEO)
-    audiobufend=(unsigned char *)audiobufend_lds;
-    if(MEMORYSIZE==64 && probed_ramsize!=64)
-    {
-        audiobufend -= (32<<20);
-    }
-#endif
+    buffer_init();
     kernel_init();
 
 #ifdef HAVE_ADJUSTABLE_CPU_FREQ
@@ -445,7 +439,6 @@ static void init(void)
     cpu_boost(true);
 #endif
     
-    buffer_init();
 
     settings_reset();
 

apps/menus/main_menu.c
@@ -182,7 +182,7 @@ static const char* info_getname(int selected_item, void *data,
 
         case INFO_BUFFER: /* buffer */
         {
-            long kib = (audiobufend - audiobuf) / 1024; /* to KiB */
+            long kib = buffer_available() / 1024; /* to KiB */
             output_dyn_value(s1, sizeof(s1), kib, kbyte_units, true);
             snprintf(buffer, buffer_len, "%s %s", str(LANG_BUFFER_STAT), s1);
         }
@@ -272,7 +272,7 @@ static int info_speak_item(int selected_item, void * data)
         case INFO_BUFFER: /* buffer */
         {
             talk_id(LANG_BUFFER_STAT, false);
-            long kib = (audiobufend - audiobuf) / 1024; /* to KiB */
+            long kib = buffer_available() / 1024; /* to KiB */
             output_dyn_value(NULL, 0, kib, kbyte_units, true);
             break;
         }

apps/misc.c
@@ -766,7 +766,10 @@ void check_bootfile(bool do_rolo)
                     static const struct text_message message={ lines, 2 };
                     button_clear_queue(); /* Empty the keyboard buffer */
                     if(gui_syncyesno_run(&message, NULL, NULL) == YESNO_YES)
+                    {
+                        audio_hard_stop();
                         rolo_load(BOOTDIR "/" BOOTFILE);
+                    }
                 }
             }
             wrtdate = info.wrtdate;

apps/mp3data.c
@@ -311,17 +311,18 @@ unsigned long find_next_frame(int fd,
 #ifndef __PCTOOL__
 static int fnf_read_index;
 static int fnf_buf_len;
+static unsigned char *fnf_buf;
 
 static int buf_getbyte(int fd, unsigned char *c)
 {
     if(fnf_read_index < fnf_buf_len)
     {
-        *c = audiobuf[fnf_read_index++];
+        *c = fnf_buf[fnf_read_index++];
         return 1;
     }
     else
     {
-        fnf_buf_len = read(fd, audiobuf, audiobufend - audiobuf);
+        fnf_buf_len = read(fd, fnf_buf, fnf_buf_len);
         if(fnf_buf_len < 0)
             return -1;
 
@@ -329,7 +330,7 @@ static int buf_getbyte(int fd, unsigned char *c)
 
         if(fnf_buf_len > 0)
         {
-            *c = audiobuf[fnf_read_index++];
+            *c = fnf_buf[fnf_read_index++];
             return 1;
         }
         else
@@ -345,7 +346,7 @@ static int buf_seek(int fd, int len)
     {
         len = fnf_read_index - fnf_buf_len;
         
-        fnf_buf_len = read(fd, audiobuf, audiobufend - audiobuf);
+        fnf_buf_len = read(fd, fnf_buf, fnf_buf_len);
         if(fnf_buf_len < 0)
             return -1;
 
@@ -361,9 +362,10 @@ static int buf_seek(int fd, int len)
         return 0;
 }
 
-static void buf_init(void)
+static void buf_init(unsigned char* buf, size_t buflen)
 {
-    fnf_buf_len = 0;
+    fnf_buf = buf;
+    fnf_buf_len = buflen;
     fnf_read_index = 0;
 }
 
@@ -372,8 +374,9 @@ static unsigned long buf_find_next_frame(int fd, long *offset, long max_offset)
     return __find_next_frame(fd, offset, max_offset, 0, buf_getbyte, true);
 }
 
-static int audiobuflen;
-static int mem_pos;
+static size_t mem_buflen;
+static unsigned char* mem_buf;
+static size_t mem_pos;
 static int mem_cnt;
 static int mem_maxlen;
 
@@ -381,8 +384,8 @@ static int mem_getbyte(int dummy, unsigned char *c)
 {
     dummy = dummy;
     
-    *c = audiobuf[mem_pos++];
-    if(mem_pos >= audiobuflen)
+    *c = mem_buf[mem_pos++];
+    if(mem_pos >= mem_buflen)
         mem_pos = 0;
 
     if(mem_cnt++ >= mem_maxlen)
@@ -394,9 +397,11 @@ static int mem_getbyte(int dummy, unsigned char *c)
 unsigned long mem_find_next_frame(int startpos, 
                                   long *offset, 
                                   long max_offset,
-                                  unsigned long reference_header)
+                                  unsigned long reference_header,
+                                  unsigned char* buf, size_t buflen)
 {
-    audiobuflen = audiobufend - audiobuf;
+    mem_buf = buf;
+    mem_buflen = buflen;
     mem_pos = startpos;
     mem_cnt = 0;
     mem_maxlen = max_offset;
@@ -620,8 +625,9 @@ static void long2bytes(unsigned char *buf, long val)
     buf[3] = val & 0xff;
 }
 
-int count_mp3_frames(int fd, int startpos, int filesize,
-                     void (*progressfunc)(int))
+int count_mp3_frames(int fd,  int startpos,  int filesize,
+                     void (*progressfunc)(int),
+                     unsigned char* buf, size_t buflen)
 {
     unsigned long header = 0;
     struct mp3info info;
@@ -637,7 +643,7 @@ int count_mp3_frames(int fd, int startpos, int filesize,
     if(lseek(fd, startpos, SEEK_SET) < 0)
         return -1;
 
-    buf_init();
+    buf_init(buf, buflen);
 
     /* Find out the total number of frames */
     num_frames = 0;
@@ -687,7 +693,8 @@ static const char cooltext[] = "Rockbox - rocks your box";
 int create_xing_header(int fd, long startpos, long filesize,
                        unsigned char *buf, unsigned long num_frames,
                        unsigned long rec_time, unsigned long header_template,
-                       void (*progressfunc)(int), bool generate_toc)
+                       void (*progressfunc)(int), bool generate_toc,
+                       unsigned char *tempbuf, size_t tempbuflen )
 {   
     struct mp3info info;
     unsigned char toc[100];
@@ -705,7 +712,7 @@ int create_xing_header(int fd, long startpos, long filesize,
     if(generate_toc)
     {
         lseek(fd, startpos, SEEK_SET);
-        buf_init();
+        buf_init(tempbuf, tempbuflen);
 
         /* Generate filepos table */
         last_pos = 0;

apps/mp3data.h
@@ -26,6 +26,8 @@
 #define MPEG_VERSION2   1
 #define MPEG_VERSION2_5 2
 
+#include <string.h> /* size_t */
+
 struct mp3info {
     /* Standard MP3 frame header fields */
     int version;
@@ -63,23 +65,21 @@ unsigned long find_next_frame(int fd,
                               unsigned long reference_header);
 unsigned long mem_find_next_frame(int startpos, 
                                   long *offset, 
-                                  long max_offset, 
-                                  unsigned long reference_header);
+                                  long max_offset,
+                                  unsigned long reference_header,
+                                  unsigned char* buf, size_t buflen);
 int get_mp3file_info(int fd, 
                      struct mp3info *info);
-int count_mp3_frames(int fd, 
-                     int startpos, 
-                     int filesize,
-                     void (*progressfunc)(int));
-int create_xing_header(int fd, 
-                       long startpos, 
-                       long filesize,
-                       unsigned char *buf, 
-                       unsigned long num_frames,
-                       unsigned long rec_time, 
-                       unsigned long header_template,
-                       void (*progressfunc)(int), 
-                       bool generate_toc);
+
+int count_mp3_frames(int fd,  int startpos,  int filesize,
+                     void (*progressfunc)(int),
+                     unsigned char* buf, size_t buflen);
+
+int create_xing_header(int fd, long startpos, long filesize,
+                       unsigned char *buf, unsigned long num_frames,
+                       unsigned long rec_time, unsigned long header_template,
+                       void (*progressfunc)(int), bool generate_toc,
+                       unsigned char *tempbuf, size_t tempbuflen );
 
 extern unsigned long bytes2int(unsigned long b0,
                                unsigned long b1,

apps/mpeg.c
@@ -37,6 +37,7 @@
 #include "mp3data.h"
 #include "buffer.h"
 #include "mp3_playback.h"
+#include "talk.h"
 #include "sound.h"
 #include "bitswap.h"
 #include "appevents.h"
@@ -144,19 +145,19 @@ static unsigned int mpeg_errno;
 static bool playing = false;    /* We are playing an MP3 stream */
 static bool is_playing = false; /* We are (attempting to) playing MP3 files */
 static bool paused;             /* playback is paused */
+static char* mpeg_audiobuf;     /* the audio buffer */
+static long audiobuflen;        /* length of the audio buffer */
 
 #ifdef SIMULATOR
 static char mpeg_stack[DEFAULT_STACK_SIZE];
 static struct mp3entry taginfo;
-
 #else /* !SIMULATOR */
 static struct event_queue mpeg_queue SHAREDBSS_ATTR;
 static long mpeg_stack[(DEFAULT_STACK_SIZE + 0x1000)/sizeof(long)];
 
-static int audiobuflen;
 static int audiobuf_write;
 static int audiobuf_swapwrite;
-static int audiobuf_read;
+static long audiobuf_read;
 
 static int mpeg_file;
 
@@ -490,6 +491,18 @@ unsigned long mpeg_get_last_header(void)
 #endif /* !SIMULATOR */
 }
 
+
+unsigned char * audio_get_buffer(bool talk_buf, size_t *buffer_size)
+{
+    (void)talk_buf; /* always grab the voice buffer for now */
+
+    audio_hard_stop();
+    if (buffer_size) /* special case for talk_init() */
+        return buffer_get_buffer(buffer_size);
+    return NULL;
+}
+
+
 #ifndef SIMULATOR
 /* Send callback events to notify about removing old tracks. */
 static void generate_unbuffer_events(void)
@@ -708,7 +721,7 @@ void rec_tick(void)
                 
             xor_b(0x08, &PADRH);                 /* Set PR inactive */
 
-            audiobuf[audiobuf_write++] = data;
+            mpeg_audiobuf[audiobuf_write++] = data;
 
             if (audiobuf_write >= audiobuflen)
                 audiobuf_write = 0;
@@ -825,7 +838,7 @@ static void transfer_end(unsigned char** ppbuf, size_t* psize)
             }
 
             *psize = last_dma_chunk_size & 0xffff;
-            *ppbuf = audiobuf + audiobuf_read;
+            *ppbuf = mpeg_audiobuf + audiobuf_read;
             track = get_trackdata(0);
             if(track)
                 track->id3.offset += last_dma_chunk_size;
@@ -1128,7 +1141,7 @@ static void start_playback_if_ready(void)
             playing = true;
 
             last_dma_chunk_size = MIN(0x2000, get_unplayed_space_current_song());
-            mp3_play_data(audiobuf + audiobuf_read, last_dma_chunk_size, transfer_end);
+            mp3_play_data(mpeg_audiobuf + audiobuf_read, last_dma_chunk_size, transfer_end);
             dma_underrun = false;
 
             if (!paused)
@@ -1173,7 +1186,7 @@ static bool swap_one_chunk(void)
         amount_to_swap = MIN(audiobuf_write - audiobuf_swapwrite,
                              amount_to_swap);
 
-    bitswap(audiobuf + audiobuf_swapwrite, amount_to_swap);
+    bitswap(mpeg_audiobuf + audiobuf_swapwrite, amount_to_swap);
 
     audiobuf_swapwrite += amount_to_swap;
     if(audiobuf_swapwrite >= audiobuflen)
@@ -1341,7 +1354,7 @@ static void mpeg_thread(void)
                     track_change();
                     audiobuf_read = get_trackdata(0)->mempos;
                     last_dma_chunk_size = MIN(0x2000, get_unplayed_space_current_song());
-                    mp3_play_data(audiobuf + audiobuf_read, last_dma_chunk_size, transfer_end);
+                    mp3_play_data(mpeg_audiobuf + audiobuf_read, last_dma_chunk_size, transfer_end);
                     dma_underrun = false;
                     last_dma_tick = current_tick;
 
@@ -1501,7 +1514,7 @@ static void mpeg_thread(void)
                         /* resume will start at new position */
                         last_dma_chunk_size = 
                             MIN(0x2000, get_unplayed_space_current_song());
-                        mp3_play_data(audiobuf + audiobuf_read, 
+                        mp3_play_data(mpeg_audiobuf + audiobuf_read, 
                             last_dma_chunk_size, transfer_end);
                         dma_underrun = false;
                     }
@@ -1632,7 +1645,7 @@ static void mpeg_thread(void)
                 {
                     DEBUGF("R\n");
                     t1 = current_tick;
-                    len = read(mpeg_file, audiobuf + audiobuf_write,
+                    len = read(mpeg_file, mpeg_audiobuf + audiobuf_write,
                                amount_to_read);
                     if(len > 0)
                     {
@@ -1659,7 +1672,7 @@ static void mpeg_thread(void)
                                 if(tagptr >= audiobuflen)
                                     tagptr -= audiobuflen;
 
-                                if(audiobuf[tagptr] != tag[i])
+                                if(mpeg_audiobuf[tagptr] != tag[i])
                                 {
                                     taglen = 0;
                                     break;
@@ -1773,19 +1786,20 @@ static void mpeg_thread(void)
                         startpos = prerecord_buffer[startpos].mempos;
 
                         DEBUGF("Start looking at address %x (%x)\n",
-                               audiobuf+startpos, startpos);
+                               mpeg_audiobuf+startpos, startpos);
 
                         saved_header = mpeg_get_last_header();
                         
                         mem_find_next_frame(startpos, &offset, 1800,
-                                            saved_header);
+                                            saved_header, mpeg_audiobuf,
+                                            audiobuflen);
 
                         audiobuf_read = startpos + offset;
                         if(audiobuf_read >= audiobuflen)
                             audiobuf_read -= audiobuflen;
 
                         DEBUGF("New audiobuf_read address: %x (%x)\n",
-                               audiobuf+audiobuf_read, audiobuf_read);
+                               mpeg_audiobuf+audiobuf_read, audiobuf_read);
 
                         level = disable_irq_save();
                         num_rec_bytes = get_unsaved_space();
@@ -1894,7 +1908,8 @@ static void mpeg_thread(void)
                             save_endpos += audiobuflen;
 
                         rc = mem_find_next_frame(save_endpos, &offset, 1800,
-                                                 saved_header);
+                                                 saved_header, mpeg_audiobuf,
+                                                 audiobuflen);
                         if (!rc) /* No header found, save whole buffer */
                             offset = 1800;
 
@@ -1936,7 +1951,7 @@ static void mpeg_thread(void)
 #elif MEMORYSIZE == 8
                     amount_to_save = MIN(0x100000, amount_to_save);
 #endif
-                    rc = write(mpeg_file, audiobuf + audiobuf_read,
+                    rc = write(mpeg_file, mpeg_audiobuf + audiobuf_read,
                                amount_to_save);
                     if (rc < 0)
                     {
@@ -2256,21 +2271,21 @@ static void prepend_header(void)
     if(audiobuf_read < 0)
     {
         /* Clear the bottom half */
-        memset(audiobuf, 0, audiobuf_read + MPEG_RESERVED_HEADER_SPACE);
+        memset(mpeg_audiobuf, 0, audiobuf_read + MPEG_RESERVED_HEADER_SPACE);
 
         /* And the top half */
         audiobuf_read += audiobuflen;
-        memset(audiobuf + audiobuf_read, 0, audiobuflen - audiobuf_read);
+        memset(mpeg_audiobuf + audiobuf_read, 0, audiobuflen - audiobuf_read);
     }
     else
     {
-        memset(audiobuf + audiobuf_read, 0, MPEG_RESERVED_HEADER_SPACE);
+        memset(mpeg_audiobuf + audiobuf_read, 0, MPEG_RESERVED_HEADER_SPACE);
     }
     /* Copy the empty ID3 header */
     startpos = audiobuf_read;
     for(i = 0; i < sizeof(empty_id3_header); i++)
     {
-        audiobuf[startpos++] = empty_id3_header[i];
+        mpeg_audiobuf[startpos++] = empty_id3_header[i];
         if(startpos == audiobuflen)
             startpos = 0;
     }
@@ -2297,7 +2312,8 @@ static void update_header(void)
         /* saved_header is saved right before stopping the MAS */
         framelen = create_xing_header(fd, 0, last_rec_bytes, xing_buffer,
                                       frames, last_rec_time * (1000/HZ),
-                                      saved_header, NULL, false);
+                                      saved_header, NULL, false,
+                                      mpeg_audiobuf, audiobuflen);
 
         lseek(fd, MPEG_RESERVED_HEADER_SPACE - framelen, SEEK_SET);
         write(fd, xing_buffer, framelen);
@@ -2645,8 +2661,22 @@ void audio_set_recording_options(struct audio_recording_options *options)
 #endif /* SIMULATOR */
 #endif /* CONFIG_CODEC == MAS3587F */
 
+static void audio_reset_buffer(void)
+{
+    size_t bufsize; /* dont break strict-aliasing */
+    talk_buffer_steal(); /* will use the mp3 buffer */
+
+    /* release buffer on behalf of any audio_get_buffer() caller,
+     * non-fatal if there was none */
+    buffer_release_buffer(0);
+    /* re-aquire */
+    mpeg_audiobuf = buffer_get_buffer(&bufsize);
+    audiobuflen = bufsize;
+}
+
 void audio_play(long offset)
 {
+    audio_reset_buffer();
 #ifdef SIMULATOR
     char name_buf[MAX_PATH+1];
     const char* trackname;
@@ -2676,7 +2706,6 @@ void audio_play(long offset)
     } while(1);
 #else /* !SIMULATOR */
     is_playing = true;
-
     queue_post(&mpeg_queue, MPEG_PLAY, offset);
 #endif /* !SIMULATOR */
 
@@ -2700,12 +2729,22 @@ void audio_stop(void)
     is_playing = false;
     playing = false;
 #endif /* SIMULATOR */
+    /* give voice our entire buffer */
+    talkbuf_init(mpeg_audiobuf);
 }
 
 /* dummy */
 void audio_stop_recording(void)
 {
     audio_stop(); 
+}
+
+void audio_hard_stop(void)
+{
+    audio_stop();
+    /* tell voice we obtain the buffer before freeing */
+    talk_buffer_steal();
+    buffer_release_buffer(0);
 }
 
 void audio_pause(void)
@@ -2864,8 +2903,12 @@ void audio_init(void)
     if (global_settings.cuesheet)
         curr_cuesheet = (struct cuesheet*)buffer_alloc(sizeof(struct cuesheet));
 
+    size_t bufsize; /* don't break strict-aliasing */
+    mpeg_audiobuf = buffer_get_buffer(&bufsize);
+    audiobuflen = bufsize;
+    /* give voice buffer until we start to play */
+    talkbuf_init(mpeg_audiobuf);
 #ifndef SIMULATOR
-    audiobuflen = audiobufend - audiobuf;
     queue_init(&mpeg_queue, true);
 #endif /* !SIMULATOR */
     create_thread(mpeg_thread, mpeg_stack,

commit 4087875f1cf2677cc8542d7dd19cc3b4a8d26c2b

Dircache: Fix bug introduced in r30308.
dircache_root wasn't initialized at all and the giving allocated_size
passed to buffer_release_buffer() didn't account for alignment padding.

git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30318 a1c6a512-1295-4272-9138-f99709370657

firmware/common/dircache.c
@@ -864,8 +864,9 @@ int dircache_build(int last_size)
      * free bytes inbetween */
     size_t got_size;
     char* buf = buffer_get_buffer(&got_size);
-    ALIGN_BUFFER(buf, got_size, sizeof(struct dircache_entry));
-    d_names_start = d_names_end = (char*)dircache_root + got_size - 1;
+    dircache_root = (struct dircache_entry*)ALIGN_UP(buf,
+                                                sizeof(struct dircache_entry));
+    d_names_start = d_names_end = buf + got_size - 1;
     dircache_size = 0;
     generate_dot_d_names();
 
@@ -897,8 +898,8 @@ int dircache_build(int last_size)
     dot -= offset;
     dotdot -= offset;
     
-    /* equivalent to dircache_size + DIRCACHE_RESERVE */
-    allocated_size = (d_names_end - (char*)dircache_root);
+    /* equivalent to dircache_size + DIRCACHE_RESERVE + align */
+    allocated_size = (d_names_end - buf);
     reserve_used = 0;
 
     buffer_release_buffer(allocated_size);