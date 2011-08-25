bc6dd127e32f61599f5becb264095f70757ae216
Thomas Jarosch <tomj@simonv.com>
2011-08-25 19:34:15 +0000

    Fix use of uninitialized memory in xlcd_scroll_left() / xlcd_scroll_right() in special cases
    Only valid for:
    LCD_PIXELFORMAT == HORIZONTAL_PACKING && LCD_DEPTH != 2
    
    Found by "cppcheck".
    
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30348 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/lib/xlcd_scroll.c
+++ apps/plugins/lib/xlcd_scroll.c
@@ -138,15 +138,15 @@ void xlcd_scroll_down(int count)
 /* Scroll left */
 void xlcd_scroll_left(int count)
 {
-    int bitcount, oldmode;
-    int blockcount, blocklen;
+    int bitcount=0, oldmode;
+    int blockcount=0, blocklen;
 
     if ((unsigned) count >= LCD_WIDTH)
     {
         rb->lcd_clear_display();
         return;
     }
-        
+
 #if LCD_DEPTH == 2
     blockcount = count >> 2;
     blocklen = LCD_FBWIDTH - blockcount;
@@ -196,15 +196,15 @@ void xlcd_scroll_left(int count)
 /* Scroll right */
 void xlcd_scroll_right(int count)
 {
-    int bitcount, oldmode;
-    int blockcount, blocklen;
+    int bitcount=0, oldmode;
+    int blockcount=0, blocklen;
 
     if ((unsigned) count >= LCD_WIDTH)
     {
         rb->lcd_clear_display();
         return;
     }
-        
+
 #if LCD_DEPTH == 2
     blockcount = count >> 2;
     blocklen = LCD_FBWIDTH - blockcount;
