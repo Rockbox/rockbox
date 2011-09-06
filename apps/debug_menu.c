4c896d9fe2f951da712dc1b3a5f6b5dbdf113f35
Marcin Bukat <marcin.bukat@gmail.com>
2011-09-06 12:39:41 +0000

    rk27xx - enable debug screen
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30458 a1c6a512-1295-4272-9138-f99709370657

--- apps/debug_menu.c
+++ apps/debug_menu.c
@@ -2021,7 +2021,8 @@ static const struct the_menu_item menuitems[] = {
 #endif
 #if CONFIG_CPU == SH7034 || defined(CPU_COLDFIRE) || defined(CPU_PP) \
     || CONFIG_CPU == S3C2440 || CONFIG_CPU == IMX31L || CONFIG_CPU == AS3525 \
-    || CONFIG_CPU == DM320 || defined(CPU_S5L870X) || CONFIG_CPU == AS3525v2
+    || CONFIG_CPU == DM320 || defined(CPU_S5L870X) || CONFIG_CPU == AS3525v2 \
+    || CONFIG_CPU == RK27XX
         { "View I/O ports", dbg_ports },
 #endif
 #if (CONFIG_RTC == RTC_PCF50605) && (CONFIG_PLATFORM & PLATFORM_NATIVE)
