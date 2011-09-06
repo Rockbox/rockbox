1371dfbc86036e3589be207eb2279119f5f2f7de
Marcin Bukat <marcin.bukat@gmail.com>
2011-09-06 12:38:13 +0000

    rk27xx - nand stub
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30442 a1c6a512-1295-4272-9138-f99709370657

---
+++ firmware/target/arm/rk27xx/nand-rk27xx.c
@@ -0,0 +1,99 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2010 by Bertrik Sikken
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+ 
+ #include "config.h"
+ #include "nand-target.h"
+
+const struct nand_device_info_type* nand_get_device_type(uint32_t bank);
+
+
+uint32_t nand_read_page(uint32_t bank, uint32_t page, void* databuffer,
+                        void* sparebuffer, uint32_t doecc,
+                        uint32_t checkempty)
+{
+    /* TODO implement */
+    (void)bank;
+    (void)page;
+    (void)databuffer;
+    (void)sparebuffer;
+    (void)doecc;
+    (void)checkempty;
+    return 0;
+}
+
+uint32_t nand_write_page(uint32_t bank, uint32_t page, void* databuffer,
+                         void* sparebuffer, uint32_t doecc)
+{
+    /* TODO implement */
+    (void)bank;
+    (void)page;
+    (void)databuffer;
+    (void)sparebuffer;
+    (void)doecc;
+    return 0;
+}
+
+uint32_t nand_block_erase(uint32_t bank, uint32_t page)
+{
+    /* TODO implement */
+    (void)bank;
+    (void)page;
+    return 0;
+}
+
+uint32_t nand_reset(uint32_t bank)
+{
+    /* TODO implement */
+    (void)bank;
+    return 0;
+}
+
+uint32_t nand_device_init(void)
+{
+    /* TODO implement */
+    return 0;
+}
+
+void nand_power_up(void)
+{
+    /* TODO implement */
+}
+
+void nand_power_down(void)
+{
+    /* TODO implement */
+}
+
+void nand_set_active(void)
+{
+    /* TODO implement */
+}
+
+long nand_last_activity(void)
+{
+    /* TODO implement */
+    return 0;
+}
+
+int nand_spinup_time(void)
+{
+    return 0;
+}
