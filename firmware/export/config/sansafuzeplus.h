3d46b080fda6f271da0949f1338667c7805b145e
Amaury Pouly <pamaury@rockbox.org>
2011-09-13 23:38:42 +0000

    imx233/fuze+: use random size for plugin and codec buf (must be non-zero for init attr)
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30522 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/config/sansafuzeplus.h
+++ firmware/export/config/sansafuzeplus.h
@@ -97,10 +97,10 @@
 #define HAVE_SW_POWEROFF
 
 /* The number of bytes reserved for loadable codecs */
-#define CODEC_SIZE 0
+#define CODEC_SIZE 0x100000
 
 /* The number of bytes reserved for loadable plugins */
-#define PLUGIN_BUFFER_SIZE 0
+#define PLUGIN_BUFFER_SIZE 0x80000
 
 #define AB_REPEAT_ENABLE
 
