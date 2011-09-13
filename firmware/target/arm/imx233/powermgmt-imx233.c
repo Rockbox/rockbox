45537a4e87c8696629cbda8efcd721b74f084ff1
Amaury Pouly <pamaury@rockbox.org>
2011-09-13 23:40:06 +0000

    imx233/fuze+: implement power management stubs
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30533 a1c6a512-1295-4272-9138-f99709370657

--- firmware/target/arm/imx233/powermgmt-imx233.c
+++ firmware/target/arm/imx233/powermgmt-imx233.c
@@ -21,6 +21,36 @@
 
 #include "powermgmt.h"
 
+const unsigned short battery_level_dangerous[BATTERY_TYPES_COUNT] =
+{
+    3659
+};
+
+const unsigned short battery_level_shutoff[BATTERY_TYPES_COUNT] =
+{
+    3630
+};
+
+/* voltages (millivolt) of 0%, 10%, ... 100% when charging disabled */
+const unsigned short percent_to_volt_discharge[BATTERY_TYPES_COUNT][11] =
+{
+    /* Toshiba Gigabeat S Li Ion 700mAH figured from discharge curve */
+    { 3659, 3719, 3745, 3761, 3785, 3813, 3856, 3926, 3984, 4040, 4121 },
+};
+
+/* voltages (millivolt) of 0%, 10%, ... 100% when charging enabled */
+const unsigned short percent_to_volt_charge[11] =
+{
+    /* Toshiba Gigabeat S Li Ion 700mAH figured from charge curve */
+    4028, 4063, 4087, 4111, 4135, 4156, 4173, 4185, 4194, 4202, 4208
+};
+
+/* Returns battery voltage from ADC [millivolts] */
+unsigned int battery_adc_voltage(void)
+{
+    return 4100;
+}
+
 void powermgmt_init_target(void)
 {
 }
