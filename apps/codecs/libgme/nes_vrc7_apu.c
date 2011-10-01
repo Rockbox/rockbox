da09f7f0f14f2b6514a1e5dd0e223ff8e9f766d0
Andree Buschmann <AndreeBuschmann@t-online.de>
2011-10-01 08:16:33 +0000

    Fix detuned VRC7. In VRC7 apu the 2413 emu must run at the default clock rate. Thanks to Mauricio Garrido.
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30623 a1c6a512-1295-4272-9138-f99709370657

--- apps/codecs/libgme/nes_vrc7_apu.c
+++ apps/codecs/libgme/nes_vrc7_apu.c
@@ -32,7 +32,6 @@ void Vrc7_reset( struct Nes_Vrc7_Apu* this )
 void Vrc7_set_rate( struct Nes_Vrc7_Apu* this, int r )
 {
 	OPLL_set_quality( &this->opll, r < 44100 ? 0 : 1 );
-    OPLL_set_rate( &this->opll, (e_uint32)r );
 }
 
 void Vrc7_write_reg( struct Nes_Vrc7_Apu* this, int data )
