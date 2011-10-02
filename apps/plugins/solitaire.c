cbe1ca1948f7a1b3aafb7b9ddf45d9df20eb0032
Amaury Pouly <pamaury@rockbox.org>
2011-10-02 16:21:08 +0000

    fuze+: add solitaire keymap (unsure about it)
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30671 a1c6a512-1295-4272-9138-f99709370657

--- apps/plugins/solitaire.c
+++ apps/plugins/solitaire.c
@@ -507,6 +507,25 @@ CONFIG_KEYPAD == MROBE500_PAD
 #   define HK_CUR2STACK    "PLAY"
 #   define HK_REM2STACK    "PLAY...."
 
+#elif CONFIG_KEYPAD == SANSA_FUZEPLUS_PAD
+#   define SOL_QUIT          BUTTON_POWER
+#   define SOL_UP            BUTTON_UP
+#   define SOL_DOWN          BUTTON_DOWN
+#   define SOL_LEFT          BUTTON_LEFT
+#   define SOL_RIGHT         BUTTON_RIGHT
+#   define SOL_MOVE          (BUTTON_SELECT|BUTTON_REL)
+#   define SOL_DRAW          (BUTTON_PLAYPAUSE|BUTTON_REL)
+#   define SOL_REM2CUR       BUTTON_LEFT
+#   define SOL_CUR2STACK_PRE (BUTTON_RIGHT | BUTTON_REPEAT)
+#   define SOL_CUR2STACK     BUTTON_RIGHT
+#   define SOL_REM2STACK     (BUTTON_LEFT|BUTTON_REPEAT)
+#   define SOL_REM2STACK_PRE BUTTON_LEFT
+#   define HK_MOVE           "SHORT SELECT"
+#   define HK_DRAW           "SHORT PLAY"
+#   define HK_REM2CUR        "LEFT"
+#   define HK_CUR2STACK      "DOUBLE SELECT"
+#   define HK_REM2STACK      "LEFT"
+
 #else
 #error No keymap defined!
 #endif
