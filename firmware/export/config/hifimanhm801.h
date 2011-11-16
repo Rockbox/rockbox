b6252abb2518cb97dda7c1fc197bc80a57f931c3
Andrew Ryabinin <ryabinin.a.a@gmail.com>
2011-11-16 16:07:06 +0000

    fix target id  for HiFiMAN HM-801
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@31001 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/config/hifimanhm801.h
+++ firmware/export/config/hifimanhm801.h
@@ -4,7 +4,7 @@
 #define TARGET_TREE /* this target is using the target tree system */
 
 /* For Rolo and boot loader */
-#define MODEL_NUMBER 80
+#define MODEL_NUMBER 82
 
 #define MODEL_NAME   "HiFiMAN HM-801"
 
