1d81683e0896e8b82d133fb0e577b6c79263ff39
Thomas Jarosch <tomj@simonv.com>
2011-12-03 17:11:54 +0000

    Fix red: Mark variable as unused the rockbox way
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@31125 a1c6a512-1295-4272-9138-f99709370657

--- apps/mp3data.c
+++ apps/mp3data.c
@@ -379,8 +379,10 @@ static size_t mem_pos;
 static int mem_cnt;
 static int mem_maxlen;
 
-static int mem_getbyte(int /*dummy*/, unsigned char *c)
+static int mem_getbyte(int dummy, unsigned char *c)
 {
+    (void)dummy;
+
     *c = mem_buf[mem_pos++];
     if(mem_pos >= mem_buflen)
         mem_pos = 0;
