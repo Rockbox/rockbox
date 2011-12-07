31136370502e3dac4b7b2f0ebaf1ff707d931a79
Frank Gevaerts <frank@gevaerts.be>
2011-12-07 17:58:28 +0000

    Pass VERSION to *all* make invocations in release scripts
    git-svn-id: svn://svn.rockbox.org/rockbox/branches/v3_10@31165 a1c6a512-1295-4272-9138-f99709370657

--- tools/release/manuals.pl
+++ tools/release/manuals.pl
@@ -65,10 +65,10 @@ sub buildit {
     `$c`;
 
     print "Run 'make'\n" if($verbose);
-    `make manual 2>/dev/null`;
+    `make manual VERSION=$version 2>/dev/null`;
 
     print "Run 'make manual-zip'\n" if($verbose);
-    `make manual-zip 2>/dev/null`;
+    `make manual-zip VERSION=$version 2>/dev/null`;
 }
 
 # run make in tools first to make sure they're up-to-date
