c9a8031695de392e229fdb389ca4a99421e02bdd
Bertrik Sikken <bertrik@sikken.nl>
2011-12-10 09:45:28 +0000

    mkamsboot: add support for sansa clip zip firmware version v1.01.18
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@31195 a1c6a512-1295-4272-9138-f99709370657

--- rbutil/mkamsboot/mkamsboot.c
+++ rbutil/mkamsboot/mkamsboot.c
@@ -175,6 +175,7 @@ static struct md5sums sansasums[] = {
     { MODEL_CLIPZIP, "1.01.12", "45adea0873326b5af34f096e5c402f78" },
     { MODEL_CLIPZIP, "1.01.15", "f62af954334cd9ba1a87a7fa58ec6074" },
     { MODEL_CLIPZIP, "1.01.17", "27bcb343d6950f35dc261629e22ba60c" },
+    { MODEL_CLIPZIP, "1.01.18", "ef16aa9e02b49885ebede5aa149502e8" },
 };
 
 #define NUM_MD5S (sizeof(sansasums)/sizeof(sansasums[0]))
