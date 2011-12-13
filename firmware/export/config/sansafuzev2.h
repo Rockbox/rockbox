3183010dfaf8a4bae3116484258b093334b1d88b
Rafaël Carré <rafael.carre@gmail.com>
2011-12-13 06:30:44 +0000

    AMSv2: enable USB
    This will help debugging, and can be disabled for releases if needed
    
    git-svn-id: svn://svn.rockbox.org/rockbox/trunk@31226 a1c6a512-1295-4272-9138-f99709370657

--- firmware/export/config/sansafuzev2.h
+++ firmware/export/config/sansafuzev2.h
@@ -203,8 +203,7 @@
 
 /* enable these for the experimental usb stack */
 #define HAVE_USBSTACK
-#define USB_HANDLED_BY_OF
-//#define USE_ROCKBOX_USB
+#define USE_ROCKBOX_USB
 #define USB_VENDOR_ID 0x0781
 #define USB_PRODUCT_ID 0x74c3   /* MSC = 0x74c3, MTP = 0x74c2 */
 
