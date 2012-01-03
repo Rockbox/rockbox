Rockbox/rockbox

commit 6a67707b5ec3b2c649c401550bb7fdef2b7c8d07

Commit to certain names for cache coherency APIs and discard the aliases.
Wouldn't surprise me a bit to get some non-green.


git-svn-id: svn://svn.rockbox.org/rockbox/trunk@31339 a1c6a512-1295-4272-9138-f99709370657

firmware/target/arm/rk27xx/system-rk27xx.c
@@ -163,7 +163,7 @@ void udelay(unsigned usecs)
     );
 }
 
-void cpucache_commit_discard(void)
+void commit_discard_idcache(void)
 {
     /* invalidate cache way 0 */
     CACHEOP = 0x02;
@@ -177,8 +177,6 @@ void cpucache_commit_discard(void)
     /* wait for invalidate process to complete */
     while (CACHEOP & 0x03);   
 }
-
-void cpucache_invalidate(void) __attribute__((alias("cpucache_commit_discard")));
 
 void commit_discard_dcache_range (const void *base, unsigned int size)
 {
@@ -196,4 +194,5 @@ void commit_discard_dcache_range (const void *base, unsigned int size)
     }
 }
 
-void clean_dcache_range(const void *base, unsigned int size) __attribute__((alias("commit_discard_dcache_range")));
+void commit_dcache_range(const void *base, unsigned int size)
+    __attribute__((alias("commit_discard_dcache_range")));

firmware/target/arm/system-target.h
@@ -141,6 +141,10 @@ static inline void wake_core(int core)
 }
 #endif
 
+void commit_dcache(void);
+void commit_discard_dcache(void);
+void commit_discard_idcache(void);
+
 #if defined(BOOTLOADER) && !defined(HAVE_BOOTLOADER_USB_MODE)
 /* All addresses within rockbox are in IRAM in the bootloader so
    are therefore uncached */
@@ -160,15 +164,6 @@ static inline void wake_core(int core)
 
 #if defined(CPU_PP502x) && defined(HAVE_ATA_DMA)
 #define STORAGE_WANTS_ALIGN
-#endif
-
-/** cache functions **/
-#if !defined(BOOTLOADER) || defined(HAVE_BOOTLOADER_USB_MODE)
-#define HAVE_CPUCACHE_COMMIT_DISCARD
-#define HAVE_CPUCACHE_COMMIT
-/* deprecated alias */
-#define HAVE_CPUCACHE_INVALIDATE
-#define HAVE_CPUCACHE_FLUSH
 #endif
 
 #if defined(IPOD_VIDEO) && !defined(BOOTLOADER)

firmware/target/coldfire/system-target.h
@@ -194,10 +194,6 @@ static inline uint32_t swap_odd_even32_hw(uint32_t value)
     return value;
 }
 
-#define HAVE_CPUCACHE_COMMIT_DISCARD
-/* deprecated alias */
-#define HAVE_CPUCACHE_INVALIDATE
-
 #define DEFAULT_PLLCR_AUDIO_BITS 0x10400000
 void coldfire_set_pllcr_audio_bits(long bits);
 
@@ -222,5 +218,9 @@ extern void cf_set_cpu_frequency(long frequency);
 /* 124.1856 MHz */
 #define CPUFREQ_MAX_MULT     11
 #define CPUFREQ_MAX          (CPUFREQ_MAX_MULT * CPU_FREQ)
+
+void commit_discard_idcache(void);
+static inline void commit_discard_dcache(void) {}
+static inline void commit_dcache(void) {}
 
 #endif /* SYSTEM_TARGET_H */

firmware/target/hosted/android/system-target.h
@@ -26,6 +26,10 @@
 #define disable_irq_save() 0
 #define restore_irq(level) (void)level
 
+static inline void commit_dcache(void) {}
+static inline void commit_discard_dcache(void) {}
+static inline void commit_discard_idcache(void) {}
+
 void power_off(void);
 void wait_for_interrupt(void);
 void interrupt(void);

firmware/target/sh/system-target.h
@@ -133,4 +133,8 @@ extern const unsigned bit_n_table[32];
     : bit_n_table[n] \
 )
 
+static inline void commit_dcache(void) {}
+static inline void commit_discard_dcache(void) {}
+static inline void commit_discard_idcache(void) {}
+
 #endif /* SYSTEM_TARGET_H */

apps/codec_thread.c
@@ -550,9 +550,9 @@ static void do_callback(void (* callback)(void))
 
     if (callback)
     {
-        cpucache_commit_discard();
+        commit_discard_idcache();
         callback();
-        cpucache_commit();
+        commit_dcache();
     }
 }
 

apps/codecs.c
@@ -116,8 +116,8 @@ struct codec_api ci = {
     semaphore_release,
 #endif
 
-    cpucache_flush,
-    cpucache_invalidate,
+    commit_dcache,
+    commit_discard_dcache,
 
     /* strings and memory */
     strcpy,
@@ -165,6 +165,8 @@ struct codec_api ci = {
 
     /* new stuff at the end, sort into place next time
        the API gets incompatible */
+
+    commit_discard_idcache,
 };
 
 void codec_get_full_path(char *path, const char *codec_root_fn)

apps/codecs.h
@@ -75,7 +75,7 @@
 #define CODEC_ENC_MAGIC 0x52454E43 /* RENC */
 
 /* increase this every time the api struct changes */
-#define CODEC_API_VERSION 43
+#define CODEC_API_VERSION 44
 
 /* update this to latest version if a change to the api struct breaks
    backwards compatibility (and please take the opportunity to sort in any
@@ -169,8 +169,8 @@ struct codec_api {
     void (*semaphore_release)(struct semaphore *s);
 #endif /* NUM_CORES */
 
-    void (*cpucache_flush)(void);
-    void (*cpucache_invalidate)(void);
+    void (*commit_dcache)(void);
+    void (*commit_discard_dcache)(void);
 
     /* strings and memory */
     char* (*strcpy)(char *dst, const char *src);
@@ -223,6 +223,7 @@ struct codec_api {
 
     /* new stuff at the end, sort into place next time
        the API gets incompatible */
+    void (*commit_discard_idcache)(void);
 };
 
 /* codec header */

commit 40786042df12b2297161ca986b431d64a5df56db

rk27xx: Fix cache broken in r31339. Remove deprecated aliases.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@31537 a1c6a512-1295-4272-9138-f99709370657

firmware/target/arm/rk27xx/system-rk27xx.c
@@ -177,6 +177,7 @@ void commit_discard_idcache(void)
     /* wait for invalidate process to complete */
     while (CACHEOP & 0x03);   
 }
+void commit_discard_dcache (void) __attribute__((alias("commit_discard_idcache")));
 
 void commit_discard_dcache_range (const void *base, unsigned int size)
 {
@@ -193,6 +194,3 @@ void commit_discard_dcache_range (const void *base, unsigned int size)
         opcode += 32;
     }
 }
-
-void commit_dcache_range(const void *base, unsigned int size)
-    __attribute__((alias("commit_discard_dcache_range")));

firmware/target/arm/rk27xx/system-target.h
@@ -40,15 +40,13 @@ static inline void core_sleep(void)
     /* SCU_CPUPD = 0xdeedbabe; */
 }
 
-#define HAVE_CPUCACHE_COMMIT_DISCARD
-/* deprecated alias */
-#define HAVE_CPUCACHE_INVALIDATE
-
 /* Write DCache back to RAM for the given range and remove cache lines
  * from DCache afterwards */
 void commit_discard_dcache_range(const void *base, unsigned int size);
-/* deprecated alias */
-void invalidate_dcache_range(const void *base, unsigned int size);
+
+static inline void commit_dcache(void) {}
+void commit_discard_dcache(void);
+void commit_discard_idcache(void);
 
 #define CPUFREQ_NORMAL 200000000
 #define CPUFREQ_MAX    200000000