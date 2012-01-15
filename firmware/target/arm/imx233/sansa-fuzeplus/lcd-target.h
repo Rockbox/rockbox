8cadb587e88797e7ff94a57464bac3a0bab3523a
Amaury Pouly <amaury.pouly@gmail.com>
2012-01-15 01:37:37 +0100

    fuzeplus: fix lcd-target.h (LCD_FRAMEBUF_ADDR must point to lcd_framebuffer and not FRAME)
    Change-Id: Ia1f16f9b8e3041517b60336c06aedd40dfd2be12

--- firmware/target/arm/imx233/sansa-fuzeplus/lcd-target.h
+++ firmware/target/arm/imx233/sansa-fuzeplus/lcd-target.h
@@ -21,7 +21,7 @@
 #ifndef LCD_TARGET_H
 #define LCD_TARGET_H
 
-#define LCD_FRAMEBUF_ADDR(col, row) ((fb_data *)FRAME + (row)*LCD_WIDTH + (col))
+#define LCD_FRAMEBUF_ADDR(col, row) ((fb_data *)lcd_framebuffer + (row)*LCD_WIDTH + (col))
 
 /* Not really optimized, but are unusual */
 #define LCD_OPTIMIZED_UPDATE
