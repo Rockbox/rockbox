7f26a10d31911c3468312563cd2667a96278944b
Jean-Louis Biasini <metaphysiciendouteux@jabber.org>
2012-01-15 02:28:10 +0100

    fuzeplus: update plugins keymaps (FS#12405) Signed-off-by: Amaury Pouly <amaury.pouly@gmail.com>
    Change-Id: Ib94cec07c80892eb50471c87b83e2701911ea0d7

--- apps/plugins/lib/pluginlib_actions.c
+++ apps/plugins/lib/pluginlib_actions.c
@@ -403,7 +403,7 @@ const struct button_mapping pla_main_ctx[] =
     {PLA_SELECT_REPEAT,     BUTTON_PLAY|BUTTON_REPEAT,          BUTTON_NONE},
 #elif (CONFIG_KEYPAD == SANSA_FUZEPLUS_PAD)                                      
     {PLA_CANCEL,            BUTTON_BACK,                        BUTTON_NONE},
-    {PLA_EXIT,              BUTTON_POWER,                       BUTTON_NONE},
+    {PLA_EXIT,              BUTTON_BACK|BUTTON_REPEAT,          BUTTON_NONE},
     {PLA_SELECT,            BUTTON_SELECT,                      BUTTON_NONE},
     {PLA_SELECT_REL,        BUTTON_SELECT|BUTTON_REL,           BUTTON_SELECT},
     {PLA_SELECT_REPEAT,     BUTTON_SELECT|BUTTON_REPEAT,        BUTTON_NONE},
