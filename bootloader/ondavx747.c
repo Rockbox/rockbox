cc1ab6260f3b829a698d22268ff0b9bd2cdf2467
Marcin Bukat <marcin.bukat@gmail.com>
2012-03-04 15:34:29 +0100

    Move load_firmware() to separate file to share it between bootloaders and rolo().
    Change-Id: I1656ed91946d7a05cb7c9fa7a16793c3c862a5cd

--- bootloader/ondavx747.c
+++ bootloader/ondavx747.c
@@ -29,6 +29,8 @@
 #include "system.h"
 #include "button.h"
 #include "common.h"
+#include "rb-loader.h"
+#include "loader_strerror.h"
 #include "storage.h"
 #include "disk.h"
 #include "string.h"
@@ -148,7 +150,7 @@ static int boot_rockbox(void)
 
     printf("Loading firmware...");
     rc = load_firmware((unsigned char *)CONFIG_SDRAM_START, BOOTFILE, 0x400000);
-    if(rc < 0)
+    if(rc < EOK)
         return rc;
     else
     {
@@ -304,7 +306,7 @@ int main(void)
         rc = boot_rockbox();
 
     if(rc < 0)
-        printf("Error: %s", strerror(rc));
+        printf("Error: %s", loader_strerror(rc));
 
     /* Halt */
     while (1)
