2aae74545855e133d6f0e6ce56bbb8995fd1b51e
Dominik Riebeling <Dominik.Riebeling@gmail.com>
2012-04-12 21:14:11 +0200

    Add new tarball generation script.
    The old script depended on svn. An earlier version of this script has already
    been used in the 3.11. branch.
    
    Change-Id: Id03abb8f7bd005ede343243194c4453f0b2e8943

---
+++ utils/common/tarball.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import gitscraper
+import os
+import sys
+
+if len(sys.argv) < 2:
+    print "Usage: %s <version|hash>" % sys.argv[0]
+    sys.exit()
+
+repository = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "/../..")
+if '.' in sys.argv[1]:
+    version = sys.argv[1]
+    basename = "rockbox-" + version
+    ref = "refs/tags/v" + version + "-final"
+    refs = gitscraper.get_refs(repository)
+    if ref in refs:
+        tree = refs[ref]
+    else:
+        print "Could not find hash for version!"
+        sys.exit()
+else:
+    tree = sys.argv[1]
+    basename = "rockbox-" + tree
+
+gitscraper.archive_files(repository, tree, [], basename, archive="7z")
+
+print "done."
+
+
