a32cbf33465367cd6fe36d636da8d03485e9d15d
Dominik Riebeling <Dominik.Riebeling@gmail.com>
2012-04-30 22:06:59 +0200

    Only update mountpoint on Accept when empty.
    Always updating the mountpoint overwrites a manually entered mountpoint (only
    available in debug builds).
    
    Change-Id: I6298cb873496d66f4836f219e89ca8ae17261de9

--- rbutil/rbutilqt/configure.cpp
+++ rbutil/rbutilqt/configure.cpp
@@ -152,7 +152,9 @@ void Config::accept()
     RbSettings::setValue(RbSettings::Language, language);
 
     // make sure mountpoint is read from dropdown box
-    updateMountpoint(ui.mountPoint->currentIndex());
+    if(mountpoint.isEmpty()) {
+        updateMountpoint(ui.mountPoint->currentIndex());
+    }
 
     // mountpoint
     if(mountpoint.isEmpty()) {
