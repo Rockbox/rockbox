Rockbox/rockbox

commit 6b8330d2166dc188ec464693bf9db03cf39e8567

Enable USING_STORAGE_CALLBACK on hosted targets. Fixes config.cfg not written properly.
Change-Id: I3c87b41baae284b818be5c272d984d937ae0e350

firmware/export/ata_idle_notify.h
@@ -43,10 +43,10 @@ enum {
     DISK_EVENT_SPINUP = (EVENT_CLASS_DISK|1),
 };
 
-#define USING_STORAGE_CALLBACK  (CONFIG_PLATFORM & PLATFORM_NATIVE) \
-                            && ! ((CONFIG_STORAGE & STORAGE_NAND) \
-                               && (CONFIG_NAND == NAND_IFP7XX)) \
-                            && !defined(BOOTLOADER)
+#define USING_STORAGE_CALLBACK  ((CONFIG_STORAGE & STORAGE_NAND) \
+                                && (CONFIG_NAND == NAND_IFP7XX)) \
+                                && !defined(BOOTLOADER) || \
+                                (CONFIG_PLATFORM & PLATFORM_HOSTED)
 
 extern void register_storage_idle_func(void (*function)(void *data));
 #if USING_STORAGE_CALLBACK

commit 3f72ba0e3f34ac47a83a426e6a19b96045b842de

Fix broken logic for enabling USING_STORAGE_CALLBACK.
The logic got broken by 6b8330d2.

Since the ifp700 port is basically unmaintained and unfinished and doesn't
build anyway, we can remove the special-casing for it. This simplifies the
logic to "hosted or native, except for bootloaders", which simplifies
further to "not bootloaders".

Fixes FS#12685

Change-Id: Iaaad534067da22b74bf647aa58dcbb678bbc5f70

firmware/export/ata_idle_notify.h
@@ -43,10 +43,10 @@ enum {
     DISK_EVENT_SPINUP = (EVENT_CLASS_DISK|1),
 };
 
-#define USING_STORAGE_CALLBACK  ((CONFIG_STORAGE & STORAGE_NAND) \
-                                && (CONFIG_NAND == NAND_IFP7XX)) \
-                                && !defined(BOOTLOADER) || \
-                                (CONFIG_PLATFORM & PLATFORM_HOSTED)
+/* Enable storage callbacks everywhere except for bootloaders. Both
+ * hosted and native targets need this.
+ */
+#define USING_STORAGE_CALLBACK  !defined(BOOTLOADER)
 
 extern void register_storage_idle_func(void (*function)(void *data));
 #if USING_STORAGE_CALLBACK