Rockbox/rockbox

commit 91b52a1ade10272da5af7020e6f28ce78ef22072

Fix another uninitialized var warning.
Change-Id: I27d06003d5e9218a940057af1b5c5c1557e7653c

uisimulator/common/lcd-playersim.c
@@ -41,15 +41,15 @@ static int double_height = 1;
 
 void lcd_print_icon(int x, int icon_line, bool enable, char **icon)
 {
-    int row, col;
+    int row = 0, col = 0; /* shut up gcc */
     int y = (ICON_HEIGHT+(CHAR_HEIGHT*2+2)*CHAR_PIXEL) * icon_line;
 
     y += BORDER_MARGIN;
     x += BORDER_MARGIN;
 
-    for (row = 0; icon[row]; row++)
+    for (; icon[row]; row++)
     {
-        for (col = 0; icon[row][col]; col++)
+        for (; icon[row][col]; col++)
         {
             switch (icon[row][col])
             {

commit 7038c558932ccbedb2a2296751acf9a70f792f1c

Fix a bug introduced in 91b52a1, broken icons in the Archos Player simulator.
Change-Id: I2c4ef0828a9e44084f35b82ea71e430d0c14ecfb

uisimulator/common/lcd-playersim.c
@@ -49,7 +49,7 @@ void lcd_print_icon(int x, int icon_line, bool enable, char **icon)
 
     for (; icon[row]; row++)
     {
-        for (; icon[row][col]; col++)
+        for (col = 0; icon[row][col]; col++)
         {
             switch (icon[row][col])
             {