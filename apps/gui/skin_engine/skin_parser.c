3f42a08fbe0893c6c1c3e04a495dc0dc9f07757c
Jonathan Gordon <rockbox@jdgordon.info>
2012-06-24 21:52:18 +1000

    skin_engine: Automatically create touch regions for skin bars
    skin bars now automatically create the touch region the same size
    as the bar on touchscreen targets. This means touches will
    magically "just work" for reveresed bars (rtl or otherwise).
    
    Kudos to my new wife for figuring out the bleedingly obvious way to
    do this!
    
    Change-Id: I997a7bcaa70fce9885808aae27953c7676e9c2ff

--- apps/gui/skin_engine/skin_parser.c
+++ apps/gui/skin_engine/skin_parser.c
@@ -1055,6 +1055,46 @@ static int parse_progressbar_tag(struct skin_element* element,
         token->type = SKIN_TOKEN_LIST_SCROLLBAR;
     pb->type = token->type;
 
+#ifdef HAVE_TOUCHSCREEN
+    if (token->type == SKIN_TOKEN_VOLUMEBAR ||
+        token->type == SKIN_TOKEN_PROGRESSBAR)
+    {
+        struct touchregion *region = skin_buffer_alloc(sizeof(*region));
+
+        if (!region)
+            return 0;
+
+        if (token->type == SKIN_TOKEN_VOLUMEBAR)
+            region->action = ACTION_TOUCH_VOLUME;
+        else
+            region->action = ACTION_TOUCH_SCROLLBAR;
+
+		/* try to add some extra space on either end to make pressing the
+		 * full bar easier. ~5% on either side
+		 */
+        region->x = pb->x - pb->width*5/100;
+        if (region->x < 0)
+			region->x = 0;
+        region->y = pb->y;
+        region->width = pb->width;
+        if (region->width + pb->width*5/100 < curr_vp->vp.x + curr_vp->vp.width)
+			region->width += pb->width*5/100;
+        region->height = pb->height;
+        region->wvp = PTRTOSKINOFFSET(skin_buffer, curr_vp);
+        region->reverse_bar = false;
+        region->allow_while_locked = false;
+        region->press_length = PRESS;
+        region->last_press = 0xffff;
+        region->armed = false;
+        region->bar = PTRTOSKINOFFSET(skin_buffer, pb);
+
+        struct skin_token_list *item = new_skin_token_list_item(NULL, region);
+        if (!item)
+            return WPS_ERROR_INVALID_PARAM;
+        add_to_ll_chain(&wps_data->touchregions, item);
+    }
+#endif
+
     return 0;
 
 #else
@@ -1429,6 +1469,7 @@ static int parse_touchregion(struct skin_element *element,
     region->last_press = 0xffff;
     region->press_length = PRESS;
     region->allow_while_locked = false;
+    region->bar = -1;
     action = get_param_text(element, p++);
 
     /* figure out the action */
