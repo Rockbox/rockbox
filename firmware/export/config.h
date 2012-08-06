97c1dc0368dadf3f404bc4c5e1af1193eeb713b3
Michael Giacomelli <giac2000@hotmail.com>
2012-08-06 19:07:54 -0400

    Enable logdiskf to for all device targets, but not bootloaders or application builds.
    Change-Id: I651900fda676433841cfd14dba05bcb4116e0002

--- firmware/export/config.h
+++ firmware/export/config.h
@@ -1133,4 +1133,9 @@ Lyre prototype 1 */
 #define HAVE_PITCHCONTROL
 #endif
 
+/* enable logging messages to disk*/
+#if CONFIG_PLATFORM == PLATFORM_NATIVE  && !defined(SIMULATOR) && !defined(BOOTLOADER)
+#define ROCKBOX_HAS_LOGDISKF
+#endif
+
 #endif /* __CONFIG_H__ */
