bc6c08c4b9d9dd3dbe1b68c26eec891d2c96f1b1
Nils Wallménius <nils@rockbox.org>
2012-10-17 20:53:04 +0200

    opus: only put the mdct function in iram for cf, doesn't make any speed diff on pp
    Change-Id: Ia6484e2a2bbf72d33a583470918c0fa267fd9b97

--- lib/rbcodec/codecs/libopus/celt/mdct.h
+++ lib/rbcodec/codecs/libopus/celt/mdct.h
@@ -61,10 +61,15 @@ void clt_mdct_forward(const mdct_lookup *l, kiss_fft_scalar *in,
       kiss_fft_scalar * OPUS_RESTRICT out,
       const opus_val16 *window, int overlap, int shift, int stride);
 
+#if defined(CPU_COLDFIRE)
+#define MDCT_ICODE ICODE_ATTR
+#else
+#define MDCT_ICODE
+#endif
 /** Compute a backward MDCT (no scaling) and performs weighted overlap-add
     (scales implicitly by 1/2) */
 void clt_mdct_backward(const mdct_lookup *l, kiss_fft_scalar *in,
       kiss_fft_scalar * OPUS_RESTRICT out,
-      const opus_val16 * OPUS_RESTRICT window, int overlap, int shift, int stride) ICODE_ATTR;
+      const opus_val16 * OPUS_RESTRICT window, int overlap, int shift, int stride);
 
 #endif
