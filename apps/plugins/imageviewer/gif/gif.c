39131297d7bf812da721088e213ef1c049430942
Marcin Bukat <marcin.bukat@gmail.com>
2012-11-02 13:03:58 +0100

    imageviewer: gif viewer based on giflib-5.0.2
    This adds ability to view gif images in rockbox.
    Works both on color and gray/monochrome targets (greylib).
    Aspect correction is supported as well.
    
    Limitations:
    - Only first frame from animated gif is rendered. In order
      to fully support animated gifs imageviewer framework needs
      to be tweaked as it is not designed to decoder change
      the content on-the-fly.
    - Multiblock gifs with separate color maps for each block
      (so called true RGB gifs) are probably broken. This are rare
      though. Once I prepare sample file it should be fairly easy
      to fix.
    - Transparency support is very simple and may not work correctly
      in all cases.
    - Broken gifs are outright rejected. Some of thouse are rendered
      correctly by various image viewers so the checks should be
      relaxed probably.
    
    Change-Id: I61501f801ddcd403410e38d83e6bddc9883e7ede

---
+++ apps/plugins/imageviewer/gif/gif.c
@@ -0,0 +1,239 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (c) 2012 Marcin Bukat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include "plugin.h"
+#include "lcd.h"
+#include <lib/pluginlib_bmp.h>
+#include "../imageviewer.h"
+#include "bmp.h"
+#include "gif_decoder.h"
+#include "gif_lib.h"
+
+/* decoder context struct */
+static struct gif_decoder decoder;
+
+static char print[32]; /* use a common snprintf() buffer */
+
+/* decompressed image in the possible sizes (1,2,4,8), wasting the other */
+static unsigned char *disp[9];
+static unsigned char *disp_buf;
+
+#if defined(HAVE_LCD_COLOR)
+#define resize_bitmap   smooth_resize_bitmap
+#else
+#define resize_bitmap   grey_resize_bitmap
+#endif
+
+#if defined(USEGSLIB) && (CONFIG_PLATFORM & PLATFORM_HOSTED)
+/* hack: fix error "undefined reference to `_grey_info'". */
+GREY_INFO_STRUCT
+#endif /* USEGSLIB */
+
+static void draw_image_rect(struct image_info *info,
+                            int x, int y, int width, int height)
+{
+    unsigned char **pdisp = (unsigned char **)info->data;
+
+#ifdef HAVE_LCD_COLOR
+    rb->lcd_bitmap_part((fb_data *)*pdisp, info->x + x, info->y + y,
+                        STRIDE(SCREEN_MAIN, info->width, info->height), 
+                        x + MAX(0, (LCD_WIDTH-info->width)/2),
+                        y + MAX(0, (LCD_HEIGHT-info->height)/2),
+                        width, height);
+#else
+    mylcd_ub_gray_bitmap_part(*pdisp,
+                              info->x + x, info->y + y, info->width,
+                              x + MAX(0, (LCD_WIDTH-info->width)/2),
+                              y + MAX(0, (LCD_HEIGHT-info->height)/2),
+                              width, height);
+#endif
+}
+
+static int img_mem(int ds)
+{
+    struct gif_decoder *p_decoder = &decoder;
+
+#ifdef USEGSLIB
+    return (p_decoder->width/ds) * (p_decoder->height/ds);
+#else
+    return (p_decoder->width/ds) * 
+           (p_decoder->height/ds) * 
+           FB_DATA_SZ;
+#endif
+}
+
+static int load_image(char *filename, struct image_info *info,
+                      unsigned char *buf, ssize_t *buf_size)
+{
+    int w, h;
+    long time = 0; /* measured ticks */
+    struct gif_decoder *p_decoder = &decoder;
+
+    unsigned char *memory, *memory_max;
+    size_t memory_size;
+
+    /* cleanup */
+    memset(&disp, 0, sizeof(disp));
+
+    /* align buffer */
+    memory = (unsigned char *)((intptr_t)(buf + 3) & ~3);
+    memory_max = (unsigned char *)((intptr_t)(memory + *buf_size) & ~3);
+    memory_size = memory_max - memory;
+
+#ifdef DISK_SPINDOWN
+        if (iv->running_slideshow && iv->immediate_ata_off) {
+            /* running slideshow and time is long enough: power down disk */
+            rb->storage_sleep();
+        }
+#endif
+
+        /* Initialize decoder context struct, set buffer decoder is free
+         * to use.
+         * Decoder assumes that raw image file is loaded at the very
+         * end of the allocated buffer
+         */
+        gif_decoder_init(p_decoder, memory, memory_size);
+
+        /* populate internal data from gif file control structs */
+        gif_open(filename, p_decoder);
+
+        if (!p_decoder->error)
+        {
+
+            if (!iv->running_slideshow)
+            {
+                rb->lcd_putsf(0, 2, "image %dx%d",
+                              p_decoder->width,
+                              p_decoder->height);
+                rb->lcd_putsf(0, 3, "decoding %d*%d",
+                              p_decoder->width,
+                              p_decoder->height);
+                rb->lcd_update();
+            }
+
+            /* the actual decoding */
+            time = *rb->current_tick;
+
+#ifdef HAVE_ADJUSTABLE_CPU_FREQ
+            rb->cpu_boost(true);
+#endif
+            gif_decode(p_decoder, iv->cb_progress);
+
+#ifdef HAVE_ADJUSTABLE_CPU_FREQ
+            rb->cpu_boost(false);
+#endif
+            time = *rb->current_tick - time;
+        }
+
+    if (!iv->running_slideshow && !p_decoder->error)
+    {
+        rb->snprintf(print, sizeof(print), " %ld.%02ld sec ", time/HZ, time%HZ);
+        rb->lcd_getstringsize(print, &w, &h); /* centered in progress bar */
+        rb->lcd_putsxy((LCD_WIDTH - w)/2, LCD_HEIGHT - h, print);
+        rb->lcd_update();
+    }
+
+    if (p_decoder->error)
+    {
+        rb->splashf(HZ, "%s", GifErrorString(p_decoder->error));
+        return PLUGIN_ERROR;
+    }
+
+    info->x_size = p_decoder->width;
+    info->y_size = p_decoder->height;
+
+    p_decoder->native_img_size = (p_decoder->native_img_size + 3) & ~3;
+    disp_buf = p_decoder->mem + p_decoder->native_img_size;
+    *buf_size = memory_max - disp_buf;
+
+    return PLUGIN_OK;
+}
+
+static int get_image(struct image_info *info, int ds)
+{
+    unsigned char **p_disp = &disp[ds]; /* short cut */
+    struct gif_decoder *p_decoder = &decoder;
+
+    info->width = p_decoder->width / ds;
+    info->height = p_decoder->height / ds;
+    info->data = p_disp;
+
+    if (*p_disp != NULL)
+    {
+        /* we still have it */
+        return PLUGIN_OK;
+    }
+
+    /* assign image buffer */
+    if (ds > 1) {
+        if (!iv->running_slideshow)
+        {
+            rb->lcd_putsf(0, 3, "resizing %d*%d", info->width, info->height);
+            rb->lcd_update();
+        }
+        struct bitmap bmp_src, bmp_dst;
+
+        int size = img_mem(ds);
+
+        if (disp_buf + size >= p_decoder->mem + p_decoder->mem_size) {
+            /* have to discard the current */
+            int i;
+            for (i=1; i<=8; i++)
+                disp[i] = NULL; /* invalidate all bitmaps */
+
+            /* start again from the beginning of the buffer */
+            disp_buf = p_decoder->mem + p_decoder->native_img_size;
+        }
+
+        *p_disp = disp_buf;
+        disp_buf += size;
+
+        bmp_src.width = p_decoder->width;
+        bmp_src.height = p_decoder->height;
+        bmp_src.data = p_decoder->mem;
+
+        bmp_dst.width = info->width;
+        bmp_dst.height = info->height;
+        bmp_dst.data = *p_disp;
+
+#ifdef HAVE_ADJUSTABLE_CPU_FREQ
+        rb->cpu_boost(true);
+#endif
+        resize_bitmap(&bmp_src, &bmp_dst);
+
+#ifdef HAVE_ADJUSTABLE_CPU_FREQ
+        rb->cpu_boost(false);
+#endif
+    } else {
+        *p_disp = p_decoder->mem;
+    }
+
+    return PLUGIN_OK;
+}
+
+const struct image_decoder image_decoder = {
+    true,
+    img_mem,
+    load_image,
+    get_image,
+    draw_image_rect,
+};
+
+IMGDEC_HEADER
---
+++ apps/plugins/imageviewer/gif/gif_decoder.c
@@ -0,0 +1,367 @@
+#include <lib/pluginlib_bmp.h>
+#include "bmp.h"
+#if LCD_DEPTH < 8
+#include <lib/grey.h>
+#endif
+
+#include "gif_lib.h"
+#include "gif_decoder.h"
+
+#ifndef resize_bitmap
+#if defined(HAVE_LCD_COLOR)
+#define resize_bitmap   smooth_resize_bitmap
+#else
+#define resize_bitmap   grey_resize_bitmap
+#endif
+#endif
+
+static GifFileType *GifFile;
+
+#if 0
+typedef struct {
+    unsigned int byteCount;
+    char         *bytes;
+} ExtBlock;
+
+static int ext_block_append(ExtBlock *extBlock,
+                            unsigned int len,
+                            unsigned char extData[])
+{
+    extBlock->bytes = realloc(extBlock->bytes,extBlock->byteCount+len);
+    if (extBlock->bytes == NULL)
+    {
+        return (GIF_ERROR);
+    }
+
+    memcpy(&(extBlock->bytes[extBlock->byteCount]), &extData[0], len);
+    extBlock->byteCount += len;
+    return (GIF_OK);
+}
+#endif
+
+/* convert one decoded gif line into 24bit RGB
+ * and then to native pixel format using
+ * dedicated function
+ */
+static void gif2native(struct scaler_context *ctx,
+                       ColorMapObject *ColorMap,
+                       GraphicsControlBlock *GCB,
+                       GifPixelType *Line,
+                       int Row)
+{
+    int x;
+    GifColorType *ColorMapEntry;
+
+#ifdef HAVE_LCD_COLOR
+    struct uint8_rgb *pixel;
+    struct uint8_rgb *buf = (struct uint8_rgb *)malloc(ctx->bm->width * sizeof(struct uint8_rgb));
+    const struct custom_format *cformat = &format_native;
+#else
+    unsigned char *pixel;
+    uint8_t *buf = (unsigned char *)malloc(ctx->bm->width);
+    struct uint8_rgb rgb;
+    const struct custom_format *cformat = &format_grey;
+#endif
+
+    void (*output_row_8)(uint32_t, void*, struct scaler_context*) = cformat->output_row_8;
+    pixel = buf;
+
+    for (x = 0; x < ctx->bm->width; x++)
+    {
+        ColorMapEntry = &ColorMap->Colors[Line[x]];
+
+#ifdef HAVE_LCD_COLOR
+        /* Assumes black background */
+        /* set RGB tripple according to gif colormap */
+        if (GCB && GCB->TransparentColor == Line[x])
+        {
+            pixel->red = 0;
+            pixel->green = 0;
+            pixel->blue = 0;
+        }
+        else
+        {
+            pixel->red = ColorMapEntry->Red;
+            pixel->green = ColorMapEntry->Green;
+            pixel->blue = ColorMapEntry->Blue;
+        }
+#else
+        /* set RGB tripple according to gif colormap
+         * and calculate luminance value
+         */
+        if (GCB && GCB->TransparentColor == Line[x])
+            *pixel = 0;
+        else
+        {
+            rgb.red = ColorMapEntry->Red;
+            rgb.green = ColorMapEntry->Green;
+            rgb.blue = ColorMapEntry->Blue;
+            *pixel = brightness(rgb);
+        }
+#endif
+        pixel++;
+    }
+
+    output_row_8(Row, (void *)buf, ctx);
+
+    free(buf);
+}
+
+void gif_decoder_init(struct gif_decoder *d, void *mem, size_t size)
+{
+    memset(d, 0, sizeof(struct gif_decoder));
+
+    d->mem = mem;
+    d->mem_size = size;
+
+    /* mem allocator init */
+    init_memory_pool(d->mem, d->mem_size);
+}
+
+void gif_open(char *filename, struct gif_decoder *d)
+{
+    if ((GifFile = DGifOpenFileName(filename, &d->error)) == NULL)
+        return;
+
+    d->width = GifFile->SWidth;
+    d->height = GifFile->SHeight;
+}
+
+/* var names adhere to giflib coding style */
+void gif_decode(struct gif_decoder *d,
+                void (*pf_progress)(int current, int total))
+{
+    bool skip = false;
+    int i, j;
+
+    int Size;
+    int Row;
+    int Width;
+    int Height;
+    int ExtCode;
+
+    GifPixelType *Line;
+    GifRecordType RecordType;
+    ColorMapObject *ColorMap;
+    GraphicsControlBlock *GCB = NULL;
+    GifByteType *Extension;
+
+    const char InterlacedOffset[] = { 0, 4, 2, 1 }; /* The way Interlaced image should. */
+    const char InterlacedJumps[] = { 8, 8, 4, 2 };  /* be read - offsets and jumps... */
+
+    /* used for color conversion */
+    struct bitmap bm;
+    struct scaler_context ctx = {
+        .bm = &bm,
+        .dither = 0
+    };
+
+    Size = GifFile->SWidth * sizeof(GifPixelType); /* Size in bytes one row.*/
+    Line = (GifPixelType *)malloc(Size); /* allocate space for one row */
+
+    bm.width = GifFile->SWidth;
+    bm.height = GifFile->SHeight;
+#ifdef HAVE_LCD_COLOR
+    d->native_img_size = GifFile->SWidth * GifFile->SHeight * FB_DATA_SZ;
+    bm.data = malloc(d->native_img_size);
+#else
+    d->native_img_size = GifFile->SWidth * GifFile->SHeight;
+    bm.data = malloc(d->native_img_size);
+#endif
+
+    if (Line == NULL)
+    {
+        // error allocating temp space
+        d->error = D_GIF_ERR_NOT_ENOUGH_MEM;
+        return;
+    }
+
+    if (pf_progress != NULL)
+        pf_progress(0, 100);
+
+    /* Scan the content of the GIF file and load the image(s) in: */
+    do
+    {
+        if (DGifGetRecordType(GifFile, &RecordType) == GIF_ERROR)
+        {
+            d->error = GifFile->Error;
+            return;
+        }
+
+        switch (RecordType)
+        {
+            case IMAGE_DESC_RECORD_TYPE:
+
+                if (DGifGetImageDesc(GifFile) == GIF_ERROR)
+                {
+                    d->error = GifFile->Error;
+                    return;
+                }
+
+                /* Image Position relative to canvas */
+                Row = GifFile->Image.Top;
+                Width = GifFile->Image.Width;
+                Height = GifFile->Image.Height;
+
+                /* Color map to use */
+                ColorMap = (GifFile->Image.ColorMap ?
+                            GifFile->Image.ColorMap :
+                            GifFile->SColorMap);
+
+                if (ColorMap == NULL)
+                {
+                    d->error = D_GIF_ERR_NO_COLOR_MAP;
+                    return;
+                }
+
+                /* sanity check */
+                if (GifFile->Image.Left + GifFile->Image.Width > GifFile->SWidth ||
+                   GifFile->Image.Top + GifFile->Image.Height > GifFile->SHeight)
+                {
+                    d->error = D_GIF_ERR_DATA_TOO_BIG;
+                    return;
+                }
+
+                if (GifFile->Image.Interlace)
+                {
+                    /* Need to perform 4 passes on the image */
+                    for (i = 0; i < 4; i++)
+                    {
+                        for (j = Row + InterlacedOffset[i]; j < Row + Height; j += InterlacedJumps[i])
+                        {
+                            if (DGifGetLine(GifFile, Line, Width) == GIF_ERROR)
+                            {
+                                d->error = GifFile->Error;
+                                return;
+                            }
+
+                            if (!skip || (GCB && GCB->DelayTime == 0))
+                                gif2native(&ctx, ColorMap, GCB, Line, j);
+                        }
+
+                        pf_progress(25*(i+1), 100);
+                    }
+                }
+                else
+                {
+                    for (i = 0; i < Height; i++)
+                    {
+                        /* load single line into buffer */
+                        if (DGifGetLine(GifFile, Line, Width) == GIF_ERROR)
+                        {
+                            d->error = GifFile->Error;
+                            return;
+                        }
+
+                        if (!skip || (GCB && GCB->DelayTime == 0))
+                            gif2native(&ctx, ColorMap, GCB, Line, i);
+
+                        pf_progress(100*(i+1)/Height, 100);
+                    }
+                }
+                skip = true;
+
+                break;
+
+            case EXTENSION_RECORD_TYPE:
+                if (DGifGetExtension(GifFile, &ExtCode, &Extension) == GIF_ERROR)
+                {
+                    d->error = GifFile->Error;
+                    return;
+                }
+
+                if (ExtCode == GRAPHICS_EXT_FUNC_CODE)
+                {
+                    GCB = (GraphicsControlBlock *)malloc(sizeof(GraphicsControlBlock));
+
+                    if (DGifExtensionToGCB(Extension[0], Extension+1, GCB) == GIF_ERROR)
+                    {
+                        d->error = GifFile->Error;
+                        return;
+                    }
+                }
+
+                /* Skip anything else */
+                while (Extension != NULL)
+                {
+                    if (DGifGetExtensionNext(GifFile, &Extension) == GIF_ERROR)
+                    {
+                        d->error = GifFile->Error;
+                        return;
+                    }
+                }
+                break;
+
+            /* including TERMINATE_RECORD_TYPE */
+            default:
+                break;
+        }
+
+    } while (RecordType != TERMINATE_RECORD_TYPE);
+
+    /* free all internal allocated data */
+    if (DGifCloseFile(GifFile) == GIF_ERROR)
+    {
+        d->error = GifFile->Error;
+        return;
+    }
+ 
+    free(GCB);
+
+    /* WARNING !!!! */
+    /* GifFile object is trashed from now on, DONT use it */
+    /* Move bitmap in native format to the front of the buff */
+    memmove(d->mem, bm.data, d->native_img_size);
+
+    /* correct aspect ratio */
+#if (LCD_PIXEL_ASPECT_HEIGHT != 1 || LCD_PIXEL_ASPECT_WIDTH != 1)
+    struct bitmap img_src, img_dst;    /* scaler vars */
+    struct dim dim_src, dim_dst;       /* recalc_dimensions vars */
+    size_t c_native_img_size;             /* size of the image after correction */
+
+    dim_src.width = bm.width;
+    dim_src.height = bm.height;
+
+    dim_dst.width = bm.width;
+    dim_dst.height = bm.height;
+
+    /* defined in apps/recorder/resize.c */
+    if (!recalc_dimension(&dim_dst, &dim_src))
+    {
+        /* calculate 'corrected' image size */
+#ifdef HAVE_LCD_COLOR
+        c_native_img_size = dim_dst.width * dim_dst.height * FB_DATA_SZ;
+#else
+        c_native_img_size = dim_dst.width * dim_dst.height;
+#endif
+
+        /* check memory constraints
+         * do the correction only if there is enough
+         * free memory
+         */
+        if (d->native_img_size + c_native_img_size <= d->mem_size)
+        {
+            img_src.width = dim_src.width;
+            img_src.height = dim_src.height;
+            img_src.data = (unsigned char *)d->mem;
+
+            img_dst.width = dim_dst.width;
+            img_dst.height = dim_dst.height;
+            img_dst.data = (unsigned char *)d->mem + d->native_img_size;
+
+            /* scale the bitmap to correct physical
+             * pixel dimentions
+             */
+            resize_bitmap(&img_src, &img_dst);
+
+            /* update decoder struct */
+            d->width = img_dst.width;
+            d->height = img_dst.height;
+            d->native_img_size = c_native_img_size;
+
+            /* copy back corrected image to the begining of the buffer */
+            memmove(img_src.data, img_dst.data, d->native_img_size);
+        }
+    }
+#endif
+}
