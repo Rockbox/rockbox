f6d088c232faef38b27dc74b41f78ce50076ee17
Michael Giacomelli <giac2000@hotmail.com>
2012-11-24 02:54:35 +0100

    Clip radio keymap update by Shiloh Hawley and myself.
    Makes FM radio keys consistent with the updated Clip keymap in the rest of rockbox.  Home now goes to the main menu, power stops the radio, and several other keys are a little more obvious.
    
    Change-Id: Iaab207acf936820d5602ae1ce4ffbbe30bf7877e

--- apps/keymaps/keymap-clip.c
+++ apps/keymaps/keymap-clip.c
@@ -242,14 +242,14 @@ static const struct button_mapping button_context_recscreen[]  = {
 
 /** FM Radio Screen **/
 static const struct button_mapping button_context_radio[]  = {
-    /* Copied from keymap-c200.c 20081207. Feel free to suggest a better one.*/
-    { ACTION_NONE,              BUTTON_UP,                      BUTTON_NONE },
-    { ACTION_FM_MENU,           BUTTON_DOWN,                    BUTTON_NONE },
-    { ACTION_FM_PRESET,         BUTTON_SELECT,                  BUTTON_NONE },
-    { ACTION_FM_STOP,           BUTTON_UP|BUTTON_REPEAT,        BUTTON_UP   },
-    { ACTION_FM_MODE,           BUTTON_HOME|BUTTON_REL,         BUTTON_HOME },
-    { ACTION_FM_EXIT,           BUTTON_POWER|BUTTON_REL,        BUTTON_POWER },
-    { ACTION_FM_PLAY,           BUTTON_UP|BUTTON_REL,           BUTTON_UP },
+    { ACTION_NONE,              BUTTON_UP|BUTTON_REPEAT,        BUTTON_UP},
+    { ACTION_NONE,              BUTTON_SELECT,                  BUTTON_NONE },
+    { ACTION_FM_MODE,           BUTTON_DOWN,                    BUTTON_NONE },
+    { ACTION_FM_PRESET,         BUTTON_SELECT|BUTTON_REL,       BUTTON_SELECT },
+    { ACTION_FM_STOP,           BUTTON_POWER|BUTTON_REL,        BUTTON_POWER },
+    { ACTION_FM_MENU,           BUTTON_SELECT|BUTTON_REPEAT,    BUTTON_SELECT},
+    { ACTION_FM_EXIT,           BUTTON_HOME|BUTTON_REL,         BUTTON_HOME },
+    { ACTION_FM_PLAY,           BUTTON_UP,                      BUTTON_NONE },
     { ACTION_SETTINGS_INC,      BUTTON_VOL_UP,                  BUTTON_NONE },
     { ACTION_SETTINGS_INCREPEAT,BUTTON_VOL_UP|BUTTON_REPEAT,    BUTTON_NONE },
     { ACTION_SETTINGS_DEC,      BUTTON_VOL_DOWN,                BUTTON_NONE },
