fa6dc058ef702002cdc3019c7f53df518c38fdc8
Amaury Pouly <amaury.pouly@gmail.com>
2013-01-15 22:49:43 +0000

    zenxfi2: forgot a simulator file
    Change-Id: I5c42fc57286440c45ce08396ac1b232d611b30b1

---
+++ uisimulator/buttonmap/creative-zenxfi2.c
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2011 by Amaury Pouly
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+
+#include <SDL.h>
+#include "button.h"
+#include "buttonmap.h"
+
+int key_to_button(int keyboard_button)
+{
+    int new_btn = BUTTON_NONE;
+    switch (keyboard_button)
+    {
+        case SDLK_PAGEDOWN:
+            new_btn = BUTTON_MENU;
+            break;
+        case SDLK_HOME:
+            new_btn = BUTTON_POWER;
+            break;
+    }
+    return new_btn;
+}
+
+struct button_map bm[] = {
+    { SDLK_PAGEDOWN, 162, 136, 26, "Menu" },
+    { 0, 0, 0, 0, "None" }
+};
+ 
