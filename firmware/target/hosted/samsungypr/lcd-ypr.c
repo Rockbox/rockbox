aafb9b12b5e223593ea119c9815167b933321113
Lorenzo Miori <memorys60@gmail.com>
2013-02-23 19:59:22 +0100

    Samsung YP-R0 LCD enable improvements
    This patch adds to YP-R0 (and other future targets using Linux
    framebuffer) the ability to use LCD_ENABLE for a better
    intereface and, moreover, to save some CPU while display is
    powered off.
    Everything is compatible with lcd-memframe driver, I actually
    used its facilities.
    By the way, changed the way to blank-unblank LCD: now using
    ioctl call, slightly more efficient.
    
    Change-Id: I544de77f5abd4ac1c13d3fe3a6e40a30f7c0bece

--- firmware/target/hosted/samsungypr/lcd-ypr.c
+++ firmware/target/hosted/samsungypr/lcd-ypr.c
@@ -8,6 +8,7 @@
  * $Id$
  *
  * Copyright (C) 2011 Lorenzo Miori, Thomas Martitz
+ * Copyright (C) 2013 Lorenzo Miori
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,7 +23,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#include "string.h"
 #include <linux/fb.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
@@ -37,13 +37,14 @@
 static int dev_fd = 0;
 fb_data *dev_fb = 0;
 
-/* FIXME: never used! Place this into final teardown */
+#ifdef HAVE_LCD_SHUTDOWN
 void lcd_shutdown(void)
 {
     printf("FB closed.");
     munmap(dev_fb, FRAMEBUFFER_SIZE);
     close(dev_fd);
 }
+#endif
 
 void lcd_init_device(void)
 {
@@ -59,7 +60,7 @@ void lcd_init_device(void)
     }
     printf("The framebuffer device was opened successfully.\n");
 
-    /* Get the fixed properties */
+    /* Get the fixed properties, not really nedeed but we as a check */
     if (ioctl(dev_fd, FBIOGET_FSCREENINFO, &finfo) == -1) {
         perror("Error reading fixed information");
         exit(2);
@@ -71,7 +72,7 @@ void lcd_init_device(void)
         exit(3);
     }
 
-    vinfo.bits_per_pixel = 16;
+    vinfo.bits_per_pixel = LCD_DEPTH;
 
     if (ioctl(dev_fd, FBIOPUT_VSCREENINFO, &vinfo)) {
             perror("fbset(ioctl)");
@@ -82,8 +83,7 @@ void lcd_init_device(void)
 
     /* Figure out the size of the screen in bytes */
     screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
-    if (screensize != FRAMEBUFFER_SIZE)
-    {
+    if (screensize != FRAMEBUFFER_SIZE) {
         exit(4);
         perror("Display and framebuffer mismatch!\n");
     }
@@ -95,25 +95,24 @@ void lcd_init_device(void)
         exit(4);
     }
     printf("The framebuffer device was mapped to memory successfully.\n");
+#ifdef HAVE_LCD_ENABLE
+    /* Be sure to turn on display at startup */
+    lcd_enable(true);
+#endif
 }
 
-/*TODO: implemented in a better way -> ie using LCD_ENABLE */
+#ifdef HAVE_LCD_ENABLE
+void lcd_enable(bool enable)
+{
+    if (lcd_active() == enable)
+        return;
 
-/* Turn off display
- * This translates - in most of the cases - in power supply off
- */
-void _backlight_lcd_sleep(void)
-{
-    int fp = open("/sys/class/graphics/fb0/blank", O_RDWR);
-    write(fp, "1", 1);
-    close(fp);
+    lcd_set_active(enable);
+
+    /* Turn on or off the display using Linux interface */
+    ioctl(dev_fd, FBIOBLANK, enable ? VESA_NO_BLANKING : VESA_POWERDOWN);
+
+    if (enable)
+        send_event(LCD_EVENT_ACTIVATION, NULL);
 }
-/* Turn on LCD screen
- * This translates - in most of the cases - in power supply on
- */
-void _backlight_lcd_wake(void)
-{
-    int fp = open("/sys/class/graphics/fb0/blank", O_RDWR);
-    write(fp, "0", 1);
-    close(fp);
-}
+#endif
