05671063176b36d46751ffbdb5f1bc967c119463
Andrew Ryabinin <ryabinin.a.a@gmail.com>
2013-04-15 09:51:22 +0400

    Introduce HiFi E.T MA9 port.
    Change-Id: I79aadc958fd5222f26f91ed127f8c6fb2c465dc2

---
+++ firmware/target/arm/rk27xx/ma/button-target.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2013 Andrew Ryabinin
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#ifndef _BUTTON_TARGET_H_
+#define _BUTTON_TARGET_H_
+
+#define BUTTON_UP          0x00000001
+#define BUTTON_DOWN        0x00000002
+#define BUTTON_LEFT        0x00000004
+#define BUTTON_RIGHT       0x00000008
+#define BUTTON_PLAY        0x00000010
+#define BUTTON_MENU        0x00000020
+#define BUTTON_BACK        0x00000040
+
+#define BUTTON_MAIN (BUTTON_UP|BUTTON_DOWN| \
+                     BUTTON_RIGHT|BUTTON_LEFT| \
+                     BUTTON_PLAY|BUTTON_MENU|BUTTON_BACK)
+
+#define POWEROFF_BUTTON BUTTON_PLAY
+#define POWEROFF_COUNT 30
+
+#endif /* _BUTTON_TARGET_H_ */
