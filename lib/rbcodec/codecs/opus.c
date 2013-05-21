Rockbox/rockbox

commit 2e9aa3d8b04ff56fad4ffb2f855e40dfffbd827b

Opus: avoid allocating space for comment packets
Fixes playback of files with large embedded album art.

Change-Id: I94d336e3da968a93047dd00a5fa65e4c3423a7da

lib/rbcodec/codecs/opus.c
@@ -384,6 +384,12 @@ enum codec_status codec_run(void)
                 stream_init = 1;
             }
 
+            /* Do this to avoid allocating space for huge comment packets
+               (embedded Album Art) */
+            if(os.packetno == 1){
+              ogg_sync_reset(&oy);
+            }
+
             /* Add page to the bitstream */
             ogg_stream_pagein(&os, &og);
 

commit de86b4a3c5f6e2add2dc7588e6be37c8df2302f0

Opus: fix glitch caused by 2e9aa3d
Change-Id: I1519f3bf2cdf74f3d4741951973352b2678b7722

lib/rbcodec/codecs/opus.c
@@ -385,17 +385,17 @@ enum codec_status codec_run(void)
                 stream_init = 1;
             }
 
-            /* Do this to avoid allocating space for huge comment packets
-               (embedded Album Art) */
-            if(os.packetno == 1){
-              ogg_sync_reset(&oy);
-            }
-
             /* Add page to the bitstream */
             ogg_stream_pagein(&os, &og);
 
             page_granule = ogg_page_granulepos(&og);
             granule_pos = page_granule;
+
+            /* Do this to avoid allocating space for huge comment packets
+               (embedded Album Art) */
+            if(os.packetno == 1 && ogg_stream_packetpeek(&os, &op) != 1){
+              ogg_sync_reset(&oy);
+            }
 
             while ((ogg_stream_packetout(&os, &op) == 1) && !op.e_o_s) {
                 if (op.packetno == 0){