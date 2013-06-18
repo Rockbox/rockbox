d997dafb6ca1644fdb07fa9850b72eb178ba45d4
Amaury Pouly <amaury.pouly@gmail.com>
2013-06-18 16:15:07 +0200

    fuzeplus: drop audio routing driver in favor of the generic one
    Change-Id: I89828ef858606bf09e115ff9c27ff9ecc8bf8b8e

--- firmware/target/arm/imx233/audio-imx233.h
+++ firmware/target/arm/imx233/audio-imx233.h
@@ -17,7 +17,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- ****************************************************************************/ 
+ ****************************************************************************/
 #ifndef __audio_imx233__
 #define __audio_imx233__
 
