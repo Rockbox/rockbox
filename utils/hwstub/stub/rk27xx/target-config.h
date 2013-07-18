4d9b03f90dd07f50f899fdbd698770d34bf47ecc
Marcin Bukat <marcin.bukat@gmail.com>
2013-07-18 23:55:35 +0200

    hwstub rk27xx port WIP
    Change-Id: I85ac57117911544b65ccd56eb16303e30be67cab

---
+++ utils/hwstub/stub/rk27xx/target-config.h
@@ -0,0 +1,9 @@
+#define CONFIG_RK27XX
+#define IRAM_ORIG       0x60000000
+#define IRAM_SIZE       0x8000
+#define DRAM_ORIG       0x60000000
+#define DRAM_SIZE       (MEMORYSIZE * 0x100000)
+#define CPU_ARM
+#define ARM_ARCH        5
+#define USB_BASE            0x180A000
+#define USB_NUM_ENDPOINTS   2
