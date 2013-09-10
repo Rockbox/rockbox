e84b096e0479ddec82396137510d379aad586807
Lorenzo Miori <memoryS60@gmail.com>
2013-09-10 22:48:34 +0200

    Samsung YP-R1 target port
    This is the basic port to the new target Samsung
    YP-R1, which runs on a similar platform as YP-R0.
    Port is usable, although there are still
    some optimizations that have to be done.
    
    Change-Id: If83a8e386369e413581753780c159026d9e41f04

--- firmware/drivers/audio/wm8978.c
+++ firmware/drivers/audio/wm8978.c
@@ -497,7 +497,7 @@ void audiohw_enable_depth_3d(bool enable)
     audiohw_set_depth_3d(wmc_vol.enh_3d);
 }
 
-#ifdef HAVE_RECORDING
+#if defined(HAVE_RECORDING) || defined(SAMSUNG_YPR1)
 void audiohw_set_recsrc(int source, bool recording)
 {
     switch (source)
@@ -559,6 +559,20 @@ void audiohw_set_recsrc(int source, bool recording)
             /* Enable bypass to L/R mixers */
             wmc_set(WMC_LEFT_MIXER_CTRL, WMC_BYPL2LMIX);
             wmc_set(WMC_RIGHT_MIXER_CTRL, WMC_BYPR2RMIX);
+#ifdef SAMSUNG_YPR1
+            wmc_set(WMC_LEFT_MIXER_CTRL, WMC_AUXL2LMIX);
+            wmc_set(WMC_RIGHT_MIXER_CTRL, WMC_AUXR2RMIX);
+            /* Set L/R gain to 0dB */
+            wmc_write_masked(WMC_LEFT_MIXER_CTRL, 0x05 << WMC_AUXLMIXVOL_POS,
+                     WMC_AUXLMIXVOL);
+            wmc_write_masked(WMC_RIGHT_MIXER_CTRL, 0x05 << WMC_AUXRMIXVOL_POS,
+                     WMC_AUXRMIXVOL);
+            wmc_set(WMC_LEFT_MIXER_CTRL, WMC_DACL2LMIX);
+            wmc_set(WMC_RIGHT_MIXER_CTRL, WMC_DACR2RMIX);
+            wmc_set(WMC_POWER_MANAGEMENT1, WMC_OUT3MIXEN);
+            wmc_set(WMC_POWER_MANAGEMENT3, WMC_RMIXEN);
+            wmc_set(WMC_POWER_MANAGEMENT3, WMC_LMIXEN);
+#endif
         }
         break;
     }
