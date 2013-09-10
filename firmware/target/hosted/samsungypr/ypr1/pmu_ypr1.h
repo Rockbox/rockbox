de4c5c2ff5db01eab22317fc073d8087cde14b21
Lorenzo Miori <memoryS60@gmail.com>
2013-09-10 22:48:34 +0200

    Samsung YP-R1 target port
    This is the basic port to the new target Samsung
    YP-R1, which runs on a similar platform of YP-R0
    Port is usable right now, although there are still
    some optimizations that has to be done as well fixing
    the radio (works but no sound is present)
    
    Change-Id: If83a8e386369e413581753780c159026d9e41f04

--- firmware/target/hosted/samsungypr/ypr1/pmu_ypr1.h
+++ firmware/target/hosted/samsungypr/ypr1/pmu_ypr1.h
@@ -6,9 +6,7 @@
  *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
  *                     \/            \/     \/    \/            \/
  *
- * Module wrapper for GPIO, using kernel module of Samsung YP-R0/YP-R1
- *
- * Copyright (c) 2011 Lorenzo Miori
+ * Copyright (C) 2013 Lorenzo Miori
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,29 +18,14 @@
  *
  ****************************************************************************/
 
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <gpio-target.h> /* includes common ioctl device definitions */
-#include <sys/ioctl.h>
+#ifndef __PMU_YPR1_H__
+#define __PMU_YPR1_H__
 
-static int gpio_dev = 0;
+#include "r1PmuIoctl.h"
 
-void gpio_init(void)
-{
-    gpio_dev = open(GPIO_DEVICE, O_RDONLY);
-    if (gpio_dev < 0)
-        printf("GPIO device open error!");
-}
+void pmu_init(void);
+void pmu_close(void);
+int pmu_get_dev(void);
+int pmu_ioctl(int request, int *data);
 
-void gpio_close(void)
-{
-    if (gpio_dev >= 0)
-        close(gpio_dev);
-}
-
-int gpio_control(int request, int num, int mode, int val)
-{
-    struct gpio_info r = { .num = num, .mode = mode, .val = val, };
-    return ioctl(gpio_dev, request, &r);
-}
+#endif
