89afa32584f534f89f94c493f915c49dad51539d
Jean-Louis Biasini <jlbiasini@gmail.com>
2013-09-18 23:05:56 +0200

    fuze+: fix volume bottom limit
    Even with volume at minimal fuze+ was still playing sound pretty loud.
    this fix it.
    
    Change-Id: I5b18e9c39ced240eebb0457c433b0ca150668860
    Reviewed-on: http://gerrit.rockbox.org/621
    Reviewed-by: Amaury Pouly <amaury.pouly@gmail.com>

--- firmware/export/imx233-codec.h
+++ firmware/export/imx233-codec.h
@@ -27,9 +27,7 @@
 #define AUDIOHW_CAPS    (DEPTH_3D_CAP | BASS_CAP | TREBLE_CAP | \
                          LIN_GAIN_CAP | MIC_GAIN_CAP)
 
-/* minimum is -57.5dB and max is 6dB in DAC mode
- * and -51.5dB / 12dB in Line1 mode */
-AUDIOHW_SETTING(VOLUME,     "dB", 0, 1, -58,  12,  -25)
+AUDIOHW_SETTING(VOLUME,     "dB", 0, 1, -100,  12,  -25)
 /* HAVE_SW_TONE_CONTROLS */
 #ifdef HAVE_RECORDING
 /* Depending on the input, we have three available volumes to tweak:
