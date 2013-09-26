f75310598c6daa51f0d2e5839f031bbf100ba4ba
Amaury Pouly <amaury.pouly@gmail.com>
2013-09-26 18:30:36 +0200

    imx233/bl: usb mode has priority over hold
    Change-Id: I0d124eaed0ebf1ca18c022684a96dfbf7d740c7e

--- bootloader/imx233.c
+++ bootloader/imx233.c
@@ -156,23 +156,6 @@ void main(uint32_t arg, uint32_t addr)
     backlight_init();
 
     button_init();
-    /* dummy read, might be necessary to init things */
-#ifdef HAVE_BUTTON_DATA
-    int data;
-    button_read_device(&data);
-#else
-    button_read_device();
-#endif
-
-#ifdef HAS_BUTTON_HOLD
-    if(button_hold())
-    {
-        printf("Hold switch on");
-        printf("Shutting down...");
-        sleep(HZ);
-        power_off();
-    }
-#endif
 
     printf("%s: %s", MSG(240, "Ver", "Boot version"), RBVERSION);
     printf("%s: %x ", MSG(240, "Arg", "Boot arg"), arg);
@@ -201,6 +184,24 @@ void main(uint32_t arg, uint32_t addr)
 
     if(usb_detect() == USB_INSERTED)
         usb_mode(HZ);
+
+    /* dummy read, might be necessary to init things */
+#ifdef HAVE_BUTTON_DATA
+    int data;
+    button_read_device(&data);
+#else
+    button_read_device();
+#endif
+
+#ifdef HAS_BUTTON_HOLD
+    if(button_hold())
+    {
+        printf("Hold switch on");
+        printf("Shutting down...");
+        sleep(HZ);
+        power_off();
+    }
+#endif
 
     printf("Loading firmware");
 
