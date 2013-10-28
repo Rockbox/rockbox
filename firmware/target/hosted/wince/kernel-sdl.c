e348ce5cb9bd3163f1141d0161dfacdda5750830
Benjamin Brown <foolsh.person@gmail.com>
2013-10-28 19:28:45 -0400

    SDL APP Wince SDL Target plus Toolchain - Mingw32ce
    This is the SDL app WinCE port of rockbox.  The toolchain
    can be built with the rockboxdev.sh script (option c)
    SDL 1.2.6 libraries will also be built for mingw32ce
    and placed in the mingw32ce include and lib directory
    
    Change-Id: Ibcd849b5a69e0108a36445e658fea6c876d2b766
    
    Added mingw32ce () crosscompiler section to rockbox/tools/configure
    
    Change-Id: I43d75443ba9f938310e4cc5fd6edbcaed28027cb
    
    Added sdlappwince as target "208) Wince SDL" with
    target_id=100 using mingw32ce toolchain
    
    Change-Id: Ic07a48083b835da2602f64b69501a7a9d97d9fa0
    
    Set uname=`uname` in tools/configure
    
    Change-Id: Ib51431c9b80a66a097780763bb359cf5371951ca
    
    Added #elif SDLAPP_WINCE section to firmware/export/config.h
    
    Change-Id: Iee5ee7301793000342cc0ed832cfe34f3c038fd9
    
    Set #define CONFIG_CODEC SWCODEC in /firmware/exports/config/sdlappwince.h
    
    Change-Id: Ib8f10710524faffcb2fd3301be73a7fefa5b5c11
    
    Imported all files from /firmware/target/hosted/sdl/
    
    Change-Id: Id23ae746bcef7989d967dc966b77a37be6532556
    
    Enabled all defines in sdlappwince.h
    
    Change-Id: I7b20e2c1e87c8e686880ff1666be9c6668d255e7
    
    Set t-cpu="hosted" in 208 sdlappwince section of tools/configure
    
    Change-Id: I19e35ae6e14a2afd534a29b462971c7b0e7a82df
    
    Rename firmware/target/hosted/wince to firmware/target/hosted/mingw32ce
    
    Change-Id: Ieb42b980719580821d6f1bc675bb408afc90f3b0
    
    Add #define MINSIGSTKSZ	2048 to firmware/export/config/sdlappwince.h
    
    Change-Id: Iba746fb5b57be8a3f3b1dc4627dd1f90c709d830
    
    Removed TAB from define firmware/export/config/sdlappwince.h
    
    Change-Id: Id340abd3c766674f703090c5df4a180554695ed5
    
    Rename firmware/target/hosted/minw32ce back to /wince
    
    Change-Id: I395bffc19fa56e0103c3e7c91e940ac79b2fd46d
    
    Clean up - Remove whitespace from ./tools./rockboxdev.sh
    
    Change-Id: Ie98867177472eb7115ac4406d30ef66a045b5197
    
    Add mingw32ce to target arch list & fix list alignment
    
    Change-Id: I41e174a7f7964cc39643603d2ce219f7965e3fde
    
    Added [Cc] section to case $arch in rockboxdev.sh
    
    Cleaned up more whitespace. Add blank build_mingw32ce function.
    Add comment # Cegcc's mingw32ce has a dedicated build script.
    
    Change-Id: I43011e2a8e6550dd6baf1b3857eabafb4b6e12b7
    
    Some more formatting and comments.
    
    Outline flow build_mingw32ce needs to take as.
    
    Change-Id: Icfa5d3210eeec549236cc329600a34450872fc45
    
    Working git sequence for mingw32ce in rockboxdev.sh
    
    Filling out of build_mingw32ce.  Uses git inside of rockbox build directory.
    Uses copy of test build directory function from build_ctng, will change that later.
    
    Change-Id: Idc45a09e838533eaadfd97813747364b46b5ddac
    
    Mingw32ce fetches and builds.
    
    It works but still needs headers patched.
    
    Change-Id: Ib3c08002e3cbd10b86a1b4e97f19d63103f83688
    
    Mingw32ce ToolChain and SDL CE added to rockboxdev.sh
    
    The toolchain build script for mingw32ce SDL app target.
    
    Change-Id: Id52f54f34f95a181e8af0aeb719e79ce0b6c39a8

---
+++ firmware/target/hosted/wince/kernel-sdl.c
@@ -0,0 +1,213 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 by Felix Arends
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_thread.h>
+#include <inttypes.h>
+#include "system-sdl.h"
+#include "thread-sdl.h"
+#include "kernel.h"
+#include "thread.h"
+#include "panic.h"
+#include "debug.h"
+
+static SDL_TimerID tick_timer_id;
+long start_tick;
+
+#ifndef HAVE_SDL_THREADS
+/* for the wait_for_interrupt function */
+static SDL_cond *wfi_cond;
+static SDL_mutex *wfi_mutex;
+#endif
+/* Condition to signal that "interrupts" may proceed */
+static SDL_cond *sim_thread_cond;
+/* Mutex to serialize changing levels and exclude other threads while
+ * inside a handler */
+static SDL_mutex *sim_irq_mtx;
+/* Level: 0 = enabled, not 0 = disabled */
+static int volatile interrupt_level = HIGHEST_IRQ_LEVEL;
+/* How many handers waiting? Not strictly needed because CondSignal is a
+ * noop if no threads were waiting but it filters-out calls to functions
+ * with higher overhead and provides info when debugging. */
+static int handlers_pending = 0;
+/* 1 = executing a handler; prevents CondSignal calls in set_irq_level
+ * while in a handler */
+static int status_reg = 0;
+
+/* Nescessary logic:
+ * 1) All threads must pass unblocked
+ * 2) Current handler must always pass unblocked
+ * 3) Threads must be excluded when irq routine is running
+ * 4) No more than one handler routine should execute at a time
+ */
+int set_irq_level(int level)
+{
+    SDL_LockMutex(sim_irq_mtx);
+
+    int oldlevel = interrupt_level;
+
+    if (status_reg == 0 && level == 0 && oldlevel != 0)
+    {
+        /* Not in a handler and "interrupts" are going from disabled to
+         * enabled; signal any pending handlers still waiting */
+        if (handlers_pending > 0)
+            SDL_CondBroadcast(sim_thread_cond);
+    }
+
+    interrupt_level = level; /* save new level */
+
+    SDL_UnlockMutex(sim_irq_mtx);
+    return oldlevel;
+}
+
+void sim_enter_irq_handler(void)
+{
+    SDL_LockMutex(sim_irq_mtx);
+    handlers_pending++;
+
+    /* Check each time before proceeding: disabled->enabled->...->disabled
+     * is possible on an app thread before a handler thread is ever granted
+     * the mutex; a handler can also leave "interrupts" disabled during
+     * its execution */
+    while (interrupt_level != 0)
+        SDL_CondWait(sim_thread_cond, sim_irq_mtx);
+
+    status_reg = 1;
+}
+
+void sim_exit_irq_handler(void)
+{
+    /* If any others are waiting, give the signal */
+    if (--handlers_pending > 0)
+        SDL_CondSignal(sim_thread_cond);
+
+    status_reg = 0;
+    SDL_UnlockMutex(sim_irq_mtx);
+#ifndef HAVE_SDL_THREADS
+    SDL_CondSignal(wfi_cond);
+#endif
+}
+
+static bool sim_kernel_init(void)
+{
+    sim_irq_mtx = SDL_CreateMutex();
+    if (sim_irq_mtx == NULL)
+    {
+        panicf("Cannot create sim_handler_mtx\n");
+        return false;
+    }
+
+    sim_thread_cond = SDL_CreateCond();
+    if (sim_thread_cond == NULL)
+    {
+        panicf("Cannot create sim_thread_cond\n");
+        return false;
+    }
+#ifndef HAVE_SDL_THREADS
+    wfi_cond = SDL_CreateCond();
+    if (wfi_cond == NULL)
+    {
+        panicf("Cannot create wfi\n");
+        return false;
+    }
+    wfi_mutex = SDL_CreateMutex();
+    if (wfi_mutex == NULL)
+    {
+        panicf("Cannot create wfi mutex\n");
+        return false;
+    }
+#endif
+    return true;
+}
+
+void sim_kernel_shutdown(void)
+{
+    SDL_RemoveTimer(tick_timer_id);
+#ifndef HAVE_SDL_THREADS
+    SDL_DestroyCond(wfi_cond);
+    SDL_UnlockMutex(wfi_mutex);
+    SDL_DestroyMutex(wfi_mutex);
+#endif
+    enable_irq();
+    while(handlers_pending > 0)
+        SDL_Delay(10);
+
+    SDL_DestroyMutex(sim_irq_mtx);
+    SDL_DestroyCond(sim_thread_cond); 
+}
+
+Uint32 tick_timer(Uint32 interval, void *param)
+{
+    long new_tick;
+
+    (void) interval;
+    (void) param;
+    
+    new_tick = (SDL_GetTicks() - start_tick) / (1000/HZ);
+        
+    while(new_tick != current_tick)
+    {
+        sim_enter_irq_handler();
+
+        /* Run through the list of tick tasks - increments tick
+         * on each iteration. */
+        call_tick_tasks();
+
+        sim_exit_irq_handler();
+    }
+    
+    return interval;
+}
+
+void tick_start(unsigned int interval_in_ms)
+{
+    if (!sim_kernel_init())
+    {
+        panicf("Could not initialize kernel!");
+        exit(-1);
+    }
+
+    if (tick_timer_id != NULL)
+    {
+        SDL_RemoveTimer(tick_timer_id);
+        tick_timer_id = NULL;
+    }
+    else
+    {
+        start_tick = SDL_GetTicks();
+    }
+
+    tick_timer_id = SDL_AddTimer(interval_in_ms, tick_timer, NULL);
+#ifndef HAVE_SDL_THREADS
+    SDL_LockMutex(wfi_mutex);
+#endif
+}
+
+#ifndef HAVE_SDL_THREADS
+void wait_for_interrupt(void)
+{
+    /* the exit may come at any time, during the CondWait or before,
+     * so check it twice */
+    SDL_CondWait(wfi_cond, wfi_mutex);
+}
+#endif
