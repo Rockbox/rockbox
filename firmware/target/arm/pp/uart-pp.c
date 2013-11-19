7e4fc0de59302c0efb55aba08f6bd18532c27fe1
andypotter <liveboxandy@gmail.com>
2013-11-19 21:04:43 +0000

    Add IAP support to iPod Mini 1G.
    Tested on iPod Mini 1G.
    
    Change-Id: I67ac9b7ed84c34533107136d0aa72e5ce3bcc5bc

--- firmware/target/arm/pp/uart-pp.c
+++ firmware/target/arm/pp/uart-pp.c
@@ -48,9 +48,9 @@ void serial_setup (void)
 {
     int tmp;
 
-#if defined(IPOD_COLOR) || defined(IPOD_4G) || defined(IPOD_MINI2G)
+#if defined(IPOD_COLOR) || defined(IPOD_4G) || defined(IPOD_MINI) || defined(IPOD_MINI2G)
 
-    /* Route the Tx/Rx pins.  4G Ipod, ser1, dock connector */
+    /* Route the Tx/Rx pins.  4G Ipod, MINI & MINI2G ser1, dock connector */
     GPIO_CLEAR_BITWISE(GPIOD_ENABLE, 0x6);
     GPIO_CLEAR_BITWISE(GPIOD_OUTPUT_EN, 0x6);
 
