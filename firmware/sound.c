d09ea916028d41eca7a73894ba24ad8ea12d584a
PurlingNayuki <cyq.yzfl@gmail.com>
2013-12-21 21:33:54 +0800

    Implement volume limiter feature
    Add a "volume limit" parameter to the configuration file. Each time when
    setvol() is excuted, Rockbox will check if the
    global_settings.volume value larger than global_settings.volume_limit.
    If larger, take the value of volume_limit instead. The volume_limit
    argument shares the same preference values with volume argument. Also
    implement a AUDIOHW_SETTING_CLN() macro to clone a AUDIOHW_SETTING item
    from an existing one. Bumped PLUGIN_API_VERSION by 1 as well.
    
    Change-Id: I8ca9bc6ea6e617b48fc346aae5f2a0a1d259ae36

--- firmware/sound.c
+++ firmware/sound.c
@@ -136,6 +136,8 @@ static struct
 #endif
 } sound_prescaler;
 
+static short current_volume, volume_limit;
+
 #if defined(AUDIOHW_HAVE_BASS) || defined (AUDIOHW_HAVE_TREBLE) \
         || defined(AUDIOHW_HAVE_EQ)
 #define TONE_PRESCALER
@@ -217,12 +219,25 @@ void sound_set_volume(int value)
     if (!audio_is_initialized)
         return;
 
+    if (value > volume_limit)
+        value = volume_limit;
+    current_volume = value;
 #if defined(AUDIOHW_HAVE_CLIPPING)
     audiohw_set_volume(value);
 #else
     sound_prescaler.volume = sound_value_to_cb(SOUND_VOLUME, value);
     set_prescaled_volume();
 #endif
+}
+
+void sound_set_volume_limit(int value)
+{
+    if (!audio_is_initialized)
+        return;
+
+    if (current_volume > value)
+        sound_set_volume(value);
+    volume_limit = value;
 }
 
 void sound_set_balance(int value)
