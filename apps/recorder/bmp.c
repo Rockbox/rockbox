Rockbox/rockbox

commit a62bd9282e8cebe4e34c1930da04670695f8974b

Fix warning and re-order code a bit.
Change-Id: I1d733b3d0bd17cc541218f9a2b7712712ad314dd

apps/recorder/bmp.c
@@ -718,11 +718,11 @@ int read_bmp_fd(int fd,
 
       case 32:
         if (compression == 3) { /* BI_BITFIELDS */
-            bool found;
+            bool found = false;
             int i, j;
 
             /* (i == 0) is 15bit, (i == 1) is 16bit, (i == 2) is 32bit */
-            for (i = 0; i < ARRAY_SIZE(bitfields); i++) {
+            for (i = 0; i < ARRAY_SIZE(bitfields) && !found; i++) {
                 /* for 15bpp and higher numcolors has the number of color masks */
                 for (j = 0; j < numcolors; j++) {
                     if (!rgbcmp(&palette[j], &bitfields[i][j])) {
@@ -732,16 +732,13 @@ int read_bmp_fd(int fd,
                         break;
                     }
                 }
-                if (found) {
-                    if (i == 0) /* 15bit */
-                        depth = 15;
-                    else if (i == 3) /* 32bit, ABGR bitmap */
-                        order = ABGR;
-                    break;
-                }
             }
-            if (found)
-                break;
+            if (found) {
+                if (i == 0) /* 15bit */
+                    depth = 15;
+                else if (i == 3) /* 32bit, ABGR bitmap */
+                    order = ABGR;
+            }
         }   /* else fall through */
 
       default:

commit 9f878b105dbb51e18adfc3023adc09bc6f61fad3

Fix errors introduced by a62bd92.
Change-Id: I3f65ca5347d08e1b77196c73ccb3a0951f14c850

apps/recorder/bmp.c
@@ -721,7 +721,7 @@ int read_bmp_fd(int fd,
             bool found = false;
             int i, j;
 
-            /* (i == 0) is 15bit, (i == 1) is 16bit, (i == 2) is 32bit */
+            /* (i == 0) is 15bit, (i == 1) is 16bit, (i == {2,3}) is 32bit */
             for (i = 0; i < ARRAY_SIZE(bitfields) && !found; i++) {
                 /* for 15bpp and higher numcolors has the number of color masks */
                 for (j = 0; j < numcolors; j++) {
@@ -734,10 +734,11 @@ int read_bmp_fd(int fd,
                 }
             }
             if (found) {
-                if (i == 0) /* 15bit */
+                if (i == 1) /* 15bit */
                     depth = 15;
-                else if (i == 3) /* 32bit, ABGR bitmap */
+                else if (i == 4) /* 32bit, ABGR bitmap */
                     order = ABGR;
+                break;
             }
         }   /* else fall through */
 