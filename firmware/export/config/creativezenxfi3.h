ebb7057bcff9023caaa53648240144eb9f1539e2
Amaury Pouly <amaury.pouly@gmail.com>
2014-02-22 20:41:32 +0100

    zenxfi3: enable headphone detection
    Change-Id: If45e71248c1572afc0be43f49fb9c78736ee0dae

--- firmware/export/config/creativezenxfi3.h
+++ firmware/export/config/creativezenxfi3.h
@@ -89,6 +89,7 @@
 #define HAVE_SW_TONE_CONTROLS
 
 #define CONFIG_KEYPAD CREATIVE_ZENXFI3_PAD
+#define HAVE_HEADPHONE_DETECTION
 
 /* Define this to enable morse code input */
 #define HAVE_MORSE_INPUT
