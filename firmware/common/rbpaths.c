Rockbox/rockbox

commit 731072de7acfbbeacd992eea1274bc8f35a42163

application: Remove trailing slashes from the input in opendir().
This is needed for readdir() and dir_get_info() to work properly because
they depend on hash/path matching.

Change-Id: Ibd23def10eebabd76cdddac2739a5274159e88a9

firmware/common/rbpaths.c
@@ -341,10 +341,14 @@ DIR* app_opendir(const char *_name)
         return NULL;
 
     struct __dir *this = (struct __dir*)buf;
-    /* definitely fits due to strlen() */
-    strcpy(this->path, name);
-
-    this->dir = opendir(name);
+    /* carefully remove any trailing slash from the input, so that
+     * hash/path matching in readdir() works properly */
+    while (name[name_len-1] == '/')
+        name_len -= 1;
+    /* strcpy cannot be used because of trailing slashes */
+    memcpy(this->path, name, name_len);
+    this->path[name_len] = 0;
+    this->dir = opendir(this->path);
 
     if (!this->dir)
     {

commit 6b01da8bed7f5a49f601021bdf62267ff7585c4c

application: fix opendir() for "/" directory.
"/" couldn't be opened. This regression was introduced by 731072d. Trailing
slash should not be stripped for the root directory.

Change-Id: Idd2d9b1f33e7be789bcf432e586e6715eb4152f4

firmware/common/rbpaths.c
@@ -343,7 +343,7 @@ DIR* app_opendir(const char *_name)
     struct __dir *this = (struct __dir*)buf;
     /* carefully remove any trailing slash from the input, so that
      * hash/path matching in readdir() works properly */
-    while (name[name_len-1] == '/')
+    while (name[name_len-1] == '/' && name_len > 1)
         name_len -= 1;
     /* strcpy cannot be used because of trailing slashes */
     memcpy(this->path, name, name_len);