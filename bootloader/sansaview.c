0eae33c60a6fb8bea24ec7bb867da8aba33ea04b
Szymon Dziok <b0hoon@o2.pl>
2014-03-27 23:31:04 +0000

    Sansa View: replace real bootloader with a simple test code.
    After placing the firmware.mi4 file in the root dir of the player in UMS mode of
    the OF, Sansa should do stupid blinking with the backlight and buttonlight
    alternately. Recovering from this state is possible through the recovery mode
    (see Wiki), by putting an original copy of the firmware.mi4.
    
    Change-Id: Ia913442b97e8c405f55c4676b9a2bf0b1b1d05d6

---
+++ bootloader/sansaview.c
@@ -0,0 +1,65 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2014 by Szymon Dziok
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+/*
+SANSA VIEW: TESTING CODE
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include "config.h"
+#include "inttypes.h"
+#include "cpu.h"
+#include "system.h"
+#include "lcd.h"
+#include "kernel.h"
+#include "thread.h"
+#include "storage.h"
+#include "fat.h"
+#include "disk.h"
+#include "font.h"
+#include "backlight.h"
+#include "backlight-target.h"
+#include "button.h"
+#include "panic.h"
+#include "power.h"
+#include "file.h"
+#include "common.h"
+
+void main(void)
+{
+    system_init();
+    kernel_init();
+    disable_irq();
+
+    while(1)
+    {
+       _backlight_on();
+       _buttonlight_off();
+       sleep(HZ/4);
+       _backlight_off();
+       _buttonlight_on();
+       sleep(HZ/4);
+    }
+}
