e4df0edd8a549857caf13641641228d5eebaac4e
Franklin Wei <frankhwei536@gmail.com>
2014-04-20 12:15:11 -0400

    Made the simulation more realistic and implemented button support
    Change-Id: I30dfebea443eb31923fb426f9d16e48fc1c6b083

--- apps/plugins/rockphys.c
+++ apps/plugins/rockphys.c
@@ -3,12 +3,14 @@
 #include <stdlib.h>
 #include "lib/pluginlib_actions.h"
 #define KEY_QUIT PLA_CANCEL
-#define KEY_UP PLA_UO
+#define KEY_UP PLA_UP
 #define KEY_DOWN PLA_DOWN
 #define KEY_LEFT PLA_LEFT
 #define KEY_RIGHT PLA_RIGHT
 #define BALL_SIZE 2
-#define NUM_BALLS 128
+#define NUM_BALLS 1
+#define MANUAL_SPEED 1
+static const struct button_mapping *plugin_contexts[] = { pla_main_ctx };
 void pxl_on(int x, int y)
 {
   rb->lcd_set_foreground(LCD_WHITE);
@@ -23,12 +25,11 @@ struct ball {
   double x, y, dx, dy, friction, elasticity;
 };
 struct ball balls[NUM_BALLS];
+struct ball next_world[NUM_BALLS];
 void step(struct ball *b)
-{ 
-  double gravity=1;
+{
+  const double gravity=.5;
   int oldx=b->x, oldy=b->y;
-  /* Apply gravity */
-  b->dy+=gravity;
   /* Find new position */
   b->x+=b->dx;
   b->y+=b->dy;
@@ -47,6 +48,8 @@ void step(struct ball *b)
       b->dx*=b->friction;
       b->y+=2*b->dy;
     }
+  if(b->y<=LCD_HEIGHT-BALL_SIZE-gravity)
+    b->dy+=gravity;
   /* TODO: Calculate collisions with other balls!!! */
   pxl_off(oldx, oldy);
   pxl_on(b->x, b->y);
@@ -58,6 +61,7 @@ signed int rand_range(int min, int max)
 enum plugin_status plugin_start(const void* parameter)
 {
   (void)parameter;
+  rb->srand((unsigned int)rb->current_tick+(unsigned int)parameter);
   rb->lcd_clear_display();
   rb->lcd_puts(0,0,"RockPhys v0.1-alpha");
   rb->lcd_puts(0,1,"(C) Franklin Wei");
@@ -76,6 +80,50 @@ enum plugin_status plugin_start(const void* parameter)
     }
   for(;;)
     {
+      int button;
+      button = pluginlib_getaction(0, plugin_contexts,
+				   ARRAYLEN(plugin_contexts));
+      switch(button)
+	{
+	case 0:
+	  break;
+	case KEY_QUIT:
+	  return PLUGIN_OK;
+	case KEY_UP:
+	  {
+	    for(int i=0;i<NUM_BALLS;++i)
+	      {
+		balls[i].dy-=MANUAL_SPEED;
+	      }
+	    break;
+	  }
+	case KEY_LEFT:
+	  {
+	    for(int i=0;i<NUM_BALLS;++i)
+	      {
+		balls[i].dx-=MANUAL_SPEED;
+	      }
+	    break;
+	  }
+	case KEY_RIGHT:
+	  {
+	    for(int i=0;i<NUM_BALLS;++i)
+	      {
+		balls[i].dx+=MANUAL_SPEED;
+	      }
+	    break;
+	  }
+	case KEY_DOWN:
+	  {
+	    for(int i=0;i<NUM_BALLS;++i)
+	      {
+		balls[i].dy+=MANUAL_SPEED;
+	      }
+	    break;
+	  }
+	default:
+	  break;
+	}
       for(int i=0;i<NUM_BALLS;++i)
 	{
 	  step(&balls[i]);
