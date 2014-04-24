93d39c04face29a5c2453d6da86fc5354ccdb494
Michael Sevakis <jethead71@rockbox.org>
2014-04-24 04:09:18 -0400

    Add multi-reader, single-writer locks to kernel.
    Any number of readers may be in the critical section at a time and writers
    are mutually exclusive to all other threads. They are a better choice when
    data is rarely modified but often read and multiple threads can safely
    access it for reading.
    
    Priority inheritance is fully implemented along with other changes to the
    kernel to fully support it on multiowner objects. Some tweaking to other
    files happened since the implementation itself is also cleaner.
    
    This includes a typed bitarray (bitarray.h) that is used extensively in
    my new filesystem code (I mean very, very extensively). It found use here
    too.
    
    This patch doesn't add anything to firmware/SOURCES. There is no reason
    to do so until other code is pushed that uses them.
    
    This code is tested and appears to work properly.
    
    Change-Id: Idbe43dcd9170358e06d48d00f1c69728ff45b0e3

---
+++ firmware/kernel/include/mrsw_lock.h
@@ -0,0 +1,69 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2014 by Michael Sevakis
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#ifndef MRSW_LOCK_H
+#define MRSW_LOCK_H
+
+/* MRSW access policy (could become a setting in mrsw_init()) */
+#define MRSW_POL_WRT  0 /* Grants preferential access to writers, blocks
+                           further readers if any writers are waiting */
+#define MRSW_POL_RR   1 /* Grants access to threads in round-robin fashion;
+                           single writers or runs of readers */
+
+#ifdef HAVE_PRIORITY_SCHEDULING
+#define MRSW_ACC_POL MRSW_POL_RR
+#else
+#define MRSW_ACC_POL MRSW_POL_WRT /* Right now, didn't bother with RR */
+#endif
+
+/* Multi-reader, single-writer object that allows mutltiple readers or a
+ * single writer thread access to a critical section.
+ *
+ * Readers and writers, as with a mutex, may claim the lock recursively for
+ * the same type of access they were already granted.
+ *
+ * Writers are trivially granted read access by ignoring the request; the
+ * object is not changed.
+ *
+ * Reader promotion to writer is NOT supported and a reader attempting write
+ * access will result in a deadlock. That could change but for now, be warned.
+ *
+ * Full priority inheritance is implemented.
+ */
+struct mrsw_lock
+{
+    int volatile count; /* rd/wr counter; >0 = reader(s), <0 = writer */
+    struct thread_entry *queue;
+#if MRSW_ACC_POL == MRSW_POL_WRT
+    struct thread_entry *reader0;
+    struct thread_entry *writer0;
+#endif
+    struct blocker_splay splay; /* priority inheritance info
+                                   for waiters */
+    IF_COP( struct corelock cl; )
+};
+
+void mrsw_init(struct mrsw_lock *mrsw);
+void mrsw_read_acquire(struct mrsw_lock *mrsw);
+void mrsw_read_release(struct mrsw_lock *mrsw);
+void mrsw_write_acquire(struct mrsw_lock *mrsw);
+void mrsw_write_release(struct mrsw_lock *mrsw);
+
+#endif /* MRSW_LOCK_H */
