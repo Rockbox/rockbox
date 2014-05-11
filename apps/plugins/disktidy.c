ac104811197ed989861215ef30bd864474dcb4e2
Richard Burke <rich.g.burke@gmail.com>
2014-05-11 15:45:22 +0200

    Fixed disktidy bug and added a couple of new features to disktidy.
    The following updates were made to disktidy:
    - Fixed bug FS#12825. disktidy now checks subdirectories again for files to delete.
    - Once disktidy finishes a run it now returns to it's main menu rather than exiting.
    - Added "Last Run Stats" view to disktidy. This shows how many files and directories
      were deleted in the last run as well as the total size of those files, the length of
      time the run took and when the run took place (for players with RTC).
    - Added "Playback Control" option to disktidy main menu.
    
    Change-Id: I9b7d6d5d08aef2b5f85fb63fcd2ec60f1c1ec2e0

--- apps/plugins/disktidy.c
+++ apps/plugins/disktidy.c
@@ -18,12 +18,21 @@
  * KIND, either express or implied.
  *
  ****************************************************************************/
+
 #include "plugin.h"
 #include "errno.h"
+#include "lib/playback_control.h"
+#include "lib/display_text.h"
 
-
-static int removed = 0; /* number of items removed */
-static bool user_abort;
+struct run_statistics {
+    int files_removed; /* Number of files removed */
+    int dirs_removed; /* Number of directories removed */
+    int run_duration; /* Duration of last run in seconds */
+    double removed_size; /* Size of items removed */
+#if CONFIG_RTC
+    struct tm last_run_time; /* Last time disktidy was run */
+#endif
+};
 
 struct tidy_type {
     char filestring[64];
@@ -33,12 +42,14 @@ struct tidy_type {
     bool remove;
 } tidy_types[64];
 
+static struct run_statistics run_stats;
 static size_t tidy_type_count;
-
-bool tidy_loaded_and_changed = false;
+static bool user_abort;
+static bool tidy_loaded_and_changed = false;
 
 #define DEFAULT_FILES PLUGIN_APPS_DATA_DIR "/disktidy.config"
 #define CUSTOM_FILES  PLUGIN_APPS_DATA_DIR "/disktidy_custom.config"
+#define LAST_RUN_STATS_FILE PLUGIN_APPS_DATA_DIR "/disktidy.stats"
 
 static void add_item(const char* name, int index)
 {
@@ -135,6 +146,127 @@ static void tidy_load_file(const char* file)
     rb->close(fd);
 }
 
+static bool save_run_stats(void)
+{
+    int fd = rb->open(LAST_RUN_STATS_FILE, O_WRONLY|O_CREAT, 0666);
+
+    if (fd < 0) {
+        return false;
+    }
+
+    bool save_success = rb->write(fd, &run_stats,
+        sizeof(struct run_statistics)) > 0;
+
+    rb->close(fd);
+
+    return save_success;
+}
+
+static bool load_run_stats(void)
+{
+    int fd = rb->open(LAST_RUN_STATS_FILE, O_RDONLY);
+
+    if (fd < 0) {
+        return false;
+    }
+
+    bool load_success = rb->read(fd, &run_stats,
+        sizeof(struct run_statistics)) == sizeof(struct run_statistics);
+
+    rb->close(fd);
+
+    return load_success;
+}
+
+static enum plugin_status display_run_stats(void)
+{
+    if (!load_run_stats()) {
+        rb->splash(HZ * 2, "Unable to load last run stats");
+        return PLUGIN_OK;
+    }
+
+#if CONFIG_RTC
+    static const char *months[] = {
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    };
+#endif
+    static const char *size_units[] = {
+        "B", "KB", "MB", "GB", "TB", "PB"
+    };
+
+    int magnitude = 0;
+    double rm_size = run_stats.removed_size;
+
+    while (rm_size >= 1000) {
+        rm_size /= 1024;
+        magnitude++;
+    }
+
+    char total_removed[8];
+    rb->snprintf(total_removed, sizeof(total_removed), "%d",
+                 run_stats.files_removed + run_stats.dirs_removed);
+
+    char files_removed[8];
+    rb->snprintf(files_removed, sizeof(files_removed), "%d",
+                 run_stats.files_removed);
+
+    char dirs_removed[8];
+    rb->snprintf(dirs_removed, sizeof(dirs_removed), "%d",
+                 run_stats.dirs_removed);
+
+    char removed_size[9];
+    rb->snprintf(removed_size, sizeof(removed_size), "%d.%d%s",
+                 (int)rm_size, (int)((rm_size - (int)rm_size) * 100),
+                 size_units[magnitude]);
+
+    char run_time[9];
+    rb->snprintf(run_time, sizeof(run_time), "%02d:%02d:%02d",
+                 run_stats.run_duration / 3600, run_stats.run_duration / 60,
+                 run_stats.run_duration % 60);
+
+#if CONFIG_RTC
+    char last_run[18];
+    rb->snprintf(last_run, sizeof(last_run), "%02d:%02d %d/%s/%d",
+                 run_stats.last_run_time.tm_hour,
+                 run_stats.last_run_time.tm_min, run_stats.last_run_time.tm_mday,
+                 months[run_stats.last_run_time.tm_mon],
+                 2000 + (run_stats.last_run_time.tm_year % 100));
+#endif
+
+    char* last_run_text[] = {
+        "Last Run Stats" , ""           , "",
+        "Total Removed: ", total_removed, "",
+        "Files Removed: ", files_removed, "",
+        "Dirs Removed: " , dirs_removed , "",
+        "Removed Size: " , removed_size , "",
+        "Run Time: "     , run_time     , "",
+#if CONFIG_RTC
+        "Run: "          , last_run
+#endif
+    };
+
+    static struct style_text display_style[] = {
+        { 0, C_ORANGE | TEXT_CENTER },
+        { 3, C_BLUE },
+        { 6, C_BLUE },
+        { 9, C_BLUE },
+        { 12, C_BLUE },
+        { 15, C_BLUE },
+#if CONFIG_RTC
+        { 18, C_BLUE },
+#endif
+        LAST_STYLE_ITEM
+    };
+
+    if (display_text(ARRAYLEN(last_run_text), last_run_text,
+                     display_style, NULL, true)) {
+        return PLUGIN_USB_CONNECTED;
+    }
+
+    return PLUGIN_OK;
+}
+
 static bool match(struct tidy_type *tidy_type, const char *string, int len)
 {
     char *pattern = tidy_type->filestring;
@@ -168,7 +300,8 @@ static void tidy_lcd_status(const char *name)
     rb->lcd_puts(0, 0, "Working ...");
     rb->lcd_puts(0, 1, name);
 #ifdef HAVE_LCD_BITMAP
-    rb->lcd_putsf(0, 2, "Cleaned up %d items", removed);
+    rb->lcd_putsf(0, 2, "Cleaned up %d items",
+        run_stats.files_removed + run_stats.dirs_removed);
 #endif
     rb->lcd_update();
 }
@@ -205,7 +338,7 @@ static void tidy_path_remove_entry(char *path, int old_path_length, int *path_le
 }
 
 /* path is assumed to be array of size MAX_PATH. */
-static enum plugin_status tidy_clean(char *path, int *path_length, bool rmdir)
+static enum plugin_status tidy_clean(char *path, int *path_length, bool rm_all)
 {
     int old_path_length = *path_length;
 
@@ -215,6 +348,7 @@ static enum plugin_status tidy_clean(char *path, int *path_length, bool rmdir)
     if (!dir)
         return PLUGIN_ERROR;
 
+    enum plugin_status disktidy_status = PLUGIN_OK;
     struct dirent *entry;
     while ((entry = rb->readdir(dir)))
     {
@@ -235,8 +369,38 @@ static enum plugin_status tidy_clean(char *path, int *path_length, bool rmdir)
         rb->yield();
 
         struct dirinfo info = rb->dir_get_info(dir, entry);
-        if (!rmdir && !tidy_remove_item(entry->d_name, info.attribute))
+
+        bool remove = tidy_remove_item(entry->d_name, info.attribute) || rm_all;
+
+        if (info.attribute & ATTR_DIRECTORY) {
+            if (rb->strcmp(entry->d_name, ".") == 0 ||
+                rb->strcmp(entry->d_name, "..") == 0) {
+                continue;
+            }
+
+            if (!remove) {
+                /* get absolute path, returns an error if path is too long */
+                if (!tidy_path_append_entry(path, entry, path_length)) {
+                    continue; /* silent error */
+                }
+
+                /* If this is a directory that doesn't need to be deleted then
+                   check it for files/directores that may need to deleted */
+                disktidy_status = tidy_clean(path, path_length, false);
+
+                tidy_path_remove_entry(path, old_path_length, path_length);
+
+                if (disktidy_status != PLUGIN_OK) {
+                    break;
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        if (!remove) {
             continue;
+        }
 
         /* get absolute path, returns an error if path is too long */
         if(!tidy_path_append_entry(path, entry, path_length))
@@ -244,34 +408,49 @@ static enum plugin_status tidy_clean(char *path, int *path_length, bool rmdir)
 
         if (info.attribute & ATTR_DIRECTORY)
         {
-            /* dir ignore "." and ".." */
-            if (rb->strcmp(entry->d_name, ".") && rb->strcmp(entry->d_name, ".."))
-                tidy_clean(path, path_length, true);
+            /* Remove this directory and all files/directores it contains */
+            disktidy_status = tidy_clean(path, path_length, true);
+
+            if (disktidy_status != PLUGIN_OK) {
+                break;
+            }
+
+            run_stats.removed_size += info.size;
         }
         else
         {
-            removed++;
+            run_stats.files_removed++;
+            run_stats.removed_size += info.size;
             rb->remove(path);
         }
 
         /* restore path */
         tidy_path_remove_entry(path, old_path_length, path_length);
     }
+
     rb->closedir(dir);
 
-    if (rmdir)
-    {
-        removed++;
+    if (rm_all && disktidy_status == PLUGIN_OK) {
         rb->rmdir(path);
+        run_stats.dirs_removed++;
     }
 
-    return PLUGIN_OK;
+    return disktidy_status;
 }
 
 static enum plugin_status tidy_do(void)
 {
     /* clean disk and display num of items removed */
     char path[MAX_PATH];
+
+    run_stats.files_removed = 0;
+    run_stats.dirs_removed = 0;
+    run_stats.removed_size = 0;
+    long start_time = *rb->current_tick;
+
+#if CONFIG_RTC
+    run_stats.last_run_time = *rb->get_time();
+#endif
 
 #ifdef HAVE_ADJUSTABLE_CPU_FREQ
     rb->cpu_boost(true);
@@ -285,6 +464,9 @@ static enum plugin_status tidy_do(void)
     rb->cpu_boost(false);
 #endif
 
+    run_stats.run_duration = (*rb->current_tick - start_time) / HZ;
+    save_run_stats();
+
     if (status == PLUGIN_OK)
     {
         rb->lcd_clear_display();
@@ -293,8 +475,11 @@ static enum plugin_status tidy_do(void)
             rb->splash(HZ, "User aborted");
             rb->lcd_clear_display();
         }
-        rb->splashf(HZ*2, "Cleaned up %d items", removed);
+        rb->lcd_update();
+        rb->splashf(HZ*2, "Cleaned up %d items",
+            run_stats.files_removed + run_stats.dirs_removed);
     }
+
     return status;
 }
 
@@ -349,30 +534,51 @@ static int list_action_callback(int action, struct gui_synclist *lists)
     return ACTION_REDRAW;
 }
 
-static void tidy_lcd_menu(void)
+static enum plugin_status tidy_lcd_menu(void)
 {
+    enum plugin_status disktidy_status = PLUGIN_OK;
+    bool exit = false;
     int selection = 0;
     struct simplelist_info list;
 
     MENUITEM_STRINGLIST(menu, "Disktidy Menu", NULL, "Start Cleaning",
-                        "Files to Clean", "Quit");
+                              "Files to Clean", "Last Run Stats",
+                              "Playback Control", "Quit");
 
-    for(;;)
-        switch(rb->do_menu(&menu, &selection, NULL, false))
-        {
-        default:
-            user_abort = true;
-        case 0:
-            return; /* start cleaning */
+    while (!exit && disktidy_status == PLUGIN_OK) {
+        switch(rb->do_menu(&menu, &selection, NULL, false)) {
+            case 0:
+                disktidy_status = tidy_do();
+                break;
+            case 1:
+                rb->simplelist_info_init(&list, "Files to Clean",
+                    tidy_type_count, NULL);
+                list.get_icon = get_icon;
+                list.get_name = get_name;
+                list.action_callback = list_action_callback;
+                rb->simplelist_show_list(&list);
+                break;
+            case 2:
+                if (rb->file_exists(LAST_RUN_STATS_FILE)) {
+                    disktidy_status = display_run_stats();
+                } else {
+                    rb->splash(HZ * 2, "Run disktidy to generate stats");
+                }
 
-        case 1:
-            rb->simplelist_info_init(&list, "Files to Clean", tidy_type_count, NULL);
-            list.get_icon = get_icon;
-            list.get_name = get_name;
-            list.action_callback = list_action_callback;
-            rb->simplelist_show_list(&list);
-            break;
+                break;
+            case 3:
+                if (playback_control(NULL)) {
+                    disktidy_status = PLUGIN_USB_CONNECTED;
+                }
+
+                break;
+            default:
+                exit = true;
+                break;
         }
+    }
+
+    return disktidy_status;
 }
 
 /* Creates a file and writes information about what files to
@@ -400,14 +606,18 @@ enum plugin_status plugin_start(const void* parameter)
 
     tidy_load_file(DEFAULT_FILES);
     tidy_load_file(CUSTOM_FILES);
+
     if (tidy_type_count == 0)
     {
         rb->splash(3*HZ, "Missing disktidy.config file");
         return PLUGIN_ERROR;
     }
-    tidy_lcd_menu();
-    if (tidy_loaded_and_changed)
-        save_config();
 
-    return user_abort ? PLUGIN_OK : tidy_do();
+    enum plugin_status disktidy_status = tidy_lcd_menu();
+
+    if (tidy_loaded_and_changed) {
+        save_config();
+    }
+
+    return disktidy_status;
 }
