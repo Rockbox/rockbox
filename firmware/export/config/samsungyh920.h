6aef81a0f653ecd3cb77635961570d3333b12f77
Szymon Dziok <b0hoon@o2.pl>
2014-06-01 14:49:10 +0000

    Samsung YH-920: enable inverting of colours on the lcd.
    Change-Id: Iaf70e9e854a52c6ddd12bc81e59580dbc3938888

--- firmware/export/config/samsungyh920.h
+++ firmware/export/config/samsungyh920.h
@@ -67,7 +67,7 @@
 /* todo #define HAVE_LCD_FLIP */
 
 /* define this if you can invert the colours on your LCD */
-/* todo #define HAVE_LCD_INVERT */
+#define HAVE_LCD_INVERT
 
 /* put the lcd frame buffer in IRAM */
 /* #define IRAM_LCDFRAMEBUFFER IDATA_ATTR */
