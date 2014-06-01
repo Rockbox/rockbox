8326f173f99ed8d37f97375bb380e5575267ee42
Szymon Dziok <b0hoon@o2.pl>
2014-06-01 19:34:58 +0000

    Samsung YH-920: properly implement and enable lcd flipping.
    Change-Id: I0a8609c9b4849332a97a125d186f339a61e4d584

--- firmware/target/arm/samsung/yh920/lcd-yh920.c
+++ firmware/target/arm/samsung/yh920/lcd-yh920.c
@@ -106,17 +106,17 @@ void lcd_set_invert_display(bool yesno)
 /* turn the display upside down (call lcd_update() afterwards) */
 void lcd_set_flip(bool yesno)
 {
-    if (yesno) 
+    if (yesno)
     {
         lcd_send_cmd(LCD_CNTL_COLUMN_ADDRESS_DIR | 1);
-        lcd_send_cmd(LCD_CNTL_COMMON_OUTPUT_STATUS | 0);
-        lcd_write_reg_ex(LCD_CNTL_DUTY_SET, 0x20, 0);
+        lcd_send_cmd(LCD_CNTL_COMMON_OUTPUT_STATUS | 1);
+        lcd_write_reg_ex(LCD_CNTL_DUTY_SET, 0x1f, 1);
     }
     else
     {
         lcd_send_cmd(LCD_CNTL_COLUMN_ADDRESS_DIR | 0);
-        lcd_send_cmd(LCD_CNTL_COMMON_OUTPUT_STATUS | 1);
-        lcd_write_reg_ex(LCD_CNTL_DUTY_SET, 0x20, 1);
+        lcd_send_cmd(LCD_CNTL_COMMON_OUTPUT_STATUS | 0);
+        lcd_write_reg_ex(LCD_CNTL_DUTY_SET, 0x1f, 0);
     }
 }
 
