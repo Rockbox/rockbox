4596bf7ef15c2a94972240b4e03d74224fd99b10
Franklin Wei <frankhwei536@gmail.com>
2014-08-22 17:47:35 -0400

    Chip8 bugfix
    Change-Id: Ibe1d7a98953700c0f7f6f655de060b36795e56e9

--- apps/plugins/chip8.c
+++ apps/plugins/chip8.c
@@ -1299,7 +1299,7 @@ static byte chip8_keymap[16];
 static unsigned long starttimer; /* Timer value at the beginning */
 static unsigned long cycles; /* Number of update cycles (50Hz) */
 
-#if (CONFIG_PLATFORM & PLATFORM_NATIVE)
+#if (CONFIG_CODEC != SWCODEC) && !defined(SIMULATOR)
 static bool is_playing;
 
 /* one frame of bitswapped mp3 data */
@@ -1315,7 +1315,6 @@ static unsigned char beep[]={255,
  20,100, 18, 96, 41, 96, 78,102,  7,201,122, 76,119, 20,137, 37,177, 15,132,224,
  20, 17,191, 67,147,187,116,211, 41,169, 63,172,182,186,217,155,111,140,104,254,
 111,181,184,144, 17,148, 21,101,166,227,100, 86, 85, 85, 85}; 
-
 /* callback to request more mp3 data */
 static void callback(const void** start, size_t* size)
 {
@@ -1329,7 +1328,7 @@ static void callback(const void** start, size_t* size)
 /****************************************************************************/
 static void chip8_sound_on (void) 
 {
-#if (CONFIG_PLATFORM & PLATFORM_NATIVE)
+#if(CONFIG_CODEC != SWCODEC) && !defined(SIMULATOR)
     if (!is_playing)
         rb->mp3_play_pause(true); /* kickoff audio */
 #endif
@@ -1340,7 +1339,7 @@ static void chip8_sound_on (void)
 /****************************************************************************/
 static void chip8_sound_off (void) 
 { 
-#if (CONFIG_PLATFORM & PLATFORM_NATIVE)
+#if (CONFIG_CODEC != SWCODEC) && !defined(SIMULATOR)
     if (!is_playing)
         rb->mp3_play_pause(false); /* pause audio */
 #endif
@@ -1541,8 +1540,7 @@ static bool chip8_run(const char* file)
     rb->lcd_drawrect(CHIP8_X-1,CHIP8_Y-1,CHIP8_LCDWIDTH+2,CHIP8_HEIGHT+2);
 #endif
     rb->lcd_update();
-
-#if (CONFIG_PLATFORM & PLATFORM_NATIVE)
+#if (CONFIG_CODEC != SWCODEC)
     /* init sound */
     is_playing = rb->mp3_is_playing(); /* would we disturb playback? */
     if (!is_playing) /* no? then we can make sound */
@@ -1556,7 +1554,7 @@ static bool chip8_run(const char* file)
     cycles = 0;
     chip8();
 
-#if (CONFIG_PLATFORM & PLATFORM_NATIVE)
+#if (CONFIG_CODEC != SWCODEC) && !defined(SIMULATOR)
     if (!is_playing)
     {   /* stop it if we used audio */
         rb->mp3_play_stop(); /* Stop audio playback */
