b5bf2ed90c7901164398d623804d2be16421e0b6
Benjamin Brown <foolsh.person@gmail.com>
2014-10-13 21:00:47 -0400

    [WIP] Xworld Another World interpreter for Rockbox
    Benjamin Brown, Franklin Wei
    
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Changes:
    
    * Dump of Fabien Sanglard's git hub repository from
    https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter
    * Visualstudio and Xcode dirs removed for rockbox intergration
    * .cpp files renamed to .c
    * remove endian.h, as we'll use rbendian.h
    
    --------------------------------------------------------------------
    Notes:
    
    * Don't even think about compiling this yet, it's not ready
    * http://fabiensanglard.net/anotherWorld_code_review/ a breakdown of
    Fabien Sanglard's "Fabother World"
    
    --------------------------------------------------------------------
    To be done:
    
    * convert c++ to c <60% done>
    * plugin init and destroy <75% done>
    * menu
    * convert input to rockbox PLA <75% done>
    * convert draw and fill calls to native rockbox functions
    <100% done, UNTESTED AND HIGHLY UNLIKELY TO WORK>
    * sound
    
    Change-Id: Ib4193557ff48bc4f874bc1b0def46fb7ef4ec7c4

---
+++ apps/plugins/xworld/serializer.c
@@ -0,0 +1,133 @@
+/* Raw - Another World Interpreter
+ * Copyright (C) 2004 Gregory Montoir
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "serializer.h"
+#include "file.h"
+
+
+ser_Serializer(struct Serializer* c, File *stream, Mode mode, uint8_t *ptrBlock, uint16_t saveVer)
+	: _stream(stream), _mode(mode), _ptrBlock(ptrBlock), _saveVer(saveVer) {
+}
+
+void ser_saveOrLoadEntries(struct Serializer* c, Entry *entry) {
+	debug(DBG_SER, "ser_saveOrLoadEntries() _mode=%d", _mode);
+	_bytesCount = 0;
+	switch (_mode) {
+	case SM_SAVE:
+		saveEntries(entry);
+		break;
+	case SM_LOAD:
+		loadEntries(entry);
+		break;
+	}
+	debug(DBG_SER, "ser_saveOrLoadEntries() _bytesCount=%d", _bytesCount);
+}
+
+void ser_saveEntries(struct Serializer* c, Entry *entry) {
+	debug(DBG_SER, "ser_saveEntries()");
+	for (; entry->type != SET_END; ++entry) {
+		if (entry->maxVer == CUR_VER) {
+			switch (entry->type) {
+			case SET_INT:
+				saveInt(entry->size, entry->data);
+				_bytesCount += entry->size;
+				break;
+			case SET_ARRAY:
+				if (entry->size == ser_SES_INT8) {
+					_stream->write(entry->data, entry->n);
+					_bytesCount += entry->n;
+				} else {
+					uint8_t *p = (uint8_t *)entry->data;
+					for (int i = 0; i < entry->n; ++i) {
+						saveInt(entry->size, p);
+						p += entry->size;
+						_bytesCount += entry->size;
+					}
+				}
+				break;
+			case SET_PTR:
+				_stream->writeUint32BE(*(uint8_t **)(entry->data) - _ptrBlock);
+				_bytesCount += 4;
+				break;
+			case SET_END:
+				break;
+			}
+		}
+	}
+}
+
+void ser_loadEntries(struct Serializer* c, Entry *entry) {
+	debug(DBG_SER, "ser_loadEntries()");
+	for (; entry->type != SET_END; ++entry) {
+		if (_saveVer >= entry->minVer && _saveVer <= entry->maxVer) {
+			switch (entry->type) {
+			case SET_INT:
+				loadInt(entry->size, entry->data);
+				_bytesCount += entry->size;
+				break;
+			case SET_ARRAY:
+				if (entry->size == ser_SES_INT8) {
+					_stream->read(entry->data, entry->n);
+					_bytesCount += entry->n;
+				} else {
+					uint8_t *p = (uint8_t *)entry->data;
+					for (int i = 0; i < entry->n; ++i) {
+						loadInt(entry->size, p);
+						p += entry->size;
+						_bytesCount += entry->size;
+					}
+				}
+				break;
+			case SET_PTR:
+				*(uint8_t **)(entry->data) = _ptrBlock + _stream->readUint32BE();
+				_bytesCount += 4;
+				break;
+			case SET_END:
+				break;
+			}
+		}
+	}
+}
+
+void ser_saveInt(struct Serializer* c, uint8_t es, void *p) {
+	switch (es) {
+	case 1:
+		_stream->writeByte(*(uint8_t *)p);
+		break;
+	case 2:
+		_stream->writeUint16BE(*(uint16_t *)p);
+		break;
+	case 4:
+		_stream->writeUint32BE(*(uint32_t *)p);
+		break;
+	}
+}
+
+void ser_loadInt(struct Serializer* c, uint8_t es, void *p) {
+	switch (es) {
+	case 1:
+		*(uint8_t *)p = _stream->readByte();
+		break;
+	case 2:
+		*(uint16_t *)p = _stream->readUint16BE();
+		break;
+	case 4:
+		*(uint32_t *)p = _stream->readUint32BE();
+		break;
+	}
+}
