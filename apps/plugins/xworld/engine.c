c6a05d239d6596bec33f59969e9e47831d7a327b
Franklin Wei <frankhwei536@gmail.com>
2014-10-13 21:00:47 -0400

    [WIP] Xworld Another World interpreter for Rockbox
    Benjamin Brown, Franklin Wei
    
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Changes:
    
    * Dump of Fabien Sanglard's git hub repository from
    https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter
    * Visualstudio and Xcode dirs removed for rockbox intergration
    * .cpp files renamed to .c
    
    --------------------------------------------------------------------
    Notes:
    
    * Don't even think about compiling this yet, it's not ready
    * http://pastebin.com/h0hccUcc current compile errors for the curious
    * http://fabiensanglard.net/anotherWorld_code_review/ a breakdown of
    Fabien Sanglard's "Fabother World"
    
    --------------------------------------------------------------------
    To be done:
    
    * convert c++ to c <50% done>
    * plugin init and destroy <75% done>
    * menu
    * convert input to rockbox PLA <75% done>
    * convert draw and fill calls to native rockbox functions <75% done>
    * sound
    
    Change-Id: Ib4193557ff48bc4f874bc1b0def46fb7ef4ec7c4

---
+++ apps/plugins/xworld/engine.c
@@ -0,0 +1,188 @@
+/* Raw - Another World Interpreter
+ * Copyright (C) 2004 Gregory Montoir
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "engine.h"
+#include "file.h"
+#include "serializer.h"
+#include "sys.h"
+#include "parts.h"
+
+engine_Engine(System *paramSys, const char *dataDir, const char *saveDir)
+: sys(paramSys), vm(&mixer, &res, &player, &video, sys), mixer(sys), res(&video, dataDir),
+    player(&mixer, &res, sys), video(&res, sys), _dataDir(dataDir), _saveDir(saveDir), _stateSlot(0) {
+}
+
+void engine_run(struct Engine* e) {
+
+    while (!e->sys->input.quit) {
+
+        vm_checkThreadRequests(&e->vm);
+
+        vm_inp_updatePlayer(&e->vm);
+
+        engine_processInput(e);
+
+        vm_hostFrame(&e->vm);
+    }
+
+
+}
+void engine_create(struct System* stub, struct Engine*, const char* dataDir, const char* saveDir)
+{
+    e->sys=stub;
+    e->_dataDir=dataDir;
+    e->_saveDir=saveDir;
+}
+void engine_init() {
+
+
+    //Init system
+    sys_init("Out Of This World");
+
+    video_init(&e->video);
+
+    res.allocMemBlock();
+
+    res.readEntries();
+
+    vm_init(&e->vm);
+
+    //mixer.init();
+
+    //player.init();
+
+    //Init virtual machine, legacy way
+    vm_initForPart(&e->vm, GAME_PART_FIRST); // This game part is the protection screen
+
+
+
+    // Try to cheat here. You can jump anywhere but the VM crashes afterward.
+    // Starting somewhere is probably not enough, the variables and calls return are probably missing.
+    //vm.initForPart(GAME_PART2); // Skip protection screen and go directly to intro
+    //vm.initForPart(GAME_PART3); // CRASH
+    //vm.initForPart(GAME_PART4); // Start directly in jail but then crash
+    //vm.initForPart(GAME_PART5);   //CRASH
+    //vm.initForPart(GAME_PART6);   // Start in the battlechar but CRASH afteward
+    //vm.initForPart(GAME_PART7); //CRASH
+    //vm.initForPart(GAME_PART8); //CRASH
+    //vm.initForPart(GAME_PART9); // Green screen not doing anything
+}
+
+void engine_finish(struct Engine* e) {
+    //player.free();
+    //mixer.free();
+    e->res.freeMemBlock();
+}
+
+void engine_processInput(struct Engine* e) {
+    if (e->sys->input.load) {
+        engine_loadGameState(e, e->_stateSlot);
+        e->sys->input.load = false;
+    }
+    if (e->sys->input.save) {
+        engine_saveGameState(e, e->_stateSlot, "quicksave");
+        e->sys->input.save = false;
+    }
+    if (e->sys->input.fastMode) {
+        e->vm._fastMode = !e->vm._fastMode;
+        e->sys->input.fastMode = false;
+    }
+    if (e->sys->input.stateSlot != 0) {
+        int8_t slot = e->_stateSlot + e->sys->input.stateSlot;
+        if (slot >= 0 && slot < MAX_SAVE_SLOTS) {
+            e->_stateSlot = slot;
+            debug(DBG_INFO, "Current game state slot is %d", _stateSlot);
+        }
+        e->sys->input.stateSlot = 0;
+    }
+}
+
+void engine_makeGameStateName(struct Engine* e, uint8_t slot, char *buf, int sz) {
+    (void) e;
+    rb->snprintf(buf, sz, "raw.s%02d", slot);
+}
+
+void engine_saveGameState(struct Engine* e, uint8_t slot, const char *desc) {
+    char stateFile[20];
+    engine_makeGameStateName(e, slot, stateFile, 20);
+    File f(true);
+    if (!f.open(stateFile, _saveDir, "wb")) {
+        warning("Unable to save state file '%s'", stateFile);
+    } else {
+        // header
+        f.writeUint32BE('AWSV');
+        f.writeUint16BE(Serializer::CUR_VER);
+        f.writeUint16BE(0);
+        char hdrdesc[32];
+        strncpy(hdrdesc, desc, sizeof(hdrdesc) - 1);
+        f.write(hdrdesc, sizeof(hdrdesc));
+        // contents
+        Serializer s(&f, Serializer::SM_SAVE, res._memPtrStart);
+        vm.saveOrLoad(s);
+        res.saveOrLoad(s);
+        video.saveOrLoad(s);
+        player.saveOrLoad(s);
+        mixer.saveOrLoad(s);
+        if (f.ioErr()) {
+            warning("I/O error when saving game state");
+        } else {
+            debug(DBG_INFO, "Saved state to slot %d", _stateSlot);
+        }
+    }
+}
+
+void engine_loadGameState(struct Engine* e, uint8_t slot) {
+    char stateFile[20];
+    engine_makeGameStateName(e, slot, stateFile);
+    File f(true);
+    if (!f.open(stateFile, e->_saveDir, "rb")) {
+        warning("Unable to open state file '%s'", stateFile);
+    } else {
+        uint32_t id = f.readUint32BE();
+        if (id != 'AWSV') {
+            warning("Bad savegame format");
+        } else {
+            // mute
+            player.stop();
+            mixer.stopAll();
+            // header
+            uint16_t ver = f.readUint16BE();
+            f.readUint16BE();
+            char hdrdesc[32];
+            f.read(hdrdesc, sizeof(hdrdesc));
+            // contents
+            Serializer s(&f, Serializer::SM_LOAD, res._memPtrStart, ver);
+            vm.saveOrLoad(s);
+            res.saveOrLoad(s);
+            video.saveOrLoad(s);
+            player.saveOrLoad(s);
+            mixer.saveOrLoad(s);
+        }
+        if (f.ioErr()) {
+            warning("I/O error when loading game state");
+        } else {
+            debug(DBG_INFO, "Loaded state from slot %d", _stateSlot);
+        }
+    }
+}
+
+
+const char* engine_getDataDir(struct Engine* e)
+{
+    return e->_dataDir;
+}
---
+++ apps/plugins/xworld/sys.c
@@ -0,0 +1,111 @@
+#include "plugin.h"
+#include "lib/pluginlib_actions.h"
+#include "sys.h"
+void sys_init(struct System* sys, const char* title)
+{
+    rb->splash(HZ, title);
+    int x, y;
+    memset(&sys->input, 0, sizeof(input));
+}
+void sys_destroy(struct System* sys)
+{
+    rb->splash(HZ, "Exiting...");
+}
+void sys_setPallete(struct System* sys, uint8_t start, uint8_t n, const uint8_t *buf)
+{
+    for(int i=start;i<start+n;++i)
+    {
+        uint8_t c[3];
+        for (int j = 0; j < 3; j++) {
+            uint8_t col = buf[i * 3 + j];
+            c[j] = (col << 2) | (col & 3);
+        }
+        pallete[i]=LCD_RGBPACK(c[0],c[1],c[2]);
+    }
+}
+void sys_copyRect(struct System* sys, uint16_t x, uint16_t y, uint16_t w, uint16_t h, const uint8_t *buf, uint32_t pitch)
+{
+    buf += y * pitch + x;
+    while (height--) {
+        /* one byte gives two pixels */
+        for (int i = 0; i < width / 2; ++i) {
+            uint8_t pix1=*(buf+i)>>4;
+            uint8_t pix2=*(buf+i)&0xF;
+            rb->lcd_set_foreground(sys->pallete[pix1]);
+            rb->lcd_drawpixel(x+i, height);
+            rb->lcd_set_foreground(sys->pallete[pix2]);
+            rb->lcd_drawpixel(x+i+1, height);
+        }
+        p += SCREEN_W;
+        buf += pitch;
+    }
+    rb->lcd_update();
+}
+
+void sys_processEvents(struct System* sys)
+{
+    static const struct button_mapping *plugin_contexts[] = { pla_main_ctx };
+    int button=pluginlib_getaction(0, plugin_contexts, ARRAYLEN(plugin_contexts));
+    if(button&PLA_UP)
+    {
+        sys->input.dirMask|=DIR_UP;
+    }
+    if(button&PLA_DOWN)
+    {
+        sys->input.dirMask|=DIR_DOWN;
+    }
+    if(button&PLA_LEFT)
+    {
+        sys->input.dirMask|=DIR_LEFT;
+    }
+    if(button&PLA_RIGHT)
+    {
+        sys->input.dirMask|=DIR_RIGHT;
+    }
+}
+void sys_sleep(struct System* sys, int32_t duration)
+{
+    (void) sys;
+    rb->sleep(duration/10);
+}
+uint32_t sys_getTimeStamp(struct System* sys)
+{
+    (void) sys;
+    return (uint32_t) *rb->current_tick;
+}
+
+void sys_startAudio(struct System* sys, AudioCallback callback, void *param)
+{
+}
+void sys_stopAudio(struct System* sys)
+{
+}
+uint32_t sys_getOutputSampleRate(struct System* sys)
+{
+    return 44100;
+}
+
+void *sys_addTimer(struct System* sys, uint32_t delay, TimerCallback callback, void *param)
+{
+}
+void sys_removeTimer(struct System* sys, void *timerId)
+{
+}
+
+void *sys_createMutex(struct System* sys)
+{
+}
+void sys_destroyMutex(struct System* sys, void *mutex)
+{
+}
+void sys_lockMutex(struct System* sys, void *mutex)
+{
+}
+void sys_unlockMutex(struct System* sys, void *mutex)
+{
+}
+
+uint8_t* getOffScreenFramebuffer(struct System* sys)
+{
+    return NULL;
+}
