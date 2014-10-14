3735d2ff896b3010686741d7a724ee9796171a63
Franklin Wei <frankhwei536@gmail.com>
2014-10-13 21:00:47 -0400

    [WIP] Xworld Another World interpreter for Rockbox
    Co-conspirators: Franklin Wei, Benjamin Brown
    
    --------------------------------------------------------------------
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Progress:
    
    * The plugin runs pretty nicely (with sound!) on most color targets
    * Keymaps for color LCD targets are complete
    * The manual entry is finished
    * Grayscale/monochrome support is NOT PLANNED
      - the game looks horrible in grayscale! :p
    
    --------------------------------------------------------------------
    Notes:
    
    * The original game strings were built-in to the executable, and
      were copyrighted and could not be used.
    * This port ships with an alternate set of strings by default, but
      can load the "official" strings from a file at runtime.
    
    --------------------------------------------------------------------
    To be done (in descending order of importance):
    
    * vertical stride compatibility                          <30% done>
    * optimization                                           <10% done>
    
    Change-Id: Ib4193557ff48bc4f874bc1b0def46fb7ef4ec7c4

---
+++ apps/plugins/xworld/video_data.h
@@ -0,0 +1,7 @@
+#include "video.h"
+#include "stdint.h"
+extern const uint8_t video_font[];
+
+extern struct StrEntry video_stringsTableEng[141];
+
+extern const struct StrEntry video_stringsTableDemo[];
