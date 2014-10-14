aa671098308b9291c25a7cf0042ad40b89048f00
Benjamin Brown <foolsh.person@gmail.com>
2014-10-13 21:00:47 -0400

    (WIP) Initial commit of Xworld, Another World interpreter for Rockbox
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Changes:
    
    * Dump of Fabien Sanglard's git hub repository from
    https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter
    * Visualstudio and Xcode dirs removed for rockbox intergration
    * .cpp files renamed to .c
    
    --------------------------------------------------------------------
    Notes:
    
    * Don't even think about compiling this yet, it's not ready
    * http://pastebin.com/7szaZpkC current compile errors for the curious
    * http://fabiensanglard.net/anotherWorld_code_review/ a breakdown of
    Fabien Sanglard's "Fabother World"
    
    --------------------------------------------------------------------
    To be done:
    
    * plugin init and destroy
    * menu
    * convert input to rockbox PLA
    * convert draw and fill calls to native rockbox functions
    * sound
    
    --------------------------------------------------------------------
    
    Change-Id: Ib4193557ff48bc4f874bc1b0def46fb7ef4ec7c4

---
+++ apps/plugins/xworld/bank.h
@@ -0,0 +1,49 @@
+/* Raw - Another World Interpreter
+ * Copyright (C) 2004 Gregory Montoir
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __BANK_H__
+#define __BANK_H__
+
+#include "intern.h"
+
+struct MemEntry;
+
+struct UnpackContext {
+	uint16_t size;
+	uint32_t crc;
+	uint32_t chk;
+	int32_t datasize;
+};
+
+struct Bank {
+	UnpackContext _unpCtx;
+	const char *_dataDir;
+	uint8_t *_iBuf, *_oBuf, *_startBuf;
+
+	Bank(const char *dataDir);
+
+	bool read(const MemEntry *me, uint8_t *buf);
+	void decUnk1(uint8_t numChunks, uint8_t addCount);
+	void decUnk2(uint8_t numChunks);
+	bool unpack();
+	uint16_t getCode(uint8_t numChunks);
+	bool nextChunk();
+	bool rcr(bool CF);
+};
+
+#endif
---
+++ apps/plugins/xworld/endian.h
@@ -0,0 +1,96 @@
+/* Raw - Another World Interpreter
+ * Copyright (C) 2004 Gregory Montoir
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __SYS_H__
+#define __SYS_H__
+
+#include <stdint.h>
+
+//FCS added for windows build
+//JWS added automatic platform detection
+#if defined(AUTO_DETECT_PLATFORM)
+  #if defined(_WIN32) || defined(_WIN64)
+  //windows is special, support 64-bit windows
+  //Not sure about winrt at this point
+  	#define SYS_LITTLE_ENDIAN
+  #else //gcc, clang, or icc
+    #if defined(__i386__) || defined(i386) || defined(__i386) || \
+        defined(_M_IX86) || defined(_X86_) || defined(_i386) || defined(__X86__)
+       #define SYS_LITTLE_ENDIAN
+    #elif defined(__x86_64__) || defined(_M_X64) || defined(_M_AMD64) || \
+          defined(__amd64__) || defined(__amd64) || defined(__x86_64)
+       #define SYS_LITTLE_ENDIAN
+    #elif defined(__sparc__)
+       #define SYS_BIG_ENDIAN
+    #elif defined(__ia64__) || defined(_IA64) || defined(__IA64__) || \
+          defined(__ia64) || defined(_M_IA64) || defined(__itanium__)
+    //ia64 linux is little endian by default
+       #if defined(__linux__)
+           #define SYS_LITTLE_ENDIAN
+       #else
+           #warning "Unknown Itanium platform detected..."
+           #define SYS_BIG_ENDIAN
+       #endif
+    //powerpc32 and 64 bit
+    #elif defined(__ppc__) || defined(__powerpc) || defined(__powerpc__) || \
+          defined(__POWERPC__) || defined(_M_PPC) || defined(_ARCH_PPC) || \
+          defined(__ppc64__)
+       #define SYS_BIG_ENDIAN
+    #elif defined(__arm__) || defined(__aarch64__)
+    //arm is bi-endian and we should eventually add OS detection routines for
+    //proper detection
+       #define SYS_LITTLE_ENDIAN
+    #elif defined(__mips__) || defined(__mips) || defined(__MIPS__)
+       #define SYS_BIG_ENDIAN
+    #else
+       #warning "Unknown architecture detected..."
+    #endif
+  #endif
+#endif
+
+
+#if defined SYS_LITTLE_ENDIAN
+//#warning "LITTLE ENDIAN SELECTED"
+inline uint16_t READ_BE_UINT16(const void *ptr) {
+	const uint8_t *b = (const uint8_t *)ptr;
+	return (b[0] << 8) | b[1];
+}
+
+inline uint32_t READ_BE_UINT32(const void *ptr) {
+	const uint8_t *b = (const uint8_t *)ptr;
+	return (b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3];
+}
+
+#elif defined SYS_BIG_ENDIAN
+//#warning "BIG ENDIAN SELECTED"
+
+inline uint16_t READ_BE_UINT16(const void *ptr) {
+	return *(const uint16_t *)ptr;
+}
+
+inline uint32_t READ_BE_UINT32(const void *ptr) {
+	return *(const uint32_t *)ptr;
+}
+
+#else
+
+#error No endianness defined
+
+#endif
+
+#endif
