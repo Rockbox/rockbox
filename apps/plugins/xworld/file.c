f8d831ae5a9ffec5a29622bd7e4059c3dcabe136
Franklin Wei <frankhwei536@gmail.com>
2014-10-13 21:00:47 -0400

    [WIP] Xworld Another World interpreter for Rockbox
    Benjamin Brown, Franklin Wei
    
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Changes:
    
    * Dump of Fabien Sanglard's git hub repository from
    https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter
    * Visualstudio and Xcode dirs removed for rockbox intergration
    * .cpp files renamed to .c
    
    --------------------------------------------------------------------
    Notes:
    
    * Don't even think about compiling this yet, it's not ready
    * http://pastebin.com/RnWtnSFs slightly outdated compile errors for the curious
    * http://fabiensanglard.net/anotherWorld_code_review/ a breakdown of
    Fabien Sanglard's "Fabother World"
    
    --------------------------------------------------------------------
    To be done (in descending order of importance):
    
    * convert c++ to c <50% done>
    * convert draw and fill calls to native rockbox functions <100% done, UNTESTED>
    * plugin init and destroy <90% done>
    * convert input to rockbox PLA <75% done>
    * sound <0% done>
    * menu <0% done>
    
    Change-Id: Ib4193557ff48bc4f874bc1b0def46fb7ef4ec7c4

---
+++ apps/plugins/xworld/file.c
@@ -0,0 +1,183 @@
+/* Raw - Another World Interpreter
+ * Copyright (C) 2004 Gregory Montoir
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "file.h"
+
+/*
+struct File_impl {
+	bool _ioErr;
+	File_impl() : _ioErr(false) {}
+	virtual bool open(const char *path, const char *mode) = 0;
+	virtual void close() = 0;
+	virtual void seek(int32_t off) = 0;
+	virtual void read(void *ptr, uint32_t size) = 0;
+	virtual void write(void *ptr, uint32_t size) = 0;
+};
+
+struct stdFile : File_impl {
+	FILE *_fp;
+	stdFile() : _fp(0) {}
+	bool open(const char *path, const char *mode) {
+		_ioErr = false;
+		_fp = fopen(path, mode);
+		return (_fp != NULL);
+	}
+	void close() {
+		if (_fp) {
+			fclose(_fp);
+			_fp = 0;
+		}
+	}
+	void seek(int32_t off) {
+		if (_fp) {
+			fseek(_fp, off, SEEK_SET);
+		}
+	}
+	void read(void *ptr, uint32_t size) {
+		if (_fp) {
+			uint32_t r = fread(ptr, 1, size, _fp);
+			if (r != size) {
+				_ioErr = true;
+			}
+		}
+	}
+	void write(void *ptr, uint32_t size) {
+		if (_fp) {
+			uint32_t r = fwrite(ptr, 1, size, _fp);
+			if (r != size) {
+				_ioErr = true;
+			}
+		}
+	}
+};
+
+struct zlibFile : File_impl {
+	gzFile _fp;
+	zlibFile() : _fp(0) {}
+	bool open(const char *path, const char *mode) {
+		_ioErr = false;
+		_fp = gzopen(path, mode);
+		return (_fp != NULL);
+	}
+	void close() {
+		if (_fp) {
+			gzclose(_fp);
+			_fp = 0;
+		}
+	}
+	void seek(int32_t off) {
+		if (_fp) {
+			gzseek(_fp, off, SEEK_SET);
+		}
+	}
+	void read(void *ptr, uint32_t size) {
+		if (_fp) {
+			uint32_t r = gzread(_fp, ptr, size);
+			if (r != size) {
+				_ioErr = true;
+			}
+		}
+	}
+	void write(void *ptr, uint32_t size) {
+		if (_fp) {
+			uint32_t r = gzwrite(_fp, ptr, size);
+			if (r != size) {
+				_ioErr = true;
+			}
+		}
+	}
+};
+*/
+void file_create(struct File* f, bool gzipped) {
+	if (gzipped) {
+		_impl = new zlibFile;
+	} else {
+		_impl = new stdFile;
+	}
+}
+
+void file_close(struct File* f) {
+	_impl->close();
+	delete _impl;
+}
+
+bool file_open(struct File* f, const char *filename, const char *directory, const char *mode) {
+	_impl->close();
+	char buf[512];
+	sprintf(buf, "%s/%s", directory, filename);
+	char *p = buf + strlen(directory) + 1;
+	string_lower(p);
+	bool opened = _impl->open(buf, mode);
+	if (!opened) { // let's try uppercase
+		string_upper(p);
+		opened = _impl->open(buf, mode);
+	}
+	return opened;
+}
+
+void file_close(struct File* f) {
+	_impl->close();
+}
+
+bool file_ioErr(struct File* f) {
+	return _impl->_ioErr;
+}
+
+void file_seek(struct File* f, int32_t off) {
+	_impl->seek(off);
+}
+
+void file_read(struct File* f, void *ptr, uint32_t size) {
+	_impl->read(ptr, size);
+}
+
+uint8_t file_readByte(struct File* f) {
+	uint8_t b;
+	read(&b, 1);
+	return b;
+}
+
+uint16_t file_readUint16BE(struct File* f) {
+	uint8_t hi = readByte();
+	uint8_t lo = readByte();
+	return (hi << 8) | lo;
+}
+
+uint32_t file_readUint32BE(struct File* f) {
+	uint16_t hi = readUint16BE();
+	uint16_t lo = readUint16BE();
+	return (hi << 16) | lo;
+}
+
+void file_write(struct File* f, void *ptr, uint32_t size) {
+	_impl->write(ptr, size);
+}
+
+void file_writeByte(struct File* f, uint8_t b) {
+	write(&b, 1);
+}
+
+void file_writeUint16BE(struct File* f, uint16_t n) {
+	writeByte(n >> 8);
+	writeByte(n & 0xFF);
+}
+
+void file_writeUint32BE(struct File* f, uint32_t n) {
+	writeUint16BE(n >> 16);
+	writeUint16BE(n & 0xFFFF);
+}
