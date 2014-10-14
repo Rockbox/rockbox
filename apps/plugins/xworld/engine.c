8bea74f27555063c16a72b1d441ccd268eebec7d
Franklin Wei <frankhwei536@gmail.com>
2014-10-13 21:00:47 -0400

    [WIP] My Xworld branch, Another World interpreter for Rockbox
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Changes:
    
    * Dump of Fabien Sanglard's git hub repository from
    https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter
    * Visualstudio and Xcode dirs removed for rockbox intergration
    * .cpp files renamed to .c
    
    --------------------------------------------------------------------
    Notes:
    
    * Don't even think about compiling this yet, it's not ready
    * http://pastebin.com/g5V7N7GN current compile errors for the curious
    * http://fabiensanglard.net/anotherWorld_code_review/ a breakdown of
    Fabien Sanglard's "Fabother World"
    
    --------------------------------------------------------------------
    To be done:
    
    * convert c++ to c
    * plugin init and destroy
    * menu
    * convert input to rockbox PLA
    * convert draw and fill calls to native rockbox functions
    * sound
    
    --------------------------------------------------------------------
    
    Change-Id: Ib4193557ff48bc4f874bc1b0def46fb7ef4ec7c4
    
    Xworld work
    
    Change-Id: I33774c4e4e8bf08d171840dfa0dc9eff0b348865
    
    Conflicts:
    	apps/plugins/xworld/intern.h

---
+++ apps/plugins/xworld/engine.c
@@ -0,0 +1,189 @@
+/* Raw - Another World Interpreter
+ * Copyright (C) 2004 Gregory Montoir
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "engine.h"
+#include "file.h"
+#include "serializer.h"
+#include "sys.h"
+#include "parts.h"
+
+Engine::Engine(System *paramSys, const char *dataDir, const char *saveDir)
+	: sys(paramSys), vm(&mixer, &res, &player, &video, sys), mixer(sys), res(&video, dataDir),
+	player(&mixer, &res, sys), video(&res, sys), _dataDir(dataDir), _saveDir(saveDir), _stateSlot(0) {
+}
+
+void Engine::run() {
+
+	while (!sys->input.quit) {
+
+		vm.checkThreadRequests();
+
+		vm.inp_updatePlayer();
+
+		processInput();
+
+		vm.hostFrame();
+	}
+
+
+}
+
+Engine::~Engine(){
+
+	finish();
+	sys->destroy();
+}
+
+
+void Engine::init() {
+
+
+	//Init system
+	sys->init("Out Of This World");
+
+	video.init();
+
+	res.allocMemBlock();
+
+	res.readEntries();
+
+	vm.init();
+
+	mixer.init();
+
+	player.init();
+
+	//Init virtual machine, legacy way
+	vm.initForPart(GAME_PART_FIRST); // This game part is the protection screen
+
+
+
+	// Try to cheat here. You can jump anywhere but the VM crashes afterward.
+	// Starting somewhere is probably not enough, the variables and calls return are probably missing.
+	//vm.initForPart(GAME_PART2); // Skip protection screen and go directly to intro
+	//vm.initForPart(GAME_PART3); // CRASH
+	//vm.initForPart(GAME_PART4); // Start directly in jail but then crash
+	//vm.initForPart(GAME_PART5);   //CRASH
+	//vm.initForPart(GAME_PART6);   // Start in the battlechar but CRASH afteward
+	//vm.initForPart(GAME_PART7); //CRASH
+	//vm.initForPart(GAME_PART8); //CRASH
+	//vm.initForPart(GAME_PART9); // Green screen not doing anything
+}
+
+void Engine::finish() {
+	player.free();
+	mixer.free();
+	res.freeMemBlock();
+}
+
+void Engine::processInput() {
+	if (sys->input.load) {
+		loadGameState(_stateSlot);
+		sys->input.load = false;
+	}
+	if (sys->input.save) {
+		saveGameState(_stateSlot, "quicksave");
+		sys->input.save = false;
+	}
+	if (sys->input.fastMode) {
+		vm._fastMode = !vm._fastMode;
+		sys->input.fastMode = false;
+	}
+	if (sys->input.stateSlot != 0) {
+		int8_t slot = _stateSlot + sys->input.stateSlot;
+		if (slot >= 0 && slot < MAX_SAVE_SLOTS) {
+			_stateSlot = slot;
+			debug(DBG_INFO, "Current game state slot is %d", _stateSlot);
+		}
+		sys->input.stateSlot = 0;
+	}
+}
+
+void Engine::makeGameStateName(uint8_t slot, char *buf) {
+	sprintf(buf, "raw.s%02d", slot);
+}
+
+void Engine::saveGameState(uint8_t slot, const char *desc) {
+	char stateFile[20];
+	makeGameStateName(slot, stateFile);
+	File f(true);
+	if (!f.open(stateFile, _saveDir, "wb")) {
+		warning("Unable to save state file '%s'", stateFile);
+	} else {
+		// header
+		f.writeUint32BE('AWSV');
+		f.writeUint16BE(Serializer::CUR_VER);
+		f.writeUint16BE(0);
+		char hdrdesc[32];
+		strncpy(hdrdesc, desc, sizeof(hdrdesc) - 1);
+		f.write(hdrdesc, sizeof(hdrdesc));
+		// contents
+		Serializer s(&f, Serializer::SM_SAVE, res._memPtrStart);
+		vm.saveOrLoad(s);
+		res.saveOrLoad(s);
+		video.saveOrLoad(s);
+		player.saveOrLoad(s);
+		mixer.saveOrLoad(s);
+		if (f.ioErr()) {
+			warning("I/O error when saving game state");
+		} else {
+			debug(DBG_INFO, "Saved state to slot %d", _stateSlot);
+		}
+	}
+}
+
+void Engine::loadGameState(uint8_t slot) {
+	char stateFile[20];
+	makeGameStateName(slot, stateFile);
+	File f(true);
+	if (!f.open(stateFile, _saveDir, "rb")) {
+		warning("Unable to open state file '%s'", stateFile);
+	} else {
+		uint32_t id = f.readUint32BE();
+		if (id != 'AWSV') {
+			warning("Bad savegame format");
+		} else {
+			// mute
+			player.stop();
+			mixer.stopAll();
+			// header
+			uint16_t ver = f.readUint16BE();
+			f.readUint16BE();
+			char hdrdesc[32];
+			f.read(hdrdesc, sizeof(hdrdesc));
+			// contents
+			Serializer s(&f, Serializer::SM_LOAD, res._memPtrStart, ver);
+			vm.saveOrLoad(s);
+			res.saveOrLoad(s);
+			video.saveOrLoad(s);
+			player.saveOrLoad(s);
+			mixer.saveOrLoad(s);
+		}
+		if (f.ioErr()) {
+			warning("I/O error when loading game state");
+		} else {
+			debug(DBG_INFO, "Loaded state from slot %d", _stateSlot);
+		}
+	}
+}
+
+
+const char* Engine::getDataDir()
+{
+	return this->_dataDir;
+}
---
+++ apps/plugins/xworld/engine.h
@@ -0,0 +1,59 @@
+/* Raw - Another World Interpreter
+ * Copyright (C) 2004 Gregory Montoir
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __ENGINE_H__
+#define __ENGINE_H__
+
+#include "intern.h"
+#include "vm.h"
+#include "mixer.h"
+#include "sfxplayer.h"
+#include "resource.h"
+#include "video.h"
+
+struct System;
+
+struct Engine {
+	enum {
+		MAX_SAVE_SLOTS = 100
+	};
+
+	System *sys;
+	VirtualMachine vm;
+	Mixer mixer;
+	Resource res;
+	SfxPlayer player;
+	Video video;
+	const char *_dataDir, *_saveDir;
+	uint8_t _stateSlot;
+
+	Engine(System *stub, const char *dataDir, const char *saveDir);
+	~Engine();
+
+	void run();
+	void init();
+	void finish();
+	void processInput();
+
+	void makeGameStateName(uint8_t slot, char *buf);
+	void saveGameState(uint8_t slot, const char *desc);
+	void loadGameState(uint8_t slot);
+	const char* getDataDir();
+};
+
+#endif
