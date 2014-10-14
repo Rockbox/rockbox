ebebf5a1beafb0644b463948b53bc4881d4be2d5
Franklin Wei <frankhwei536@gmail.com>
2014-10-13 21:00:47 -0400

    Xworld - Another World interpreter for Rockbox
    Co-conspirators: Franklin Wei, Benjamin Brown
    
    --------------------------------------------------------------------
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Progress:
    
    * The plugin runs pretty nicely (with sound!) on most color targets
    * Keymaps for color LCD targets are complete
    * The manual entry is finished
    * Grayscale/monochrome support is NOT PLANNED
      - the game looks horrible in grayscale! :p
    
    --------------------------------------------------------------------
    Notes:
    
    * The original game strings were built-in to the executable, and
      were copyrighted and could not be used.
    * This port ships with an alternate set of strings by default, but
      can load the "official" strings from a file at runtime.
    
    --------------------------------------------------------------------
    To be done (in descending order of importance):
    
    * vertical stride compatibility                          <30% done>
    * optimization                                           <10% done>
    
    Change-Id: I3155b0d97c2ac470cb8a2040f40d4139ddcebfa5

---
+++ apps/plugins/xworld/video_data.h
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2014 Franklin Wei
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
+
+#include "video.h"
+#include "stdint.h"
+#define FONT_SIZE (8 * (0x80 - ' '))
+#define MAX_STRING_TABLE_SIZE 255
+
+extern uint8_t video_font[FONT_SIZE];
+
+extern struct StrEntry video_stringsTableEng[MAX_STRING_TABLE_SIZE];
