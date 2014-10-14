ed4367d2821e5bd4be52c9c8ba5ecc8a19b142b0
Franklin Wei <frankhwei536@gmail.com>
2014-10-13 21:00:47 -0400

    [WIP] Xworld Another World interpreter for Rockbox
    Benjamin Brown, Franklin Wei
    
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Changes:
    
    * Dump of Fabien Sanglard's git hub repository from
    https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter
    * Visualstudio and Xcode dirs removed for rockbox intergration
    * .cpp files renamed to .c
    
    --------------------------------------------------------------------
    Notes:
    
    * Don't even think about compiling this yet, it's not ready
    * http://pastebin.com/RnWtnSFs slightly outdated compile errors for the curious
    * http://fabiensanglard.net/anotherWorld_code_review/ a breakdown of
    Fabien Sanglard's "Fabother World"
    
    --------------------------------------------------------------------
    To be done (in descending order of importance):
    
    * convert c++ to c                                        <75%  done>
    * make the `File' class work with Rockbox (gzip I/O)      <50%  done>
    * convert draw and fill calls to native rockbox functions <100% done, UNTESTED>
    * plugin init and destroy                                 <90%  done>
    * convert input to rockbox PLA                            <75%  done>
    * sound                                                   <0%   done>
    * menu                                                    <0%   done>
    
    Change-Id: Ib4193557ff48bc4f874bc1b0def46fb7ef4ec7c4

---
+++ apps/plugins/xworld/mixer.h
@@ -0,0 +1,70 @@
+/* Raw - Another World Interpreter
+ * Copyright (C) 2004 Gregory Montoir
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __MIXER_H__
+#define __MIXER_H__
+
+#include "intern.h"
+
+struct MixerChunk {
+    const uint8_t *data;
+    uint16_t len;
+    uint16_t loopPos;
+    uint16_t loopLen;
+};
+
+struct MixerChannel {
+    uint8_t active;
+    uint8_t volume;
+    struct MixerChunk chunk;
+    uint32_t chunkPos;
+    uint32_t chunkInc;
+};
+
+struct Serializer;
+struct System;
+
+#define AUDIO_NUM_CHANNELS 4
+
+struct Mixer {
+
+
+    void *_mutex;
+    struct System *sys;
+
+    // Since the virtal machine and SDL are running simultaneously in two different threads
+    // any read or write to an elements of the sound channels MUST be synchronized with a
+    // mutex.
+    struct MixerChannel _channels[AUDIO_NUM_CHANNELS];
+
+};
+void mixer_create(struct Mixer*, System *stub);
+void mixer_init(struct Mixer*);
+void mixer_free(struct Mixer*);
+
+void mixer_playChannel(struct Mixer*, uint8_t channel, const MixerChunk *mc, uint16_t freq, uint8_t volume);
+void mixer_stopChannel(struct Mixer*, uint8_t channel);
+void mixer_setChannelVolume(struct Mixer*, uint8_t channel, uint8_t volume);
+void mixer_stopAll(struct Mixer*);
+void mixer_mix(struct Mixer*, mxint8_t *buf, int len);
+
+static void mixer_mixCallback(struct Mixer*, void *param, uint8_t *buf, int len);
+
+void mixer_saveOrLoad(struct Mixer*, struct Serializer *ser);
+
+#endif
