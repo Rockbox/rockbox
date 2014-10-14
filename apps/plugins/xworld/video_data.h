26317509aa2c27d6621385aa1c923f051344d763
Benjamin Brown <foolsh.person@gmail.com>
2014-10-13 21:00:47 -0400

    [WIP] Xworld Another World interpreter for Rockbox
    Benjamin Brown, Franklin Wei
    
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Changes:
    
    * Dump of Fabien Sanglard's git hub repository from
    https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter
    * Visualstudio and Xcode dirs removed for rockbox intergration
    * .cpp files renamed to .c
    
    --------------------------------------------------------------------
    Notes:
    
    * Don't even think about compiling this yet, it's not ready
    * http://pastebin.com/pzkNsR5t current compile errors for the curious
    * http://fabiensanglard.net/anotherWorld_code_review/ a breakdown of
    Fabien Sanglard's "Fabother World"
    
    --------------------------------------------------------------------
    To be done (in descending order of importance):
    
    * convert c++ to c <50% done>
    * convert draw and fill calls to native rockbox functions <100% done, UNTESTED>
    * plugin init and destroy <90% done>
    * convert input to rockbox PLA <75% done>
    * sound <0% done>
    * menu <0% done>
    
    Change-Id: Ib4193557ff48bc4f874bc1b0def46fb7ef4ec7c4

---
+++ apps/plugins/xworld/video_data.h
@@ -0,0 +1,7 @@
+#include "video.h"
+#include "stdint.h"
+extern const uint8_t video_font[];
+
+extern const struct StrEntry video__stringsTableEng[];
+
+extern const struct StrEntry video_stringsTableDemo[];
