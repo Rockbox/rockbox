048cf64edf25af23ce74e37729bbc82417dc0db2
Franklin Wei <frankhwei536@gmail.com>
2014-10-13 21:00:47 -0400

    [WIP] Xworld Another World interpreter for Rockbox
    Benjamin Brown, Franklin Wei
    
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Changes:
    
    * Dump of Fabien Sanglard's git hub repository from
    https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter
    * Visualstudio and Xcode dirs removed for rockbox intergration
    * .cpp files renamed to .c
    
    --------------------------------------------------------------------
    Notes:
    
    * Don't even think about compiling this yet, it's not ready
    * http://pastebin.com/RnWtnSFs slightly outdated compile errors for the curious
    * http://fabiensanglard.net/anotherWorld_code_review/ a breakdown of
    Fabien Sanglard's "Fabother World"
    
    --------------------------------------------------------------------
    To be done (in descending order of importance):
    
    * convert c++ to c                                        <75%  done>
    * make the `File' class work with Rockbox (gzip I/O)      <50%  done>
    * convert draw and fill calls to native rockbox functions <100% done, UNTESTED>
    * plugin init and destroy                                 <90%  done>
    * convert input to rockbox PLA                            <75%  done>
    * sound                                                   <25%  done>
    * menu                                                    <0%   done>
    
    Change-Id: Ib4193557ff48bc4f874bc1b0def46fb7ef4ec7c4

---
+++ apps/plugins/xworld/serializer.h
@@ -0,0 +1,79 @@
+/* Raw - Another World Interpreter
+ * Copyright (C) 2004 Gregory Montoir
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __SERIALIZER_H__
+#define __SERIALIZER_H__
+
+#include "intern.h"
+
+#define CUR_VER 2
+
+#define VER(x) x
+
+#define SE_INT(i,sz,ver)     { Serializer::SET_INT, sz, 1, i, ver, Serializer::CUR_VER }
+#define SE_ARRAY(a,n,sz,ver) { Serializer::SET_ARRAY, sz, n, a, ver, Serializer::CUR_VER }
+#define SE_PTR(p,ver)        { Serializer::SET_PTR, 0, 0, p, ver, Serializer::CUR_VER }
+#define SE_END()             { Serializer::SET_END, 0, 0, 0, 0, 0 }
+
+struct File;
+enum EntryType {
+    SET_INT,
+    SET_ARRAY,
+    SET_PTR,
+    SET_END
+};
+
+enum {
+    SES_BOOL  = 1,
+    SES_INT8  = 1,
+    SES_INT16 = 2,
+    SES_INT32 = 4
+};
+
+enum Mode {
+    SM_SAVE,
+    SM_LOAD
+};
+
+struct Entry {
+    enum EntryType type;
+    uint8_t size;
+    uint16_t n;
+    void *data;
+    uint16_t minVer;
+    uint16_t maxVer;
+};
+
+struct Serializer {
+    File *_stream;
+    enum Mode _mode;
+    uint8_t *_ptrBlock;
+    uint16_t _saveVer;
+    uint32_t _bytesCount;
+};
+
+void ser_create(struct Serializer*, File *stream, enum Mode mode, uint8_t *ptrBlock, uint16_t saveVer);
+
+void ser_saveOrLoadEntries(struct Serializer*, struct Entry *entry);
+
+void ser_saveEntries(struct Serializer*, struct Entry *entry);
+void ser_loadEntries(struct Serializer*, struct Entry *entry);
+
+void ser_saveInt(struct Serializer*, uint8_t es, void *p);
+void ser_loadInt(struct Serializer*, uint8_t es, void *p);
+#endif
