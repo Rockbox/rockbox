9d3f43360d521cc412641ebdbc5cfc2d1abd60be
Franklin Wei <frankhwei536@gmail.com>
2014-10-13 21:00:47 -0400

    Xworld - Another World interpreter for Rockbox
    Co-conspirators: Franklin Wei, Benjamin Brown
    
    --------------------------------------------------------------------
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Progress:
    
    * The plugin runs pretty nicely (with sound!) on most color targets
    * Keymaps for color LCD targets are complete
    * The manual entry is finished
    * Grayscale/monochrome support is NOT PLANNED
      - the game looks horrible in grayscale! :p
    
    --------------------------------------------------------------------
    Notes:
    
    * The original game strings were built-in to the executable, and
      were copyrighted and could not be used.
    * This port ships with an alternate set of strings by default, but
      can load the "official" strings from a file at runtime.
    
    --------------------------------------------------------------------
    To be done (in descending order of importance):
    
    * vertical stride compatibility                          <30% done>
    * optimization                                           <10% done>
    
    Change-Id: I3155b0d97c2ac470cb8a2040f40d4139ddcebfa5

---
+++ apps/plugins/xworld/util.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2014 Franklin Wei, Benjamin Brown
+ * Copyright (C) 2004 Gregory Montoir
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
+
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include "intern.h"
+
+/* #define XWORLD_DEBUG */
+
+#ifdef XWORLD_DEBUG
+#define debug(m,f,...) debug_real(m, f, ##__VA_ARGS__)
+#else
+#define debug(m,f,...)
+#endif
+
+enum {
+    DBG_VM = 1 << 0,
+    DBG_BANK  = 1 << 1,
+    DBG_VIDEO = 1 << 2,
+    DBG_SND   = 1 << 3,
+    DBG_SER   = 1 << 4,
+    DBG_INFO  = 1 << 5,
+    DBG_RES   = 1 << 6,
+    DBG_FILE  = 1 << 7,
+    DBG_SYS   = 1 << 8,
+    DBG_ENG   = 1 << 9
+};
+
+extern uint16_t g_debugMask;
+#ifdef XWORLD_DEBUG
+extern void debug_real(uint16_t cm, const char *msg, ...);
+#endif
+extern void error(const char *msg, ...);
+extern void warning(const char *msg, ...);
+
+extern void string_lower(char *p);
+extern void string_upper(char *p);
+
+#endif
