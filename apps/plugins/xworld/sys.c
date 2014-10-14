0e9c8a41554c9e934e4ed4c4a2eb1d52b1e31128
Franklin Wei <frankhwei536@gmail.com>
2014-10-13 21:00:47 -0400

    [WIP] Xworld Another World interpreter for Rockbox
    Benjamin Brown, Franklin Wei
    
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Changes:
    
    * Dump of Fabien Sanglard's git hub repository from
    https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter
    * Visualstudio and Xcode dirs removed for rockbox intergration
    * .cpp files renamed to .c
    
    --------------------------------------------------------------------
    Notes:
    
    * It compiles, but doesn't work
    * http://fabiensanglard.net/anotherWorld_code_review/ a breakdown of
    Fabien Sanglard's "Fabother World"
    
    --------------------------------------------------------------------
    To be done (in descending order of importance):
    
    * convert c++ to c                                        <95%  done>
    * make the `File' class work with Rockbox (gzip needed)   <60%  done>
    * convert draw and fill calls to native rockbox functions <100% done, UNTESTED>
    * plugin init and destroy                                 <95%  done>
    * convert input to rockbox PLA                            <75%  done>
    * sound                                                   <50%  done>
    * menu                                                    <0%   done>
    
    Change-Id: Ib4193557ff48bc4f874bc1b0def46fb7ef4ec7c4

---
+++ apps/plugins/xworld/sys.c
@@ -0,0 +1,134 @@
+#include "plugin.h"
+#include "lib/pluginlib_actions.h"
+#include "sys.h"
+void sys_init(struct System* sys, const char* title)
+{
+    rb->splash(HZ, title);
+    memset(&sys->input, 0, sizeof(sys->input));
+    sys->mutex_bitfield=0xFFFF;
+}
+void sys_destroy(struct System* sys)
+{
+    (void) sys;
+    rb->splash(HZ, "Exiting...");
+}
+void sys_setPalette(struct System* sys, uint8_t start, uint8_t n, const uint8_t *buf)
+{
+    for(int i=start;i<start+n;++i)
+    {
+        uint8_t c[3];
+        for (int j = 0; j < 3; j++) {
+            uint8_t col = buf[i * 3 + j];
+            c[j] = (col << 2) | (col & 3);
+        }
+        sys->pallete[i]=LCD_RGBPACK(c[0],c[1],c[2]);
+    }
+}
+void sys_copyRect(struct System* sys, uint16_t x, uint16_t y, uint16_t w, uint16_t h, const uint8_t *buf, uint32_t pitch)
+{
+    debug(DBG_SYS, "sys is 0x%08x", sys);
+    buf += y * pitch + x;
+    while (h--) {
+        /* one byte gives two pixels */
+        for (int i = 0; i < w / 2; ++i) {
+            uint8_t pix1=*(buf+i)>>4;
+            uint8_t pix2=*(buf+i)&0xF;
+            rb->lcd_set_foreground(sys->pallete[pix1]);
+            rb->lcd_drawpixel(x+i*2, y+h);
+            rb->lcd_set_foreground(sys->pallete[pix2]);
+            rb->lcd_drawpixel(x+i*2+1, y+h);
+        }
+        buf += pitch;
+    }
+    rb->lcd_update();
+}
+
+void sys_processEvents(struct System* sys)
+{
+    static const struct button_mapping *plugin_contexts[] = { pla_main_ctx };
+    int button=pluginlib_getaction(0, plugin_contexts, ARRAYLEN(plugin_contexts));
+    if(button&PLA_UP)
+    {
+        sys->input.dirMask|=DIR_UP;
+    }
+    if(button&PLA_DOWN)
+    {
+        sys->input.dirMask|=DIR_DOWN;
+    }
+    if(button&PLA_LEFT)
+    {
+        sys->input.dirMask|=DIR_LEFT;
+    }
+    if(button&PLA_RIGHT)
+    {
+        sys->input.dirMask|=DIR_RIGHT;
+    }
+}
+void sys_sleep(struct System* sys, uint32_t duration)
+{
+    (void) sys;
+    rb->sleep(duration/10);
+}
+
+uint32_t sys_getTimeStamp(struct System* sys)
+{
+    (void) sys;
+    return (uint32_t) *rb->current_tick;
+}
+
+void sys_startAudio(struct System* sys, AudioCallback callback, void *param)
+{
+}
+void sys_stopAudio(struct System* sys)
+{
+}
+uint32_t sys_getOutputSampleRate(struct System* sys)
+{
+    (void) sys;
+    return 44100;
+}
+
+void *sys_addTimer(struct System* sys, uint32_t delay, TimerCallback callback, void *param)
+{
+}
+void sys_removeTimer(struct System* sys, void *timerId)
+{
+}
+
+void *sys_createMutex(struct System* sys)
+{
+    if(!sys)
+        error("sys is NULL!");
+    for(int i=0;i<MAX_MUTEXES;++i)
+    {
+        if(sys->mutex_bitfield & (1<<i))
+        {
+            rb->mutex_init(&sys->mutex_memory[i]);
+            sys->mutex_bitfield |= (1 << i);
+            return &sys->mutex_memory[i];
+        }
+    }
+    warning("Out of mutexes!");
+    return NULL;
+}
+void sys_destroyMutex(struct System* sys, void *mutex)
+{
+    int mutex_number=(mutex-(void*)sys->mutex_memory)/sizeof(struct mutex); /* pointer arithmetic! check for bugs! */
+    sys->mutex_bitfield &= ~(1 << mutex_number);
+}
+void sys_lockMutex(struct System* sys, void *mutex)
+{
+    (void) sys;
+    rb->mutex_lock((struct mutex*) mutex);
+}
+void sys_unlockMutex(struct System* sys, void *mutex)
+{
+    (void) sys;
+    rb->mutex_unlock((struct mutex*) mutex);
+}
+
+uint8_t* getOffScreenFramebuffer(struct System* sys)
+{
+    (void) sys;
+    return NULL;
+}
