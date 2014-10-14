4f3c8f082b3bef5b6706656a898a746896fbade7
Franklin Wei <frankhwei536@gmail.com>
2014-10-13 21:00:47 -0400

    [WIP] My Xworld branch, Another World interpreter for Rockbox
    This work is based on:
    - Fabien Sanglard's "Fabother World" based on
    - Piotr Padkowski's newRaw interpreter which was based on
    - Gregory Montoir's reverse engineering of
    - Eric Chahi's assembly code
    
    --------------------------------------------------------------------
    Changes:
    
    * Dump of Fabien Sanglard's git hub repository from
    https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter
    * Visualstudio and Xcode dirs removed for rockbox intergration
    * .cpp files renamed to .c
    
    --------------------------------------------------------------------
    Notes:
    
    * Don't even think about compiling this yet, it's not ready
    * http://pastebin.com/g5V7N7GN current compile errors for the curious
    * http://fabiensanglard.net/anotherWorld_code_review/ a breakdown of
    Fabien Sanglard's "Fabother World"
    
    --------------------------------------------------------------------
    To be done:
    
    * convert c++ to c
    * plugin init and destroy
    * menu
    * convert input to rockbox PLA
    * convert draw and fill calls to native rockbox functions
    * sound
    
    Change-Id: Ib4193557ff48bc4f874bc1b0def46fb7ef4ec7c4

---
+++ apps/plugins/xworld/resource.h
@@ -0,0 +1,105 @@
+/* Raw - Another World Interpreter
+ * Copyright (C) 2004 Gregory Montoir
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __RESOURCE_H__
+#define __RESOURCE_H__
+
+#include "intern.h"
+
+
+#define MEMENTRY_STATE_END_OF_MEMLIST 0xFF
+#define MEMENTRY_STATE_NOT_NEEDED 0
+#define MEMENTRY_STATE_LOADED 1
+#define MEMENTRY_STATE_LOAD_ME 2
+
+/*
+  This is a directory entry. When the game starts, it loads memlist.bin and
+  populate and array of MemEntry
+*/
+typedef struct MemEntry {
+    uint8_t state;         // 0x0
+    uint8_t type;          // 0x1, Resource::ResType
+    uint8_t *bufPtr;       // 0x2
+    uint16_t unk4;         // 0x4, unused
+    uint8_t rankNum;       // 0x6
+    uint8_t bankId;       // 0x7
+    uint32_t bankOffset;      // 0x8 0xA
+    uint16_t unkC;         // 0xC, unused
+    uint16_t packedSize;   // 0xE
+    // All ressources are packed (for a gain of 28% according to Chahi)
+
+    uint16_t unk10;        // 0x10, unused
+    uint16_t size; // 0x12
+} MemEntry;
+/*
+  Note: state is not a boolean, it can have value 0, 1, 2 or 255, respectively meaning:
+  0:NOT_NEEDED
+  1:LOADED
+  2:LOAD_ME
+  255:END_OF_MEMLIST
+
+  See MEMENTRY_STATE_* #defines above.
+*/
+
+struct Serializer;
+struct Video;
+
+#define MEM_BLOCK_SIZE  (600 * 1024)
+
+struct Resource {
+#define RT_SOUND 0
+#define RT_MUSIC 1
+#define RT_POLY_ANIM 2
+#define RT_PALETTE 3
+#define RT_BYTECODE 4
+#define RT_POLY_CINEMATIC 5
+
+
+
+    Video *video;
+    const char *_dataDir;
+    MemEntry _memList[150];
+    uint16_t _numMemList;
+    uint16_t currentPartId, requestedNextPart;
+    uint8_t _memPtrStart[MEM_BLOCK_SIZE], *_scriptBakPtr, *_scriptCurPtr, *_vidBakPtr, *_vidCurPtr;
+    bool _useSegVideo2;
+
+    uint8_t *segPalettes;
+    uint8_t *segBytecode;
+    uint8_t *segCinematic;
+    uint8_t *_segVideo2;
+
+};
+
+
+Resource(Video *vid, const char *dataDir);
+
+void readBank(const MemEntry *me, uint8_t *dstBuf);
+void readEntries();
+void loadMarkedAsNeeded();
+void invalidateAll();
+void invalidateRes();
+void loadPartsOrMemoryEntry(uint16_t num);
+void setupPart(uint16_t ptrId);
+void allocMemBlock();
+void freeMemBlock();
+
+void saveOrLoad(Serializer &ser);
+
+const char* getDataDir();
+#endif
