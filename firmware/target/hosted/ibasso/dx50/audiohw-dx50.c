e9d08279ebbd2b6861f95796681ab0930be92e1e
Udo Schläpfer <rockbox-2014.10@desktopwarrior.net>
2014-11-16 15:21:33 +0100

    Major code cleanup and reorganization for iBasso DX50/DX90.
    Reorganization
    
    - Separated iBasso devices from PLATFORM_ANDROID. These are now standlone
      hosted targets. Most device specific code is in the
      firmware/target/hosted/ibasso directory.
    - No dependency on Android SDK, only the Android NDK is needed.
      32 bit Android NDK and Android API Level 16.
    - Separate implementation for each device where feasible.
    
    Code cleanup
    
    - Rewrite of existing code, from simple reformat to complete reimplementation.
    - New backlight interface, seperating backlight from touchscreen.
    - Rewrite of device button handler, removing unneeded code and fixing memory
      leaks.
    - New Debug messages interface logging to Android adb logcat (DEBUGF, panicf,
      logf).
    - Rewrite of lcd device handler, removing unneeded code and fixing memory leaks.
    - Rewrite of audiohw device handler/pcm interface, removing unneeded code and
      fixing memory leaks.
    - Rewrite of power and powermng, proper shutdown, using batterybench results.
    - Rewrite of configure (Android NDK) and device specific config.
    - Rewrite of the Android NDK specific Makefile.
    
    Misc
    
    - (Almost) all plugins/games/demos activated.
    - Update tinyalsa to latest from https://github.com/tinyalsa/tinyalsa.
    - New device specific options for hold switch, touchscreen, physical button
      interaction; usb and cpu frequency scaling governor.
    
    Includes
    
    - http://gerrit.rockbox.org/r/#/c/1029/ (only for these devices)
    - http://gerrit.rockbox.org/r/#/c/1013/
    - http://gerrit.rockbox.org/r/#/c/993/
    - http://gerrit.rockbox.org/r/#/c/1010/
    - http://gerrit.rockbox.org/r/#/c/1035/
    
    Does not include http://gerrit.rockbox.org/r/#/c/1007/ due to new backlight
    interface and new option for hold switch, touchscreen, physical button
    interaction.
    
    To do
    
    - Forced shutdown of Rockbox by Android due to USB mass storage connection.
    - Applying this code clean up to the "bootloader".
    - Vold changes by Ilia Sergachev (?) are not available?
    
    Tested on iBasso DX50.
    Compiled (not tested) for iBasso DX90.
    Compiled (not tested) for PLATFORM_ANDROID.
    
    Change-Id: I5f5e22e68f5b4cf29c28e2b40b2c265f2beb7ab7

---
+++ firmware/target/hosted/ibasso/dx50/audiohw-dx50.c
@@ -0,0 +1,79 @@
+/***************************************************************************
+ *             __________               __   ___
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2014 by Ilia Sergachev: Initial Rockbox port to iBasso DX50
+ * Copyright (C) 2014 by Mario Basister: iBasso DX90 port
+ * Copyright (C) 2014 by Simon Rothen: Initial Rockbox repository submission, additional features
+ * Copyright (C) 2014 by Udo Schläpfer: Code clean up, additional features
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+
+#include <stdio.h>
+
+#include "config.h"
+#include "debug.h"
+#include "panic.h"
+
+#include "debug-ibasso.h"
+
+
+extern void set_software_volume( void );
+
+
+void audiohw_set_volume( int volume )
+{
+    set_software_volume();
+
+    /*
+        See codec-dx50.h.
+        -128db        -> -1 (adjusted to 0, mute)
+        -127dB to 0dB ->  1 to 255 in steps of 2
+        volume is in centibels (tenth-decibels).
+    */
+    int volume_adjusted = ( volume / 10 ) * 2 + 255;
+
+    DEBUGF( "DEBUG %s: volume: %d, volume_adjusted: %d.", __func__, volume, volume_adjusted );
+
+    if( volume_adjusted > 255 )
+    {
+        DEBUGF( "DEBUG %s: Adjusting volume from %d to 255.", __func__, volume );
+        volume_adjusted = 255;
+    }
+    if( volume_adjusted < 0 )
+    {
+        DEBUGF( "DEBUG %s: Adjusting volume from %d to 0.", __func__, volume );
+        volume_adjusted = 0;
+    }
+
+    FILE *f = fopen( "/dev/codec_volume", "w" );
+    if( f == NULL )
+    {
+        DEBUGF( "ERROR %s: Can not open /dev/codec_volume for writing.", __func__ );
+        panicf( "ERROR %s: Can not open /dev/codec_volume for writing.", __func__ );
+        return;
+    }
+
+    if( fprintf( f, "%d", volume_adjusted ) < 1 )
+    {
+        fclose( f );
+        DEBUGF( "ERROR %s: Write failed for /dev/codec_volume.", __func__ );
+        panicf( "ERROR %s: Write failed for /dev/codec_volume.", __func__ );
+        return;
+    }
+
+    fclose( f );
+}
---
+++ firmware/target/hosted/ibasso/governor-ibasso.h
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *             __________               __   ___
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2014 by Ilia Sergachev: Initial Rockbox port to iBasso DX50
+ * Copyright (C) 2014 by Mario Basister: iBasso DX90 port
+ * Copyright (C) 2014 by Simon Rothen: Initial Rockbox repository submission, additional features
+ * Copyright (C) 2014 by Udo Schläpfer: Code clean up, additional features
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+
+#ifndef _GOVERNOR_IBASSO_H_
+#define _GOVERNOR_IBASSO_H_
+
+
+/* Supported freq scaling governors. */
+enum IBASSO_GOVERNORS
+{
+    GOVERNOR_CONSERVATIVE = 0,
+    GOVERNOR_ONDEMAND,
+    GOVERNOR_POWERSAVE,
+    GOVERNOR_INTERACTIVE,
+    GOVERNOR_PERFORMANCE
+};
+
+
+/* Set the active freq scaling governor. */
+void set_governor( enum IBASSO_GOVERNORS governor );
+
+
+#endif
