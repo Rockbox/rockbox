1c8a199263843e66213fef4a6139b7fed70bdeba
Udo Schläpfer <rockbox-2014.10@desktopwarrior.net>
2014-11-29 01:00:00 +0100

    Battery Log for iBasso DX50/DX90.
    Depends on http://gerrit.rockbox.org/r/#/c/1043/.
    
    Similar to batterybench, but exporting all the information from the kernel
    device file system.
    
    New setting: Settings -> General -> System -> Battery -> Battery Log
    
    Creates /mnt/sdcard/batterylog.csv.
    
    Change-Id: I7bb4c5a3e2293c9893b7141fc35a4b3965a34149

---
+++ firmware/target/hosted/ibasso/batterylog-ibasso.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *             __________               __   ___
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2014 by Ilia Sergachev: Initial Rockbox port to iBasso DX50
+ * Copyright (C) 2014 by Mario Basister: iBasso DX90 port
+ * Copyright (C) 2014 by Simon Rothen: Initial Rockbox repository submission, additional features
+ * Copyright (C) 2014 by Udo Schläpfer: Code clean up, additional features
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#ifndef _BATTERYLOG_IBASSO_H_
+#define _BATTERYLOG_IBASSO_H_
+
+
+/*
+    Adds the battery log to the tick tasks.
+    Battery log will be updated every 60 seconds.
+*/
+void batterylog_start( void );
+
+
+/* Removes the battery log from the tick tasks. */
+void batterylog_stop( void );
+
+
+#endif
