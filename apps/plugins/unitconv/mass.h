7d3152f4bc984e17bab397b7b5349a0b894823fa
Franklin Wei <frankhwei536@gmail.com>
2014-12-27 12:55:54 -0500

    [WIP] Unit Converter
    Todo:
    -----
     - Add more units
     - Add more unit types
     - clean up the UI
    
    Change-Id: I61ab4a5defda8333cbd89ce7c3dfa1fa73d4b940

---
+++ apps/plugins/unitconv/mass.h
@@ -0,0 +1,8 @@
+#include "unit.h"
+struct unit_t mass_table[] = {
+    {"Grams", "g.", UNIT, UNIT, 0},
+    {"Kilograms", "kg.", UNIT * 1000, UNIT/10, 0},
+    {"Pounds", "lbs.", UNIT * 453.593, UNIT/16, 0},
+    {"Short tons", "tons", UNIT * 453.593 * 2000, UNIT/2000, 0},
+    {"Long tons", "tonnes", UNIT * 1000 * 1000, UNIT/2240, 0},
+};
---
+++ apps/plugins/unitconv/unitconv.c
@@ -0,0 +1,103 @@
+#include "plugin.h"
+#include "lib/pluginlib_actions.h"
+#include <stdbool.h>
+/* data headers go below */
+#include "mass.h"
+
+static void convert_units(struct unit_t *convtab, const size_t len, size_t idx)
+{
+    for(size_t i=0;i<len;++i)
+    {
+        if(i != idx)
+        {
+            convtab[i].value = (double)convtab[idx].value / (double)convtab[i].ratio * (double)convtab[idx].ratio;
+        }
+    }
+}
+
+static void draw_everything(struct unit_t *convtab, const size_t len, size_t idx)
+{
+    convert_units(convtab, len, idx);
+    rb->lcd_set_background(LCD_BLACK);
+    rb->lcd_clear_display();
+    for(size_t i=0;i<len;++i)
+    {
+        if(i == idx)
+        {
+            rb->lcd_set_foreground(LCD_BLACK);
+            rb->lcd_set_background(LCD_WHITE);
+        }
+        else
+        {
+            rb->lcd_set_foreground(LCD_WHITE);
+            rb->lcd_set_background(LCD_BLACK);
+        }
+        rb->lcd_putsf(0, i, "%llu.%03llu %s",
+                      convtab[i].value/convtab[0].ratio,
+                      convtab[i].value%convtab[0].ratio,
+                      convtab[i].abbr);
+    }
+    rb->lcd_update();
+}
+
+static void do_unitconv(struct unit_t *convtab, const size_t len)
+{
+    const struct button_mapping *plugin_contexts[] = { pla_main_ctx };
+    size_t idx = 0;
+    bool quit = false;
+    while(!quit)
+    {
+        draw_everything(convtab, len, idx);
+        switch(pluginlib_getaction(-1, plugin_contexts, ARRAYLEN(plugin_contexts)))
+        {
+        case PLA_DOWN:
+        case PLA_DOWN_REPEAT:
+            if(idx++ >= len)
+                idx=0;
+            break;
+        case PLA_UP:
+        case PLA_UP_REPEAT:
+            if((signed int)idx-- < 0)
+                idx = len-1;
+            break;
+        case PLA_RIGHT:
+        case PLA_RIGHT_REPEAT:
+            convtab[idx].value += convtab[idx].step;
+            break;
+        case PLA_LEFT:
+        case PLA_LEFT_REPEAT:
+            convtab[idx].value -= convtab[idx].step;
+            break;
+        case PLA_CANCEL:
+            return;
+        default:
+            break;
+        }
+        rb->yield();
+    }
+}
+
+enum plugin_status plugin_start(const void* parameter)
+{
+    (void) parameter;
+    MENUITEM_STRINGLIST(menu, "Unit Converter", NULL,
+                        "Mass",
+                        "Quit");
+    bool quit = false;
+    while(!quit)
+    {
+        switch(rb->do_menu(&menu, NULL, NULL, false))
+        {
+        case 0:
+            /* mass */
+            do_unitconv(mass_table, ARRAYLEN(mass_table));
+            break;
+        case 1:
+            quit=true;
+            break;
+        default:
+            break;
+        }
+    }
+    return PLUGIN_OK;
+}
