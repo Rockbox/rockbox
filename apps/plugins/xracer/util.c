f6e39d74460eae64f30dadc65f9418a915047644
Franklin Wei <frankhwei536@gmail.com>
2015-01-07 16:40:33 -0500

    [WIP] XRacer - a racing game
    Plan:
    -----
    
    A simple racing game like Pole Position or Enduro.
    
    Status:
    -------
     - Currently generates a random road and scrolls through it
     - Random road generation (curves and hills)
     - Rather nice looking :D
     - Sprites in progress (see render() in graphics.c)
    
    Todo:
    -----
     - Convert to fixed-point math in all places
     - Improve/fix track looping
     - Improve random road generation to start and end at the same height
     - Finish sprite code
     - Make sprites
     - Game-ify!
    
    Change-Id: Ia2ff60b3c43d9f2e3a4f63e0ad90d2cb571c605e

---
+++ apps/plugins/xracer/util.c
@@ -0,0 +1,42 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2015 Franklin Wei
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
+
+#include "plugin.h"
+#include "util.h"
+
+void *util_alloc(size_t sz)
+{
+    static void *plugin_buffer = NULL;
+    static size_t bytes_left   = -1;
+    if(!plugin_buffer)
+    {
+        plugin_buffer = rb->plugin_get_buffer(&bytes_left);
+    }
+    void *ret = plugin_buffer;
+    plugin_buffer += sz;
+    if((signed long) bytes_left - (signed long) sz < 0)
+    {
+        LOGF("%s: OOM", __func__);
+        return NULL;
+    }
+    bytes_left -= sz;
+    return ret;
+}
