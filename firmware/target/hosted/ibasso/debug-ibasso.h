fc9821292441839bec381615cf320567c5afda5c
Udo Schläpfer <rockbox-2014.10@desktopwarrior.net>
2015-01-17 22:53:55 +0100

    iBasso DX50/DX90: Major code cleanup and reorganization.
    Reorganization
    
    - Separated iBasso devices from PLATFORM_ANDROID. These are now standlone
      hosted targets. Most device specific code is in the
      firmware/target/hosted/ibasso directory.
    - No dependency on Android SDK, only the Android NDK is needed.
      32 bit Android NDK and Android API Level 16.
    - Separate implementation for each device where feasible.
    
    Code cleanup
    
    - Rewrite of existing code, from simple reformat to complete reimplementation.
    - New backlight interface, seperating backlight from touchscreen.
    - Rewrite of device button handler, removing unneeded code and fixing memory
      leaks.
    - New Debug messages interface logging to Android adb logcat (DEBUGF, panicf,
      logf).
    - Rewrite of lcd device handler, removing unneeded code and fixing memory leaks.
    - Rewrite of audiohw device handler/pcm interface, removing unneeded code and
      fixing memory leaks.
    - Rewrite of power and powermng, proper shutdown, using batterylog results
      (see http://gerrit.rockbox.org/r/#/c/1047/).
    - Rewrite of configure (Android NDK) and device specific config.
    - Rewrite of the Android NDK specific Makefile.
    
    Misc
    
    - All plugins/games/demos activated.
    - Update tinyalsa to latest from https://github.com/tinyalsa/tinyalsa.
    
    Includes
    
    - http://gerrit.rockbox.org/r/#/c/993/
    - http://gerrit.rockbox.org/r/#/c/1010/
    - http://gerrit.rockbox.org/r/#/c/1035/
    
    Does not include http://gerrit.rockbox.org/r/#/c/1007/ due to new backlight
    interface and new option for hold switch, touchscreen, physical button
    interaction.
    
    To do
    
    See http://gerrit.rockbox.org/r/#/c/1099/
    - Forced shutdown of Rockbox by Android due to USB mass storage connection.
    - Applying this code clean up to the "bootloader".
    
    Tested on iBasso DX50.
    Compiled (not tested) for iBasso DX90.
    Compiled (not tested) for PLATFORM_ANDROID.
    
    Change-Id: I5f5e22e68f5b4cf29c28e2b40b2c265f2beb7ab7

---
+++ firmware/target/hosted/ibasso/debug-ibasso.h
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *             __________               __   ___
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2014 by Ilia Sergachev: Initial Rockbox port to iBasso DX50
+ * Copyright (C) 2014 by Mario Basister: iBasso DX90 port
+ * Copyright (C) 2014 by Simon Rothen: Initial Rockbox repository submission, additional features
+ * Copyright (C) 2014 by Udo Schläpfer: Code clean up, additional features
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+
+#ifndef _DEBUG_IBASSO_H_
+#define _DEBUG_IBASSO_H_
+
+
+void debug_trace(const char* function);
+
+
+#ifdef DEBUG
+#define TRACE debug_trace(__func__)
+#else
+#define TRACE
+#endif
+
+#endif
