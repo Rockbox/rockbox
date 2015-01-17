7505c983a360506932abb6538d66caf410700331
Udo Schläpfer <rockbox-2014.10@desktopwarrior.net>
2015-01-17 14:52:36 +0100

    iBasso DX50/DX90: User selectable hold switch mode.
    Depends on http://gerrit.rockbox.org/r/#/c/1043/.
    
    This patch adds a new setting in Settings -> General
    -> System: Hold Switch Lock Mode
    
    Possible settings are:
    - All: Default Rockbox behaviour, hold switch engaged will lock all input.
    - Touchscreen Only: Hold switch engaged will lock touch screen only.
    Physical buttons continue to work.
    - Touchscreen, Power, Volume: Hold switch engaged will lock touch screen,
    power and volume buttons. Left, play and right will continue to work.
    
    German translation provided.
    
    Change-Id: Idbdb368fed98483ec60f7eb41471bc34f755ecc1

--- apps/settings_list.c
+++ apps/settings_list.c
@@ -2150,6 +2150,20 @@ const struct settings_list settings[] = {
                   NULL, "root menu order",
                   root_menu_load_from_cfg, root_menu_write_to_cfg,
                   root_menu_is_changed, root_menu_set_default),
+
+#if defined(DX50) || defined(DX90)
+    CHOICE_SETTING(0,
+                   hold_switch_mode,
+                   LANG_IBASSO_HOLD_SWITCH_LOCK_MODE,
+                   HOLD_SWITCH_LOCK_ALL,
+                   "hold switch mode",
+                   "all,touch,touch power volume",
+                   NULL,
+                   3,
+                   ID2P(LANG_IBASSO_HOLD_SWITCH_MODE_LOCK_ALL),
+                   ID2P(LANG_IBASSO_HOLD_SWITCH_MODE_LOCK_TOUCH),
+                   ID2P(LANG_IBASSO_HOLD_SWITCH_MODE_LOCK_TOUCH_POWER_VOLUME)),
+#endif
 };
 
 const int nb_settings = sizeof(settings)/sizeof(*settings);
