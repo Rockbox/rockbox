9de09e052699371b518a26863d9561895daaa2b6
Michael Giacomelli <giac2000@hotmail.com>
2015-01-19 21:00:10 +0100

    Disable new DSP effects in our command line test program, warble.
    This shouldn't be necessary, but at the moment core_alloc isn't
    implemented in warble, and writing a wrapper around malloc would be
    a fair amount of effort for something that would most likely only be
    used when debugging DSP effects.
    
    Change-Id: If4dc60c39eb8e3d28014f58f2e6d4a90932cc49a

--- lib/rbcodec/dsp/dsp_proc_database.h
+++ lib/rbcodec/dsp/dsp_proc_database.h
@@ -46,9 +46,11 @@ DSP_PROC_DB_START
 #ifdef HAVE_SW_TONE_CONTROLS
     DSP_PROC_DB_ITEM(TONE_CONTROLS) /* bass and treble */
 #endif
+#ifndef WARBLE
     DSP_PROC_DB_ITEM(PBE)           /* perceptual bass enhancement */
     DSP_PROC_DB_ITEM(AFR)           /* auditory fatigue reduction */
     DSP_PROC_DB_ITEM(SURROUND)      /* haas surround */
+#endif
     DSP_PROC_DB_ITEM(CHANNEL_MODE)  /* channel modes */
     DSP_PROC_DB_ITEM(COMPRESSOR)    /* dynamic-range compressor */
 DSP_PROC_DB_STOP
