314b705e88d7de64a93f3134c1ad4b75b677ee52
Udo Schläpfer <rockbox-2014.10@desktopwarrior.net>
2015-01-23 21:26:41 +0100

    iBasso DX50/DX90: Major code cleanup and reorganization.
    Reorganization
    
    - Separated iBasso devices from PLATFORM_ANDROID. These are now standlone
      hosted targets. Most device specific code is in the
      firmware/target/hosted/ibasso directory.
    - No dependency on Android SDK, only the Android NDK is needed.
      32 bit Android NDK and Android API Level 16.
    - Separate implementation for each device where feasible.
    
    Code cleanup
    
    - Rewrite of existing code, from simple reformat to complete reimplementation.
    - New backlight interface, seperating backlight from touchscreen.
    - Rewrite of device button handler, removing unneeded code and fixing memory
      leaks.
    - New Debug messages interface logging to Android adb logcat (DEBUGF, panicf,
      logf).
    - Rewrite of lcd device handler, removing unneeded code and fixing memory leaks.
    - Rewrite of audiohw device handler/pcm interface, removing unneeded code and
      fixing memory leaks, enabling 44.1/48kHz pthreaded playback.
    - Rewrite of power and powermng, proper shutdown, using batterylog results
      (see http://gerrit.rockbox.org/r/#/c/1047/).
    - Rewrite of configure (Android NDK) and device specific config.
    - Rewrite of the Android NDK specific Makefile.
    
    Misc
    
    - All plugins/games/demos activated.
    - Update tinyalsa to latest from https://github.com/tinyalsa/tinyalsa.
    
    Includes
    
    - http://gerrit.rockbox.org/r/#/c/993/
    - http://gerrit.rockbox.org/r/#/c/1010/
    - http://gerrit.rockbox.org/r/#/c/1035/
    
    Does not include http://gerrit.rockbox.org/r/#/c/1007/ due to new backlight
    interface and new option for hold switch, touchscreen, physical button
    interaction.
    
    To do
    
    See http://gerrit.rockbox.org/r/#/c/1099/
    - Forced shutdown of Rockbox by Android due to USB mass storage connection.
    - Applying this code clean up to the "bootloader".
    
    Tested on iBasso DX50.
    Compiled (not tested) for iBasso DX90.
    Compiled (not tested) for PLATFORM_ANDROID.
    
    Change-Id: I5f5e22e68f5b4cf29c28e2b40b2c265f2beb7ab7

---
+++ firmware/target/hosted/ibasso/sysfs-ibasso.h
@@ -0,0 +1,111 @@
+/***************************************************************************
+ *             __________               __   ___
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2014 by Ilia Sergachev: Initial Rockbox port to iBasso DX50
+ * Copyright (C) 2014 by Mario Basister: iBasso DX90 port
+ * Copyright (C) 2014 by Simon Rothen: Initial Rockbox repository submission, additional features
+ * Copyright (C) 2014 by Udo Schläpfer: Code clean up, additional features
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+
+#ifndef _SYSFS_IBASSO_H_
+#define _SYSFS_IBASSO_H_
+
+
+#include <stdbool.h>
+
+
+/*
+    Sys FS path identifiers.
+    See SYSFS_PATHS in sysfs-ibasso.c.
+*/
+enum sys_fs_interface_id
+{
+    SYSFS_DX50_CODEC_VOLUME = 0,
+    SYSFS_HOLDKEY,
+    SYSFS_DX90_ES9018_VOLUME,
+    SYSFS_MUTE,
+    SYSFS_WM8740_MUTE,
+    SYSFS_BATTERY_CAPACITY,
+    SYSFS_BATTERY_CURRENT_NOW,
+    SYSFS_BATTERY_ENERGY_FULL_DESIGN,
+    SYSFS_BATTERY_HEALTH,
+    SYSFS_BATTERY_MODEL_NAME,
+    SYSFS_BATTERY_ONLINE,
+    SYSFS_BATTERY_PRESENT,
+    SYSFS_BATTERY_STATUS,
+    SYSFS_BATTERY_TECHNOLOGY,
+    SYSFS_BATTERY_TEMP,
+    SYSFS_BATTERY_TYPE,
+    SYSFS_BATTERY_VOLTAGE_MAX_DESIGN,
+    SYSFS_BATTERY_VOLTAGE_MIN_DESIGN,
+    SYSFS_BATTERY_VOLTAGE_NOW,
+    SYSFS_USB_POWER_CURRENT_NOW,
+    SYSFS_USB_POWER_ONLINE,
+    SYSFS_USB_POWER_PRESENT,
+    SYSFS_USB_POWER_VOLTAGE_NOW,
+    SYSFS_BACKLIGHT_POWER,
+    SYSFS_BACKLIGHT_BRIGHTNESS,
+    SYSFS_POWER_STATE,
+    SYSFS_POWER_WAKE_LOCK
+};
+
+
+/*
+    Read a integer value from the sys fs interface given by id.
+    Returns true on success, false else.
+*/
+bool sysfs_get_int(enum sys_fs_interface_id id, int* value);
+
+
+/*
+    Write a integer value to the sys fs interface given by id.
+    Returns true on success, false else.
+*/
+bool sysfs_set_int(enum sys_fs_interface_id id, int value);
+
+
+/*
+    Read a char value from the sys fs interface given by id.
+    Returns true on success, false else.
+*/
+bool sysfs_get_char(enum sys_fs_interface_id id, char* value);
+
+
+/*
+    Write a char value to the sys fs interface given by id.
+    Returns true on success, false else.
+*/
+bool sysfs_set_char(enum sys_fs_interface_id id, char value);
+
+/*
+    Read a single line of text from the sys fs interface given by id.
+    A newline will be discarded.
+    size: The size of value.
+    Returns true on success, false else.
+*/
+bool sysfs_get_string(enum sys_fs_interface_id id, char* value, int size);
+
+
+/*
+    Write text to the sys fs interface given by id.
+    Returns true on success, false else.
+*/
+bool sysfs_set_string(enum sys_fs_interface_id id, char* value);
+
+
+#endif
