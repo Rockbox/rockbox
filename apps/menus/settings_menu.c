57d020f766a92ed4c99082ae23f032ad0c4c1c31
Udo Schläpfer <rockbox-2014.10@desktopwarrior.net>
2015-01-30 22:47:30 +0100

    iBasso DX50/DX90: User selectable freq scaling governor.
    Depends on http://gerrit.rockbox.org/r/#/c/1043/.
    
    This patch adds a new setting in Settings -> General
    -> System: Freq Scaling Governor
    
    Usable in Quickscreen and Shortcuts.
    
    Possible settings are:
    - Conservative: Slow frequency switching.
    - Ondemand or Interactive: Fast frequency switching.
    - Powersave: Allways lowest frequency.
    - Performance: Allways highest frequency.
    
    German translation provided.
    
    This may be genric for Android kernel based devices but is only enabled
    for iBasso Devices. Other maintainers may choose do adopt this.
    
    Change-Id: I10296f5be9586ad3a409105db0cd03682a30e9c1

--- apps/menus/settings_menu.c
+++ apps/menus/settings_menu.c
@@ -50,6 +50,10 @@
 #endif
 #include "folder_select.h"
 
+#if defined(DX50) || defined(DX90)
+#include "governor-ibasso.h"
+#endif
+
 /***********************************/
 /*    TAGCACHE MENU                */
 #ifdef HAVE_TAGCACHE
@@ -325,6 +329,10 @@ MENUITEM_SETTING(touchpad_deadzone, &global_settings.touchpad_deadzone, NULL);
 MENUITEM_SETTING(shortcuts_replaces_quickscreen, &global_settings.shortcuts_replaces_qs, NULL);
 #endif
 
+#if defined(DX50) || defined(DX90)
+MENUITEM_SETTING(governor, &global_settings.governor, NULL);
+#endif
+
 MAKE_MENU(system_menu, ID2P(LANG_SYSTEM),
           0, Icon_System_menu,
 #if (BATTERY_CAPACITY_INC > 0) || (BATTERY_TYPES_COUNT > 1)
@@ -376,6 +384,10 @@ MAKE_MENU(system_menu, ID2P(LANG_SYSTEM),
 #endif
 #if defined(USB_ENABLE_STORAGE) && defined(HAVE_MULTIDRIVE)
             &usb_skip_first_drive,
+#endif
+
+#if defined(DX50) || defined(DX90)
+            &governor,
 #endif
          );
 
---
+++ firmware/target/hosted/ibasso/governor-ibasso.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+ *             __________               __   ___
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2014 by Ilia Sergachev: Initial Rockbox port to iBasso DX50
+ * Copyright (C) 2014 by Mario Basister: iBasso DX90 port
+ * Copyright (C) 2014 by Simon Rothen: Initial Rockbox repository submission, additional features
+ * Copyright (C) 2014 by Udo Schläpfer: Code clean up, additional features
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+
+#ifndef _GOVERNOR_IBASSO_H_
+#define _GOVERNOR_IBASSO_H_
+
+
+/* Supported freq scaling governors. */
+enum ibasso_governors
+{
+    /* Slow frequency switching. */
+    GOVERNOR_CONSERVATIVE = 0,
+
+    /* Fast frequency switching. */
+    GOVERNOR_ONDEMAND,
+    GOVERNOR_INTERACTIVE,
+
+    /* Allways lowest frequency. */
+    GOVERNOR_POWERSAVE,
+
+    /* Allways highest frequency. */
+    GOVERNOR_PERFORMANCE
+};
+
+
+/*
+    Set the active freq scaling governor.
+    governor: ibasso_governors
+*/
+void ibasso_set_governor(int governor);
+
+
+#endif
