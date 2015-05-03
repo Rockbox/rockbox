e37dab655e27d8a8b7c145870e9b2052db5db293
Boris Gjenero <boris.gjenero@gmail.com>
2015-05-03 12:32:39 -0400

    Storage align test_disk buffer so DMA can be used for aligned tests
    Change-Id: I32ef0866a2902a6af94d7f59ecf1712cb7e8fef4

--- apps/plugins/test_disk.c
+++ apps/plugins/test_disk.c
@@ -439,7 +439,6 @@ enum plugin_status plugin_start(const void* parameter)
                         "Disk speed", "Write & verify");
     int selected=0;
     bool quit = false;
-    int align;
     DIR *dir;
 
     (void)parameter;
@@ -458,10 +457,8 @@ enum plugin_status plugin_start(const void* parameter)
     }
 
     audiobuf = rb->plugin_get_audio_buffer(&audiobuflen);
-    /* align start and length to 32 bit */
-    align = (-(intptr_t)audiobuf) & 3;
-    audiobuf += align;
-    audiobuflen = (audiobuflen - align) & ~3;
+    /* align start and length for optimum storage access */
+    STORAGE_ALIGN_BUFFER(audiobuf, audiobuflen);
 
     rb->srand(*rb->current_tick);
 
