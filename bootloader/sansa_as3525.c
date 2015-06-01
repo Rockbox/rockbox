Rockbox/rockbox

commit 8b8b85433f6e9ac673adb155b3bd82d56830a240

Fix USB speed detection
Change-Id: I615d2d5366ffa1e2e9c6b0837bf5a62a7e471fca
Reviewed-on: http://gerrit.rockbox.org/994
Reviewed-by: Frank Gevaerts <frank@gevaerts.be>
Tested: Mihail Zenkov <mihail.zenkov@gmail.com>
Reviewed-by: Marcin Bukat <marcin.bukat@gmail.com>

bootloader/sansa_as3525.c
@@ -124,11 +124,13 @@ void main(void)
 
 #ifdef USE_ROCKBOX_USB
     usb_init();
-    usb_start_monitoring();
 
     /* Enter USB mode if USB is plugged and SELECT button is pressed */
-    if(btn & BUTTON_SELECT && usb_detect() == USB_INSERTED)
-        usb_mode();
+    if(btn & BUTTON_SELECT) {
+        usb_start_monitoring();
+        if(usb_detect() == USB_INSERTED)
+            usb_mode();
+    }
 #endif /* USE_ROCKBOX_USB */
 
     while((ret = disk_mount_all()) <= 0)

commit d80228ad2dffd9192ef2a9315725967ff2571f3e

AMS: fix usb initialization in bootloader
Regression after 8b8b85433f6e9ac673adb

Change-Id: I86389be0b85c5c2ad8a32d7089a6a79a6b7c8708

bootloader/sansa_as3525.c
@@ -137,6 +137,7 @@ void main(void)
     {
 #ifdef HAVE_BOOTLOADER_USB_MODE
         error(EDISK, ret, false);
+        usb_start_monitoring();
         usb_mode();
 #else
         error(EDISK, ret, true);
@@ -152,6 +153,7 @@ void main(void)
     {
 #ifdef HAVE_BOOTLOADER_USB_MODE
         error(EBOOTFILE, ret, false);
+        usb_start_monitoring();
         usb_mode();
 #else
         error(EBOOTFILE, ret, true);