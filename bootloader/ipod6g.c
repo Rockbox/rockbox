fd936f670d9ba8d90e62c0223735ebd12ab1a85a
Cástor Muñoz <cmvidal@gmail.com>
2015-09-26 05:09:06 +0200

    iPod Classic: dual bootloader - v1
    First of all, this bootloader is based at 90% on emCORE code,
    so you know who to blame if you iPod gets bricked.
    
    Seriously, it is very difficult (if not impossible) to brick an
    iPod 6G. At most it could need to be restored using iTunes, so
    backup your songs before installing the dualboot, anyways this
    should not happen with this current version, i have tested it
    dozens of times on two devices with no problem.
    
    mkdfu generates a .dfu file containing an installer/uninstaller
    and the RB bootloader. The installer moves the original NORBOOT
    and install the RB dual bootloader. When the iPod boots then the
    RB dual bootloader launch Rockbox or the OF using the original
    NORBOOT.
    
    See mk6gboot/README for more info on how to install.
    
    I started this as a preliminary version, actually it is functional,
    but lots of things needs to be done:
    
    TODO:
    - mk6gboot: only tested on Linux, needs to integrate ipoddfu_c
      utility by user890104, add the new target to RB installation
      utility, and test everything on Windows and Linux.
    - some rearrangement of the code, there is a clickwheel patch
      included that should be separated, optimize NOR read/write
      using DMA, battery trap...
    - DFU graphic install/uninstall, does it worth?
    - ...
    
    Change-Id: If45e2927ee75ae797efbbf5675f2b454fdc72f5d

---
+++ bootloader/ipod6g.c
@@ -0,0 +1,533 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2005 by Dave Chapman
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+#include "config.h"
+
+#include "inttypes.h"
+#include "cpu.h"
+#include "system.h"
+#include "lcd.h"
+#include "../kernel-internal.h"
+#include "file_internal.h"
+#include "storage.h"
+#include "fat.h"
+#include "disk.h"
+#include "font.h"
+#include "backlight.h"
+#include "backlight-target.h"
+#include "button.h"
+#include "panic.h"
+#include "power.h"
+#include "file.h"
+#include "common.h"
+#include "rb-loader.h"
+#include "loader_strerror.h"
+#include "version.h"
+
+#include "s5l8702.h"
+#include "pmu-target.h"
+#include "i2c-s5l8702.h"
+#include "clocking-s5l8702.h"
+#include "crypto-s5l8702.h"
+#include "cwheel-s5l8702.h"
+#include "piezo.h"
+
+
+/*
+ * Preliminary HW initialization
+ */
+
+/* -- clocks and power -- */
+void syscon_preinit(void)
+{
+    /* at this moment CG16_SYS is using PLL0 @108 MHz
+       CClk = 108 MHz, HClk = 54 MHz, PClk = 27 MHz */
+
+    CLKCON0 &= ~CLKCON0_SDR_DISABLE_BIT;
+
+    PLLMODE &= ~PLLMODE_OSCSEL_BIT; /* CG16_SEL_OSC = OSC0 */
+    cg16_config(&CG16_SYS, true, CG16_SEL_OSC, 1, 1);
+    soc_set_system_divs(1, 1, 1);
+
+    /* stop all PLLs */
+    for (int pll = 0; pll < 3; pll++)
+        pll_onoff(pll, false);
+
+    pll_config(2, PLLOP_DM, 1, 36, 1, 32400);
+    pll_onoff(2, true);
+    soc_set_system_divs(1, 2, 2 /*hprat*/);
+    cg16_config(&CG16_SYS,   true,  CG16_SEL_PLL2, 1, 1);
+    cg16_config(&CG16_2L,    false, CG16_SEL_OSC,  1, 1);
+    cg16_config(&CG16_SVID,  false, CG16_SEL_OSC,  1, 1);
+    cg16_config(&CG16_AUD0,  false, CG16_SEL_OSC,  1, 1);
+    cg16_config(&CG16_AUD1,  false, CG16_SEL_OSC,  1, 1);
+    cg16_config(&CG16_AUD2,  false, CG16_SEL_OSC,  1, 1);
+    cg16_config(&CG16_RTIME, true,  CG16_SEL_OSC,  1, 1);
+    cg16_config(&CG16_5L,    false, CG16_SEL_OSC,  1, 1);
+
+    soc_set_hsdiv(1);
+
+    PWRCON_AHB = ~((1 << CLOCKGATE_SMx) |
+                   (1 << CLOCKGATE_SM1));
+    PWRCON_APB = ~((1 << (CLOCKGATE_TIMER - 32)) |
+                   (1 << (CLOCKGATE_GPIO - 32)));
+}
+
+/* -- GPIO -- */
+static uint32_t gpio_data[16] =
+{
+    0x5322222F, 0xEEEEEE00, 0x2332EEEE, 0x3333E222,
+    0x33333333, 0x33333333, 0x3F000E33, 0xEEEEEEEE,
+    0xEEEEEEEE, 0xEEEEEEEE, 0xE0EEEEEE, 0xEE00EE0E,
+    0xEEEE0EEE, 0xEEEEEEEE, 0xEE2222EE, 0xEEEE0EEE
+};
+
+void gpio_preinit(void)
+{
+    for (int i = 0; i < 16; i++) {
+        PCON(i) = gpio_data[i];
+        PUNB(i) = 0;
+        PUNC(i) = 0;
+    }
+}
+
+/* -- i2c -- */
+void i2c_preinit(int bus)
+{
+    #define wait_rdy(bus)   {while(IICUNK10(bus));}
+
+    clockgate_enable(CLOCKGATE_I2C0, true);
+    wait_rdy(bus);
+    IICADD(bus) = 0x40;   /* own slave address */
+    wait_rdy(bus);
+    IICUNK14(bus) = 0;
+    wait_rdy(bus);
+    IICUNK18(bus) = 0;
+    wait_rdy(bus);
+    IICSTAT(bus) = 0x80;  /* master Rx mode, Tx/Rx off */
+    wait_rdy(bus);
+    IICCON(bus) = 0;
+    wait_rdy(bus);
+    IICSTAT(bus) = 0;     /* slave Rx mode, Tx/Rx off */
+    wait_rdy(bus);
+    IICDS(bus) = 0x40;
+    wait_rdy(bus);
+    IICCON(bus) = (1 << 8) |  /* unknown */
+                  (1 << 7) |  /* ACK_GEN */
+                  (0 << 6) |  /* CLKSEL = PCLK/16 */
+                  (0 << 5) |  /* INT_EN = disabled */
+                  (4 << 0);   /* CK_REG */
+    wait_rdy(bus);
+    IICSTAT(bus) = 0x10;  /* slave Rx mode, Tx/Rx on */
+    wait_rdy(bus);
+    clockgate_enable(CLOCKGATE_I2C0, false);
+}
+
+/* -- PMU -- */
+#include "pcf5063x.h"
+void pmu_preinit(void)
+{
+    unsigned char rd_buf[8];
+
+    /* reset OOC shutdown register */
+    pmu_wr(PCF5063X_REG_OOCSHDWN, 0);
+
+    /* configure LDOs (mV = 900+100*val) */
+    pmu_wr_multiple(PCF5063X_REG_LDO1OUT, 14,
+            "\x15\x01"    /* LDO_UNK1:   3000 mV, enabled */
+            "\x15\x01"    /* LDO_UNK2:   3000 mV, enabled */
+            "\x15\x01"    /* LDO_LCD:    3000 mV, enabled */
+            "\x09\x01"    /* LDO_CODEC:  1800 mV, enabled */
+            "\x15\x01"    /* LDO_UNK5:   3000 mV, disabled */
+            "\x15\x04"    /* LDO_CWHEEL: 3000 mV, ON when GPIO2==1 */
+            "\x18\x00");  /* LDO_ACCY:   3300 mV, disabled */
+
+    /* LDO_CWHEEL is ON in standby state,
+       LDO_CWHEEL and MEMLDO are ON in UNKNOWN state (TBC) */
+    pmu_wr_multiple(PCF5063X_REG_STBYCTL1, 2, "\x00\x8C");
+
+    /* GPIO1,2 = input, GPIO3 = output */
+    pmu_wr_multiple(PCF5063X_REG_GPIOCTL, 3, "\x03\x00\x00");
+
+    /* GPO selection = fixed 0 */
+    pmu_wr(PCF5063X_REG_GPOCFG, 0);
+
+    /* DOWN2 converter (SDRAM): 1800 mV (625+25*val), enabled,
+       startup current limit = 15mA*0x10 (TBC) */
+    pmu_wr_multiple(PCF5063X_REG_DOWN2OUT, 4, "\x2F\x01\x00\x10");
+    /* MEMLDO: 1800 mV (900+100*val), enabled */
+    pmu_wr_multiple(PCF5063X_REG_MEMLDOOUT, 2, "\x09\x01");
+
+    /* AUTOLDO (HDD): 3400 mV (625+25*val), disabled,
+       current limit = 40mA*0x19 = 1000 mA, limit always active */
+    pmu_wr_multiple(PCF5063X_REG_AUTOOUT, 4, "\x6F\x00\x00\x59");
+
+    /* Vbatok = 3.2V, Vsysok = 2.9V, 62ms debounce filter enabled */
+    pmu_wr_multiple(PCF5063X_REG_BVMCTL, 2, "\x0A\x04");
+
+    pmu_wr(0x58, 0); /* reserved */
+
+    /* configure PMU interrupt masks */
+    pmu_wr_multiple(PCF5063X_REG_INT1M, 5, "\xB0\x03\xFE\xFC\xFF");
+    pmu_wr(IPOD6G_PCF50635_REG_INT6M, 0xFD);
+
+    /* OCC: all enabled (!ONKEY, EXTON1..3, RTC, USB, ADAPTER),
+       config debounce time, activation phases and internal CLK32K,
+       wakeup on rising edge for EXTON1 and EXTON2,
+       wakeup on falling edge for EXTON3 and !ONKEY */
+    pmu_wr_multiple(PCF5063X_REG_OOCWAKE, 5, "\xDF\xAA\x4A\x05\x27");
+
+    /* clear PMU interrupts */
+    pmu_rd_multiple(PCF5063X_REG_INT1, 5, rd_buf);
+    pmu_rd(IPOD6G_PCF50635_REG_INT6);
+
+    /* GPO selection = LED external NFET drive signal */
+    pmu_wr(PCF5063X_REG_GPOCFG, 1);
+    /* LED converter OFF, overvoltage protection enabled,
+       OCP limit is 500 mA, led_dimstep = 16 * 0x06 / 32768 */
+    pmu_wr_multiple(PCF5063X_REG_LEDENA, 3, "\x00\x05\x06");
+}
+
+/* -- MIU -- */
+void miu_preinit(bool selfrefreshing)
+{
+    if (selfrefreshing)
+        MIUCON = 0x11;      /* TBC: self-refresh -> IDLE */
+
+    MIUCON = 0x80D;         /* remap = 1 (IRAM mapped to 0x0),
+                               TBC: SDRAM bank and column configuration */
+    MIU_REG(0xF0) = 0x0;
+
+    MIUAREF = 0x6105D;      /* Auto-Refresh enabled,
+                               Row refresh interval = 0x5d/12MHz = 7.75 uS */
+    MIUSDPARA = 0x1FB621;
+
+    MIU_REG(0x200) = 0x1845;
+    MIU_REG(0x204) = 0x1845;
+    MIU_REG(0x210) = 0x1800;
+    MIU_REG(0x214) = 0x1800;
+    MIU_REG(0x220) = 0x1845;
+    MIU_REG(0x224) = 0x1845;
+    MIU_REG(0x230) = 0x1885;
+    MIU_REG(0x234) = 0x1885;
+    MIU_REG(0x14) = 0x19;       /* 2^19 = 0x2000000 = SDRAMSIZE (32Mb) */
+    MIU_REG(0x18) = 0x19;       /* 2^19 = 0x2000000 = SDRAMSIZE (32Mb) */
+    MIU_REG(0x1C) = 0x790682B;
+    MIU_REG(0x314) &= ~0x10;
+
+    for (int i = 0; i < 0x24; i++)
+        MIU_REG(0x2C + i*4) &= ~(1 << 24);
+
+    MIU_REG(0x1CC) = 0x540;
+    MIU_REG(0x1D4) |= 0x80;
+
+    MIUCOM = 0x33;     /* No action CMD */
+    MIUCOM = 0x33;
+    MIUCOM = 0x233;    /* Precharge all banks CMD */
+    MIUCOM = 0x33;
+    MIUCOM = 0x33;
+    MIUCOM = 0x33;
+    MIUCOM = 0x333;    /* Auto-refresh CMD */
+    MIUCOM = 0x33;
+    MIUCOM = 0x33;
+    MIUCOM = 0x33;
+    MIUCOM = 0x333;    /* Auto-refresh CMD */
+    MIUCOM = 0x33;
+    MIUCOM = 0x33;
+    MIUCOM = 0x33;
+
+    if (!selfrefreshing)
+    {
+        MIUMRS = 0x33;    /* MRS: Bust Length = 8, CAS = 3 */
+        MIUCOM = 0x133;   /* Mode Register Set CMD */
+        MIUCOM = 0x33;
+        MIUCOM = 0x33;
+        MIUCOM = 0x33;
+        MIUMRS = 0x8040;  /* EMRS: Strength = 1/4, Self refresh area = Full */
+        MIUCOM = 0x133;   /* Mode Register Set CMD */
+        MIUCOM = 0x33;
+        MIUCOM = 0x33;
+        MIUCOM = 0x33;
+    }
+
+    MIUAREF |= 0x61000;   /* Auto-refresh enabled */
+}
+
+static void hw_preinit(void)
+{
+    bool gpio3out, coldboot;
+
+    syscon_preinit();
+    gpio_preinit();
+    i2c_preinit(0);
+
+    /* get (previously) configured output selection for GPIO3 */
+    gpio3out = (pmu_rd(PCF5063X_REG_GPIO3CFG) & 7);
+    /* coldboot: when set, device has been in NoPower state */
+    coldboot = (pmu_rd(PCF5063X_REG_OOCSHDWN) >> 3) & 1;
+
+    pmu_preinit();
+    miu_preinit(!coldboot && !gpio3out);
+}
+
+
+/* Safety measure - maximum allowed firmware image size.
+   The largest known current (October 2009) firmware is about 6.2MB so
+   we set this to 8MB.
+*/
+#define MAX_LOADSIZE (8*1024*1024)
+
+#define EMCORE_BIN "/.apps/emcore.bin"
+
+void bss_init(void);
+extern uint32_t _movestart;
+extern uint32_t start_loc;
+
+/* The buffer to load the firmware into - use an uncached alias of 0x8000000 */
+unsigned char *loadbuffer = (unsigned char *)S5L8702_UNCACHED_ADDR(0x8000000);
+
+/* tone sequences: period (uS), duration (ms), silence (ms) */
+static uint16_t alive[] = { 500,100,0, 0 };
+
+extern int line;
+
+void fatal_error(void)
+{
+    extern int line;
+    bool holdstatus=false;
+
+    /* System font is 6 pixels wide */
+    printf("Hold MENU+SELECT to");
+    printf("reboot then SELECT+PLAY");
+    printf("for disk mode");
+    lcd_update();
+
+    while (1) {
+        if (button_hold() != holdstatus) {
+            if (button_hold()) {
+                holdstatus=true;
+                lcd_puts(0, line, "Hold switch on!");
+            } else {
+                holdstatus=false;
+                lcd_puts(0, line, "               ");
+            }
+            lcd_update();
+        }
+    }
+}
+
+static int load_bin(unsigned char* buf, const char* filename, int buffer_size)
+{
+    int fd;
+    int rc;
+    int len;
+    int ret = 0;
+
+    /* full path passed */
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+        return EFILE_NOT_FOUND;
+
+    len = filesize(fd) - 8;
+
+    if (len > buffer_size)
+    {
+        ret = EFILE_TOO_BIG;
+        goto end;
+    }
+
+    rc = read(fd, buf, len);
+    if(rc < len)
+    {
+        ret = EREAD_IMAGE_FAILED;
+        goto end;
+    }
+
+    ret = len;
+
+end:
+    close(fd);
+    return ret;
+}
+
+/* IM3 size aligned to NOR sector size */
+#define GET_NOR_SZ(hinfo) \
+    (ALIGN_UP(IM3HDR_SZ + get_uint32le((hinfo)->data_sz), 0x1000))
+
+static uint32_t get_uint32le(unsigned char *p)
+{
+    return p[0] | (p[1] << 8) | (p[2] << 16) | (p[3] << 24);
+}
+
+static int load_of(void)
+{
+    struct Im3Info *hinfo;
+    void *fw_addr;
+    int nor_offset;
+
+    /* ROMBOOT loads IM3 header at IRAM0_ORIG and data at IRAM1_ORIG,
+     * actually IRAM1_ORIG contains current RB loader IM3 header, it
+     * will be replaced by OF IM3 header.
+     */
+    hinfo = (struct Im3Info*)IRAM1_ORIG; /* actual RB loader IM3 hdr */
+    fw_addr = (void*)IRAM0_ORIG;
+    nor_offset = NORBOOT_OFF + GET_NOR_SZ(hinfo); /* OF NOR offset */
+
+    if (read_im3(nor_offset, hinfo, fw_addr))
+        return 0;
+
+    return -1;
+}
+
+void main(void)
+{
+    int btn = BUTTON_NONE;
+    int rc = 0;
+    bool button_was_held;
+    char *filename, *bootname;
+
+    usec_timer_init();
+    piezo_seq(alive);
+
+    int32_t stop_time = USEC_TIMER + 1000000;
+
+    /* Check the button hold status as soon as possible - to
+       give the user maximum chance to turn it on in order to
+       reset the settings in rockbox. */
+    cwheel_set_gpio(CWMODE_MAN | CWFLAG_HSPOLL);
+    button_was_held = button_hold();
+
+    /* loads OF in IRAM0, interrupt vectors are destroyed */
+    rc = load_of();
+
+    while ((int32_t)USEC_TIMER - stop_time < 0);
+
+    if (!button_was_held)
+        btn = read_all_buttons(5);
+
+    if (button_was_held ||
+            ((btn != BUTTON_NONE) && (btn != BUTTON_RIGHT))) {
+        if (rc >= 0)
+            /* Branch to start of IRAM */
+            asm volatile("mov pc, %0"::"r"(IRAM0_ORIG));
+    }
+    /* clever decision! restore interrupt vectors */
+    memcpy((void*)IRAM0_ORIG, &_movestart, 4*(&start_loc-&_movestart));
+
+    hw_preinit();
+
+    memory_init();
+
+    /*
+     * XXX: BSS is initialized now, do not use .bss before this line
+     */
+    bss_init();
+
+    system_init();
+    kernel_init();
+    i2c_init();
+
+    enable_irq();
+
+    lcd_init();
+
+    lcd_set_foreground(LCD_WHITE);
+    lcd_set_background(LCD_BLACK);
+    lcd_clear_display();
+    lcd_update();
+    sleep(HZ/30);
+
+    font_init();
+    lcd_setfont(FONT_SYSFIXED);
+
+    button_init();
+
+    /* TODO: battery trap */
+    /*powermgmt_init();*/
+
+    backlight_init(); /* Turns on the backlight */
+
+    verbose = true;
+
+    printf("Rockbox boot loader");
+    printf("Version: %s", rbversion);
+
+    if (rc<0) {
+        printf("load OF error: %d",rc);
+        fatal_error();
+    }
+
+    rc = storage_init();
+    if (rc != 0) {
+        printf("ATA error: %d", rc);
+        fatal_error();
+    }
+
+    filesystem_init();
+
+    rc = disk_mount_all();
+    if (rc<=0) {
+        printf("No partition found");
+        fatal_error();
+    }
+
+    if (btn == BUTTON_RIGHT) {
+        bootname = "emCORE";
+        filename = EMCORE_BIN;
+        printf("Loading %s...", bootname);
+        rc = load_bin(loadbuffer, filename, MAX_LOADSIZE);
+    }
+    else {
+        bootname = "Rockbox";
+        filename = BOOTFILE;
+        printf("Loading %s...", bootname);
+        rc = load_firmware(loadbuffer, filename, MAX_LOADSIZE);
+    }
+
+    if (rc <= EFILE_EMPTY) {
+        printf("Error!");
+        printf("Can't load %s: ", filename);
+        printf(loader_strerror(rc));
+        fatal_error();
+    }
+
+    printf("%s loaded.", bootname);
+
+    /* If we get here, we have a new firmware image at 0x08000000, run it */
+    printf("Executing...");
+
+    disable_irq();
+
+    /* Disable caches and protection unit */
+    asm volatile(
+        "mrc 15, 0, r0, c1, c0, 0 \n"
+        "bic r0, r0, #0x1000      \n"
+        "bic r0, r0, #0x5         \n"
+        "mcr 15, 0, r0, c1, c0, 0 \n"
+    );
+
+    /* Branch to start of DRAM */
+    asm volatile("mov pc, %0"::"r"(DRAM_ORIG));
+}
---
+++ rbutil/mk6gboot/dualboot/dualboot.c
@@ -0,0 +1,270 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2015 by Cástor Muñoz
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#include <stdint.h>
+#include <string.h>
+
+#include "config.h"
+#include "system.h"
+#include "s5l8702.h"
+#include "clocking-s5l8702.h"
+#include "crypto-s5l8702.h"
+#include "nor-target.h"
+#include "piezo.h"
+
+/*
+ *         1MB  ______________
+ *             |              |
+ *             |     DIR      |
+ *   1MB-0x200 |______________|
+ *             |              |
+ *             |    File 1    |
+ *             |..............|
+ *             |              |
+ *             .              .
+ *             .              .
+ *             .              .
+ *             |              |
+ *             |..............|
+ *             |              |                 .              .
+ *             |    File N    |                 .              .
+ *             |______________|                 |______________|
+ *             |              |                 |              |
+ *             |              |                 |              |
+ *             |              |                 |     FREE     |
+ *             |              |                 |              |
+ *             |     FREE     |                 |______________|
+ *             |              |                 |              |
+ *             |              |                 |              |
+ *             |              |                 |      OF      |
+ *     <=160KB |______________|                 |   NOR Boot   |
+ *             |              |                 |              |
+ *             |              |                 |              |
+ *             |      OF      |       32KB+BLSZ |______________|
+ *             |   NOR Boot   |                 |              |
+ *             |              |                 |   Rockbox    |
+ *             |              |                 |  Bootloader  |
+ *        32KB |______________|            32KB |______________|
+ *             |              |                 |              |
+ *             |              |                 .              .
+ *             |              |                 .              .
+ *             |______________|
+ *             |              |
+ *             | Device ident |
+ *           0 |______________|
+ *
+ */
+
+#define OF_LOADADDR     IRAM1_ORIG
+
+/* tone sequences: period (uS), duration (ms), silence (ms) */
+uint16_t alive[] = { 500,100,0, 0 };
+uint16_t happy[] = { 1000,100,0, 500,150,0, 0 };
+uint16_t sad[]   = { 3000,500,500, 0 };
+
+/* iPod Classic: decrypted hashes for known OFs */
+unsigned char of_sha[][SIGN_SZ] = {
+    "\x66\x66\x76\xDC\x1D\x32\xB2\x46\xA6\xC9\x7D\x5A\x61\xD3\x49\x4C", /* v1.1.2 */
+    "\x06\x85\xDF\x28\xE4\xD7\xF4\x82\xC0\x73\xB0\x53\x26\xFC\xB0\xFE"  /* v2.0.4 */
+    /* TODO?: v2.0.1 */
+};
+#define N_OF (int)(sizeof(of_sha)/SIGN_SZ)
+
+/* we can assume that unknown FW is a RB bootloader */
+#define FW_RB   N_OF
+
+/* IM3 size aligned to NOR sector size */
+#define GET_NOR_SZ(hinfo) \
+    (ALIGN_UP(IM3HDR_SZ + get_uint32le((hinfo)->data_sz), 0x1000))
+
+static uint32_t get_uint32le(unsigned char *p)
+{
+    return p[0] | (p[1] << 8) | (p[2] << 16) | (p[3] << 24);
+}
+
+int identify_fw(struct Im3Info *hinfo)
+{
+    unsigned char hash[SIGN_SZ];
+    int of;
+
+    /* decrypt hash to identify OF */
+    memcpy(hash, hinfo->enc12.data_sign, SIGN_SZ);
+    hwkeyaes(HWKEYAES_DECRYPT, HWKEYAES_UKEY, hash, SIGN_SZ);
+
+    for (of = 0; of < N_OF; of++)
+        if (memcmp(hash, of_sha[of], SIGN_SZ) == 0)
+            break;
+
+    return of;
+}
+
+#ifdef DUALBOOT_UNINSTALL
+/* Uninstall RB bootloader */
+void main(void)
+{
+    struct Im3Info *hinfo;
+    void *fw_addr;
+    uint16_t *status;
+    uint32_t bl_nor_sz;
+
+    usec_timer_init();
+
+    piezo_seq(alive);
+
+    hinfo = (struct Im3Info*)OF_LOADADDR;
+    fw_addr = (void*)hinfo + IM3HDR_SZ;
+
+    if (!read_im3(NORBOOT_OFF, hinfo, NULL)) {
+        status = sad;
+        goto bye; /* no FW found */
+    }
+
+    if (identify_fw(hinfo) != FW_RB) {
+        status = happy;
+        goto bye; /* RB bootloader not installed, nothing to do */
+    }
+
+    /* if found FW is RB bootloader, OF should start just behind it */
+    bl_nor_sz = GET_NOR_SZ(hinfo);
+    if (!read_im3(NORBOOT_OFF + bl_nor_sz, hinfo, fw_addr)
+                            || (identify_fw(hinfo) == FW_RB)) {
+        status = sad;
+        goto bye; /* OF not found */
+    }
+
+    /* OF correctly loaded */
+    #if 1
+    crypt_fw(HWKEYAES_ENCRYPT, hinfo, fw_addr);
+    #else
+    /* use current decrypted imagen (faster load) */
+    #endif
+
+    /* restore OF to it's original place */
+    if (!write_im3(NORBOOT_OFF, hinfo)) {
+        /* fatal: corrupted NOR, use iTunes to restore */
+        while (true)
+            piezo_seq(sad);
+    }
+
+    /* erase freed NOR blocks */
+    bootflash_init(SPI_PORT);
+    bootflash_erase_blocks(SPI_PORT,
+            (NORBOOT_OFF + GET_NOR_SZ(hinfo)) >> 12, bl_nor_sz >> 12);
+    bootflash_close(SPI_PORT);
+
+    status = happy;
+
+bye:
+    udelay(300000);
+    piezo_seq(status);
+    WDTCON = 0x100000; /* WDT reset */
+    while (1);
+}
+
+#else
+/* Install RB bootloader */
+struct Im3Info bl_hinfo __attribute__((section(".im3info.data"))) =
+{
+    .ident = IM3_IDENT,
+    .version = IM3_VERSION,
+    .enc_type = 2,
+};
+
+void main(void)
+{
+    struct Im3Info *hinfo;
+    void *fw_addr;
+    uint16_t *status;
+
+    usec_timer_init();
+
+    piezo_seq(alive);
+
+    hinfo = (struct Im3Info*)OF_LOADADDR;
+    fw_addr = (void*)hinfo + IM3HDR_SZ;
+
+    if (!read_im3(NORBOOT_OFF, hinfo, fw_addr)) {
+        status = sad;
+        goto bye; /* no FW found */
+    }
+
+    if (identify_fw(hinfo) == FW_RB) {
+        /* FW found, but not OF, assume it is a RB bootloader,
+           already decrypted OF should be located just behind */
+        int nor_offset = NORBOOT_OFF + GET_NOR_SZ(hinfo);
+        if (!read_im3(nor_offset, hinfo, fw_addr) ||
+                                (identify_fw(hinfo) == FW_RB)) {
+            status = sad;
+            goto bye; /* OF not found, use iTunes to restore */
+        }
+    }
+
+    /* safety check, verify we are not going to overwrite useful data */
+    if (bootflash_fs_tail(SPI_PORT) <
+                NORBOOT_OFF + GET_NOR_SZ(&bl_hinfo) + GET_NOR_SZ(hinfo)) {
+        /* TODO?: probably this should not happen, realloc files in
+           flash to get free space if so */
+        piezo_seq(sad);
+        status = sad;
+        goto bye; /* no space if flash, try to use iTunes to restore */
+    }
+
+    /* write decrypted OF, if OF is corretly written then prepare RB
+       bootloader to be written to NOR, else we will try to restore OF
+       to its original place */
+    if (write_im3(NORBOOT_OFF + GET_NOR_SZ(&bl_hinfo), hinfo)) {
+        hinfo = &bl_hinfo;
+        fw_addr = (void*)hinfo + IM3HDR_SZ;
+        /* sign RB bootloader */
+        mksign(HWKEYAES_UKEY, fw_addr,
+                get_uint32le(hinfo->data_sz), hinfo->enc12.data_sign);
+        mksign(HWKEYAES_UKEY, hinfo, IM3INFOSIGN_SZ, hinfo->info_sign);
+    }
+
+    #if 0
+    crypt_fw(HWKEYAES_ENCRYPT, hinfo, fw_addr);
+    #else
+    /* use current decrypted imagen (faster load) */
+    #endif
+
+    /* write RB bootloader (or restore OF to is original place) */
+    if (!write_im3(NORBOOT_OFF, hinfo)) {
+        /* fatal: corrupted NOR, use iTunes to restore */
+        while (true)
+            piezo_seq(sad);
+    }
+
+    /* RB bootloader not succesfully intalled, but device
+       was restored and should be working as before */
+    if (hinfo != &bl_hinfo) {
+        status = sad;
+        goto bye;
+    }
+
+    /* enjoy RB! */
+    status = happy;
+
+bye:
+    udelay(300000);
+    piezo_seq(status);
+    WDTCON = 0x100000; /* WDT reset */
+    while (1);
+}
+#endif  /* DUALBOOT_UNINSTALL */
