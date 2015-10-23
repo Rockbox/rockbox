75e0c0902cd74e21018b4a157e215ae9c3da7e66
Amaury Pouly <amaury.pouly@gmail.com>
2015-10-23 01:03:06 +0100

    hwstub: rewrite library (WIP)
    Note: this is a WIP, essentially the library will be rewritten in C++ with
    a cleaner design, see hwstub.hpp for the prototype header. See hwstub.cpp
    and hwstub_usb.cpp for a beginning of implementation. See hwstub_test.cpp
    for an example use. The library is still incomplete.
    
    Change-Id: I981a9019b9f63da31701d86b75a47dcf622d2b28

--- utils/hwstub/lib/hwstub_internal.h
+++ utils/hwstub/lib/hwstub_internal.h
@@ -26,50 +26,13 @@
  */
 
 #include "hwstub.h"
-#include <string.h>
-#include <stdlib.h>
-
-struct hwstub_device_t;
-
-/* Release function */
-typedef void (*hwstub_release_fn_t)(struct hwstub_device_t *dev);
-/* Get descriptor: return <0 on error, amount read otherwise */
-typedef int (*hwstub_get_desc_fn_t)(struct hwstub_device_t *dev, uint16_t desc, void *info, size_t sz);
-/* Get log: return <0 on error, amount read otherwise */
-typedef int (*hwstub_get_log_fn_t)(struct hwstub_device_t *dev, void *buf, size_t sz);
-/* Execute function: return <0 on error, 0 on success */
-typedef int (*hwstub_exec_fn_t)(struct hwstub_device_t *dev, uint32_t addr, uint16_t flags);
-/* Read function: return <0 on error, amount read otherwise. Size is always lower
- * then maximum buffer size */
-typedef int (*hwstub_read_fn_t)(struct hwstub_device_t *dev, uint8_t breq, uint32_t addr,
-    void *buf, size_t sz);
-/* Write function: same as read */
-typedef int (*hwstub_write_fn_t)(struct hwstub_device_t *dev, uint8_t breq, uint32_t addr,
-    const void *buf, size_t sz);
-
-
-struct hwstub_device_vtable_t
-{
-    hwstub_release_fn_t release;
-    hwstub_get_desc_fn_t get_desc;
-    hwstub_get_log_fn_t get_log;
-    hwstub_read_fn_t read;
-    hwstub_write_fn_t write;
-    hwstub_exec_fn_t exec;
-};
-
-struct hwstub_device_t
-{
-    struct hwstub_device_vtable_t vtable;
-    unsigned buf_sz; /* device buffer size, no transfer can be greater than this */
-    uint16_t id; /* transaction ID */
-    uint8_t minor_ver; /* maximum level of compatibility between the device and library */
-};
 
 /**
  * Probing
  */
 
+/* Returns 0 if a device interface is usable, or -1 if none was found */
+int hwstub_probe(libusb_device *dev);
 /* return <0 on failure, interface index on success */
 int hwstub_rb_probe(struct libusb_device_descriptor *dev,
     struct libusb_config_descriptor *config);
