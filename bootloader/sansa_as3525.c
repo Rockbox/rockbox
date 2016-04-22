a1190923bfd4d14569918fbfb8eac19b0775a606
Mihail Zenkov <mihail.zenkov@gmail.com>
2016-04-22 12:17:35 +0000

    Fix AMSv2 variant 1 detection if rockbox loaded from SD card
    Change-Id: Ie7c9c06170601e109f8d3de9686773c38a6e224a

--- bootloader/sansa_as3525.c
+++ bootloader/sansa_as3525.c
@@ -160,6 +160,11 @@ void main(void)
 #endif
     }
 
+#if defined(SANSA_FUZEV2) || defined(SANSA_CLIPPLUS) || defined(SANSA_CLIPZIP)
+    /* It is necessary for proper detection AMSv2 variant 1 */
+    if (amsv2_variant == 1)
+        GPIOB_PIN(5) = 1 << 5;
+#endif
     kernel_entry = (void*) loadbuffer;
     commit_discard_idcache();
     printf("Executing");
--- firmware/rolo.c
+++ firmware/rolo.c
@@ -275,6 +275,11 @@ int rolo_load(const char* filename)
     /* Set CVDD1 power supply to default*/
     ascodec_write_pmu(0x17, 1, 0);
 #endif
+#if defined(SANSA_FUZEV2) || defined(SANSA_CLIPPLUS) || defined(SANSA_CLIPZIP)
+    /* It is necessary for proper detection AMSv2 variant 1 */
+    if (amsv2_variant == 1)
+        GPIOB_PIN(5) = 1 << 5;
+#endif
 
 #if CONFIG_CPU != IMX31L /* We're not finished yet */
 #ifdef CPU_ARM
