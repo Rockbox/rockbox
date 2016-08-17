73d15921161b6505429c62f46e6302b3d184866c
Amaury Pouly <amaury.pouly@gmail.com>
2016-08-17 20:47:10 +0100

    Initial commit for the Fiio X1 (WIP)
    Status: unusable at the moemnt
    
    Change-Id: Iad63e63e0e5860f9702d8d32aab365ff3efb4167

---
+++ firmware/target/mips/jz4760b/fiio-x1/button-target.h
@@ -0,0 +1,51 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2016 by Amaury Pouly
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#ifndef _BUTTON_TARGET_H_
+#define _BUTTON_TARGET_H_
+
+#include <stdbool.h>
+
+/* Main unit's buttons */
+#define BUTTON_POWER                0x00000001
+#define BUTTON_VOL_UP               0x00000002
+#define BUTTON_VOL_DOWN             0x00000004
+/* Virtual buttons */
+#define BUTTON_LEFT                 0x00000008
+#define BUTTON_UP                   0x00000010
+#define BUTTON_RIGHT                0x00000020
+#define BUTTON_DOWN                 0x00000040
+#define BUTTON_SELECT               0x00000080
+#define BUTTON_PLAYPAUSE            0x00000100
+#define BUTTON_BACK                 0x00000200
+#define BUTTON_BOTTOMLEFT           0x00000400
+#define BUTTON_BOTTOMRIGHT          0x00000800
+
+
+#define BUTTON_MAIN (BUTTON_VOL_UP|BUTTON_VOL_DOWN|BUTTON_POWER|BUTTON_LEFT| \
+                     BUTTON_UP|BUTTON_RIGHT|BUTTON_DOWN|BUTTON_SELECT| \
+                     BUTTON_PLAYPAUSE|BUTTON_BACK| \
+                     BUTTON_BOTTOMRIGHT|BUTTON_BOTTOMLEFT)
+/* Software power-off */
+#define POWEROFF_BUTTON BUTTON_POWER
+#define POWEROFF_COUNT 10
+
+#endif /* _BUTTON_TARGET_H_ */
+ 
