43564a243dae16136b6718e4e614c190f6abbd90
Franklin Wei <frankhwei536@gmail.com>
2016-11-20 15:16:41 -0500

    [WIP] Port of Simon Tatham's Puzzle Collection
    Original revision: 5123b1bf68777ffa86e651f178046b26a87cf2d9
    
    MIT Licensed. Some games still crash and others are unplayable due to
    issues with controls. Still need a "real" polygon filling algorithm.
    
    Change-Id: I7c69b6860ab115f973c8d76799502e9bb3d52368

---
+++ apps/plugins/puzzles/no-icon.c
@@ -0,0 +1,8 @@
+
+/*
+ * Dummy source file which replaces the files generated in the
+ * `icons' subdirectory, when they're absent.
+ */
+
+const char *const *const xpm_icons[] = { 0 };
+const int n_xpm_icons = 0;
