82ccab7c776d5adbe24c356ba022f5444bed6ad4
Amaury Pouly <amaury.pouly@gmail.com>
2016-11-22 13:05:33 +0100

    Initial commit for the Sony NWZ linux port and NWZ-E460 (WIP)
    NOTES:
    - bootloader makefile convert an extra font to be installed alongside the bootloader
      since sysfont is way too small
    - the toolsicon bitmap comes from the Oxygen iconset
    - touchscreen driver is untested
    
    TODO:
    - split this commit between toolchain, nwztools and port
    - implement audio routing driver (pcm is handled by pcm-alsa)
    - fix playback: it crashes on illegal instruction in DEBUG builds
    - implement mknwzboot
    - find out why the browser starts at / instead of /contents
    - implement radio support
    - implement return to OF for usb handling
    - calibrate battery curve (NB: of can report a battery level on a 0-5 scale but
      probabl don't want to use that ?)
    - implement power off? At the moment we return to OF. OF uses suspend and not
      power off
    - implement simulator build (we need a nice image of the player)
    - add support for other devices: A860 is very much like E460 with touchscreen (will
      need keymapping changes). The E580 and E470 might break because the key and touchscreen
      drivers changed names I think, we need to confirm they work the same way as the old ones
    - figure out if we can detect jack removal
    
    POTENTIAL TODOS:
    - try to build a usb serial gadget and gdbserver
    
    Change-Id: Ic3b56364aa174ab6bd2abff3ddff13f02c399e4d

--- utils/nwztools/scripts/install_dualboot.sh
+++ utils/nwztools/scripts/install_dualboot.sh
@@ -51,7 +51,11 @@ fi
 
 # NOTE some platforms use ext4 with a custom mount program
 # (/usr/local/bin/icx_mount.ext4), some probably use an mtd too
+# try ext3 and if it fails, try ext2
 mount -t ext3 $COMMON_ROOTFS_PARTITION $ROOTFS_TMP_DIR
+if [ "$?" != 0 ]; then
+    mount -t ext2 $COMMON_ROOTFS_PARTITION $ROOTFS_TMP_DIR
+fi
 if [ "$?" != 0 ]; then
     lcdmsg -f /usr/local/bin/font_08x12.bmp -l 0,15 "ERROR: mount failed"
     sleep 3
