fd1b82f2a1be0d87969b125c5822ca7418c6a07d
Amaury Pouly <amaury.pouly@gmail.com>
2017-01-08 22:30:47 +0100

    Initial commit for the Sony NWZ linux port and NWZ-E460 (WIP)
    NOTES:
    - bootloader makefile convert an extra font to be installed alongside the bootloader
      since sysfont is way too small
    - the toolsicon bitmap comes from the Oxygen iconset
    - touchscreen driver is untested
    
    TODO:
    - split this commit between toolchain, nwztools and port
    - implement audio routing driver (pcm is handled by pcm-alsa)
    - fix playback: it crashes on illegal instruction in DEBUG builds
    - implement mknwzboot
    - find out why the browser starts at / instead of /contents
    - implement radio support
    - implement return to OF for usb handling
    - calibrate battery curve (NB: of can report a battery level on a 0-5 scale but
      probabl don't want to use that ?)
    - implement power off? At the moment we return to OF. OF uses suspend and not
      power off
    - implement simulator build (we need a nice image of the player)
    - add support for other devices: A860 is very much like E460 with touchscreen (will
      need keymapping changes). The E580 and E470 might break because the key and touchscreen
      drivers changed names I think, we need to confirm they work the same way as the old ones
    - figure out if we can detect jack removal
    
    POTENTIAL TODOS:
    - try to build a usb serial gadget and gdbserver
    
    Change-Id: Ic77d71e0651355d47cc4e423a40fb64a60c69a80

--- rbutil/mknwzboot/install_script.sh
+++ rbutil/mknwzboot/install_script.sh
@@ -6,10 +6,6 @@
 # always clear to make sure we don't end up being screwed
 nvpflag fup 0xFFFFFFFF
 
-#
-# FIXME document this
-#
-
 # go to /tmp
 cd /tmp
 
@@ -66,28 +62,35 @@ if [ ! -e $SPIDERAPP_PATH.of ]; then
     mv $SPIDERAPP_PATH $SPIDERAPP_PATH.of
 fi
 
-# extract our payload executable
+# extract our payload: the second file in the upgrade is a tar file
+# the files in the archive have paths of the form ./absolute/path and we extract
+# it at the rootfs mount it, so it can create/overwrite any file
+#
+# we need a small trick here: we want to pipe directly the output of the decryption
+# tool to tar, to avoid using space in /tmp/ or on the user partition
 lcdmsg -f /usr/local/bin/font_08x12.bmp -l 0,9 "Install rockbox"
-fwpchk -f /contents/$_UPDATE_FN_.UPG -c -1 $SPIDERAPP_PATH
+FIFO_FILE=/tmp/rb.fifo
+mkfifo $FIFO_FILE
+if [ "$?" != 0 ]; then
+    umount "$ROOTFS_TMP_DIR"
+    lcdmsg -f /usr/local/bin/font_08x12.bmp -l 0,15 "ERROR: cannot create fifo"
+    sleep 3
+    exit 0
+fi
+fwpchk -f /contents/$_UPDATE_FN_.UPG -c -1 $FIFO_FILE &
+#tar -tvf $FIFO_FILE
+tar -C $ROOTFS_TMP_DIR -xvf $FIFO_FILE
+if [ "$?" != 0 ]; then
+    umount "$ROOTFS_TMP_DIR"
+    lcdmsg -f /usr/local/bin/font_08x12.bmp -l 0,15 "ERROR: extraction failed"
+    sleep 3
+    exit 0
+fi
+# wait for fwpchk
+wait
 if [ "$?" != 0 ]; then
     umount "$ROOTFS_TMP_DIR"
     lcdmsg -f /usr/local/bin/font_08x12.bmp -l 0,15 "ERROR: no file to extract"
-    sleep 3
-    exit 0
-fi
-
-# make it executable and change user/group
-chmod 775 $SPIDERAPP_PATH
-if [ "$?" != 0 ]; then
-    umount "$ROOTFS_TMP_DIR"
-    lcdmsg -f /usr/local/bin/font_08x12.bmp -l 0,15 "ERROR: cannot make it executable"
-    sleep 3
-    exit 0
-fi
-chown 500:500 $SPIDERAPP_PATH
-if [ "$?" != 0 ]; then
-    umount "$ROOTFS_TMP_DIR"
-    lcdmsg -f /usr/local/bin/font_08x12.bmp -l 0,15 "ERROR: cannot change owner"
     sleep 3
     exit 0
 fi
@@ -100,15 +103,6 @@ ln -s "$CONTENTS/.rockbox" "$ROOTFS_TMP_DIR/.rockbox"
 if [ "$?" != 0 ]; then
     umount "$ROOTFS_TMP_DIR"
     lcdmsg -f /usr/local/bin/font_08x12.bmp -l 0,15 "ERROR: cannot create rockbox symlink"
-    sleep 3
-    exit 0
-fi
-
-# change user/group
-chown -h 500:500 "$ROOTFS_TMP_DIR/.rockbox"
-if [ "$?" != 0 ]; then
-    umount "$ROOTFS_TMP_DIR"
-    lcdmsg -f /usr/local/bin/font_08x12.bmp -l 0,15 "ERROR: cannot change symlink owner"
     sleep 3
     exit 0
 fi
@@ -138,3 +132,4 @@ sync
 echo "Installation successful"
 # finish
 exit 0
+ 
