78b9cec4573f350ca9b61c2a69b40a929608517b
William Wilgus <me.theuser@yahoo.com>
2017-02-08 04:46:54 +0100

    Add boot data to MOST AS3525 Devices
    Adds boot data to as3525 devices Sansa C200v2 E200v2 Clip Clipv2 Clip+ ClipZip
    fuze, fuzev2 m200v4
    
    Adds boot_data to features.txt
    
    default arm crt0.s now had boot data if HAVE_BOOTDATA is defined
    
    Change-Id: I614a556696540511a69fc12a4520b01c268bf8a9

--- firmware/export/config/sansafuze.h
+++ firmware/export/config/sansafuze.h
@@ -5,6 +5,8 @@
 /* For Rolo and boot loader */
 #define MODEL_NUMBER 43
 #define MODEL_NAME   "Sandisk Sansa Fuze"
+/* Define if boot data from bootloader has been enabled for the target */
+#define HAVE_BOOTDATA
 
 #define HW_SAMPR_CAPS       SAMPR_CAP_ALL
 
