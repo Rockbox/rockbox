e9de9c1452499d852c8b4ec75cde06520fe7c961
William Wilgus <me.theuser@yahoo.com>
2017-02-10 13:53:46 +0100

    Add boot from other volumes in bootloader on targets with HAVE_MULTIVOLUME
    Adds the ability to load firmware from other drives on MULTIVOLUME targets
    Mihail Zenkov <mihail.zenkov@gmail.com> had posted a hard coded patch
    to allow this on several Sansa players, I made it more universal
    
    Redirect file rockbox_main.<name> should placed in root of
    drive you would like to be main, if this file empty or there a single
    slash '/' firmware will be loaded from /.rockbox in root of this drive
    If instead a /<*DIRECTORY*> is supplied in rockbox_main.<name> then
    firmware will be loaded from /<dir>/.rockbox/
    
    NOTES*
    
    The directory can have multiple levels however..
    leading slash MUST be included
    trailing slash can be omitted
    (eg. /test/.rockbox would be simply '/test' in the redirect file)
    
    Redirect file will not work on internal drive (whatever is default boot drive)
    
    Volume with the highest index containing redirect file will be loaded
    first.
    
    Firmware file is checked for boot data region, if missing, firmware
    image will not be loaded.
    
    On failure or if no redirect file is found load will fallback to
    internal drive
    
    Currently only Sansa Fuze+, Sansa Clip+,
    Sansa Clip Zip, Sansa Fuzev2, and Sansa Fuzev1 are implemented.
    
    Players (with HAVE_MULTIVOLUME)
    will need #define HAVE_BOOTDATA and #define BOOT_REDIR "rockbox_main.<name>"
    added to their config file
    
    boot_data is implemented in crt0.s file (See g#1552)
    ARM and IMX233 have aleady been implemented
    
    Once these conditions are met <HAVE_MULTIBOOT> will be defined by config.h
    
    Partitions on the drives are able to have a redirect as
    well.
    
    Change-Id: Iada3263919f6bcad7d0d7d8279b4239aafa07ee9

--- firmware/export/config/sansafuzev2.h
+++ firmware/export/config/sansafuzev2.h
@@ -8,6 +8,9 @@
 /* Define if boot data from bootloader has been enabled for the target */
 #define HAVE_BOOTDATA
 
+/* define boot redirect file name allows booting from external drives */
+#define BOOT_REDIR "rockbox_main.fuze2"
+
 #define HW_SAMPR_CAPS       SAMPR_CAP_ALL
 
 /* define this if you have recording possibility */
