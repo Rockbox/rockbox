f0f5316f9069748c1ec20a1c5b5b33dc749ba79e
Amaury Pouly <amaury.pouly@gmail.com>
2017-02-23 11:33:19 +0100

    Initial commit for the Sony NWZ linux port (WIP)
    SUPPORTED SERIES:
    - NWZ-E450
    - NWZ-E460
    - NWZ-E470
    - NWZ-E580
    - NWZ-A10
    
    NOTES:
    - bootloader makefile convert an extra font to be installed alongside the bootloader
      since sysfont is way too small
    - the toolsicon bitmap comes from the Oxygen iconset
    - touchscreen driver is untested
    
    TODO:
    - implement audio routing driver (pcm is handled by pcm-alsa)
    - fix playback: it crashes on illegal instruction in DEBUG builds
    - implement mknwzboot
    - find out why the browser starts at / instead of /contents
    - implement radio support
    - implement return to OF for usb handling
    - calibrate battery curve (NB: of can report a battery level on a 0-5 scale but
      probabl don't want to use that ?)
    - implement power off? At the moment we return to OF. OF uses suspend and not
      power off
    - implement simulator build (we need a nice image of the player)
    - add support for other devices: A860 is very much like E460 with touchscreen (will
      need keymapping changes). The E580 and E470 might break because the key and touchscreen
      drivers changed names I think, we need to confirm they work the same way as the old ones
    - figure out if we can detect jack removal
    
    POTENTIAL TODOS:
    - try to build a usb serial gadget and gdbserver
    
    Change-Id: Ic77d71e0651355d47cc4e423a40fb64a60c69a80

---
+++ firmware/export/config/sonynwza10.h
@@ -0,0 +1,16 @@
+/*
+ * This config file is for the Sony NW-A10 series
+ */
+
+/* For Rolo and boot loader */
+#define MODEL_NUMBER 104
+
+#define MODEL_NAME   "Sony NWZ-A10 Series"
+
+/* LCD dimensions */
+#define LCD_WIDTH  240
+#define LCD_HEIGHT 320
+/* sqrt(240^2 + 320^2) / 2 = 200 */
+#define LCD_DPI 200
+
+#include "sonynwzlinux.h"
