b5ad755719d05cb28643d5bd790031ccaf234449
Lorenzo Miori <memoryS60@gmail.com>
2017-04-26 21:16:12 +0200

    Samsung YP-R0/YP-R1: use Linux i2c-dev for radio chip access
    Linux offers the high-level i2c-dev driver to directly access the
    i2c bus(ses) on the system. This system device is used to get rid
    of the (rather silly) radio chip kernel module for ypr0 target and
    correctly enables radio access also for the ypr1 target.
    fm-radio chip is located on i2c-0 bus on the ypr0 target while it
    is located on i2c-1 bus on the ypr1 target.
    Power-up (RST) pin is also handled for both targets, which is wired
    to another GPIO of the i.MX 37 platform.
    Additionally, this patch simplifies the RDS low-level handling by
    exploiting the Si4709 debug interface which comes with a mutex
    protection as free bonus.
    
    Change-Id: I839282bec4a27ad0ad8403c5a8dd86963b77e1bf

--- firmware/target/hosted/samsungypr/ypr0/i2c-target.h
+++ firmware/target/hosted/samsungypr/ypr0/i2c-target.h
@@ -6,9 +6,10 @@
  *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
  *                     \/            \/     \/    \/            \/
  *
- * Module wrapper for SI4709 FM Radio Chip, using /dev/si470x (si4709.ko) of Samsung YP-R0
+ * Module wrapper for SI4709 FM Radio Chip, using /dev/si470x (si4709.ko)
+ *      Samsung YP-R0 & Samsung YP-R1
  *
- * Copyright (c) 2012 Lorenzo Miori
+ * Copyright (c) 2017 Lorenzo Miori
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,16 +21,10 @@
  *
  ****************************************************************************/
 
-#ifndef __RADIO_YPR0_H__
-#define __RADIO_YPR0_H__
+#ifndef I2C_TARGET_H_
+#define I2C_TARGET_H_
 
-#include "stdint.h"
-#include "rds.h"
-#include "si4700.h"
+/** Define the FM Radio Chip I2C bus */
+#define I2C_BUS_FM_RADIO        ("/dev/i2c-0")
 
-void radiodev_open(void);
-void radiodev_close(void);
-void si4709_write_reg(int addr, uint16_t value);
-uint16_t si4709_read_reg(int addr);
-
-#endif /*__RADIO-YPR0_H__*/
+#endif /* I2C_TARGET_H_ */
