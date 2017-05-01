c7abc9dddd084e630d92e4e7f6863a93aca9aefd
Lorenzo Miori <memoryS60@gmail.com>
2017-05-01 12:54:14 +0200

    Samsung YP-R0: rbutil support
    This change implements the automatic firmware patching operation in
    the Rockbox Utility application.
    This patch consists of:
    
    - a modified patcher which creates a compressed binary diff (Linux only) and
      the equivalent data written into .c/.h source code as array
    - rbutil first-stage integration: create a patched R0.ROM and copy it to the
      target
    
    The binary patch is created by the software tool "bsdiff". We are using
    here a modified version of the tool that is easily used as a generic library.
    https://github.com/mendsley/bsdiff
    Details are available in the source code.
    
    I have only stripped the embedded bzip compression away so to be completely
    free of additional libraries. Compression is anyway required since the bsdiff
    tool creates a file that consists of almost zeros, but the different bytes.
    Therefore, the gzip utility is used in the firmware patcher script.
    
    Extracting, patching and repacking a CRAMFS filesystem image is painful to handle
    on non-Linux operating systems (permissions, filenames and what not).
    Therefore, instead of e.g. modifiying cramfs tools to handle all patching in RAM
    it has been chosen to simply provide a binary patch.
    The binary patch is easily producible by everyone so that no malicious / non-free
    stuff can go into the patched. The patched firmware is protected by md5sum as well.
    
    Change-Id: I83aa56be08a14579d31f468befb9e9157649da9b

---
+++ rbutil/rbutilqt/base/bootloaderinstallypr.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ *   Copyright (C) 2017 by Lorenzo Miori
+ *
+ * All files in this archive are subject to the GNU General Public License.
+ * See the file COPYING in the source tree root for full license agreement.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#ifndef BOOTLOADERINSTALLYPR_H
+#define BOOTLOADERINSTALLYPR_H
+
+#include <QtCore>
+#include "bootloaderinstallbase.h"
+
+
+// bootloader installation derivate based on fwpatcher
+// This will patch a given ypr file using (de)scramble / mkboot
+// and put it on the player.
+class BootloaderInstallYpr : public BootloaderInstallBase
+{
+    Q_OBJECT
+
+    public:
+        BootloaderInstallYpr(QObject *parent = 0);
+        bool install(void);
+        bool uninstall(void);
+        BootloaderInstallBase::BootloaderType installed(void);
+        Capabilities capabilities(void);
+        QString ofHint();
+
+    private:
+        int m_hashindex;
+        int m_model;
+        QTemporaryFile m_descrambled;
+        QString patchError(int);
+
+    private slots:
+        void installStage2(void);
+};
+
+
+#endif
+
