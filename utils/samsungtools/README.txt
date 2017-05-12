a0fb5a82d3d9695be611a061096b5157cc52cd18
Lorenzo Miori <memoryS60@gmail.com>
2017-05-12 22:38:08 +0200

    Samsung YP-Q2 / YP-Z5: OF patcher to load hosted rb executable
    This patch aims at implementing a similar approach as done for other
    hosted targets like ypr0 and ypr1.
    Requisite tools:
    - cramfs
    - squashfs
    
    This initial commit does not provide the additional tools yet but patches
    the firmware to load a script from the USB drive, which for the moment
    is still UNTESTED. Do NOT merge!
    
    TODO:
    
    - would be nice to integrate the Safe Mode (ypr0/ypr1)
    - integrate the exact same approach to switch to OF at boot
    
    YP-Z5:
    - remove root password to allow password-less serial port login for
      development
    
    Change-Id: I1622ce2f1a44ae22003c39fcd9cff7fd7d65556c

---
+++ utils/samsungtools/README.txt
@@ -0,0 +1,1 @@
+sudo ./fwunpack.sh Z5Update.dat && sudo ./fwpatch.sh files firmware && ./fwpack.sh firmware && ./fwcrypt firmware.tar -o Z5UpdateMod.dat 
---
+++ utils/samsungtools/files/z5/appfs/start.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# source user script if available
+SOURCE="/mnt/usb/rc.user"
+[ -f $SOURCE ] && . $SOURCE
+
+if [ -e /mnt/usb/Oasis.tar.gz ] ; then
+	rm -f /mnt/usb/Oasis.tar.gz
+fi
+
+OASISROOT=/Oasis/usr/Oasis
+
+if [ ! -e /Oasis/start.sh ] ; then
+	mount -t cramfs /dev/ufdrawc /Oasis
+fi
+#echo "usbcontrol 1" > /proc/gadget_udc
+#echo "mode 1" > /proc/clkctrl
+
+cd $OASISROOT
+#insmod gadgetfs
+cd bin
+./create-folders.sh
+cd ..
+./bin/runsound.sh
+./bin/run.sh
