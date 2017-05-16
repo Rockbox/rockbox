62e5a3076ca540b2ab54d2e12f8ada42ffe42148
William Wilgus <me.theuser@yahoo.com>
2017-05-17 01:12:25 +0200

    MultiBoot_Firmware
    Stop gap till I get a chance to work on the root folder stuff
    
    Makes whatever volume marked by bootdata.boot_volume
    the base or root volume, mounts all other volumes after
    the specified boot volume.
    Has the effect of swapping the SD card to the slot for internal
    and Internal storage is mounted as the Sd-card.
    
    Does not honor root redirect -- uses .rockbox folder in the root of boot volume
    
    Change-Id: I0f786aa0c8721e21cba607366775be3e3c0cf474

--- firmware/common/disk.c
+++ firmware/common/disk.c
@@ -30,6 +30,11 @@
 #include "dircache_redirect.h"
 #include "disk.h"
 
+#if defined(HAVE_BOOTDATA) && !defined(SIMULATOR)
+#include "bootdata.h"
+#include "crc32.h"
+#endif
+
 #ifndef CONFIG_DEFAULT_PARTNUM
 #define CONFIG_DEFAULT_PARTNUM 0
 #endif
@@ -258,7 +263,23 @@ int disk_mount_all(void)
     for (int i = 0; i < NUM_VOLUMES; i++)
         vol_drive[i] = -1; /* mark all as unassigned */
 
+#if defined(HAVE_BOOTDATA) && !defined(SIMULATOR)
+    unsigned int crc = 0;
+    int boot_volume = 0;
+    crc = crc_32(boot_data.payload, boot_data.length, 0xffffffff);
+    if(crc == boot_data.crc)
+    {
+        boot_volume = boot_data.boot_volume; /* boot volume contained in uint8_t payload */
+    }
+    #ifdef HAVE_HOTSWAP
+        if (storage_present(boot_volume))
+    #endif
+            mounted += disk_mount(boot_volume); /* mount boot volume first */
     for (int i = 0; i < NUM_DRIVES; i++)
+    if (i != boot_volume)
+#else
+    for (int i = 0; i < NUM_DRIVES; i++)
+#endif
     {
     #ifdef HAVE_HOTSWAP
         if (storage_present(i))
