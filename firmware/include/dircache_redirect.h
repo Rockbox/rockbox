c79b4ad101015858d8360e112f5761cbc519bc57
William Wilgus <me.theuser@yahoo.com>
2017-05-19 23:33:31 +0200

    Multiboot Firmware Root Redirect - Do Not Commit
    Loads external drive into root namespace
    
    Root Redirects can now be put into different folders
    For instance placing '/_test' into SD1/rockbox_main.<playername>
    will redirect to /<1>/_test/.rockbox
    
    Debug menu>Bootdata now has root directory listed in addition to RAW Bootdata
    
    Redirect root work from Michael Sevakis g#1556
    
    Redirect will be updated once file code refactor is done
    
    Requires Bootdata and Multiboot bootloader see g#1552, g#1557, and g#1558
    
    Change-Id: I697b3d0499f85e789c3020bc2133fbe0023f72a2

--- firmware/include/dircache_redirect.h
+++ firmware/include/dircache_redirect.h
@@ -24,6 +24,15 @@
 #include "pathfuncs.h"
 #include "dir.h"
 #include "dircache.h"
+<<<<<<< HEAD
+=======
+
+#if defined(HAVE_MULTIBOOT) && !defined(SIMULATOR)
+#include "rb-loader.h"
+#include "bootdata.h"
+#include "crc32.h"
+#endif
+>>>>>>> Multiboot Firmware Root Redirect - Do Not Commit
 
 /***
  ** Internal redirects that depend upon whether or not dircache is made
@@ -132,11 +141,58 @@ static inline void volume_onmount_internal(IF_MV_NONVOID(int volume))
 #else
     const char *path = PATH_ROOTSTR;
 #endif
+<<<<<<< HEAD
+=======
+#if defined(HAVE_MULTIBOOT) && !defined(SIMULATOR)
+    static char rootpath[VOL_MAX_LEN+2] = RB_ROOT_CONTENTS_DIR;
+    static bool redirected = false;
+    int boot_volume = 0;
+    unsigned int crc = 0;
+
+        root_mount_path(path, NSITEM_MOUNTED);
+        crc = crc_32(boot_data.payload, boot_data.length, 0xffffffff);
+        if (crc == boot_data.crc)
+        {
+            boot_volume = boot_data.boot_volume; /* boot volume contained in uint8_t payload */
+            root_mount_path(path, volume == boot_volume ? NSITEM_HIDDEN :
+                                                          NSITEM_MOUNTED);
+            if (volume == boot_volume && !redirected)
+            {
+                if (get_redirect_dir(rootpath, sizeof(rootpath),
+                                                    boot_volume, "", "") > 0)
+                {
+                    rootpath[path_strip_trailing_separators(rootpath,NULL)] = '\0';
+                }
+                else /* Error occurred, card removed? Set root to default */
+                {
+                    root_mount_path(RB_ROOT_CONTENTS_DIR, NSITEM_CONTENTS);
+                    boot_volume = path_strip_volume(RB_ROOT_CONTENTS_DIR,
+                                                                    NULL, false);
+                }
+            }
+        } /*CRC OK*/
+        else
+            root_mount_path(path, RB_ROOT_VOL_HIDDEN(volume) ? NSITEM_HIDDEN : 0);
+
+        if (volume == boot_volume)
+        {
+            redirected = true;
+            root_mount_path(rootpath, NSITEM_CONTENTS);
+        }
+
+#else
+
+>>>>>>> Multiboot Firmware Root Redirect - Do Not Commit
     root_mount_path(path, RB_ROOT_VOL_HIDDEN(volume) ? NSITEM_HIDDEN : 0);
 #ifdef HAVE_MULTIVOLUME
     if (volume == path_strip_volume(RB_ROOT_CONTENTS_DIR, NULL, false))
 #endif
         root_mount_path(RB_ROOT_CONTENTS_DIR, NSITEM_CONTENTS);
+<<<<<<< HEAD
+=======
+#endif /* HAVE_MULTIBOOT */
+
+>>>>>>> Multiboot Firmware Root Redirect - Do Not Commit
 #ifdef HAVE_DIRCACHE
     dircache_mount();
 #endif
