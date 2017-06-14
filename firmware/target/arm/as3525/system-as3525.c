9f7476b575d20c71e64ea3a21c36668404672783
Michael Giacomelli <giac2000@hotmail.com>
2017-06-14 17:37:29 +0200

    Slightly increase CVDD2 on AMSv2.
    At least one person found an SD card that wouldn't work at 2.7v.
    
    Change-Id: I84001f07acc9ce36c71165706cce28c2899ac6bf

--- firmware/target/arm/as3525/system-as3525.c
+++ firmware/target/arm/as3525/system-as3525.c
@@ -349,8 +349,8 @@ void kernel_device_init(void)
     ascodec_write_pmu(0x18, 1, 0x35);
     /* AVDD17:    set AVDD17 power supply to 2.5V */
     ascodec_write_pmu(0x18, 7, 0x31);
-    /* CVDD2:     set CVDD2 power supply (digital for DAC/SD/etc) to 2.70V */
-    ascodec_write_pmu(0x17, 2, 0x80 | 114);
+    /* CVDD2:     set CVDD2 power supply (digital for DAC/SD/etc) to 2.75V */
+    ascodec_write_pmu(0x17, 2, 0x80 | 115);
 #else /* HAVE_AS3543 */
     ascodec_write(AS3514_CVDD_DCDC3, AS314_CP_DCDC3_SETTING);
 #endif /* HAVE_AS3543 */
