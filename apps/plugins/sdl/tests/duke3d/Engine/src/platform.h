6d091392cd76c3b3402f2f4bc069361f38da2cf2
Franklin Wei <git@fwei.tk>
2017-09-04 14:00:53 -0400

    Wolfenstein 3D / Duke Nukem 3D Mega-Patch
    This ports wolf4sdl and Chocolate Duke to run on my SDL port.  Place
    the original data files in /.rockbox/wolf3d/ and /.rockbox/duke3d/ for
    either games, then run the Demos > sdl plugin. Surprisingly most
    things work.
    
    Note that I don't plan on merging this.
    
    Wolf3D TODO:
    * better scaling options (rotate, etc.)
    * test if it actually works on targets other than ipod6g
    * copy DOOM's control scheme
    * configuration screen (before start)
    * maybe make this a standalone plugin
    * decide whether or not to merge this (SDL port is really hacky)
    
    Duke3D TODO:
    * music (figure out why timidity doesn't work)
    * better sound
    * fix weird freezes on menu
    * decide whether or not to merge this (probably not)
    * make it work with Atomic Edition
    * see if caching uses too much RAM
    
    Change-Id: I8f2c4c78af19de10c1633ed7bb7a997b43256dd9

---
+++ apps/plugins/sdl/tests/duke3d/Engine/src/platform.h
@@ -0,0 +1,38 @@
+#ifndef _INCLUDE_PLATFORM_DUKE_H_
+#define _INCLUDE_PLATFORM_DUKE_H_
+
+#if (defined PLATFORM_WIN32)
+    #include "win32_compat.h"
+#elif (defined PLATFORM_UNIX)
+    #include "unix_compat.h"
+#elif (defined PLATFORM_DOS)
+    #include "doscmpat.h"
+#elif (defined __APPLE__)
+    #include "macos_compat.h"
+#elif (defined ROCKBOX)
+    #include "rockbox_compat.h"
+#else
+#error Define your platform!
+#endif
+
+#if (!defined __EXPORT__)
+    #define __EXPORT__
+#endif
+
+uint16_t _swap16(uint16_t D);
+unsigned int _swap32(unsigned int D);
+#if defined(PLATFORM_MACOSX) && defined(__ppc__)
+#define PLATFORM_BIGENDIAN 1
+#define BUILDSWAP_INTEL16(x) _swap16(x)
+#define BUILDSWAP_INTEL32(x) _swap32(x)
+#else
+#define PLATFORM_LITTLEENDIAN 1
+#define BUILDSWAP_INTEL16(x) (x)
+#define BUILDSWAP_INTEL32(x) (x)
+#endif
+
+#endif  /* !defined _INCLUDE_PLATFORM_H_ */
+
+/* end of platform.h ... */
+
+
