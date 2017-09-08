597fd1ad385e365166566a39c42dce4e858f0b0a
Michael Sevakis <jethead71@rockbox.org>
2017-09-08 19:28:02 -0400

    Implement a much more capable vuprintf()
    New support as well as some buggy support fixed.
    
    Support:
    * Flags: '-', '+', ' ', '#', '0'
    
    * Width and precision: 'n', '.n', '*' and '.*'
    
    * Length modifiers: 'hh', 'h', 'l', 'll', 'z'
    
    * Radix: 'd', 'i', 'n', 'o', 'p/P', 'u', 'x/X'
    
    Provision exists to turn off lesser-used stuff or when certain
    functionality isn't desired (bootloader?). It will compile with
    int, long and long long being different ranges.
    
    * Move fdprintf() to its own file. It was in a strange place.
    
    * Make callers compatible and fix a couple snprintf() bugs while at it.
    
    Change-Id: Ibdc613a9b6775802c188b29b9dd46c568c94f7c3

--- firmware/libc/sprintf.c
+++ firmware/libc/sprintf.c
@@ -21,71 +21,71 @@
 
 /*
  * Minimal printf and snprintf formatting functions
- *
- * These support %c %s %d and %x
- * Field width and zero-padding flag only
  */
 
 #include <stdio.h>
-#include <stdarg.h>
-#include <stdbool.h>
 #include <limits.h>
-#include "format.h"
+#include "vuprintf.h"
 
 /* ALSA library requires a more advanced snprintf, so let's not
    override it in simulator for Linux.  Note that Cygwin requires
    our snprintf or it produces garbled output after a while. */
 
 struct for_snprintf {
-    unsigned char *ptr; /* where to store it */
-    size_t bytes; /* amount already stored */
-    size_t max;   /* max amount to store */
+    char *ptr;  /* where to store it */
+    size_t rem; /* unwritten buffer remaining */
 };
 
-static int sprfunc(void *ptr, unsigned char letter)
+static int sprfunc(void *ptr, int letter)
 {
     struct for_snprintf *pr = (struct for_snprintf *)ptr;
-    if(pr->bytes < pr->max) {
-        *pr->ptr = letter;
-        pr->ptr++;
-        pr->bytes++;
-        return true;
-    }
-    return false; /* filled buffer */
-}
 
+    if (pr->rem) {
+        if (!--pr->rem) {
+            return 0; /* filled buffer */
+        }
+
+        *pr->ptr++ = letter;
+    }
+
+    return 1;
+}
 
 int snprintf(char *buf, size_t size, const char *fmt, ...)
 {
-    va_list ap;
+    int bytes;
     struct for_snprintf pr;
 
-    pr.ptr = (unsigned char *)buf;
-    pr.bytes = 0;
-    pr.max = size;
+    pr.ptr = buf;
+    pr.rem = size;
 
+    va_list ap;
     va_start(ap, fmt);
-    format(sprfunc, &pr, fmt, ap);
+    bytes = vuprintf(sprfunc, &pr, fmt, ap);
     va_end(ap);
 
     /* make sure it ends with a trailing zero */
-    pr.ptr[(pr.bytes < pr.max) ? 0 : -1] = '\0';
-    
-    return pr.bytes;
+    if (size) {
+        *pr.ptr = '\0';
+    }
+
+    return bytes;
 }
 
 int vsnprintf(char *buf, size_t size, const char *fmt, va_list ap)
 {
+    int bytes;
     struct for_snprintf pr;
 
-    pr.ptr = (unsigned char *)buf;
-    pr.bytes = 0;
-    pr.max = size;
+    pr.ptr = buf;
+    pr.rem = size;
 
-    format(sprfunc, &pr, fmt, ap);
+    bytes = vuprintf(sprfunc, &pr, fmt, ap);
 
     /* make sure it ends with a trailing zero */
-    pr.ptr[(pr.bytes < pr.max) ? 0 : -1] = '\0';
-    
-    return pr.bytes;
+    if (size) {
+        *pr.ptr = '\0';
+    }
+
+    return bytes;
 }
