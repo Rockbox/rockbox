8b744571c0fa6a2805334a1de6f139db6c119393
Marcin Bukat <marcin.bukat@gmail.com>
2017-09-15 21:38:57 +0200

    ATJ hwstub: Add cache coherency
    All the hard work was done by pamaury. I simply added proper
    defines.
    
    Change-Id: Ib374eea7cd20f35518ad8a68d771c57c54ae01ca

--- utils/hwstub/stub/atj213x/target-config.h
+++ utils/hwstub/stub/atj213x/target-config.h
@@ -3,6 +3,13 @@
 #define IRAM_SIZE       0x18000
 #define DRAM_ORIG       0x80000000 /* KSEG1 cached unmapped */
 #define DRAM_SIZE       0x800000
+#define DCACHE_SIZE     0x4000     /* 16 kB */
+#define DCACHE_LINE_SIZE    0x10   /* 16 B */
+#define ICACHE_SIZE     0x4000     /* 16 kB */
+#define ICACHE_LINE_SIZE    0x10   /* 61 B */
+/* we need to flush caches before executing */
+#define CONFIG_FLUSH_CACHES
+
 #define CPU_MIPS
 
 /* something provides define
