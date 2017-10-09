73b56cdd5f31a3fa4f2c5fc0168135f1bba38317
William Wilgus <me.theuser@yahoo.com>
2017-10-09 23:17:33 +0200

    EXPOSE Original Firmware Area clip+, clipzip - NEVER COMMIT
    while usb is plugged allows device to read the 'hidden'
    32mb area from rockbox
    
    Upped CVDD2 from 2.75v to 3.3v
    Disabled 4-bit widebus mode
    
    Change-Id: I08da19aa55bd13dc212a9a788b00a5858a75677b

--- firmware/target/arm/as3525/sd-as3525v2.c
+++ firmware/target/arm/as3525/sd-as3525v2.c
@@ -546,7 +546,7 @@ static int sd_init_card(const int drive)
     if(drive == INTERNAL_AS3525) /* The OF is stored in the first blocks */
         card_info[INTERNAL_AS3525].numblocks -= AMS_OF_SIZE;
 
-#ifndef BOOTLOADER
+#if !defined(BOOTLOADER) && !defined(EXPOSE_OF_RECOVERY)
     /*  Switch to to 4 bit widebus mode  */
 
     /*  CMD7 w/rca: Select card to put it in TRAN state */
@@ -760,8 +760,14 @@ static int sd_transfer_sectors(IF_MD(int drive,) unsigned long start,
     const int drive = 0;
 #endif
     bool aligned = !((uintptr_t)buf & (CACHEALIGN_SIZE - 1));
+#if defined(EXPOSE_OF_RECOVERY)
+    int retry_all = 200;
+    int const retry_data_max = 300;
+#else
     int retry_all = 2;
     int const retry_data_max = 3;
+#endif
+
     int retry_data;
     unsigned int real_numblocks;
 
@@ -783,6 +789,15 @@ static int sd_transfer_sectors(IF_MD(int drive,) unsigned long start,
     /* skip SanDisk OF */
     if (drive == INTERNAL_AS3525)
         start += AMS_OF_SIZE;
+#if defined(EXPOSE_OF_RECOVERY)
+    if (drive == INTERNAL_AS3525)
+    {    
+        if (usb_detect() == USB_INSERTED)
+        {
+            start -= (AMS_OF_SIZE);
+        }
+    }
+#endif 
 
     while (!card_info[drive].initialized)
     {
