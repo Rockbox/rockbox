611720fd03acfb0e2d8c90dc1669cf0642551439
Franklin Wei <git@fwei.tk>
2018-02-11 15:34:30 -0500

    Quake!
    This ports id Software's Quake to run on the SDL plugin runtime. The
    source code originated from id under the GPLv2 license. I used
    https://github.com/ahefner/sdlquake as the base of my port.
    
    Currently only tested on ipod6g, and relies on some dirty hacks which
    will needed to be cleaned up before merge.
    
    Sound works well enough, but in-game music is not supported. I've
    written ARM assembly routines for the inner sound mixing loop, but
    I've disabled it for now because they cause a very rare but consistent
    crash which goes away when using the C version. (To reproduce: Play
    first level. Go to open space with water. Go to secret passage. Pick
    up megahealth.)
    
    Also, make sure you turn the "brightness" all the way down, or colors
    will look funky.
    
    To run, extract Quake's data files to /.rockbox/quake. Have fun!
    
    To do:
     - flesh out manual
     - optimize
     - debug all the weird crashes, esp. on simulator
    
    Change-Id: I4285036e967d7f0722802d43cf2096c808ca5799

--- apps/plugins/sdl/include/SDL_config_rockbox.h
+++ apps/plugins/sdl/include/SDL_config_rockbox.h
@@ -38,7 +38,8 @@
 
 //#define COMBINED_SDL
 
-/* games will use this sample rate */
+/* "recommended" sample rate for Rockbox. Games should use this by
+ * default unless necessary to do otherwise. */
 #ifdef SIMULATOR
 #define RB_SAMPR SAMPR_44
 #else
@@ -57,6 +58,7 @@
 /* woot */
 #define SDL_AUDIO_DRIVER_ROCKBOX 1
 #define SDL_THREAD_ROCKBOX       1
+#undef  SDL_THREAD_PTHREAD
 #define SDL_TIMER_ROCKBOX        1
 #define SDL_VIDEO_DRIVER_ROCKBOX 1
 
@@ -194,6 +196,7 @@ int vprintf(const char *fmt, va_list ap);
 void fatal(char *fmt, ...);
 void rb_exit(int rc);
 void rbsdl_atexit(void (*)(void));
+float atof_wrapper (char *str);
 
 /* speed */
 static inline uint16_t readLE16(void *addr)
