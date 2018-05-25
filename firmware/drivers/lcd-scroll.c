038a10220e122807a23887fd4363fc2588e98712
William Wilgus <me.theuser@yahoo.com>
2018-05-25 19:16:45 +0200

    Fix Remove lcd_scroll_worker from bootloader except remote displays/charcell
    This patch was supposed to only remove scroll worker from devices without
    remote displays, the build system said otherwise
    
    Change-Id: I240db89ee6b55818dbd2567338515b53ebe09300
    fixed: conditional define

--- firmware/drivers/lcd-scroll.c
+++ firmware/drivers/lcd-scroll.c
@@ -200,7 +200,8 @@ bool LCDFN(scroll_now)(struct scrollinfo *s)
 
     return ended;
 }
-#if (!defined(BOOTLOADER) && !defined(HAVE_REMOTE_LCD)) || defined(HAVE_LCD_CHARCELLS)
+
+#if !defined(BOOTLOADER) || defined(HAVE_REMOTE_LCD) || defined(HAVE_LCD_CHARCELLS)
 static void LCDFN(scroll_worker)(void)
 {
     int index;
