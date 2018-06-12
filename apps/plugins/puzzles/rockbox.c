Rockbox/rockbox

commit fbb6a2ff6d751adaaef73a86a98700f91571d0a1

Agptek Rocker: Build plugins
Patch provided by Aapo Tahkola.

Change-Id: I37a42a950d78d6b8aa3927ec7aeb30030f7be7a5

apps/plugins/puzzles/rockbox.c
@@ -177,7 +177,13 @@ static void zoom_drawpixel(int x, int y)
     if(x < zoom_clipl || x >= zoom_clipr)
         return;
 
-#if LCD_DEPTH == 24
+#if LCD_DEPTH > 24
+    unsigned int pix = rb->lcd_get_foreground();
+    zoom_fb[y * zoom_w + x].b = RGB_UNPACK_BLUE(pix);
+    zoom_fb[y * zoom_w + x].g = RGB_UNPACK_GREEN(pix);
+    zoom_fb[y * zoom_w + x].r = RGB_UNPACK_RED(pix);
+	zoom_fb[y * zoom_w + x].x = 255;
+#elif LCD_DEPTH == 24
     /* I hate these */
     unsigned int pix = rb->lcd_get_foreground();
     zoom_fb[y * zoom_w + x].b = RGB_UNPACK_BLUE(pix);
@@ -204,7 +210,13 @@ static void zoom_hline(int l, int r, int y)
     if(r >= zoom_clipr)
         r = zoom_clipr;
 
-#if LCD_DEPTH == 24
+#if LCD_DEPTH > 24
+    fb_data pixel = { RGB_UNPACK_BLUE(rb->lcd_get_foreground()),
+                      RGB_UNPACK_GREEN(rb->lcd_get_foreground()),
+                      RGB_UNPACK_RED(rb->lcd_get_foreground()),
+                      255
+	};
+#elif LCD_DEPTH == 24
     fb_data pixel = { RGB_UNPACK_BLUE(rb->lcd_get_foreground()),
                       RGB_UNPACK_GREEN(rb->lcd_get_foreground()),
                       RGB_UNPACK_RED(rb->lcd_get_foreground()) };
@@ -285,7 +297,12 @@ static void zoom_mono_bitmap(const unsigned char *bits, int x, int y, int w, int
             {
                 if(column & 1)
                 {
-#if LCD_DEPTH == 24
+#if LCD_DEPTH > 24
+                    zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].b = RGB_UNPACK_BLUE(pix);
+                    zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].g = RGB_UNPACK_GREEN(pix);
+                    zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].r = RGB_UNPACK_RED(pix);
+					zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].r = 255;
+#elif LCD_DEPTH == 24
                     zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].b = RGB_UNPACK_BLUE(pix);
                     zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].g = RGB_UNPACK_GREEN(pix);
                     zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].r = RGB_UNPACK_RED(pix);
@@ -625,7 +642,7 @@ static inline void plot(fb_data *fb, int w, int h,
     fb_data *ptr = fb + y * w + x;
     fb_data orig = *ptr;
     unsigned long r2, g2, b2;
-#if LCD_DEPTH != 24
+#if LCD_DEPTH < 24
     r2 = RGB_UNPACK_RED(orig);
     g2 = RGB_UNPACK_GREEN(orig);
     b2 = RGB_UNPACK_BLUE(orig);
@@ -640,8 +657,10 @@ static inline void plot(fb_data *fb, int w, int h,
     g = ((g1 * a) + (g2 * (256 - a))) >> 8;
     b = ((b1 * a) + (b2 * (256 - a))) >> 8;
 
-#if LCD_DEPTH != 24
+#if LCD_DEPTH < 24
     *ptr = LCD_RGBPACK(r, g, b);
+#elif LCD_DEPTH > 24
+    *ptr = (fb_data) {b, g, r, 255};
 #else
     *ptr = (fb_data) {b, g, r};
 #endif
@@ -1454,7 +1473,7 @@ void get_random_seed(void **randseed, int *randseedsize)
 
 static void timer_cb(void)
 {
-#if LCD_DEPTH != 24
+#if LCD_DEPTH < 24
     if(debug_settings.timerflash)
     {
         static bool what = false;

apps/plugins/battery_bench.c
@@ -273,6 +273,12 @@
 #define BATTERY_OFF_TXT "Power"
 #define BATTERY_ON_TXT  "Menu - start"
 
+#elif CONFIG_KEYPAD == AGPTEK_ROCKER_PAD
+#define BATTERY_ON      BUTTON_LEFT
+#define BATTERY_OFF     BUTTON_RIGHT
+#define BATTERY_OFF_TXT "Right"
+#define BATTERY_ON_TXT  "Left - start"
+
 #else
 #error No keymap defined!
 #endif

apps/plugins/blackjack.c
@@ -559,6 +559,20 @@ enum {
 #define BJACK_QUIT          BUTTON_POWER
 #define BJACK_QUIT_NAME     "Power"
 
+#elif CONFIG_KEYPAD == AGPTEK_ROCKER_PAD
+#define BJACK_SELECT        BUTTON_SELECT
+#define BJACK_QUIT          BUTTON_POWER
+#define BJACK_STAY          (BUTTON_VOLUP|BUTTON_SELECT)
+#define BJACK_DOUBLEDOWN    (BUTTON_VOLUP|BUTTON_DOWN)
+#define BJACK_UP            BUTTON_UP
+#define BJACK_DOWN          BUTTON_DOWN
+#define BJACK_RIGHT         BUTTON_RIGHT
+#define BJACK_LEFT          BUTTON_LEFT
+#define BJACK_SELECT_NAME   "Select"
+#define BJACK_STAY_NAME     "Option+Select"
+#define BJACK_QUIT_NAME     "Volume up"
+#define BJACK_DOUBLE_NAME   "Option+Down"
+
 #else
 #error No keymap defined!
 #endif

apps/plugins/brickmania.c
@@ -324,6 +324,14 @@ CONFIG_KEYPAD == SANSA_CONNECT_PAD
 #define SELECT    BUTTON_MENU
 #define CONTINUE_TEXT "Press MENU To Continue"
 
+#elif CONFIG_KEYPAD == AGPTEK_ROCKER_PAD
+#define QUIT      BUTTON_POWER
+#define LEFT      BUTTON_LEFT
+#define RIGHT     BUTTON_RIGHT
+#define SELECT    BUTTON_SELECT
+#define UP        BUTTON_UP
+#define DOWN      BUTTON_DOWN
+
 #else
 #error No keymap defined!
 #endif

apps/plugins/calculator.c
@@ -515,6 +515,16 @@ F3: equal to "="
 #define CALCULATOR_QUIT     BUTTON_POWER
 #define CALCULATOR_INPUT    BUTTON_MENU
 
+#elif CONFIG_KEYPAD == AGPTEK_ROCKER_PAD
+#define CALCULATOR_LEFT    BUTTON_LEFT
+#define CALCULATOR_RIGHT   BUTTON_RIGHT
+#define CALCULATOR_UP      BUTTON_UP
+#define CALCULATOR_DOWN    BUTTON_DOWN
+#define CALCULATOR_QUIT    BUTTON_POWER
+#define CALCULATOR_INPUT   BUTTON_SELECT
+#define CALCULATOR_CALC    BUTTON_VOLUP
+#define CALCULATOR_CLEAR   (BUTTON_SELECT|BUTTON_REPEAT)
+
 #else
 #error No keymap defined!
 #endif

apps/plugins/calendar.c
@@ -403,6 +403,16 @@
 #define CALENDAR_SELECT     BUTTON_MENU
 #define CALENDAR_EVENT_MENU_NAME "menu"
 
+#elif CONFIG_KEYPAD == AGPTEK_ROCKER_PAD
+#define CALENDAR_QUIT        BUTTON_POWER
+#define CALENDAR_SELECT      BUTTON_SELECT
+#define CALENDAR_NEXT_DAY    BUTTON_RIGHT
+#define CALENDAR_PREV_DAY    BUTTON_LEFT
+#define CALENDAR_NEXT_WEEK   BUTTON_DOWN
+#define CALENDAR_PREV_WEEK   BUTTON_UP
+#define CALENDAR_NEXT_MONTH  (BUTTON_VOLDOWN)
+#define CALENDAR_PREV_MONTH  (BUTTON_VOLUP)
+
 #else
 #error "No keypad setting."
 #endif

apps/plugins/chessbox/chessbox_pgn.h
@@ -544,6 +544,20 @@
 #elif CONFIG_KEYPAD == CREATIVE_ZENXFI2_PAD
 #define CB_MENU    BUTTON_MENU
 
+#elif CONFIG_KEYPAD == AGPTEK_ROCKER_PAD
+#define CB_SELECT  BUTTON_SELECT
+#define CB_UP      BUTTON_UP
+#define CB_DOWN    BUTTON_DOWN
+#define CB_LEFT    BUTTON_LEFT
+#define CB_RIGHT   BUTTON_RIGHT
+#define CB_PLAY    BUTTON_VOLDOWN
+#define CB_MENU    BUTTON_POWER
+#define CB_LEVEL   BUTTON_VOLUP
+#define CB_SCROLL_UP     (BUTTON_UP|BUTTON_REPEAT)
+#define CB_SCROLL_DOWN   (BUTTON_DOWN|BUTTON_REPEAT)
+#define CB_SCROLL_LEFT   (BUTTON_LEFT|BUTTON_REPEAT)
+#define CB_SCROLL_RIGHT  (BUTTON_RIGHT|BUTTON_REPEAT)
+
 #else
 #error No keymap defined!
 #endif

apps/plugins/chessclock.c
@@ -402,6 +402,16 @@
 #define CHC_QUIT            BUTTON_POWER
 #define CHC_STARTSTOP       BUTTON_MENU
 
+#elif CONFIG_KEYPAD == AGPTEK_ROCKER_PAD
+#define CHC_QUIT            (BUTTON_POWER|BUTTON_REPEAT)
+#define CHC_RESET           (BUTTON_LEFT)
+#define CHC_MENU            BUTTON_RIGHT
+#define CHC_STARTSTOP       BUTTON_SELECT
+#define CHC_SETTINGS_INC    BUTTON_VOLUP
+#define CHC_SETTINGS_DEC    BUTTON_VOLDOWN
+#define CHC_SETTINGS_CANCEL BUTTON_RIGHT
+#define CHC_SETTINGS_OK     (BUTTON_SELECT|BUTTON_REPEAT)
+
 #else
 #error No keymap defined!
 #endif

commit c2d5ac17623dd4a0143b884cc8d80905119e10ba

puzzles: fix typo introduced by fbb6a2f
When zoomed in, the original commit made a mono bitmap (a.k.a. text) always
have the red component set to 255. This would cause drawn text to always be
red. Fixed.

Change-Id: Iec06256a3a783948c60c70557de042b375224448

apps/plugins/puzzles/rockbox.c
@@ -301,7 +301,7 @@ static void zoom_mono_bitmap(const unsigned char *bits, int x, int y, int w, int
                     zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].b = RGB_UNPACK_BLUE(pix);
                     zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].g = RGB_UNPACK_GREEN(pix);
                     zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].r = RGB_UNPACK_RED(pix);
-					zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].r = 255;
+                    zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].x = 255;
 #elif LCD_DEPTH == 24
                     zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].b = RGB_UNPACK_BLUE(pix);
                     zoom_fb[(y + i * 8 + dy) * zoom_w + x + j].g = RGB_UNPACK_GREEN(pix);