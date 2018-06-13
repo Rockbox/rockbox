419219e06d86c4c2fe64da35872f5a5c833f2419
Marcin Bukat <marcin.bukat@gmail.com>
2018-06-13 13:28:38 +0200

    Agptek Rocker: Update tools to alter .upt update images
    Change all references to official rockbox repository
    
    Change-Id: I22d305bc6c6f89b8737b2bc15378bfd7fe10621e

--- tools/agptek_rocker/bootloader_install.sh
+++ tools/agptek_rocker/bootloader_install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+ROCKBOX_SRC=$HOME/rockbox
+BUILD=$ROCKBOX_SRC/build
+ISO=$HOME/iso
+ROOTFS=$HOME/rootfs
 
 [ -z "$UPT_DIR" ] && UPT_DIR=`pwd` 
 cd $HOME
@@ -9,9 +13,9 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!! STEP 0: Get sources !!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
-[ -d "$HOME/rockbox-wodz" ] || git clone https://github.com/wodz/rockbox-wodz.git
+[ -d "$ROCKBOX_SRC" ] || git clone ttp://gerrit.rockbox.org/p/rockbox
 
-cd $HOME/rockbox-wodz
+cd $ROCKBOX_SRC
 
 # build bootloader
 echo
@@ -20,10 +24,9 @@ echo "!!! STEP 1: Build bootloader !!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
 
-[ -d "$HOME/rockbox-wodz/build" ] && rm -rf $HOME/rockbox-wodz/build
-git checkout agptek-rocker && \
+[ -d "$BUILD" ] && rm -rf $BUILD
 git pull && \
-mkdir $HOME/rockbox-wodz/build && cd $HOME/rockbox-wodz/build && \
+mkdir $BUILD && cd $BUILD && \
 ../tools/configure --target=240 --type=b && \
 make clean && \
 make && \
@@ -38,9 +41,9 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!! STEP 2: Extract upt file !!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
-[ -d "$HOME/iso" ] && rm -rf $HOME/iso
-mkdir $HOME/iso && \
-xorriso -osirrox on -ecma119_map lowercase -indev $UPT_DIR/update.upt -extract / $HOME/iso
+[ -d "$ISO" ] && rm -rf $ISO
+mkdir $ISO && \
+xorriso -osirrox on -ecma119_map lowercase -indev $UPT_DIR/update.upt -extract / $ISO
 
 # Extract rootfs files. Preserve permissions (although this are wrong!)
 echo
@@ -48,7 +51,7 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!! STEP 3: Extract system.ubi !!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
-ubireader_extract_files -k -o $HOME/rootfs $HOME/iso/system.ubi
+ubireader_extract_files -k -o $ROOTFS $ISO/system.ubi
 
 # Copy rockbox bootloader
 echo
@@ -56,8 +59,8 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!! STEP 4: Copy bootloader !!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
-cp $HOME/rockbox-wodz/build/bootloader.elf $HOME/rootfs/usr/bin/rb_bootloader && \
-mipsel-rockbox-linux-gnu-strip --strip-unneeded $HOME/rootfs/usr/bin/rb_bootloader
+cp $BUILD/bootloader.elf $ROOTFS/usr/bin/rb_bootloader && \
+mipsel-rockbox-linux-gnu-strip --strip-unneeded $ROOTFS/usr/bin/rb_bootloader
 
 # Overwrite default player starting script with one running our bootloader
 echo
@@ -65,8 +68,8 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!! STEP 5: Modify startup script !!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
-cp $HOME/rockbox-wodz/tools/agptek_rocker//hiby_player.sh $HOME/rootfs/usr/bin/hiby_player.sh && \
-chmod 755 $HOME/rootfs/usr/bin/hiby_player.sh
+cp $ROCKBOX_SRC/tools/agptek_rocker//hiby_player.sh $ROOTFS/usr/bin/hiby_player.sh && \
+chmod 755 $ROOTFS/usr/bin/hiby_player.sh
 
 # Rebuild ubifs
 echo
@@ -74,8 +77,8 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!! STEP 6: Rebuild system.ubi !!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
-mkfs.ubifs --min-io-size=2048 --leb-size=126976 --max-leb-cnt=1024 -o $HOME/system_rb.ubi -r $HOME/rootfs && \
-mv $HOME/system_rb.ubi $HOME/iso/system.ubi
+mkfs.ubifs --min-io-size=2048 --leb-size=126976 --max-leb-cnt=1024 -o $HOME/system_rb.ubi -r $ROOTFS && \
+mv $HOME/system_rb.ubi $ISO/system.ubi
 
 # Fixup update.txt file with correct md5
 echo
@@ -83,7 +86,7 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!! STEP 7: Fixup update.txt !!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
-python $HOME/rockbox-wodz/tools/agptek_rocker/update_update.py $HOME/iso/update.txt
+python $ROCKBOX_SRC/tools/agptek_rocker/update_update.py $ISO/update.txt
 
 # Rebuild .upt file
 echo
@@ -91,7 +94,7 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!! STEP 8: Rebuild upt file !!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
-xorriso -as mkisofs -volid 'CDROM' --norock -output $UPT_DIR/update_rb.upt $HOME/iso
+xorriso -as mkisofs -volid 'CDROM' --norock -output $UPT_DIR/update_rb.upt $ISO
 
 # Build rockbox.zip
 echo
@@ -99,7 +102,7 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!! STEP 9: Build rockbox application !!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
-cd $HOME/rockbox-wodz/build && \
+cd $BUILD && \
 ../tools/configure --target=240 --type=n && \
 make clean && \
 make && \
@@ -112,9 +115,9 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!! STEP 10: Cleanup !!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
-rm -rf $HOME/rockbox-wodz/build
-rm -rf $HOME/iso
-rm -rf $HOME/rootfs
+rm -rf $BUILD
+rm -rf $ISO
+rm -rf $ROOTFS
 
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!                  Building finished                               !"
@@ -126,4 +129,4 @@ echo "1) Unzip rockbox.zip file in the root directory of SD card"
 echo "2) Copy update_rb.upt to the root directory of SD card"
 echo "3) Rename update_rb.upt to update.upt in SD card"
 echo "4) Select update firmware on device"
-echo
+    echo
