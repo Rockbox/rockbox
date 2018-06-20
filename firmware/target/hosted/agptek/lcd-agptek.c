Rockbox/rockbox

commit 5c015ada908d491fa2f9fa37b83e3fc9c56b4d86

Agptek Rocker: Implement lcd_enable()
In theory this should slightly improve battery life.

Change-Id: Iaf55beb7dbc20e7562017958aeb0386459d49a31

firmware/target/hosted/agptek/lcd-agptek.c
@@ -29,6 +29,7 @@
 #include "lcd.h"
 #include "lcd-target.h"
 #include "backlight-target.h"
+#include "sysfs.h"
 #include "panic.h"
 
 static int fd = -1;
@@ -72,6 +73,30 @@ void lcd_init_device(void)
     if((void *)framebuffer == MAP_FAILED)
     {
         panicf("Cannot map framebuffer");
+    }
+}
+
+#ifdef HAVE_LCD_SHUTDOWN
+void lcd_shutdown(void)
+{
+    munmap(framebuffer, FRAMEBUFFER_SIZE);
+    close(fd);
+}
+#endif
+
+void lcd_enable(bool on)
+{
+    const char * const sysfs_fb_blank = "/sys/class/graphics/fb0/blank";
+
+    if (lcd_active() != on)
+    {
+        sysfs_set_int(sysfs_fb_blank, on ? 0 : 1);
+        lcd_set_active(on);
+
+        if (on)
+        {
+            send_event(LCD_EVENT_ACTIVATION, NULL);
+        }
     }
 }
 

firmware/target/hosted/agptek/lcd-target.h
@@ -21,6 +21,10 @@
 #ifndef __LCD_TARGET_H__
 #define __LCD_TARGET_H__
 
-extern fb_data *framebuffer; /* see lcd-nwz.c */
+/* Agptek needs special ioctl() to redraw updated framebuffer content */
+#define LCD_OPTIMIZED_UPDATE
+#define LCD_OPTIMIZED_UPDATE_RECT
+
+extern fb_data *framebuffer; /* see lcd-agptek.c */
 #define LCD_FRAMEBUF_ADDR(col, row) (framebuffer + (row)*LCD_WIDTH + (col))
 #endif /* __LCD_TARGET_H__ */

firmware/SOURCES
@@ -114,6 +114,7 @@ target/hosted/sonynwz/nwz-db.c
 #endif
 
 #if defined(AGPTEK_ROCKER) && !defined(SIMULATOR)
+drivers/lcd-memframe.c
 target/hosted/backtrace-glibc.c
 target/hosted/kernel-unix.c
 target/hosted/filesystem-unix.c

firmware/export/config/agptekrocker.h
@@ -23,8 +23,10 @@
 /* define this if you have a colour LCD */
 #define HAVE_LCD_COLOR
 
+#define HAVE_LCD_ENABLE
+
 /* Define this if the LCD can shut down */
-//#define HAVE_LCD_SHUTDOWN
+#define HAVE_LCD_SHUTDOWN
 
 /* define this if you want album art for this target */
 #define HAVE_ALBUMART

commit 28cfeed4c1a07262ac9d0abc4e9263f10971633a

Agptek Rocker: Add missing lcd_set_active(true) in lcd_init_device()
This should fix problem introduced in 5c015ad resulting
in not updating screen properly in wps.

Change-Id: Ie36b6edfcfc7d425c84381402adae79d77fcc92f

firmware/target/hosted/agptek/lcd-agptek.c
@@ -74,6 +74,10 @@ void lcd_init_device(void)
     {
         panicf("Cannot map framebuffer");
     }
+
+#ifdef HAVE_LCD_ENABLE
+    lcd_set_active(true);
+#endif
 }
 
 #ifdef HAVE_LCD_SHUTDOWN

firmware/target/hosted/agptek/lcd-target.h
@@ -27,4 +27,6 @@
 
 extern fb_data *framebuffer; /* see lcd-agptek.c */
 #define LCD_FRAMEBUF_ADDR(col, row) (framebuffer + (row)*LCD_WIDTH + (col))
+
+extern void lcd_set_active(bool active);
 #endif /* __LCD_TARGET_H__ */