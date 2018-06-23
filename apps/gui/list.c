8da8581acfae3171d11877367773c1d90731e1b2
Johannes Rauh <johnbthetenth@gmail.com>
2018-06-23 11:27:18 +0200

    Volume adjustment in LISTS ignored AUDIOHW_SETTING values
    AUDIOHW_SETTING() defines number of decimals and step size.
    This is taken into account in sound menu but ignored in lists
    (had been recently fixed in WPS).
    This was not a problem so far since all drivers used 0 decimal
    places and step size equal 1.

--- apps/gui/list.c
+++ apps/gui/list.c
@@ -696,12 +696,13 @@ bool gui_synclist_do_button(struct gui_synclist * lists,
 
 #ifdef HAVE_VOLUME_IN_LIST
         case ACTION_LIST_VOLUP:
-            global_settings.volume += 2;
-            /* up two because the falthrough brings it down one */
-        case ACTION_LIST_VOLDOWN:
-            global_settings.volume--;
+	    global_settings.volume += sound_steps(SOUND_VOLUME);
             setvol();
-            return true;
+	    return true;
+        case ACTION_LIST_VOLDOWN:
+	    global_settings.volume -= sound_steps(SOUND_VOLUME);
+	    setvol();
+	    return true;
 #endif
         case ACTION_STD_PREV:
         case ACTION_STD_PREVREPEAT:
