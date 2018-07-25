d527ecb022ecd2034e7c49110f5d474ca7905961
William Wilgus <me.theuser@yahoo.com>
2018-07-25 13:51:39 +0200

    Ignore truncation warning for cuesheet
    Change-Id: Id58a22a0cc9332030779f467f2f2a1bc5d8be6a5

--- apps/cuesheet.c
+++ apps/cuesheet.c
@@ -361,12 +361,15 @@ void browse_cuesheet(struct cuesheet *cue)
     int action;
     bool done = false;
     char title[MAX_PATH];
+	int len;
+
     struct cuesheet_file cue_file;
     struct mp3entry *id3 = audio_current_track();
 
-	int remaining = 0;
-    snprintf(title, sizeof(title), "%s: %n%.*s", cue->performer, 
-					&remaining, MAX_PATH - remaining, cue->title);
+    len = snprintf(title, sizeof(title), "%s: %s", cue->performer, cue->title);
+
+	if ((unsigned) len > sizeof(title))
+		DEBUGF("browse_cuesheet title truncated\n");
 
     gui_synclist_init(&lists, list_get_name_cb, cue, false, 2, NULL);
     gui_synclist_set_nb_items(&lists, 2*cue->track_count);
