2d9c8e9000a6a0f868a807f0d08292e61ff5991a
William Wilgus <me.theuser@yahoo.com>
2018-10-17 18:35:02 -0400

    Lua Boomshine Update with circles + levels
    added a few more levels and increased difficulty based on level
    
    Rliimage allows us to use circles in boomshine
    Optimized some of the code to exceed the speed of the original
    
    Change-Id: I68eb74f8b68fcdc8a9d022cd172374744425f15d

--- apps/plugins/boomshine.lua
+++ apps/plugins/boomshine.lua
@@ -11,6 +11,7 @@
  See http://www.yvoschaap.com/chainrxn/ and http://www.k2xl.com/games/boomshine/
 
  Copyright (C) 2009 by Maurus Cuelenaere
+ Copyright (C) 2018 William Wilgus -- Added circles, blit cursor
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -24,11 +25,20 @@
 
 require "actions"
 
+local _LCD = rb.lcd_framebuffer()
+local BSAND = 0x8
+local rocklib_image = getmetatable(rb.lcd_framebuffer())
+local _ellipse = rocklib_image.ellipse
+
+local default_cursor
+local backdrop = rb.new_image()
+
 local CYCLETIME = rb.HZ / 50
 local HAS_TOUCHSCREEN = rb.action_get_touchscreen_press ~= nil
 local DEFAULT_BALL_SIZE = rb.LCD_HEIGHT > rb.LCD_WIDTH and rb.LCD_WIDTH  / 30
                                                        or  rb.LCD_HEIGHT / 30
 local MAX_BALL_SPEED = DEFAULT_BALL_SIZE / 2
+local DEC_BALL_SPEED = DEFAULT_BALL_SIZE / 8
 local DEFAULT_FOREGROUND_COLOR = rb.lcd_get_foreground ~= nil
                                                      and rb.lcd_get_foreground()
                                                      or  0
@@ -46,7 +56,10 @@ local levels = {
                 {30, 45},
                 {37, 50},
                 {48, 55},
-                {55, 60}
+                {55, 60},
+                {29, 30},
+                {24, 25},
+                {14, 15}
            }
 
 local Ball = {
@@ -55,16 +68,43 @@ local Ball = {
                 implosion = false
              }
 
-function Ball:new(o)
+local function create_cursor(size, default)
+    if not default then
+        default_cursor = rb.new_image(size, size)
+        default_cursor:clear(0)
+        local sz2 = size / 2
+        local sz4 = size / 4
+
+        default_cursor:line(1, 1, sz4, 1, 1)
+        default_cursor:line(1, 1, 1, sz4, 1)
+
+        default_cursor:line(1, size, sz4, size, 1)
+        default_cursor:line(1, size, 1, size - sz4, 1)
+
+        default_cursor:line(size, size, size - sz4, size, 1)
+        default_cursor:line(size, size, size, size - sz4, 1)
+
+        default_cursor:line(size, 1, size - sz4, 1, 1)
+        default_cursor:line(size, 1, size, sz4, 1)
+
+        --crosshairs
+        default_cursor:line(sz2 - sz4, sz2, sz2 + sz4, sz2, 1)
+        default_cursor:line(sz2, sz2 - sz4, sz2, sz2 + sz4, 1)
+    end
+    return default_cursor
+end
+
+function Ball:new(o, level)
+    level = level or 1
     if o == nil then
         o = {
                 x = math.random(0, rb.LCD_WIDTH - self.size),
                 y = math.random(0, rb.LCD_HEIGHT - self.size),
                 color = random_color(),
-                up_speed = Ball:generateSpeed(),
-                right_speed = Ball:generateSpeed(),
+                up_speed = Ball:generateSpeed(level),
+                right_speed = Ball:generateSpeed(level),
                 explosion_size = math.random(2*self.size, 4*self.size),
-                life_duration = math.random(rb.HZ, rb.HZ*5)
+                life_duration = math.random(rb.HZ / level, rb.HZ*5),
             }
     end
 
@@ -73,8 +113,10 @@ function Ball:new(o)
     return o
 end
 
-function Ball:generateSpeed()
-    local speed = math.random(-MAX_BALL_SPEED, MAX_BALL_SPEED)
+function Ball:generateSpeed(level)
+    local ballspeed = MAX_BALL_SPEED - (DEC_BALL_SPEED * (level - 1))
+    if ballspeed < 2 then ballspeed = 2 end
+    local speed = math.random(-ballspeed, ballspeed)
     if speed == 0 then
         speed = 1       -- Make sure all balls move
     end
@@ -82,30 +124,43 @@ function Ball:generateSpeed()
     return speed
 end
 
+function Ball:draw_exploded()
+    --[[
+         I know these aren't circles, but as there's no current circle
+         implementation in Rockbox, rectangles will just do fine (drawing
+         circles from within Lua is far too slow).
+    --set_foreground(self.color)
+    --rb.lcd_fillrect(self.x, self.y, self.size, self.size)
+    ]]
+
+    _ellipse(_LCD, self.x, self.y, self.x + self.size, self.y + self.size , self.color, nil, true)
+end
+
 function Ball:draw()
     --[[
          I know these aren't circles, but as there's no current circle
          implementation in Rockbox, rectangles will just do fine (drawing
          circles from within Lua is far too slow).
-    ]]--
-    set_foreground(self.color)
-    rb.lcd_fillrect(self.x, self.y, self.size, self.size)
+    --set_foreground(self.color)
+    --rb.lcd_fillrect(self.x, self.y, self.size, self.size)
+    ]]
+
+    _ellipse(_LCD, self.x, self.y, self.x + self.size, self.y + self.size , self.color, self.color, true)
+end
+
+function Ball:step_exploded()
+    if self.implosion and self.size > 0 then
+        self.size = self.size - 2
+        self.x = self.x + 1 -- We do this because we want to stay centered
+        self.y = self.y + 1
+    elseif self.size < self.explosion_size then
+        self.size = self.size + 2
+        self.x = self.x - 1 -- We do this for the same reasons as above
+        self.y = self.y - 1
+    end
 end
 
 function Ball:step()
-    if self.exploded then
-        if self.implosion and self.size > 0 then
-            self.size = self.size - 2
-            self.x = self.x + 1 -- We do this because we want to stay centered
-            self.y = self.y + 1
-        elseif self.size < self.explosion_size then
-            self.size = self.size + 2
-            self.x = self.x - 1 -- We do this for the same reasons as above
-            self.y = self.y - 1
-        end
-        return
-    end
-
     self.x = self.x + self.right_speed
     self.y = self.y + self.up_speed
     if (self.right_speed > 0 and self.x + self.size >= rb.LCD_WIDTH) or
@@ -137,7 +192,8 @@ end
 local Cursor = {
                 size = DEFAULT_BALL_SIZE*2,
                 x = rb.LCD_WIDTH/2,
-                y = rb.LCD_HEIGHT/2
+                y = rb.LCD_HEIGHT/2,
+                image = create_cursor(DEFAULT_BALL_SIZE*2, default_cursor)
              }
 
 function Cursor:new()
@@ -176,6 +232,7 @@ function Cursor:do_action(action)
 end
 
 function Cursor:draw()
+--[[
     set_foreground(DEFAULT_FOREGROUND_COLOR)
 
     rb.lcd_hline(self.x - self.size/2, self.x - self.size/4, self.y - self.size/2)
@@ -189,6 +246,9 @@ function Cursor:draw()
 
     rb.lcd_hline(self.x - self.size/4, self.x + self.size/4, self.y)
     rb.lcd_vline(self.x, self.y - self.size/4, self.y + self.size/4)
+]]
+    rocklib_image.copy(_LCD, self.image, self.x - self.size/2, self.y - self.size/2,
+                       _NIL, _NIL, _NIL, _NIL, true, BSAND, DEFAULT_FOREGROUND_COLOR)
 end
 
 function draw_positioned_string(bottom, right, str)
@@ -213,16 +273,19 @@ end
 
 function start_round(level, goal, nrBalls, total)
     local player_added, score, exit, nrExpendedBalls = false, 0, false, 0
-    local balls, explodedBalls = {}, {}
+    local Balls, explodedBalls = {}, {}
     local cursor = Cursor:new()
+    local hit_detected = true
 
     -- Initialize the balls
     for _=1,nrBalls do
-        table.insert(balls, Ball:new())
+        table.insert(Balls, Ball:new(nil, level))
     end
 
     -- Make sure there are no unwanted touchscreen presses
     rb.button_clear_queue()
+
+    set_foreground(DEFAULT_FOREGROUND_COLOR) -- color for text
 
     while true do
         local endtick = rb.current_tick() + CYCLETIME
@@ -239,6 +302,7 @@ function start_round(level, goal, nrBalls, total)
             break
         end
         if not player_added and cursor:do_action(action) then
+
             local player = Ball:new({
                                 x = cursor.x,
                                 y = cursor.y,
@@ -248,18 +312,17 @@ function start_round(level, goal, nrBalls, total)
                                 exploded = true,
                                 death_time = rb.current_tick() + rb.HZ * 3
                             })
-            table.insert(explodedBalls, player)
+            explodedBalls[1] = player
             player_added = true
         end
 
-        -- Check for hits
-        for i, ball in ipairs(balls) do
+       -- Check for hits
+        for i, Ball in ipairs(Balls) do
             for _, explodedBall in ipairs(explodedBalls) do
-                if ball:checkHit(explodedBall) then
-                    score = score + 100*level
-                    nrExpendedBalls = nrExpendedBalls + 1
-                    table.insert(explodedBalls, ball)
-                    table.remove(balls, i)
+                if Ball:checkHit(explodedBall) then
+                    explodedBalls[#explodedBalls + 1] = Ball
+                    table.remove(Balls, i)
+                    hit_detected = true
                     break
                 end
             end
@@ -277,26 +340,34 @@ function start_round(level, goal, nrBalls, total)
         end
 
         -- Drawing phase
-        rb.lcd_clear_display()
+        if hit_detected then
+            hit_detected = false;
+            -- Calculate score
+            nrExpendedBalls = nrBalls - #Balls
+            score = nrExpendedBalls * level * 100
 
-        set_foreground(DEFAULT_FOREGROUND_COLOR)
-        draw_positioned_string(0, 0, string.format("%d balls expended", nrExpendedBalls))
-        draw_positioned_string(0, 1, string.format("Level %d", level))
-        draw_positioned_string(1, 1, string.format("%d level points", score))
-        draw_positioned_string(1, 0, string.format("%d total points", total+score))
+            rb.lcd_clear_display()
+            draw_positioned_string(0, 0, string.format("%d balls expended", nrExpendedBalls))
+            draw_positioned_string(0, 1, string.format("Level %d", level))
+            draw_positioned_string(1, 1, string.format("%d level points", score))
+            draw_positioned_string(1, 0, string.format("%d total points", total + score))
+            backdrop:copy(_LCD)
+        else
+            _LCD:copy(backdrop)
 
-        for _, ball in ipairs(balls) do
-            ball:step()
-            ball:draw()
+            if not HAS_TOUCHSCREEN and not player_added then
+                cursor:draw()
+            end
+        end
+
+        for _, Ball in ipairs(Balls) do
+            Ball:step()
+            Ball:draw()
         end
 
         for _, explodedBall in ipairs(explodedBalls) do
-            explodedBall:step()
-            explodedBall:draw()
-        end
-
-        if not HAS_TOUCHSCREEN and not player_added then
-            cursor:draw()
+            explodedBall:step_exploded()
+            explodedBall:draw_exploded()
         end
 
         -- Push framebuffer to the LCD
@@ -359,6 +430,8 @@ local idx, highscore = 1, 0
 while levels[idx] ~= nil do
     local goal, nrBalls = levels[idx][1], levels[idx][2]
 
+    collectgarbage("collect") --run gc now to hopefully prevent interruption later
+
     display_message(rb.HZ*2, "Level %d: get %d out of %d balls", idx, goal, nrBalls)
 
     local exit, score, nrExpendedBalls = start_round(idx, goal, nrBalls, highscore)
@@ -383,3 +456,4 @@ end
 
 -- Restore user backlight settings
 rb.backlight_use_settings()
+
