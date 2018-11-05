8b4892aa2987e1bf0f5831ea1011d045b1a2c83f
Marcin Bukat <marcin.bukat@gmail.com>
2018-11-05 13:01:55 +0100

    xDuoo X3II and X20 port  (Untested!)
    Provided by Roman Stolyarov
    Integration and Upstreaming work by Solomon Peachy
    
    Change-Id: I34a674051d368efcc75d1d18c725971fe46c3eee

--- apps/plugins/cube.c
+++ apps/plugins/cube.c
@@ -423,6 +423,26 @@
 #define CUBE_PAUSE         BUTTON_HOME
 #define CUBE_HIGHSPEED     BUTTON_PLAY
 
+#elif (CONFIG_KEYPAD == XDUOO_X3II_PAD)
+#define CUBE_QUIT          BUTTON_POWER
+#define CUBE_NEXT          BUTTON_NEXT
+#define CUBE_PREV          BUTTON_PREV
+#define CUBE_INC           BUTTON_VOL_UP
+#define CUBE_DEC           BUTTON_VOL_DOWN
+#define CUBE_MODE          BUTTON_OPTION
+#define CUBE_PAUSE         BUTTON_HOME
+#define CUBE_HIGHSPEED     BUTTON_PLAY
+
+#elif (CONFIG_KEYPAD == XDUOO_X20_PAD)
+#define CUBE_QUIT          BUTTON_POWER
+#define CUBE_NEXT          BUTTON_NEXT
+#define CUBE_PREV          BUTTON_PREV
+#define CUBE_INC           BUTTON_VOL_UP
+#define CUBE_DEC           BUTTON_VOL_DOWN
+#define CUBE_MODE          BUTTON_OPTION
+#define CUBE_PAUSE         BUTTON_HOME
+#define CUBE_HIGHSPEED     BUTTON_PLAY
+
 #elif (CONFIG_KEYPAD == IHIFI_770_PAD)
 #define CUBE_QUIT          BUTTON_POWER
 #define CUBE_NEXT          BUTTON_NEXT
