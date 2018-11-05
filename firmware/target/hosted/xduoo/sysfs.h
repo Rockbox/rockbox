f75a37670ec672ccab6d3bd31acb2e02e2df0c68
Marcin Bukat <marcin.bukat@gmail.com>
2018-11-05 13:01:55 +0100

    xDuoo X3II and X20 port  (Untested!)
    Provided by Roman Stolyarov
    Integration and Upstreaming work by Solomon Peachy
    
    Change-Id: I34a674051d368efcc75d1d18c725971fe46c3eee

---
+++ firmware/target/hosted/xduoo/sysfs.h
@@ -0,0 +1,31 @@
+/***************************************************************************
+ *             __________               __   ___
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2014 by Ilia Sergachev: Initial Rockbox port to iBasso DX50
+ * Copyright (C) 2014 by Mario Basister: iBasso DX90 port
+ * Copyright (C) 2014 by Simon Rothen: Initial Rockbox repository submission, additional features
+ * Copyright (C) 2014 by Udo Schläpfer: Code clean up, additional features
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include <stdbool.h>
+
+bool sysfs_get_int(const char *path, int *value);
+bool sysfs_set_int(const char *path, int value);
+bool sysfs_get_char(const char *path, char *value);
+bool sysfs_set_char(const char *path, char value);
+bool sysfs_get_string(const char *path, char *value, int size);
+bool sysfs_set_string(const char *path, char *value);
