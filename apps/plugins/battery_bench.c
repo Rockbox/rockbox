1b6aa649cadcef7a09ef349452080c82addd8756
Marcin Bukat <marcin.bukat@gmail.com>
2018-11-05 13:01:55 +0100

    xDuoo X3II and X20 port
    Provided by Roman Stolyarov
    Integration, Refactoring, and Upstreaming by Solomon Peachy
    
    X3II confirmed working by forum tester, X20 is nearly identical.
    
    This includes bootloader, main firmware, and the flash image patcher.
    
    Eventual Todo:
    
     * Further refactor AGPTek Rocker & xduoo hiby bootloaders
     * Further refactor AGPTek Rocker & xduoo hosted platform code
    
    Change-Id: I34a674051d368efcc75d1d18c725971fe46c3eee

--- apps/plugins/battery_bench.c
+++ apps/plugins/battery_bench.c
@@ -298,6 +298,18 @@
 #define BATTERY_ON_TXT  "PLAY - start"
 #define BATTERY_OFF_TXT "POWER"
 
+#elif CONFIG_KEYPAD == XDUOO_X3II_PAD
+#define BATTERY_ON      BUTTON_PLAY
+#define BATTERY_OFF     BUTTON_POWER
+#define BATTERY_ON_TXT  "Play - start"
+#define BATTERY_OFF_TXT "POWER"
+
+#elif CONFIG_KEYPAD == XDUOO_X20_PAD
+#define BATTERY_ON      BUTTON_PLAY
+#define BATTERY_OFF     BUTTON_POWER
+#define BATTERY_ON_TXT  "Play - start"
+#define BATTERY_OFF_TXT "POWER"
+
 #else
 #error No keymap defined!
 #endif
