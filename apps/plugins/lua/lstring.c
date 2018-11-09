14ebeddb734fc94dee09323311670568855e11db
William Wilgus <me.theuser@yahoo.com>
2018-11-09 01:46:34 -0500

    Lua Small memory optimization in Lua strings
    Get rid of the "reserved" byte in the TString data type. Might actually
    save more than one byte, depdending on the compiler structure alignment
    rules (and the architecture). Tested with the Lua 5.1 test suite.
    
    bogdanm -- elua https://github.com/elua/
    
    Change-Id: I9d3469ffeccb99ec967948737cbb15c807d4da74

--- apps/plugins/lua/lstring.c
+++ apps/plugins/lua/lstring.c
@@ -58,7 +58,6 @@ static TString *newlstr (lua_State *L, const char *str, size_t l,
   ts->tsv.hash = h;
   ts->tsv.marked = luaC_white(G(L));
   ts->tsv.tt = LUA_TSTRING;
-  ts->tsv.reserved = 0;
   memcpy(ts+1, str, l*sizeof(char));
   ((char *)(ts+1))[l] = '\0';  /* ending 0 */
   tb = &G(L)->strt;
