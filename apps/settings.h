Rockbox/rockbox

commit a06d9c85f7475d650cc451fb0f537623c0206f5a

Auto-Ranging Time Formatting For Menus (hh:mm:ss:mss)
Unifies time formatting in settings_list.c allows time format to
display as HH:MM:SS.MSS or any consecutive combination thereof
(hh:mm:ss, mm:ss, mm:ss.mss, ss.mss, hh, mm, ss ,mss)
works in INT and TABLE settings with the addition of flag 'F_TIME_SETTING'

Time is auto-ranged dependent on value

Adds talk_time_intervals to allow time values to be spoken similar to
display format:  x Hours, x Minutes, x Seconds, x Milliseconds

Table lookups merged or removed from recording, clip meter and lcd timeout
-String_Choice replaced with TABLE_SETTING or INT_SETTING for these
functions as well, cleaned-up cfg_vals that get saved to cfgfile

RTL Languages ARE supported

Negative values ARE supported

Backlight on/off are now Always and Never to share formatter with LCD
Timeout

Added flag to allow ranged units to be locked to a minimum index

Added flag to allow leading zero to be supressed from the largest unit

merged talk_time_unit() and talk_time_intervals()

optimized time_split()

optimized format_time_auto()

Backlight time-out list same as original

Change-Id: I59027c62d3f2956bd16fdcc1a48b2ac32c084abd

apps/settings.h
@@ -396,11 +396,7 @@ struct user_settings
     bool rec_editable; /* true means that the bit reservoir is off */
 
     /* note: timesplit setting is not saved */
-    int rec_timesplit; /* 0 = off,
-                          1 = 00:05, 2 = 00:10, 3 = 00:15, 4 = 00:30
-                          5 = 01:00, 6 = 02:00, 7 = 04:00, 8 = 06:00
-                          9 = 08:00, 10= 10:00, 11= 12:00, 12= 18:00,
-                          13= 24:00 */
+    int rec_timesplit;  /* IN MINUTES 0 = off */
     int rec_sizesplit; /* 0 = off,
                           1 = 5MB, 2 = 10MB, 3 = 15MB, 4 = 32MB
                           5 = 64MB, 6 = 75MB, 7 = 100MB, 8 = 128MB

firmware/backlight.c
@@ -191,10 +191,6 @@ static int remote_backlight_on_button_hold = 0;
 
 #ifdef HAVE_LCD_SLEEP
 #ifdef HAVE_LCD_SLEEP_SETTING
-const signed char lcd_sleep_timeout_value[10] =
-{
-    -1, 0, 5, 10, 15, 20, 30, 45, 60, 90
-};
 static int lcd_sleep_timeout = 10*HZ;
 #else
 /* Target defines needed value */
@@ -885,23 +881,21 @@ void backlight_set_on_button_hold(int index)
 #endif /* HAS_BUTTON_HOLD */
 
 #ifdef HAVE_LCD_SLEEP_SETTING
-void lcd_set_sleep_after_backlight_off(int index)
+void lcd_set_sleep_after_backlight_off(int timeout_seconds)
 {
-    if ((unsigned)index >= sizeof(lcd_sleep_timeout_value))
-        /* if given a weird value, use default */
-        index = 3;
-
-    lcd_sleep_timeout = HZ * lcd_sleep_timeout_value[index];
+    lcd_sleep_timeout = HZ * timeout_seconds;
 
     if (is_backlight_on(true))
-        /* Timer will be set when bl turns off or bl set to on. */
+    /* Timer will be set when bl turns off or bl set to on. */
         return;
 
     /* Backlight is Off */
     if (lcd_sleep_timeout < 0)
+        lcd_sleep_timer = 0; /* Never */
+    else if (lcd_sleep_timeout == 0)
         lcd_sleep_timer = 1; /* Always - sleep next tick */
     else
-        lcd_sleep_timer = lcd_sleep_timeout; /* Never, other */
+        lcd_sleep_timer = lcd_sleep_timeout; /* other */
 }
 #endif /* HAVE_LCD_SLEEP_SETTING */
 

apps/gui/option_select.c
@@ -48,18 +48,15 @@ int option_value_as_int(const struct settings_list *setting)
         temp = *(int*)setting->setting;
     return temp;
 }
-static const char *unit_strings[] = 
-{   
-    [UNIT_INT] = "",    [UNIT_MS]  = "ms",
-    [UNIT_SEC] = "s",   [UNIT_MIN] = "min", 
-    [UNIT_HOUR]= "hr",  [UNIT_KHZ] = "kHz",
-    [UNIT_DB]  = "dB",  [UNIT_PERCENT] = "%",
-    [UNIT_MAH] = "mAh", [UNIT_PIXEL] = "px",
-    [UNIT_PER_SEC] = "per sec",
-    [UNIT_HERTZ] = "Hz",
-    [UNIT_MB]  = "MB",  [UNIT_KBIT]  = "kb/s",
-    [UNIT_PM_TICK] = "units/10ms",
-};
+
+/* return an auto ranged time string, unit_idx specifies lowest or
+   base index of the passed value -- flag F_TIME_SETTING calls this */
+static const char *option_get_timestring(char *buf, int buf_len,
+                                         int val, int unit_idx)
+{
+    return format_time_auto(buf, buf_len, val, unit_idx | UNIT_TRIM_ZERO, false);
+}
+
 /* these two vars are needed so arbitrary values can be added to the
    TABLE_SETTING settings if the F_ALLOW_ARBITRARY_VALS flag is set */
 static int table_setting_oldval = 0, table_setting_array_position = 0;
@@ -87,22 +84,28 @@ const char *option_get_valuestring(const struct settings_list *setting,
     {
         const struct int_setting *int_info = setting->int_setting;
         const struct table_setting *tbl_info = setting->table_setting;
-        const char *unit;
+        int info_unit;
+        const char *str_unit;
         const char* (*formatter)(char*, size_t, int, const char*);
         if ((setting->flags & F_INT_SETTING) == F_INT_SETTING)
         {
             formatter = int_info->formatter;
-            unit = unit_strings[int_info->unit];
+            info_unit = int_info->unit;
         }
         else
         {
             formatter = tbl_info->formatter;
-            unit = unit_strings[tbl_info->unit];
+            info_unit = tbl_info->unit;
         }
+
+        if ((setting->flags & F_TIME_SETTING) == F_TIME_SETTING)
+            str = option_get_timestring(buffer, buf_len, (long)temp_var, info_unit);
+
+        str_unit = unit_strings_core[info_unit];
         if (formatter)
-            str = formatter(buffer, buf_len, (int)temp_var, unit);
-        else
-            snprintf(buffer, buf_len, "%d %s", (int)temp_var, unit?unit:"");
+            str = formatter(buffer, buf_len, (int)temp_var, str_unit);
+        else if ((setting->flags & F_TIME_SETTING) != F_TIME_SETTING)
+            snprintf(buffer, buf_len, "%d %s", (int)temp_var, str_unit?str_unit:"");
     }
     else if ((setting->flags & F_T_SOUND) == F_T_SOUND)
     {
@@ -167,6 +170,8 @@ void option_talk_value(const struct settings_list *setting, int value, bool enqu
         }
         if (get_talk_id)
             talk_id(get_talk_id(value, unit), enqueue);
+        else if ((setting->flags & F_TIME_SETTING) == F_TIME_SETTING)
+            talk_time_intervals(value, unit, enqueue);
         else
             talk_value(value, unit, enqueue);
     }

apps/lang/english.lang
@@ -2697,19 +2697,19 @@
 </phrase>
 <phrase>
   id: LANG_NEVER
-  desc: in lcd settings
+  desc: deprecated
   user: core
   <source>
     *: none
-    lcd_sleep: "Never"
+    lcd_sleep: ""
   </source>
   <dest>
     *: none
-    lcd_sleep: "Never"
+    lcd_sleep: ""
   </dest>
   <voice>
     *: none
-    lcd_sleep: "Never"
+    lcd_sleep: ""
   </voice>
 </phrase>
 <phrase>
@@ -13785,5 +13785,19 @@
   </dest>
   <voice>
     *: "No viewers found"
+  </voice>
+</phrase>
+<phrase>
+  id: LANG__NEVER
+  desc: in lcd settings
+  user: core
+  <source>
+    *: "Never"
+  </source>
+  <dest>
+    *: "Never"
+  </dest>
+  <voice>
+    *: "Never"
   </voice>
 </phrase>

apps/misc.c
@@ -28,6 +28,8 @@
 #include "misc.h"
 #include "system.h"
 #include "lcd.h"
+#include "language.h" /* is_lang_rtl() */
+
 #ifdef HAVE_DIRCACHE
 #include "dircache.h"
 #endif
@@ -116,6 +118,20 @@ const unsigned char * const byte_units[] =
 };
 
 const unsigned char * const * const kibyte_units = &byte_units[1];
+
+/* units used with format_time_auto, option_select.c->option_get_valuestring() */
+const unsigned char * const unit_strings_core[] =
+{
+    [UNIT_INT] = "",    [UNIT_MS]  = "ms",
+    [UNIT_SEC] = "s",   [UNIT_MIN] = "min",
+    [UNIT_HOUR]= "hr",  [UNIT_KHZ] = "kHz",
+    [UNIT_DB]  = "dB",  [UNIT_PERCENT] = "%",
+    [UNIT_MAH] = "mAh", [UNIT_PIXEL] = "px",
+    [UNIT_PER_SEC] = "per sec",
+    [UNIT_HERTZ] = "Hz",
+    [UNIT_MB]  = "MB",  [UNIT_KBIT]  = "kb/s",
+    [UNIT_PM_TICK] = "units/10ms",
+};
 
 /* Format a large-range value for output, using the appropriate unit so that
  * the displayed value is in the range 1 <= display < 1000 (1024 for "binary"
@@ -1096,6 +1112,216 @@ char* skip_whitespace(char* const str)
     return s;
 }
 
+/*  time_split_units()
+    split time values depending on base unit
+    unit_idx: UNIT_HOUR, UNIT_MIN, UNIT_SEC, UNIT_MS
+    abs_value: absolute time value
+    units_in: array of unsigned ints with UNIT_IDX_TIME_COUNT fields
+*/
+unsigned int time_split_units(int unit_idx, unsigned long abs_val,
+                              unsigned long (*units_in)[UNIT_IDX_TIME_COUNT])
+{
+    unsigned int base_idx = UNIT_IDX_HR;
+    unsigned long hours;
+    unsigned long minutes  = 0;
+    unsigned long seconds  = 0;
+    unsigned long millisec = 0;
+
+    switch (unit_idx & UNIT_IDX_MASK) /*Mask off upper bits*/
+    {
+            case UNIT_MS:
+                base_idx = UNIT_IDX_MS;
+                millisec = abs_val;
+                abs_val  = abs_val  /  1000U;
+                millisec = millisec - (1000U * abs_val);
+                /* fallthrough and calculate the rest of the units */
+            case UNIT_SEC:
+                if (base_idx == UNIT_IDX_HR)
+                    base_idx = UNIT_IDX_SEC;
+                seconds  = abs_val;
+                abs_val  = abs_val  / 60U;
+                seconds  = seconds - (60U * abs_val);
+                /* fallthrough and calculate the rest of the units */
+            case UNIT_MIN:
+                if (base_idx == UNIT_IDX_HR)
+                    base_idx = UNIT_IDX_MIN;
+                minutes  = abs_val;
+                abs_val  = abs_val / 60U;
+                minutes  = minutes -(60U * abs_val);
+                /* fallthrough and calculate the rest of the units */
+            case UNIT_HOUR:
+            default:
+                hours    = abs_val;
+                break;
+    }
+
+    (*units_in)[UNIT_IDX_HR]  = hours;
+    (*units_in)[UNIT_IDX_MIN] = minutes;
+    (*units_in)[UNIT_IDX_SEC] = seconds;
+    (*units_in)[UNIT_IDX_MS]  = millisec;
+
+    return base_idx;
+}
+
+/* format_time_auto - return an auto ranged time string;
+   buffer:  needs to be at least 25 characters for full range
+
+   unit_idx: specifies lowest or base index of the value
+   add | UNIT_LOCK_ to keep place holder of units that would normally be
+   discarded.. For instance, UNIT_LOCK_HR would keep the hours place, ex: string
+   00:10:10 (0 HRS 10 MINS 10 SECONDS) normally it would return as 10:10
+   add | UNIT_TRIM_ZERO to supress leading zero on the largest unit
+
+   value: should be passed in the same form as unit_idx
+
+   supress_unit: may be set to true and in this case the
+   hr, min, sec, ms identifiers will be left off the resulting string but
+   since right to left languages are handled it is advisable to leave units
+   as an indication of the text direction
+*/
+
+const char *format_time_auto(char *buffer, int buf_len, long value,
+                                  int unit_idx, bool supress_unit)
+{
+    const char * const sign        = &"-"[value < 0 ? 0 : 1];
+    bool               is_rtl      = lang_is_rtl();
+    char               timebuf[25]; /* -2147483648:00:00.00\0 */
+    int                len, left_offset;
+    unsigned char      base_idx, max_idx;
+
+    unsigned long  units_in[UNIT_IDX_TIME_COUNT];
+    unsigned char  fwidth[UNIT_IDX_TIME_COUNT] =
+                   {
+                        [UNIT_IDX_HR]  = 0, /* hr is variable length */
+                        [UNIT_IDX_MIN] = 2,
+                        [UNIT_IDX_SEC] = 2,
+                        [UNIT_IDX_MS]  = 3,
+                   }; /* {0,2,2,3}; Field Widths */
+    unsigned char  offsets[UNIT_IDX_TIME_COUNT] =
+                   {
+                        [UNIT_IDX_HR]  = 10,/* ?:59:59.999 Std offsets    */
+                        [UNIT_IDX_MIN] = 7, /*0?:+1:+4.+7 need calculated */
+                        [UNIT_IDX_SEC] = 4,/* 999.59:59:0  RTL offsets    */
+                        [UNIT_IDX_MS]  = 0,/* 0  .4 :7 :10 won't change   */
+                   }; /* {10,7,4,0}; Offsets */
+    const uint16_t unitlock[UNIT_IDX_TIME_COUNT] =
+                   {
+                        [UNIT_IDX_HR]  = UNIT_LOCK_HR,
+                        [UNIT_IDX_MIN] = UNIT_LOCK_MIN,
+                        [UNIT_IDX_SEC] = UNIT_LOCK_SEC,
+                        [UNIT_IDX_MS]  = 0,
+                   }; /* unitlock */
+    const uint16_t units[UNIT_IDX_TIME_COUNT] =
+                   {
+                        [UNIT_IDX_HR]  = UNIT_HOUR,
+                        [UNIT_IDX_MIN] = UNIT_MIN,
+                        [UNIT_IDX_SEC] = UNIT_SEC,
+                        [UNIT_IDX_MS]  = UNIT_MS,
+                   }; /* units */
+
+#if 0 /* unused */
+    if (idx_pos != NULL)
+    {
+        (*idx_pos)[0] = MIN((*idx_pos)[0], UNIT_IDX_TIME_COUNT - 1);
+        unit_idx |= unitlock[(*idx_pos)[0]];
+    }
+#endif
+
+    base_idx = time_split_units(unit_idx, labs(value), &units_in);
+
+    if (units_in[UNIT_IDX_HR] || (unit_idx & unitlock[UNIT_IDX_HR]))
+        max_idx = UNIT_IDX_HR;
+    else if (units_in[UNIT_IDX_MIN] || (unit_idx & unitlock[UNIT_IDX_MIN]))
+        max_idx = UNIT_IDX_MIN;
+    else if (units_in[UNIT_IDX_SEC] || (unit_idx & unitlock[UNIT_IDX_SEC]))
+        max_idx = UNIT_IDX_SEC;
+    else if (units_in[UNIT_IDX_MS])
+        max_idx = UNIT_IDX_MS;
+    else /* value is 0 */
+        max_idx = base_idx;
+
+    if (!is_rtl)
+    {
+        len = snprintf(timebuf, sizeof(timebuf),
+                       "%02lu:%02lu:%02lu.%03lu",
+                       units_in[UNIT_IDX_HR],
+                       units_in[UNIT_IDX_MIN],
+                       units_in[UNIT_IDX_SEC],
+                       units_in[UNIT_IDX_MS]);
+
+        fwidth[UNIT_IDX_HR]   = len - offsets[UNIT_IDX_HR];
+
+        /* calculate offsets of the other fields based on length of previous */
+        offsets[UNIT_IDX_MS]  = fwidth[UNIT_IDX_HR] + offsets[UNIT_IDX_MIN];
+        offsets[UNIT_IDX_SEC] = fwidth[UNIT_IDX_HR] + offsets[UNIT_IDX_SEC];
+        offsets[UNIT_IDX_MIN] = fwidth[UNIT_IDX_HR] + 1;
+        offsets[UNIT_IDX_HR]  = 0;
+
+        timebuf[offsets[base_idx] + fwidth[base_idx]] = '\0';
+
+        left_offset  = -(offsets[max_idx]);
+        left_offset += strlcpy(buffer, sign, buf_len);
+
+        /* trim leading zero on the max_idx */
+        if ((unit_idx & UNIT_TRIM_ZERO) == UNIT_TRIM_ZERO &&
+            timebuf[offsets[max_idx]] == '0' && fwidth[max_idx] > 1)
+        {
+            offsets[max_idx]++;
+        }
+
+        strlcat(buffer, &timebuf[offsets[max_idx]], buf_len);
+
+        if (!supress_unit)
+        {
+            strlcat(buffer, " ", buf_len);
+            strlcat(buffer, unit_strings_core[units[max_idx]], buf_len);
+        }
+    }
+    else /*RTL Languages*/
+    {
+        len = snprintf(timebuf, sizeof(timebuf),
+                       "%03lu.%02lu:%02lu:%02lu",
+                       units_in[UNIT_IDX_MS],
+                       units_in[UNIT_IDX_SEC],
+                       units_in[UNIT_IDX_MIN],
+                       units_in[UNIT_IDX_HR]);
+
+        fwidth[UNIT_IDX_HR] = len - offsets[UNIT_IDX_HR];
+
+        left_offset = -(offsets[base_idx]);
+
+        /* trim leading zero on the max_idx */
+        if ((unit_idx & UNIT_TRIM_ZERO) == UNIT_TRIM_ZERO &&
+            timebuf[offsets[max_idx]] == '0' && fwidth[max_idx] > 1)
+        {
+            timebuf[offsets[max_idx]] = timebuf[offsets[max_idx]+1];
+            fwidth[max_idx]--;
+        }
+
+        timebuf[offsets[max_idx] + fwidth[max_idx]] = '\0';
+
+        if (!supress_unit)
+        {
+            strlcpy(buffer, unit_strings_core[units[max_idx]], buf_len);
+            left_offset += strlcat(buffer, " ", buf_len);
+            strlcat(buffer, &timebuf[offsets[base_idx]], buf_len);
+        }
+        else
+            strlcpy(buffer, &timebuf[offsets[base_idx]], buf_len);
+
+        strlcat(buffer, sign, buf_len);
+    }
+#if 0 /* unused */
+    if (idx_pos != NULL)
+    {
+        (*idx_pos)[1]= fwidth[*(idx_pos)[0]];
+        (*idx_pos)[0]= left_offset + offsets[(*idx_pos)[0]];
+    }
+#endif
+
+    return buffer;
+}
+
 /* Format time into buf.
  *
  * buf      - buffer to format to.
@@ -1104,13 +1330,12 @@ char* skip_whitespace(char* const str)
  */
 void format_time(char* buf, int buf_size, long t)
 {
-    unsigned long time = labs(t / 1000);
-    unsigned long hours = time / 3600;
-    unsigned long minutes = time / 60 - hours * 60;
-    unsigned long seconds = time % 60;
-    int hashours = hours > 0;
+    unsigned long units_in[UNIT_IDX_TIME_COUNT] = {0};
+    time_split_units(UNIT_MS, labs(t), &units_in);
+    int hashours = units_in[UNIT_IDX_HR] > 0;
     snprintf(buf, buf_size, "%.*s%.0lu%.*s%.*lu:%.2lu",
-             t < 0, "-", hours, hashours, ":", hashours+1, minutes, seconds);
+             t < 0, "-", units_in[UNIT_IDX_HR], hashours, ":",
+             hashours+1, units_in[UNIT_IDX_MIN], units_in[UNIT_IDX_SEC]);
 }
 
 /**

apps/misc.h
@@ -28,7 +28,7 @@
 
 extern const unsigned char * const byte_units[];
 extern const unsigned char * const * const kibyte_units;
-
+extern const unsigned char * const unit_strings_core[];
 /* Format a large-range value for output, using the appropriate unit so that
  * the displayed value is in the range 1 <= display < 1000 (1024 for "binary"
  * units) if possible, and 3 significant digits are shown. If a buffer is
@@ -40,6 +40,50 @@ char *output_dyn_value(char *buf,
                        const unsigned char * const *units,
                        unsigned int unit_count,
                        bool binary_scale);
+
+
+/* format_time_auto */
+enum e_fmt_time_auto_idx
+{
+    UNIT_IDX_HR = 0,
+    UNIT_IDX_MIN,
+    UNIT_IDX_SEC,
+    UNIT_IDX_MS,
+    UNIT_IDX_TIME_COUNT,
+};
+#define UNIT_IDX_MASK       0x01FFU /*Return only Unit_IDX*/
+#define UNIT_TRIM_ZERO      0x0200U /*Don't show leading zero on max_idx*/
+#define UNIT_LOCK_HR        0x0400U /*Don't Auto Range below this field*/
+#define UNIT_LOCK_MIN       0x0800U /*Don't Auto Range below this field*/
+#define UNIT_LOCK_SEC       0x1000U /*Don't Auto Range below this field*/
+
+/*  time_split_units()
+    split time values depending on base unit
+    unit_idx: UNIT_HOUR, UNIT_MIN, UNIT_SEC, UNIT_MS
+    abs_value: absolute time value
+    units_in: array of unsigned ints with IDX_TIME_COUNT fields
+*/
+unsigned int time_split_units(int unit_idx, unsigned long abs_val,
+                        unsigned long (*units_in)[UNIT_IDX_TIME_COUNT]);
+
+/* format_time_auto - return an auto ranged time string;
+   buffer:  needs to be at least 25 characters for full range
+
+   unit_idx: specifies lowest or base index of the value
+   add | UNIT_LOCK_ to keep place holder of units that would normally be
+   discarded.. For instance, UNIT_LOCK_HR would keep the hours place, ex: string
+   00:10:10 (0 HRS 10 MINS 10 SECONDS) normally it would return as 10:10
+   add | UNIT_TRIM_ZERO to supress leading zero on the largest unit
+
+   value: should be passed in the same form as unit_idx
+
+   supress_unit: may be set to true and in this case the
+   hr, min, sec, ms identifiers will be left off the resulting string but
+   since right to left languages are handled it is advisable to leave units
+   as an indication of the text direction
+*/
+const char *format_time_auto(char *buffer, int buf_len, long value,
+                                  int unit_idx, bool supress_unit);
 
 /* Format time into buf.
  *

apps/playback.c
@@ -3769,20 +3769,12 @@ void audio_set_cuesheet(bool enable)
 }
 
 #ifdef HAVE_DISK_STORAGE
-/* Set the audio antiskip buffer margin by index */
-void audio_set_buffer_margin(int setting)
+/* Set the audio antiskip buffer margin in SECONDS */
+void audio_set_buffer_margin(int seconds)
 {
-    static const unsigned short lookup[] =
-        { 5, 15, 30, 60, 120, 180, 300, 600 };
-
-    if ((unsigned)setting >= ARRAYLEN(lookup))
-        setting = 0;
-
-    logf("buffer margin: %u", (unsigned)lookup[setting]);
-
-    LOGFQUEUE("audio > audio Q_AUDIO_UPDATE_WATERMARK: %u",
-              (unsigned)lookup[setting]);
-    audio_queue_post(Q_AUDIO_UPDATE_WATERMARK, lookup[setting]);
+    logf("buffer margin: %u", (unsigned) seconds);
+    LOGFQUEUE("audio > audio Q_AUDIO_UPDATE_WATERMARK: %u",(unsigned) seconds);
+    audio_queue_post(Q_AUDIO_UPDATE_WATERMARK, (unsigned) seconds); /*SECONDS*/
 }
 #endif /* HAVE_DISK_STORAGE */
 

apps/recorder/peakmeter.c
@@ -97,10 +97,10 @@ static unsigned short pm_db_min = 0;      /* minimum of range in 1/100 dB */
 static unsigned short pm_db_max = 9000;   /* maximum of range in 1/100 dB */
 static unsigned short pm_db_range = 9000; /* range width in 1/100 dB */
 /* Timing behaviour */
-static int pm_peak_hold = 1;          /* peak hold timeout index */
-static int pm_peak_release = 8;       /* peak release in units per read */
-static int pm_clip_hold = 16;         /* clip hold timeout index */
-static bool pm_clip_eternal = false;  /* true if clip timeout is disabled */
+static int pm_peak_hold     = HZ / 5;  /* peak hold timeout ticks */
+static int pm_peak_release  = 8;       /* peak release in units per read */
+static int pm_clip_hold     = HZ * 60; /* clip hold timeout ticks */
+static bool pm_clip_eternal = false;   /* true if clip timeout is disabled */
 
 #ifdef HAVE_RECORDING
 static unsigned short trig_strt_threshold;
@@ -171,22 +171,6 @@ static int history_pos = 0;
 
 static void peak_meter_draw(struct screen *display, struct meter_scales *meter_scales,
                             int x, int y, int width, int height);
-
-/* time out values for max */
-static const short peak_time_out[] = {
-    0 * HZ, HZ / 5, 30, HZ / 2, HZ, 2 * HZ, 
-    3 * HZ, 4 * HZ, 5 * HZ, 6 * HZ, 7 * HZ, 8 * HZ,
-    9 * HZ, 10 * HZ, 15 * HZ, 20 * HZ, 30 * HZ, 60 * HZ
-};
-
-/* time out values for clip */
-static const long clip_time_out[] = {
-    0 * HZ, 1  * HZ, 2 * HZ, 3 * HZ, 4 * HZ, 5 * HZ, 
-    6 * HZ, 7 * HZ, 8 * HZ, 9 * HZ, 10 * HZ, 15 * HZ,
-    20 * HZ, 25 * HZ, 30 * HZ, 45 * HZ, 60 * HZ, 90 * HZ,
-    120 * HZ, 180 * HZ, 300 * HZ, 600L * HZ, 1200L * HZ,
-    2700L * HZ, 5400L * HZ
-};
 
 /* precalculated peak values that represent magical
    dBfs values. Used to draw the scale */
@@ -522,18 +506,16 @@ void peak_meter_init_range( bool dbfs, int range_min, int range_max)
  * Initialize the peak meter with all relevant values concerning times.
  * @param int release - Set the maximum amount of pixels the meter is allowed
  *                      to decrease with each redraw
- * @param int hold - Select the time preset for the time the peak indicator 
- *                   is reset after a peak occurred. The preset values are 
- *                   stored in peak_time_out.
- * @param int clip_hold - Select the time preset for the time the peak 
- *                        indicator is reset after a peak occurred. The preset 
- *                        values are stored in clip_time_out.
+ * @param int hold_ms - Select the time in ms for the time the peak indicator
+ *                      is reset after a peak occurred.
+ * @param int clip_hold_sec - Select the time in seconds for the time the peak
+ *                            indicator is reset after a peak occurred.
  */
-void peak_meter_init_times(int release, int hold, int clip_hold) 
+void peak_meter_init_times(int release, int hold_ms, int clip_hold_sec)
 {
-    pm_peak_hold = hold;
+    pm_peak_hold = hold_ms/(1000UL/HZ); /* convert ms to ticks */
     pm_peak_release = release;
-    pm_clip_hold = clip_hold;
+    pm_clip_hold = HZ * clip_hold_sec;
 }
 
 #ifdef HAVE_RECORDING
@@ -657,8 +639,7 @@ void peak_meter_peek(void)
         (left == MAX_PEAK - 1)) {
 #endif
         pm_clip_left = true;
-        pm_clip_timeout_l = 
-            current_tick + clip_time_out[pm_clip_hold];
+        pm_clip_timeout_l = current_tick + pm_clip_hold;
     }
 
 #if CONFIG_CODEC == SWCODEC
@@ -668,8 +649,7 @@ void peak_meter_peek(void)
         (right == MAX_PEAK - 1)) {
 #endif
         pm_clip_right = true;
-        pm_clip_timeout_r = 
-            current_tick + clip_time_out[pm_clip_hold];
+        pm_clip_timeout_r = current_tick + pm_clip_hold;
     }
 
 #ifdef HAVE_RECORDING
@@ -1099,14 +1079,12 @@ static void peak_meter_draw(struct screen *display, struct meter_scales *scales,
         /* check for new max values */
         if (left > scales->pm_peak_left) {
             scales->pm_peak_left = left - 1;
-            scales->pm_peak_timeout_l =
-                current_tick + peak_time_out[pm_peak_hold];
+            scales->pm_peak_timeout_l = current_tick + pm_peak_hold;
         }
 
         if (right > scales->pm_peak_right) {
             scales->pm_peak_right = right - 1;
-            scales->pm_peak_timeout_r = 
-                current_tick + peak_time_out[pm_peak_hold];
+            scales->pm_peak_timeout_r = current_tick + pm_peak_hold;
         }
     }
 

commit 3e504c3dcd16ec93fb70b10c4f5a361633d7a77b

Fix reversal of "always/never" in "LCD setting > Sleep (After Backlight Off)"
The bug was introduced in a06d9c8.
The patch also fixes some (more or less) related obsolete comments.

Change-Id: I5e491d64574c37cdbc46b146d7cfc555d86b9a11

apps/settings.h
@@ -668,9 +668,8 @@ struct user_settings
 #endif
     unsigned char kbd_file[MAX_FILENAME+1];  /* last keyboard */
 #endif /* HAVE_LCD_BITMAP */
-    int  backlight_timeout;  /* backlight off timeout:  0-18 0=never,
-                               1=always,
-                               then according to timeout_values[] */
+    int  backlight_timeout;  /* backlight off timeout:  -1=never,
+                                0=always, or time in seconds */
     bool caption_backlight; /* turn on backlight at end and start of track */
     bool bl_filter_first_keypress;   /* filter first keypress when dark? */
 #if CONFIG_CHARGING
@@ -689,7 +688,8 @@ struct user_settings
 #endif
 #ifdef HAVE_LCD_SLEEP_SETTING
     int lcd_sleep_after_backlight_off; /* when to put lcd to sleep after backlight
-                                          has turned off */
+                                          has turned off:  -1=never, 0=always,
+                                          or time in seconds */
 #endif
 #endif /* HAVE_BACKLIGHT */
 
@@ -707,8 +707,8 @@ struct user_settings
 #ifdef HAVE_REMOTE_LCD
     /* remote lcd */
     int  remote_contrast;          /* lcd contrast:       0-63 0=low 63=high */
-    int  remote_backlight_timeout; /* backlight off timeout:  0-18 0=never,
-                                   1=always, then according to timeout_values[] */
+    int  remote_backlight_timeout; /* backlight off timeout:  -1=never,
+                                      0=always, or time in seconds */
     int  remote_backlight_timeout_plugged;
     int  remote_scroll_speed;      /* long texts scrolling speed: 1-30 */
     int  remote_scroll_delay;      /* delay (in 1/10s) before starting scroll */

firmware/backlight.c
@@ -209,15 +209,18 @@ static void backlight_lcd_sleep_countdown(bool start)
     }
 
     /* Start LCD sleep countdown */
-    if (lcd_sleep_timeout < 0)
+    if (lcd_sleep_timeout <= 0)
     {
-        lcd_sleep_timer = 0; /* Setting == Always */
-        /* Ensure lcd_sleep() is called from backlight_thread() */
+        lcd_sleep_timer = 0;
+        if (lcd_sleep_timeout == 0) /* Setting == Always */
+        {
+            /* Ensure lcd_sleep() is called from backlight_thread() */
 #if (CONFIG_BACKLIGHT_FADING == BACKLIGHT_FADING_PWM)
-        queue_post(&backlight_queue, LCD_SLEEP, 0);
+            queue_post(&backlight_queue, LCD_SLEEP, 0);
 #else
-        lcd_sleep();
+            lcd_sleep();
 #endif
+        }
     }
     else
     {