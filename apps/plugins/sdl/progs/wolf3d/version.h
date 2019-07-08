a7f963382281ad55808fe4160a513afe0bb9c8e5
Franklin Wei <git@fwei.tk>
2019-07-07 22:00:20 -0400

    Wolfenstein 3-D!
    This is a port of Wolf4SDL, which is derived from the original id
    software source release. The port runs on top of the SDL plugin
    runtime and is loaded as an overlay.
    
    Licensing of the game code is not an issue, as discussed below
    (essentially, the Debian project treats Wolf4SDL as GPLv2, with an
    email from John Carmack backing it up):
    
      http://forums.rockbox.org/index.php?topic=52872
    
    Included is a copy of MAME's Yamaha OPL sound chip emulator
    (fmopl_gpl.c).  This file was not part of the original Wolf4SDL source
    (which includes a non-GPL'd version), but was rather rebased from me
    from a later MAME source which had relicensed to GPLv2.
    
    Change-Id: I64c2ba035e0be7e2f49252f40640641416613439

---
+++ apps/plugins/sdl/progs/wolf3d/version.h
@@ -0,0 +1,55 @@
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#ifndef VERSIONALREADYCHOSEN              // used for batch compiling
+
+#ifndef DATADIR
+#define DATADIR "/.rockbox/wolf3d/"
+#endif
+
+/* Defines used for different versions */
+
+//#define SPEAR
+//#define SPEARDEMO
+//#define UPLOAD
+#define GOODTIMES
+#define CARMACIZED
+//#define APOGEE_1_0
+//#define APOGEE_1_1
+//#define APOGEE_1_2
+
+/*
+    Wolf3d Full v1.1 Apogee (with ReadThis)   - define CARMACIZED and APOGEE_1_1
+    Wolf3d Full v1.4 Apogee (with ReadThis)   - define CARMACIZED
+    Wolf3d Full v1.4 GT/ID/Activision         - define CARMACIZED and GOODTIMES
+    Wolf3d Shareware v1.0                     - define UPLOAD and APOGEE_1_0
+    Wolf3d Shareware v1.1                     - define CARMACIZED and UPLOAD and APOGEE_1_1
+    Wolf3d Shareware v1.2                     - define CARMACIZED and UPLOAD and APOGEE_1_2
+    Wolf3d Shareware v1.4                     - define CARMACIZED and UPLOAD
+    Spear of Destiny Full and Mission Disks   - define CARMACIZED and SPEAR
+                                                (and GOODTIMES for no FormGen quiz)
+    Spear of Destiny Demo                     - define CARMACIZED and SPEAR and SPEARDEMO
+*/
+
+#endif
+
+//#define USE_FEATUREFLAGS    // Enables the level feature flags (see bottom of wl_def.h)
+//#define USE_SHADING         // Enables shading support (see wl_shade.cpp)
+//#define USE_DIR3DSPR        // Enables directional 3d sprites (see wl_dir3dspr.cpp)
+//#define USE_FLOORCEILINGTEX // Enables floor and ceiling textures stored in the third mapplane (see wl_floorceiling.cpp)
+//#define USE_HIRES           // Enables high resolution textures/sprites (128x128)
+//#define USE_PARALLAX 16     // Enables parallax sky with 16 textures per sky (see wl_parallax.cpp)
+//#define USE_CLOUDSKY        // Enables cloud sky support (see wl_cloudsky.cpp)
+//#define USE_STARSKY         // Enables star sky support (see wl_atmos.cpp)
+//#define USE_RAIN            // Enables rain support (see wl_atmos.cpp)
+//#define USE_SNOW            // Enables snow support (see wl_atmos.cpp)
+//#define FIXRAINSNOWLEAKS    // Enables leaking ceilings fix (by Adam Biser, only needed if maps with rain/snow and ceilings exist)
+
+#define DEBUGKEYS             // Comment this out to compile without the Tab debug keys
+#define ARTSEXTERN
+#define DEMOSEXTERN
+#define PLAYDEMOLIKEORIGINAL  // When playing or recording demos, several bug fixes do not take
+                              // effect to let the original demos work as in the original Wolf3D v1.4
+                              // (actually better, as the second demo rarely worked)
+
+#endif
