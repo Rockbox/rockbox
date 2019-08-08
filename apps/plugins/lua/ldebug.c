Rockbox/rockbox

commit 8d77ec88a5f7c839f149b0bd5d159b762daa0cfb

lua LCD fix
forgot to set new size after realloc

forgot to set sizelineinfo to 0 after free

Change-Id: Iaf7533aafaba1e2abf9599c9e1d0b75d913a8c36

apps/plugins/lua/ldebug.c
@@ -253,6 +253,7 @@ static int stripdebug (lua_State *L, Proto *f, const int level) {
     case 3:
       len += f->sizelineinfo;
       f->packedlineinfo = luaM_freearray(L, f->packedlineinfo, f->sizelineinfo, unsigned char);
+      f->f->sizelineinfo = 0;
     case 2:
       len += f->sizelocvars * (sizeof(struct LocVar) + sizeof(dummy->tsv) + sizeof(struct LocVar *));
       f->locvars = luaM_freearray(L, f->locvars, f->sizelocvars, struct LocVar);

apps/plugins/lua/lparser.c
@@ -370,6 +370,7 @@ static void close_func (LexState *ls) {
   f->packedlineinfo[fs->lastlineOffset+1]=0;
   luaM_reallocvector(L, f->packedlineinfo, f->sizelineinfo,
                      fs->lastlineOffset+2, unsigned char);
+  f->sizelineinfo = fs->lastlineOffset + 2;
 #else
   luaM_reallocvector(L, f->lineinfo, f->sizelineinfo, fs->pc, int);
   f->sizelineinfo = fs->pc;

commit 0b938e3f9c4d06ac2541a7765b2f05d00ba86a49

Fix lua compile error on most targets, introduced in 8d77ec8
Change-Id: I217f78a9e348a18552563c6c3b235c9aed956f54

apps/plugins/lua/ldebug.c
@@ -253,7 +253,7 @@ static int stripdebug (lua_State *L, Proto *f, const int level) {
     case 3:
       len += f->sizelineinfo;
       f->packedlineinfo = luaM_freearray(L, f->packedlineinfo, f->sizelineinfo, unsigned char);
-      f->f->sizelineinfo = 0;
+      f->sizelineinfo = 0;
     case 2:
       len += f->sizelocvars * (sizeof(struct LocVar) + sizeof(dummy->tsv) + sizeof(struct LocVar *));
       f->locvars = luaM_freearray(L, f->locvars, f->sizelocvars, struct LocVar);