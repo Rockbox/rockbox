3d7849798ee825cc7da33aabd0cabef6351198a7
Solomon Peachy <pizza@shaftnet.org>
2019-08-20 07:21:09 -0400

    FS#9356 -- Allow delay before button repeat/longpress to be configurable
    Original patch by Alexander Levin
    Updated by Igor Poretsky
    Further updated by Solomon Peachy
    
    Change-Id: I839d2f8e34ee82486f1645c8d11f5a26f754b7eb

--- firmware/export/button.h
+++ firmware/export/button.h
@@ -84,6 +84,12 @@ void wheel_send_events(bool send);
 int button_apply_acceleration(const unsigned int data);
 #endif
 
+void set_button_long_press_duration(int ticks);
+
+/* Default button long press duration */
+#define BUTTON_LONGPRESS_TICKS  30
+
+
 #define BUTTON_NONE         0x00000000
 
 /* Button modifiers */
