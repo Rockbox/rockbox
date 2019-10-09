4ddd1ee98af7576ec78f085251c5fd8c723b4b51
William Wilgus <me.theuser@yahoo.com>
2019-10-09 17:22:48 -0500

    Fix timer Agptek Rocker (other hosted players)
    on timer register the old timer is not deleted
    on timer_unregister callbacks are not removed
    
    Change-Id: Ia155c3a4e4af89f474d55ed845560ccc1fab85aa

--- firmware/target/hosted/kernel-unix.c
+++ firmware/target/hosted/kernel-unix.c
@@ -105,9 +105,9 @@ void tick_start(unsigned int interval_in_ms)
 #define cycles_to_microseconds(cycles) \
     ((int)((1000000*cycles)/TIMER_FREQ))
 
-
 static timer_t timer_tid;
 static int timer_prio = -1;
+void timer_unregister(void);
 void (*global_unreg_callback)(void);
 void (*global_timer_callback)(void);
 
@@ -129,8 +129,12 @@ bool timer_register(int reg_prio, void (*unregister_callback)(void),
     if (reg_prio <= timer_prio || in_us <= 0)
         return false;
 
-    if (timer_prio >= 0 && global_unreg_callback)
-        global_unreg_callback();
+    if(timer_prio >= 0)
+    {
+        if (global_unreg_callback)
+            global_unreg_callback();
+        timer_unregister();
+    }
 
     memset(&sigev, 0, sizeof(sigevent_t));
     sigev.sigev_notify = SIGEV_THREAD,
@@ -166,4 +170,6 @@ void timer_unregister(void)
 {
     timer_delete(timer_tid);
     timer_prio = -1;
+    global_unreg_callback = NULL;
+    global_timer_callback = NULL;
 }
