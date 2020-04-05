f469c26820a8d7e8b08b666a9f943cf92cac25f3
Solomon Peachy <pizza@shaftnet.org>
2020-04-04 20:24:33 -0400

    SDL:  Silence a large number of compile warnings   (WIP)
    There are some real bugs in here, but we're drowning in warnings.
    
    Change-Id: I7c2c0eafc8426327521bdd8a3ac2d3742ac16864

--- apps/plugins/sdl/progs/wolf3d/id_us.h
+++ apps/plugins/sdl/progs/wolf3d/id_us.h
@@ -96,6 +96,6 @@ void	        USL_PrintInCenter(const char *s,Rect r);
 char 	        *USL_GiveSaveName(word game);
 
 void            US_InitRndT(int randomize);
-int             US_RndT();
+int             US_RndT(void);
 
 #endif
