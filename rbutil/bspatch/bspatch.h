83b8822e113b14b5229461c99798f47569af9fa2
Solomon Peachy <pizza@shaftnet.org>
2020-04-15 17:15:57 -0400

    bspatch:  Add a __cplusplus wrapper in the header so rbutil can use it
    Change-Id: Iba4e173ec73bfb38144ebb83edf3ed8161d03ebd

--- rbutil/bspatch/bspatch.h
+++ rbutil/bspatch/bspatch.h
@@ -5,6 +5,15 @@
 #ifndef _BSPATCH_H
 #define _BSPATCH_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int apply_bspatch(const char *infile, const char *outfile, const char *patchfile);
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif /* _BSPATCH_H */
