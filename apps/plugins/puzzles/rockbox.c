729d6b0cb782a5360b0dd043579bef1e195f2024
Franklin Wei <franklin@rockbox.org>
2020-06-25 16:45:54 -0400

    puzzles: update copyright date
    Change-Id: I57c0554cb0d2951631d28e249eacb34c4b36ef21

--- apps/plugins/puzzles/rockbox.c
+++ apps/plugins/puzzles/rockbox.c
@@ -7,7 +7,7 @@
  *                     \/            \/     \/    \/            \/
  * $Id$
  *
- * Copyright (C) 2018 Franklin Wei
+ * Copyright (C) 2016-2020 Franklin Wei
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
