Rockbox/rockbox

commit a2fbccf19423be4e33faac106c365168af4861a2

playback:  Nothing should call ata_spinup_time() directly
Change-Id: I9d20b8bfd0f1e47f33ca402f80a9e08bd00fbcd8

apps/playback.c
@@ -1006,7 +1006,7 @@ static void audio_update_filebuf_watermark(int seconds)
     size_t bytes = 0;
 
 #ifdef HAVE_DISK_STORAGE
-    int spinup = ata_spinup_time();
+    int spinup = storage_spinup_time();
 
     if (seconds == 0)
     {

commit d14b5f8fc44590187ea655ec73a11467dcf35e99

Fix build errors introduced in a2fbccf
Change-Id: I413989858432cd206e09d6d71dec07b4f7e06836

apps/playback.c
@@ -26,7 +26,6 @@
 #include "panic.h"
 #include "core_alloc.h"
 #include "sound.h"
-#include "ata.h"
 #include "codecs.h"
 #include "codec_thread.h"
 #include "voice_thread.h"
@@ -39,6 +38,7 @@
 #include "pcmbuf.h"
 #include "audio_thread.h"
 #include "playback.h"
+#include "storage.h"
 #include "misc.h"
 #include "settings.h"
 