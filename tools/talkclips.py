dcdf2713f610cb3cfaa615ab9b8d4b9412e7a8b6
Solomon Peachy <pizza@shaftnet.org>
2020-07-09 10:36:06 -0400

    Python3-related fixes in some of our scripts
    Change-Id: I8acbe43e3f27e51159a7a78fcc361476d7406cbb

--- tools/talkclips.py
+++ tools/talkclips.py
@@ -23,40 +23,40 @@ import sys
 # The following lines provide variables which you can modify to adjust
 # settings... See comments next to each line.
 # Start opts:
-espeak = '/usr/bin/espeak' # location of easpeak binary 
+espeak = '/usr/bin/espeak' # location of easpeak binary
 rbspeexenc = './rbspeexenc' # path to rbspeexenc binary (default currentdir)
-VOPTS=espeak+" -s 320 -z" # Your espeak opts 
-ROPTS=rbspeexenc+" -q 4 -c 10" # rbspeex opts 
+VOPTS=espeak+" -s 320 -z" # Your espeak opts
+ROPTS=rbspeexenc+" -q 4 -c 10" # rbspeex opts
 logfile="/tmp/talkclips.log" # a file where output should be logged
-# End opts 
-# Don't touch the below settings. Unless you know what your doing. 
-log=open(logfile, 'w') # logging leave this var alone. 
-USAGE="Usage: %s <directory>" % (sys.argv[0]) # usage prompt don't touch 
+# End opts
+# Don't touch the below settings. Unless you know what your doing.
+log=open(logfile, 'w') # logging leave this var alone.
+USAGE="Usage: %s <directory>" % (sys.argv[0]) # usage prompt don't touch
 if not os.path.exists(rbspeexenc):
-  print "%s not found, please change your rbspeexenc path appropriately,\n"\
+  print ("%s not found, please change your rbspeexenc path appropriately,\n"\
       "or place the binary in %s\n"\
-      % (rbspeexenc, os.path.realpath(rbspeexenc))
-  print USAGE
-  exit (-1) # Rbspeexenc not found 
+      % (rbspeexenc, os.path.realpath(rbspeexenc)))
+  print (USAGE)
+  exit (-1) # Rbspeexenc not found
 if not os.path.exists(espeak):
-  print "Espeak not found, please install espeak, or adjust the path of\n"\
-      'the "espeak" variable appropriately.\n'
-  print USAGE
-  exit (-1) # espeak not found 
+  print ("Espeak not found, please install espeak, or adjust the path of\n"\
+      'the "espeak" variable appropriately.\n')
+  print (USAGE)
+  exit (-1) # espeak not found
 
 if len(sys.argv) != 2:
-  print USAGE
+  print (USAGE)
   exit (-1) # user failed to supply enough arguments
 
 RBDIR=sys.argv[1] # grab user input on the command line (don't touch)
 if not os.path.exists(sys.argv[1]):
-  print "The path %s doesn't exist, please try again.\n\n%s"\
-      % (sys.argv[1], USAGE)
+  print ("The path %s doesn't exist, please try again.\n\n%s"\
+      % (sys.argv[1], USAGE))
   exit(-1) # path doesn't exist
 else: # check if it's a dir
   if not os.path.isdir(sys.argv[1]): # a file
-    print "This script only currently works for directories.\n\n%s" % (USAGE)
-    exit (-1) # not a dir 
+    print ("This script only currently works for directories.\n\n%s" % (USAGE))
+    exit (-1) # not a dir
 
 def gentalkclip(clipname, fullpath, isdir):
   """Generate an individual talk clip.
@@ -65,23 +65,23 @@ def gentalkclip(clipname, fullpath, isdir):
   synth, and encoder, and save accordingly."""
 
   if isdir: # directory
-    output=os.path.join(fullpath, "_dirname.talk") # dir clip name 
+    output=os.path.join(fullpath, "_dirname.talk") # dir clip name
     if os.path.exists(output):
       return True # no need to create again
     try: # Don't let the script stop if bash raises filename errors
       os.system('%s "%s" -w "%s"' % (VOPTS, clipname, output+".tmp"))
       os.system('%s "%s" "%s"' % (ROPTS, output+".tmp", output))
-      os.remove(output+".tmp") # delete the old wav file 
+      os.remove(output+".tmp") # delete the old wav file
     except OSError:
-      log.write('Failed to create clip for directory: "%s"\n' % (clipname)) 
-      return False 
+      log.write('Failed to create clip for directory: "%s"\n' % (clipname))
+      return False
     log.write( 'Created clip for directory: "%s"\n' % (clipname)) # log
-    return True 
+    return True
   else: # file
     output=fullpath+".talk"
     if os.path.exists(output):
       return True # no need to create again
-    try: 
+    try:
       os.system('%s "%s" -w "%s"' % (VOPTS, clipname, output+".tmp"))
       os.system('%s "%s" "%s"' % (ROPTS, output+".tmp", output))
       os.remove (output+".tmp")
@@ -89,7 +89,7 @@ def gentalkclip(clipname, fullpath, isdir):
       log.write('Failed to create clip for file: "%s"\n' % (clipname))
       return False # something failed, so continue with next file
     log.write('Created clip for file: "%s"\n' % (clipname)) # logging
-  return True # clips created 
+  return True # clips created
 
 def istalkclip(file):
   """Is file a talkclip?
@@ -110,12 +110,12 @@ def walker(directory):
   for item in os.listdir(directory): # get subdirs and files
     if os.path.isdir(os.path.join(directory, item)):
       gentalkclip (item, os.path.join(directory, item), True) # its a dir
-      walker(os.path.join(directory, item)) # go down into this sub dir 
+      walker(os.path.join(directory, item)) # go down into this sub dir
       continue
     else: # a file
       if istalkclip (item):
         continue # is a talk clip
-      else: # create clip 
+      else: # create clip
         gentalkclip(item, os.path.join(directory, item), False) # a file
         continue
 
