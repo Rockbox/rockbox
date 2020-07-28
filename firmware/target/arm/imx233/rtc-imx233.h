b58803b599740017e7f948402c2c0ea81db1b3dc
Moshe Piekarski <dev.rockbox@melachim.net>
2020-07-28 10:48:47 -0400

    imx233: rtc: generalize PERSISTENT0 fix
    Change-Id: Id8452055534df239dcf5e535943ee918a93ec10c

--- firmware/target/arm/imx233/rtc-imx233.h
+++ firmware/target/arm/imx233/rtc-imx233.h
@@ -84,7 +84,10 @@ static inline void imx233_rtc_init(void)
     BF_CLR(RTC_CTRL, SFTRST);
     udelay(5);  /* only need 3 GPMI clocks (1us) */
     BF_CLR(RTC_CTRL, CLKGATE);
-    HW_RTC_PERSISTENT0_SET=0x082114;
+#ifdef BM_RTC_PERSISTENT0_DISABLE_XTALOK
+    BF_SET(RTC_PERSISTENT0, XTAL24MHZ_PWRUP, DISABLE_XTALOK);
+#endif
+    BF_CLR(RTC_PERSISTENT0, CLOCKSOURCE);
     imx233_rtc_enable_watchdog(false);
 }
 
