e6def215a78ad5579f9eb5bf01060d0904fc4629
William Wilgus <wilgus.william@gmail.com>
2020-07-31 22:45:10 -0400

    Add open_plugin to core
    open_plugin allows arbitrary plugins to be called
    in hotkey and start screen
    
    replaces PictureFlow Integration
    shortcuts menu plays plugins now too
    
    rather than store paths and parameters in the settings
    that reside in memory instead entries in a file are searched by hash.
    after all, the plugin has to be loaded from disk anyways
    
    ---------------------------------------------------------------------
    Now including the open_plugins viewer plugin
    this will allow you to use Open With.. to
    call plugins with parameters
    and acts as a shortcut list for plugins
    
      open_plugins.rock interfaces with the open_plugin core
    
      When opened directly it acts as a viewer for the plugin.dat file
      this allows you to edit the paths and parameters for
      core shortcuts as well as your added plugins
    
      If a plugin is supplied to the viewer it is added to the dat file
    
      If instead the plugin has previously been added then it is run
      with the parameters you previously supplied
    
    -----------------------------------------------------------------------------
    Added export to .opx files-- this allows shortcuts to plugins with parameters
    to be called from the file browser
    
    shortcut_viewer.rock--  can now call plugins rather than taking you to them
    in the browser
    
    -----------------------------------------------------------------------------
    Added a new option to menus:
     F_CB_ON_SELECT_ONLY
     instead of option callback every time a item is accessed
     F_CB_ON_SELECT_ONLY fires callback only when item is selected
    
    Added manual entries
    
    Change-Id: I078b57b1d2b4dd633c89212c1082fcbc1b516e6a

---
+++ apps/open_plugin.h
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2020 by William Wilgus
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#ifndef OPEN_PLUGIN_H
+#define OPEN_PLUGIN_H
+
+/* Open_plugin module
+ * OP stores and retrieves plugin path and parameters by key
+ * from a dictionary file
+ *
+ * plugins can load other plugins
+ * return rb->plugin_open(path, parameter);
+ */
+
+#ifndef __PCTOOL__
+/* open_plugin path lookup */
+#define OPEN_PLUGIN_DAT PLUGIN_DIR "/plugin.dat"
+#define OPEN_PLUGIN_BUFSZ MAX_PATH
+#define OPEN_PLUGIN_NAMESZ 32
+struct open_plugin_entry_t
+{
+    uint32_t hash;
+    int32_t  lang_id;
+    char name[OPEN_PLUGIN_NAMESZ+1];
+    /*char key[OPEN_PLUGIN_BUFSZ+1];*/
+    char path[OPEN_PLUGIN_BUFSZ+1];
+    char param[OPEN_PLUGIN_BUFSZ+1];
+};
+
+inline static void open_plugin_get_hash(const char *key, uint32_t *hash)
+{
+    /* Calculate modified FNV1a hash of string */
+    const uint32_t p = 16777619;
+    *hash = 0x811C9DC5; //seed, 2166136261;
+    while(*key)
+        *hash = (*key++ ^ *hash) * p;
+}
+
+#ifndef PLUGIN
+extern struct open_plugin_entry_t open_plugin_entry;
+uint32_t open_plugin_add_path(const char *key, const char *plugin, const char *parameter);
+int open_plugin_get_entry(const char *key, struct open_plugin_entry_t *entry);
+void open_plugin_browse(const char *key);
+void open_plugin_remove(const char *key);
+int open_plugin_run(const char *key);
+#endif
+
+#endif /*ndef __PCTOOL__ */
+#endif /* OPEN_PLUGIN_H */
