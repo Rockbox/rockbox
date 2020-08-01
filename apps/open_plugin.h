9b0dfd5d475d7bd89a7459afd65b773561431328
William Wilgus <wilgus.william@gmail.com>
2020-07-31 22:45:10 -0400

    Add open_plugin to core
    open_plugin allows arbitrary plugins to be called
    in hotkey and start screen
    
    I plan to expand this into a viewer plugin as well
    that will allow you to  call plugins with parameters
    
    rather than store paths and parameters in the settings
    that reside in memory instead entries in a file are searched by hash.
    after all,  the plugin has to be loaded from disk anyways
    
    Change-Id: I078b57b1d2b4dd633c89212c1082fcbc1b516e6a

---
+++ apps/open_plugin.h
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *             __________               __   ___.                  
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___  
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /  
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <   
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \  
+ *                     \/            \/     \/    \/            \/ 
+ * $Id$
+ *
+ * Copyright (C) 2020 by William Wilgus
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#ifndef OPEN_PLUGIN_H
+#define OPEN_PLUGIN_H
+
+//#include <stdbool.h>
+//#include <inttypes.h>
+//#include "config.h"
+//#include "screen_access.h"
+#ifndef __PCTOOL__
+#define OPEN_PLUGIN_BUFSZ MAX_PATH
+#define OPEN_PLUGIN_NAMESZ 32
+struct open_plugin_entry_t
+{
+    uint32_t hash;
+    int32_t  lang_id;
+    char name[OPEN_PLUGIN_NAMESZ+1];
+    /*char key[OPEN_PLUGIN_BUFSZ+1];*/
+    char path[OPEN_PLUGIN_BUFSZ+1];
+    char param[OPEN_PLUGIN_BUFSZ+1];
+};
+
+extern struct open_plugin_entry_t open_plugin_entry;
+void open_plugin_add_path(const char *key, const char *plugin, const char *parameter);
+int open_plugin_get_entry(const char *key, struct open_plugin_entry_t *entry);
+void open_plugin_browse(const char *key);
+void open_plugin_remove(const char *key);
+int open_plugin_run(const char *key);
+#endif
+#endif /* OPEN_PLUGIN_H */
