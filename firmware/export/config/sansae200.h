2d8882a3d6838b74eae386c576accafaf8e7448a
Johannes Rauh <johnbthetenth@gmail.com>
2020-08-15 18:04:47 +0200

    Enable boot from SD for Sansa e200 (WIP)
    Initial trial.
    
    Change-Id: I0940f2cd5fc914d6d5061b5798b1a636009649b7

--- firmware/export/config/sansae200.h
+++ firmware/export/config/sansae200.h
@@ -8,6 +8,12 @@
 
 #define HW_SAMPR_CAPS       (SAMPR_CAP_48 | SAMPR_CAP_44 | SAMPR_CAP_32)
 
+/* Define if boot data from bootloader has been enabled for the target */
+#define HAVE_BOOTDATA
+
+/* define boot redirect file name allows booting from external drives */
+#define BOOT_REDIR "rockbox_main.e200"
+
 /* define this if you have recording possibility */
 #define HAVE_RECORDING
 
