a08cee3cafcb552d2e5b8e84caae7a106b5291f3
Johannes Rauh <johnbthetenth@gmail.com>
2020-08-15 18:04:47 +0200

    Enable boot from SD for Sansa e200 (WIP)
    Initial trial.
    
    Change-Id: I0940f2cd5fc914d6d5061b5798b1a636009649b7

--- bootloader/main-pp.c
+++ bootloader/main-pp.c
@@ -131,7 +131,7 @@ int load_mi4_part(unsigned char* buf, struct partinfo* pinfo,
     unsigned long sum;
     
     /* Read header to find out how long the mi4 file is. */
-    storage_read_sectors(pinfo->start + PPMI_SECTOR_OFFSET,
+    storage_read_sectors(IF_MD(0,) pinfo->start + PPMI_SECTOR_OFFSET,
                          PPMI_SECTORS, &ppmi_header);
     
     /* The first four characters at 0x80000 (sector 1024) should be PPMI*/
@@ -141,7 +141,7 @@ int load_mi4_part(unsigned char* buf, struct partinfo* pinfo,
     printf("BL mi4 size: %x", ppmi_header.length);
     
     /* Read mi4 header of the OF */
-    storage_read_sectors(pinfo->start + PPMI_SECTOR_OFFSET + PPMI_SECTORS 
+    storage_read_sectors(IF_MD(0,) pinfo->start + PPMI_SECTOR_OFFSET + PPMI_SECTORS 
                        + (ppmi_header.length/512), MI4_HEADER_SECTORS, &mi4header);
     
     /* We don't support encrypted mi4 files yet */
@@ -164,7 +164,7 @@ int load_mi4_part(unsigned char* buf, struct partinfo* pinfo,
     printf("Binary type: %.4s", mi4header.type);
 
     /* Load firmware */
-    storage_read_sectors(pinfo->start + PPMI_SECTOR_OFFSET + PPMI_SECTORS
+    storage_read_sectors(IF_MD(0,) pinfo->start + PPMI_SECTOR_OFFSET + PPMI_SECTORS
                         + (ppmi_header.length/512) + MI4_HEADER_SECTORS,
                         (mi4header.mi4size-MI4_HEADER_SIZE)/512, buf);
 
@@ -183,9 +183,9 @@ int load_mi4_part(unsigned char* buf, struct partinfo* pinfo,
         
         printf("Disabling database rebuild");
         
-        storage_read_sectors(pinfo->start + 0x3c08, 1, block);
+        storage_read_sectors(IF_MD(0,) pinfo->start + 0x3c08, 1, block);
         block[0xe1] = 0;
-        storage_write_sectors(pinfo->start + 0x3c08, 1, block);
+        storage_write_sectors(IF_MD(0,) pinfo->start + 0x3c08, 1, block);
     }
 #else
     (void) disable_rebuild;
