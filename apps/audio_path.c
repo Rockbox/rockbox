b5d1b7e43503077985cfa80fedd159e8d992d275
William Wilgus <wilgus.william@gmail.com>
2020-09-17 14:53:29 -0400

    Headphone / lineout  pause/resume  #FS13237
    Allow Lineout to behave like headphone port in regards to
    plug/unplug pause/resume
    
    Change-Id: I9cb2c9c40e0bdf3bf7e1e272164acd343f6b3850

--- apps/audio_path.c
+++ apps/audio_path.c
@@ -169,10 +169,20 @@ int audio_get_spdif_sample_rate(void)
 #ifdef HAVE_SPEAKER
 void audio_enable_speaker(int mode)
 {
-#ifdef HAVE_HEADPHONE_DETECTION
+#if defined(HAVE_HEADPHONE_DETECTION) || defined(HAVE_LINEOUT_DETECTION)
     /* if needed, query jack state */
     if(mode == 2)
-        mode = !headphones_inserted();
+    {
+    	mode = 0; /*disable*/
+#ifdef HAVE_HEADPHONE_DETECTION
+        if (headphones_inserted())
+            mode = 1;
+#endif
+#ifdef HAVE_LINEOUT_DETECTION
+        if (lineout_inserted())
+            mode = 2;
+#endif  
+    }
 #endif
     /* treat any nonzero value as enable */
     audiohw_enable_speaker(mode);
