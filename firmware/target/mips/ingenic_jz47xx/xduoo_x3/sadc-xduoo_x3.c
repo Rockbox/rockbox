f22ad8414657508e83554ef51f327fb181f19d1a
William Wilgus <wilgus.william@gmail.com>
2020-09-17 14:53:29 -0400

    Headphone / lineout  pause/resume  #FS13237
    Allow Lineout to behave like headphone port in regards to
    plug/unplug pause/resume
    
    Change-Id: I9cb2c9c40e0bdf3bf7e1e272164acd343f6b3850

--- firmware/target/mips/ingenic_jz47xx/xduoo_x3/sadc-xduoo_x3.c
+++ firmware/target/mips/ingenic_jz47xx/xduoo_x3/sadc-xduoo_x3.c
@@ -68,7 +68,7 @@ bool lineout_inserted(void)
 {
     /* We want to prevent LO being "enabled" if HP is attached
        to avoid potential eardrum damage */
-    return (__gpio_get_pin(PIN_LO_DECT) == 0) && !headphones_inserted();
+    return (__gpio_get_pin(PIN_LO_DECT) == 0 && !headphones_inserted();
 }
 
 void button_init_device(void)
@@ -89,10 +89,12 @@ void button_init_device(void)
     __gpio_as_output(PIN_CHARGE_CON);
 
     __gpio_as_input(PIN_PH_DECT);
+    __gpio_enable_pull(PIN_PH_DECT);
     /*__gpio_disable_pull(PIN_PH_DECT); // Spurious Detections */
 
     __gpio_as_input(PIN_LO_DECT);
-    __gpio_disable_pull(PIN_LO_DECT);
+    __gpio_enable_pull(PIN_LO_DECT);
+    /*__gpio_disable_pull(PIN_LO_DECT); // Spurious Detections */
 }
 
 bool button_hold(void)
