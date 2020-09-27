2e6a0df40e165e54b5e54eb2c0051c304690b641
Dominik Riebeling <Dominik.Riebeling@gmail.com>
2020-09-27 12:38:17 +0200

    rbutil: Enable HiDPI support on recent Qt versions.
    Change-Id: Iafafacbac93304e1dd955dab0d8cba78a9622f6e

--- rbutil/rbutilqt/main.cpp
+++ rbutil/rbutilqt/main.cpp
@@ -38,6 +38,10 @@ Q_IMPORT_PLUGIN(AccessibleFactory)
 
 
 int main( int argc, char ** argv ) {
+#if QT_VERSION >= 0x050600
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
     QApplication app( argc, argv );
     ConsoleAppender* consoleAppender = new ConsoleAppender();
     consoleAppender->setFormat("[%{file}:%{line} %{type}] %{message}\n");
