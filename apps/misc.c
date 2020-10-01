Rockbox/rockbox

commit b030bf5885dc5e018dd38eacbb294f1321f2b400

xduoox3ii/x20:  Proper lineout detection and volume mangling.
hotplugging hp and lineout works, without blowing out eardrums.

Change-Id: I2df5c7a618bb2d1d77d416548d45dff9cfc619db

firmware/target/hosted/xduoo/button-xduoo.c
@@ -166,25 +166,16 @@ int button_read_device(void)
 
 bool headphones_inserted(void)
 {
-    int status = 0;
-    const char * const sysfs_lo_switch = "/sys/class/switch/lineout/state";
-    const char * const sysfs_hs_switch = "/sys/class/switch/headset/state";
-#ifdef XDUOO_X20
-    const char * const sysfs_bal_switch = "/sys/class/switch/balance/state";
-#endif
+    int ps = xduoo_get_outputs();
 
-    sysfs_get_int(sysfs_lo_switch, &status);
-    if (status) return true;
+    return (ps == 2 || ps == 3);
+}
 
-    sysfs_get_int(sysfs_hs_switch, &status);
-    if (status) return true;
+bool lineout_inserted(void)
+{
+    int ps = xduoo_get_outputs();
 
-#ifdef XDUOO_X20
-    sysfs_get_int(sysfs_bal_switch, &status);
-    if (status) return true;
-#endif
-
-    return false;
+    return (ps == 1);
 }
 
 void button_close_device(void)

apps/playback.c
@@ -3851,10 +3851,6 @@ static void audio_change_frequency_callback(unsigned short id, void *data)
     static bool starting_playback = false;
     struct mp3entry *id3;
 
-#ifdef AUDIOHW_HAVE_SET_OUTPUT
-    audiohw_set_output();
-#endif
-
     switch (id)
     {
     case PLAYBACK_EVENT_START_PLAYBACK:

firmware/drivers/audio/xduoolinux_codec.c
@@ -24,6 +24,7 @@
 #include "config.h"
 #include "audio.h"
 #include "audiohw.h"
+#include "button.h"
 #include "system.h"
 #include "kernel.h"
 #include "panic.h"
@@ -32,6 +33,7 @@
 #include "pcm-alsa.h"
 
 static int fd_hw;
+static int inited = 0;
 
 static long int vol_l_hw = 255;
 static long int vol_r_hw = 255;
@@ -72,11 +74,12 @@ void audiohw_mute(int mute)
     }
 }
 
-void audiohw_set_output(void)
-{
+int xduoo_get_outputs(void){
     long int ps = 2; // headset
 
     int status = 0;
+
+    if (!inited) return ps;
 
     const char * const sysfs_lo_switch = "/sys/class/switch/lineout/state";
     const char * const sysfs_hs_switch = "/sys/class/switch/headset/state";
@@ -84,22 +87,32 @@ void audiohw_set_output(void)
     const char * const sysfs_bal_switch = "/sys/class/switch/balance/state";
 #endif
 
-    sysfs_get_int(sysfs_lo_switch, &status);
-    if (status) ps = 1; // lineout
-
     sysfs_get_int(sysfs_hs_switch, &status);
     if (status) ps = 2; // headset
+
+    sysfs_get_int(sysfs_lo_switch, &status);
+    if (status) ps = 1; // lineout
 
 #if defined(XDUOO_X20)
     sysfs_get_int(sysfs_bal_switch, &status);
     if (status) ps = 3; // balance
 #endif
 
+    xduoo_set_output(ps);
+
+    return ps;
+}
+
+void xduoo_set_output(int ps)
+{
+    if (!inited) return;
+
     if (last_ps != ps)
     {
         /* Output port switch */
         last_ps = ps;
         alsa_controls_set_ints("Output Port Switch", 1, &last_ps);
+	audiohw_set_volume(vol_l_hw, vol_r_hw);
     }
 }
 
@@ -107,15 +120,17 @@ void audiohw_preinit(void)
 {
     alsa_controls_init();
     hw_open();
+    inited = 1;
 }
 
 void audiohw_postinit(void)
 {
-    audiohw_set_output();
+    xduoo_set_output(xduoo_get_outputs());
 }
 
 void audiohw_close(void)
 {
+    inited = 0;
     hw_close();
     alsa_controls_close();
 }
@@ -127,16 +142,45 @@ void audiohw_set_frequency(int fsel)
 
 void audiohw_set_volume(int vol_l, int vol_r)
 {
-    vol_l_hw = -vol_l/5;
-    vol_r_hw = -vol_r/5;
- 
-    alsa_controls_set_ints("Left Playback Volume", 1, &vol_l_hw);
-    alsa_controls_set_ints("Right Playback Volume", 1, &vol_r_hw);
+    long l,r;
+
+    vol_l_hw = vol_l;
+    vol_r_hw = vol_r;
+
+    if (lineout_inserted()) {
+        l = 0;
+        r = 0;
+    } else {
+        l = -vol_l/5;
+        r = -vol_r/5;
+    }
+
+    alsa_controls_set_ints("Left Playback Volume", 1, &l);
+    alsa_controls_set_ints("Right Playback Volume", 1, &r);
+}
+
+void audiohw_set_lineout_volume(int vol_l, int vol_r)
+{
+    long l,r;
+
+    (void)vol_l;
+    (void)vol_r;
+
+    if (lineout_inserted()) {
+        l = 0;
+        r = 0;
+    } else {
+        l = -vol_l_hw/5;
+        r = -vol_r_hw/5;
+    }
+
+    alsa_controls_set_ints("Left Playback Volume", 1, &l);
+    alsa_controls_set_ints("Right Playback Volume", 1, &r);
 }
 
 void audiohw_set_filter_roll_off(int value)
 {
-    /* 0 = Sharp; 
+    /* 0 = Sharp;
        1 = Slow;
        2 = Short Sharp
        3 = Short Slow */

firmware/export/audiohw.h
@@ -452,10 +452,6 @@ void audiohw_set_volume(int vol_l, int vol_r);
 void audiohw_set_lineout_volume(int vol_l, int vol_r);
 #endif
 
-#ifdef AUDIOHW_HAVE_SET_OUTPUT
-void audiohw_set_output(void);
-#endif
-
 #ifndef AUDIOHW_HAVE_CLIPPING
 #if defined(AUDIOHW_HAVE_BASS) || defined(AUDIOHW_HAVE_TREBLE) \
     || defined(AUDIOHW_HAVE_EQ)

firmware/export/config/xduoox20.h
@@ -67,6 +67,7 @@
 #define PLUGIN_BUFFER_SIZE 0x100000
 
 #define HAVE_HEADPHONE_DETECTION
+#define HAVE_LINEOUT_DETECTION
 
 /* KeyPad configuration for plugins */
 #define CONFIG_KEYPAD XDUOO_X20_PAD
@@ -81,7 +82,6 @@
 /* We have usb power and can detect usb but it is handled by Linux */
 #define HAVE_USB_POWER
 
-#define AUDIOHW_HAVE_SET_OUTPUT
 #endif
 
 #define CONFIG_BATTERY_MEASURE PERCENTAGE_MEASURE

firmware/export/config/xduoox3.h
@@ -85,9 +85,6 @@
 /* Define this if a programmable hotkey is mapped */
 #define HAVE_HOTKEY
 
-
-
-
 #ifndef BOOTLOADER
 /* define this if you have a real-time clock */
 #define CONFIG_RTC RTC_JZ4760

firmware/export/config/xduoox3ii.h
@@ -67,6 +67,7 @@
 #define PLUGIN_BUFFER_SIZE 0x100000
 
 #define HAVE_HEADPHONE_DETECTION
+#define HAVE_LINEOUT_DETECTION
 
 /* KeyPad configuration for plugins */
 #define CONFIG_KEYPAD XDUOO_X3II_PAD
@@ -81,7 +82,6 @@
 /* We have usb power and can detect usb but it is handled by Linux */
 #define HAVE_USB_POWER
 
-#define AUDIOHW_HAVE_SET_OUTPUT
 #endif
 
 #define CONFIG_BATTERY_MEASURE VOLTAGE_MEASURE

firmware/export/xduoolinux_codec.h
@@ -1,10 +1,11 @@
 #ifndef __XDUOOLINUX_CODEC__
 #define __XDUOOLINUX_CODEC__
 
-#define AUDIOHW_CAPS (FILTER_ROLL_OFF_CAP)
+#define AUDIOHW_CAPS (LINEOUT_CAP | FILTER_ROLL_OFF_CAP)
 AUDIOHW_SETTING(VOLUME, "dB", 0, 1, -127,  0, -30)
 AUDIOHW_SETTING(FILTER_ROLL_OFF, "", 0, 1, 0, 4, 0)
 #endif
 
 void audiohw_mute(int mute);
-void audiohw_set_output(void);
+void xduoo_set_output(int ps);
+int xduoo_get_outputs(void);

commit a96eb1a7f7aff5aea41c14b9edcec34564ab27b6

Fix warnings and errors introduced in b030bf5
Change-Id: I23966b6a79c0d2cbab2e01c649c1b991f460d8ad

apps/misc.c
@@ -534,7 +534,9 @@ static void lo_unplug_change(bool inserted)
 #ifdef HAVE_LINEOUT_POWEROFF
     lineout_set(inserted);
 #else
+#ifdef AUDIOHW_HAVE_LINEOUT
     audiohw_set_lineout_volume(0,0); /*hp vol re-set by this function as well*/
+#endif
     static bool lineout_caused_pause = true;
 
     if (global_settings.unplug_mode)

firmware/drivers/audio/sdl.c
@@ -66,6 +66,13 @@ void audiohw_set_volume(int vol_l, int vol_r)
 }
 #endif /* AUDIOHW_HAVE_MONO_VOLUME */
 
+#ifdef AUDIOHW_HAVE_LINEOUT
+void audiohw_set_lineout_volume(int vol_l, int vol_r)
+{
+    (void)vol_l; (void)vol_r;
+}
+#endif
+
 #if defined(AUDIOHW_HAVE_PRESCALER)
 void audiohw_set_prescaler(int value)
 {

firmware/target/hosted/xduoo/button-xduoo.c
@@ -36,6 +36,7 @@
 #include "kernel.h"
 #include "backlight.h"
 #include "backlight-target.h"
+#include "xduoolinux_codec.h"
 
 static bool soft_hold = false;
 #ifndef BOOTLOADER