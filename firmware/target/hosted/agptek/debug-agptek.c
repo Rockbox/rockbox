0da64537134eb82cb970232aaf03628fd094eb77
Solomon Peachy <pizza@shaftnet.org>
2020-09-30 22:12:35 -0400

    WIP:  hosted pcm-alsa improvements
     * xduoo x3ii/x20:  Better line out support
     * less granular volume settings (too many steps before)
     * Better handling of swiching sample rates
     * Log actual sample rate in debug menu
    
    Most credit goes to Roman Stolyarov
    Additional integration [re]work by myself
    
    Change-Id: I63af3740678cf2ed3170f61534e1029c81826bb6

--- firmware/target/hosted/agptek/debug-agptek.c
+++ firmware/target/hosted/agptek/debug-agptek.c
@@ -1,6 +1,52 @@
-#include <stdbool.h>
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2020 by Solomon Peachy
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
 
-bool debug_hw_info(void)
+#include "config.h"
+#include "font.h"
+#include "lcd.h"
+#include "kernel.h"
+#include "button.h"
+
+#ifndef BOOTLOADER
+
+#include "pcm-alsa.h"
+
+static int line = 0;
+
+bool dbg_hw_info(void)
 {
-    return false;
+    int btn = 0;
+
+    lcd_setfont(FONT_SYSFIXED);
+
+    while(btn ^ BUTTON_POWER) {
+        lcd_clear_display();
+        line = 0;
+
+        lcd_putsf(0, line++, "pcm srate: %d", pcm_alsa_get_rate());
+        btn = button_read_device();
+
+        lcd_update();
+        sleep(HZ/16);
+    }
+    return true;
 }
+
+#endif /* !BOOTLOADER */
