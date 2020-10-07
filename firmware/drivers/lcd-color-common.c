55bdfa320a2022b9043aa803b2d50f91fc4eb63e
William Wilgus <wilgus.william@gmail.com>
2020-10-07 02:01:35 -0400

    LCD core move buf ptr and address look up function viewport struct WIP
    I'm currently running up against the limitations of the lcd_draw functions
    I want these functions to be able to be used on any size buffer not
    just buffers with a stride matching the underlying device
    
    this patch will..(eventually)
    
    allow the framebuffer to be decoupled from the device framebuffer
    allow for some simple blit like transformations
    remove the device framebuffer from the plugin api
    
    Change-Id: I0d04c3834e464eca84a5a715743a297a0cefd0af

--- firmware/drivers/lcd-color-common.c
+++ firmware/drivers/lcd-color-common.c
@@ -40,6 +40,10 @@ fb_data lcd_static_framebuffer[LCD_FBHEIGHT][LCD_FBWIDTH]
     IRAM_LCDFRAMEBUFFER CACHEALIGN_AT_LEAST_ATTR(16);
 fb_data *lcd_framebuffer = &lcd_static_framebuffer[0][0];
 
+static fb_data *lcd_frameaddress_default(int x, int y);
+fb_data *(*lcd_frameaddress)(int x, int y) = &lcd_frameaddress_default;
+
+
 static fb_data* lcd_backdrop = NULL;
 static long lcd_backdrop_offset IDATA_ATTR = 0;
 
@@ -49,13 +53,25 @@ static struct viewport default_vp =
     .y        = 0,
     .width    = LCD_WIDTH,
     .height   = LCD_HEIGHT,
+    .stride   = STRIDE_MAIN(LCD_FBWIDTH, LCD_FBHEIGHT),
     .font     = FONT_SYSFIXED,
     .drawmode = DRMODE_SOLID,
-    .fg_pattern = LCD_DEFAULT_FG,
-    .bg_pattern = LCD_DEFAULT_BG,
+    .data     = &lcd_static_framebuffer[0][0],
+    .frameaddress = &lcd_frameaddress_default,
+    .fg_pattern   = LCD_DEFAULT_FG,
+    .bg_pattern   = LCD_DEFAULT_BG,
 };
 
 static struct viewport* current_vp IDATA_ATTR = &default_vp;
+
+static fb_data *lcd_frameaddress_default(int x, int y)
+{
+#if defined(LCD_STRIDEFORMAT) && LCD_STRIDEFORMAT == VERTICAL_STRIDE
+    return lcd_framebuffer + ((x) * LCD_FBHEIGHT) + (y);
+#else
+    return lcd_framebuffer + ((y) * LCD_FBWIDTH) + (x);
+#endif
+}
 
 /* LCD init */
 void lcd_init(void)
