8ddcdb6fcde561bd0290cf8ec8d537578cc3197d
Solomon Peachy <pizza@shaftnet.org>
2020-10-08 09:47:40 -0400

    New port:  AIGO EROS Q / EROS K
    The Q and K have a slightly different case, but the hardware under the
    shell is completely identical.
    
    These models are rebadged versions:
    
     * Hifiwalker H2   (== Q)
     * AGPTek H3       (== K)
     * Surfans F20     (== K)
    
    Other notes:
    
      * Significant improvements in the shared Hiby-platform launcher/loader
        * SD card can theoretically be hot-swapped now
        * Support external USB mass storage!
      * Some consolidation of Hiby-platform targets
      * Some consolidation of plugin keymaps
    
    Todo/known issues:
    
     * Keymaps need to be gone over properly
     * Convert to HAVE_SCROLLWHEEL?
    
    Change-Id: I5a8a4f22c38a5b69392ca7c0a8ad8c4e07d9523c

--- apps/plugins/snake.c
+++ apps/plugins/snake.c
@@ -24,7 +24,7 @@ Snake!
 
 by Itai Shaked
 
-ok, a little explanation - 
+ok, a little explanation -
 board holds the snake and apple position - 1+ - snake body (the number
 represents the age [1 is the snake's head]).
 -1 is an apple, and 0 is a clear spot.
@@ -273,15 +273,7 @@ dir is the current direction of the snake - 0=up, 1=right, 2=down, 3=left;
 #define SNAKE_DOWN      BUTTON_OPTION
 #define SNAKE_PLAYPAUSE BUTTON_PLAY
 
-#elif CONFIG_KEYPAD == XDUOO_X3II_PAD
-#define SNAKE_QUIT      BUTTON_POWER
-#define SNAKE_LEFT      BUTTON_PREV
-#define SNAKE_RIGHT     BUTTON_NEXT
-#define SNAKE_UP        BUTTON_HOME
-#define SNAKE_DOWN      BUTTON_OPTION
-#define SNAKE_PLAYPAUSE BUTTON_PLAY
-
-#elif CONFIG_KEYPAD == XDUOO_X20_PAD
+#elif CONFIG_KEYPAD == XDUOO_X3II_PAD || CONFIG_KEYPAD == XDUOO_X20_PAD
 #define SNAKE_QUIT      BUTTON_POWER
 #define SNAKE_LEFT      BUTTON_PREV
 #define SNAKE_RIGHT     BUTTON_NEXT
@@ -297,7 +289,7 @@ dir is the current direction of the snake - 0=up, 1=right, 2=down, 3=left;
 #define SNAKE_DOWN      BUTTON_OPTION
 #define SNAKE_PLAYPAUSE BUTTON_PLAY
 
-#elif CONFIG_KEYPAD == IHIFI_770_PAD
+#elif CONFIG_KEYPAD == IHIFI_770_PAD || CONFIG_KEYPAD == IHIFI_800_PAD
 #define SNAKE_QUIT      BUTTON_POWER
 #define SNAKE_LEFT      BUTTON_HOME
 #define SNAKE_RIGHT     BUTTON_VOL_DOWN
@@ -305,10 +297,10 @@ dir is the current direction of the snake - 0=up, 1=right, 2=down, 3=left;
 #define SNAKE_DOWN      BUTTON_NEXT
 #define SNAKE_PLAYPAUSE BUTTON_PLAY
 
-#elif CONFIG_KEYPAD == IHIFI_800_PAD
+#elif CONFIG_KEYPAD == EROSQ_PAD
 #define SNAKE_QUIT      BUTTON_POWER
-#define SNAKE_LEFT      BUTTON_HOME
-#define SNAKE_RIGHT     BUTTON_VOL_DOWN
+#define SNAKE_LEFT      BUTTON_SCROLL_BACK
+#define SNAKE_RIGHT     BUTTON_SCROLL_FWD
 #define SNAKE_UP        BUTTON_PREV
 #define SNAKE_DOWN      BUTTON_NEXT
 #define SNAKE_PLAYPAUSE BUTTON_PLAY
