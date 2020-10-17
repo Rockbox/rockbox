e21852203e625b1680e34eac091cf96c99884b46
Solomon Peachy <pizza@shaftnet.org>
2020-10-17 18:32:43 -0400

    hosted:  Consolidate common config of hiby-based units into a common header
     * Enabled MULTIDRIVE and ROOTFS_HOTPLUG for full hotswap!
       (Requires updated bootloader/integration!)
    
    Change-Id: I7dad1f8d375e087c36fce37bec3e9184602ede50

--- firmware/export/config/xduoox20.h
+++ firmware/export/config/xduoox20.h
@@ -13,64 +13,8 @@
 /* sqrt(240^2 + 320^2) / 2.4 = 166 */
 #define LCD_DPI 166
 
-#ifndef SIMULATOR
-#define CONFIG_PLATFORM (PLATFORM_HOSTED)
-#define PIVOT_ROOT "/mnt/sd_0"
-#endif
-
-#define HIBY_LINUX
-
-#define HAVE_FPU
-
-/* define this if you have a colour LCD */
-#define HAVE_LCD_COLOR
-
-#define HAVE_LCD_ENABLE
-
-/* Define this if the LCD can shut down */
-#define HAVE_LCD_SHUTDOWN
-
-/* define this if you want album art for this target */
-#define HAVE_ALBUMART
-
-/* define this to enable bitmap scaling */
-#define HAVE_BMP_SCALING
-
-/* define this to enable JPEG decoding */
-#define HAVE_JPEG
-
 /* define this if you have access to the quickscreen */
 #define HAVE_QUICKSCREEN
-
-/* define this if you would like tagcache to build on this target */
-#define HAVE_TAGCACHE
-
-#define LCD_DEPTH  32
-/* Check that but should not matter */
-#define LCD_PIXELFORMAT XRGB8888
-
-#define HAVE_BACKLIGHT
-#define HAVE_BACKLIGHT_BRIGHTNESS
-
-/* Main LCD backlight brightness range and defaults: the backlight driver
- * has levels from 0 to 255. But 0 is off so start at 1.
- */
-#define MIN_BRIGHTNESS_SETTING      1
-#define MAX_BRIGHTNESS_SETTING      255
-#define BRIGHTNESS_STEP             5
-#define DEFAULT_BRIGHTNESS_SETTING  70
-
-/* Which backlight fading type? */
-#define CONFIG_BACKLIGHT_FADING BACKLIGHT_FADING_SW_SETTING
-
-/* define this if you have a real-time clock */
-#define CONFIG_RTC APPLICATION
-
-/* The number of bytes reserved for loadable codecs */
-#define CODEC_SIZE 0x80000
-
-/* The number of bytes reserved for loadable plugins */
-#define PLUGIN_BUFFER_SIZE 0x100000
 
 #define HAVE_HEADPHONE_DETECTION
 #define HAVE_LINEOUT_DETECTION
@@ -81,32 +25,13 @@
 /* define this if the target has volume keys which can be used in the lists */
 #define HAVE_VOLUME_IN_LIST
 
-#ifndef SIMULATOR
-/* We have usb power and can detect usb but it is handled by Linux */
-#define HAVE_USB_POWER
-
-#endif
-
 #define CONFIG_BATTERY_MEASURE PERCENTAGE_MEASURE
-
-/* Linux controlls charging, we can monitor */
-#define CONFIG_CHARGING CHARGING_MONITOR
-
-/* define this if the hardware can be powered off while charging */
-#define HAVE_POWEROFF_WHILE_CHARGING
-
-/* same dimensions as gigabeats */
-#define CONFIG_LCD LCD_INGENIC_LINUX
 
 /* Define this if you have a software controlled poweroff */
 #define HAVE_SW_POWEROFF
 
 /* Define this to the CPU frequency */
 #define CPU_FREQ           1008000000
-
-/* No special storage */
-#define CONFIG_STORAGE STORAGE_HOSTFS
-#define HAVE_STORAGE_FLUSH
 
 /* Battery */
 #define BATTERY_TYPES_COUNT  1
@@ -135,3 +60,5 @@
 #define USB_VID_STR "C502"
 #define USB_PID_STR "003C" // XXX this is probably wrong
 
+/* Generic HiBy stuff */
+#include "hibylinux.h"
