3cad8cab0d1024c89626cbf8bd5b7966439c00ce
William Wilgus <wilgus.william@gmail.com>
2020-10-23 00:40:52 -0400

    ClipPlus BOOTLOADER DONT FIT!
    REMOVED FROM ALL NATIVE BOOTLOADERS:
    finish removing the text scrolling
    pare down printf to a minimal subset (%c %s %l %d %u and %x(%p))
    remove diacritic and rtl language support
    
    GOAL 134000
    
    START 135305
    
    CURRENT 133687
    
    SUCCESS! (ASSUMING IT WORKS -- UNESTED)
    
    Change-Id: Ic3f6ac1dc260578f581ee53458b3e5bb47d313ec

--- firmware/common/diacritic.c
+++ firmware/common/diacritic.c
@@ -194,7 +194,7 @@ static const struct diac_range diac_ranges[] =
 };
 
 #define MRU_MAX_LEN 32
-
+#ifndef BOOTLOADER
 bool is_diacritic(const unsigned short char_code, bool *is_rtl)
 {
     static uint8_t mru_len = 0;
@@ -248,4 +248,12 @@ Found:
 
     return (char_code < diac->base + (info & DIAC_CNT));
 }
-
+#else /*BOOTLOADER*/
+inline bool is_diacritic(const unsigned short char_code, bool *is_rtl)
+{
+    (void)char_code;
+    if (is_rtl)
+        *is_rtl = false;
+    return false;
+}
+#endif /* ndef BOOTLOADER*/
