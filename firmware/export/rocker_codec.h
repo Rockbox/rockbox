39a0d8bf86da56b9725682c25aa9fd76e91badad
Solomon Peachy <pizza@shaftnet.org>
2020-10-25 19:01:57 -0400

    x3ii/x20/rocker:  Fix volume scaling.
    Change-Id: Ie70d709b4c18f16cdd67aa05d9183b4ace2e66b4

--- firmware/export/rocker_codec.h
+++ firmware/export/rocker_codec.h
@@ -2,7 +2,7 @@
 #define __ROCKER_CODEC__
 
 #define AUDIOHW_CAPS 0
-AUDIOHW_SETTING(VOLUME, "dB", 0, 1, -127,  0, -30)
+AUDIOHW_SETTING(VOLUME, "dB", 1, 5, -102*10,  0, -30*10)
 #endif
 
 #define AUDIOHW_MUTE_ON_PAUSE
