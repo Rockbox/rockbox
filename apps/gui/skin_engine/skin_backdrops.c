Rockbox/rockbox

commit c85d8e286549589a38e33d57e2a92d6c99ce5edf

Framebuffer_viewport Rewrite -- BUG FIX
stride was not initialized for the skin_viewport

fixed a few other questionable areas

Change-Id: I9cc7830a4406857bf3aba26a328c288e3702cddd

apps/gui/skin_engine/skin_backdrops.c
@@ -210,7 +210,8 @@ void skin_backdrop_set_buffer(int backdrop_id, struct skin_viewport *svp)
         return;
     else if (backdrop_id < 0)
     {
-        svp->vp.buffer = NULL; /*Default*/
+        /* SCREEN_MAIN is ok here screen only matters if passed VP is NULL */
+        screens[SCREEN_MAIN].viewport_set_buffer(&svp->vp, NULL); /*Default*/
         return;
     }
 
@@ -224,6 +225,7 @@ void skin_backdrop_set_buffer(int backdrop_id, struct skin_viewport *svp)
     {
         svp->framebuf.elems = LCD_BACKDROP_BYTES / sizeof(fb_data);
     }
+    svp->framebuf.stride = 0; /* default stride */
     svp->framebuf.get_address_fn = NULL; /*Default iterator*/
     screens[screen].viewport_set_buffer(&svp->vp, &svp->framebuf);
 }

apps/gui/skin_engine/skin_render.c
@@ -358,7 +358,6 @@ static void do_tags_in_hidden_conditional(struct skin_element* branch,
 {
     struct gui_wps *gwps = info->gwps;
     struct wps_data *data = gwps->data;
-    struct viewport *last_vp;
 
     /* Tags here are ones which need to be "turned off" or cleared
      * if they are in a conditional branch which isnt being used */
@@ -436,22 +435,20 @@ static void do_tags_in_hidden_conditional(struct skin_element* branch,
                             {
                                 skin_backdrop_set_buffer(data->backdrop_id, skin_viewport);
                                 skin_backdrop_show(-1);
-                            }
-#endif
-                            last_vp = gwps->display->set_viewport(&skin_viewport->vp);
-                            gwps->display->clear_viewport();
-                            gwps->display->set_viewport_ex(&info->skin_vp->vp, 0);
-                            skin_viewport->hidden_flags |= VP_DRAW_HIDDEN;
-
-#if (LCD_DEPTH > 1) || (defined(HAVE_REMOTE_LCD) && (LCD_REMOTE_DEPTH > 1))
-                            if (skin_viewport->output_to_backdrop_buffer)
-                            {
-                                gwps->display->set_viewport_ex(last_vp, 0);
+                                gwps->display->set_viewport(&skin_viewport->vp);
+                                gwps->display->clear_viewport();
+                                gwps->display->set_viewport_ex(&info->skin_vp->vp, 0);
+                                skin_backdrop_set_buffer(-1, skin_viewport);
                                 skin_backdrop_show(data->backdrop_id);
                             }
-#else
-    (void)last_vp;
+                            else
 #endif
+                            {
+                                gwps->display->set_viewport(&skin_viewport->vp);
+                                gwps->display->clear_viewport();
+                                gwps->display->set_viewport_ex(&info->skin_vp->vp, 0);
+                            }
+                            skin_viewport->hidden_flags |= VP_DRAW_HIDDEN;
                         }
                     }
                 }
@@ -743,7 +740,7 @@ void skin_render_viewport(struct skin_element* viewport, struct gui_wps *gwps,
         struct wps_token *token = SKINOFFSETTOPTR(skin_buffer, imglist->token);
         if (token) {
             struct gui_img *img = (struct gui_img *)SKINOFFSETTOPTR(skin_buffer, token->value.data);
-            if (img)
+        if (img)
                 img->display = -1;
         }
         imglist = SKINOFFSETTOPTR(skin_buffer, imglist->next);
@@ -823,19 +820,18 @@ void skin_render(struct gui_wps *gwps, unsigned refresh_mode)
     int old_refresh_mode = refresh_mode;
     skin_buffer = get_skin_buffer(gwps->data);
 
-    struct viewport* first_vp;
-    /* should already be the default buffer */
-    first_vp = display->set_viewport(NULL);
-
     /* Framebuffer is likely dirty */
     if ((refresh_mode&SKIN_REFRESH_ALL) == SKIN_REFRESH_ALL)
     {
+        /* should already be the default buffer */
+        struct viewport * first_vp = display->set_viewport_ex(NULL, 0);
         if ((first_vp->flags & VP_FLAG_VP_SET_CLEAN) == VP_FLAG_VP_DIRTY &&
             get_current_activity() == ACTIVITY_WPS) /* only clear if in WPS */
         {
             display->clear_viewport();
         }
     }
+
     viewport = SKINOFFSETTOPTR(skin_buffer, data->tree);
     if (!viewport) return;
     skin_viewport = SKINOFFSETTOPTR(skin_buffer, viewport->data);
@@ -898,6 +894,7 @@ void skin_render(struct gui_wps *gwps, unsigned refresh_mode)
         refresh_mode = old_refresh_mode;
     }
 #if (LCD_DEPTH > 1) || (defined(HAVE_REMOTE_LCD) && (LCD_REMOTE_DEPTH > 1))
+    skin_backdrop_set_buffer(-1, skin_viewport);
     skin_backdrop_show(data->backdrop_id);
 #endif
 
@@ -907,8 +904,8 @@ void skin_render(struct gui_wps *gwps, unsigned refresh_mode)
          * to redraw itself */
         send_event(GUI_EVENT_NEED_UI_UPDATE, NULL);
     }
-    /* Restore the first viewport */
-    display->set_viewport_ex(first_vp, VP_FLAG_VP_SET_CLEAN);
+    /* Restore the default viewport */
+    display->set_viewport_ex(NULL, VP_FLAG_VP_SET_CLEAN);
     display->update();
 }
 

apps/gui/skin_engine/wps_internals.h
@@ -175,7 +175,7 @@ struct gradient_config {
 #define VP_DEFAULT_LABEL_STRING "|"
 struct skin_viewport {
     struct viewport vp;   /* The LCD viewport struct */
-    struct frame_buffer_t framebuf;
+    struct frame_buffer_t framebuf; /* holds reference to current framebuffer */
     char hidden_flags;
     bool is_infovp;
     OFFSETTYPE(char*) label;

commit c5c17fa7995c62267a1695e58841bd17b9fb2099

Fix display regression from c85d8e2
Basically setting a null buffer is valid but it must be selected
into a screen to initialize to the default buffer

I wrongly assumed screen type wouldn't matter but since I decided to
reference backdrops directly to the default buffer
(since they are saved as an offset from what it later assumes to be the
default framebuffer)
SCREEN_MAIN/SCREEN_REMOTE are not longer optional

Change-Id: I8a8afbbe1e3ed0bfe6abd40ce287638e9fc6da60

apps/gui/skin_engine/skin_backdrops.c
@@ -210,8 +210,15 @@ void skin_backdrop_set_buffer(int backdrop_id, struct skin_viewport *svp)
         return;
     else if (backdrop_id < 0)
     {
-        /* SCREEN_MAIN is ok here screen only matters if passed VP is NULL */
-        screens[SCREEN_MAIN].viewport_set_buffer(&svp->vp, NULL); /*Default*/
+#if 1
+        /* ensure the current vp has been removed so it has to be reselected */
+        screens[SCREEN_MAIN].set_viewport_ex(NULL, 0);
+#   if defined(HAVE_REMOTE_LCD)
+        screens[SCREEN_REMOTE].set_viewport_ex(NULL, 0);
+#   endif
+#endif
+        /* WARNING: vp-> buffer is invaid till viewport is set to a screen */
+        svp->vp.buffer = NULL; /*Default*/
         return;
     }
 