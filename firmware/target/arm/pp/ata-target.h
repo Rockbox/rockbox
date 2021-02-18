b98c1be69913c3f1f11fb93dcd5a84cc29b46b9c
Christian Soffke <christian.soffke@gmail.com>
2021-02-18 23:36:17 +0100

    Fall back to UDMA 1 on PP5020
    ATA DMA was enabled for all PP502x targets in d118f47 after previously reported instabilities were thought to have been fixed. The iPod 4G target remains unstable when UDMA 2 is enabled. File system corruption will eventually occur even using stock hardware in normal usage, according to both my own experience and that of several other forum users. UDMA 1 appears to be stable.
    
    Change-Id: I8526bad9e879f5dad5174cfe07cd8828d8b72406

--- firmware/target/arm/pp/ata-target.h
+++ firmware/target/arm/pp/ata-target.h
@@ -63,7 +63,7 @@
 /* Maximum multi-word DMA mode supported by the controller */
 #define ATA_MAX_MWDMA 2
 
-#ifndef BOOTLOADER    
+#if !defined(BOOTLOADER) && CONFIG_CPU != PP5020
 /* The PP5020 supports UDMA 4, but it needs cpu boosting and only
  * improves performance by ~10% with a stock disk.
  * UDMA 2 is stable at 30 Mhz.
@@ -77,7 +77,8 @@
 #error "CPU speeds under 24Mhz have not been tested with DMA"
 #endif
 #else
-/* The bootloader runs at 24 Mhz and needs a slower mode */
+/* A slower mode is used on PP5020 due to reported instabilities.
+The bootloader runs at 24 Mhz and needs a slower mode */
 #define ATA_MAX_UDMA 1
 #endif
 
