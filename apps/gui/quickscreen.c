85e5d430c4797413592fcad403277f8dddfa929a
Christian Soffke <christian.soffke@gmail.com>
2021-02-18 23:43:25 +0100

    iPods: Allow using scroll wheel to change volume on QuickScreen
    Change-Id: Ifb989f9513a9e3c59e4ab0f38c0a8e2675c68dd8

--- apps/gui/quickscreen.c
+++ apps/gui/quickscreen.c
@@ -26,6 +26,7 @@
 #include "font.h"
 #include "kernel.h"
 #include "misc.h"
+#include "sound.h"
 #include "action.h"
 #include "settings_list.h"
 #include "lang.h"
@@ -358,6 +359,12 @@ static bool gui_syncquickscreen_run(struct gui_quickscreen * qs, int button_ente
                                      vps[i], &vp_icons[i]);
             if (qs->callback)
                 qs->callback(qs);
+        } else if (button == ACTION_WPS_VOLUP) {
+            global_settings.volume += sound_steps(SOUND_VOLUME);
+            setvol();
+        } else if (button == ACTION_WPS_VOLDOWN) {
+            global_settings.volume -= sound_steps(SOUND_VOLUME);
+            setvol();
         }
         else if (button == button_enter)
             can_quit = true;
