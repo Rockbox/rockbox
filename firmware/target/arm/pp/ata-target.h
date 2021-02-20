964569e534babff082b24602c8d3cf60d3e36410
Christian Soffke <christian.soffke@gmail.com>
2021-02-21 00:53:01 +0100

    Disable UDMA 2 on iPod4G target
    ATA DMA was enabled for all PP502x targets in d118f47 after previously reported instabilities were thought to have been fixed. The iPod 4G target remains unstable when UDMA 2 is enabled. File system corruption will eventually occur even using stock hardware in normal usage, according to both my own experience and that of several other forum users. UDMA 1 appears to be stable.
    
    Change-Id: I8526bad9e879f5dad5174cfe07cd8828d8b72406

--- firmware/target/arm/pp/ata-target.h
+++ firmware/target/arm/pp/ata-target.h
@@ -68,8 +68,10 @@
  * improves performance by ~10% with a stock disk.
  * UDMA 2 is stable at 30 Mhz.
  * UDMA 1 is stable at 24 Mhz.
+ *
+ * A slower mode is used on iPod 4Gs due to reported instabilities.
  */
-#if CPUFREQ_NORMAL >= 30000000
+#if CPUFREQ_NORMAL >= 30000000 && !defined(IPOD_4G)
 #define ATA_MAX_UDMA 2
 #elif CPUFREQ_NORMAL >= 24000000
 #define ATA_MAX_UDMA 1
