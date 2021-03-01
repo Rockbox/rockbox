5c3bbc6a9c9954778002e379799c701d4900b29e
Christian Soffke <christian.soffke@gmail.com>
2021-03-02 00:46:09 +0100

    Reset filetype colors when resetting theme colors
    Even though filetype colors can't currently be changed by going to Settings->Theme Settings->Colors, a user likely wishes to still reset them when selecting 'Reset Colors' after a .colours file may have been applied by a theme.
    
    Change-Id: I4494c83a502666900f6d9d2cc3fb1825af5e45f6

--- apps/menus/theme_menu.c
+++ apps/menus/theme_menu.c
@@ -42,6 +42,9 @@
 #include "statusbar-skinned.h"
 #include "skin_engine/skin_engine.h"
 #include "icons.h"
+#ifdef HAVE_LCD_COLOR
+#include "filetypes.h"
+#endif
 
 #ifdef HAVE_BACKDROP_IMAGE
 /**
@@ -116,7 +119,10 @@ static int reset_color(void)
     global_settings.lse_color = LCD_DEFAULT_BG;
     global_settings.lst_color = LCD_DEFAULT_FG;
     global_settings.list_separator_color = LCD_DARKGRAY;
-    
+    global_settings.colors_file[0] = '-';
+    global_settings.colors_file[1] = '\0';
+
+    read_color_theme_file();
     settings_save();
     settings_apply(false);
     settings_apply_skins();
