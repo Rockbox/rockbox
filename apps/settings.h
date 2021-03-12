245926a169b4a67d63cff30372f91b8618957041
Christian Soffke <christian.soffke@gmail.com>
2021-03-12 19:24:58 +0100

    Customizable Current Playlist Submenu
    Options to shuffle tracks and to queue tracks
    in the Current Playlist Submenu can now be
    hidden, or the latter can also be put into its own submenu.
    Users can customize this in Settings  by going to
    "General Settings - Playlist -> Current Playlist"
    and choosing from "Shuffled Adding" (Show / Hide) or
    "Queuing" (Show / Hide / Relocate to Submenu).
    
    Allows for the paring down of the set of choices to
    a minimum of the 4 more common ones for adding tracks
    to a dynamic playlist (coming from the current total
    of 11).
    
    Defaults have been set so that users have to actively
    modify their settings to notice any difference, which
    makes it unlikely that anybody's workflow would be
    negatively affected by this change.
    
    Change-Id: Ibe48fc4da2c79f54cd7272df8e1e3ba9955203e5

--- apps/settings.h
+++ apps/settings.h
@@ -109,6 +109,18 @@ enum
     NUM_REPEAT_MODES
 };
 
+enum
+{
+    SHOW_SHUFFLED_ADDING = 0,
+    HIDE_SHUFFLED_ADDING
+};
+
+enum
+{
+    SHOW_QUEUE = 0,
+    HIDE_QUEUE,
+    SUBMENU_QUEUE
+};
 
 /* dir filter options */
 /* Note: Any new filter modes need to be added before NUM_FILTER_MODES.
@@ -574,6 +586,8 @@ struct user_settings
     bool fade_on_stop; /* fade on pause/unpause/stop */
     bool playlist_shuffle;
     bool warnon_erase_dynplaylist; /* warn when erasing dynamic playlist */
+    int shuffled_adding; /* how and whether to display options for adding shuffled tracks to dynamic playlist */
+    int queue_options; /* how and whether to display options to queue tracks */
 
     /* playlist viewer settings */
     bool playlist_viewer_icons; /* display icons on viewer */
