8db60e371763d7682b2e75612e0f0904419b3ab2
amachronic <amachronic@protonmail.com>
2021-04-06 15:07:32 +0100

    (DO NOT MERGE) Debugging a hardware init problem with the FiiO M3K
    There are three possible reasons for the hang which I can think of:
    
    1. panic or exception which we can't see because of LCD driver lockup
    2. soft hang, the bootloader is stuck in an infinite loop
    3. stuck in IRQ loop, or maybe we don't get any IRQs
    
    Bottom of the screen will now contain the following information:
    
    ipr0:<hex> ipr1:<hex>
    irq epc:<addr>
    t = <number>
    
    this debugs the IRQ processing. If for some reason we end up stuck
    with no IRQs, then t will stop increasing.
    
    If there is an IRQ loop, then the t number will increase, and I think
    the ipr0, ipr1 values will hold steady, at least one of them will be
    nonzero, and that indicates which IRQ is triggering.
    
    In either of the above cases, the value of ipr0, ipr1, and irq epc
    should be reported.
    
    If on the other hand t increases, but ipr0, ipr1 are both steadily 0,
    then it's probably a soft lockup. In this case press the PLAY button,
    and another line will appear below:
    
    btn <count> epc:<addr>
    
    this can be used to debug a soft lockup. The <count> increases by 1
    each time the button is pressed, makes it easier to confirm that
    something is actually happening when you press the button.
    
    You should record the EPC after each press, and press the button
    more than once... if the EPC is changing then it's probably not
    worth getting a ton of values, just 3 or 4 should be enough.
    
    Oh and if you notice any other quirks or strange behavior then I'm
    happy to hear about it. :P
    
    PLEASE NOTE the "Read..." step in the bootloader will take several
    seconds because of all the LCD updating that's going on, just hang
    on and wait until it finishes.
    
    IMPORTANT! In order for the EPC values to be useful I will need
    the *.elf and *.map file from the bootloader build that you test with.
    Thanks!
    
    Change-Id: Ife5c95600e0d4592bd6493152a6732d01bd049b7

--- apps/main.c
+++ apps/main.c
@@ -142,6 +142,8 @@
 #endif
 #endif
 
+extern void DIE(void);
+
 /*#define AUTOROCK*/ /* define this to check for "autostart.rock" on boot */
 
 static void init(void);
@@ -307,6 +309,7 @@ static void init_tagcache(void)
 static void init(void)
 {
     system_init();
+    DIE();
     core_allocator_init();
     kernel_init();
 #ifdef APPLICATION
@@ -390,6 +393,8 @@ static void init(void)
     core_allocator_init();
     kernel_init();
 
+    backlight_init();
+
     /* early early early! */
     filesystem_init();
 
@@ -455,7 +460,7 @@ static void init(void)
     m5636_init();
 #endif
 
-    backlight_init();
+    //backlight_init();
 
     button_init();
 
