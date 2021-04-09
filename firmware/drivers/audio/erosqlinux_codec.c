Rockbox/rockbox

commit ad55da5f87261d3e855998b02cc811581abe861c

ErosQ: Set Line Level Output volume
With a full-scale 440Hz tone, the line out voltage
measured approx. 5.8Vpp at the 0 setting. WAY too hot!
(9 dBV, in fact)

For 0.894Vpp (-10 dBV - consumer devices), -18 appears to be
about right for line level signals, but for "pro" equipment
a different level may be desired.

Therefore, the user to cap the line out level by re-using the global
volume limit setting.

Change-Id: I0d1d6482ea95537e9a2d00884eaee2713771c614

firmware/drivers/audio/erosqlinux_codec.c
@@ -35,6 +35,8 @@
 #include "pcm-alsa.h"
 #include "pcm_sw_volume.h"
 
+#include "settings.h"
+
 #include "logf.h"
 
 /*
@@ -175,8 +177,12 @@ void audiohw_set_volume(int vol_l, int vol_r)
     vol_r_hw = vol_r;
 
     if (lineout_inserted()) {
-        l = 0;
-        r = 0;
+        /* On the EROS Q/K hardware, line out is _very_ hot
+           at ~5.8Vpp. As the hardware provides no way to reduce
+           output gain, we have to back off on the PCM signal
+           to avoid blowing out the signal.
+        */
+        l = r = global_settings.volume_limit;
     } else {
         l = vol_l_hw;
         r = vol_r_hw;
@@ -198,8 +204,8 @@ void audiohw_set_lineout_volume(int vol_l, int vol_r)
     (void)vol_r;
 
     if (lineout_inserted()) {
-        l = 0;
-        r = 0;
+        l = -180;
+        r = -180;
     } else {
         l = vol_l_hw;
         r = vol_r_hw;

commit e17337c9aab231875234e0485f7175a5a613fb4c

ErosQ: fix panic on startup caused by ad55da5f
It turns out #include "settings.h" pulls in rbpaths.h which ends up
remapping open() to the path-mangling rockbox open().

By defining RB_FILESYSTEM_OS we prevent the remap.  My mistake for not
testing this before committing!

Change-Id: I2978eb7b413693c4cb887b7ac7b2457780db7d25

firmware/drivers/audio/erosqlinux_codec.c
@@ -20,6 +20,8 @@
  * KIND, either express or implied.
  *
  ****************************************************************************/
+#define RB_FILESYSTEM_OS  // VERY important this is before settings.h
+                          // because that pulls in rbpaths.h which breaks open()
 
 //#define LOGF_ENABLE
 
@@ -204,8 +206,7 @@ void audiohw_set_lineout_volume(int vol_l, int vol_r)
     (void)vol_r;
 
     if (lineout_inserted()) {
-        l = -180;
-        r = -180;
+        l = r = global_settings.volume_limit;
     } else {
         l = vol_l_hw;
         r = vol_r_hw;