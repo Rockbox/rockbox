Rockbox/rockbox

commit 4dc602dd7f763a2ceea7c3e01501cdc3e9de375c

erosq: Tweak volume scale to be approximately correct
Tested on eros q, everything measured from line out,
open circuit.

- volume steps were approximately double the dB they
  were labelled as, so "-2 dB" would result in a change
  of about -4 dB from maximum (0, +6.2dBV)
- maximum volume defining the line out volume only
  changed every 10 values, and then was not close
  to correct- "-10 dB" resulted in -2.5 dB from maximum

This gets the volume dB approximately correct, and
maximum volume correctly sets the line out volume.
I was unable to get odd values in the max volume
to work, so set the step size to 2 instead of one.

For "consumer level" (-10dBV), set to -16.
For "Pro level" (+4dBu -> ~1.8dBV), set to -4.

Change-Id: I898b85d768153579a893b23551019af88f865d21

firmware/drivers/audio/erosqlinux_codec.c
@@ -151,7 +151,7 @@ void audiohw_set_frequency(int fsel)
 }
 
 /* min/max for pcm volume */
-const int min_pcm = -430;
+const int min_pcm = -740;
 const int max_pcm = 0;
 
 void audiohw_set_volume(int vol_l, int vol_r)
@@ -169,7 +169,7 @@ void audiohw_set_volume(int vol_l, int vol_r)
            output gain, we have to back off on the PCM signal
            to avoid blowing out the signal.
         */
-        l = r = global_settings.volume_limit;
+        l = r = global_settings.volume_limit * 10;
     } else {
         l = vol_l_hw;
         r = vol_r_hw;
@@ -177,7 +177,7 @@ void audiohw_set_volume(int vol_l, int vol_r)
 
     int sw_volume_l = l <= min_pcm ? min_pcm : MIN(l, max_pcm);
     int sw_volume_r = r <= min_pcm ? min_pcm : MIN(r, max_pcm);
-    pcm_set_mixer_volume(sw_volume_l / 10, sw_volume_r / 10);
+    pcm_set_mixer_volume(sw_volume_l / 20, sw_volume_r / 20);
 }
 
 void audiohw_set_lineout_volume(int vol_l, int vol_r)
@@ -190,7 +190,7 @@ void audiohw_set_lineout_volume(int vol_l, int vol_r)
     (void)vol_r;
 
     if (lineout_inserted()) {
-        l = r = global_settings.volume_limit;
+        l = r = global_settings.volume_limit * 10;
     } else {
         l = vol_l_hw;
         r = vol_r_hw;
@@ -198,5 +198,5 @@ void audiohw_set_lineout_volume(int vol_l, int vol_r)
 
     int sw_volume_l = l <= min_pcm ? min_pcm : MIN(l, max_pcm);
     int sw_volume_r = r <= min_pcm ? min_pcm : MIN(r, max_pcm);
-    pcm_set_mixer_volume(sw_volume_l / 10, sw_volume_r / 10);
+    pcm_set_mixer_volume(sw_volume_l / 20, sw_volume_r / 20);
 }

firmware/export/erosqlinux_codec.h
@@ -3,7 +3,7 @@
 
 #define AUDIOHW_CAPS (LINEOUT_CAP)
 
-AUDIOHW_SETTING(VOLUME, "dB", 0,  1, -43, 0, -20)
+AUDIOHW_SETTING(VOLUME, "dB", 0,  2, -74, 0, -40)
 
 //#define AUDIOHW_NEEDS_INITIAL_UNMUTE
 

commit e274a2ab2499379d3780da26cc8edb9a1fd3ff38

erosq:  global volume limit uses the same units as volume
(Bug introduced in 4dc602dd7)

Change-Id: I88a147c8bbd489c72f5e69e078e7b7e9e724e9b9

docs/CREDITS
@@ -703,6 +703,8 @@ Georg Gadinger
 Wolfram Sang
 Aidan MacDonald
 Caleb Connolly
+Spencer Brennessel
+Dana Conrad
 
 The libmad team
 The wavpack team
@@ -743,5 +745,3 @@ The Pocket Quake team (Dan East and others)
 The bzip2 team
 The bsdiff team
 The libtomcrypt team
-Spencer Brennessel
-Dana Conrad

firmware/drivers/audio/erosqlinux_codec.c
@@ -169,7 +169,7 @@ void audiohw_set_volume(int vol_l, int vol_r)
            output gain, we have to back off on the PCM signal
            to avoid blowing out the signal.
         */
-        l = r = global_settings.volume_limit * 10;
+        l = r = global_settings.volume_limit;
     } else {
         l = vol_l_hw;
         r = vol_r_hw;
@@ -190,7 +190,7 @@ void audiohw_set_lineout_volume(int vol_l, int vol_r)
     (void)vol_r;
 
     if (lineout_inserted()) {
-        l = r = global_settings.volume_limit * 10;
+        l = r = global_settings.volume_limit;
     } else {
         l = vol_l_hw;
         r = vol_r_hw;