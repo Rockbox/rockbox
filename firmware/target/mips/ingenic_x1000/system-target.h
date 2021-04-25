d6220f618ba6daa043f70e42e421b8fad79e7731
Aidan MacDonald <amachronic@protonmail.com>
2021-04-25 13:43:58 +0100

    MIPS: emulate -ffunction-sections with macros in mmu-mips
    Using a macro to put each function in its own .icode-based section
    allows us to put the functions in IRAM _and_ have linker GC. This
    removes a troublesome #ifdef BOOTLOADER_SPL on the X1000 target.
    
    Change-Id: Ia7b59778f5c36b7970dee4280547e434a1f4fc5a

--- firmware/target/mips/ingenic_x1000/system-target.h
+++ firmware/target/mips/ingenic_x1000/system-target.h
@@ -28,15 +28,6 @@
 #define CACHEALIGN_BITS 5
 #define CACHE_SIZE      (16*1024)
 
-#ifdef BOOTLOADER_SPL
-/* This saves ~200 bytes in the SPL by allowing -ffunction-sections to split
- * up the cache management functions, most of which aren't called by the SPL.
- * If they are placed in .icode, then they all end up in one section and the
- * linker can't discard the unused functions.
- */
-# define MIPS_CACHEFUNC_ATTR
-#endif
-
 #ifdef DEBUG
 /* Define this to get CPU idle stats, visible in the debug menu. */
 # define X1000_CPUIDLE_STATS
