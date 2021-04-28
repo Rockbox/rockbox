a1ee26b8c2e905a6eb321b4738d2dc470a5f1113
William Wilgus <wilgus.william@gmail.com>
2021-04-27 23:12:49 -0400

    lua add submenu module + cleanup
    allows menus + submenus + context menus all with simple tables
    menu_t which is a table of strings
    func_t which are the corresponding functions to go with those strings
    
    see lua_scripts/submenu_demo.lua
    
    Change-Id: I907b74b4abef0ecbe49f181d0ced6e6d20e94de5

---
+++ apps/plugins/lua/include_lua/menubuttons.lua
@@ -0,0 +1,16 @@
+-- Menu Button definitions --
+return {
+    CANCEL = rb.actions.PLA_CANCEL,
+    DOWN = rb.actions.PLA_DOWN,
+    DOWNR = rb.actions.PLA_DOWN_REPEAT,
+    EXIT = rb.actions.PLA_EXIT,
+    LEFT = rb.actions.PLA_LEFT,
+    LEFTR = rb.actions.PLA_LEFT_REPEAT,
+    RIGHT = rb.actions.PLA_RIGHT,
+    RIGHTR = rb.actions.PLA_RIGHT_REPEAT,
+    SEL = rb.actions.PLA_SELECT,
+    SELREL = rb.actions.PLA_SELECT_REL,
+    SELR = rb.actions.PLA_SELECT_REPEAT,
+    UP = rb.actions.PLA_UP,
+    UPR = rb.actions.PLA_UP_REPEAT,
+}
