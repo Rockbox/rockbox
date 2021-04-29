410895c7ebda2d42476a6e119879991af4656cdb
William Wilgus <wilgus.william@gmail.com>
2021-04-29 01:56:49 -0400

    lua submenus add a way to dynamically add items
    updated example script and renamed some functions as well
    
    fixed bug in printtable cursor position if greater than maxlines for the
    screen would reset to item 1
    
    now we move the list start and select it
    
    Change-Id: I01dead0481ef2e925af8b4cc6c14e36c2859dbba

--- apps/plugins/lua/include_lua/printsubmenu.lua
+++ apps/plugins/lua/include_lua/printsubmenu.lua
@@ -21,7 +21,11 @@
  ****************************************************************************/
 ]]
 if not rb.lcd_framebuffer then rb.splash(rb.HZ, "No Support!") return nil end
+--GLOBALS
 menu_ctx = {}
+submenu_insert = table.insert
+submenu_remove = table.remove
+
 local last_ctx = false
 local p_settings
 
@@ -110,8 +114,6 @@ local function dpad(x, xi, xir, y, yi, yir, timeout, overflow, selected)
     return cancel, select, x_chg, x, y_chg, y, 0xffff
 end -- dpad
 
-
-
 local function menu_set_defaults(settings, ctx)
     p_settings = settings or {wrap = true, hasheader = true, justify = "left", dpad_fn = dpad}
     menu_ctx = ctx or {collapse_fn = {}, lv = 0, update = false, start = 1}
@@ -130,13 +132,9 @@ local function ctx_loop()
     ctx_loop = loopfn --restore for another run
 end
 
-function get_menu()
-    return menu_t, func_t
-end
-
 local function push_ctx(new_getmenu)
     last_ctx = last_ctx or {}
-    table.insert(last_ctx, menu_ctx)
+    submenu_insert(last_ctx, menu_ctx)
     menu_ctx.getmenu = get_menu
     menu_ctx.settings = p_settings
     --menu_ctx is a new variable after this point
@@ -148,7 +146,7 @@ local function push_ctx(new_getmenu)
 end
 
 local function pop_ctx()
-    menu_ctx = table.remove(last_ctx)
+    menu_ctx = submenu_remove(last_ctx)
     if menu_ctx then
         get_menu = menu_ctx.getmenu
         p_settings = menu_ctx.settings
@@ -166,7 +164,7 @@ end
 local function display_context_menu_internal(sel)
 
         if sel <= 0 or not menu_ctx.user_context_fn then return false end
-        local parent = get_parent() or 0
+        local parent = submenu_get_parent() or 0
         local user_context_fn = menu_ctx.user_context_fn
         local function display_context_menu(i, menu_t, func_t)
 
@@ -192,29 +190,35 @@ local function display_context_menu_internal(sel)
         return true
 end
 
-function get_parent(lv)
-    lv = lv or #menu_ctx.collapse_fn
+--[[ submenu_get_parent() gets the parent of the top most level
+     if lv is supplied it instead gets the parent of that level ]]
+function submenu_get_parent(lv)
+    lv = lv or #menu_ctx.collapse_fn or 1
     collectgarbage("step")
     local t = menu_ctx.collapse_fn[lv] or {}
-    return t[2] or -1
+    return t[2] or -1, lv
 end
 
-function set_menu(mt, ft, user_context_fn, settings)
-    local function empty_fn() end
-    menu_set_defaults(settings)
-    if type(user_context_fn) == 'function' then
-        display_context_menu = display_context_menu_internal
-        menu_ctx.user_context_fn = user_context_fn
-    else
-        display_context_menu = empty_fn
-        menu_ctx.user_context_fn = false
-    end
-    p_settings = settings or p_settings
-    menu_t, func_t = mt, ft
-    ctx_loop()
+--[[ submenu_collapse() collapses submenu till level or ROOT is reached ]]
+function submenu_collapse(lv)
+    local lv_out, menu_sz
+    local items_removed = 0
+    repeat
+        local collapse_fn = submenu_remove(menu_ctx.collapse_fn)
+        if collapse_fn then
+            lv_out, _, menu_sz = collapse_fn[1](parent, menu_t, func_t)
+            items_removed = items_removed + menu_sz
+        end
+
+    until not collapse_fn or lv >= lv_out
+    return lv_out, items_removed
 end
 
-function create_sub_menu(lv, mt, ft)
+--[[create_submenu() supply level of submenu > 0 ROOT is lv 0
+    supply menu strings table and function table
+    closure returned run this function to expand the menu
+]]
+function create_submenu(lv, mt, ft)
     if lv < 1 then error("Level < 1") end
     -- everything in lua is 1 based menu level is no exception
     local lv_tab = string.rep ("\t", lv)
@@ -224,7 +228,7 @@ function create_sub_menu(lv, mt, ft)
         local item_in, item_out = i, i
         if lv <= #menu_ctx.collapse_fn then --something else expanded??
             repeat
-                local collapse_fn = table.remove(menu_ctx.collapse_fn)
+                local collapse_fn = submenu_remove(menu_ctx.collapse_fn)
                 if collapse_fn then
                     lv_out, item_out, menusz_out = collapse_fn[1](i, m, f)
                     -- if the item i is below this menu, it needs to shift too
@@ -241,23 +245,44 @@ function create_sub_menu(lv, mt, ft)
         menu_ctx.update = true
         for item, _ in ipairs(mt) do
             i = i + 1
-            table.insert(m, i, lv_tab .. mt[item])
-            table.insert(f, i, ft[item])
+            submenu_insert(m, i, lv_tab .. mt[item])
+            submenu_insert(f, i, ft[item])
         end
 
         local function collapse_closure(i, m, f)
             --creates a closure around lv, start_item and menu_sz
             for j = 1, menu_sz, 1 do
-                table.remove(m, start_item + 1)
-                table.remove(f, start_item + 1)
+                submenu_remove(m, start_item + 1)
+                submenu_remove(f, start_item + 1)
             end
             return lv, start_item, menu_sz
         end
 
-        table.insert(menu_ctx.collapse_fn, lv, {collapse_closure, start_item})
+        submenu_insert(menu_ctx.collapse_fn, lv, {collapse_closure, start_item})
         return true
     end
 
     return submenu_closure
 end
 
+
+--[[[ get_menu() returns the ROOT string and fn tables]]
+function get_menu()
+    return menu_t, func_t
+end
+
+--[[set_menu() set your menu and the menu has now been entered ]]
+function set_menu(mt, ft, user_context_fn, settings)
+    local function empty_fn() end
+    menu_set_defaults(settings)
+    if type(user_context_fn) == 'function' then
+        display_context_menu = display_context_menu_internal
+        menu_ctx.user_context_fn = user_context_fn
+    else
+        display_context_menu = empty_fn
+        menu_ctx.user_context_fn = false
+    end
+    p_settings = settings or p_settings
+    menu_t, func_t = mt, ft
+    ctx_loop()
+end
