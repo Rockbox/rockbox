cb57d785b67e332610d0f3ca9f3aa8232906f2d6
Spencer Brennessel <heyspencerb@gmail.com>
2021-05-31 20:04:17 -0400

    Improved search menu for Fiio M3K
    Change-Id: I6d5cc3faa47e41b04c4ad364d4c0542d8df6b128

--- apps/recorder/icons.h
+++ apps/recorder/icons.h
@@ -10,7 +10,7 @@
  * Copyright (C) 2002 Robert E. Hak
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
+ * modify it under the terms Fof the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
