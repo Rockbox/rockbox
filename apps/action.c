6e8f12df268c8524624d513859b35aac4d0c865b
Dana Conrad <dconrad@fastmail.com>
2021-06-06 13:33:03 -0500

    FS#13297: M3K Autolock allows one action before disabling touchpad
    I think I was grossly over-thinking this before. This patchset
    appears to do the job just fine - if I let it autolock, it ignores
    the first touchstrip press, and then if I let it autolock again
    it does /not/ ignore the first play button press.
    
    Thoughts?
    
    Change-Id: Ic3582764df490d96abc2d78116f23cbe0fdd6173

--- apps/action.c
+++ apps/action.c
@@ -683,6 +683,17 @@ static inline int do_auto_softlock(action_last_t *last, action_cur_t *cur)
     if (is_timeout)
     {
         do_key_lock(true);
+
+#if defined(HAVE_TOUCHPAD)
+        /* if the touchpad is supposed to be off and the current buttonpress
+         * is from the touchpad, nullify it. */
+        if (!touchpad_filter(cur->button) &&
+            (!has_flag(action_last.softlock_mask, SEL_ACTION_ENABLED) ||
+            has_flag(action_last.softlock_mask, SEL_ACTION_NOTOUCH)))
+        {
+            cur->button = BUTTON_NONE;
+        }
+#endif
     }
     else if (action == ACTION_STD_KEYLOCK)
     {
