de7454e5bda4cad67b14cb6921d4a95db6f94869
Dana Conrad <dconrad@fastmail.com>
2021-06-26 12:07:11 -0500

    Eros Q Native port?
    https://thesciencedog.files.wordpress.com/2013/09/golden-retriever-and-science1.jpg
    
    Input/Changes very much welcome
    
    - USB booting seems to be working
    - LCD is working in 16-bit RGB565 DMA Mode
    - all buttons appear to work
    - lcd can go to sleep and wake up just fine
    - SD Card appears to work
    - Battery level and charging/not charging seem to be working
    - Scrollwheel is working well, interrupt-driven
    - USB is working, HID keys not yet defined
    - screen no longer flashes white when shutting down
    
    Next thing... ...audio?
    
    I think all the gpio lines are correct now, save maybe AXP_IRQ?
    There's still a couple commented out that might need adding.
    
    Several signals to audio devices I'm not sure if they're hooked up:
    DAC: 44.1k de-emphasis, filt, format
    Stereo Mux: Direction select, AC_DC
    I don't think we need them ^^, but would be nice to know they're in
    the correct state...
    
    How the heck does the bluetooth module connect?
    
    This is messy.
    
    Change-Id: I70dda8fc092c6e3f4352f2245e4164193f803c33

---
+++ firmware/target/mips/ingenic_x1000/erosqnative/audiohw-erosqnative.c
@@ -0,0 +1,77 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2021 Aidan MacDonald, Dana Conrad
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include "audiohw.h"
+#include "system.h"
+#include "pcm_sampr.h"
+#include "aic-x1000.h"
+#include "i2c-x1000.h"
+#include "gpio-x1000.h"
+#include "logf.h"
+//#include "gpio-target.h"
+
+// static int cur_fsel = HW_FREQ_48;
+
+void audiohw_init(void)
+{
+    aic_set_external_codec(true);
+    aic_set_i2s_mode(AIC_I2S_MASTER_MODE);
+    aic_enable_i2s_master_clock(true);
+    
+    aic_enable_i2s_bit_clock(false);
+    aic_set_i2s_clock(X1000_CLK_SCLK_A, hw_freq_sampr[HW_FREQ_44], 256);
+    aic_enable_i2s_bit_clock(true);
+    
+    //audiohw_set_frequency(HW_FREQ_44);
+    
+    gpio_set_level(GPIO_PCM5102A_XMIT, 1);
+    gpio_set_level(GPIO_MAX97220_SHDN, 1);
+    gpio_set_level(GPIO_ISL54405_MUTE, 0);
+    gpio_set_level(GPIO_ISL54405_SEL, 1);
+}
+
+void audiohw_postinit(void)
+{
+
+}
+
+void audiohw_close(void)
+{
+
+}
+
+void audiohw_set_volume(int vol_l, int vol_r)
+{
+
+}
+
+void audiohw_set_frequency(int fsel)
+{
+    int sampr = hw_freq_sampr[fsel];
+    int mult = sampr >= SAMPR_176 ? 128 : 256;
+
+    aic_enable_i2s_bit_clock(false);
+    aic_set_i2s_clock(X1000_CLK_SCLK_A, sampr, 64);
+    aic_enable_i2s_bit_clock(true);
+
+    
+}
+
