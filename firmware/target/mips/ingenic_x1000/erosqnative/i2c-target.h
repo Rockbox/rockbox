a58bd99d1f57a5936054332d8a49141b0bb63668
Dana Conrad <dconrad@fastmail.com>
2021-06-26 12:07:11 -0500

    Eros Q Native port?
    https://thesciencedog.files.wordpress.com/2013/09/golden-retriever-and-science1.jpg
    
    Input/Changes very much welcome
    
    - USB booting seems to be working
    - LCD is working in 16-bit RGB565 DMA Mode
    - all buttons appear to work
    - lcd can go to sleep and wake up just fine
    - SD Card appears to work
    - Battery level and charging/not charging seem to be working
    - Scrollwheel is working well, interrupt-driven
    - USB is working, but not HID multimedia keys? tested on linux, mac
    - screen no longer flashes white when shutting down
    
    Next thing... ...audio?
    
    I think all the gpio lines are correct now, save maybe AXP_IRQ?
    There's still a couple commented out that might need adding.
    
    Several signals to audio devices I'm not sure if they're hooked up:
    DAC: 44.1k de-emphasis, filt, format
    Stereo Mux: Direction select, AC_DC
    I don't think we need them ^^, but would be nice to know they're in
    the correct state...
    
    How the heck does the bluetooth module connect?
    
    Issue: Gerrit patch 3240 (wake backlight without locking keys) doesn't
           work correctly... I wonder why that could be?
    Edit: My m3k seems to do it as well, I don't think it's unique to this.
    
    This is messy.
    
    Change-Id: I70dda8fc092c6e3f4352f2245e4164193f803c33

---
+++ firmware/target/mips/ingenic_x1000/erosqnative/i2c-target.h
@@ -0,0 +1,31 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2021 Aidan MacDonald, Dana Conrad
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#ifndef __I2C_TARGET_H__
+#define __I2C_TARGET_H__
+
+#define I2C_ASYNC_BUS_COUNT  3
+#define I2C_ASYNC_QUEUE_SIZE 4
+
+#define AXP_PMU_BUS  2
+#define AXP_PMU_ADDR 0x34
+
+#endif /* __I2C_TARGET_H__ */
