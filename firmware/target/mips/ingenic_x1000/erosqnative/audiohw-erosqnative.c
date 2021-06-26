f116cbe89104ac35363c49d00fa34073b451abcf
Dana Conrad <dconrad@fastmail.com>
2021-06-26 12:07:11 -0500

    Eros Q Native port?
    https://thesciencedog.files.wordpress.com/2013/09/golden-retriever-and-science1.jpg
    
    Input/Changes very much welcome
    
    - USB booting seems to be working
    - LCD is working in 16-bit RGB565 DMA Mode?
    - patchset is mostly cleaned up from lcd debugging
    - all buttons except scrollwheel appear to work just fine
    - lcd can go to sleep and wake up just fine
    
    Well, those last two were easy... Fixed the buttons and apparently
    it took care of the backlight sleeping as well. Next thing is probably
    the scrollwheel?
    
    This is messy.
    
    Change-Id: I70dda8fc092c6e3f4352f2245e4164193f803c33

---
+++ firmware/target/mips/ingenic_x1000/erosqnative/audiohw-erosqnative.c
@@ -0,0 +1,92 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2021 Aidan MacDonald, Dana Conrad
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include "audiohw.h"
+#include "system.h"
+#include "pcm_sampr.h"
+#include "aic-x1000.h"
+#include "i2c-x1000.h"
+#include "gpio-x1000.h"
+#include "logf.h"
+
+static int cur_fsel = HW_FREQ_48;
+static int cur_power_mode = 0;
+/*
+static void set_ak_freqmode(void)
+{
+    int freq = hw_freq_sampr[cur_fsel];
+    int mult = freq >= SAMPR_176 ? 128 : 256;
+
+    aic_enable_i2s_bit_clock(false);
+    aic_set_i2s_clock(X1000_CLK_SCLK_A, freq, mult);
+    //ak4376_set_freqmode(cur_fsel, mult, cur_power_mode);
+    aic_enable_i2s_bit_clock(true);
+}
+*/
+void audiohw_init(void)
+{
+    /* Configure AIC */
+    aic_set_external_codec(true);
+    aic_set_i2s_mode(AIC_I2S_MASTER_MODE);
+    aic_enable_i2s_master_clock(true);
+
+    /* Initialize DAC */
+    //i2c_x1000_set_freq(AK4376_BUS, I2C_FREQ_400K);
+    //ak4376_open();
+    //set_ak_freqmode();
+}
+
+void audiohw_postinit(void)
+{
+}
+
+void audiohw_close(void)
+{
+    //ak4376_close();
+}
+
+// void audiohw_set_volume(int vol_l, int vol_r)
+// {
+//     //ak4376_set_volume(vol_l, vol_r);
+// }
+
+// void audiohw_set_filter_roll_off(int val)
+// {
+//     //ak4376_set_filter_roll_off(val);
+// }
+
+void audiohw_set_frequency(int fsel)
+{
+    //cur_fsel = fsel;
+    //set_ak_freqmode();
+}
+
+// void audiohw_set_power_mode(int mode)
+// {
+//     //cur_power_mode = mode;
+//     //set_ak_freqmode();
+// }
+/*
+void ak4376_set_pdn_pin(int level)
+{
+    gpio_set_level(GPIO_AK4376_POWER, level ? 1 : 0);
+}
+*/
