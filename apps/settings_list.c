07f54a049c47986599d10d932b8a2ac73cf308b8
Solomon Peachy <pizza@shaftnet.org>
2021-07-09 22:48:19 -0400

    xduoox3ii:  Add "Super Slow" filter roll-off
    I'm not happy with the proliferation of filter-roll-off options
    but I don't have a less ugly solution.
    
    Change-Id: I740fca006fa0c3443a467acfea55b6574d48346b

--- apps/settings_list.c
+++ apps/settings_list.c
@@ -848,8 +848,11 @@ const struct settings_list settings[] = {
 
 #ifdef AUDIOHW_HAVE_FILTER_ROLL_OFF
     CHOICE_SETTING(F_SOUNDSETTING, roll_off, LANG_FILTER_ROLL_OFF, 0,
-#if defined(AUDIOHW_HAVE_SHORT2_ROLL_OFF)
-                   "roll_off", "sharp,slow,short sharp,short slow", sound_set_filter_roll_off,
+#if defined(AUDIOHW_HAVE_SS_ROLL_OFF)
+                   "roll_off", "sharp,slow,short sharp, short slow", sound_set_filter_roll_off,
+                   5, ID2P(LANG_FILTER_SHARP), ID2P(LANG_FILTER_SLOW), ID2P(LANG_FILTER_SHORT_SHARP), ID2P(LANG_FILTER_SHORT_SLOW), ID2P(LANG_FILTER_SUPER_SLOW)),
+#elif defined(AUDIOHW_HAVE_SHORT2_ROLL_OFF)
+                   "roll_off", "sharp,slow,short sharp, short slow", sound_set_filter_roll_off,
                    4, ID2P(LANG_FILTER_SHARP), ID2P(LANG_FILTER_SLOW), ID2P(LANG_FILTER_SHORT_SHARP), ID2P(LANG_FILTER_SHORT_SLOW)),
 #elif defined(AUDIOHW_HAVE_SHORT_ROLL_OFF)
                    "roll_off", "sharp,slow,short,bypass", sound_set_filter_roll_off,
