0f68866ae5b02e3e154f31b6da12b090380db4b0
Aidan MacDonald <amachronic@protonmail.com>
2021-10-18 23:00:38 +0100

    fiiom3k: bump power on delay to 200ms
    Needed due to a report that 80ms was not working.
    
    Change-Id: I5f0f750b0530f5b84ffba58c5d7ff8b283095c4b

--- firmware/target/mips/ingenic_x1000/fiiom3k/power-fiiom3k.c
+++ firmware/target/mips/ingenic_x1000/fiiom3k/power-fiiom3k.c
@@ -77,7 +77,7 @@ void power_init(void)
     axp_set_charge_current(780);
 
     /* Short delay to give power outputs time to stabilize */
-    mdelay(80);
+    mdelay(200);
 }
 
 #ifdef HAVE_USB_CHARGING_ENABLE
