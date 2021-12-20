dc677208d0c2ce1e7d627ae92ac420ef850b301b
Dominik Riebeling <Dominik.Riebeling@gmail.com>
2021-12-20 10:42:19 +0100

    rbutil: Fix unit tests on Windows.
    Change-Id: I124882eb26b01e1acfadfba55bc718ab330ab52a

--- utils/rbutilqt/test/stubs/stubs-compareversion.cpp
+++ utils/rbutilqt/test/stubs/stubs-compareversion.cpp
@@ -2,6 +2,7 @@
 #include "playerbuildinfo.h"
 #include "rbsettings.h"
 #include "rockboxinfo.h"
+#include "system.h"
 
 // not used by the test, just to make things compile.
 QVariant RbSettings::value(RbSettings::UserSettings setting)
@@ -28,4 +29,10 @@ RockboxInfo::RockboxInfo(QString, QString)
 {
 }
 
+#if defined(Q_OS_WIN32)
+enum System::userlevel System::userPermissions(void)
+{
+    return USER;
+}
+#endif
 
