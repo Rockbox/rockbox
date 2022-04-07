1c3f6e063a1e8444873b02463db2a055950ff3c4
Christian Soffke <christian.soffke@gmail.com>
2022-04-07 22:04:16 +0200

    fiiom3k Yes/No screen: Only accept Play button for YES
    According to the on screen instructions, any other button
    should mean No.
    
    More importantly, in my experience at least, the Select
    "button", since it is touch-activated, can sometimes be
    pressed by accident, especially when a warning screen
    is displayed as a result of pressing that same button.
    
    Change-Id: Ieeadfa8018b5df99605297bc47948a181c22dab4

--- apps/keymaps/keymap-fiiom3k.c
+++ apps/keymaps/keymap-fiiom3k.c
@@ -203,7 +203,7 @@ static const struct button_mapping button_context_pitchscreen[] = {
 
 static const struct button_mapping button_context_yesnoscreen[] = {
     {ACTION_YESNO_ACCEPT,           BUTTON_PLAY,                        BUTTON_NONE},
-    {ACTION_YESNO_ACCEPT,           BUTTON_SELECT,                      BUTTON_NONE},
+    {ACTION_STD_CANCEL,             BUTTON_SELECT,                      BUTTON_NONE},
     {ACTION_STD_CANCEL,             BUTTON_BACK,                        BUTTON_NONE},
     {ACTION_STD_CANCEL,             BUTTON_POWER,                       BUTTON_NONE},
     {ACTION_STD_CANCEL,             BUTTON_RIGHT,                       BUTTON_NONE},
