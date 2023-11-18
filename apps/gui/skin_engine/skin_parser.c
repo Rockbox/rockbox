Rockbox/rockbox

commit 01cbb795e9a059238f0e39d44d279c1d52874e06

don't load the same bmp image more than once in a skin
Change-Id: Ie38146a80bb356f40810f017c262b81edac69688

apps/gui/skin_engine/skin_parser.c
@@ -1826,7 +1826,7 @@ static bool load_skin_bitmaps(struct wps_data *wps_data, char *bmpdir)
     {
         struct wps_token *token = SKINOFFSETTOPTR(skin_buffer, list->token);
         struct gui_img *img = (struct gui_img*)SKINOFFSETTOPTR(skin_buffer, token->value.data);
-        if (img->bm.data)
+        if (!img->loaded)
         {
             if (img->using_preloaded_icons)
             {
@@ -1835,10 +1835,30 @@ static bool load_skin_bitmaps(struct wps_data *wps_data, char *bmpdir)
             }
             else
             {
-                img->buflib_handle = load_skin_bmp(wps_data, &img->bm, bmpdir);
+                char path[MAX_PATH];
+                int handle;
+                strcpy(path, img->bm.data);
+                handle = load_skin_bmp(wps_data, &img->bm, bmpdir);
+                img->buflib_handle = handle;
                 img->loaded = img->buflib_handle >= 0;
                 if (img->loaded)
+                {
+                    struct skin_token_list *imglist = SKINOFFSETTOPTR(skin_buffer, list->next);
+
                     img->subimage_height = img->bm.height / img->num_subimages;
+                    while (imglist)
+                    {
+                        token = SKINOFFSETTOPTR(skin_buffer, imglist->token);
+                        img = (struct gui_img*)SKINOFFSETTOPTR(skin_buffer, token->value.data);
+                        if (!strcmp(path, img->bm.data))
+                        {
+                            img->loaded = true;
+                            img->buflib_handle = handle;
+                            img->subimage_height = img->bm.height / img->num_subimages;
+                        }
+                        imglist = SKINOFFSETTOPTR(skin_buffer, imglist->next);
+                    }
+                }
                 else
                     retval = false;
             }

commit 35f9a5b41501502d6bb21cad64be515a73bc360c

Skin Engine: Fix loading images with same file name
Looks like this was a regression introduced in 01cbb79.
The duplicate img uses an existing buflib handle for
the data, but still didn't have access to the dimensions
from the bitmap struct.

Test case: DancePuffDuo theme for Sansa E200. Only one
dancepuff was displayed. Thank you to goatikins for
reporting the issue.

Change-Id: I32c3ebd1f00738f7db52e7a66f98c4ab3489ee4a

apps/gui/skin_engine/skin_parser.c
@@ -2007,6 +2007,7 @@ static bool load_skin_bitmaps(struct wps_data *wps_data, char *bmpdir)
                 {
                     struct skin_token_list *imglist = SKINOFFSETTOPTR(skin_buffer, list->next);
                     img->subimage_height = img->bm.height / img->num_subimages;
+                    struct bitmap* loaded_bm = &img->bm;
                     while (imglist)
                     {
                         token = SKINOFFSETTOPTR(skin_buffer, imglist->token);
@@ -2016,6 +2017,7 @@ static bool load_skin_bitmaps(struct wps_data *wps_data, char *bmpdir)
                             {
                                 img->loaded = true;
                                 img->buflib_handle = handle;
+                                img->bm = *loaded_bm;
                                 img->subimage_height = img->bm.height / img->num_subimages;
                             }
                         }