Rockbox/rockbox

commit 034b6d5bfb5bdedaf843079a02ff6df31488f394

Remove some shadowed variables identified by -Wshadow
there are plenty more but these are the low hanging fruit

Change-Id: I86d9f4f56e8cd9b381d1bf6a6679cd58c6a4004d

firmware/drivers/lcd-color-common.c
@@ -242,6 +242,8 @@ void lcd_blit_yuv(unsigned char * const src[3],
                   int x, int y, int width, int height)
 {
     const unsigned char *ysrc, *usrc, *vsrc;
+    int cb, cr, rv, guv, bu, r, g, b;
+
     int linecounter;
     fb_data *dst, *row_end;
     long z;
@@ -272,8 +274,6 @@ void lcd_blit_yuv(unsigned char * const src[3],
     {
         do
         {
-            int y, cb, cr, rv, guv, bu, r, g, b;
-
             y  = YFAC*(*ysrc++ - 16);
             cb = *usrc++ - 128;
             cr = *vsrc++ - 128;
@@ -337,8 +337,6 @@ void lcd_blit_yuv(unsigned char * const src[3],
 
         do
         {
-            int y, cb, cr, rv, guv, bu, r, g, b;
-
             y  = YFAC*(*ysrc++ - 16);
             cb = *usrc++ - 128;
             cr = *vsrc++ - 128;

apps/cuesheet.c
@@ -65,7 +65,7 @@ static bool search_for_cuesheet(const char *path, struct cuesheet_file *cue_file
         strcpy(cuepath, CUE_DIR);
         if (strlcat(cuepath, slash, MAX_PATH) >= MAX_PATH)
             goto skip; /* overflow */
-        char *dot = strrchr(cuepath, '.');
+        dot = strrchr(cuepath, '.');
         strcpy(dot, ".cue");
         if (!file_exists(cuepath))
         {

apps/filetree.c
@@ -667,7 +667,7 @@ int ft_enter(struct tree_context* c)
                     break;
                 }
 
-                struct entry* file = tree_get_entry_at(c, c->selected_item);
+                file = tree_get_entry_at(c, c->selected_item);
                 if (!file)
                 {
                     splashf(HZ, str(LANG_READ_FAILED), str(LANG_UNKNOWN));

apps/gui/folder_select.c
@@ -208,7 +208,7 @@ static struct folder* load_folder(struct folder* parent, char *folder)
         if ((dn[0] == '.') && (dn[1] == '\0' || (dn[1] == '.' && dn[2] == '\0')))
             continue;
         /* copy entry name to end of buffer, save pointer */
-        int len = strlen((char *)entry->d_name);
+        len = strlen((char *)entry->d_name);
         char *name = folder_alloc_from_end(len+1); /*for NULL*/
         if (name == NULL)
         {

apps/menu.c
@@ -522,7 +522,6 @@ int do_menu(const struct menu_item_ex *start_menu, int *start_selected,
                     MENUITEM_STRINGLIST(notquickscreen_able_option,
                                         ID2P(LANG_ONPLAY_MENU_TITLE), NULL,
                                         ID2P(LANG_RESET_SETTING));
-                    const struct menu_item_ex *menu;
                     const struct settings_list *setting =
                             find_setting(temp->variable, NULL);
 #ifdef HAVE_QUICKSCREEN

apps/recorder/keyboard.c
@@ -549,8 +549,8 @@ int kbd_input(char* text, int buflen, unsigned short *kbd)
         {
             /* declare scoped pointers inside screen loops - hide the
                declarations from previous block level */
-            struct keyboard_parameters *pm = &param[l];
             struct screen *sc = &screens[l];
+            pm = &param[l];
             sc->clear_display();
             kbd_draw_picker(pm, sc, &state);
             kbd_draw_edit_line(pm, sc, &state);

apps/tagcache.c
@@ -4333,7 +4333,7 @@ static bool load_tagcache(void)
                 unsigned int searchflag = auto_update ? DCS_STORAGE_PATH :
                                                         DCS_CACHED_PATH;
 
-                int rc = dircache_search(searchflag | DCS_UPDATE_FILEREF,
+                rc = dircache_search(searchflag | DCS_UPDATE_FILEREF,
                                          &tcrc_dcfrefs[idx_id], filename);
                 if (rc > 0)           /* in cache and we have fileref */
                     idx->flag |= FLAG_DIRCACHE;

apps/tagtree.c
@@ -215,11 +215,11 @@ static int move_callback(int handle, void* current, void* new)
     /* loop over menus */
     for(int i = 0; i < menu_count; i++)
     {
-        struct menu_root* menu = menus[i];
+        struct menu_root* menuroot = menus[i];
         /* then over the menu_entries of a menu */
-        for(int j = 0; j < menu->itemcount; j++)
+        for(int j = 0; j < menuroot->itemcount; j++)
         {
-            struct menu_entry* mentry = menu->items[j];
+            struct menu_entry* mentry = menuroot->items[j];
             /* then over the search_instructions of each menu_entry */
             for(int k = 0; k < mentry->si.tagorder_count; k++)
             {
@@ -230,7 +230,7 @@ static int move_callback(int handle, void* current, void* new)
                     UPDATE(mentry->si.clause[k][l], diff);
                 }
             }
-            UPDATE(menu->items[j], diff);
+            UPDATE(menuroot->items[j], diff);
         }
         UPDATE(menus[i], diff);
     }

commit 6e90bfe029fc69ad5a3b0e34b60b8a50f9105c2c

lcd-color: Fix mpeg_player regression introduced in 034b6d5b
The lcd_blit_yuv() function shadowed a variable, but was "fixed" by
removing the inner declaration entirely, causing the inner loop to
clobber a variable used by the outer loop.

Fix this by declaring these variables independently, and for clarity
moved the inner loop declarations to a narrower scope.

Change-Id: I07652199929b84298b783374b8551a49baf093f3

firmware/drivers/lcd-color-common.c
@@ -239,10 +239,9 @@ __attribute__((weak))
 #endif
 void lcd_blit_yuv(unsigned char * const src[3],
                   int src_x, int src_y, int stride,
-                  int x, int y, int width, int height)
+                  int dst_x, int dst_y, int width, int height)
 {
     const unsigned char *ysrc, *usrc, *vsrc;
-    int cb, cr, rv, guv, bu, r, g, b;
 
     int linecounter;
     fb_data *dst, *row_end;
@@ -253,10 +252,10 @@ void lcd_blit_yuv(unsigned char * const src[3],
     linecounter = height >> 1;
 
 #if LCD_WIDTH >= LCD_HEIGHT
-    dst     = FBADDR(x, y);
+    dst     = FBADDR(dst_x, dst_y);
     row_end = dst + width;
 #else
-    dst     = FBADDR(LCD_WIDTH - y - 1, x);
+    dst     = FBADDR(LCD_WIDTH - dst_y - 1, dst_x);
     row_end = dst + LCD_WIDTH * width;
 #endif
 
@@ -272,6 +271,8 @@ void lcd_blit_yuv(unsigned char * const src[3],
 
     do
     {
+        int y, cb, cr, rv, guv, bu, r, g, b;
+
         do
         {
             y  = YFAC*(*ysrc++ - 16);
@@ -401,9 +402,9 @@ void lcd_blit_yuv(unsigned char * const src[3],
     while (--linecounter > 0);
 
 #if LCD_WIDTH >= LCD_HEIGHT
-    lcd_update_rect(x, y, width, height);
+    lcd_update_rect(dst_x, dst_y, width, height);
 #else
-    lcd_update_rect(LCD_WIDTH - y - height, x, height, width);
+    lcd_update_rect(LCD_WIDTH - dst_y - height, dst_x, height, width);
 #endif
 }
 