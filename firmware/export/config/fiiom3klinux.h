6e058fe77f48dc66a032de0a1e1cf495f85fe98f
Roman Artiukhin <bahusdrive@gmail.com>
2024-03-25 09:25:30 +0200

    platform: hiby: Increase CODEC_SIZE to 1 Mb
    Unifies codec size with most targets.
    
    Apart from other things increased size improves seek accuracy in long m4b audiobooks.
    
    Tested on Agptek Rocker and XDuoo X3II
    
    Related discussion: https://www.rockbox.org/tracker/task/13049#comment44591
    
    Change-Id: Id246969e78386b71ac66aace128cfba72a3f209e

--- firmware/export/config/fiiom3klinux.h
+++ firmware/export/config/fiiom3klinux.h
@@ -69,7 +69,7 @@
 #define CONFIG_RTC APPLICATION
 
 /* The number of bytes reserved for loadable codecs */
-#define CODEC_SIZE 0x80000
+#define CODEC_SIZE 0x100000
 
 /* The number of bytes reserved for loadable plugins */
 #define PLUGIN_BUFFER_SIZE 0x100000
