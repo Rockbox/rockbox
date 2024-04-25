04181bb832da8039994c320f6d4c370ceec98673
Solomon Peachy <pizza@shaftnet.org>
2024-04-25 13:31:40 -0400

    Fix sim build error with GCC14.
    This is a (thankfully harmless) long-standing callback prototype
    mismatch; I'm surprised this wasn't caught before!
    
    Change-Id: Idb3ebc6d7d4fd64841d0a10981c4bf1b10d192c7

--- apps/plugins/puzzles/rockbox.c
+++ apps/plugins/puzzles/rockbox.c
@@ -3612,8 +3612,11 @@ static void save_game(void)
     rb->lcd_update();
 }
 
-static int mainmenu_cb(int action, const struct menu_item_ex *this_item)
+static int mainmenu_cb(int action,
+                       const struct menu_item_ex *this_item,
+                       struct gui_synclist *this_list)
 {
+    (void)this_list;
     int i = (intptr_t) this_item;
     if(action == ACTION_REQUEST_MENUITEM)
     {
