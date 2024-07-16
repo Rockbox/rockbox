Rockbox/rockbox

commit 4b423e21f697afa3841ac4ae75d830e0d8c70c27

pp5020:  Use DMA Writes by default for SSDs
It is likely that these things accept data far faster than the spinning
rust drives ever would, and while there is a question about PIO timings,
DMA timings seem to be solid.

Change-Id: I70644e0ad85a6ed429c8704e66ca7af91d78765c

firmware/target/arm/pp/ata-pp5020.c
@@ -173,15 +173,13 @@ bool ata_dma_setup(void *addr, unsigned long bytes, bool write) {
         return false;
 
     /* Writes only need to be word-aligned, but by default DMA
-     * is not used for writing as it appears to be slower.
+     * is not used for writing on non-SSDs as it appears to be slower.
      */
-#ifdef ATA_DMA_WRITES
+    if (!ata_disk_isssd())
+        return false;
+
     if (write && ((unsigned long)addr & 3))
         return false;
-#else
-    if (write)
-        return false;
-#endif
 
 #if ATA_MAX_UDMA > 2
     if (dma_needs_boost && !dma_boosted) {

commit 9c53fa1b0ad35c314c5b18775856032fb200bcbb

pp5020:  It's okay to use DMA reads on non-SSDs
Fixes a regression introduced in 4b423e21f697af

Change-Id: Ief4a4a562840524b98ad5988999bac87586dec41

firmware/target/arm/pp/ata-pp5020.c
@@ -175,11 +175,12 @@ bool ata_dma_setup(void *addr, unsigned long bytes, bool write) {
     /* Writes only need to be word-aligned, but by default DMA
      * is not used for writing on non-SSDs as it appears to be slower.
      */
-    if (!ata_disk_isssd())
-        return false;
-
-    if (write && ((unsigned long)addr & 3))
-        return false;
+    if (write) {
+        if ((unsigned long)addr & 3)
+            return false;
+        if (!ata_disk_isssd())
+            return false;
+    }
 
 #if ATA_MAX_UDMA > 2
     if (dma_needs_boost && !dma_boosted) {