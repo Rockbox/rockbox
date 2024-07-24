Rockbox/rockbox

commit 89b4cd59ef1fcc1a83565dddde4127bb89e8e083

[BugFix] extra slashes in recording directory
the define for HOME_DIR has a single slash for native

hosted does or doesn't but native always has 2

Change-Id: I2ff546da8e422feb04027de164caf3747f6d9355

firmware/export/rbpaths.h
@@ -54,6 +54,8 @@
 
 #define RB_ROOT_VOL_HIDDEN(v)   (IF_MV_VOL(v) == 0)
 #define RB_ROOT_CONTENTS_DIR    "/" IF_MV("<0>")
+#define REC_BASE_DIR        HOME_DIR "Recordings"
+#define PLAYLIST_CATALOG_DEFAULT_DIR HOME_DIR "Playlists"
 
 #else /* APPLICATION */
 
@@ -67,13 +69,11 @@
 #define CODECS_DIR          ROCKBOX_LIBRARY_PATH "/rockbox/codecs"
 #endif
 
+#define REC_BASE_DIR        HOME_DIR "/Recordings"]
+#define PLAYLIST_CATALOG_DEFAULT_DIR HOME_DIR "/Playlists"
 #endif /* !APPLICATION || SAMSUNG_YPR0 */
 
-#define REC_BASE_DIR        HOME_DIR "/Recordings"
-#define PLAYLIST_CATALOG_DEFAULT_DIR HOME_DIR "/Playlists"
-
 #define LANG_DIR            ROCKBOX_DIR "/langs"
-
 #define PLUGIN_GAMES_DIR    PLUGIN_DIR "/games"
 #define PLUGIN_APPS_DIR     PLUGIN_DIR "/apps"
 #define PLUGIN_DEMOS_DIR    PLUGIN_DIR "/demos"

commit e8975cf85cda7d69f5bc39e3c6970ec4e231aa34

Fix typo inadvertantly introduced in 89b4cd59ef1
Didn't cause any compile problems becuase we have no APPLICATION
targets that support recording.

Change-Id: I84594481abaccde01646a491e7d9c6b3abc36954

firmware/export/rbpaths.h
@@ -69,7 +69,7 @@
 #define CODECS_DIR          ROCKBOX_LIBRARY_PATH "/rockbox/codecs"
 #endif
 
-#define REC_BASE_DIR        HOME_DIR "/Recordings"]
+#define REC_BASE_DIR        HOME_DIR "/Recordings"
 #define PLAYLIST_CATALOG_DEFAULT_DIR HOME_DIR "/Playlists"
 #endif /* !APPLICATION || SAMSUNG_YPR0 */
 