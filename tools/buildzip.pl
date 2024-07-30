4d26f559eedebe92439c296111c303a47feb36a8
Solomon Peachy <pizza@shaftnet.org>
2024-07-30 10:17:01 -0400

    fonts:  Rename COPYING to COPYING-fonts.txt in the fonts zip
    Change-Id: I45c4a768de5a3c9b57e632bf796f08246a035cc3

--- tools/buildzip.pl
+++ tools/buildzip.pl
@@ -380,7 +380,7 @@ sub buildzip {
         my $cmd = "$ROOT/tools/convbdf -f $ROOT/fonts/*bdf >/dev/null 2>&1";
         print($cmd."\n") if $verbose;
         system($cmd);
-        copy("$ROOT/fonts/COPYING", ".");
+        copy("$ROOT/fonts/COPYING", "COPYING-fonts.txt");
         chdir("../../");
 
         if($fonts < 2) {
