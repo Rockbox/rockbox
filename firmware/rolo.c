56dd7ead63c0ced711e86d9c69c1d632596760b9
Dana Conrad <dconrad@fastmail.com>
2024-08-24 00:52:03 +0000

    rolo: Write bootdata regardless of whether running from root or redirect
    As long as bootdata is valid, write it.
    
    Change-Id: I63114b835d43c800217d2b688f9f7e2c2fb34c15

--- firmware/rolo.c
+++ firmware/rolo.c
@@ -250,9 +250,8 @@ int rolo_load(const char* filename)
 
     err = LOAD_FIRMWARE(filebuf, filename, filebuf_size);
 #if defined(HAVE_BOOTDATA) && !defined(SIMULATOR)
-    /* write the bootdata as if rolo were the bootloader
-     * FIXME: this won't work for root redirect... */
-    if (!strcmp(filename, BOOTDIR "/" BOOTFILE) && boot_data_valid)
+    // Write bootdata as long as the existing bootdata is valid
+    if (boot_data_valid)
     {
         int volume = 0;
 
