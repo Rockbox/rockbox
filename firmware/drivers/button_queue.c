Rockbox/rockbox

commit da9d67a0fe7a4ee6858d58e21614ba605188b4d9

Button queue handling is split from main button driver
First half of
https://gerrit.rockbox.org/r/c/rockbox/+/570

Change-Id: Icc64dfd8194c18f69564ed5f8bf7dd70a4330eb9

firmware/drivers/button_queue.c
@@ -1,0 +1,185 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2002 by Daniel Stenberg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#include "config.h"
+#include "system.h"
+#include "kernel.h"
+#include "button.h"
+
+static struct event_queue button_queue SHAREDBSS_ATTR;
+static intptr_t button_data; /* data value from last message dequeued */
+
+#ifdef HAVE_ADJUSTABLE_CPU_FREQ
+static bool button_boosted = false;
+static long button_unboost_tick;
+#define BUTTON_UNBOOST_TMO HZ
+
+static void button_boost(bool state)
+{
+    if (state)
+    {
+        /* update the unboost time each button_boost(true) call */
+        button_unboost_tick = current_tick + BUTTON_UNBOOST_TMO;
+
+        if (!button_boosted)
+        {
+            button_boosted = true;
+            cpu_boost(true);
+        }
+    }
+    else if (!button_boosted && TIME_AFTER(current_tick, button_unboost_tick))
+    {
+        button_boosted = false;
+        cpu_boost(false);
+    }
+}
+
+static void button_queue_wait(struct queue_event *evp, int timeout)
+{
+    /* Loop once after wait time if boosted in order to unboost and wait the
+       full remaining time */
+    do
+    {
+        int ticks = timeout;
+
+        if (ticks == TIMEOUT_NOBLOCK)
+            ;
+        else if (ticks > 0)
+        {
+            if (button_boosted && ticks > BUTTON_UNBOOST_TMO)
+                ticks = BUTTON_UNBOOST_TMO;
+
+            timeout -= ticks;
+        }
+        else if (button_boosted) /* TIMEOUT_BLOCK (ticks < 0) */
+        {
+            ticks = BUTTON_UNBOOST_TMO;
+        }
+
+        queue_wait_w_tmo(&button_queue, evp, ticks);
+        if (evp->id != SYS_TIMEOUT)
+        {
+            /* GUI boost build gets immediate kick, otherwise at least 3
+               messages had to be there */
+        #ifndef HAVE_GUI_BOOST
+            if (queue_count(&button_queue) >= 2)
+        #endif
+                button_boost(true);
+
+            break;
+        }
+        button_boost(false);
+    }
+    while (timeout);
+}
+#else /* ndef HAVE_ADJUSTABLE_CPU_FREQ */
+static inline void button_queue_wait(struct queue_event *evp, int timeout)
+{
+    queue_wait_w_tmo(&button_queue, evp, timeout);
+}
+#endif /* HAVE_ADJUSTABLE_CPU_FREQ */
+
+void button_queue_post(long id, intptr_t data)
+{
+    queue_post(&button_queue, id, data);
+}
+
+void button_queue_post_remove_head(long id, intptr_t data)
+{
+    queue_remove_from_head(&button_queue, id);
+    queue_post(&button_queue, id, data);
+}
+
+bool button_queue_try_post(long button, int data)
+{
+#ifdef HAVE_TOUCHSCREEN
+    /* one can swipe over the scren very quickly,
+     * for this to work we want to forget about old presses and
+     * only respect the very latest ones */
+    const bool force_post = true;
+#else
+    /* Only post events if the queue is empty,
+     * to avoid afterscroll effects.
+     * i.e. don't post new buttons if previous ones haven't been
+     * processed yet - but always post releases */
+    const bool force_post = button & BUTTON_REL;
+#endif
+
+    if (!queue_empty(&button_queue))
+    {
+        if (force_post)
+            queue_remove_from_head(&button_queue, button);
+        else
+            return false;
+    }
+
+    queue_post(&button_queue, button, data);
+
+    /* on touchscreen we posted unconditionally */
+    return true;
+}
+
+int button_queue_count(void)
+{
+    return queue_count(&button_queue);
+}
+
+bool button_queue_empty(void)
+{
+    return queue_empty(&button_queue);
+}
+
+bool button_queue_full(void)
+{
+    return queue_full(&button_queue);
+}
+
+void button_clear_queue(void)
+{
+    queue_clear(&button_queue);
+}
+
+long button_get_w_tmo(int ticks)
+{
+    struct queue_event ev;
+    button_queue_wait(&ev, ticks);
+
+    if (ev.id == SYS_TIMEOUT)
+        ev.id = BUTTON_NONE;
+    else
+        button_data = ev.data;
+
+    return ev.id;
+}
+
+long button_get(bool block)
+{
+    return button_get_w_tmo(block ? TIMEOUT_BLOCK : TIMEOUT_NOBLOCK);
+}
+
+intptr_t button_get_data(void)
+{
+    return button_data;
+}
+
+void INIT_ATTR button_queue_init(void)
+{
+    queue_init(&button_queue, true);
+}

apps/gui/bitmap/list.c
@@ -694,7 +694,7 @@ static int kinetic_callback(struct timeout *tmo)
 
     /* let get_action() timeout, which loads to a
      * gui_synclist_draw() call from the main thread */
-    queue_post(&button_queue, BUTTON_REDRAW, 0);
+    button_queue_post(BUTTON_REDRAW, 0);
     /* stop if the velocity hit or crossed zero */
     if (!data->velocity)
     {

apps/gui/statusbar-skinned.c
@@ -198,7 +198,7 @@ void do_sbs_update_callback(unsigned short id, void *param)
     skin_request_full_update(CUSTOM_STATUSBAR);
     force_waiting = true;
     /* force timeout in wps main loop, so that the update is instantly */
-    queue_post(&button_queue, BUTTON_NONE, 0);
+    button_queue_post(BUTTON_NONE, 0);
 }
 
 void sb_skin_set_update_delay(int delay)

apps/gui/wps.c
@@ -514,7 +514,7 @@ static void wps_lcd_activation_hook(unsigned short id, void *param)
     (void)param;
     skin_request_full_update(WPS);
     /* force timeout in wps main loop, so that the update is instantly */
-    queue_post(&button_queue, BUTTON_NONE, 0);
+    button_queue_post(BUTTON_NONE, 0);
 }
 #endif
 

apps/plugin.c
@@ -249,7 +249,7 @@ static const struct plugin_api rockbox_api = {
     lcd_set_mode,
 #endif
 #if defined(HAVE_LCD_ENABLE) || defined(HAVE_LCD_SLEEP)
-    &button_queue,
+    button_queue_post,
 #endif
     bidi_l2v,
     is_diacritic,
@@ -448,6 +448,7 @@ static const struct plugin_api rockbox_api = {
     talk_idarray,
     talk_file,
     talk_file_or_spell,
+    talk_fullpath,
     talk_dir_or_spell,
     talk_number,
     talk_value_decimal,
@@ -840,8 +841,6 @@ static const struct plugin_api rockbox_api = {
 
     /* new stuff at the end, sort into place next time
        the API gets incompatible */
-
-    talk_fullpath,
 };
 
 static int plugin_buffer_handle;

apps/plugin.h
@@ -163,7 +163,7 @@ int plugin_open(const char *plugin, const char *parameter);
  * when this happens please take the opportunity to sort in
  * any new functions "waiting" at the end of the list.
  */
-#define PLUGIN_API_VERSION 271
+#define PLUGIN_API_VERSION 272
 
 /* 239 Marks the removal of ARCHOS HWCODEC and CHARCELL */
 
@@ -282,7 +282,7 @@ struct plugin_api {
 #endif
 
 #if defined(HAVE_LCD_ENABLE) || defined(HAVE_LCD_SLEEP)
-    struct event_queue *button_queue;
+    void (*button_queue_post)(long id, intptr_t data);
 #endif
     unsigned short *(*bidi_l2v)( const unsigned char *str, int orientation );
     bool (*is_diacritic)(const unsigned short char_code, bool *is_rtl);
@@ -511,6 +511,7 @@ struct plugin_api {
                      const char *ext, const long *prefix_ids, bool enqueue);
     int (*talk_file_or_spell)(const char *dirname, const char* filename,
                               const long *prefix_ids, bool enqueue);
+    int (*talk_fullpath)(const char* path, bool enqueue);
     int (*talk_dir_or_spell)(const char* filename,
                              const long *prefix_ids, bool enqueue);
     int (*talk_number)(long n, bool enqueue);
@@ -977,8 +978,6 @@ struct plugin_api {
 #endif
     /* new stuff at the end, sort into place next time
        the API gets incompatible */
-
-    int (*talk_fullpath)(const char* path, bool enqueue);
 };
 
 /* plugin header */

apps/plugins/mpegplayer/mpeg_settings.c
@@ -772,7 +772,7 @@ static void get_start_time_lcd_enable_hook(unsigned short id, void *param)
 {
     (void)id;
     (void)param;
-    rb->queue_post(rb->button_queue, LCD_ENABLE_EVENT_0, 0);
+    rb->button_queue_post(LCD_ENABLE_EVENT_0, 0);
 }
 #endif /* HAVE_LCD_ENABLE */
 

apps/plugins/mpegplayer/mpegplayer.c
@@ -1208,7 +1208,7 @@ static void osd_lcd_enable_hook(unsigned short id, void* param)
 {
     (void)id;
     (void)param;
-    rb->queue_post(rb->button_queue, LCD_ENABLE_EVENT_1, 0);
+    rb->button_queue_post(LCD_ENABLE_EVENT_1, 0);
 }
 #endif
 

commit 8bd62e35ccd41399d6fb2f10ad16b7128f5bf8fe

button_queue: fix cpu never unboosted
regression introduced in da9d67a

Change-Id: Id1496873f1ab72fecc225dcfc78e633eea441089

firmware/drivers/button_queue.c
@@ -48,7 +48,7 @@ static void button_boost(bool state)
             cpu_boost(true);
         }
     }
-    else if (!button_boosted && TIME_AFTER(current_tick, button_unboost_tick))
+    else if (button_boosted && TIME_AFTER(current_tick, button_unboost_tick))
     {
         button_boosted = false;
         cpu_boost(false);