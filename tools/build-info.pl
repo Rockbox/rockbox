7d51b14cca187cb17f68acad9ff3e221ef275774
Solomon Peachy <pizza@shaftnet.org>
2025-04-01 07:35:12 -0400

    tools:  Alter the 'voice_url' in the [release] build-info
    Now it's consistent with everything else.  All old files have been renamed.
    
    Change-Id: I5c6974fb30cc90a7aedb732d8f970800c7de74c4

--- tools/build-info.pl
+++ tools/build-info.pl
@@ -14,7 +14,7 @@ my $baseurl="https://download.rockbox.org";
 
 print "[release]\n";
 print "build_url=$baseurl/release/%VERSION%/rockbox-%TARGET%-%VERSION%.zip\n";
-print "voice_url=$baseurl/release/%VERSION%/%TARGET%-%VERSION%-%LANGUAGE%.zip\n";
+print "voice_url=$baseurl/release/%VERSION%/voice-%TARGET%-%VERSION%-%LANGUAGE%.zip\n";
 print "manual_url=$baseurl/release/%VERSION%/rockbox-%TARGET%-%VERSION%%FORMAT%\n";
 print "font_url=$baseurl/release/%VERSION%/rockbox-fonts-%VERSION%.zip\n";
 print "source_url=$baseurl/release/%VERSION%/rockbox-source-%VERSION%.7z\n";
