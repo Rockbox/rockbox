4bdbd0785b5376362919d62677a82ae7477f87d3
Solomon Peachy <pizza@shaftnet.org>
2025-04-01 17:26:08 -0400

    tools:  Update release script so that it generates 7z archives
    Change-Id: Icc41a54af590d71a97baff16ad1c0124b26b0d0c

--- tools/release/sources.sh
+++ tools/release/sources.sh
@@ -10,4 +10,9 @@ outdir="output/source"
 
 mkdir -p "${outdir}"
 git archive --prefix=rockbox-$version/ -o "${outdir}/rockbox-source-${version}.tar" ${tag}
-xz -f "${outdir}/rockbox-source-${version}.tar"
+# either compress with xz... or...
+#xz -f "${outdir}/rockbox-source-${version}.tar"
+# recompress with 7zip
+tar -xf "${outdir}/rockbox-source-${version}.tar"
+7z a -mx=9 "${outdir}/rockbox-source-${version}.7z" "rockbox-${version}"
+rm -Rf "rockbox-${version}" "${outdir}/rockbox-source-${version}.tar"
