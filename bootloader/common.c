Rockbox/rockbox

commit 888ce7cae80f9ae77aab7f033636e578889573b0

disk:  Dump partition tables when we can't find a usable partition
Change-Id: I10a1b4f149657da4292f34ac8a14e58ec6cdcf72

bootloader/common.c
@@ -43,6 +43,7 @@
 #else
 #include "rb-loader.h"
 #endif
+#include "disk.h"
 
 /* TODO: Other bootloaders need to be adjusted to set this variable to true
    on a button press - currently only the ipod, H10, Vibe 500, Sansa, and xDuoo x3 versions do. */
@@ -113,10 +114,17 @@ void error(int errortype, int error, bool shutdown)
         printf("ATA error: %d", error);
         break;
 
-    case EDISK:
+    case EDISK: {
+        struct partinfo pinfo;
         printf("No partition found");
+        for (int i = 0 ; i < NUM_VOLUMES ; i++) {
+            disk_partinfo(i, &pinfo);
+            if (pinfo.type)
+                printf("P%d T%02x S%08lx",
+                       i, pinfo.type, pinfo.size);
+        }
         break;
-
+    }
     case EBOOTFILE:
         printf(loader_strerror(error));
         break;

apps/main.c
@@ -636,16 +636,24 @@ static void init(void)
         CHART("<disk_mount_all");
         if (rc<=0)
         {
+            int line=0;
             lcd_clear_display();
-            lcd_puts(0, 0, "No partition");
-            lcd_putsf(0, 1, "found (%d).", rc);
+            lcd_putsf(0, line++, "No partition found (%d).", rc);
 #ifndef USB_NONE
-            lcd_puts(0, 2, "Insert USB cable");
-            lcd_puts(0, 3, "and fix it.");
+            lcd_puts(0, line++, "Insert USB cable");
+            lcd_puts(0, line++, "and fix it.");
 #elif !defined(DEBUG) && !(CONFIG_STORAGE & STORAGE_RAMDISK)
-            lcd_puts(0, 2, "Rebooting in 5s");
+            lcd_puts(0, line++, "Rebooting in 5s");
 #endif
-            lcd_puts(0, 4, rbversion);
+            lcd_puts(0, line++, rbversion);
+
+            struct partinfo pinfo;
+            for (int i = 0 ; i < NUM_VOLUMES ; i++) {
+                disk_partinfo(i, &pinfo);
+                if (pinfo.type)
+                    lcd_putsf(0, line++, "P%d T%02x S%08lx",
+                              i, pinfo.type, pinfo.size);
+            }
             lcd_update();
 
 #if defined(MAX_VIRT_SECTOR_SIZE) && defined(DEFAULT_VIRT_SECTOR_SIZE)

bootloader/ipod-s5l87xx.c
@@ -476,7 +476,7 @@ static void run_of(void)
         printf("Booting OF in %d...", tmo);
         sleep(HZ*1);
     }
-    
+
     int rc = kernel_launch_onb();
     printf("Load OF error: %d", rc);
     sleep(HZ*10);
@@ -754,7 +754,14 @@ void main(void)
 
     rc = disk_mount_all();
     if (rc <= 0) {
+        struct partinfo pinfo;
         printf("No partition found");
+        for (int i = 0 ; i < NUM_VOLUMES ; i++) {
+            disk_partinfo(i, &pinfo);
+            if (pinfo.type)
+                printf("P%d T%02x S%08lx",
+                       i, pinfo.type, pinfo.size);
+        }
         fatal_error(ERR_RB);
     }
 

bootloader/ipod.c
@@ -364,13 +364,16 @@ void* main(void)
     rc = disk_mount_all();
     if (rc<=0)
     {
+        for (int i = 0 ; i < NUM_VOLUMES ; i++) {
+            disk_partinfo(i, &pinfo);
+            if (pinfo.type)
+                printf("P%d T%02x S%08lx",
+                       i, pinfo.type, pinfo.size);
+        }
+
         printf("No partition found");
         fatal_error();
     }
-
-    disk_partinfo(1, &pinfo);
-    printf("Partition 1: 0x%02x %ld sectors",
-           pinfo.type, pinfo.size);
 
     if (button_was_held || (btn==BUTTON_MENU)) {
         /* If either the hold switch was on, or the Menu button was held, then

bootloader/iriver_h1x0.c
@@ -561,8 +561,15 @@ void main(void)
     rc = disk_mount_all();
     if (rc<=0)
     {
+        struct partinfo pinfo;
         reset_screen();
         printf("No partition found");
+        for (int i = 0 ; i < NUM_VOLUMES ; i++) {
+            disk_partinfo(i, &pinfo);
+            if (pinfo.type)
+                printf("P%d T%02x S%08lx",
+                       i, pinfo.type, pinfo.size);
+        }
         while(button_get(true) != SYS_USB_CONNECTED) {};
     }
 

bootloader/iriver_h300.c
@@ -621,8 +621,15 @@ void main(void)
     rc = disk_mount_all();
     if (rc<=0)
     {
+        struct partinfo pinfo;
         reset_screen();
         printf("No partition found");
+        for (int i = 0 ; i < NUM_VOLUMES ; i++) {
+            disk_partinfo(i, &pinfo);
+            if (pinfo.type)
+                printf("P%d T%02x S%08lx",
+                       i, pinfo.type, pinfo.size);
+        }
         while(button_get(true) != SYS_USB_CONNECTED) {};
     }
 

commit 9e79ebf78d81aa9bea8dc74b820245affd75affd

bootloader: Fix red on hosted bootloaders
Fixes: 888ce7cae80f9ae77aab7f033636e578889573b0
Change-Id: I802779cf158634d5b3b547d74999798cb56d2882

bootloader/common.c
@@ -115,14 +115,16 @@ void error(int errortype, int error, bool shutdown)
         break;
 
     case EDISK: {
-        struct partinfo pinfo;
         printf("No partition found");
+#if !(CONFIG_PLATFORM & PLATFORM_HOSTED)
         for (int i = 0 ; i < NUM_VOLUMES ; i++) {
+            struct partinfo pinfo;
             disk_partinfo(i, &pinfo);
             if (pinfo.type)
                 printf("P%d T%02x S%08lx",
                        i, pinfo.type, pinfo.size);
         }
+#endif
         break;
     }
     case EBOOTFILE: