Rockbox/rockbox

commit cde144233b8163dec69a86a757a0d37801ffe14a

misc: Suppress -Wformat-security when building with GCC7
Making splash -> splashf means its arguments are now checked
at compile time, but the "format" is nearly
always one of our virtual pointers instead of a string/format
literal.

Our gcc494 (and upcoming gcc950) toolchains handle this fine,
but simulator builds with gcc7 complain about this, so suppress
the warning for those environments.

(GCC 12, GCC 14, and GCC15 all seem to be okay with this too)

Change-Id: Ifaf061f14e2552db73a7515f61950ad83116e8b5

firmware/include/gcc_extensions.h
@@ -24,7 +24,7 @@
 /* Support for some GCC extensions */
 
 /* Compile time check of format for printf/scanf like functions */
-#ifdef __GNUC__
+#ifdef __GNUC__ && __GNUC__ != 7
 #define ATTRIBUTE_PRINTF(fmt, arg1) __attribute__( ( format( printf, fmt, arg1 ) ) )
 #define ATTRIBUTE_SCANF(fmt, arg1) __attribute__( ( format( scanf, fmt, arg1 ) ) )
 #else

commit afcebf1b486e6b29c04638a15ea1c0afc1c6d45b

Fix sea of errors introduced in cde144233b8163dec69a86a757a0d37801ffe14a
I swear I compiled this successfully; clearly I had not.

Change-Id: Id604abeeeb895aa9c61acaad261b7190f0140089

firmware/include/gcc_extensions.h
@@ -24,7 +24,7 @@
 /* Support for some GCC extensions */
 
 /* Compile time check of format for printf/scanf like functions */
-#ifdef __GNUC__ && __GNUC__ != 7
+#if defined(__GNUC__) && (__GNUC__ != 7)
 #define ATTRIBUTE_PRINTF(fmt, arg1) __attribute__( ( format( printf, fmt, arg1 ) ) )
 #define ATTRIBUTE_SCANF(fmt, arg1) __attribute__( ( format( scanf, fmt, arg1 ) ) )
 #else