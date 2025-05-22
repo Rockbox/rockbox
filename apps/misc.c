Rockbox/rockbox

commit 15e52374698ba7395ff0ece0d3d70435a66406c4

storage:  64-bit sector offsets
 * Create new 'sector_t' type alias:
    * uint64_t for all targets with HAVE_LBA48 or HAVE_SDUC
    * unsigned long for the everything else
 * Alter all storage APIs to use sector_t instead of 'unsigned long'
 * Alter Volume/Partition/storage info structures to use sector_t
 * Disk cache converted to sector_t
 * ATA Core:
    * convert to using sector_t for sector addresses and drive sizes
    * Always fill out upper 16 bits of LBA48 addresses
    * IDENTIFY INFO is fixed at 512 bytes, not SECTOR_SIZE
 * USB mass storage:
    * convert to using sector_t for sector addesses and drive sizes
    * Implement READ_16/WRITE_16 for LBA48 addresses
 * Convert FAT code to use sector_t for all sector references
 * output_dyn_value() now accepts int64_t instead of 'int'
 * Corrected "rockbox info" to work for (MULTIVOLUME & !MULTIDRIVE)
 * Better reporting of disk and (logical+physical) sector sizes in debug info
 * Detect SDUC cards and report on storage debug_info screen

To-do: SDUC

 * Refactor SD core to remove duplicate code in every driver
   * Card probe and init state machine
 * Implement core SDUC support
   * SD2.0 needs to be 2.0+ (fixed for jz47xx and x1000)
   * Host and Card ID (ACMD41)
   * 32-bit addressing for all read/write/erase operations (CMD22)
 * ADD SDUC to target device drivers, defining HAVE_SDUC as appropriate

Change-Id: Ib0138781a0081664d11511037685503df1b93608

apps/misc.c
@@ -139,7 +139,7 @@ const unsigned char * const unit_strings_core[] =
  * voiced.*/
 char *output_dyn_value(char *buf,
                        int buf_size,
-                       int value,
+                       int64_t value,
                        const unsigned char * const *units,
                        unsigned int unit_count,
                        bool binary_scale)
@@ -147,8 +147,9 @@ char *output_dyn_value(char *buf,
     unsigned int scale = binary_scale ? 1024 : 1000;
     unsigned int fraction = 0;
     unsigned int unit_no = 0;
-    unsigned int value_abs = (value < 0) ? -value : value;
+    uint64_t value_abs = (value < 0) ? -value : value;
     char tbuf[5];
+    int value2;
 
     while (value_abs >= scale && unit_no < (unit_count - 1))
     {
@@ -157,7 +158,7 @@ char *output_dyn_value(char *buf,
         unit_no++;
     }
 
-    value = (value < 0) ? -value_abs : value_abs; /* preserve sign */
+    value2 = (value < 0) ? -value_abs : value_abs; /* preserve sign */
     fraction = (fraction * 1000 / scale) / 10;
 
     if (value_abs >= 100 || fraction >= 100 || !unit_no)
@@ -170,10 +171,10 @@ char *output_dyn_value(char *buf,
     if (buf)
     {
         if (*tbuf)
-            snprintf(buf, buf_size, "%d%s%s%s", value, str(LANG_POINT),
+            snprintf(buf, buf_size, "%d%s%s%s", value2, str(LANG_POINT),
                      tbuf, P2STR(units[unit_no]));
         else
-            snprintf(buf, buf_size, "%d%s", value, P2STR(units[unit_no]));
+            snprintf(buf, buf_size, "%d%s", value2, P2STR(units[unit_no]));
     }
     else
     {
@@ -1851,7 +1852,7 @@ enum current_activity get_current_activity(void)
 * ** Extended error info truth table **
 *  [ Handle ][buf_reqd]
 *  [  > 0   ][  > 0   ] buf_reqd indicates how many bytes were used
-*  [ALOC_ERR][  > 0   ] buf_reqd indicates how many bytes are needed 
+*  [ALOC_ERR][  > 0   ] buf_reqd indicates how many bytes are needed
 *  [ALOC_ERR][READ_ERR] there was an error reading the file or it is empty
 */
 int core_load_bmp(const char * filename, struct bitmap *bm, const int bmformat,

apps/debug_menu.c
@@ -557,14 +557,16 @@ static const char* dbg_partitions_getname(int selected_item, void *data,
     if (!disk_partinfo(partition, &p))
         return buffer;
 
+    // XXX fix this up to use logical sector size
+    // XXX and if mounted, show free info...
     if (selected_item%2)
     {
-        snprintf(buffer, buffer_len, "   T:%x %ld MB", p.type,
-                 p.size / ( 2048 / ( SECTOR_SIZE / 512 )));
+        snprintf(buffer, buffer_len, "   T:%x %llu MB", p.type,
+                 (uint64_t)(p.size / ( 2048 / ( SECTOR_SIZE / 512 ))));
     }
     else
     {
-        snprintf(buffer, buffer_len, "P%d: S:%lx", partition, p.start);
+        snprintf(buffer, buffer_len, "P%d: S:%llx", partition, (uint64_t)p.start);
     }
     return buffer;
 }
@@ -572,7 +574,7 @@ static const char* dbg_partitions_getname(int selected_item, void *data,
 static bool dbg_partitions(void)
 {
     struct simplelist_info info;
-    simplelist_info_init(&info, "Partition Info", NUM_DRIVES * 4, NULL);
+    simplelist_info_init(&info, "Partition Info", NUM_DRIVES * MAX_PARTITIONS_PER_DRIVE, NULL);
     info.selection_size = 2;
     info.scroll_all = true;
     info.get_name = dbg_partitions_getname;
@@ -1343,6 +1345,22 @@ static int disk_callback(int btn, struct gui_synclist *lists)
                     "R2W: *%d", card->r2w_factor);
 #if (CONFIG_STORAGE & STORAGE_SD)
             int csd_structure = card_extract_bits(card->csd, 127, 2);
+            const char *ver;
+            switch(csd_structure) {
+            case 0:
+                    ver = "1 (SD)";
+                    break;
+            case 1:
+                    ver = "2 (SDHC/SDXC)";
+                    break;
+            case 2:
+                    ver = "3 (SDUC)";
+                    break;
+            default:
+                    ver = "Unknown";
+                    break;
+            }
+            simplelist_addline("SDVer: %s\n", ver);
             if (csd_structure == 0) /* CSD version 1.0 */
 #endif
             {
@@ -1407,15 +1425,41 @@ static int disk_callback(int btn, struct gui_synclist *lists)
     buf[8]=0;
     simplelist_addline(
              "Firmware: %s", buf);
-    snprintf(buf, sizeof buf, "%ld MB",
-             ((unsigned long)identify_info[61] << 16 |
-              (unsigned long)identify_info[60]) / 2048 );
+
+    uint64_t total_sectors = identify_info[60] | (identify_info[61] << 16);
+#ifdef HAVE_LBA48
+    if (identify_info[83] & 0x0400
+        && total_sectors == 0x0FFFFFFF)
+        total_sectors = identify_info[100] | (identify_info[101] << 16) | ((uint64_t)identify_info[102] << 32) | ((uint64_t)identify_info[103] << 48);
+#endif
+
+    uint32_t sector_size;
+
+    /* Logical sector size > 512B ? */
+    if ((identify_info[106] & 0xd000) == 0x5000)
+        sector_size = identify_info[117] | (identify_info[118] << 16);
+    else
+        sector_size = SECTOR_SIZE;
+
+    total_sectors *= sector_size;   /* Convert to bytes */
+    total_sectors /= (1024 * 1024); /* Convert to MB */
+
+    simplelist_addline("Size: %llu MB", total_sectors);
+    simplelist_addline("Logical sector size: %u B", sector_size);
+
+    if((identify_info[106] & 0xe000) == 0x6000)
+        sector_size *= BIT_N(identify_info[106] & 0x000f);
     simplelist_addline(
-             "Size: %s", buf);
-    unsigned long free;
+            "Physical sector size: %d B", sector_size);
+
+#ifndef HAVE_MULTIVOLUME
+    // XXX this needs to be fixed for multi-volume setups
+    sector_t free;
     volume_size( IF_MV(0,) NULL, &free );
     simplelist_addline(
-             "Free: %ld MB", free / 1024);
+             "Free: %llu MB", free / 1024);
+#endif
+
     simplelist_addline("SSD detected: %s", ata_disk_isssd() ? "yes" : "no");
     simplelist_addline(
              "Spinup time: %d ms", storage_spinup_time() * (1000/HZ));
@@ -1452,11 +1496,7 @@ static int disk_callback(int btn, struct gui_synclist *lists)
         simplelist_addline(
                  "No timing info");
     }
-    int sector_size = 512;
-    if((identify_info[106] & 0xe000) == 0x6000)
-        sector_size *= BIT_N(identify_info[106] & 0x000f);
-    simplelist_addline(
-            "Physical sector size: %d", sector_size);
+
 #ifdef HAVE_ATA_DMA
     if (identify_info[63] & (1<<0)) {
         simplelist_addline(
@@ -1751,8 +1791,8 @@ static int disk_callback(int btn, struct gui_synclist *lists)
     simplelist_addline("Model: %s", info.product);
     simplelist_addline("Firmware: %s", info.revision);
     simplelist_addline(
-             "Size: %ld MB", info.num_sectors*(info.sector_size/512)/2024);
-    unsigned long free;
+            "Size: %lld MB", (uint64_t)(info.num_sectors*(info.sector_size/512)/2048));
+    storage_t free;
     volume_size( IF_MV(0,) NULL, &free );
     simplelist_addline(
              "Free: %ld MB", free / 1024);
@@ -1771,13 +1811,13 @@ static bool dbg_identify_info(void)
         const unsigned short *identify_info = ata_get_identify();
 #ifdef ROCKBOX_LITTLE_ENDIAN
         /* this is a pointer to a driver buffer so we can't modify it */
-        for (int i = 0; i < SECTOR_SIZE/2; ++i)
+        for (int i = 0; i < ATA_IDENTIFY_WORDS; ++i)
         {
             unsigned short word = swap16(identify_info[i]);
             write(fd, &word, 2);
         }
 #else
-        write(fd, identify_info, SECTOR_SIZE);
+        write(fd, identify_info, ATA_IDENTIFY_WORDS*2);
 #endif
         close(fd);
     }

apps/menus/main_menu.c
@@ -134,8 +134,8 @@ static int show_legal(void)
 struct info_data
 
 {
-    unsigned long size[NUM_VOLUMES];
-    unsigned long free[NUM_VOLUMES];
+    sector_t size[NUM_VOLUMES];
+    sector_t free[NUM_VOLUMES];
     unsigned long name[NUM_VOLUMES];
     bool new_data;
 };
@@ -162,16 +162,19 @@ enum infoscreenorder
 */
 static int refresh_data(struct info_data *info)
 {
-    int i = 0;
+#ifdef HAVE_MULTIVOLUME
 #ifdef HAVE_MULTIDRIVE
-    int drive;
     int max = -1;
-
+#endif
+    int drive = 0;
+    int i = 0;
     for (i = 0 ; CHECK_VOL(i) ; i++) {
 #endif
 	volume_size(IF_MV(i,) &info->size[i], &info->free[i]);
+#ifdef HAVE_MULTIVOLUME
 #ifdef HAVE_MULTIDRIVE
 	drive = volume_drive(i);
+#endif
 	if (drive > 0 || info->size[i] == 0)
 	    info->name[i] = LANG_DISK_NAME_MMC;
 	else
@@ -182,9 +185,12 @@ static int refresh_data(struct info_data *info)
 	    max = drive;
 	else if (drive < max)
 	    break;
+#elif defined(HAVE_MULTIVOLUME)
+        if (volume_partition(i) == -1)
+            break;
+#endif
+#ifdef HAVE_MULTIVOLUME
     }
-#else
-    i++;
 #endif
 
     info->new_data = false;

apps/misc.h
@@ -1,10 +1,10 @@
 /***************************************************************************
- *             __________               __   ___.                  
- *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___  
- *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /  
- *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <   
- *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \  
- *                     \/            \/     \/    \/            \/ 
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
  * $Id$
  *
  * Copyright (C) 2002 by Daniel Stenberg
@@ -22,6 +22,7 @@
 #define MISC_H
 
 #include <stdbool.h>
+#include <stdint.h>
 #include <inttypes.h>
 #include "config.h"
 #include "screen_access.h"
@@ -36,7 +37,7 @@ extern const unsigned char * const unit_strings_core[];
  * voiced.*/
 char *output_dyn_value(char *buf,
                        int buf_size,
-                       int value,
+                       int64_t value,
                        const unsigned char * const *units,
                        unsigned int unit_count,
                        bool binary_scale);
@@ -113,9 +114,9 @@ bool warn_on_pl_erase(void);
 bool show_search_progress(bool init, int count, int current, int total);
 
 /* Read (up to) a line of text from fd into buffer and return number of bytes
- * read (which may be larger than the number of bytes stored in buffer). If 
- * an error occurs, -1 is returned (and buffer contains whatever could be 
- * read). A line is terminated by a LF char. Neither LF nor CR chars are 
+ * read (which may be larger than the number of bytes stored in buffer). If
+ * an error occurs, -1 is returned (and buffer contains whatever could be
+ * read). A line is terminated by a LF char. Neither LF nor CR chars are
  * stored in buffer.
  */
 int read_line(int fd, char* buffer, int buffer_size);

apps/plugin.h
@@ -649,7 +649,7 @@ struct plugin_api {
     int (*memcmp)(const void *s1, const void *s2, size_t n);
     char *(*strcasestr) (const char* phaystack, const char* pneedle);
     char* (*strtok_r)(char *ptr, const char *sep, char **end);
-    char* (*output_dyn_value)(char *buf, int buf_size, int value,
+    char* (*output_dyn_value)(char *buf, int buf_size, int64_t value,
                               const unsigned char * const *units,
                               unsigned int unit_count, bool binary_scale);
     /* unicode stuff */

firmware/common/disk.c
@@ -172,6 +172,8 @@ bool disk_init(IF_MD_NONVOID(int drive))
 	    }
 	}
 
+        // XXX backup GPT header at final LBA of drive...
+
 	while (is_gpt) {
 	    /* Re-start partition parsing using GPT */
 	    uint64_t part_lba;
@@ -243,20 +245,24 @@ reload:
                     goto skip; /* Any flag makes us ignore this */
                 }
                 tmp = BYTES2INT64(pptr, 32); /* FIRST LBA */
-                if (tmp > UINT32_MAX) { // XXX revisit when we resize struct partinfo!
-                    DEBUGF("GPT: partition starts after 2GiB mark\n");
+#ifndef STORAGE_64BIT_SECTOR
+                if (tmp > UINT32_MAX) {
+                    DEBUGF("GPT: partition starts after 2TiB mark\n");
                     goto skip;
                 }
+#endif
                 if (tmp < 34) {
                     DEBUGF("GPT: Invalid start LBA\n");
                     goto skip;
                 }
                 pinfo[part].start = tmp;
                 tmp = BYTES2INT64(pptr, 40); /* LAST LBA */
-                if (tmp > UINT32_MAX) { // XXX revisit when we resize struct partinfo!
-                    DEBUGF("GPT: partition ends after 2GiB mark\n");
+#ifndef STORAGE_64BIT_SECTOR
+                if (tmp > UINT32_MAX) {
+                    DEBUGF("GPT: partition ends after 2TiB mark\n");
                     goto skip;
                 }
+#endif
                 if (tmp <= pinfo[part].start) {
                     DEBUGF("GPT: Invalid end LBA\n");
                     goto skip;
@@ -264,7 +270,7 @@ reload:
                 pinfo[part].size = tmp - pinfo[part].start + 1;
                 pinfo[part].type = PARTITION_TYPE_FAT32_LBA;
 
-                DEBUGF("GPart%d: start: %08lx size: %08lx\n",
+                DEBUGF("GPart%d: start: %016lx size: %016lx\n",
                        part,pinfo[part].start,pinfo[part].size);
                 part++;
 
@@ -499,13 +505,13 @@ unsigned int volume_get_cluster_size(IF_MV_NONVOID(int volume))
     return clustersize;
 }
 
-void volume_size(IF_MV(int volume,) unsigned long *sizep, unsigned long *freep)
+void volume_size(IF_MV(int volume,) sector_t *sizep, sector_t *freep)
 {
     disk_reader_lock();
 
     if (!CHECK_VOL(volume) || !fat_size(IF_MV(volume,) sizep, freep))
     {
-        if (freep) *sizep = 0;
+        if (sizep) *sizep = 0;
         if (freep) *freep = 0;
     }
 

firmware/common/disk_cache.c
@@ -71,12 +71,12 @@ struct disk_cache_entry
 #ifdef HAVE_MULTIVOLUME
     unsigned char volume;   /* volume of sector */
 #endif
-    unsigned long sector;   /* cached disk sector number */
+    sector_t sector;   /* cached disk sector number */
 };
 
 BITARRAY_TYPE_DECLARE(cache_map_entry_t, cache_map, DC_NUM_ENTRIES)
 
-static inline unsigned int map_sector(unsigned long sector)
+static inline unsigned int map_sector(sector_t sector)
 {
     /* keep sector hash simple for now */
     return sector % DC_MAP_NUM_ENTRIES;
@@ -172,7 +172,7 @@ static inline void cache_discard_entry(struct disk_cache_entry *dce,
 /* search the cache for the specified sector, returning a buffer, either
    to the specified sector, if it exists, or a new/evicted entry that must
    be filled */
-void * dc_cache_probe(IF_MV(int volume,) unsigned long sector,
+void * dc_cache_probe(IF_MV(int volume,) sector_t sector,
                       unsigned int *flagsp)
 {
     unsigned int mapnum = map_sector(sector);
@@ -200,7 +200,7 @@ void * dc_cache_probe(IF_MV(int volume,) unsigned long sector,
     if (old_flags)
     {
         int old_volume = IF_MV_VOL(dce->volume);
-        unsigned long sector = dce->sector;
+        sector_t sector = dce->sector;
         unsigned int old_mapnum = map_sector(sector);
 
         if (old_flags & DCE_DIRTY)

firmware/drivers/ata.c
@@ -109,15 +109,15 @@ static long last_disk_activity = -1;
 static long power_off_tick = 0;
 #endif
 
-static unsigned long total_sectors;
+static sector_t total_sectors;
 static int multisectors; /* number of supported multisectors */
-static unsigned short identify_info[SECTOR_SIZE/2];
+static unsigned short identify_info[ATA_IDENTIFY_WORDS];
 
 #ifdef MAX_PHYS_SECTOR_SIZE
 
 struct sector_cache_entry {
     bool inuse;
-    unsigned long sectornum;  /* logical sector */
+    sector_t sectornum;  /* logical sector */
     unsigned char data[MAX_PHYS_SECTOR_SIZE];
 };
 /* buffer for reading and writing large physical sectors */
@@ -381,7 +381,7 @@ static ICODE_ATTR void copy_write_sectors(const unsigned char* buf,
 }
 #endif /* !ATA_OPTIMIZED_WRITING */
 
-static int ata_transfer_sectors(unsigned long start,
+static int ata_transfer_sectors(uint64_t start,
                                 int incount,
                                 void* inbuf,
                                 int write)
@@ -443,9 +443,9 @@ static int ata_transfer_sectors(unsigned long start,
             ATA_OUT8(ATA_NSECTOR, count & 0xff);
             ATA_OUT8(ATA_SECTOR, (start >> 24) & 0xff); /* 31:24 */
             ATA_OUT8(ATA_SECTOR, start & 0xff); /* 7:0 */
-            ATA_OUT8(ATA_LCYL, 0); /* 39:32 */
+            ATA_OUT8(ATA_LCYL, (start >> 32) & 0xff); /* 39:32 */
             ATA_OUT8(ATA_LCYL, (start >> 8) & 0xff); /* 15:8 */
-            ATA_OUT8(ATA_HCYL, 0); /* 47:40 */
+            ATA_OUT8(ATA_HCYL, (start >> 40) & 0xff); /* 47:40 */
             ATA_OUT8(ATA_HCYL, (start >> 16) & 0xff); /* 23:16 */
             ATA_OUT8(ATA_SELECT, SELECT_LBA | ata_device);
 #ifdef HAVE_ATA_DMA
@@ -592,7 +592,7 @@ static int ata_transfer_sectors(unsigned long start,
 
 #ifndef MAX_PHYS_SECTOR_SIZE
 int ata_read_sectors(IF_MD(int drive,)
-                     unsigned long start,
+                     sector_t start,
                      int incount,
                      void* inbuf)
 {
@@ -607,7 +607,7 @@ int ata_read_sectors(IF_MD(int drive,)
 }
 
 int ata_write_sectors(IF_MD(int drive,)
-                      unsigned long start,
+                      sector_t start,
                       int count,
                       const void* buf)
 {
@@ -623,7 +623,7 @@ int ata_write_sectors(IF_MD(int drive,)
 #endif /* ndef MAX_PHYS_SECTOR_SIZE */
 
 #ifdef MAX_PHYS_SECTOR_SIZE
-static int cache_sector(unsigned long sector)
+static int cache_sector(sector_t sector)
 {
     int rc;
 
@@ -652,7 +652,7 @@ static inline int flush_current_sector(void)
 }
 
 int ata_read_sectors(IF_MD(int drive,)
-                     unsigned long start,
+                     sector_t start,
                      int incount,
                      void* inbuf)
 {
@@ -718,7 +718,7 @@ int ata_read_sectors(IF_MD(int drive,)
 }
 
 int ata_write_sectors(IF_MD(int drive,)
-                      unsigned long start,
+                      sector_t start,
                       int count,
                       const void* buf)
 {
@@ -916,7 +916,7 @@ static int identify(void)
         return -2;
     }
 
-    for (i=0; i<SECTOR_SIZE/2; i++) {
+    for (i=0; i<ATA_IDENTIFY_WORDS; i++) {
         /* the IDENTIFY words are already swapped, so we need to treat
            this info differently that normal sector data */
         identify_info[i] = ATA_SWAP_IDENTIFY(ATA_IN16(ATA_DATA));
@@ -1269,10 +1269,8 @@ int STORAGE_INIT_ATTR ata_init(void)
         if (identify_info[83] & 0x0400       /* 48 bit address support */
             && total_sectors == 0x0FFFFFFF)  /* and disk size >= 128 GiB */
         {                                    /* (needs BigLBA addressing) */
-            if (identify_info[102] || identify_info[103])
-                panicf("Unsupported disk size: >= 2^32 sectors");
+            total_sectors = identify_info[100] | (identify_info[101] << 16) | ((uint64_t)identify_info[102] << 32) | ((uint64_t)identify_info[103] << 48);
 
-            total_sectors = identify_info[100] | (identify_info[101] << 16);
             lba48 = true; /* use BigLBA */
         }
 #endif /* HAVE_LBA48 */
@@ -1360,7 +1358,13 @@ void ata_get_info(IF_MD(int drive,)struct storage_info *info)
     (void)drive; /* unused for now */
 #endif
     int i;
-    info->sector_size = SECTOR_SIZE;
+
+    /* Logical sector size */
+    if ((identify_info[106] & 0xd000) == 0x5000)
+        info->sector_size = identify_info[117] | (identify_info[118] << 16);
+    else
+        info->sector_size = SECTOR_SIZE;
+
     info->num_sectors = total_sectors;
 
     src = (unsigned short*)&identify_info[27];

commit 83a8fc4c7e0255060df509147c6345b56caeb360

voice: output_dyn_value: fix scale
regression introduced in 15e5237

Change-Id: Ib107be08d2712b9c74e2795180ac8565c146af0b

apps/misc.c
@@ -178,7 +178,7 @@ char *output_dyn_value(char *buf,
     }
     else
     {
-        talk_fractional(tbuf, value, P2ID(units[unit_no]));
+        talk_fractional(tbuf, value2, P2ID(units[unit_no]));
     }
     return buf;
 }