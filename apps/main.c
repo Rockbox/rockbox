9d92d161d8535a41eff7cbb42f2cc3e131a38393
William Wilgus <wilgus.william@gmail.com>
2025-07-20 12:16:15 -0400

    Splash modified playername.txt on device startup
    if playername.txt is modified its now splashed on device start-up
    
    should be less confusing than having it in the WPS theme I hope..
    
    Change-Id: Ifcec491f75a0c977a98d26292509f4ca62d79dac

--- apps/main.c
+++ apps/main.c
@@ -201,6 +201,7 @@ int main(void)
 
 #if !defined(BOOTLOADER)
     allocate_playback_log();
+    static char playername[64];
     if (!file_exists(ROCKBOX_DIR"/playername.txt"))
     {
         int fd = open(ROCKBOX_DIR"/playername.txt", O_CREAT|O_WRONLY|O_TRUNC, 0666);
@@ -208,6 +209,17 @@ int main(void)
         {
             fdprintf(fd, "%s", MODEL_NAME);
             close(fd);
+        }
+    }
+    else
+    {
+        int fd = open(ROCKBOX_DIR"/playername.txt", O_RDONLY);
+        if(fd >= 0)
+        {
+            read_line(fd, playername, sizeof(playername));
+            close(fd);
+            if (strcmp(playername, MODEL_NAME) != 0)
+                splashf(HZ * 2, "%s", playername);
         }
     }
 #endif
