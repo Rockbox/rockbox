Rockbox/rockbox

commit 26c612f6c0e9e1f5ff4e25dc4dd6a2825bf878ba

FS#13348: Don't voice/spell out the file extension if it's not displayed
   (This only applies if there is no talk clip for the file)

Change-Id: I0f071313f480052ecf4b912919155a8f0c2430d1

apps/tree.c
@@ -275,8 +275,40 @@ static int tree_voice_cb(int selected_item, void * data)
     }
 
     /* spell name AFTER voicing filetype */
-    if (spell_name)
+    if (spell_name) {
+        /* Don't spell the extension if it's not displayed */
+        if (!is_dir) {
+            bool stripit;
+            switch(global_settings.show_filename_ext) {
+            case 0:
+                /* show file extension: off */
+                stripit = true;
+                break;
+            case 1:
+                /* show file extension: on */
+                stripit = false;
+                break;
+            case 2:
+                /* show file extension: only unknown types */
+                stripit = filetype_supported(attr);
+                break;
+            case 3:
+            default:
+                /* show file extension: only when viewing all */
+                stripit = (*(local_tc->dirfilter) != SHOW_ID3DB) &&
+                          (*(local_tc->dirfilter) != SHOW_ALL);
+                break;
+            }
+
+            if (stripit) {
+                char *ext = strrchr(name, '.');
+                if (ext)
+                    *ext = 0;
+            }
+        }
+
         talk_spell(name, true);
+    }
 
     return 0;
 }
@@ -455,11 +487,11 @@ static int update_dir(void)
                     }
                     icon = filetype_get_icon(ATTR_DIRECTORY);
                 }
-            }           
+            }
         }
     }
 
-    /* set title and icon, if nothing is set, clear the title 
+    /* set title and icon, if nothing is set, clear the title
      * with NULL and icon as NOICON as the list is reused */
     gui_synclist_set_title(list, title, icon);
 

commit 2ddcc9542366d0881917a3875e11fa4510c8f234

tagtree: Fix FS#13651 database track titles not voiced with extensions turned off
regression introduced in commit 26c612f

Change-Id: Id2328a9f0de1fefdfddc7572524cb22a0668af96

apps/tree.c
@@ -124,7 +124,7 @@ static struct entry *get_valid_entry(const char* funcname,
 
 static bool ext_stripit(bool isdir, int attr, int dirfilter)
 {
-    if(!isdir)
+    if((dirfilter != SHOW_ID3DB) && !isdir)
     {
         switch(global_settings.show_filename_ext)
         {
@@ -141,7 +141,7 @@ static bool ext_stripit(bool isdir, int attr, int dirfilter)
             case 3:
             default:
                 /* show file extension: only when viewing all */
-                return (dirfilter != SHOW_ID3DB && dirfilter != SHOW_ALL);
+                return (dirfilter != SHOW_ALL);
         }
     }
     return false;