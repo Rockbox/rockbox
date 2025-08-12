b40cf73d36d4d8dd45ed8f9d6e4786ec9b3a598a
Solomon Peachy <pizza@shaftnet.org>
2025-08-12 10:46:10 -0400

    doom:  Fix warning with RGNano Simulator
    Change-Id: I311c7afb203d00a229d79ce655f41b702634a03f

--- apps/plugins/doom/rockdoom.c
+++ apps/plugins/doom/rockdoom.c
@@ -202,11 +202,7 @@ const unsigned char versions_builtin[7][20] =
    "TNT"
 };
 
-#ifdef RG_NANO /* Path is a bit longer */
-const unsigned char wads_builtin[7][34] =
-#else
-const unsigned char wads_builtin[7][30] =
-#endif
+const unsigned char wads_builtin[7][35] =
 {
    GAMEBASE"doom1.wad",
    GAMEBASE"doom.wad",
