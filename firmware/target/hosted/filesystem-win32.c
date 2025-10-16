Rockbox/rockbox

commit a2c10f6189e078977db95e8493d79133d9b7c1e3

unicode:  Support characters beyond the first unicode plane
We used 16-bit variables to store the 'character code' everywhere but
this won't let us represent anything beyond U+FFFF.

This patch changes those variables to a custom type that can be 32 or 16
bits depending on the build, and adjusts numerous internal APIs and
datastructures to match.  This includes:

 * utf8decode() and friends
 * font manipulation, caching, rendering, and generation
 * on-screen keyboard
 * FAT filesystem (parsing and generating utf16 LFNs)
 * WIN32 simulator platform code

Note that this patch doesn't _enable_ >16bit unicode support; a followup
patch will turn that on for appropriate targets.

Appears to work on:

  * hosted linux, native, linux simulator in both 16/32-bit modes.

Needs testing on:

  * windows and macos simulator (16bit+32bit)

Change-Id: Iba111b27d2433019b6bff937cf1ebd2c4353a0e8

firmware/target/hosted/filesystem-win32.c
@@ -63,55 +63,78 @@ static void win32_last_error_errno(void)
 static HANDLE win32_open(const char *ospath);
 static int win32_stat(const char *ospath, LPBY_HANDLE_FILE_INFORMATION lpInfo);
 
-unsigned short * strcpy_utf8ucs2(unsigned short *buffer,
-                                 const unsigned char *utf8)
+static unsigned short * strcpy_utf8utf16(unsigned short *buffer,
+                                         const unsigned char *utf8)
 {
-    for (wchar_t *ucs2 = buffer;
-         ((utf8 = utf8decode(utf8, ucs2)), *ucs2); ucs2++);
+    for (wchar_t *ucs = buffer; *ucs ; ucs++) {
+        ucschar_t cp;
+        utf8 = utf8decode(utf8, &cp);
+#ifdef UNICODE32
+        if (cp > 0x10000) {
+            cp -= 0x10000;
+            *ucs++ = 0xd800 | (cp >> 10);
+            cp = 0xdc00 | (cp & 0x3ff);
+        }
+#endif
+        *ucs = cp;
+    }
     return buffer;
 }
 
-#if 0
-unsigned char * strcpy_ucs2utf8(unsigned char *buffer,
-                                const unsigned short *ucs2)
+#if 0 /* Unused in current code */
+static unsigned char * strcpy_utf16utf8(unsigned char *buffer,
+                                        const unsigned short *utf16buf)
 {
-    for (unsigned char *utf8 = buffer;
-         ((utf8 = utf8encode(*ucs2, utf8)), *ucs2); ucs2++);
+    unsigned char *utf8 = buffer;
+
+    /* windows is always LE */
+    const int le = 1;
+
+    while (*utf16buf) {
+        const unsigned char *utf16 = (const unsigned char *)utf16buf;
+        unsigned long ucs;
+        /* Check for a surrogate pair */
+        if (*(utf16 + le) >= 0xD8 && *(utf16 + le) < 0xE0) {
+            ucs = 0x10000 + ((utf16[1 - le] << 10) | ((utf16[le] - 0xD8) << 18)
+                  | utf16[2 + (1 - le)] | ((utf16[2 + le] - 0xDC) << 8));
+            utf16buf += 2;
+        } else {
+            ucs = utf16[le] << 8 | utf16[1 - le];
+            utf16buf++;
+        }
+        utf8 = utf8encode(ucs, utf8);
+    }
     return buffer;
 }
-
-size_t strlen_utf8ucs2(const unsigned char *utf8)
-{
-    /* This won't properly count multiword ucs2 so use the alternative
-       below for now which doesn't either */
-    size_t length = 0;
-    unsigned short ucschar[2];
-    for (unsigned char c = *utf8; c;
-         ((utf8 = utf8decode(utf8, ucschar)), c = *utf8))
-        length++;
-
-    return length;
-}
-#endif /* 0 */
-
-size_t strlen_utf8ucs2(const unsigned char *utf8)
-{
-    return utf8length(utf8);
-}
-
-size_t strlen_ucs2utf8(const unsigned short *ucs2)
+static size_t strlen_utf16utf8(const unsigned short *utf16buf)
 {
     size_t length = 0;
     unsigned char utf8char[4];
 
-    for (unsigned short c = *ucs2; c; (c = *++ucs2))
-        length += utf8encode(c, utf8char) - utf8char;
+    /* windows is always LE */
+    const int le = 1;
 
+    while (*utf16buf) {
+        const unsigned char *utf16 = (const unsigned char *)utf16buf;
+        unsigned long ucs;
+        /* Check for a surrogate pair */
+        if (*(utf16 + le) >= 0xD8 && *(utf16 + le) < 0xE0) {
+            ucs = 0x10000 + ((utf16[1 - le] << 10) | ((utf16[le] - 0xD8) << 18)
+                  | utf16[2 + (1 - le)] | ((utf16[2 + le] - 0xDC) << 8));
+            utf16buf += 2;
+        } else {
+            ucs = utf16[le] << 8 | utf16[1 - le];
+            utf16buf++;
+        }
+        length += utf8encode(ucs, utf8char) - utf8char;
+    }
     return length;
 }
+#endif
 
-size_t strlcpy_ucs2utf8(char *buffer, const unsigned short *ucs2,
-                        size_t bufsize)
+/* Note: Must be exported */
+size_t strlcpy_utf16utf8(char *buffer, const unsigned short *utf16,
+                         size_t bufsize)
 {
     if (!buffer)
         bufsize = 0;
@@ -119,12 +142,24 @@ size_t strlcpy_ucs2utf8(char *buffer, const unsigned short *ucs2,
     size_t length = 0;
     unsigned char utf8char[4];
 
-    for (unsigned short c = *ucs2; c; (c = *++ucs2))
+    unsigned long ucc;
+    while(*utf16)
     {
+        /* Check for a surrogate UTF16 pair */
+        if (*utf16 >= 0xd800 && *utf16 < 0xdc00 &&
+            *(utf16+1) >= 0xdc00 && *(utf16+1) < 0xe000) {
+            ucc = 0x10000 + (((*utf16 & 0x3ff) << 10) | (*(utf16+1) & 0x3ff));
+            utf16++;
+        } else {
+            ucc = *utf16;
+        }
+
         /* If the last character won't fit, this won't split it */
-        size_t utf8size = utf8encode(c, utf8char) - utf8char;
+        size_t utf8size = utf8encode(ucc, utf8char) - utf8char;
         if ((length += utf8size) < bufsize)
             buffer = mempcpy(buffer, utf8char, utf8size);
+
+        utf16++;
     }
 
     /* Above won't ever copy to very end */
@@ -134,44 +169,44 @@ size_t strlcpy_ucs2utf8(char *buffer, const unsigned short *ucs2,
     return length;
 }
 
-#define _toucs2(utf8) \
+#define _toutf16(utf8) \
     ({ const char *_utf8 = (utf8);         \
-       size_t _l = strlen_utf8ucs2(_utf8); \
+       size_t _l = utf16len_utf8(_utf8); \
        void *_buffer = alloca((_l + 1)*2); \
-       strcpy_utf8ucs2(_buffer, _utf8); })
+       strcpy_utf8utf16(_buffer, _utf8); })
 
-#define _toutf8(ucs2) \
-    ({ const char *_ucs2 = (ucs2);         \
-       size_t _l = strlen_ucs2utf8(_ucs2); \
+#define _toutf8(utf16) \
+    ({ const char *_ucs = (utf16);         \
+       size_t _l = strlen_utf16utf8(_ucs); \
        void *_buffer = alloca(_l + 1);     \
-       strcpy_ucs2utf8(_buffer, _ucs2); })
+       strcpy_utf16utf8(_buffer, _ucs); })
 
 int os_open(const char *ospath, int oflag, ...)
 {
-    return _wopen(_toucs2(ospath), oflag __OPEN_MODE_ARG);
+    return _wopen(_toutf16(ospath), oflag __OPEN_MODE_ARG);
 }
 
 int os_creat(const char *ospath, mode_t mode)
 {
-    return _wcreat(_toucs2(ospath), mode);
+    return _wcreat(_toutf16(ospath), mode);
 }
 
 int os_stat(const char *ospath, struct _stat *s)
 {
-    return _wstat(_toucs2(ospath), s);
+    return _wstat(_toutf16(ospath), s);
 }
 
 int os_remove(const char *ospath)
 {
-    return _wremove(_toucs2(ospath));
+    return _wremove(_toutf16(ospath));
 }
 
 int os_rename(const char *osold, const char *osnew)
 {
     int errnum = errno;
 
-    const wchar_t *wchosold = _toucs2(osold);
-    const wchar_t *wchosnew = _toucs2(osnew);
+    const wchar_t *wchosold = _toutf16(osold);
+    const wchar_t *wchosnew = _toutf16(osnew);
 
     int rc = _wrename(wchosold, wchosnew);
     if (rc < 0 && errno == EEXIST)
@@ -213,18 +248,18 @@ bool os_file_exists(const char *ospath)
 
 _WDIR * os_opendir(const char *osdirname)
 {
-    return _wopendir(_toucs2(osdirname));
+    return _wopendir(_toutf16(osdirname));
 }
 
 int os_mkdir(const char *ospath, mode_t mode)
 {
-    return _wmkdir(_toucs2(ospath));
+    return _wmkdir(_toutf16(ospath));
     (void)mode;
 }
 
 int os_rmdir(const char *ospath)
 {
-    return _wrmdir(_toucs2(ospath));
+    return _wrmdir(_toutf16(ospath));
 }
 
 int os_dirfd(_WDIR *osdirp)
@@ -288,7 +323,7 @@ static HANDLE win32_open(const char *ospath)
 {
     /* FILE_FLAG_BACKUP_SEMANTICS is required for this to succeed at opening
        a directory */
-    HANDLE h = CreateFileW(_toucs2(ospath), GENERIC_READ,
+    HANDLE h = CreateFileW(_toutf16(ospath), GENERIC_READ,
                            FILE_SHARE_READ | FILE_SHARE_WRITE |
                            FILE_SHARE_DELETE, NULL, OPEN_EXISTING,
                            FILE_FLAG_BACKUP_SEMANTICS, NULL);
@@ -479,7 +514,7 @@ void volume_size(IF_MV(int volume,) sector_t *sizep, sector_t *freep)
 
     char volpath[MAX_PATH];
     if (os_volume_path(IF_MV(volume, ) volpath, sizeof (volpath)) >= 0)
-        GetDiskFreeSpaceExW(_toucs2(volpath), &free, &size, NULL);
+        GetDiskFreeSpaceExW(_toutf16(volpath), &free, &size, NULL);
 
     if (sizep)
         *sizep = size.QuadPart / 1024;

apps/hosted/android/keyboard.c
@@ -82,7 +82,7 @@ static void kdb_init(void)
         sleep(HZ/10);
 }
 
-int kbd_input(char* text, int buflen, unsigned short *kbd)
+int kbd_input(char* text, int buflen, ucschar_t *kbd)
 {
 	(void)kbd;
     JNIEnv e            = *env_ptr;
@@ -107,7 +107,7 @@ int kbd_input(char* text, int buflen, unsigned short *kbd)
     e->DeleteLocalRef(env_ptr, str);
     e->DeleteLocalRef(env_ptr, ok_text);
     e->DeleteLocalRef(env_ptr, cancel_text);
-    
+
     return !accepted; /* return 0 on success */
 }
 

apps/keyboard.h
@@ -23,7 +23,7 @@
 
 /* '*kbd', same format as https://www.rockbox.org/wiki/LoadableKeyboardLayouts */
 
-int kbd_input(char* buffer, int buflen, unsigned short *kbd);
+int kbd_input(char* buffer, int buflen, ucschar_t *kbd);
 
 int load_kbd(unsigned char* filename);
 

apps/plugin.h
@@ -176,7 +176,7 @@ int plugin_open(const char *plugin, const char *parameter);
  * when this happens please take the opportunity to sort in
  * any new functions "waiting" at the end of the list.
  */
-#define PLUGIN_API_VERSION 273
+#define PLUGIN_API_VERSION 274
 
 /* 239 Marks the removal of ARCHOS HWCODEC and CHARCELL */
 
@@ -296,15 +296,15 @@ struct plugin_api {
 #if defined(HAVE_LCD_ENABLE) || defined(HAVE_LCD_SLEEP)
     void (*button_queue_post)(long id, intptr_t data);
 #endif
-    unsigned short *(*bidi_l2v)( const unsigned char *str, int orientation );
-    bool (*is_diacritic)(const unsigned short char_code, bool *is_rtl);
-    const unsigned char *(*font_get_bits)( struct font *pf, unsigned short char_code );
+    ucschar_t *(*bidi_l2v)(const unsigned char *str, int orientation);
+    bool (*is_diacritic)(const ucschar_t char_code, bool *is_rtl);
+    const unsigned char *(*font_get_bits)(struct font *pf, ucschar_t char_code);
     int (*font_load)(const char *path);
     void (*font_unload)(int font_id);
     struct font* (*font_get)(int font);
     int  (*font_getstringsize)(const unsigned char *str, int *w, int *h,
                                int fontnumber);
-    int (*font_get_width)(struct font* pf, unsigned short char_code);
+    int (*font_get_width)(struct font* pf, ucschar_t char_code);
     void (*screen_clear_area)(struct screen * display, int xstart, int ystart,
                               int width, int height);
     void (*gui_scrollbar_draw)(struct screen * screen, int x, int y,
@@ -667,7 +667,7 @@ struct plugin_api {
                               const unsigned char * const *units,
                               unsigned int unit_count, bool binary_scale);
     /* unicode stuff */
-    const unsigned char* (*utf8decode)(const unsigned char *utf8, unsigned short *ucs);
+    const unsigned char* (*utf8decode)(const unsigned char *utf8, ucschar_t *ucs);
     unsigned char* (*iso_decode)(const unsigned char *iso, unsigned char *utf8, int cp, int count);
     unsigned char* (*utf16LEdecode)(const unsigned char *utf16, unsigned char *utf8, int count);
     unsigned char* (*utf16BEdecode)(const unsigned char *utf16, unsigned char *utf8, int count);
@@ -923,7 +923,7 @@ struct plugin_api {
     int  (*rand)(void);
     void (*qsort)(void *base, size_t nmemb, size_t size,
                   int(*compar)(const void *, const void *));
-    int (*kbd_input)(char* buffer, int buflen, unsigned short *kbd);
+    int (*kbd_input)(char* buffer, int buflen, ucschar_t *kbd);
     struct tm* (*get_time)(void);
     struct tm * (*gmtime_r)(const time_t *timep, struct tm *tm);
 #if CONFIG_RTC

apps/plugins/announce_status.c
@@ -70,7 +70,7 @@ static const char keybd_layout[] =
  * - \n does not create a key, but it also consumes one element
  * - the final null terminator is equivalent to \n
  * - since sizeof includes the null terminator we don't need +1 for that. */
-static unsigned short kbd_buf[sizeof(keybd_layout)];
+static ucschar_t kbd_buf[sizeof(keybd_layout)];
 
 /****************** prototypes ******************/
 void print_scroll(char* string); /* implements a scrolling screen */
@@ -164,7 +164,7 @@ static void config_set_defaults(void)
     gAnnounce.announce_on = 0;
     gAnnounce.grouping = 0;
     gAnnounce.wps_fmt[0] = '\0';
-    gAnnounce.show_prompt = true; 
+    gAnnounce.show_prompt = true;
 }
 
 static void config_reset_voice(void)
@@ -250,7 +250,7 @@ static int announce_menu_cb(int action,
                             struct gui_synclist *this_list)
 {
     (void)this_item;
-    unsigned short* kbd_p;
+    ucschar_t *kbd_p;
 
     int selection = rb->gui_synclist_get_sel_pos(this_list);
 

apps/plugins/frotz/frotz.c
@@ -128,7 +128,7 @@ void wait_for_key()
         case PLA_EXIT:
             hot_key_quit();
             break;
-        
+
         case PLA_SELECT:
             return;
         }
@@ -159,7 +159,7 @@ zchar do_input(int timeout, bool show_cursor)
         {
         case PLA_EXIT:
             return ZC_HKEY_QUIT;
-        
+
         case PLA_CANCEL:
             menu_ret = menu();
             if (menu_ret != ZC_BAD)
@@ -174,7 +174,7 @@ zchar do_input(int timeout, bool show_cursor)
             return ZC_BAD;
 
         default:
-            if (timeout != TIMEOUT_BLOCK && 
+            if (timeout != TIMEOUT_BLOCK &&
                     !TIME_BEFORE(*rb->current_tick, timeout_at))
                 return ZC_TIME_OUT;
         }
@@ -185,7 +185,7 @@ zchar os_read_key(int timeout, bool show_cursor)
 {
     int r;
     char inputbuf[5];
-    short key;
+    ucschar_t key;
     zchar zkey;
 
     for(;;)
@@ -214,7 +214,7 @@ zchar os_read_line(int max, zchar *buf, int timeout, int width, int continued)
     char inputbuf[256];
     const char *in;
     char *out;
-    short key;
+    ucschar_t key;
     zchar zkey;
 
     for(;;)

apps/plugins/keyremap.c
@@ -206,8 +206,8 @@ static int prompt_filename(char *buf, size_t bufsz)
 {
 #define KBD_LAYOUT "abcdefghijklmnop\nqrstuvwxyz |()[]\n1234567890 /._-+\n\n" \
                    "\nABCDEFGHIJKLMNOP\nQRSTUVWXYZ |()[]\n1234567890 /._-+"
-    unsigned short kbd[sizeof(KBD_LAYOUT) + 10];
-    unsigned short *kbd_p = kbd;
+    ucschar_t kbd[sizeof(KBD_LAYOUT) + 10];
+    ucschar_t *kbd_p = kbd;
     if (!kbd_create_layout(KBD_LAYOUT, kbd, sizeof(kbd)))
         kbd_p = NULL;
 
@@ -1002,7 +1002,7 @@ next_line:
                 {
                     bufleft = bufsz - (pctx - filenamebuf);
                     ctx = -1;
-                    int ctx_x_flag_count = (LAST_CONTEXT_PLACEHOLDER 
+                    int ctx_x_flag_count = (LAST_CONTEXT_PLACEHOLDER
                                            * ARRAYLEN(context_flags));
 
                     for (int i=0;i < ctx_x_flag_count ;i++)
@@ -2058,7 +2058,7 @@ static void synclist_set(int id, int selected_item, int items, int sel_size)
     }
     else if (menu_id == MENU_ID(M_SETKEYS))
     {
-        keyset.view_columns = printcell_set_columns(&lists, NULL, 
+        keyset.view_columns = printcell_set_columns(&lists, NULL,
                                                   ACTVIEW_HEADER, Icon_Rockbox);
         printcell_enable(true);
         int curcol = printcell_get_column_selected();

apps/plugins/lib/grey_draw.c
@@ -199,8 +199,8 @@ void grey_hline(int x1, int x2, int y)
     /* nothing to draw? */
     if (y < _grey_info.clip_t || y >= _grey_info.clip_b ||
         x1 >= _grey_info.clip_r || x2 < _grey_info.clip_l)
-        return;  
-    
+        return;
+
     /* drawmode and optimisation */
     if (vp->drawmode & DRMODE_INVERSEVID)
     {
@@ -251,7 +251,7 @@ void grey_vline(int x, int y1, int y2)
     unsigned char *dst, *dst_end;
     void (*pfunc)(unsigned char *address);
     int dwidth;
-    
+
     /* direction flip */
     if (y2 < y1)
     {
@@ -264,7 +264,7 @@ void grey_vline(int x, int y1, int y2)
     if (x < _grey_info.clip_l || x >= _grey_info.clip_r ||
         y1 >= _grey_info.clip_b || y2 < _grey_info.clip_t)
         return;
-    
+
     /* clipping */
     if (y1 < _grey_info.clip_t)
         y1 = _grey_info.clip_t;
@@ -425,7 +425,7 @@ void grey_fillrect(int x, int y, int width, int height)
 
     if (height <= 0)
         return;
-    
+
     dwidth  = _grey_info.cb_width;
     dst     = &_grey_info.curbuffer[
                 _GREY_MULUQ(dwidth, _grey_info.vp->y - _grey_info.cb_y + y) +
@@ -653,8 +653,8 @@ void grey_gray_bitmap(const unsigned char *src, int x, int y, int width,
 /* Put a string at a given pixel position, skipping first ofs pixel columns */
 void grey_putsxyofs(int x, int y, int ofs, const unsigned char *str)
 {
-    int ch;
-    unsigned short *ucs;
+    ucschar_t ch;
+    ucschar_t *ucs;
     struct font* pf;
 
     if (_grey_info.clip_b <= _grey_info.clip_t)
@@ -680,7 +680,7 @@ void grey_putsxyofs(int x, int y, int ofs, const unsigned char *str)
         bits = rb->font_get_bits(pf, ch);
 
         grey_mono_bitmap_part(bits, ofs, 0, width, x, y, width - ofs, pf->height);
-        
+
         x += width - ofs;
         ofs = 0;
     }
@@ -709,7 +709,7 @@ void grey_ub_clear_display(void)
 #endif
 }
 
-/* Assembler optimised helper function for copying a single line to the 
+/* Assembler optimised helper function for copying a single line to the
  * greyvalue buffer. */
 void _grey_line1(int width, unsigned char *dst, const unsigned char *src,
                  const unsigned char *lut);
@@ -725,7 +725,7 @@ void grey_ub_gray_bitmap_part(const unsigned char *src, int src_x, int src_y,
     if ((width <= 0) || (height <= 0) || (x >= _grey_info.width)
         || (y >= _grey_info.height) || (x + width <= 0) || (y + height <= 0))
         return;
-        
+
     /* clipping */
     if (x < 0)
     {
@@ -744,7 +744,7 @@ void grey_ub_gray_bitmap_part(const unsigned char *src, int src_x, int src_y,
     if (y + height > _grey_info.height)
         height = _grey_info.height - y;
 
-    src += _GREY_MULUQ(stride, src_y) + src_x; /* move starting point */ 
+    src += _GREY_MULUQ(stride, src_y) + src_x; /* move starting point */
     yc = y;
     ye = y + height;
     dst = _grey_info.values + (x << _GREY_BSHIFT);
@@ -773,7 +773,7 @@ void grey_ub_gray_bitmap_part(const unsigned char *src, int src_x, int src_y,
         }
         while (src_row < src_end);
 #endif
-        
+
         src += stride;
     }
     while (++yc < ye);

commit 60ddb0257317656088bc16247d97512384ee3ea2

winsim:  Fix filesystem access
Corrects a regression introduced in

    a2c10f6189e078977db95e8493d79133d9b7c1e3

Change-Id: I9a27a6192ff6a9d1be7829d23d23dbf98b84feae

firmware/target/hosted/filesystem-win32.c
@@ -66,8 +66,8 @@ static int win32_stat(const char *ospath, LPBY_HANDLE_FILE_INFORMATION lpInfo);
 static unsigned short * strcpy_utf8utf16(unsigned short *buffer,
                                          const unsigned char *utf8)
 {
-    for (wchar_t *ucs = buffer; *ucs ; ucs++) {
-        ucschar_t cp;
+    ucschar_t cp = 0xffffffff;
+    for (wchar_t *ucs = buffer; cp ; ucs++) {
         utf8 = utf8decode(utf8, &cp);
 #ifdef UNICODE32
         if (cp > 0x10000) {