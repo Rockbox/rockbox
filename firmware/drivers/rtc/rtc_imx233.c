Rockbox/rockbox

commit 7f282b9280cbbee697572b1178d6a5ee5b142d97

imx233:  rtc:  Initialize PERSISTENT0 register
Prevents the rtc from breaking on power removal

Change-Id: I1e434285ccc27d864d2fd9454993d02cae32711d

firmware/target/arm/imx233/rtc-imx233.h
@@ -84,6 +84,7 @@ static inline void imx233_rtc_init(void)
     BF_CLR(RTC_CTRL, SFTRST);
     udelay(5);  /* only need 3 GPMI clocks (1us) */
     BF_CLR(RTC_CTRL, CLKGATE);
+    HW_RTC_PERSISTENT0_SET=0x082114;
     imx233_rtc_enable_watchdog(false);
 }
 

commit 297af3a483191869113fea7d2e5e2122e89354ab

Creative ZEN: fix RTC regression
Creative ZEN lost it's time when shutting down.
The bug was introduced with commit 7f282b9280cb (g#2601),
followed by e3f6e9d9f6e1 (g#2616).

I guarded all persistent register writes with wait loops,
as described in the datasheet.

TODO:
test SONY_NWZE360 and SONY_NWZE370 targets

Change-Id: Ib38ab8691fd1c6e8d0771c1e2eca20dfeb6fc87f

firmware/drivers/rtc/rtc_imx233.c
@@ -28,8 +28,8 @@
 
 #define YEAR1980    315532800   /* 1980/1/1 00:00:00 in UTC */
 
-#if defined(SANSA_FUZEPLUS) || defined(CREATIVE_ZENXFI3) || defined(SONY_NWZE360) || \
-    defined(SONY_NWZE370)
+#if defined(SANSA_FUZEPLUS) || defined(SONY_NWZE360) || defined(SONY_NWZE370) || \
+    defined(CREATIVE_ZENXFI2) || defined(CREATIVE_ZENXFI3)
 #define USE_PERSISTENT
 #endif
 

firmware/target/arm/imx233/rtc-imx233.h
@@ -86,10 +86,21 @@ static inline void imx233_rtc_init(void)
     BF_CLR(RTC_CTRL, SFTRST);
     udelay(5);  /* only need 3 GPMI clocks (1us) */
     BF_CLR(RTC_CTRL, CLKGATE);
+#if defined(SANSA_FUZEPLUS)
 #ifdef BM_RTC_PERSISTENT0_DISABLE_XTALOK
+    while (BF_RD(RTC_STAT, NEW_REGS)!=0) {};
     BF_SET(RTC_PERSISTENT0, XTAL24MHZ_PWRUP, DISABLE_XTALOK);
 #endif
+    while (BF_RD(RTC_STAT, NEW_REGS)!=0) {};
     BF_CLR(RTC_PERSISTENT0, CLOCKSOURCE);
+#else
+    /* confirmed for CREATIVE_ZEN and CREATIVE_ZENXFI2 */
+    /* FIXME: test SONY_NWZE360 and SONY_NWZE370 targets */
+    while (BF_RD(RTC_STAT, NEW_REGS)!=0) {};
+    BF_SET(RTC_PERSISTENT0, XTAL32KHZ_PWRUP, CLOCKSOURCE);
+    while (BF_RD(RTC_STAT, NEW_REGS)!=0) {};
+    BF_CLR(RTC_PERSISTENT0, XTAL24MHZ_PWRUP, DISABLE_XTALOK);
+#endif
     imx233_rtc_enable_watchdog(false);
 }
 