Rockbox/rockbox

commit 350a2250b1cf1425b777d1f8f284e4e7d41241b2

usb: implement endpoint allocation
this commit has following changes:
- introduce `usb_drv_ep_spec` table to udc drivers, which represents
  endpoint characteristics.
- introduce 'ep_allocs' table to class drivers, which represents what
  endpoint type does the class driver want.
- implement endpoint matching logic to usb core.

this is a required step to implement usb config switching, because we
need to create config descriptors without actually initializing endpoints.

Change-Id: I11c324cd35189ab636744488f6259d0cdb2179f0

firmware/target/arm/as3525/usb-drv-as3525.c
@@ -36,6 +36,15 @@
 
 #include "usb-drv-as3525.h"
 
+/* OUT EP 2 is an alias for OUT EP 0 on this HW! */
+struct usb_drv_ep_spec usb_drv_ep_specs[USB_NUM_EPS] = {
+    [0] = {USB_ENDPOINT_XFER_CONTROL, USB_ENDPOINT_XFER_CONTROL},
+    [1] = {USB_ENDPOINT_TYPE_ANY, USB_ENDPOINT_TYPE_ANY},
+    [2] = {USB_ENDPOINT_TYPE_NONE, USB_ENDPOINT_TYPE_ANY},
+    [3] = {USB_ENDPOINT_TYPE_ANY, USB_ENDPOINT_TYPE_ANY},
+};
+uint8_t usb_drv_ep_specs_flags = 0;
+
 static struct usb_endpoint endpoints[USB_NUM_EPS][2];
 static int got_set_configuration = 0;
 static int usb_enum_timeout = -1;
@@ -154,16 +163,6 @@ static void dma_desc_init(int ep, int dir)
 static void reset_endpoints(int init)
 {
     int i;
-
-    /*
-     * OUT EP 2 is an alias for OUT EP 0 on this HW!
-     *
-     * Resonates with "3 bidirectional- plus 1 in-endpoints in device mode"
-     * from the datasheet, but why ep2 and not ep3?
-     *
-     * Reserve it here so we will skip over it in request_endpoint().
-     */
-    endpoints[2][1].state |= EP_STATE_ALLOCATED;
 
     for(i = 0; i < USB_NUM_EPS; i++) {
         /*
@@ -344,62 +343,37 @@ int usb_drv_port_speed(void)
     return (USB_DEV_STS & USB_DEV_STS_MASK_SPD) ? 0 : 1;
 }
 
-int usb_drv_request_endpoint(int type, int dir)
-{
-    int d = dir == USB_DIR_IN ? 0 : 1;
-    int i = 1; /* skip the control EP */
+int usb_drv_init_endpoint(int endpoint, int type, int max_packet_size) {
+    (void)max_packet_size;
 
-    for(; i < USB_NUM_EPS; i++) {
-        if (endpoints[i][d].state & EP_STATE_ALLOCATED)
-            continue;
+    int i = EP_NUM(endpoint);
+    int d = EP_DIR(endpoint) == DIR_IN ? 0 : 1;
 
-        endpoints[i][d].state |= EP_STATE_ALLOCATED;
-
-        if (dir == USB_DIR_IN) {
-            USB_IEP_CTRL(i) = USB_EP_CTRL_FLUSH |
-                              USB_EP_CTRL_SNAK  |
-                              USB_EP_CTRL_ACT   |
-                              (type << 4);
-            USB_DEV_EP_INTR_MASK &= ~(1<<i);
-        } else {
-            USB_OEP_CTRL(i) = USB_EP_CTRL_FLUSH |
-                              USB_EP_CTRL_SNAK  |
-                              USB_EP_CTRL_ACT   |
-                              (type << 4);
-            USB_DEV_EP_INTR_MASK &= ~(1<<(16+i));
-        }
-        /* logf("usb_drv_request_endpoint(%d, %d): returning %02x\n", type, dir, i | dir); */
-        return i | dir;
+    if (EP_DIR(endpoint) == DIR_IN) {
+        USB_IEP_CTRL(i) = USB_EP_CTRL_FLUSH |
+                          USB_EP_CTRL_SNAK  |
+                          USB_EP_CTRL_ACT   |
+                          (type << 4);
+        USB_DEV_EP_INTR_MASK &= ~(1<<i);
+    } else {
+        USB_OEP_CTRL(i) = USB_EP_CTRL_FLUSH |
+                          USB_EP_CTRL_SNAK  |
+                          USB_EP_CTRL_ACT   |
+                          (type << 4);
+        USB_DEV_EP_INTR_MASK &= ~(1<<(16+i));
     }
-
-    logf("usb_drv_request_endpoint(%d, %d): no free endpoint found\n", type, dir);
-    return -1;
+    return 0;
 }
 
-void usb_drv_release_endpoint(int ep)
-{
-    int i = ep & 0x7f;
-    int d = ep & USB_DIR_IN ? 0 : 1;
+int usb_drv_deinit_endpoint(int endpoint) {
+    int i = EP_NUM(endpoint);
+    int d = EP_DIR(endpoint) == DIR_IN ? 0 : 1;
 
-    if (i >= USB_NUM_EPS)
-        return;
-    /*
-     * Check for control EP and ignore it.
-     * Unfortunately the usb core calls
-     * usb_drv_release_endpoint() for ep=0..(USB_NUM_ENDPOINTS-1),
-     * but doesn't request a new control EP after that...
-     */
-    if (i == 0 || /* Don't mask control EP */
-        (i == 2 && d == 1)) /* See reset_endpoints(), EP2_OUT == EP0_OUT */
-        return;
-
-    if (!(endpoints[i][d].state & EP_STATE_ALLOCATED))
-        return;
-
-    /* logf("usb_drv_release_endpoint(%d, %d)\n", i, d); */
     endpoints[i][d].state = 0;
     USB_DEV_EP_INTR_MASK |= (1<<(16*d+i));
     USB_EP_CTRL(i, !d) = USB_EP_CTRL_FLUSH | USB_EP_CTRL_SNAK;
+
+    return 0;
 }
 
 void usb_drv_cancel_all_transfers(void)

firmware/target/arm/tms320dm320/sansa-connect/tnetv105_usb_drv.c
@@ -60,11 +60,12 @@ static bool setup_is_set_address;
 
 static cppi_info cppi;
 
+struct usb_drv_ep_spec usb_drv_ep_specs[USB_NUM_ENDPOINTS]; /* filled in usb_drv_startup */
+uint8_t usb_drv_ep_specs_flags = 0;
+
 static struct ep_runtime_t
 {
     int max_packet_size;
-    bool in_allocated;
-    bool out_allocated;
     uint8_t *rx_buf;             /* OUT */
     int rx_remaining;
     int rx_size;
@@ -1239,13 +1240,22 @@ void usb_charging_maxcurrent_change(int maxcurrent)
     }
 }
 
+void usb_drv_startup(void)
+{
+    /* fill the endpoint spec table */
+    usb_drv_ep_specs[0].type[DIR_OUT] = USB_ENDPOINT_XFER_CONTROL;
+    usb_drv_ep_specs[0].type[DIR_IN] = USB_ENDPOINT_XFER_CONTROL;
+    for(int i = 1; i < USB_NUM_ENDPOINTS; i += 1) {
+        usb_drv_ep_specs[i].type[DIR_OUT] = ep_const_data[i].type;
+        usb_drv_ep_specs[i].type[DIR_IN] = ep_const_data[i].type;
+    }
+}
+
 void usb_drv_init(void)
 {
     int epn;
     memset(ep_runtime, 0, sizeof(ep_runtime));
     ep_runtime[0].max_packet_size = EP0_MAX_PACKET_SIZE;
-    ep_runtime[0].in_allocated = true;
-    ep_runtime[0].out_allocated = true;
     for (epn = 0; epn < USB_NUM_ENDPOINTS; epn++)
     {
         semaphore_init(&ep_runtime[epn].complete, 1, 0);
@@ -1479,41 +1489,16 @@ void usb_drv_set_test_mode(int mode)
     tnetv_usb_reg_write(TNETV_USB_CTRL, usbCtrl.val);
 }
 
-int usb_drv_request_endpoint(int type, int dir)
-{
-    int epn;
-    for (epn = 1; epn < USB_NUM_ENDPOINTS; epn++)
-    {
-        if (type == ep_const_data[epn].type)
-        {
-            if ((dir == USB_DIR_IN) && (!ep_runtime[epn].in_allocated))
-            {
-                ep_runtime[epn].in_allocated = true;
-                tnetv_gadget_ep_enable(epn, true);
-                return epn | USB_DIR_IN;
-            }
-            if ((dir == USB_DIR_OUT) && (!ep_runtime[epn].out_allocated))
-            {
-                ep_runtime[epn].out_allocated = true;
-                tnetv_gadget_ep_enable(epn, false);
-                return epn | USB_DIR_OUT;
-            }
-        }
-    }
-    return -1;
+int usb_drv_init_endpoint(int endpoint, int type, int max_packet_size) {
+    (void)max_packet_size; /* FIXME: support max packet size override */
+
+    int num = EP_NUM(endpoint);
+    int dir = EP_DIR(endpoint);
+    return tnetv_gadget_ep_enable(num, dir == EP_IN);
 }
 
-void usb_drv_release_endpoint(int ep)
-{
-    int epn = EP_NUM(ep);
-    if (EP_DIR(ep) == DIR_IN)
-    {
-        ep_runtime[epn].in_allocated = false;
-        tnetv_gadget_ep_disable(epn, true);
-    }
-    else
-    {
-        ep_runtime[epn].out_allocated = false;
-        tnetv_gadget_ep_disable(epn, false);
-    }
+int usb_drv_deinit_endpoint(int endpoint) {
+    int num = EP_NUM(endpoint);
+    int dir = EP_DIR(endpoint);
+    return tnetv_gadget_ep_disable(num, dir == EP_IN);
 }

firmware/drivers/isp1583.c
@@ -29,6 +29,9 @@
 #include "logf.h"
 #include "stdio.h"
 
+struct usb_drv_ep_spec usb_drv_ep_specs[USB_NUM_ENDPOINTS]; /* filled in usb_drv_init */
+uint8_t usb_drv_ep_specs_flags = 0;
+
 struct usb_endpoint
 {
     unsigned char *out_buf;
@@ -48,7 +51,6 @@ struct usb_endpoint
     unsigned char enabled[2];
     short max_pkt_size[2];
     short type;
-    char allocation;
 };
 
 static unsigned char setup_pkt_buf[8];
@@ -470,6 +472,14 @@ void usb_drv_init(void)
 
     //tick_add_task(usb_helper);
 
+    /* Fill endpoint spec table FIXME: should be done in usb_drv_startup() */
+    usb_drv_ep_specs[0].type[DIR_OUT] = USB_ENDPOINT_XFER_CONTROL;
+    usb_drv_ep_specs[0].type[DIR_IN] = USB_ENDPOINT_XFER_CONTROL;
+    for(int i = 1; i < USB_NUM_ENDPOINTS; i += 1) {
+        usb_drv_ep_specs[i].type[DIR_OUT] = USB_ENDPOINT_XFER_BULK;
+        usb_drv_ep_specs[i].type[DIR_IN] = USB_ENDPOINT_XFER_BULK;
+    }
+
     logf("usb_init_device() finished");
 }
 
@@ -616,29 +626,15 @@ void usb_drv_cancel_all_transfers(void)
         endpoints[i].halt[0] = endpoints[i].halt[1] = 1;
 }
 
-int usb_drv_request_endpoint(int type, int dir)
+int usb_drv_init_endpoint(int endpoint, int type, int max_packet_size)
 {
-    int i, bit;
-
-    if (type != USB_ENDPOINT_XFER_BULK)
-        return -1;
-
-    bit=(dir & USB_DIR_IN)? 2:1;
-
-    for (i=1; i < USB_NUM_ENDPOINTS; i++) {
-        if((endpoints[i].allocation & bit)!=0)
-            continue;
-        endpoints[i].allocation |= bit;
-        return i | dir;
-    }
-
-    return -1;
+    (void)max_packet_size; /* FIXME: support max packet size override */
+    return 0;
 }
 
-void usb_drv_release_endpoint(int ep)
+int usb_drv_deinit_endpoint(int endpoint)
 {
-    int mask = (ep & USB_DIR_IN)? ~2:~1;
-    endpoints[ep & 0x7f].allocation &= mask;
+    return 0;
 }
 
 static void bus_reset(void)

firmware/drivers/m66591.c
@@ -59,6 +59,9 @@
  *  stack visible functions.
  ******************************************************************************/
 
+struct usb_drv_ep_spec usb_drv_ep_specs[USB_NUM_ENDPOINTS]; /* filled in usb_drv_init */
+uint8_t usb_drv_ep_specs_flags = USB_ENDPOINT_SPEC_IO_EXCLUSIVE;
+
 static volatile unsigned short * pipe_ctrl_addr(int pipe);
 static void pipe_handshake(int pipe, int handshake);
 static void pipe_c_select (int pipe, bool dir);
@@ -78,7 +81,6 @@ struct M66591_epstat {
     int length;             /* how match data will fit */
     volatile int count;     /* actual data count */
     bool waiting;           /* is there data to transfer? */
-    bool busy;              /* has the pipe been requested for use? */
 } ;
 
 static struct M66591_epstat M66591_eps[USB_NUM_ENDPOINTS];
@@ -638,82 +640,63 @@ void usb_drv_set_test_mode(int mode) {
     M66591_TESTMODE |= mode;
 }
 
-/* Request an unused endpoint */
-int usb_drv_request_endpoint(int type, int dir) {
-    int ep;
-    int pipecfg = 0;
+int usb_drv_init_endpoint(int endpoint, int type, int max_packet_size) {
+    (void)max_packet_size; /* FIXME: support max packet size override */
 
-    if (type == USB_ENDPOINT_XFER_BULK) {
+    int pipecfg;
+
+    if(type == USB_ENDPOINT_XFER_BULK) {
         /* Enable double buffer mode (only used for ep 1 and 2) */
         pipecfg |= 1<<9 | 1<<8; 
-        
-        /* Bulk endpoints must be between 1 and 4 inclusive */
-        ep=1;
-        
-        while(M66591_eps[ep].busy && ep++<5);
-        
-        /* If this reached 5 the endpoints were all busy */
-        if(ep==5) {
-            logf("mxx: ep %d busy", ep);
-            return -1;
-        }
-    } else if (type == USB_ENDPOINT_XFER_INT) {
-        ep=5;
-
+    } else if(type == USB_ENDPOINT_XFER_BULK) {
         pipecfg |= 1<<13;
-
-        while(M66591_eps[ep].busy && ++ep<USB_NUM_ENDPOINTS);
-
-        /* If this reached USB_NUM_ENDPOINTS the endpoints were all busy */
-        if(ep==USB_NUM_ENDPOINTS) {
-            logf("mxx: ep %d busy", ep);
-            return -1;
-        }
     } else {
         /* Not a supported type */
         return -1;
     }
 
+    int num = endpoint & USB_ENDPOINT_NUMBER_MASK;
+    int dir = endpoint & USB_ENDPOINT_DIR_MASK;
     if (dir == USB_DIR_IN) {
         pipecfg |= (1<<4);
     }
+
+    M66591_eps[num].dir = dir;
     
-    M66591_eps[ep].busy = true;
-    M66591_eps[ep].dir = dir;
-    
-    M66591_PIPE_CFGSEL=ep;
-    
+    M66591_PIPE_CFGSEL=num;
+
     /* Enable pipe (15) */
     pipecfg |= 1<<15; 
     
-    pipe_handshake(ep, PIPE_SHAKE_NAK);
+    pipe_handshake(num, PIPE_SHAKE_NAK);
 
     /* Setup the flags */
     M66591_PIPE_CFGWND=pipecfg;
     
-    pipe_init(ep);
+    pipe_init(num);
     
-    logf("mxx: ep req ep#: %d config: 0x%04x", ep, M66591_PIPE_CFGWND);
+    logf("mxx: ep req ep#: %d config: 0x%04x", num, M66591_PIPE_CFGWND);
 
-    return ep | dir;
+    return 0;
 }
 
 /* Used by stack to tell the helper functions that the pipe is not in use */
-void usb_drv_release_endpoint(int ep) {
-    int flags;
-    ep &= 0x7f;
+int usb_drv_deinit_endpoint(int endpoint) {
+    int num = endpoint & USB_ENDPOINT_NUMBER_MASK;
 
-    if (ep < 1 || ep > USB_NUM_ENDPOINTS || M66591_eps[ep].busy == false)
-        return ;
+    if (num < 1 || num > USB_NUM_ENDPOINTS) {
+        return -1;
+    }
 
-    flags = disable_irq_save();
+    int flags = disable_irq_save();
     
-    logf("mxx: ep %d release", ep);
+    logf("mxx: ep %d release", num);
 
-    M66591_eps[ep].busy = false;
-    M66591_eps[ep].dir = -1;
+    M66591_eps[num].dir = -1;
 
     restore_irq(flags);
+
+    return 0;
 }
 
 /* Periodically called to check if a cable was plugged into the device */
@@ -744,6 +727,18 @@ void usb_drv_init(void) {
     M66591_TRN_CTRL         |=0x0001;
 
     M66591_INTCFG_MAIN      |=0x8000; /* Enable VBUS interrupt */
+
+    /* Fill endpoint spec table FIXME: should be done in usb_drv_startup() */
+    usb_drv_ep_specs[0].type[DIR_OUT] = USB_ENDPOINT_XFER_CONTROL;
+    usb_drv_ep_specs[0].type[DIR_IN] = USB_ENDPOINT_XFER_CONTROL;
+    for(int i = 1; i < 5; i += 1) {
+        usb_drv_ep_specs[i].type[DIR_OUT] = USB_ENDPOINT_XFER_BULK;
+        usb_drv_ep_specs[i].type[DIR_IN] = USB_ENDPOINT_XFER_BULK;
+    }
+    for(int i = 5; i < USB_NUM_ENDPOINTS; i += 1) {
+        usb_drv_ep_specs[i].type[DIR_OUT] = USB_ENDPOINT_XFER_INT;
+        usb_drv_ep_specs[i].type[DIR_IN] = USB_ENDPOINT_XFER_INT;
+    }
 }
 
 /* fully enable driver */
@@ -757,7 +752,6 @@ void usb_attach(void) {
         M66591_eps[i].length = 0;
         M66591_eps[i].count = 0;
         M66591_eps[i].waiting = false;
-        M66591_eps[i].busy = false;
     }
 
     /* Issue a h/w reset */

firmware/drivers/usb-designware.c
@@ -144,6 +144,9 @@ struct usb_dw_ep0
     struct usb_ctrlrequest pending_req;
 };
 
+struct usb_drv_ep_spec usb_drv_ep_specs[USB_NUM_ENDPOINTS]; /* filled in usb_drv_init */
+uint8_t usb_drv_ep_specs_flags = 0;
+
 static const char* const dw_dir_str[USB_DW_NUM_DIRS] =
 {
     [USB_DW_EPDIR_IN]  = "IN",
@@ -1495,6 +1498,17 @@ static void usb_dw_init(void)
     /* Soft reconnect */
     udelay(3000);
     DWC_DCTL &= ~SDIS;
+
+    /* Fill endpoint spec table FIXME: should be done in usb_drv_startup() */
+    usb_drv_ep_specs[0].type[DIR_OUT] = USB_ENDPOINT_XFER_CONTROL;
+    usb_drv_ep_specs[0].type[DIR_IN] = USB_ENDPOINT_XFER_CONTROL;
+    for(int i = 1; i < USB_NUM_ENDPOINTS; i += 1) {
+        bool out_avail = usb_endpoints & (1 << (i + USB_DW_DIR_OFF(USB_DW_EPDIR_OUT)));
+        usb_drv_ep_specs[i].type[DIR_OUT] = out_avail ? USB_ENDPOINT_TYPE_ANY : USB_ENDPOINT_TYPE_NONE;
+
+        bool in_avail = usb_endpoints & (1 << (i + USB_DW_DIR_OFF(USB_DW_EPDIR_IN)));
+        usb_drv_ep_specs[i].type[DIR_IN] = in_avail ? USB_ENDPOINT_TYPE_ANY : USB_ENDPOINT_TYPE_NONE;
+    }
 }
 
 static void usb_dw_exit(void)
@@ -1592,56 +1606,50 @@ void INT_USB_FUNC(void)
     usb_dw_irq();
 }
 
-int usb_drv_request_endpoint(int type, int dir)
+int usb_drv_init_endpoint(int endpoint, int type, int max_packet_size)
 {
-    int request_ep = -1;
-    enum usb_dw_epdir epdir = (EP_DIR(dir) == DIR_IN) ?
-                                USB_DW_EPDIR_IN : USB_DW_EPDIR_OUT;
+    (void)max_packet_size; /* FIXME: support max packet size override */
+
+    enum usb_dw_epdir epdir = (EP_DIR(endpoint) == DIR_IN) ? USB_DW_EPDIR_IN : USB_DW_EPDIR_OUT;
+    struct usb_dw_ep* dw_ep = usb_dw_get_ep(EP_NUM(endpoint), epdir);
+
+    int maxpktsize;
+    if(type == EPTYP_ISOCHRONOUS)
+    {
+        maxpktsize = 1023;
+    }
+    else
+    {
+        maxpktsize = usb_drv_port_speed() ? 512 : 64;
+    }
 
     usb_dw_target_disable_irq();
-    for (int ep = 1; ep < USB_NUM_ENDPOINTS; ep++)
-    {
-        if (usb_endpoints & (1 << (ep + USB_DW_DIR_OFF(epdir))))
-        {
-            struct usb_dw_ep* dw_ep = usb_dw_get_ep(ep, epdir);
-            if (!dw_ep->active)
-            {
-                int maxpktsize = 64;
-                if (type == EPTYP_ISOCHRONOUS){
-                    maxpktsize = 1023;
-                } else {
-                    maxpktsize = usb_drv_port_speed() ? 512 : 64;
-                }
-
-                if (usb_dw_configure_ep(ep, epdir, type,
-                                maxpktsize) >= 0)
-                {
-                    dw_ep->active = true;
-                    request_ep = ep | dir;
-                }
-                break;
-            }
-        }
-    }
+    int res = usb_dw_configure_ep(EP_NUM(endpoint), epdir, type, maxpktsize);
     usb_dw_target_enable_irq();
-    return request_ep;
+
+    if(res >= 0)
+    {
+        dw_ep->active = true;
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
 }
 
-void usb_drv_release_endpoint(int endpoint)
+int usb_drv_deinit_endpoint(int endpoint)
 {
-    int epnum = EP_NUM(endpoint);
-    if (!epnum) return;
-    enum usb_dw_epdir epdir = (EP_DIR(endpoint) == DIR_IN) ?
-                                USB_DW_EPDIR_IN : USB_DW_EPDIR_OUT;
-    struct usb_dw_ep* dw_ep = usb_dw_get_ep(epnum, epdir);
+    enum usb_dw_epdir epdir = (EP_DIR(endpoint) == DIR_IN) ? USB_DW_EPDIR_IN : USB_DW_EPDIR_OUT;
+    struct usb_dw_ep* dw_ep = usb_dw_get_ep(EP_NUM(endpoint), epdir);
 
     usb_dw_target_disable_irq();
-    if (dw_ep->active)
-    {
-        usb_dw_unconfigure_ep(epnum, epdir);
-        dw_ep->active = false;
-    }
+    usb_dw_unconfigure_ep(EP_NUM(endpoint), epdir);
     usb_dw_target_enable_irq();
+
+    dw_ep->active = false;
+
+    return 0;
 }
 
 int usb_drv_recv_nonblocking(int endpoint, void* ptr, int length)

firmware/export/usb_core.h
@@ -66,9 +66,6 @@ void usb_core_handle_notify(long id, intptr_t data);
 void usb_core_notify_set_address(uint8_t addr);
 void usb_core_notify_set_config(uint8_t config);
 
-int usb_core_request_endpoint(int type, int dir,struct usb_class_driver* drv);
-void usb_core_release_endpoint(int dir);
-
 #ifdef HAVE_HOTSWAP
 void usb_core_hotswap_event(int volume,bool inserted);
 #endif

firmware/export/usb_drv.h
@@ -62,6 +62,19 @@ enum usb_control_response {
     USB_CONTROL_RECEIVE,
 };
 
+#define USB_ENDPOINT_TYPE_ANY  (-1)
+#define USB_ENDPOINT_TYPE_NONE (-2)
+
+struct usb_drv_ep_spec {
+    int8_t type[2]; /* USB_ENDPOINT_TYPE_{ANY,NONE} USB_ENDPOINT_XFER_* */
+};
+
+extern struct usb_drv_ep_spec usb_drv_ep_specs[USB_NUM_ENDPOINTS];
+
+#define USB_ENDPOINT_SPEC_FORCE_IO_TYPE_MATCH (1 << 0)
+#define USB_ENDPOINT_SPEC_IO_EXCLUSIVE        (1 << 1)
+extern uint8_t usb_drv_ep_specs_flags;
+
 /* one-time initialisation of the USB driver */
 void usb_drv_startup(void);
 void usb_drv_int_enable(bool enable); /* Target implemented */
@@ -85,8 +98,8 @@ int usb_drv_port_speed(void);
 void usb_drv_cancel_all_transfers(void);
 void usb_drv_set_test_mode(int mode);
 bool usb_drv_connected(void);
-int usb_drv_request_endpoint(int type, int dir);
-void usb_drv_release_endpoint(int ep);
+int usb_drv_init_endpoint(int endpoint, int type, int max_packet_size);
+int usb_drv_deinit_endpoint(int endpoint);
 #ifdef USB_HAS_ISOCHRONOUS
 /* returns the last received frame number (the 11-bit number contained in the last SOF):
  * - full-speed: the host sends one SOF every 1ms (so 1000 SOF/s)

firmware/target/arm/rk27xx/usb-drv-rk27xx.c
@@ -53,7 +53,6 @@ struct endpoint_t
     const int type;              /* EP type */
     const int dir;               /* DIR_IN/DIR_OUT */
     volatile unsigned long *stat; /* RXSTAT/TXSTAT register */
-    bool allocated;              /* flag to mark EPs taken */
     volatile void *buf;          /* tx/rx buffer address */
     volatile int len;            /* size of the transfer (bytes) */
     volatile int cnt;            /* number of bytes transfered/received  */
@@ -101,6 +100,9 @@ static struct endpoint_t endpoints[16] =
     ENDPOINT(14, BULK, IN,  &TX14STAT), /* BIN14 */
     ENDPOINT(15, INT,  IN,  &TX15STAT), /* IIN15 */
 };
+
+struct usb_drv_ep_spec usb_drv_ep_specs[16]; /* filled in usb_drv_startup */
+uint8_t usb_drv_ep_specs_flags = 0;
 
 static volatile bool set_address = false;
 static volatile bool set_configuration = false;
@@ -260,48 +262,47 @@ static void udc_helper(void)
         }
 }
 
+void usb_drv_startup(void) {
+    /* fill the endpoint spec table */
+    usb_drv_ep_specs[0].type[DIR_OUT] = USB_ENDPOINT_XFER_CONTROL;
+    usb_drv_ep_specs[0].type[DIR_IN] = USB_ENDPOINT_XFER_CONTROL;
+    for(int ep_num = 1; ep_num < 16; ep_num++) {
+        int dir = endpoints[ep_num].dir;
+        int type = endpoints[ep_num].type;
+        usb_drv_ep_specs[ep_num].type[dir] = type;
+        usb_drv_ep_specs[ep_num].type[!dir] = USB_ENDPOINT_TYPE_NONE;
+    }
+}
+
 /* return port speed FS=0, HS=1 */
 int usb_drv_port_speed(void)
 {
     return (DEV_INFO & DEV_SPEED) ? 0 : 1;
 }
 
-/* Reserve endpoint */
-int usb_drv_request_endpoint(int type, int dir)
-{
-    logf("req: %s %s", XFER_DIR_STR(dir), XFER_TYPE_STR(type));
+int usb_drv_init_endpoint(int endpoint, int type, int max_packet_size) {
+    (void)max_packet_size; /* FIXME: support max packet size override */
 
-    /* Find an available ep/dir pair */
-    for(int ep_num = 1; ep_num<USB_NUM_ENDPOINTS;ep_num++)
-    {
-        struct endpoint_t *endp = &endpoints[ep_num];
+    int num = EP_NUM(endpoint);
+    int dir = EP_DIR(endpoint);
 
-        if(endp->allocated || endp->type != type || endp->dir != dir)
-            continue;
-        /* allocate endpoint and enable interrupt */
-        endp->allocated = true;
-        if(dir == USB_DIR_IN)
-            TXCON(endp) = (ep_num << 8) | TXEPEN | TXNAK | TXACKINTEN | TXCFINTE;
-        else
-            RXCON(endp) = (ep_num << 8) | RXEPEN | RXNAK | RXACKINTEN | RXCFINTE | RXERRINTEN;
-        EN_INT |= 1 << (ep_num + 7);
+    struct endpoint_t *endp = &endpoints[num];
 
-        logf("add: ep%d %s", ep_num, XFER_DIR_STR(dir));
-        return ep_num | dir;
-    }
-    return -1;
+    if(EP_DIR(endpoint) == DIR_IN)
+        TXCON(endp) = (num << 8) | TXEPEN | TXNAK | TXACKINTEN | TXCFINTE;
+    else
+        RXCON(endp) = (num << 8) | RXEPEN | RXNAK | RXACKINTEN | RXCFINTE | RXERRINTEN;
+    EN_INT |= 1 << (num + 7);
+
+    return 0;
 }
 
-/* Free endpoint */
-void usb_drv_release_endpoint(int ep)
-{
-    int ep_num = EP_NUM(ep);
-
-    logf("rel: ep%d", ep_num);
-    endpoints[ep_num].allocated = false;
+int usb_drv_deinit_endpoint(int endpoint) {
+    int num = EP_NUM(endpoint);
+    struct endpoint_t *endp = &endpoints[num];
 
     /* disable interrupt from this endpoint */
-    EN_INT &= ~(1 << (ep_num + 7));
+    EN_INT &= ~(1 << (num + 7));
 }
 
 /* Set the address (usually it's in a register).

commit ef47ffe250cefefd3790d036a3077f0e7d6cf178

Fix the last of the issues introduced in 350a2250b
Change-Id: I4b58f5d9d1c4649080826a9cf1b438e13ed7ce81

firmware/target/arm/as3525/usb-drv-as3525.c
@@ -347,7 +347,7 @@ int usb_drv_init_endpoint(int endpoint, int type, int max_packet_size) {
     (void)max_packet_size;
 
     int i = EP_NUM(endpoint);
-    int d = EP_DIR(endpoint) == DIR_IN ? 0 : 1;
+//    int d = EP_DIR(endpoint) == DIR_IN ? 0 : 1;
 
     if (EP_DIR(endpoint) == DIR_IN) {
         USB_IEP_CTRL(i) = USB_EP_CTRL_FLUSH |

firmware/target/arm/tms320dm320/sansa-connect/tnetv105_usb_drv.c
@@ -1491,14 +1491,15 @@ void usb_drv_set_test_mode(int mode)
 
 int usb_drv_init_endpoint(int endpoint, int type, int max_packet_size) {
     (void)max_packet_size; /* FIXME: support max packet size override */
+    (void)type;
 
     int num = EP_NUM(endpoint);
     int dir = EP_DIR(endpoint);
-    return tnetv_gadget_ep_enable(num, dir == EP_IN);
+    return tnetv_gadget_ep_enable(num, dir == DIR_IN);
 }
 
 int usb_drv_deinit_endpoint(int endpoint) {
     int num = EP_NUM(endpoint);
     int dir = EP_DIR(endpoint);
-    return tnetv_gadget_ep_disable(num, dir == EP_IN);
+    return tnetv_gadget_ep_disable(num, dir == DIR_IN);
 }