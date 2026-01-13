450687bc3d90c88eb03809e9f266dec0eb9c2d6a
Aidan MacDonald <amachronic@protonmail.com>
2026-01-13 12:06:21 +0000

    stm32h743: fix incorrectly defined SRAM3 size
    Change-Id: Ia31e22fb2c6e7becf380abf440b03a73ec383582

--- firmware/target/arm/stm32/cpu-stm32h743.h
+++ firmware/target/arm/stm32/cpu-stm32h743.h
@@ -82,7 +82,7 @@
 #define STM32_SRAM2_SIZE        (128 * 1024)
 
 #define STM32_SRAM3_BASE        0x30040000
-#define STM32_SRAM3_SIZE        (128 * 1024)
+#define STM32_SRAM3_SIZE        (32 * 1024)
 
 #define STM32_SRAM4_BASE        0x38000000
 #define STM32_SRAM4_SIZE        (64 * 1024)
