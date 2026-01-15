fcb2289274257afa17406caf686d34ac4bb675f1
Solomon Peachy <pizza@shaftnet.org>
2026-01-15 09:20:06 -0500

    voice:  when building from vstrings targetid needs to come from within
    Change-Id: I2a1c6a249523ec7a1d339e7b08f1686dad1d0270

--- tools/voice.pl
+++ tools/voice.pl
@@ -715,6 +715,8 @@ sub generatevstringclips {
     close(VOICEFONTIDS);
 
     print("\n");
+
+    return $targetid;
 }
 
 
@@ -867,7 +869,7 @@ if (defined($B) && $B) {
     printf("Generating voice\n  Target: %s\n  Language: %s\n  Encoder (options): %s (%s)\n  TTS Engine (options): %s (%s)\n  Pool directory: %s\n",
            defined($t) ? $t : "unknown",
            $l, $e, $E, $s, "$S $tts_object->{ttsoptions}", defined($ENV{'POOL'}) ? $ENV{'POOL'} : "<none>");
-    generatevstringclips($l, $t, $e, $E, $tts_object, $S, $B);
+    $i = generatevstringclips($l, $t, $e, $E, $tts_object, $S, $B);
     shutdown_tts($tts_object);
     createvoice($l, $i, $f);
     deleteencs();
