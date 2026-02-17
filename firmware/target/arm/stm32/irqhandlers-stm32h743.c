f6dfa5144d55d33d7a9c37799624f73a43cb8575
Aidan MacDonald <amachronic@protonmail.com>
2026-02-17 10:04:32 +0000

    stm32h743: break out SAI and DMA interrupt handlers
    Change-Id: I6a8eba6211eab34915d99000426aacd6ea396769

--- firmware/target/arm/stm32/irqhandlers-stm32h743.c
+++ firmware/target/arm/stm32/irqhandlers-stm32h743.c
@@ -23,11 +23,41 @@
  * NOTE: This file must be included from system-arm-micro.c!
  */
 
-void dma_irq_handler(void) ATTR_IRQ_HANDLER;
+void dmamux1_irq_handler(void) ATTR_IRQ_HANDLER;
+void dmamux2_irq_handler(void) ATTR_IRQ_HANDLER;
+void mdma_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma2d_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma1_ch0_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma1_ch1_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma1_ch2_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma1_ch3_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma1_ch4_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma1_ch5_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma1_ch6_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma1_ch7_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma2_ch0_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma2_ch1_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma2_ch2_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma2_ch3_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma2_ch4_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma2_ch5_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma2_ch6_irq_handler(void) ATTR_IRQ_HANDLER;
+void dma2_ch7_irq_handler(void) ATTR_IRQ_HANDLER;
+void bdma_ch0_irq_handler(void) ATTR_IRQ_HANDLER;
+void bdma_ch1_irq_handler(void) ATTR_IRQ_HANDLER;
+void bdma_ch2_irq_handler(void) ATTR_IRQ_HANDLER;
+void bdma_ch3_irq_handler(void) ATTR_IRQ_HANDLER;
+void bdma_ch4_irq_handler(void) ATTR_IRQ_HANDLER;
+void bdma_ch5_irq_handler(void) ATTR_IRQ_HANDLER;
+void bdma_ch6_irq_handler(void) ATTR_IRQ_HANDLER;
+void bdma_ch7_irq_handler(void) ATTR_IRQ_HANDLER;
 void i2c_irq_handler(void) ATTR_IRQ_HANDLER;
 void lcdc_irq_handler(void) ATTR_IRQ_HANDLER;
 void otg_hs_irq_handler(void) ATTR_IRQ_HANDLER;
-void sai_irq_handler(void) ATTR_IRQ_HANDLER;
+void sai1_irq_handler(void) ATTR_IRQ_HANDLER;
+void sai2_irq_handler(void) ATTR_IRQ_HANDLER;
+void sai3_irq_handler(void) ATTR_IRQ_HANDLER;
+void sai4_irq_handler(void) ATTR_IRQ_HANDLER;
 void sdmmc1_irq_handler(void) ATTR_IRQ_HANDLER;
 void sdmmc2_irq_handler(void) ATTR_IRQ_HANDLER;
 void spi1_irq_handler(void) ATTR_IRQ_HANDLER;
