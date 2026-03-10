Rockbox/rockbox

commit a2c10f6189e078977db95e8493d79133d9b7c1e3

unicode:  Support characters beyond the first unicode plane
We used 16-bit variables to store the 'character code' everywhere but
this won't let us represent anything beyond U+FFFF.

This patch changes those variables to a custom type that can be 32 or 16
bits depending on the build, and adjusts numerous internal APIs and
datastructures to match.  This includes:

 * utf8decode() and friends
 * font manipulation, caching, rendering, and generation
 * on-screen keyboard
 * FAT filesystem (parsing and generating utf16 LFNs)
 * WIN32 simulator platform code

Note that this patch doesn't _enable_ >16bit unicode support; a followup
patch will turn that on for appropriate targets.

Appears to work on:

  * hosted linux, native, linux simulator in both 16/32-bit modes.

Needs testing on:

  * windows and macos simulator (16bit+32bit)

Change-Id: Iba111b27d2433019b6bff937cf1ebd2c4353a0e8

firmware/common/diacritic.c
@@ -28,8 +28,8 @@
 #include "system.h"
 
 #define DIAC_NUM_RANGES      (ARRAYLEN(diac_ranges))
-#define DIAC_RTL             (1 << 7)
-#define DIAC_CNT             (0xFF ^ DIAC_RTL)
+#define DIAC_RTL             (1 << 15)
+#define DIAC_CNT             (0xFFFF ^ DIAC_RTL)
 
 /* Each diac_range_ struct defines a Unicode range that begins with
  * N diacritic characters, and continues with non-diacritic characters up to the
@@ -39,8 +39,8 @@
 
 struct diac_range
 {
-    uint16_t base;
-    uint8_t  info; /* [RTL:1 CNT:7] */
+    uint16_t base; /* Not ucschar_t until we need >16b */
+    uint16_t info; /* [RTL:1 CNT:15] */
 };
 
 #define DIAC_RANGE_ENTRY(first_diac, first_non_diac, is_rtl) \
@@ -51,7 +51,7 @@ struct diac_range
 static const struct diac_range diac_ranges[] =
 {
     DIAC_RANGE_ENTRY(0x0000, 0x0000, 0),
-    DIAC_RANGE_ENTRY(FIRST_DIACRITIC, 0x0370, 0),
+    DIAC_RANGE_ENTRY(FIRST_DIACRITIC, 0x0370, 0), /* v1 - v4.1 */
     DIAC_RANGE_ENTRY(0x0483, 0x048a, 0),
     DIAC_RANGE_ENTRY(0x0591, 0x05be, 1),
     DIAC_RANGE_ENTRY(0x05bf, 0x05c0, 1),
@@ -146,6 +146,7 @@ static const struct diac_range diac_ranges[] =
     DIAC_RANGE_ENTRY(0x19c8, 0x19ca, 0),
     DIAC_RANGE_ENTRY(0x1a17, 0x1a1c, 0),
     DIAC_RANGE_ENTRY(0x1a55, 0x1a80, 0),
+    DIAC_RANGE_ENTRY(0x1ab0, 0x1b00, 0), /* v7.0 */
     DIAC_RANGE_ENTRY(0x1b00, 0x1b05, 0),
     DIAC_RANGE_ENTRY(0x1b34, 0x1b45, 0),
     DIAC_RANGE_ENTRY(0x1b6b, 0x1b74, 0),
@@ -156,10 +157,10 @@ static const struct diac_range diac_ranges[] =
     DIAC_RANGE_ENTRY(0x1cd4, 0x1ce9, 0),
     DIAC_RANGE_ENTRY(0x1ced, 0x1cee, 0),
     DIAC_RANGE_ENTRY(0x1cf2, 0x1cf3, 0),
-    DIAC_RANGE_ENTRY(0x1dc0, 0x1e00, 0),
-    DIAC_RANGE_ENTRY(0x20d0, 0x20f1, 0),
+    DIAC_RANGE_ENTRY(0x1dc0, 0x1e00, 0), /* v4.1 - v5.2 */
+    DIAC_RANGE_ENTRY(0x20d0, 0x2100, 0), /* v1.0 - v5.1 */
     DIAC_RANGE_ENTRY(0x2cef, 0x2cf2, 0),
-    DIAC_RANGE_ENTRY(0x2de0, 0x2e00, 0),
+    DIAC_RANGE_ENTRY(0x2de0, 0x2e00, 0), /* v5.1 */
     DIAC_RANGE_ENTRY(0x302a, 0x3030, 0),
     DIAC_RANGE_ENTRY(0x3099, 0x309b, 0),
     DIAC_RANGE_ENTRY(0xa66f, 0xa673, 0),
@@ -188,7 +189,7 @@ static const struct diac_range diac_ranges[] =
     DIAC_RANGE_ENTRY(0xabe3, 0xabeb, 0),
     DIAC_RANGE_ENTRY(0xabec, 0xabee, 0),
     DIAC_RANGE_ENTRY(0xfb1e, 0xfb1f, 0),
-    DIAC_RANGE_ENTRY(0xfe20, 0xfe27, 0),
+    DIAC_RANGE_ENTRY(0xfe20, 0xfe30, 0), /* v1.0 - v8.0 */
     DIAC_RANGE_ENTRY(0xfe70, 0xfe70, 1),
     DIAC_RANGE_ENTRY(0xff00, 0xff00, 0),
     DIAC_RANGE_ENTRY(0xffff, 0xffff, 0),
@@ -196,7 +197,7 @@ static const struct diac_range diac_ranges[] =
 
 #define MRU_MAX_LEN 32
 
-bool is_diacritic(const unsigned short char_code, bool *is_rtl)
+bool is_diacritic(const ucschar_t char_code, bool *is_rtl)
 {
     static uint8_t mru_len = 0;
     static uint8_t diacritic_mru[MRU_MAX_LEN];
@@ -209,7 +210,6 @@ bool is_diacritic(const unsigned short char_code, bool *is_rtl)
     /* Search in MRU */
     for (mru = 0, i = 0; mru < mru_len; mru++)
     {
-
         /* Items shifted >> 1 */
         itmp = i;
         i = diacritic_mru[mru];
@@ -250,10 +250,10 @@ Found:
     if (is_rtl)
         *is_rtl = ((DIAC_RTL & info) == DIAC_RTL);
 
-    return (char_code < diac->base + (info & DIAC_CNT));
+    return (char_code < (diac->base + (info & DIAC_CNT)));
 }
 #else /*BOOTLOADER*/
-inline bool is_diacritic(const unsigned short char_code, bool *is_rtl)
+inline bool is_diacritic(const ucschar_t char_code, bool *is_rtl)
 {
     (void)char_code;
     if (is_rtl)

apps/hosted/android/keyboard.c
@@ -82,7 +82,7 @@ static void kdb_init(void)
         sleep(HZ/10);
 }
 
-int kbd_input(char* text, int buflen, unsigned short *kbd)
+int kbd_input(char* text, int buflen, ucschar_t *kbd)
 {
 	(void)kbd;
     JNIEnv e            = *env_ptr;
@@ -107,7 +107,7 @@ int kbd_input(char* text, int buflen, unsigned short *kbd)
     e->DeleteLocalRef(env_ptr, str);
     e->DeleteLocalRef(env_ptr, ok_text);
     e->DeleteLocalRef(env_ptr, cancel_text);
-    
+
     return !accepted; /* return 0 on success */
 }
 

apps/keyboard.h
@@ -23,7 +23,7 @@
 
 /* '*kbd', same format as https://www.rockbox.org/wiki/LoadableKeyboardLayouts */
 
-int kbd_input(char* buffer, int buflen, unsigned short *kbd);
+int kbd_input(char* buffer, int buflen, ucschar_t *kbd);
 
 int load_kbd(unsigned char* filename);
 

apps/plugin.h
@@ -176,7 +176,7 @@ int plugin_open(const char *plugin, const char *parameter);
  * when this happens please take the opportunity to sort in
  * any new functions "waiting" at the end of the list.
  */
-#define PLUGIN_API_VERSION 273
+#define PLUGIN_API_VERSION 274
 
 /* 239 Marks the removal of ARCHOS HWCODEC and CHARCELL */
 
@@ -296,15 +296,15 @@ struct plugin_api {
 #if defined(HAVE_LCD_ENABLE) || defined(HAVE_LCD_SLEEP)
     void (*button_queue_post)(long id, intptr_t data);
 #endif
-    unsigned short *(*bidi_l2v)( const unsigned char *str, int orientation );
-    bool (*is_diacritic)(const unsigned short char_code, bool *is_rtl);
-    const unsigned char *(*font_get_bits)( struct font *pf, unsigned short char_code );
+    ucschar_t *(*bidi_l2v)(const unsigned char *str, int orientation);
+    bool (*is_diacritic)(const ucschar_t char_code, bool *is_rtl);
+    const unsigned char *(*font_get_bits)(struct font *pf, ucschar_t char_code);
     int (*font_load)(const char *path);
     void (*font_unload)(int font_id);
     struct font* (*font_get)(int font);
     int  (*font_getstringsize)(const unsigned char *str, int *w, int *h,
                                int fontnumber);
-    int (*font_get_width)(struct font* pf, unsigned short char_code);
+    int (*font_get_width)(struct font* pf, ucschar_t char_code);
     void (*screen_clear_area)(struct screen * display, int xstart, int ystart,
                               int width, int height);
     void (*gui_scrollbar_draw)(struct screen * screen, int x, int y,
@@ -667,7 +667,7 @@ struct plugin_api {
                               const unsigned char * const *units,
                               unsigned int unit_count, bool binary_scale);
     /* unicode stuff */
-    const unsigned char* (*utf8decode)(const unsigned char *utf8, unsigned short *ucs);
+    const unsigned char* (*utf8decode)(const unsigned char *utf8, ucschar_t *ucs);
     unsigned char* (*iso_decode)(const unsigned char *iso, unsigned char *utf8, int cp, int count);
     unsigned char* (*utf16LEdecode)(const unsigned char *utf16, unsigned char *utf8, int count);
     unsigned char* (*utf16BEdecode)(const unsigned char *utf16, unsigned char *utf8, int count);
@@ -923,7 +923,7 @@ struct plugin_api {
     int  (*rand)(void);
     void (*qsort)(void *base, size_t nmemb, size_t size,
                   int(*compar)(const void *, const void *));
-    int (*kbd_input)(char* buffer, int buflen, unsigned short *kbd);
+    int (*kbd_input)(char* buffer, int buflen, ucschar_t *kbd);
     struct tm* (*get_time)(void);
     struct tm * (*gmtime_r)(const time_t *timep, struct tm *tm);
 #if CONFIG_RTC

apps/plugins/announce_status.c
@@ -70,7 +70,7 @@ static const char keybd_layout[] =
  * - \n does not create a key, but it also consumes one element
  * - the final null terminator is equivalent to \n
  * - since sizeof includes the null terminator we don't need +1 for that. */
-static unsigned short kbd_buf[sizeof(keybd_layout)];
+static ucschar_t kbd_buf[sizeof(keybd_layout)];
 
 /****************** prototypes ******************/
 void print_scroll(char* string); /* implements a scrolling screen */
@@ -164,7 +164,7 @@ static void config_set_defaults(void)
     gAnnounce.announce_on = 0;
     gAnnounce.grouping = 0;
     gAnnounce.wps_fmt[0] = '\0';
-    gAnnounce.show_prompt = true; 
+    gAnnounce.show_prompt = true;
 }
 
 static void config_reset_voice(void)
@@ -250,7 +250,7 @@ static int announce_menu_cb(int action,
                             struct gui_synclist *this_list)
 {
     (void)this_item;
-    unsigned short* kbd_p;
+    ucschar_t *kbd_p;
 
     int selection = rb->gui_synclist_get_sel_pos(this_list);
 

apps/plugins/frotz/frotz.c
@@ -128,7 +128,7 @@ void wait_for_key()
         case PLA_EXIT:
             hot_key_quit();
             break;
-        
+
         case PLA_SELECT:
             return;
         }
@@ -159,7 +159,7 @@ zchar do_input(int timeout, bool show_cursor)
         {
         case PLA_EXIT:
             return ZC_HKEY_QUIT;
-        
+
         case PLA_CANCEL:
             menu_ret = menu();
             if (menu_ret != ZC_BAD)
@@ -174,7 +174,7 @@ zchar do_input(int timeout, bool show_cursor)
             return ZC_BAD;
 
         default:
-            if (timeout != TIMEOUT_BLOCK && 
+            if (timeout != TIMEOUT_BLOCK &&
                     !TIME_BEFORE(*rb->current_tick, timeout_at))
                 return ZC_TIME_OUT;
         }
@@ -185,7 +185,7 @@ zchar os_read_key(int timeout, bool show_cursor)
 {
     int r;
     char inputbuf[5];
-    short key;
+    ucschar_t key;
     zchar zkey;
 
     for(;;)
@@ -214,7 +214,7 @@ zchar os_read_line(int max, zchar *buf, int timeout, int width, int continued)
     char inputbuf[256];
     const char *in;
     char *out;
-    short key;
+    ucschar_t key;
     zchar zkey;
 
     for(;;)

apps/plugins/keyremap.c
@@ -206,8 +206,8 @@ static int prompt_filename(char *buf, size_t bufsz)
 {
 #define KBD_LAYOUT "abcdefghijklmnop\nqrstuvwxyz |()[]\n1234567890 /._-+\n\n" \
                    "\nABCDEFGHIJKLMNOP\nQRSTUVWXYZ |()[]\n1234567890 /._-+"
-    unsigned short kbd[sizeof(KBD_LAYOUT) + 10];
-    unsigned short *kbd_p = kbd;
+    ucschar_t kbd[sizeof(KBD_LAYOUT) + 10];
+    ucschar_t *kbd_p = kbd;
     if (!kbd_create_layout(KBD_LAYOUT, kbd, sizeof(kbd)))
         kbd_p = NULL;
 
@@ -1002,7 +1002,7 @@ next_line:
                 {
                     bufleft = bufsz - (pctx - filenamebuf);
                     ctx = -1;
-                    int ctx_x_flag_count = (LAST_CONTEXT_PLACEHOLDER 
+                    int ctx_x_flag_count = (LAST_CONTEXT_PLACEHOLDER
                                            * ARRAYLEN(context_flags));
 
                     for (int i=0;i < ctx_x_flag_count ;i++)
@@ -2058,7 +2058,7 @@ static void synclist_set(int id, int selected_item, int items, int sel_size)
     }
     else if (menu_id == MENU_ID(M_SETKEYS))
     {
-        keyset.view_columns = printcell_set_columns(&lists, NULL, 
+        keyset.view_columns = printcell_set_columns(&lists, NULL,
                                                   ACTVIEW_HEADER, Icon_Rockbox);
         printcell_enable(true);
         int curcol = printcell_get_column_selected();

apps/plugins/lib/grey_draw.c
@@ -199,8 +199,8 @@ void grey_hline(int x1, int x2, int y)
     /* nothing to draw? */
     if (y < _grey_info.clip_t || y >= _grey_info.clip_b ||
         x1 >= _grey_info.clip_r || x2 < _grey_info.clip_l)
-        return;  
-    
+        return;
+
     /* drawmode and optimisation */
     if (vp->drawmode & DRMODE_INVERSEVID)
     {
@@ -251,7 +251,7 @@ void grey_vline(int x, int y1, int y2)
     unsigned char *dst, *dst_end;
     void (*pfunc)(unsigned char *address);
     int dwidth;
-    
+
     /* direction flip */
     if (y2 < y1)
     {
@@ -264,7 +264,7 @@ void grey_vline(int x, int y1, int y2)
     if (x < _grey_info.clip_l || x >= _grey_info.clip_r ||
         y1 >= _grey_info.clip_b || y2 < _grey_info.clip_t)
         return;
-    
+
     /* clipping */
     if (y1 < _grey_info.clip_t)
         y1 = _grey_info.clip_t;
@@ -425,7 +425,7 @@ void grey_fillrect(int x, int y, int width, int height)
 
     if (height <= 0)
         return;
-    
+
     dwidth  = _grey_info.cb_width;
     dst     = &_grey_info.curbuffer[
                 _GREY_MULUQ(dwidth, _grey_info.vp->y - _grey_info.cb_y + y) +
@@ -653,8 +653,8 @@ void grey_gray_bitmap(const unsigned char *src, int x, int y, int width,
 /* Put a string at a given pixel position, skipping first ofs pixel columns */
 void grey_putsxyofs(int x, int y, int ofs, const unsigned char *str)
 {
-    int ch;
-    unsigned short *ucs;
+    ucschar_t ch;
+    ucschar_t *ucs;
     struct font* pf;
 
     if (_grey_info.clip_b <= _grey_info.clip_t)
@@ -680,7 +680,7 @@ void grey_putsxyofs(int x, int y, int ofs, const unsigned char *str)
         bits = rb->font_get_bits(pf, ch);
 
         grey_mono_bitmap_part(bits, ofs, 0, width, x, y, width - ofs, pf->height);
-        
+
         x += width - ofs;
         ofs = 0;
     }
@@ -709,7 +709,7 @@ void grey_ub_clear_display(void)
 #endif
 }
 
-/* Assembler optimised helper function for copying a single line to the 
+/* Assembler optimised helper function for copying a single line to the
  * greyvalue buffer. */
 void _grey_line1(int width, unsigned char *dst, const unsigned char *src,
                  const unsigned char *lut);
@@ -725,7 +725,7 @@ void grey_ub_gray_bitmap_part(const unsigned char *src, int src_x, int src_y,
     if ((width <= 0) || (height <= 0) || (x >= _grey_info.width)
         || (y >= _grey_info.height) || (x + width <= 0) || (y + height <= 0))
         return;
-        
+
     /* clipping */
     if (x < 0)
     {
@@ -744,7 +744,7 @@ void grey_ub_gray_bitmap_part(const unsigned char *src, int src_x, int src_y,
     if (y + height > _grey_info.height)
         height = _grey_info.height - y;
 
-    src += _GREY_MULUQ(stride, src_y) + src_x; /* move starting point */ 
+    src += _GREY_MULUQ(stride, src_y) + src_x; /* move starting point */
     yc = y;
     ye = y + height;
     dst = _grey_info.values + (x << _GREY_BSHIFT);
@@ -773,7 +773,7 @@ void grey_ub_gray_bitmap_part(const unsigned char *src, int src_x, int src_y,
         }
         while (src_row < src_end);
 #endif
-        
+
         src += stride;
     }
     while (++yc < ye);

commit c72ffa7a9ae5b5da7533a84d45fd91ee22ddc72f

diacritic:  Critical bugfixes
 * When the codepoint > 0xffff, don't overflow past the end
   of our diacritic table (Fixed by William Wilgus)
 * Truncation of the 'info' field causes the RTL flag to be dropped
   (RTL flag is b15 but we truncate it into an 8-bit variable)

Both bugs introduced in a2c10f6189 (September 2025)

Change-Id: Id5425606f2cf91d3b3a81f4b67a97d546de81e41

firmware/common/diacritic.c
@@ -203,9 +203,15 @@ bool is_diacritic(const ucschar_t char_code, bool *is_rtl)
     static uint8_t diacritic_mru[MRU_MAX_LEN];
 
     uint8_t i, itmp;
-    uint8_t info, mru;
+    uint8_t mru;
+    uint16_t info;
 
-    const struct diac_range *diac;
+    const struct diac_range *diac = &diac_ranges[DIAC_NUM_RANGES - 1];
+
+    /* If the codepoint exceeds the terminating entry in our table
+       then treat it as non-diacritic. */
+    if (char_code >= diac->base)
+        goto bypass;
 
     /* Search in MRU */
     for (mru = 0, i = 0; mru < mru_len; mru++)
@@ -244,6 +250,8 @@ Found:
     diacritic_mru[0] = i;
 
     diac = &diac_ranges[i];
+
+bypass:
     info = diac->info;
 
     /* Update RTL */