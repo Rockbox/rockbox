Rockbox/rockbox

commit c145d19e853cbb4c05bd03ae061b73118868c6ea

gui: align display updates, reduce UI glitches
Based on commits ce33902 and 8990d52 (without PictureFlow)
from "Rockpod" fork by Nux Li (https://github.com/nuxcodes/rockpod),
with some adjustments.

Addresses flickering when:
- plugin is opened/closed
- activity changes
- theme is toggled
- QuickScreen is opened

In these cases, skin_render will not immediately update the display
anymore, but instead will wait until the UI viewport is ready to be
drawn as well, so we don't produce unnecessary visual glitches.

Change-Id: I8bed8f06221d3e767a32450f199e69d742bc61cd

apps/gui/skin_engine/skin_render.c
@@ -84,6 +84,8 @@ static void skin_render_playlistviewer(struct playlistviewer* viewer,
                                        unsigned long refresh_type);
 
 static char* skin_buffer;
+static bool defer_rendering;
+static bool dirty;
 
 static inline struct skin_element*
 get_child(OFFSETTYPE(struct skin_element**) children, int child)
@@ -841,6 +843,27 @@ void skin_render_viewport(struct skin_element* viewport, struct gui_wps *gwps,
     wps_display_images(gwps, &skin_viewport->vp);
 }
 
+void skin_defer_rendering(bool deferred)
+{
+    defer_rendering = deferred;
+}
+
+void skin_render_deferred(struct screen *display, struct viewport *vp)
+{
+    if (dirty)
+    {
+        dirty = false;
+        display->set_viewport(NULL);
+        display->update();
+        sb_skin_force_next_update();
+    }
+    else
+    {
+        display->set_viewport(vp);
+        display->update_viewport();
+    }
+}
+
 void skin_render(struct gui_wps *gwps, unsigned refresh_mode)
 {
     const int vp_is_appearing = (VP_DRAW_WASHIDDEN|VP_DRAW_HIDEABLE);
@@ -940,7 +963,10 @@ void skin_render(struct gui_wps *gwps, unsigned refresh_mode)
     }
     /* Restore the default viewport */
     display->set_viewport_ex(NULL, VP_FLAG_VP_SET_CLEAN);
-    display->update();
+    if (defer_rendering)
+        dirty = true;
+    else
+        display->update();
 }
 
 static __attribute__((noinline))

apps/gui/bitmap/list-skinned.c
@@ -281,8 +281,7 @@ bool skinlist_draw(struct screen *display, struct gui_synclist *list)
     }
     current_column = -1;
     current_row = -1;
-    display->set_viewport(parent);
-    display->update_viewport();
+    skin_render_deferred(display, parent);
     current_drawing_line = list->selected_item;
     return true;
 }

apps/gui/bitmap/list.c
@@ -446,8 +446,7 @@ void list_draw(struct screen *display, struct gui_synclist *list)
 
         callback_draw_item(&list_info);
     }
-    display->set_viewport(parent);
-    display->update_viewport();
+    skin_render_deferred(display, parent);
     display->set_viewport(last_vp);
 }
 

apps/gui/quickscreen.c
@@ -242,8 +242,7 @@ static void gui_quickscreen_draw(const struct gui_quickscreen *qs,
             (vp_icons->width/2) - 4, vp_icons->height - 8, 7, 8);
     }
 
-    display->set_viewport(parent);
-    display->update_viewport();
+    skin_render_deferred(display, parent);
     display->set_viewport(last_vp);
 }
 

apps/gui/skin_engine/skin_engine.h
@@ -51,6 +51,11 @@ void skin_disarm_touchregions(struct gui_wps *gwps);
 void skin_update(enum skinnable_screens skin, enum screen_type screen,
                  unsigned int update_type);
 
+/* Defer updates in skin_render */
+void skin_defer_rendering(bool deferred);
+/* Render viewport together with deferred updates */
+void skin_render_deferred(struct screen *display, struct viewport *vp);
+
 bool skin_has_sbs(struct gui_wps *gwps);
 
 

apps/gui/viewport.c
@@ -154,7 +154,11 @@ static void toggle_theme(enum screen_type screen, bool force)
         }
         intptr_t force = first_boot?0:1;
 
+        skin_defer_rendering(true);
         send_event(GUI_EVENT_ACTIONUPDATE, (void*)force);
+        skin_defer_rendering(false);
+        if (!first_boot)
+            sb_skin_force_next_update();
     }
     else
     {

apps/gui/wps.c
@@ -533,7 +533,11 @@ static void gwps_leave_wps(bool theme_enabled)
                viewports drawn by the WPS. May need further thought... */
             struct wps_data *sbs = skin_get_gwps(CUSTOM_STATUSBAR, i)->data;
             if (gwps->data->use_extra_framebuffer && sbs->use_extra_framebuffer)
+            {
+                skin_defer_rendering(true);
                 skin_update(CUSTOM_STATUSBAR, i, SKIN_REFRESH_ALL);
+                skin_defer_rendering(false);
+            }
 #endif
             viewportmanager_theme_undo(i, skin_has_sbs(gwps));
         }

apps/misc.c
@@ -65,6 +65,7 @@
 #include "list.h"
 #include "fixedpoint.h"
 #include "open_plugin.h"
+#include "statusbar-skinned.h"
 
 #include "debug.h"
 
@@ -1795,8 +1796,14 @@ static void push_current_activity_refresh(enum current_activity screen, bool ref
     {
         skinlist_set_cfg(i, NULL);
         if (refresh)
+        {
+            skin_defer_rendering(true);
             skin_update(CUSTOM_STATUSBAR, i, SKIN_REFRESH_ALL);
+            skin_defer_rendering(false);
+        }
     }
+    if (refresh)
+        sb_skin_force_next_update();
 }
 
 static void pop_current_activity_refresh(bool refresh)
@@ -1806,8 +1813,14 @@ static void pop_current_activity_refresh(bool refresh)
     {
         skinlist_set_cfg(i, NULL);
         if (refresh)
+        {
+            skin_defer_rendering(true);
             skin_update(CUSTOM_STATUSBAR, i, SKIN_REFRESH_ALL);
+            skin_defer_rendering(false);
+        }
     }
+    if (refresh)
+        sb_skin_force_next_update();
 }
 
 void push_current_activity(enum current_activity screen)

commit 05f1a6605d894a9448b3d44d9c56dc33571c17f5

gui: skin_engine: fix dirty & force_waiting ignoring multiple screens
In part regression introduced in c145d19e85.

force_waiting not taking multiple screens into
account appears to be pre-existing issue.

Change-Id: Iabfc2933470145eb512c8f2763fb350e170cb1fa

apps/gui/skin_engine/skin_render.c
@@ -85,7 +85,7 @@ static void skin_render_playlistviewer(struct playlistviewer* viewer,
 
 static char* skin_buffer;
 static bool defer_rendering;
-static bool dirty;
+static bool dirty[NB_SCREENS];
 
 static inline struct skin_element*
 get_child(OFFSETTYPE(struct skin_element**) children, int child)
@@ -297,7 +297,7 @@ static bool do_non_text_tags(struct gui_wps *gwps, struct skin_draw_info *info,
             {
                 struct skin_albumart *aa = SKINOFFSETTOPTR(skin_buffer, data->albumart);
                 if (aa)
-                {    
+                {
                     int handle = playback_current_aa_hid(data->playback_aa_slot);
 #if CONFIG_TUNER
                     if (in_radio_screen() || (get_radio_status() != FMRADIO_OFF))
@@ -850,9 +850,9 @@ void skin_defer_rendering(bool deferred)
 
 void skin_render_deferred(struct screen *display, struct viewport *vp)
 {
-    if (dirty)
+    if (dirty[display->screen_type])
     {
-        dirty = false;
+        dirty[display->screen_type] = false;
         display->set_viewport(NULL);
         display->update();
         sb_skin_force_next_update();
@@ -964,7 +964,7 @@ void skin_render(struct gui_wps *gwps, unsigned refresh_mode)
     /* Restore the default viewport */
     display->set_viewport_ex(NULL, VP_FLAG_VP_SET_CLEAN);
     if (defer_rendering)
-        dirty = true;
+        dirty[display->screen_type] = true;
     else
         display->update();
 }

apps/gui/statusbar-skinned.c
@@ -182,17 +182,18 @@ int sb_get_backdrop(enum screen_type screen)
     return -1;
 }
 #endif
-static bool force_waiting = false;
+static bool force_waiting[NB_SCREENS];
 void sb_skin_update(enum screen_type screen, bool force)
 {
     struct wps_data *data = skin_get_gwps(CUSTOM_STATUSBAR, screen)->data;
-    static long next_update[NB_SCREENS] = {0};
+    static long next_update[NB_SCREENS];
     int i = screen;
     if (!data->wps_loaded)
         return;
-    if (TIME_AFTER(current_tick, next_update[i]) || force || force_waiting)
+    if (TIME_AFTER(current_tick, next_update[i]) || force ||
+        force_waiting[i])
     {
-        force_waiting = false;
+        force_waiting[i] = false;
 #if defined(HAVE_LCD_ENABLE) || defined(HAVE_LCD_SLEEP)
         /* currently, all remotes are readable without backlight
          * so still update those */
@@ -214,7 +215,8 @@ void do_sbs_update_callback(unsigned short id, void *param)
     /* the WPS handles changing the actual id3 data in the id3 pointers
      * we imported, we just want a full update */
     skin_request_full_update(CUSTOM_STATUSBAR);
-    force_waiting = true;
+    FOR_NB_SCREENS(i)
+        force_waiting[i] = true;
     /* force timeout in wps main loop, so that the update is instantly */
     button_queue_post(BUTTON_NONE, 0);
 }
@@ -226,7 +228,8 @@ void sb_skin_set_update_delay(int delay)
 
 void sb_skin_force_next_update(void)
 {
-    force_waiting = true;
+    FOR_NB_SCREENS(i)
+        force_waiting[i] = true;
 }
 
 /* This creates and loads a ".sbs" based on the user settings for: