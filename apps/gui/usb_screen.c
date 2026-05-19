04920212479111e0d096a151bd760353ba954cc6
Christian Soffke <christian.soffke@gmail.com>
2026-05-19 23:23:39 +0200

    fix yellow in commit 13a0e58b1c
    Change-Id: Id2ec3d0ace5aab9d67aeda54f4cef52d0d111b01

--- apps/gui/usb_screen.c
+++ apps/gui/usb_screen.c
@@ -235,7 +235,7 @@ void gui_usb_screen_run(bool early_usb, intptr_t seqnum)
 #endif
 
     struct usb_screen_vps_t usb_screen_vps_ar[NB_SCREENS];
-    struct viewport *title;
+    struct viewport *title = NULL;
 #if defined HAVE_TOUCHSCREEN
     enum touchscreen_mode old_mode = touchscreen_get_mode();
 
