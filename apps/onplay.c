Rockbox/rockbox

commit dfd9c10589d25a966c7e2e4dedab57c59a1987fd

Eliminate skin updates in between activities
1) Adds way to pop activity without refreshing the skin at
the same time.

Activities are sometimes popped in immediate succession,
or one activity is popped before another one is pushed right
away. This can lead to the UI appearing glitchy, due to an
activity only appearing for a split-second, which is especially
noticeable with complex skins that change the dimensions
of the UI viewport depending on the current activity

To fix this, prevent superfluous skin updates

* when switching between:
- WPS and browser
- WPS and Playlist Catalogue
- WPS and playlist
- WPS and Settings/System/Plugins

* when accessing Track Info or when displaying
bookmarks using the context menu on the WPS

* when switching from QuickScreen to Shortcuts Menu

2) The playlist viewer activity was pushed & popped
redundantly by playlist_view.

----
NB:
Behavior has remained unchanged in all instances of the
code where pop_current_activity() has been replaced by
pop_current_activity(ACTIVITY_REFRESH_NOW).

Change-Id: I56b517b8c9dba823a9fed3a3f558d7469dcea9fd

apps/onplay.c
@@ -144,6 +144,15 @@ static bool clipboard_clip(struct clipboard *clip, const char *path,
 /* interface function.                                                     */
 /* ----------------------------------------------------------------------- */
 
+
+static int bookmark_load_menu_wrapper(void)
+{
+    if (get_current_activity() == ACTIVITY_CONTEXTMENU)  /* get rid of parent activity */
+        pop_current_activity(ACTIVITY_REFRESH_DEFERRED); /* when called from ctxt menu */
+
+    return bookmark_load_menu();
+}
+
 static int bookmark_menu_callback(int action,
                                   const struct menu_item_ex *this_item,
                                   struct gui_synclist *this_list);
@@ -153,7 +162,7 @@ MENUITEM_FUNCTION(bookmark_create_menu_item, 0,
                   bookmark_menu_callback, Icon_Bookmark);
 MENUITEM_FUNCTION(bookmark_load_menu_item, 0,
                   ID2P(LANG_BOOKMARK_MENU_LIST),
-                  bookmark_load_menu, NULL,
+                  bookmark_load_menu_wrapper, NULL,
                   bookmark_menu_callback, Icon_Bookmark);
 MAKE_ONPLAYMENU(bookmark_menu, ID2P(LANG_BOOKMARK_MENU),
                 bookmark_menu_callback, Icon_Bookmark,
@@ -462,7 +471,18 @@ static bool save_playlist(void)
     return false;
 }
 
-extern struct menu_item_ex view_cur_playlist; /* from playlist_menu.c */
+static int wps_view_cur_playlist(void)
+{
+    if (get_current_activity() == ACTIVITY_CONTEXTMENU)  /* get rid of parent activity */
+        pop_current_activity(ACTIVITY_REFRESH_DEFERRED); /* when called from ctxt menu */
+
+    playlist_viewer_ex(NULL, NULL);
+
+    return 0;
+}
+
+MENUITEM_FUNCTION(wps_view_cur_playlist_item, 0, ID2P(LANG_VIEW_DYNAMIC_PLAYLIST),
+                  wps_view_cur_playlist, NULL, NULL, Icon_NOICON);
 MENUITEM_FUNCTION(search_playlist_item, 0, ID2P(LANG_SEARCH_IN_PLAYLIST),
                   search_playlist, NULL, NULL, Icon_Playlist);
 MENUITEM_FUNCTION(playlist_save_item, 0, ID2P(LANG_SAVE_DYNAMIC_PLAYLIST),
@@ -473,7 +493,7 @@ MENUITEM_FUNCTION(playing_time_item, 0, ID2P(LANG_PLAYING_TIME),
                   playing_time, NULL, NULL, Icon_Playlist);
 MAKE_ONPLAYMENU( wps_playlist_menu, ID2P(LANG_PLAYLIST),
                  NULL, Icon_Playlist,
-                 &view_cur_playlist, &search_playlist_item,
+                 &wps_view_cur_playlist_item, &search_playlist_item,
                  &playlist_save_item, &reshuffle_item, &playing_time_item
                );
 
@@ -1499,6 +1519,9 @@ MENUITEM_FUNCTION(view_cue_item, 0, ID2P(LANG_BROWSE_CUESHEET),
 
 static int browse_id3_wrapper(void)
 {
+    if (get_current_activity() == ACTIVITY_CONTEXTMENU)  /* get rid of parent activity */
+        pop_current_activity(ACTIVITY_REFRESH_DEFERRED); /* when called from ctxt menu */
+
     if (browse_id3(audio_current_track(),
             playlist_get_display_index(),
             playlist_amount()))
@@ -1954,7 +1977,9 @@ int onplay(char* file, int attr, int from, bool hotkey)
     else
         menu = &tree_onplay_menu;
     menu_selection = do_menu(menu, NULL, NULL, false);
-    pop_current_activity();
+
+    if (get_current_activity() == ACTIVITY_CONTEXTMENU) /* Activity may have been      */
+        pop_current_activity(ACTIVITY_REFRESH_NOW);     /* popped already by menu item */
 
     switch (menu_selection)
     {

apps/bookmark.c
@@ -1122,7 +1122,7 @@ int bookmark_load_menu(void)
         }
     }
 
-    pop_current_activity();
+    pop_current_activity(ACTIVITY_REFRESH_NOW);
     return ret;
 }
 
@@ -1143,7 +1143,7 @@ bool bookmark_mrb_load()
         ret = play_bookmark(bookmark);
     }
 
-    pop_current_activity();
+    pop_current_activity(ACTIVITY_REFRESH_NOW);
     return ret;
 }
 

apps/gui/option_select.c
@@ -574,7 +574,7 @@ bool option_screen(const struct settings_list *setting,
         if (function == sound_get_fn(SOUND_VOLUME))
             global_status.last_volume_change = current_tick;
     }
-    pop_current_activity();
+    pop_current_activity(ACTIVITY_REFRESH_NOW);
     return false;
 }
 

apps/gui/quickscreen.c
@@ -406,10 +406,13 @@ static int gui_syncquickscreen_run(struct gui_quickscreen * qs, int button_enter
     {   /* stop scrolling before exiting */
         for (int j = 0; j < QUICKSCREEN_ITEM_COUNT; j++)
             screens[i].scroll_stop_viewport(&vps[i][j]);
-        viewportmanager_theme_undo(i, true);
+        viewportmanager_theme_undo(i, !(ret & QUICKSCREEN_GOTO_SHORTCUTS_MENU));
     }
 
-    pop_current_activity();
+    if (ret & QUICKSCREEN_GOTO_SHORTCUTS_MENU) /* Eliminate flashing of parent during */
+        pop_current_activity(ACTIVITY_REFRESH_DEFERRED);   /* transition to Shortcuts */
+    else
+        pop_current_activity(ACTIVITY_REFRESH_NOW);
 
     return ret;
 }

apps/gui/usb_screen.c
@@ -315,5 +315,5 @@ void gui_usb_screen_run(bool early_usb)
         viewportmanager_theme_undo(i, false);
     }
 
-    pop_current_activity();
+    pop_current_activity(ACTIVITY_REFRESH_NOW);
 }

apps/menus/playlist_menu.c
@@ -138,6 +138,8 @@ int save_playlist_screen(struct playlist_info* playlist)
 static int playlist_view_(void)
 {
     playlist_viewer_ex(NULL, NULL);
+    FOR_NB_SCREENS(i) /* Playlist Viewer defers skin updates when popping its activity */
+        skin_update(CUSTOM_STATUSBAR, i, SKIN_REFRESH_ALL);
     return 0;
 }
 MENUITEM_FUNCTION(create_playlist_item, 0, ID2P(LANG_CREATE_PLAYLIST),

apps/menus/time_menu.c
@@ -304,7 +304,7 @@ int time_screen(void* ignored)
 #endif
 
     ret = do_menu(&time_menu, NULL, menu, false);
-    pop_current_activity();
+    pop_current_activity(ACTIVITY_REFRESH_NOW);
     /* see comments above in the button callback */
     if (!menu_was_pressed && ret == GO_TO_PREVIOUS)
         return 0;

apps/misc.c
@@ -1579,13 +1579,14 @@ void push_current_activity(enum current_activity screen)
     }
 }
 
-void pop_current_activity(void)
+void pop_current_activity(enum activity_refresh refresh)
 {
     current_activity_top--;
     FOR_NB_SCREENS(i)
     {
         skinlist_set_cfg(i, NULL);
-        skin_update(CUSTOM_STATUSBAR, i, SKIN_REFRESH_ALL);
+        if (ACTIVITY_REFRESH_NOW == refresh)
+            skin_update(CUSTOM_STATUSBAR, i, SKIN_REFRESH_ALL);
     }
 }
 enum current_activity get_current_activity(void)

commit 018994e8c767f4d5b345c986cd704ecf107ca2ea

gui: skinned lists: fix off-screen selection
The skinlist cfg may not have been restored in time
after leaving the onplay tree menu, because the skin
isn't updated before gui_synclist_select_item is called.

This can result in the wrong number of visible lines
being calculated for the list, and a selection that
appears off-screen.

Regression introduced in commit dfd9c10, or later.
Due to the recent drawing changes, reverting the
changes to onplay.c doesn't re-introduce any visual
glitches.

Change-Id: I853bdfe6b6ba39e2eb281534ca302a0a828253b9

apps/onplay.c
@@ -139,15 +139,6 @@ static bool clipboard_clip(struct clipboard *clip, const char *path,
 /* interface function.                                                     */
 /* ----------------------------------------------------------------------- */
 
-
-static int bookmark_load_menu_wrapper(void)
-{
-    if (get_current_activity() == ACTIVITY_CONTEXTMENU)  /* get rid of parent activity */
-        pop_current_activity_without_refresh();          /* when called from ctxt menu */
-
-    return bookmark_load_menu();
-}
-
 static int bookmark_menu_callback(int action,
                                   const struct menu_item_ex *this_item,
                                   struct gui_synclist *this_list);
@@ -157,7 +148,7 @@ MENUITEM_FUNCTION(bookmark_create_menu_item, 0,
                   bookmark_menu_callback, Icon_Bookmark);
 MENUITEM_FUNCTION(bookmark_load_menu_item, 0,
                   ID2P(LANG_BOOKMARK_MENU_LIST),
-                  bookmark_load_menu_wrapper,
+                  bookmark_load_menu,
                   bookmark_menu_callback, Icon_Bookmark);
 MAKE_ONPLAYMENU(bookmark_menu, ID2P(LANG_BOOKMARK_MENU),
                 bookmark_menu_callback, Icon_Bookmark,
@@ -203,9 +194,6 @@ static bool save_playlist(void)
 
 static int wps_view_cur_playlist(void)
 {
-    if (get_current_activity() == ACTIVITY_CONTEXTMENU)  /* get rid of parent activity */
-        pop_current_activity_without_refresh(); /* when called from ctxt menu */
-
     playlist_viewer_ex(NULL, NULL);
 
     return 0;
@@ -768,7 +756,7 @@ WPSCTX_RETURNVALUE_DYNTEXT(context_item_4, GO_TO_PREVIOUS, wps_context_item_cb,
   wps_context_get_item_name, wps_context_item_speak_item, (void*)4, Icon_NOICON);
 
 /* map item number to menu_get_name_and_icon structs so we can change icons */
-static struct menu_get_name_and_icon * const ctx_item_map[HK_CTX_ITEMS]= 
+static struct menu_get_name_and_icon * const ctx_item_map[HK_CTX_ITEMS]=
   {&context_item_0_, &context_item_1_, &context_item_2_, &context_item_3_, &context_item_4_};
 
 #ifdef HAVE_PITCHCONTROL
@@ -875,9 +863,6 @@ MENUITEM_FUNCTION(view_cue_item, 0, ID2P(LANG_BROWSE_CUESHEET),
 
 static int browse_id3_wrapper(void)
 {
-    if (get_current_activity() == ACTIVITY_CONTEXTMENU)  /* get rid of parent activity */
-        pop_current_activity_without_refresh();          /* when called from ctxt menu */
-
     if (browse_id3(audio_current_track(),
             playlist_get_display_index(),
             playlist_amount(), NULL, 1, NULL))
@@ -1006,10 +991,6 @@ static bool onplay_load_plugin(void *param)
     if (!prepare_database_sel(param))
         return false;
 #endif
-
-    if (get_current_activity() == ACTIVITY_CONTEXTMENU)  /* get rid of parent activity */
-        pop_current_activity_without_refresh();          /* when called from ctxt menu */
-
     int ret = filetype_load_plugin((const char*)param, selected_file.path);
     if (ret == PLUGIN_USB_CONNECTED)
         onplay_result = ONPLAY_RELOAD_DIR;
@@ -1727,9 +1708,7 @@ int onplay(char* file, int attr, int from_context, bool hotkey, int customaction
     else
         menu = &tree_onplay_menu;
     menu_selection = do_menu(menu, NULL, NULL, false);
-
-    if (get_current_activity() == ACTIVITY_CONTEXTMENU) /* Activity may have been      */
-        pop_current_activity();                         /* popped already by menu item */
+    pop_current_activity();
 
     if (menu_selection == GO_TO_WPS)
         return ONPLAY_START_PLAY;