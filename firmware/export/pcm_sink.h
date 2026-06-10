Rockbox/rockbox

commit dfa33c246b5f0244e0703722c63fe91f94633a97

pcm: introduce pcm_sink
move target-specific pcm operations into builtin_pcm_sink.
in subsequent commits, another pcm_sink is added, and it becomes
possible to switch between them.

Change-Id: I8f8b9661e01d6e6472f34224ddc3760856778457

firmware/export/pcm_sink.h
@@ -1,0 +1,57 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2025 by Sho Tanimoto
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#pragma once
+#include <stddef.h>
+#include <stdint.h>
+
+struct pcm_sink_caps {
+    const unsigned long* samprs;
+    uint16_t             num_samprs;
+    uint16_t             default_freq;
+};
+
+struct pcm_sink_ops {
+    void (*init)(void);
+    void (*postinit)(void);
+    void (*set_freq)(uint16_t freq);
+    void (*lock)(void);
+    void (*unlock)(void);
+    void (*play)(const void* addr, size_t size);
+    void (*stop)(void);
+};
+
+struct pcm_sink {
+    /* characteristics */
+    const struct pcm_sink_caps caps;
+
+    /* operations */
+    const struct pcm_sink_ops ops;
+
+    /* runtime states */
+    unsigned long pending_freq;
+    unsigned long configured_freq;
+};
+
+enum pcm_sink_ids {
+    PCM_SINK_BUILTIN = 0,
+};
+
+/* defined in each platform pcm source */
+extern struct pcm_sink builtin_pcm_sink;

firmware/pcm.c
@@ -42,21 +42,19 @@
  *   Semi-private -
  *      pcm_play_dma_complete_callback
  *      pcm_play_dma_status_callback
- *      pcm_play_dma_init
- *      pcm_play_dma_postinit
- *      pcm_play_dma_start
- *      pcm_play_dma_stop
+ *      pcm_get_current_sink
+ *      pcm_sink.init
+ *      pcm_sink.postinit
+ *      pcm_sink.play
+ *      pcm_sink.stop
  *   Data Read/Written within TSP -
- *      pcm_sampr (R)
- *      pcm_fsel (R)
- *      pcm_curr_sampr (R)
  *      pcm_playing (R)
  *
  * ==Playback/Recording==
  *   Public -
  *      pcm_dma_addr
  *   Semi-private -
- *      pcm_dma_apply_settings
+ *      pcm_sink.set_freq
  *
  * ==Recording==
  *   Public -
@@ -81,6 +79,12 @@
 /* 'true' when all stages of pcm initialization have completed */
 static bool pcm_is_ready = false;
 
+static struct pcm_sink* sinks[1] = {
+    [PCM_SINK_BUILTIN] = &builtin_pcm_sink,
+};
+static enum pcm_sink_ids cur_sink = PCM_SINK_BUILTIN;
+static struct mutex sink_mutex; /* protects sinks and cur_sink */
+
 /* The registered callback function to ask for more mp3 data */
 volatile pcm_play_callback_type
     pcm_callback_for_more SHAREDBSS_ATTR = NULL;
@@ -89,15 +93,13 @@ volatile pcm_status_callback_type
     pcm_play_status_callback SHAREDBSS_ATTR = NULL;
 /* PCM playback state */
 volatile bool pcm_playing SHAREDBSS_ATTR = false;
-/* samplerate of currently playing audio - undefined if stopped */
-unsigned long pcm_curr_sampr SHAREDBSS_ATTR = 0;
-/* samplerate waiting to be set */
-unsigned long pcm_sampr SHAREDBSS_ATTR = HW_SAMPR_DEFAULT;
-/* samplerate frequency selection index */
-int pcm_fsel SHAREDBSS_ATTR = HW_FREQ_DEFAULT;
 
-static void pcm_play_data_start_int(const void *addr, size_t size);
 void pcm_play_stop_int(void);
+
+struct pcm_sink* pcm_get_current_sink(void)
+{
+    return sinks[cur_sink];
+}
 
 #if !defined(HAVE_SW_VOLUME_CONTROL) || defined(PCM_SW_VOLUME_UNBUFFERED)
 /** Standard hw volume/unbuffered control functions - otherwise, see
@@ -108,12 +110,12 @@ static inline void pcm_play_dma_start_int(const void *addr, size_t size)
     /* Smoothed transition might not have happened so sync now */
     pcm_sync_pcm_factors();
 #endif
-    pcm_play_dma_start(addr, size);
+    sinks[cur_sink]->ops.play(addr, size);
 }
 
 static inline void pcm_play_dma_stop_int(void)
 {
-    pcm_play_dma_stop();
+    sinks[cur_sink]->ops.stop();
 }
 
 bool pcm_play_dma_complete_callback(enum pcm_dma_status status,
@@ -123,33 +125,15 @@ bool pcm_play_dma_complete_callback(enum pcm_dma_status status,
     if (status < PCM_DMAST_OK)
         status = pcm_play_dma_status_callback(status);
 
-    if (status >= PCM_DMAST_OK && pcm_get_more_int(addr, size))
+    if (status >= PCM_DMAST_OK && pcm_get_more_int(addr, size)) {
         return true;
+    }
 
     /* Error, callback missing or no more DMA to do */
     pcm_play_stop_int();
     return false;
 }
 #endif /* !HAVE_SW_VOLUME_CONTROL || PCM_SW_VOLUME_UNBUFFERED */
-
-static void pcm_play_data_start_int(const void *addr, size_t size)
-{
-    ALIGN_AUDIOBUF(addr, size);
-
-    if ((addr && size) || pcm_get_more_int(&addr, &size))
-    {
-        pcm_apply_settings();
-        logf(" pcm_play_dma_start_int");
-        pcm_play_dma_start_int(addr, size);
-        pcm_playing = true;
-    }
-    else
-    {
-        /* Force a stop */
-        logf(" pcm_play_stop_int");
-        pcm_play_stop_int();
-    }
-}
 
 void pcm_play_stop_int(void)
 {
@@ -223,7 +207,15 @@ void pcm_do_peak_calculation(struct pcm_peaks *peaks, bool active,
 
     if (active)
     {
-        int framecount = peaks->period*pcm_curr_sampr / HZ;
+        struct pcm_sink* sink = sinks[cur_sink];
+        if (sink->configured_freq == -1UL)
+        {
+            logf("not configured yet");
+            return;
+        }
+
+        unsigned long sampr = sink->caps.samprs[sink->configured_freq];
+        int framecount = peaks->period * sampr / HZ;
         count = MIN(framecount, count);
 
         if (count > 0)
@@ -247,16 +239,29 @@ bool pcm_is_playing(void)
  * interface
  */
 
+void pcm_play_lock(void) {
+    mutex_lock(&sink_mutex);
+    sinks[cur_sink]->ops.lock();
+    /* hold sink_mutex until pcm_play_unlock() */
+}
+
+void pcm_play_unlock(void) {
+    sinks[cur_sink]->ops.unlock();
+    mutex_unlock(&sink_mutex);
+}
+
 /* This should only be called at startup before any audio playback or
    recording is attempted */
 void pcm_init(void)
 {
     logf("pcm_init");
 
-    pcm_set_frequency(HW_SAMPR_DEFAULT);
-
-    logf(" pcm_play_dma_init");
-    pcm_play_dma_init();
+    mutex_init(&sink_mutex);
+    for(size_t i = 0; i < ARRAYLEN(sinks); i += 1) {
+        sinks[i]->pending_freq = sinks[i]->caps.default_freq;
+        sinks[i]->configured_freq = -1UL;
+        sinks[i]->ops.init();
+    }
 }
 
 /* Finish delayed init */
@@ -264,9 +269,9 @@ void pcm_postinit(void)
 {
     logf("pcm_postinit");
 
-    logf(" pcm_play_dma_postinit");
-
-    pcm_play_dma_postinit();
+    for(size_t i = 0; i < ARRAYLEN(sinks); i += 1) {
+        sinks[i]->ops.postinit();
+    }
 
     pcm_is_ready = true;
 }
@@ -274,6 +279,21 @@ void pcm_postinit(void)
 bool pcm_is_initialized(void)
 {
     return pcm_is_ready;
+}
+
+enum pcm_sink_ids pcm_current_sink(void)
+{
+    return cur_sink;
+}
+
+const struct pcm_sink_caps* pcm_sink_caps(enum pcm_sink_ids sink)
+{
+    return &sinks[sink]->caps;
+}
+
+const struct pcm_sink_caps* pcm_current_sink_caps(void)
+{
+    return pcm_sink_caps(pcm_current_sink());
 }
 
 void pcm_play_data(pcm_play_callback_type get_more,
@@ -287,8 +307,20 @@ void pcm_play_data(pcm_play_callback_type get_more,
     pcm_callback_for_more = get_more;
     pcm_play_status_callback = status_cb;
 
-    logf(" pcm_play_data_start_int");
-    pcm_play_data_start_int(start, size);
+    ALIGN_AUDIOBUF(start, size);
+    if ((start && size) || pcm_get_more_int(&start, &size))
+    {
+        pcm_apply_settings();
+        logf(" pcm_play_dma_start_int");
+        pcm_play_dma_start_int(start, size);
+        pcm_playing = true;
+    }
+    else
+    {
+        /* Force a stop */
+        logf(" pcm_play_stop_int");
+        pcm_play_stop_int();
+    }
 
     pcm_play_unlock();
 }
@@ -329,20 +361,22 @@ void pcm_set_frequency(unsigned int samplerate)
     samplerate = pcm_sampr_to_hw_sampr(samplerate, type);
 #endif /* CONFIG_SAMPR_TYPES */
 
-    index = round_value_to_list32(samplerate, hw_freq_sampr,
-                                  HW_NUM_FREQ, false);
+    mutex_lock(&sink_mutex);
+    struct pcm_sink* sink = sinks[cur_sink];
+    index = round_value_to_list32(samplerate, sink->caps.samprs, sink->caps.num_samprs, false);
 
-    if (samplerate != hw_freq_sampr[index])
-        index = HW_FREQ_DEFAULT; /* Invalid = default */
+    if (samplerate != sink->caps.samprs[index])
+        index = sink->caps.default_freq; /* Invalid = default */
 
-    pcm_sampr = hw_freq_sampr[index];
-    pcm_fsel = index;
+    sink->pending_freq = index;
+    mutex_unlock(&sink_mutex);
 }
 
 /* return last-set frequency */
 unsigned int pcm_get_frequency(void)
 {
-    return pcm_sampr;
+    struct pcm_sink* sink = sinks[cur_sink];
+    return sink->caps.samprs[sink->pending_freq];
 }
 
 /* apply pcm settings to the hardware */
@@ -352,12 +386,14 @@ void pcm_apply_settings(void)
 
     pcm_wait_for_init();
 
-    if (pcm_sampr != pcm_curr_sampr)
-    {
-        logf(" pcm_dma_apply_settings");
-        pcm_dma_apply_settings();
-        pcm_curr_sampr = pcm_sampr;
+    mutex_lock(&sink_mutex);
+    struct pcm_sink* sink = sinks[cur_sink];
+    if(sink->pending_freq != sink->configured_freq) {
+        logf(" sink->set_freq");
+        sink->ops.set_freq(sink->pending_freq);
+        sink->configured_freq = sink->pending_freq;
     }
+    mutex_unlock(&sink_mutex);
 }
 
 #ifdef HAVE_RECORDING

apps/plugin.c
@@ -647,6 +647,7 @@ static const struct plugin_api rockbox_api = {
     pcm_apply_settings,
     pcm_play_lock,
     pcm_play_unlock,
+    pcm_current_sink_caps,
     beep_play,
 #ifdef HAVE_RECORDING
     &rec_freq_sampr[0],

apps/plugin.h
@@ -93,6 +93,7 @@ int plugin_open(const char *plugin, const char *parameter);
 #include "misc.h"
 #include "pathfuncs.h"
 #include "pcm_mixer.h"
+#include "pcm_sink.h"
 #include "dsp-util.h"
 #include "dsp_core.h"
 #include "dsp_proc_settings.h"
@@ -745,6 +746,7 @@ struct plugin_api {
     void (*pcm_apply_settings)(void);
     void (*pcm_play_lock)(void);
     void (*pcm_play_unlock)(void);
+    const struct pcm_sink_caps* (*pcm_current_sink_caps)(void);
     void (*beep_play)(unsigned int frequency, unsigned int duration,
                       unsigned int amplitude);
 #ifdef HAVE_RECORDING

docs/PLUGIN_API
@@ -1783,6 +1783,10 @@ void pcm_play_unlock(void)
     \group sound
     \description
 
+const struct pcm_sink_caps* pcm_current_sink_caps(enum pcm_sink_ids sink)
+    \group sound
+    \description
+
 void pcm_record_data(pcm_rec_callback_type more_ready, pcm_status_callback_type status_cb, void *start, size_t size)
     \group sound
     \conditions (defined(HAVE_RECORDING))

firmware/export/pcm-internal.h
@@ -22,7 +22,11 @@
 #ifndef PCM_INTERNAL_H
 #define PCM_INTERNAL_H
 
+#include <stdbool.h>
+
 #include "config.h"
+#include "pcm.h"
+#include "pcm_sink.h"
 #include "gcc_extensions.h" /* for FORCE_INLINE */
 
 #ifdef HAVE_SW_VOLUME_CONTROL
@@ -129,24 +133,12 @@ void pcm_play_stop_int(void);
 bool pcm_play_dma_complete_callback(enum pcm_dma_status status,
                                     const void **addr, size_t *size);
 
-extern unsigned long pcm_curr_sampr;
-extern unsigned long pcm_sampr;
-extern int pcm_fsel;
-
 #ifdef HAVE_PCM_DMA_ADDRESS
 void * pcm_dma_addr(void *addr);
 #endif
 
 extern volatile bool pcm_playing;
-
-void pcm_play_dma_lock(void);
-void pcm_play_dma_unlock(void);
-void pcm_play_dma_init(void) INIT_ATTR;
-void pcm_play_dma_postinit(void);
-void pcm_play_dma_start(const void *addr, size_t size);
-void pcm_play_dma_stop(void);
-
-void pcm_dma_apply_settings(void);
+struct pcm_sink* pcm_get_current_sink(void);
 
 #ifdef HAVE_RECORDING
 

firmware/export/pcm.h
@@ -71,6 +71,12 @@ void pcm_init(void) INIT_ATTR;
 void pcm_postinit(void);
 bool pcm_is_initialized(void);
 
+enum pcm_sink_ids pcm_current_sink(void);
+const struct pcm_sink_caps* pcm_sink_caps(enum pcm_sink_ids sink);
+
+/* shortcut for plugins */
+const struct pcm_sink_caps* pcm_current_sink_caps(void);
+
 /* This is for playing "raw" PCM data */
 void pcm_play_data(pcm_play_callback_type get_more,
                    pcm_status_callback_type status_cb,

firmware/pcm_sw_volume.c
@@ -325,7 +325,7 @@ static void start_pcm(bool reframe)
     pcm_play_dma_status_callback(PCM_DMAST_STARTED);
     pcm_play_dma_status_callback(PCM_DMAST_STARTED);
 
-    pcm_play_dma_start(pcm_dbl_buf[1], pcm_dbl_buf_size[1]);
+    pcm_get_current_sink()->ops.play(pcm_dbl_buf[1], pcm_dbl_buf_size[1]);
 }
 
 void pcm_play_dma_start_int(const void *addr, size_t size)
@@ -338,7 +338,7 @@ void pcm_play_dma_start_int(const void *addr, size_t size)
 
 void pcm_play_dma_stop_int(void)
 {
-    pcm_play_dma_stop();
+    pcm_get_current_sink()->ops.stop();
     src_buf_addr = NULL;
     src_buf_rem = 0;
 }

commit 59a42fbf61fd5e6fa978c4f0d0184bff8cb0d7c7

pcm:  Pull pcm_is_ready back out of the sink structure
It is referenced by multiple threads, and as such needs to be both
volatile and SHAREDBSS to keep everyone happy.

...should address hangs on startup observed on nano2g and mini2g when
voiced menus are enabled

Regression introduced in dfa33c2 and made far worse by 759ef27

Change-Id: I202ec6c0d5825ff3a319b7d95d600b4ce06dd685

firmware/export/pcm_sink.h
@@ -49,7 +49,6 @@ struct pcm_sink {
     /* runtime states */
     unsigned long pending_freq;
     unsigned long configured_freq;
-    unsigned long pcm_is_ready;
 };
 
 enum pcm_sink_ids {

firmware/pcm.c
@@ -76,6 +76,9 @@
  *
  */
 
+/* 'true' when all stages of pcm initialization have completed */
+volatile static bool pcm_is_ready[PCM_SINK_NUM] SHAREDBSS_ATTR = { false };
+
 #ifdef USB_ENABLE_IAP
 extern struct pcm_sink iap_pcm_sink;
 #endif
@@ -86,7 +89,7 @@ static struct pcm_sink* sinks[PCM_SINK_NUM] = {
     [PCM_SINK_IAP] = &iap_pcm_sink,
 #endif
 };
-static enum pcm_sink_ids cur_sink = PCM_SINK_BUILTIN;
+static enum pcm_sink_ids cur_sink SHAREDBSS_ATTR = PCM_SINK_BUILTIN;
 
 /* The registered callback function to ask for more mp3 data */
 volatile pcm_play_callback_type
@@ -146,7 +149,7 @@ void pcm_play_stop_int(void)
 
 static void pcm_wait_for_init(void)
 {
-    while (!sinks[cur_sink]->pcm_is_ready)
+    while (!pcm_is_ready[cur_sink])
         sleep(0);
 }
 
@@ -256,9 +259,9 @@ void pcm_init(void)
 
     for(size_t i = 0; i < PCM_SINK_NUM; i += 1) {
         struct pcm_sink* sink = sinks[i];
+        pcm_is_ready[i] = false;
         sink->pending_freq = sink->caps.default_freq;
         sink->configured_freq = -1U;
-        sink->pcm_is_ready = false;
         sink->ops.init();
     }
 }
@@ -271,7 +274,7 @@ void pcm_postinit(void)
     for(size_t i = 0; i < PCM_SINK_NUM; i += 1) {
         struct pcm_sink* sink = sinks[i];
         sink->ops.postinit();
-        sink->pcm_is_ready = true;
+        pcm_is_ready[i] = true;
     }
 
     /* Ensure mixer is in a sane state */
@@ -280,7 +283,7 @@ void pcm_postinit(void)
 
 bool pcm_is_initialized(void)
 {
-    return sinks[cur_sink]->pcm_is_ready;
+    return pcm_is_ready[cur_sink];
 }
 
 enum pcm_sink_ids pcm_current_sink(void)