31dfd5da2e6d3522c6db027c99485314c87cc20e
Roman Artiukhin <bahusdrive@gmail.com>
2026-07-26 08:52:15 +0300

    playback: add Playlist Single Mode option
    Change-Id: I27c782ad246135fa6c98b52ed1491e86a7dfd456

--- apps/playback.c
+++ apps/playback.c
@@ -2685,6 +2685,9 @@ static bool single_mode_do_pause(int id3_hid)
         if (global_settings.single_mode == SINGLE_MODE_TRACK)
             return true;
 
+        if (global_settings.single_mode == SINGLE_MODE_PLAYLIST && skip_pending == TRACK_SKIP_AUTO_NEW_PLAYLIST)
+            return true;
+
         char *previous_tag = single_mode_get_id3_tag(id3_get(PLAYING_ID3));
         char *new_tag = single_mode_get_id3_tag(bufgetid3(id3_hid));
         return previous_tag == NULL ||
