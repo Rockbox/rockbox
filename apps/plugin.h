5448dd99a337cafe66901eae1d0b481f6c42b75c
Solomon Peachy <pizza@shaftnet.org>
2026-09-23 14:17:05 -0400

    fix red in c6abf3382a -- iriver h1xx/h3xx simulator
    Change-Id: I2b6f207aa8b349ca9cf12f8f9334848a65c4f48a

--- apps/plugin.h
+++ apps/plugin.h
@@ -1025,7 +1025,7 @@ struct plugin_api {
     void (*wheel_send_events)(bool send);
 #endif
 
-#if defined(IRIVER_H100_SERIES) || defined(IRIVER_H300_SERIES)
+#if !defined(SIMULATOR) && (defined(IRIVER_H100_SERIES) || defined(IRIVER_H300_SERIES))
     /* Routines for the iriver_flash -plugin. */
     bool (*detect_original_firmware)(void);
     bool (*detect_flashed_ramimage)(void);
